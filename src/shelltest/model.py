49a88d0a22b8d786609005501036e1da5ddec159
Emil Karlén <emil.karlen@fripost.org>
2014-12-30 11:59:42 +0100

    Version 1.0 of parsing of Plain Test Case

--- src/shelltest/model.py
+++ src/shelltest/model.py
@@ -1,5 +1,6 @@
 __author__ = 'emil'
 
+from shelltest import line_source
 from shelltest.line_source import LineSource
 from shelltest.phase import Phase
 
@@ -11,18 +12,25 @@ class InstructionApplication:
     Arguments/values are necessarily not correct.
     Whether or not depends on if they have been validated.
     """
-    pass
+    def __init__(self, source_line: line_source.Line):
+        self._source_line = source_line
+
+    def source_line(self) -> line_source.Line:
+        return self._source_line
 
 
 class InstructionApplicationSequence:
     """
     A sequence/list of InstructionApplication:s.
     """
-    def __init__(self, instruction_applications: list):
+    def __init__(self, instruction_applications: tuple):
         self._instruction_applications = instruction_applications
 
     def is_empty(self) -> bool:
-        return self._instruction_applications == []
+        return not self._instruction_applications
+
+    def instruction_applications(self):
+        return self._instruction_applications
 
 
 class Instruction:
@@ -44,19 +52,39 @@ class InstructionSet:
     def empty():
         return InstructionSet({})
 
+    def is_empty(self) -> bool:
+        return not self._instructions
+
     def lookup(self, instruction_name: str) -> Instruction:
         return self._instructions[instruction_name]
+
+
+class SourceError(Exception):
+    """
+    An exceptions related to a line in the test case.
+    """
+    def __init__(self,
+                 line: line_source.Line,
+                 message: str):
+        self._line = line
+        self._message = message
 
 
 class TestCase:
     """
     The result of parsing a test case file without encountering any errors.
     """
-    def instructions_for_anonymous_phase(self) -> InstructionApplicationSequence:
-        raise NotImplementedError()
+    def __init__(self, phase2instructions: dict):
+        """
+        :param phase2instructions dictionary str -> InstructionApplicationSequence
+        """
+        self._phase2instructions = phase2instructions
 
-    def instructions_for_phase(self, phase: Phase) -> InstructionApplicationSequence:
-        raise NotImplementedError()
+    def phases(self) -> frozenset:
+        return self._phase2instructions.keys()
+
+    def instructions_for_phase(self, phase_name: str) -> InstructionApplicationSequence:
+        return self._phase2instructions[phase_name]
 
 
 class PlainTestCaseParser:
