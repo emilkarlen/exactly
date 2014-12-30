bb5efb7e493c65968d71d9496f2bd90e038f50a6
Emil Karlén <emil.karlen@fripost.org>
2014-12-30 01:27:37 +0100

    Reorganized directory structure to faciliate testing.

---
+++ src/shelltest/line_source.py
@@ -0,0 +1,34 @@
+__author__ = 'emil'
+
+from collections import namedtuple
+
+
+Line = namedtuple('Line',
+                  ['line_number',
+                   'text'])
+
+
+class LineSource:
+    """
+    Iterable over a sequence of Line:s.
+
+    Line numbers start at 1.
+    """
+
+
+class _LineSourceForString(LineSource):
+    def __init__(self, contents: str):
+        self._lines = enumerate(contents.splitlines(),
+                                start=1)
+
+    def __iter__(self):
+        for n, s in self._lines:
+            yield Line(n, s)
+
+
+def new_for_string(contents: str) -> LineSource:
+    return _LineSourceForString(contents)
+
+
+def new_for_file(file_name: str) -> LineSource:
+    raise NotImplementedError()
