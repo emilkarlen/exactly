0b29550dcd74d00d78d086f38f36be14bec60c53
Emil Karlén <emil.karlen@fripost.org>
2015-01-01 16:51:29 +0100

    model.Document: execution of instructions

--- src/shelltest/model.py
+++ src/shelltest/model.py
@@ -17,6 +17,15 @@ class Instruction:
     def source_line(self) -> line_source.Line:
         return self._source_line
 
+    def execute(self, phase_name: str, global_environment, phase_environment):
+        """
+        Does whatever this instruction should do.
+        :param phase_name The phase in which this instruction is in.
+        :param global_environment An object passed to all instructions in the Document.
+        :param phase_environment An object passed to all instructions in the Phase.
+        """
+        raise NotImplementedError()
+
 
 class InstructionSequence:
     """
@@ -50,4 +59,18 @@ class Document:
     def instructions_for_phase(self, phase_name: str) -> InstructionSequence:
         return self._phase2instructions[phase_name]
 
-
+    def execute(self, global_environment, phases: iter):
+        """
+        Executes the given phases in the given order.
+        :param global_environment: An environment passed to every Instruction.
+        :param phases: [(phase_name: str, phase_environment)]
+        List of phases to execute, and the environment to use for each phase.
+        The phases are executed in the order they appear in the list (a phase may be executed more than
+        one time). Phases that have no counterpart in the Document are silently ignored.
+        :return:
+        """
+        for phase_name, phase_environment in phases:
+            if phase_name in self._phase2instructions:
+                instruction_sequence = self._phase2instructions[phase_name]
+                for instruction in instruction_sequence.instructions():
+                    instruction.execute(phase_name, global_environment, phase_environment)
