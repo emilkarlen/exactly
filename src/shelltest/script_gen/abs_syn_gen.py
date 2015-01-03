28c504ef55f674b4890c71abb4e57089f95c87ef
Emil Karlén <emil.karlen@fripost.org>
2015-01-03 19:57:08 +0100

    Changed non-apply phases to be implemented in python, and be executed from the shelltest executable

--- src/shelltest/script_gen/abs_syn_gen.py
+++ src/shelltest/script_gen/abs_syn_gen.py
@@ -3,7 +3,8 @@ Functionality for generating a Shell Script from a parsed Test Case file.
 """
 __author__ = 'emil'
 
-from shelltest.script_gen import stmt_gen
+from shelltest.script_gen import script_stmt_gen
+from shelltest.script_gen import py_cmd_gen
 from shelltest.phase import Phase
 from shelltest import phase
 
@@ -15,18 +16,34 @@ class PhaseEnvironmentForAnonymousPhase:
         self.home_dir = home_dir
 
 
-class PhaseEnvironmentForNamedPhase:
+class PhaseEnvironmentForShellScriptGeneration:
+    """
+    The phase-environment for phases that generate a shell script.
+    """
+
     def __init__(self):
         self.statements_generators = []
 
-    def extend_statements(self, statements_generators: list):
-        """
-        :param statements_generators: List of StatementsGeneratorForInstruction.
-        """
-        self.statements_generators.extend(statements_generators)
+    # def extend_statements(self, statements_generators: list):
+    # """
+    # :param statements_generators: List of StatementsGeneratorForInstruction.
+    #     """
+    #     self.statements_generators.extend(statements_generators)
 
-    def append_statement(self, statements_generator: stmt_gen.StatementsGeneratorForInstruction):
+    def append_statement(self, statements_generator: script_stmt_gen.StatementsGeneratorForInstruction):
         self.statements_generators.append(statements_generator)
+
+
+class PhaseEnvironmentForPythonCommands:
+    """
+    The phase-environment for phases that generate python commands.
+    """
+
+    def __init__(self):
+        self.commands = []
+
+    def append_command(self, command: py_cmd_gen.PythonCommand):
+        self.commands.append(command)
 
 
 class GlobalEnvironmentForNamedPhase:
@@ -45,8 +62,8 @@ class GlobalEnvironmentForNamedPhase:
 #
 # def __new__(cls,
 # phase: Phase,
-#                 file_generator: statement_generator.ScriptFileGenerator):
-#         return tuple.__new__(cls, (phase, file_generator))
+# file_generator: statement_generator.ScriptFileGenerator):
+# return tuple.__new__(cls, (phase, file_generator))
 #
 #     @property
 #     def phase(self) -> Phase:
@@ -80,18 +97,18 @@ class PhaseEnvironment:
 class TestCasePhase(tuple):
     def __new__(cls,
                 phase: Phase,
-                statements_generators: list):
+                phase_environment):
         """
-        :param statements_generators: List of StatementsGeneratorForInstruction.
+        :param elements: List of either StatementsGeneratorForInstruction or PythonCommand.
         """
-        return tuple.__new__(cls, (phase, statements_generators))
+        return tuple.__new__(cls, (phase, phase_environment))
 
     @property
     def phase(self) -> Phase:
         return self[0]
 
     @property
-    def statements_generators(self) -> list:
+    def phase_environment(self):
         return self[1]
 
 
@@ -126,7 +143,7 @@ def validate_and_generate(original_home_directory: str,
                           document: phase_instr_model.Document) -> TestCase:
     def test_case_from(global_env: GlobalEnvironmentForNamedPhase,
                        executed_phases: list) -> TestCase:
-        phase_list = [TestCasePhase(Phase(phase_name), phase_env.statements_generators)
+        phase_list = [TestCasePhase(Phase(phase_name), phase_env)
                       for phase_name, phase_env in executed_phases]
         return TestCase(global_env, phase_list)
 
@@ -137,9 +154,14 @@ def validate_and_generate(original_home_directory: str,
         document.execute(global_env, phases_to_execute)
         return phase_env
 
+    def phase_env_for(ph: Phase):
+        return PhaseEnvironmentForShellScriptGeneration() \
+            if ph == phase.APPLY \
+            else PhaseEnvironmentForPythonCommands()
+
     def execute_named_phases(settings_from_anonymous_phase: PhaseEnvironmentForAnonymousPhase):
         global_env = GlobalEnvironmentForNamedPhase(settings_from_anonymous_phase.home_dir)
-        phases_to_execute = [(ph.name, PhaseEnvironmentForNamedPhase())
+        phases_to_execute = [(ph.name, phase_env_for(ph))
                              for ph in phase.ALL_NAMED]
         document.execute(global_env, phases_to_execute)
         return global_env, phases_to_execute
@@ -147,3 +169,4 @@ def validate_and_generate(original_home_directory: str,
     settings = execute_anonymous_phase()
     global_env, executed_phases = execute_named_phases(settings)
     return test_case_from(global_env, executed_phases)
+
---
+++ src/shelltest/script_gen/config.py
@@ -0,0 +1,19 @@
+import pathlib
+
+__author__ = 'emil'
+
+
+class Configuration(tuple):
+
+    def __new__(cls,
+                home_dir: pathlib.Path,
+                test_root_dir: pathlib.Path):
+        return tuple.__new__(cls, (home_dir, test_root_dir))
+
+    @property
+    def home_dir(self) -> pathlib.Path:
+        return self[0]
+
+    @property
+    def test_root_dir(self) -> pathlib.Path:
+        return self[1]
