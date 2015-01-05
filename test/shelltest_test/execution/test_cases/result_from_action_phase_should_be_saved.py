d7cd4062b49179fe0ddbb1adb444f8ed5197df5e
Emil Karlén <emil.karlen@fripost.org>
2015-01-06 00:50:55 +0100

    Test of execution: Simplified test_result_from_act_phase_should_be_saved (removed testing of directories and environment variables)

--- test/shelltest_test/execution/test_cases/result_from_action_phase_should_be_saved.py
+++ test/shelltest_test/execution/test_cases/result_from_action_phase_should_be_saved.py
@@ -1,23 +1,11 @@
-import pathlib
-
 __author__ = 'emil'
-
-import os
-from shelltest.execution import execution
 
 from shelltest import phases
 from shelltest_test.execution.util.py_unit_test_case import UnitTestCaseForPyLanguage
 from shelltest.exec_abs_syn import abs_syn_gen, script_stmt_gen
 from shelltest.exec_abs_syn.config import Configuration
 from shelltest.phase_instr import line_source
-
 from shelltest_test.execution.util import utils
-
-HOME_DIR_HEADER = 'Home Dir'
-TEST_ROOT_DIR_HEADER = 'Test Root Dir'
-CURRENT_DIR_HEADER = 'Current Dir'
-
-EXIT_CODE = 5
 
 
 class TestCase(UnitTestCaseForPyLanguage):
@@ -25,10 +13,17 @@ class TestCase(UnitTestCaseForPyLanguage):
     Checks that output to stdout, stderr and the exit code are saved in the correct locations.
     """
 
+    TEXT_ON_STDOUT = 'on stdout'
+    TEXT_ON_STDERR = 'on stderr'
+    EXIT_CODE = 5
+
     def _phase_env_apply(self) -> abs_syn_gen.PhaseEnvironmentForScriptGeneration:
         return \
             abs_syn_gen.PhaseEnvironmentForScriptGeneration([
-                StatementsGeneratorThatPrintsPathsOnStdoutAndStderr()
+                StatementsGeneratorThatPrintsPathsOnStdoutAndStderr(self._next_line(),
+                                                                    self.EXIT_CODE,
+                                                                    self.TEXT_ON_STDOUT,
+                                                                    self.TEXT_ON_STDERR)
             ])
 
     def _phase_env_for_py_cmd_phase(self, phase: phases.Phase) -> abs_syn_gen.PhaseEnvironmentForPythonCommands:
@@ -37,90 +32,38 @@ class TestCase(UnitTestCaseForPyLanguage):
     def _assertions(self):
         utils.assert_is_file_with_contents(self.unittest_case,
                                            self.eds.result.exitcode_file,
-                                           str(EXIT_CODE))
+                                           str(self.EXIT_CODE))
         utils.assert_is_file_with_contents(self.unittest_case,
                                            self.eds.result.std.stdout_file,
-                                           expected_output_on('sys.stdout',
-                                                              self.test_case_execution.configuration))
+                                           self.TEXT_ON_STDOUT)
         utils.assert_is_file_with_contents(self.unittest_case,
                                            self.eds.result.std.stderr_file,
-                                           expected_output_on('sys.stderr',
-                                                              self.test_case_execution.configuration))
+                                           self.TEXT_ON_STDERR)
 
 
 class StatementsGeneratorThatPrintsPathsOnStdoutAndStderr(script_stmt_gen.StatementsGeneratorForInstruction):
-    def __init__(self):
-        super().__init__(line_source.Line(1, 'one'))
+    def __init__(self,
+                 source_line: line_source.Line,
+                 exit_code: int,
+                 text_on_stdout: str,
+                 text_on_stderr: str):
+        super().__init__(source_line)
+        self.__text_on_stdout = text_on_stdout
+        self.__text_on_stderr = text_on_stderr
+        self.__exit_code = exit_code
 
     def instruction_implementation(self,
                                    configuration: Configuration,
                                    script_language: script_stmt_gen.ScriptLanguage) -> list:
         statements = [
-                         'import sys',
-                         'import os',
-                         'import pathlib',
-                     ] + \
-                     self.print_on('sys.stdout', configuration) + \
-                     self.print_on('sys.stderr', configuration) + \
-                     ['sys.exit(%d)' % EXIT_CODE]
-
+            'import sys',
+            self.write_on('sys.stdout', self.__text_on_stdout),
+            self.write_on('sys.stderr', self.__text_on_stderr),
+            'sys.exit(%d)' % self.__exit_code
+        ]
         return script_language.raw_script_statements(statements)
 
-    def print_on(self,
-                 file_object: str,
-                 configuration: Configuration) -> list:
-        return [
-            self.write_line(file_object, file_object),
-            self.write_path_line(file_object, HOME_DIR_HEADER, configuration.home_dir),
-            self.write_path_line(file_object, TEST_ROOT_DIR_HEADER, configuration.test_root_dir),
-            self.write_prefix_and_expr(file_object, CURRENT_DIR_HEADER, 'str(pathlib.Path().resolve())'),
-            self.write_env_var(file_object, execution.ENV_VAR_HOME),
-            self.write_env_var(file_object, execution.ENV_VAR_TEST),
-        ]
-
     @staticmethod
-    def write_path_line(output_file: str,
-                        line_prefix: str,
-                        dir_path: pathlib.Path) -> str:
-        return StatementsGeneratorThatPrintsPathsOnStdoutAndStderr.write_prefix_and_expr(
-            output_file,
-            line_prefix,
-            '\'' + str(dir_path) + '\'')
-
-    @staticmethod
-    def write_env_var(output_file: str,
-                      var_name: str) -> str:
-        return StatementsGeneratorThatPrintsPathsOnStdoutAndStderr.write_prefix_and_expr(
-            output_file,
-            var_name,
-            'os.environ[\'%s\']' % var_name)
-
-    @staticmethod
-    def write_prefix_and_expr(output_file: str,
-                              prefix: str,
-                              expr: str) -> str:
-        return 'print(\'%-20s\' + %s, file=%s)' % (prefix, expr, output_file)
-
-    @staticmethod
-    def write_line(output_file: str,
-                   line: str) -> str:
-        return 'print(\'%s\', file=%s)' % (line, output_file)
-
-
-def expected_output_on(file_object: str,
-                       configuration: Configuration) -> str:
-    return os.linesep.join([
-        file_object,
-
-        output_with_header(HOME_DIR_HEADER, str(configuration.home_dir)),
-        output_with_header(TEST_ROOT_DIR_HEADER, str(configuration.test_root_dir)),
-        output_with_header(CURRENT_DIR_HEADER, str(configuration.test_root_dir)),
-
-        output_with_header(execution.ENV_VAR_HOME, str(configuration.home_dir)),
-        output_with_header(execution.ENV_VAR_TEST, str(configuration.test_root_dir)),
-        ''
-    ])
-
-
-def output_with_header(header: str, value: str) -> str:
-    return '%-20s%s' % (header, value)
+    def write_on(output_file: str,
+                 text: str) -> str:
+        return "%s.write('%s')" % (output_file, text)
