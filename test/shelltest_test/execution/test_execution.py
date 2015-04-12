540b5d440fff44948d246e0d72e38eaaaab8f103
Emil Karlén <emil.karlen@fripost.org>
2015-04-12 16:22:20 +0200

    Fixed broken tests by restoring CWD between tests.

--- test/shelltest_test/execution/test_execution.py
+++ test/shelltest_test/execution/test_execution.py
@@ -1,5 +1,7 @@
 __author__ = 'emil'
 
+import os
+
 import unittest
 
 from shelltest_test.execution.test_cases import execution_environment_variables
@@ -11,19 +13,25 @@ from shelltest_test.execution.test_cases import stdin_should_be_redirected_to_fi
 
 class Test(unittest.TestCase):
     def test_result_from_act_phase_should_be_saved(self):
-        result_from_action_phase_should_be_saved.TestCase(self).execute()
+        Test.run_with_preserved_cwd(result_from_action_phase_should_be_saved.TestCase(self))
 
     def test_environment_variables_should_be_accessible_in_all_phases(self):
-        execution_environment_variables.TestCase(self).execute()
+        Test.run_with_preserved_cwd(execution_environment_variables.TestCase(self))
 
     def test_cwd_at_start_of_each_phase_should_be_test_root_dir(self):
-        cwd_at_start_of_each_phase_should_be_test_root_dir.TestCase(self).execute()
+        Test.run_with_preserved_cwd(cwd_at_start_of_each_phase_should_be_test_root_dir.TestCase(self))
 
     def test_all_phases_should_be_executed_and_result_from_action_saved_etc(self):
-        all_phases_should_be_executed_and_result_from_action_saved_etc.TestCase(self).execute()
+        Test.run_with_preserved_cwd(all_phases_should_be_executed_and_result_from_action_saved_etc.TestCase(self))
 
     def test_stdin_should_be_redirected_to_file_if_set_in_phase_env(self):
-        stdin_should_be_redirected_to_file_if_set_in_phase_env.TestCase(self).execute()
+        Test.run_with_preserved_cwd(stdin_should_be_redirected_to_file_if_set_in_phase_env.TestCase(self))
+
+    @staticmethod
+    def run_with_preserved_cwd(tc):
+        cwd_before_test = os.getcwd()
+        tc.execute()
+        os.chdir(cwd_before_test)
 
 
 def suite():
