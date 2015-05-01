bad20d6dae60199321d5f9bec085ea81bb018428
Emil Karlén <emil.karlen@fripost.org>
2015-05-01 15:09:48 +0200

    Reverted invalid import statements

--- test/shelltest_test/execution/test_execution.py
+++ test/shelltest_test/execution/test_execution.py
@@ -2,27 +2,33 @@ __author__ = 'emil'
 
 import unittest
 
-from shelltest_test.execution import test_cases
+from shelltest_test.execution.test_cases import \
+    act_phase_should_be_transformed_to_script_file, \
+    result_from_action_phase_should_be_saved, \
+    execution_environment_variables, \
+    cwd_at_start_of_each_phase_should_be_test_root_dir, \
+    all_phases_should_be_executed_and_result_from_action_saved_etc, \
+    stdin_should_be_redirected_to_file_if_set_in_phase_env
 
 
 class Test(unittest.TestCase):
     def test_act_phase_should_be_transformed_to_script_file(self):
-        test_cases.act_phase_should_be_transformed_to_script_file.TestCase(self).execute()
+        act_phase_should_be_transformed_to_script_file.TestCase(self).execute()
 
     def test_result_from_act_phase_should_be_saved(self):
-        test_cases.result_from_action_phase_should_be_saved.TestCase(self).execute()
+        result_from_action_phase_should_be_saved.TestCase(self).execute()
 
     def test_environment_variables_should_be_accessible_in_all_phases(self):
-        test_cases.execution_environment_variables.TestCase(self).execute()
+        execution_environment_variables.TestCase(self).execute()
 
     def test_cwd_at_start_of_each_phase_should_be_test_root_dir(self):
-        test_cases.cwd_at_start_of_each_phase_should_be_test_root_dir.TestCase(self).execute()
+        cwd_at_start_of_each_phase_should_be_test_root_dir.TestCase(self).execute()
 
     def test_all_phases_should_be_executed_and_result_from_action_saved_etc(self):
-        test_cases.all_phases_should_be_executed_and_result_from_action_saved_etc.TestCase(self).execute()
+        all_phases_should_be_executed_and_result_from_action_saved_etc.TestCase(self).execute()
 
     def test_stdin_should_be_redirected_to_file_if_set_in_phase_env(self):
-        test_cases.stdin_should_be_redirected_to_file_if_set_in_phase_env.TestCase(self).execute()
+        stdin_should_be_redirected_to_file_if_set_in_phase_env.TestCase(self).execute()
 
 
 def suite():
--- test/shelltest_test/execution/test_suite.py
+++ test/shelltest_test/execution/test_suite.py
@@ -2,14 +2,15 @@ __author__ = 'emil'
 
 import unittest
 
-from shelltest_test import execution
+from shelltest_test.execution import test_execution_directory_structure, test_execution, test_execution_sequence
+
 
 
 def suite():
     ret_val = unittest.TestSuite()
-    ret_val.addTest(execution.test_execution_directory_structure.suite())
-    ret_val.addTest(execution.test_execution.suite())
-    ret_val.addTest(execution.test_execution_sequence.suite())
+    ret_val.addTest(test_execution_directory_structure.suite())
+    ret_val.addTest(test_execution.suite())
+    ret_val.addTest(test_execution_sequence.suite())
     return ret_val
 
 
