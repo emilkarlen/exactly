2a82c7a2551481bce059cf88e0e1ef4afd3feb73
Emil Karlén <emil.karlen@fripost.org>
2015-05-03 00:05:21 +0200

    New test of execution sequence: test_implementation_error_in_anonymous_phase

--- test/shelltest_test/execution/test_execution_sequence/anonymous_phase_errors.py
+++ test/shelltest_test/execution/test_execution_sequence/anonymous_phase_errors.py
@@ -5,6 +5,10 @@ from shelltest.exec_abs_syn.instructions import AnonymousPhaseInstruction, Phase
     SuccessOrHardError
 
 
+class ImplementationErrorTestException(Exception):
+    pass
+
+
 class AnonymousPhaseInstructionThatReturnsHardError(AnonymousPhaseInstruction):
     def __init__(self,
                  msg: str):
@@ -15,3 +19,16 @@ class AnonymousPhaseInstructionThatReturnsHardError(AnonymousPhaseInstruction):
                 global_environment,
                 phase_environment: PhaseEnvironmentForAnonymousPhase) -> SuccessOrHardError:
         return instruction_result.new_hard_error(self.__msg)
+
+
+class AnonymousPhaseInstructionWithImplementationError(AnonymousPhaseInstruction):
+    def __init__(self,
+                 exception_to_raise: Exception):
+        self.__exception_to_raise = exception_to_raise
+
+    def execute(self,
+                phase_name: str,
+                global_environment,
+                phase_environment: PhaseEnvironmentForAnonymousPhase) -> SuccessOrHardError:
+        raise self.__exception_to_raise
+
