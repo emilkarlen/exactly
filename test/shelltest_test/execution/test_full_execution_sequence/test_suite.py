e2fb4835483573ac1f04365f0bae6dad5904413d
Emil Karlén <emil.karlen@fripost.org>
2015-05-03 18:31:32 +0200

    New test: test_implementation_error_in_assert_phase

--- test/shelltest_test/execution/test_full_execution_sequence/test_suite.py
+++ test/shelltest_test/execution/test_full_execution_sequence/test_suite.py
@@ -167,6 +167,33 @@ class Test(unittest.TestCase):
              ],
             True).execute()
 
+    def test_implementation_error_in_assert_phase(self):
+        test_case = TestCaseThatRecordsExecutionWithExtraInstructionList() \
+            .add_assert(
+            instructions_with_errors.AssertPhaseInstructionWithImplementationError(
+                instructions_with_errors.ImplementationErrorTestException())) \
+            .add_assert_internal_recorder_of('expected to not be executed')
+        TestCaseThatRecordsExecution(
+            self,
+            test_case,
+            FullResultStatus.IMPLEMENTATION_ERROR,
+            ExpectedInstructionFailureForFailure.new_with_exception(
+                test_case.the_assert_phase_extra[0].source_line,
+                instructions_with_errors.ImplementationErrorTestException),
+            [phase_step.ANONYMOUS,
+             phase_step.SETUP,
+             phase_step.ACT__SCRIPT_GENERATION,
+             phase_step.ASSERT,
+             phase_step.CLEANUP
+             ],
+            [phase_step.SETUP,
+             phase_step.ACT__SCRIPT_GENERATION,
+             phase_step.ACT__SCRIPT_EXECUTION,
+             phase_step.ASSERT,
+             phase_step.CLEANUP
+             ],
+            True).execute()
+
 
 def suite():
     ret_val = unittest.TestSuite()
