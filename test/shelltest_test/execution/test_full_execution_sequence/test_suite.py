a15c3c2f492268614047a59a4f75a6c0f0caa1f3
Emil Karlén <emil@member.fsf.org>
2015-05-10 17:13:52 +0200

    Refactoring: renamed test files

--- test/shelltest_test/execution/test_full_execution_sequence/test_suite.py
+++ test/shelltest_test/execution/test_full_execution_sequence/test_suite.py
@@ -3,14 +3,14 @@ __author__ = 'emil'
 import unittest
 
 from shelltest_test.execution.test_full_execution_sequence import \
-    test_normal_execution_mode, \
-    test_nonnormal_execution_mode
+    test_execution_mode__normal, \
+    test_execution_mode__skipped
 
 
 def suite():
     ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(test_normal_execution_mode.Test))
-    ret_val.addTest(unittest.makeSuite(test_nonnormal_execution_mode.Test))
+    ret_val.addTest(unittest.makeSuite(test_execution_mode__normal.Test))
+    ret_val.addTest(unittest.makeSuite(test_execution_mode__skipped.Test))
     return ret_val
 
 
