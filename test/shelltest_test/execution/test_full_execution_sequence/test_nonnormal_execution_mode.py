8027032f30573ad6b7725fbbab89278ad9e11f8e
Emil Karlén <emil@member.fsf.org>
2015-05-10 17:10:30 +0200

    New test: test_execution_mode_skipped_but_failing_instruction

--- test/shelltest_test/execution/test_full_execution_sequence/test_nonnormal_execution_mode.py
+++ test/shelltest_test/execution/test_full_execution_sequence/test_nonnormal_execution_mode.py
@@ -1,5 +1,6 @@
 from shelltest.exec_abs_syn import instructions
 from shelltest.exec_abs_syn.success_or_hard_error_construction import new_success
+from shelltest_test.execution.test_full_execution_sequence import instructions_with_errors
 
 __author__ = 'emil'
 
@@ -11,7 +12,8 @@ from shelltest.execution.result import FullResultStatus
 from shelltest.execution import phase_step
 from shelltest_test.execution.test_full_execution_sequence.test_case_that_records_phase_execution import \
     TestCaseThatRecordsExecution
-from shelltest_test.execution.util.expected_instruction_failure import ExpectedInstructionFailureForNoFailure
+from shelltest_test.execution.util.expected_instruction_failure import ExpectedInstructionFailureForNoFailure, \
+    ExpectedInstructionFailureForFailure
 
 
 class AnonymousPhaseInstructionThatSetsExecutionMode(instructions.AnonymousPhaseInstruction):
@@ -36,6 +38,22 @@ class Test(unittest.TestCase):
             test_case,
             FullResultStatus.SKIPPED,
             ExpectedInstructionFailureForNoFailure(),
+            [phase_step.ANONYMOUS
+             ],
+            [],
+            False).execute()
+
+    def test_execution_mode_skipped_but_failing_instruction(self):
+        test_case = TestCaseThatRecordsExecutionWithExtraInstructionList() \
+            .add_anonymous(AnonymousPhaseInstructionThatSetsExecutionMode(instructions.ExecutionMode.SKIPPED)) \
+            .add_anonymous(instructions_with_errors.AnonymousPhaseInstructionThatReturnsHardError('hard error msg'))
+        TestCaseThatRecordsExecution(
+            self,
+            test_case,
+            FullResultStatus.HARD_ERROR,
+            ExpectedInstructionFailureForFailure.new_with_message(
+                test_case.the_anonymous_phase_extra[1].source_line,
+                'hard error msg'),
             [phase_step.ANONYMOUS
              ],
             [],
