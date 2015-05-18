7ac4fb9642b1e3070c5b8be1a857335e9e18aa1d
Emil Karlén <emil.karlen@fripost.org>
2015-05-18 21:46:15 +0200

    Test refactoring: Introduced TestCaseSetup with access to recorder for "internal" phase-steps.

--- test/shelltest_test/execution/test_full_execution/test_case_impl/test_environment_variables.py
+++ test/shelltest_test/execution/test_full_execution/test_case_impl/test_environment_variables.py
@@ -9,46 +9,38 @@ from shelltest.execution.phase_step import PhaseStep
 from shelltest.execution.result import FullResultStatus
 from shelltest_test.execution.test_full_execution.util.test_case_base import FullExecutionTestCaseBase
 from shelltest.execution import phase_step
-from shelltest_test.execution.util.instruction_that_do_and_return import TestCaseSetup, \
-    TestCaseGeneratorForTestCaseSetup, print_to_file__generate_script
+from shelltest_test.execution.util import instruction_that_record_and_return as instr_setup
+from shelltest_test.execution.util.instruction_that_do_and_return import TestCaseGeneratorForTestCaseSetup, \
+    print_to_file__generate_script
 from shelltest_test.execution.util.py_unit_test_case_with_file_output import ModulesAndStatements
 from shelltest_test.execution.util.python_code_gen import print_env_var_if_defined
 
 
-class _Recorder:
-    def __init__(self):
-        self.phaseStep2VariablesDict = dict()
-
-    def record_for_phase(self,
-                         phase_step: PhaseStep):
-        d = dict()
-        self.phaseStep2VariablesDict[phase_step] = d
-        for env_var in instructions.ALL_ENV_VARS:
-            if env_var in os.environ:
-                d[env_var] = os.environ[env_var]
-
-    def variables_for_phase_step(self,
-                                 phase_step: PhaseStep) -> dict:
-        return self.phaseStep2VariablesDict[phase_step]
+def env_vars_dict() -> dict:
+    ret_val = dict()
+    for env_var in instructions.ALL_ENV_VARS:
+        if env_var in os.environ:
+            ret_val[env_var] = os.environ[env_var]
+    return ret_val
 
 
-def _set_home_dir_to_parent__anonymous_phase(recorder: _Recorder,
+def _set_home_dir_to_parent__anonymous_phase(recorder: instr_setup.Recorder,
                                              phase_step: PhaseStep,
                                              phase_environment: instructions.PhaseEnvironmentForAnonymousPhase):
-    recorder.record_for_phase(phase_step)
+    recorder.set_phase_step_recording(phase_step, env_vars_dict())
     phase_environment.set_home_dir(phase_environment.home_dir_path.parent)
 
 
-def _action__without_eds(recorder: _Recorder,
+def _action__without_eds(recorder: instr_setup.Recorder,
                          phase_step: PhaseStep,
                          home_dir: pathlib.Path):
-    recorder.record_for_phase(phase_step)
+    recorder.set_phase_step_recording(phase_step, env_vars_dict())
 
 
-def _action__with_eds(recorder: _Recorder,
+def _action__with_eds(recorder: instr_setup.Recorder,
                       phase_step: PhaseStep,
                       global_environment: instructions.GlobalEnvironmentForNamedPhase):
-    recorder.record_for_phase(phase_step)
+    recorder.set_phase_step_recording(phase_step, env_vars_dict())
 
 
 class Test(FullExecutionTestCaseBase):
@@ -57,34 +49,29 @@ class Test(FullExecutionTestCaseBase):
                  dbg_do_not_delete_dir_structure=False):
         super().__init__(unittest_case,
                          dbg_do_not_delete_dir_structure)
-        self.recorder = _Recorder()
-        self.action_anonymous_phase = functools.partial(_set_home_dir_to_parent__anonymous_phase,
-                                                        self.recorder)
-        self.action__without_eds = functools.partial(_action__without_eds,
-                                                     self.recorder)
-        self.action__with_eds = functools.partial(_action__with_eds,
-                                                  self.recorder)
+        self.recorder = instr_setup.Recorder()
 
     def _test_case(self) -> abs_syn_gen.TestCase:
-        setup = TestCaseSetup(
-            validation_action__without_eds=self.action__without_eds,
-            anonymous_phase_action=self.action_anonymous_phase,
-            validation_action__with_eds=self.action__with_eds,
-            execution_action__with_eds=self.action__with_eds,
+        setup = instr_setup.TestCaseSetupWithRecorder(
+            validation_action__without_eds=_action__without_eds,
+            anonymous_phase_action=_set_home_dir_to_parent__anonymous_phase,
+            validation_action__with_eds=_action__with_eds,
+            execution_action__with_eds=_action__with_eds,
             execution__generate_script=script_for_print_environment_variables_to_file,
         )
-        return TestCaseGeneratorForTestCaseSetup(setup).test_case
+        plain_test_case_setup = setup.as_plain_test_case_setup(self.recorder)
+        return TestCaseGeneratorForTestCaseSetup(plain_test_case_setup).test_case
 
     def __assert_expected_internally_recorded_variables(self, expected):
         self._assert_expected_keys(expected,
-                                   self.recorder.phaseStep2VariablesDict,
+                                   self.recorder.phaseStep2recording,
                                    'phase-step')
         for ph in expected.keys():
             self._assert_expected_keys(expected[ph],
-                                       self.recorder.phaseStep2VariablesDict[ph],
+                                       self.recorder.phaseStep2recording[ph],
                                        'Environment variables for phase-step ' + str(ph))
             self._assert_expected_values(expected[ph],
-                                         self.recorder.phaseStep2VariablesDict[ph],
+                                         self.recorder.phaseStep2recording[ph],
                                          'Environment variable value for phase-step ' + str(ph))
 
     def _assertions(self):
@@ -97,7 +84,7 @@ class Test(FullExecutionTestCaseBase):
             instructions.ENV_VAR_TEST: str(self.eds.test_root_dir),
             instructions.ENV_VAR_TMP: str(self.eds.tmp_dir),
         }
-        expected = {
+        expected_recorded_internally = {
             phase_step.ANONYMOUS_EXECUTE: for_anonymous_phase,
             phase_step.SETUP_VALIDATE: for_pre_eds,
             phase_step.SETUP_EXECUTE: for_post_eds,
@@ -112,7 +99,7 @@ class Test(FullExecutionTestCaseBase):
             '%s=%s%s' % (instructions.ENV_VAR_TEST, str(self.eds.test_root_dir), os.linesep),
             '%s=%s%s' % (instructions.ENV_VAR_TMP, str(self.eds.tmp_dir), os.linesep),
         ])
-        self.__assert_expected_internally_recorded_variables(expected)
+        self.__assert_expected_internally_recorded_variables(expected_recorded_internally)
         self.__assert_expected_act_script_execution_recorded_variables(expected_act_output)
 
     def __assert_test_sanity(self):
