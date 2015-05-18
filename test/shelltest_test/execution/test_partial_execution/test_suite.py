cb3cdd1637f5001b26334652e4b72fc45bdbd0a4
Emil Karlén <emil.karlen@fripost.org>
2015-05-18 19:40:55 +0200

    Removed redundant test

--- test/shelltest_test/execution/test_partial_execution/test_suite.py
+++ test/shelltest_test/execution/test_partial_execution/test_suite.py
@@ -3,7 +3,7 @@ import unittest
 from shelltest_test.execution.test_partial_execution.test_cases import \
     all_phases_should_be_executed_and_result_from_action_saved_etc, \
     cwd_at_start_of_each_phase_should_be_test_root_dir, \
-    execution_environment_variables, act_phase_should_be_transformed_to_script_file, \
+    act_phase_should_be_transformed_to_script_file, \
     result_from_action_phase_should_be_saved, stdin_should_be_redirected_to_file_if_set_in_phase_env
 from shelltest_test.execution.util import py_unit_test_case
 
@@ -26,9 +26,6 @@ class Test(unittest.TestCase):
             self,
             stdin_should_be_redirected_to_file_if_set_in_phase_env.TestCaseDocument().test_case,
             stdin_should_be_redirected_to_file_if_set_in_phase_env.assertions)
-
-    def test_environment_variables_should_be_accessible_in_all_phases(self):
-        execution_environment_variables.TestCase(self).execute()
 
     def test_cwd_at_start_of_each_phase_should_be_test_root_dir(self):
         cwd_at_start_of_each_phase_should_be_test_root_dir.TestCase(self).execute()
