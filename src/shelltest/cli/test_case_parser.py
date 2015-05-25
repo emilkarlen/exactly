d119183d77edff8ecfe850ffdd037cdc79b83b84
Emil Karlén <emil.karlen@fripost.org>
2015-05-25 22:59:59 +0200

    First test of parse_command_line_and_execute_test_case passes

--- src/shelltest/cli/test_case_parser.py
+++ src/shelltest/cli/test_case_parser.py
@@ -1,4 +1,8 @@
+from shelltest import phases
+from shelltest.document import model
 from shelltest.document import parse
+from shelltest.document.parse import InstructionParser, SourceError, ParserForPhase
+from shelltest.general import line_source
 from shelltest.general.line_source import LineSource
 from shelltest.test_case import test_case_struct
 
@@ -9,15 +13,39 @@ class Parser:
         self.__ptc_parser = ptc_parser
 
     def apply(self,
-              line_source: LineSource) -> test_case_struct.TestCase:
-        raise NotImplementedError()
+              plain_test_case: LineSource) -> test_case_struct.TestCase:
+        document = self.__ptc_parser.apply(plain_test_case)
+        return test_case_struct.TestCase(
+            document.elements_for_phase_or_empty_if_phase_not_present(phases.ANONYMOUS.name),
+            document.elements_for_phase_or_empty_if_phase_not_present(phases.SETUP.name),
+            document.elements_for_phase_or_empty_if_phase_not_present(phases.ACT.name),
+            document.elements_for_phase_or_empty_if_phase_not_present(phases.ASSERT.name),
+            document.elements_for_phase_or_empty_if_phase_not_present(phases.CLEANUP.name),
+        )
 
 
 def new_parser() -> Parser:
-    raise NotImplementedError()
+    failing_parser = InstructionParserThatFailsUnconditionally()
+    anonymous_phase = parse.ParserForPhase(phases.ANONYMOUS.name,
+                                           failing_parser)
+    configuration = parse.PhaseAndInstructionsConfiguration(
+        anonymous_phase,
+        (
+            ParserForPhase(phases.SETUP.name,
+                           failing_parser),
+            ParserForPhase(phases.ACT.name,
+                           failing_parser),
+            ParserForPhase(phases.ASSERT.name,
+                           failing_parser),
+            ParserForPhase(phases.CLEANUP.name,
+                           failing_parser),
+        )
+    )
+    return Parser(parse.new_parser_for(configuration))
 
 
-# anonymous_phase_parser = None
-# anonymous_phase = parse.ParserForPhase(phases.ANONYMOUS.name,
-# anonymous_phase_parser)
-# configuration = parse.PhaseAndInstructionsConfiguration()
+class InstructionParserThatFailsUnconditionally(InstructionParser):
+    def apply(self, line: line_source.Line) -> model.PhaseContentElement:
+        raise SourceError(line, 'This parser fails unconditionally')
+
+
