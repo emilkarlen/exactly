49de61b7785693fa7fd8f2db379d08246363ca4e
Emil Karlén <emil@member.fsf.org>
2015-05-27 08:23:55 +0200

    Test of main script, and reorganization of test directories

--- test/shelltest_test/cli/execute-shellcheck.py
+++ test/shelltest_test/cli/execute-shellcheck.py
@@ -6,6 +6,6 @@ prj_root_dir_path = this_dir.parent.parent.parent
 src_dir_path = prj_root_dir_path / 'src'
 
 sys.path.insert(0, str(src_dir_path))
-from shelltest.cli import shellcheck
+import shellcheck
 
 shellcheck.main(sys.argv[1:])
---
+++ test/shelltest_test/shellcheck.py
@@ -0,0 +1,55 @@
+import os
+import pathlib
+import unittest
+import sys
+
+from shelltest.execution.result import FullResultStatus
+from shelltest_test.util.with_tmp_file import lines_content, run_subprocess_with_file_arg
+
+
+SRC_DIR_NAME = 'src'
+MAIN_PROGRAM_FILE_NAME = 'shellcheck.py'
+
+
+class TestCase(unittest.TestCase):
+    def test_successful_test_case(self):
+        # ARRANGE #
+        lines = [
+            '[setup]',
+            '# TODO: add instruction',
+            '[act]',
+            '# TODO: add instruction',
+            '[assert]',
+            '# TODO: add instruction',
+            '[cleanup]',
+            '# TODO: add instruction',
+        ]
+
+        cwd = pathlib.Path.cwd()
+        # print('# DEBUG: cwd: ' + str(cwd))
+        if not sys.executable:
+            self.fail('Cannot execute test since name of executable not found in sys.executable.')
+        shellcheck_src_path = cwd.parent.parent / SRC_DIR_NAME / ('%s' % MAIN_PROGRAM_FILE_NAME)
+        args_without_file = [sys.executable, str(shellcheck_src_path)]
+        # ACT #
+        result = run_subprocess_with_file_arg(args_without_file, lines_content(lines))
+        # ASSERT #
+        self.assertEqual(FullResultStatus.PASS.value,
+                         result.exitcode,
+                         'Exit code')
+        self.assertEqual(FullResultStatus.PASS.name + os.linesep,
+                         result.stdout,
+                         'Content on stdout')
+        self.assertEqual('',
+                         result.stderr,
+                         'Content on stderr')
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(unittest.makeSuite(TestCase))
+    return ret_val
+
+
+if __name__ == '__main__':
+    unittest.main()
