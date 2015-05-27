05713a9cc5ac4f1c60fa782fddd9760a681dc4cf
Emil Karlén <emil@member.fsf.org>
2015-05-27 05:46:20 +0200

    Source for executable, and test-executor of that program

---
+++ test/shelltest_test/cli/execute-shellcheck.py
@@ -0,0 +1,11 @@
+import pathlib
+import sys
+
+this_dir = pathlib.Path(sys.path[0])
+prj_root_dir_path = this_dir.parent.parent.parent
+src_dir_path = prj_root_dir_path / 'src'
+
+sys.path.insert(0, str(src_dir_path))
+from shelltest.cli import shellcheck
+
+shellcheck.main(sys.argv[1:])
