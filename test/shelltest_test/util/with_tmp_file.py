86effb5906d6bc3e0de774b263180a06dda8ef80
Emil Karlén <emil.karlen@fripost.org>
2015-05-27 05:27:13 +0200

    Functions for capturing the output from a sub process (stdout, stderr, exitcode)

--- test/shelltest_test/util/with_tmp_file.py
+++ test/shelltest_test/util/with_tmp_file.py
@@ -2,11 +2,17 @@ from contextlib import contextmanager
 import os
 import pathlib
 import tempfile
+import subprocess
+
+
+def lines_content(lines: list) -> str:
+    return os.linesep.join(lines) + os.linesep
 
 
 @contextmanager
 def tmp_file_containing(contents: str,
-                        suffix: str='') -> pathlib.Path:
+                        suffix: str='',
+                        dir=None) -> pathlib.Path:
     """
     Returns a context manager (used by with tmp_file(...) as file_path) ...
     The context manager takes care of deleting the file.
@@ -19,6 +25,7 @@ def tmp_file_containing(contents: str,
     try:
         fd, absolute_file_path = tempfile.mkstemp(prefix='shelltest-test-',
                                                   suffix=suffix,
+                                                  dir=dir,
                                                   text=True)
         fo = os.fdopen(fd, "w+")
         fo.write(contents)
@@ -35,6 +42,67 @@ def tmp_file_containing_lines(content_lines: list,
     """
     Short cut to tmp_file_containing combined with giving the contents as a string of lines.
     """
-    contents = os.linesep.join(content_lines) + os.linesep
-    return tmp_file_containing(contents,
+    return tmp_file_containing(lines_content(content_lines),
                                suffix=suffix)
+
+
+class SubProcessResult(tuple):
+    def __new__(cls,
+                exitcode: int,
+                stdout: str,
+                stderr: str):
+        return tuple.__new__(cls, (exitcode, stdout, stderr))
+
+    @property
+    def exitcode(self) -> int:
+        return self[0]
+
+    @property
+    def stdout(self) -> str:
+        return self[1]
+
+    @property
+    def stderr(self) -> str:
+        return self[2]
+
+
+stdout_file_name = 'stdout.txt'
+stderr_file_name = 'stderr.txt'
+
+
+def capture_subprocess(cmd_and_args: list,
+                       tmp_dir: pathlib.Path,
+                       cwd: str=None,
+                       stdin=subprocess.DEVNULL) -> SubProcessResult:
+    stdout_path = tmp_dir / stdout_file_name
+    stderr_path = tmp_dir / stderr_file_name
+    with open(str(stdout_path), 'w') as f_stdout:
+        with open(str(stderr_path), 'w') as f_stderr:
+            exitcode = subprocess.call(cmd_and_args,
+                                       cwd=cwd,
+                                       stdin=stdin,
+                                       stdout=f_stdout,
+                                       stderr=f_stderr)
+    return SubProcessResult(exitcode,
+                            _contents_of_file(stdout_path),
+                            _contents_of_file(stderr_path))
+
+
+def _contents_of_file(path: pathlib.Path) -> str:
+    with open(str(path)) as f:
+        return f.read()
+
+
+def capture_subprocess_in_tmp_dir(cmd_and_args: list) -> SubProcessResult:
+    with tempfile.TemporaryDirectory(prefix='shelltest-test-') as tmp_dir:
+        return capture_subprocess(cmd_and_args,
+                                  pathlib.Path(tmp_dir))
+
+
+def run_sub_process_with_file_arg(cmd_and_args_except_file_arg: list,
+                                  file_contents: str) -> SubProcessResult:
+    with tempfile.TemporaryDirectory(prefix='shelltest-test-') as tmp_dir_name:
+        with tmp_file_containing(file_contents, dir=tmp_dir_name) as file_path:
+            cmd_and_args = cmd_and_args_except_file_arg + [str(file_path)]
+            return capture_subprocess(cmd_and_args,
+                                      pathlib.Path(tmp_dir_name))
