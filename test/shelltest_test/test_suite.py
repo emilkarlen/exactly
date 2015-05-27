2b2f3bc88dd193749e8c5655cf5f9b064da25ca6
Emil Karlén <emil@member.fsf.org>
2015-05-27 08:52:56 +0200

    Test of main script, and reorganization of test directories

--- test/shelltest_test/test_suite.py
+++ test/shelltest_test/test_suite.py
@@ -5,7 +5,6 @@ from shelltest_test.document import test_suite as document_test
 from shelltest_test.test_case import test_suite as test_case_test
 from shelltest_test.execution import test_suite as execution_test
 from shelltest_test.cli import test_suite as cli_test
-from shelltest_test import shellcheck as shellcheck_test
 
 
 def suite():
@@ -15,7 +14,6 @@ def suite():
     ret_val.addTest(test_case_test.suite())
     ret_val.addTest(execution_test.suite())
     ret_val.addTest(cli_test.suite())
-    ret_val.addTest(shellcheck_test.suite())
     return ret_val
 
 
