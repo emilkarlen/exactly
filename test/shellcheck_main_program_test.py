4f455fc473c440df4e115f42cedaae12b3729bce
Emil Karlén <emil@member.fsf.org>
2015-06-04 07:06:54 +0200

    Refactoring: test: Extracted method to execute shellcheck in sub process

--- test/shellcheck_main_program_test.py
+++ test/shellcheck_main_program_test.py
@@ -4,7 +4,7 @@ import unittest
 import sys
 
 from shellcheck_lib.execution.result import FullResultStatus
-from shellcheck_lib_test.util.with_tmp_file import lines_content, run_subprocess_with_file_arg
+from shellcheck_lib_test.util.with_tmp_file import lines_content, run_subprocess_with_file_arg, SubProcessResult
 
 
 SRC_DIR_NAME = 'src'
@@ -16,6 +16,29 @@ def shellcheck_src_path(dir_of_this_file: pathlib.Path) -> pathlib.Path:
 
 
 class TestCase(unittest.TestCase):
+    # def test_empty_test_case_invoked_with_no_cli_flags(self):
+    # # ARRANGE #
+    # test_case_source = ''
+    #
+    # cwd = pathlib.Path.cwd()
+    #     # print('# DEBUG: cwd: ' + str(cwd))
+    #     if not sys.executable:
+    #         self.fail('Cannot execute test since name of executable not found in sys.executable.')
+    #     shellcheck_path = shellcheck_src_path(cwd)
+    #     args_without_file = [sys.executable, str(shellcheck_path)]
+    #     # ACT #
+    #     result = run_subprocess_with_file_arg(args_without_file, test_case_source)
+    #     # ASSERT #
+    #     self.assertEqual(FullResultStatus.PASS.value,
+    #                      result.exitcode,
+    #                      'Exit code')
+    #     self.assertEqual(FullResultStatus.PASS.name + os.linesep,
+    #                      result.stdout,
+    #                      'Content on stdout')
+    #     self.assertEqual('',
+    #                      result.stderr,
+    #                      'Content on stderr')
+
     def test_successful_test_case(self):
         # ARRANGE #
         lines = [
@@ -29,14 +52,8 @@ class TestCase(unittest.TestCase):
             '# TODO: add instruction',
         ]
 
-        cwd = pathlib.Path.cwd()
-        # print('# DEBUG: cwd: ' + str(cwd))
-        if not sys.executable:
-            self.fail('Cannot execute test since name of executable not found in sys.executable.')
-        shellcheck_path = shellcheck_src_path(cwd)
-        args_without_file = [sys.executable, str(shellcheck_path)]
         # ACT #
-        result = run_subprocess_with_file_arg(args_without_file, lines_content(lines))
+        result = self._run_shellcheck_in_sub_process(test_case_source=lines_content(lines))
         # ASSERT #
         self.assertEqual(FullResultStatus.PASS.value,
                          result.exitcode,
@@ -47,6 +64,25 @@ class TestCase(unittest.TestCase):
         self.assertEqual('',
                          result.stderr,
                          'Content on stderr')
+
+    def _run_shellcheck_in_sub_process(self,
+                                       test_case_source: str,
+                                       flags: list=()) -> SubProcessResult:
+        return run_shellcheck_in_sub_process(self,
+                                             test_case_source=test_case_source,
+                                             flags=flags)
+
+
+def run_shellcheck_in_sub_process(puc: unittest.TestCase,
+                                  test_case_source: str,
+                                  flags: list=()) -> SubProcessResult:
+    cwd = pathlib.Path.cwd()
+    # print('# DEBUG: cwd: ' + str(cwd))
+    if not sys.executable:
+        puc.fail('Cannot execute test since the name of the Python 3 interpreter is not found in sys.executable.')
+    shellcheck_path = shellcheck_src_path(cwd)
+    args_without_file = [sys.executable, str(shellcheck_path)] + list(flags)
+    return run_subprocess_with_file_arg(args_without_file, test_case_source)
 
 
 def suite():
