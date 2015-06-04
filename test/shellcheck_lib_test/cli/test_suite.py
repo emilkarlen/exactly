02bd89698acc2444c4669d45de784caca4806402
Emil Karlén <emil@member.fsf.org>
2015-06-04 06:26:23 +0200

    Argument parsers sets "initial home directory" to directory containing test case file.

--- test/shellcheck_lib_test/cli/test_suite.py
+++ test/shellcheck_lib_test/cli/test_suite.py
@@ -1,10 +1,11 @@
 import unittest
 
-from shellcheck_lib_test.cli import parse_command_line_and_execute_test_case
+from shellcheck_lib_test.cli import parse_command_line_and_execute_test_case, argument_parsing
 
 
 def suite():
     ret_val = unittest.TestSuite()
+    ret_val.addTest(argument_parsing.suite())
     ret_val.addTest(parse_command_line_and_execute_test_case.suite())
     return ret_val
 
