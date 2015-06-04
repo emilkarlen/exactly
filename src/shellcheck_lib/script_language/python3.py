d9b738aafb2259f0121d32bba66eafa752c04c5e
Emil Karlén <emil@member.fsf.org>
2015-06-04 06:00:54 +0200

    Script Language/py 3: Resolving name of Python 3 interpreter from sys.executable

--- src/shellcheck_lib/script_language/python3.py
+++ src/shellcheck_lib/script_language/python3.py
@@ -1,3 +1,5 @@
+import sys
+
 from shellcheck_lib.script_language import act_script_management
 from shellcheck_lib.script_language.act_script_management import ScriptFileManager
 
@@ -15,11 +17,14 @@ class Python3ScriptFileManager(ScriptFileManager):
     This implementation assumes that the Python 3 interpreter (python3) is in the path.
     """
 
+    def __init__(self):
+        self.__interpreter = sys.executable
+
     def base_name_from_stem(self, stem: str) -> str:
         return stem + '.py'
 
     def command_and_args_for_executing_script_file(self, script_file_name: str) -> list:
-        return ['python3', script_file_name]
+        return [self.__interpreter, script_file_name]
 
 
 def new_script_language_setup() -> act_script_management.ScriptLanguageSetup:
