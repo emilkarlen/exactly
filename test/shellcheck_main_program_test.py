44a2581b49b3dc79b723c7d32e413ab908f638d2
Emil Karlén <emil.karlen@fripost.org>
2015-06-04 07:37:05 +0200

    New test of main program: test_empty_test_case__invoked_with_no_cli_flags

--- test/shellcheck_main_program_test.py
+++ test/shellcheck_main_program_test.py
@@ -17,28 +17,14 @@ def shellcheck_src_path(dir_of_this_file: pathlib.Path) -> pathlib.Path:
 
 
 class TestCase(unittest.TestCase):
-    # def test_empty_test_case_invoked_with_no_cli_flags(self):
-    # # ARRANGE #
-    # test_case_source = ''
-    #
-    # cwd = pathlib.Path.cwd()
-    # # print('# DEBUG: cwd: ' + str(cwd))
-    # if not sys.executable:
-    # self.fail('Cannot execute test since name of executable not found in sys.executable.')
-    # shellcheck_path = shellcheck_src_path(cwd)
-    #     args_without_file = [sys.executable, str(shellcheck_path)]
-    #     # ACT #
-    #     result = run_subprocess_with_file_arg(args_without_file, test_case_source)
-    #     # ASSERT #
-    #     self.assertEqual(FullResultStatus.PASS.value,
-    #                      result.exitcode,
-    #                      'Exit code')
-    #     self.assertEqual(FullResultStatus.PASS.name + os.linesep,
-    #                      result.stdout,
-    #                      'Content on stdout')
-    #     self.assertEqual('',
-    #                      result.stderr,
-    #                      'Content on stderr')
+    def test_empty_test_case__invoked_with_no_cli_flags(self):
+        # ARRANGE #
+        test_case_source = ''
+        # ACT #
+        actual = self._run_shellcheck_in_sub_process(test_case_source)
+        # ASSERT #
+        SUCCESSFUL_RESULT.assert_matches(self,
+                                         actual)
 
     def test_test_case_with_only_phase_headers(self):
         # ARRANGE #
