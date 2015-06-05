3e5b7d8d27156b21a4c3c4f7723a7cfae28b66e2
Emil Karlén <emil.karlen@fripost.org>
2015-06-05 08:03:52 +0200

    Refactoring: main program: Moved global code to main method

--- src/shellcheck.py
+++ src/shellcheck.py
@@ -4,13 +4,26 @@ Main program for shellcheck
 import sys
 
 from shellcheck_lib.cli import test_case_parser
-
 from shellcheck_lib.cli import argument_parsing
 from shellcheck_lib.execution import full_execution
 from shellcheck_lib.script_language.act_script_management import ScriptLanguageSetup
 from shellcheck_lib.script_language import python3
 from shellcheck_lib.general import line_source
 from shellcheck_lib.test_case import test_case_struct
+
+
+def main():
+    the_cl_arguments = argument_parsing.parse(sys.argv[1:])
+    test_case = parse_test_case_source(the_cl_arguments)
+    script_language_setup = resolve_script_language(the_cl_arguments)
+    full_result = full_execution.execute(script_language_setup,
+                                         test_case,
+                                         the_cl_arguments.initial_home_dir_path,
+                                         the_cl_arguments.execution_directory_root_name_prefix,
+                                         the_cl_arguments.is_keep_execution_directory_root)
+    print_output_to_stdout(the_cl_arguments,
+                           full_result)
+    sys.exit(full_result.status.value)
 
 
 def resolve_script_language(cl_arguments: argument_parsing.Result) -> ScriptLanguageSetup:
@@ -31,14 +44,4 @@ def print_output_to_stdout(cl_arguments: argument_parsing.Result,
         print(str(the_full_result.execution_directory_structure.root_dir))
 
 
-the_cl_arguments = argument_parsing.parse(sys.argv[1:])
-test_case = parse_test_case_source(the_cl_arguments)
-script_language_setup = resolve_script_language(the_cl_arguments)
-full_result = full_execution.execute(script_language_setup,
-                                     test_case,
-                                     the_cl_arguments.initial_home_dir_path,
-                                     the_cl_arguments.execution_directory_root_name_prefix,
-                                     the_cl_arguments.is_keep_execution_directory_root)
-print_output_to_stdout(the_cl_arguments,
-                       full_result)
-sys.exit(full_result.status.value)
+main()
