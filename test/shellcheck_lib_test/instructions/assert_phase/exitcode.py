a38f29906964d5a4a4bfbc08c4d264bf5617586e
Emil Karlén <emil.karlen@fripost.org>
2015-06-14 23:10:20 +0200

    Instruction: assert/exitcode: test_that__when__actual_value_is_as_not_expected__then__fail_is_returned

--- test/shellcheck_lib_test/instructions/assert_phase/exitcode.py
+++ test/shellcheck_lib_test/instructions/assert_phase/exitcode.py
@@ -54,6 +54,19 @@ class TestParseAndExecute(unittest.TestCase):
                              main_actual.status,
                              'The assertion is expected to PASS')
 
+    def test_that__when__actual_value_is_as_not_expected__then__fail_is_returned(self):
+        parser = exitcode.Parser()
+        instruction = parser.apply('72')
+        with utils.act_phase_result(exitcode=0) as post_eds_environment:
+            validation_result = instruction.validate(post_eds_environment)
+            self.assertTrue(validation_result.is_success,
+                            'The assertion/validation is expected to succeed')
+            main_actual = instruction.main(post_eds_environment,
+                                           i.PhaseEnvironmentForInternalCommands())
+            self.assertEqual(i.PassOrFailOrHardErrorEnum.FAIL,
+                             main_actual.status,
+                             'The assertion is expected to FAIL')
+
 
 def suite():
     ret_val = unittest.TestSuite()
