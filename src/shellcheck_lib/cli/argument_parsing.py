146cfa7819884a26807f045b26b6890121d35e6f
Emil Karlén <emil.karlen@fripost.org>
2015-06-15 16:54:15 +0200

    executable/help-system: Refactoring: Moved settings from argument-parsing to new module

--- src/shellcheck_lib/cli/argument_parsing.py
+++ src/shellcheck_lib/cli/argument_parsing.py
@@ -1,59 +1,13 @@
-import enum
 import pathlib
-
 import argparse
+
+from shellcheck_lib.cli.main_program import TestCaseExecutionSettings, Output
 
 
 INTERPRETER_FOR_TEST = 'test-language'
 
 
-class Output(enum.Enum):
-    STATUS_CODE = 1
-    EXECUTION_DIRECTORY_STRUCTURE_ROOT = 2
-    ACT_PHASE_OUTPUT = 3
-
-
-class Result:
-    def __init__(self,
-                 file_path: pathlib.Path,
-                 initial_home_dir_path: pathlib.Path,
-                 output: Output,
-                 is_keep_execution_directory_root: bool=False,
-                 execution_directory_root_name_prefix: str='shellcheck-',
-                 interpreter: str=None):
-        self.__file_path = file_path
-        self.__initial_home_dir_path = initial_home_dir_path
-        self.__output = output
-        self.__is_keep_execution_directory_root = is_keep_execution_directory_root
-        self.__execution_directory_root_name_prefix = execution_directory_root_name_prefix
-        self.__interpreter = interpreter
-
-    @property
-    def file_path(self) -> pathlib.Path:
-        return self.__file_path
-
-    @property
-    def initial_home_dir_path(self) -> pathlib.Path:
-        return self.__initial_home_dir_path
-
-    @property
-    def output(self) -> Output:
-        return self.__output
-
-    @property
-    def is_keep_execution_directory_root(self) -> bool:
-        return self.__is_keep_execution_directory_root
-
-    @property
-    def execution_directory_root_name_prefix(self) -> str:
-        return self.__execution_directory_root_name_prefix
-
-    @property
-    def interpreter(self) -> str:
-        return self.__interpreter
-
-
-def parse(argv: list) -> Result:
+def parse(argv: list) -> TestCaseExecutionSettings:
     output = Output.STATUS_CODE
     is_keep_execution_directory_root = False
     namespace = _new_argument_parser().parse_args(argv)
@@ -62,10 +16,10 @@ def parse(argv: list) -> Result:
     elif namespace.keep:
         output = Output.EXECUTION_DIRECTORY_STRUCTURE_ROOT
         is_keep_execution_directory_root = True
-    return Result(pathlib.Path(namespace.file),
-                  pathlib.Path(namespace.file).parent.resolve(),
-                  output=output,
-                  is_keep_execution_directory_root=is_keep_execution_directory_root)
+    return TestCaseExecutionSettings(pathlib.Path(namespace.file),
+                                     pathlib.Path(namespace.file).parent.resolve(),
+                                     output=output,
+                                     is_keep_execution_directory_root=is_keep_execution_directory_root)
 
 
 def _new_argument_parser() -> argparse.ArgumentParser:
