7e13bde409c88c7d8b8e5236697cc8e9160cac55
Emil Karlén <emil.karlen@fripost.org>
2015-06-15 23:49:54 +0200

    executable/help-system: Improved description of instruction (exitcode)

--- src/shellcheck.py
+++ src/shellcheck.py
@@ -5,13 +5,15 @@ import sys
 
 from shellcheck_lib.cli import main_program
 from shellcheck_lib.cli.main_program_default import MainProgram
-from shellcheck_lib.cli.instruction_setup import InstructionsSetup
-
+from shellcheck_lib.cli.instruction_setup import InstructionsSetup, SingleInstructionSetup, Description
+from shellcheck_lib.instructions.assert_phase import exitcode as exitcode_instruction
 
 instructions_setup = InstructionsSetup(
     {},
     {},
-    {},
+    {'exitcode': SingleInstructionSetup(
+        exitcode_instruction.Parser(),
+        Description('Test numerical exitcode'))},
     {})
 
 program = MainProgram(main_program.StdOutputFiles(sys.stdout, sys.stderr),
