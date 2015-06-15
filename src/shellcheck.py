9206a051f15a5a271aee32194c478e81a72d749b
Emil Karlén <emil@member.fsf.org>
2015-06-16 00:00:28 +0200

    executable/help-system: Code layout

--- src/shellcheck.py
+++ src/shellcheck.py
@@ -8,7 +8,8 @@ from shellcheck_lib.cli import main_program
 from shellcheck_lib.cli.default.default_main_program import MainProgram
 from shellcheck_lib.cli.default import default_instructions_setup
 
-program = MainProgram(main_program.StdOutputFiles(sys.stdout, sys.stderr),
+program = MainProgram(main_program.StdOutputFiles(sys.stdout,
+                                                  sys.stderr),
                       default_instructions_setup.instructions_setup)
 exit_status = program.execute(sys.argv[1:])
 sys.exit(exit_status)
