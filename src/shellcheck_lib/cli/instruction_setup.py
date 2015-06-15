e18d1ad854db2446884401957081f40277680a30
Emil Karlén <emil.karlen@fripost.org>
2015-06-15 22:13:41 +0200

    executable/help-system: Structures for defining instructions (not used yet)

---
+++ src/shellcheck_lib/cli/instruction_setup.py
@@ -0,0 +1,67 @@
+from shellcheck_lib.document import model
+from shellcheck_lib.instruction_parsing.instruction_parser_for_single_phase import SingleInstructionParser
+
+
+class Description:
+    def __init__(self,
+                 single_line_description: str):
+        self.__single_line_description = single_line_description
+
+    @property
+    def single_line_description(self) -> str:
+        return self.__single_line_description
+
+
+class SingleInstructionSetup(SingleInstructionParser):
+    def __init__(self,
+                 parser: SingleInstructionParser,
+                 description: Description):
+        self._parser = parser
+        self._description = description
+
+    @property
+    def description(self) -> Description:
+        return self._description
+
+    def apply(self, instruction_argument: str) -> model.Instruction:
+        return self._parser.apply(instruction_argument)
+
+
+class InstructionsSetup:
+    def __init__(self,
+                 config_instruction_set: dict,
+                 setup_instruction_set: dict,
+                 act_instruction_set: dict,
+                 assert_instruction_set: dict,
+                 cleanup_instruction_set: dict):
+        """
+        Each dictionary is a mapping: instruction-name -> SingleInstructionSetup.
+
+        Each SingleInstructionSetup should parse and construct an instruction for
+         the correct phase (of course). I.e., sub classes of Instruction.
+        """
+        self._config_instruction_set = config_instruction_set
+        self._setup_instruction_set = setup_instruction_set
+        self._act_instruction_set = act_instruction_set
+        self._assert_instruction_set = assert_instruction_set
+        self._cleanup_instruction_set = cleanup_instruction_set
+
+    @property
+    def config_instruction_set(self) -> dict:
+        return self._config_instruction_set
+
+    @property
+    def setup_instruction_set(self) -> dict:
+        return self._setup_instruction_set
+
+    @property
+    def act_instruction_set(self) -> dict:
+        return self._act_instruction_set
+
+    @property
+    def assert_instruction_set(self) -> dict:
+        return self._assert_instruction_set
+
+    @property
+    def cleanup_instruction_set(self) -> dict:
+        return self._cleanup_instruction_set
