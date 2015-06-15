1f88e259afd9707d3345d96f9fe1a48090d23e11
Emil Karlén <emil.karlen@fripost.org>
2015-06-15 20:02:51 +0200

    executable/help-system: Refactoring: pulled members up to MainProgram base class

--- src/shellcheck_lib/cli/main_program_default.py
+++ src/shellcheck_lib/cli/main_program_default.py
@@ -7,50 +7,15 @@ from shellcheck_lib.execution import full_execution
 from shellcheck_lib.script_language.act_script_management import ScriptLanguageSetup
 from shellcheck_lib.script_language import python3
 from shellcheck_lib.general import line_source
-
 from shellcheck_lib.test_case import test_case_struct
 from shellcheck_lib.cli.execution_mode.test_case.argument_parsing import INTERPRETER_FOR_TEST
-from shellcheck_lib.cli.execution_mode.test_case import argument_parsing, test_case_parser
-from shellcheck_lib.cli import argument_parsing_utils
+from shellcheck_lib.cli.execution_mode.test_case import test_case_parser
 from shellcheck_lib.cli.execution_mode.test_case.settings import Output, TestCaseExecutionSettings
 from shellcheck_lib.cli.execution_mode.help.settings import HelpSettings
-from shellcheck_lib.cli.execution_mode.help.argument_parsing import parse as parse_help
 from . import main_program
 
 
-HELP_COMMAND = 'help'
-
-
 class MainProgram(main_program.MainProgram):
-    def __init__(self,
-                 stdout_file=sys.stdout,
-                 stderr_file=sys.stderr):
-        self._stdout_file = stdout_file
-        self._stderr_file = stderr_file
-
-    def execute(self, command_line_arguments: list) -> int:
-        if len(command_line_arguments) > 0 and command_line_arguments[0] == HELP_COMMAND:
-            return self._parse_and_execute_help(command_line_arguments[1:])
-        return self._parse_and_execute_test_case(command_line_arguments)
-
-    def _parse_and_execute_test_case(self, command_line_arguments: list) -> int:
-        try:
-            test_case_execution_settings = argument_parsing.parse(command_line_arguments)
-            return self.execute_test_case(test_case_execution_settings)
-        except argument_parsing_utils.ArgumentParsingError as ex:
-            self._stderr_file.write(ex.error_message)
-            self._stderr_file.write(os.linesep)
-            return main_program.EXIT_INVALID_USAGE
-
-    def _parse_and_execute_help(self, help_command_arguments: list) -> int:
-        try:
-            settings = parse_help(help_command_arguments)
-            return self.execute_help(settings)
-        except argument_parsing_utils.ArgumentParsingError as ex:
-            self._stderr_file.write(ex.error_message)
-            self._stderr_file.write(os.linesep)
-            return main_program.EXIT_INVALID_USAGE
-
     def execute_test_case(self, settings: TestCaseExecutionSettings) -> int:
         test_case = parse_test_case_source(settings)
         script_language_setup = resolve_script_language(settings)
