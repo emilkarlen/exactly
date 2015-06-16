3031051f04b27bf1c0aaea70c7e6e9dd212417d3
Emil Karlén <emil.karlen@fripost.org>
2015-06-16 21:50:25 +0200

    Test of phase-instr-parser: Refactoring: moved test module to reflect source package hierarchy

---
+++ test/shellcheck_lib_test/cli/default/test_suite.py
@@ -0,0 +1,18 @@
+import unittest
+
+from shellcheck_lib_test.cli.default import main_program_default
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(main_program_default.suite())
+    return ret_val
+
+
+def run_suite():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+
+if __name__ == '__main__':
+    run_suite()
--- test/shellcheck_lib_test/cli/test_suite.py
+++ test/shellcheck_lib_test/cli/test_suite.py
@@ -1,12 +1,13 @@
 import unittest
 
-from shellcheck_lib_test.cli import main_program_default, argument_parsing
+from shellcheck_lib_test.cli import argument_parsing
+from shellcheck_lib_test.cli.default import test_suite as default_test_suite
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(argument_parsing.suite())
-    ret_val.addTest(main_program_default.suite())
+    ret_val.addTest(default_test_suite.suite())
     return ret_val
 
 
