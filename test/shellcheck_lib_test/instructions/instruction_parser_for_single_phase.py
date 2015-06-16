7fc63deba9da6bbb01a56e0512e491f72827d625
Emil Karlén <emil@member.fsf.org>
2015-06-16 21:19:29 +0200

    Test of phase-instr-parser: New test: test_when_parser_fails_to_parse_instruction_name_not_in_dict_then_exception_should_be_raised

--- test/shellcheck_lib_test/instructions/instruction_parser_for_single_phase.py
+++ test/shellcheck_lib_test/instructions/instruction_parser_for_single_phase.py
@@ -2,6 +2,7 @@ import unittest
 
 from shellcheck_lib.general import line_source
 from shellcheck_lib.instructions import instruction_parser_for_single_phase as parse
+from shellcheck_lib.test_case import instructions
 from shellcheck_lib_test.document.test_resources import assert_equals_line
 
 
@@ -27,6 +28,25 @@ class TestParse(unittest.TestCase):
                                cm.ex.line,
                                'Source line')
 
+    def test_when_parser_fails_to_parse_instruction_name_not_in_dict_then_exception_should_be_raised(self):
+        parsers_dict = {'S': SingleInstructionParserThatSucceeds(),
+                        'F': SingleInstructionParserThatRaisesInvalidArgumentError('the error message')}
+        phase_parser = parse.InstructionParserForDictionaryOfInstructions(name_argument_splitter,
+                                                                          parsers_dict)
+        line = new_line('Fa')
+        with self.assertRaises(parse.InvalidInstructionArgumentException) as cm:
+            phase_parser.apply(line)
+            self.assertEqual('F',
+                             cm.ex.instruction_name,
+                             'Instruction name')
+            self.assertEqual('the error message',
+                             cm.ex.error_message,
+                             'Error message')
+            assert_equals_line(self,
+                               line,
+                               cm.ex.line,
+                               'Source line')
+
 
 # def test_handling_of_invalid_argument_exception_from_parser(self):
 # parser = exitcode.Parser()
@@ -35,13 +55,24 @@ class TestParse(unittest.TestCase):
 #                           '')
 
 
-# class SingleInstructionParserThatRaisesInvalidArgumentError(parse.SingleInstructionParser):
-#     def __init__(self,
-#                  error_message: str):
-#         self.error_message = error_message
-#
-#     def apply(self, instruction_argument: str) -> i.Instruction:
-#         raise parse.SingleInstructionInvalidArgumentException(self.error_message)
+class SingleInstructionParserThatRaisesInvalidArgumentError(parse.SingleInstructionParser):
+    def __init__(self,
+                 error_message: str):
+        self.error_message = error_message
+
+    def apply(self, instruction_argument: str) -> instructions.Instruction:
+        raise parse.SingleInstructionInvalidArgumentException(self.error_message)
+
+
+class SingleInstructionParserThatSucceeds(parse.SingleInstructionParser):
+    def apply(self, instruction_argument: str) -> instructions.Instruction:
+        return Instruction(instruction_argument)
+
+
+class Instruction(instructions.Instruction):
+    def __init__(self,
+                 argument: str):
+        self.argument = argument
 
 
 def suite():
