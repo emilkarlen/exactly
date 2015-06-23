2b0f79842a40ed3d3559b804055d1dc780cc6c47
Emil Karlén <emil.karlen@fripost.org>
2015-06-23 19:21:01 +0200

    Suite: Refactoring of test

--- test/shellcheck_lib_test/test_suite/suite_hierarchy_reading.py
+++ test/shellcheck_lib_test/test_suite/suite_hierarchy_reading.py
@@ -60,32 +60,49 @@ def write_to(dir_path: pathlib.Path,
     return dir_contents
 
 
+class FileStructureAndExpectedHierarchy:
+    def file_structure_to_read(self) -> DirContents:
+        raise NotImplementedError()
+
+    def expected_structure_based_at(self, root_path: pathlib.Path) -> structure.TestSuite:
+        raise NotImplementedError()
+
+
+class MainSuiteWithTwoReferencedCases(FileStructureAndExpectedHierarchy):
+    def expected_structure_based_at(self, root_path: pathlib.Path) -> structure.TestSuite:
+        return structure.TestSuite(
+            [],
+            [
+                structure.TestCase(root_path / '1.case'),
+                structure.TestCase(root_path / 'sub' / '2.case')
+            ])
+
+    def file_structure_to_read(self) -> DirContents:
+        return DirContents([File('main.suite',
+                                 lines_content(['[cases]',
+                                                '1.case',
+                                                os.path.join('sub', '2.case'),
+                                                ])),
+                            File('1.case', ''),
+                            Dir('sub',
+                                [File('2.case', '')])])
+
+
 class TestRead(unittest.TestCase):
-    def test_read_valid_hierarchy(self):
-        def expected_at(root_path: pathlib.Path) -> structure.TestSuite:
-            return structure.TestSuite(
-                [],
-                [
-                    structure.TestCase(root_path / '1.case'),
-                    structure.TestCase(root_path / 'sub' / '2.case')
-                ])
+    def test_main_suite_with_two_referenced_cases(self):
+        check(MainSuiteWithTwoReferencedCases(), self)
 
-        file_structure = DirContents([File('main.suite',
-                                           lines_content(['[cases]',
-                                                          '1.case',
-                                                          os.path.join('sub', '2.case'),
-                                                          ])),
-                                      File('1.case', ''),
-                                      Dir('sub',
-                                          [File('2.case', '')])])
-        with tempfile.TemporaryDirectory(prefix='shellcheck-test-') as tmp_dir:
-            tmp_dir_path = pathlib.Path(tmp_dir)
-            write_to(tmp_dir_path, file_structure)
-            actual = read(tmp_dir_path / 'main.suite')
-            expected = expected_at(tmp_dir_path)
 
-            StructureEqualityChecker(self).check_suite(expected,
-                                                       actual)
+def check(setup: FileStructureAndExpectedHierarchy,
+          put: unittest.TestCase):
+    with tempfile.TemporaryDirectory(prefix='shellcheck-test-') as tmp_dir:
+        tmp_dir_path = pathlib.Path(tmp_dir)
+        write_to(tmp_dir_path, setup.file_structure_to_read())
+        actual = read(tmp_dir_path / 'main.suite')
+        expected = setup.expected_structure_based_at(tmp_dir_path)
+
+        StructureEqualityChecker(put).check_suite(expected,
+                                                  actual)
 
 
 class StructureEqualityChecker:
