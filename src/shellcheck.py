4bb131123df375b7e58579215b8bda017b4178d3
Emil Karlén <emil.karlen@fripost.org>
2015-06-26 12:57:16 +0200

    suite: Refactoring: Move to new package: Default implementation of main-program for execution-mode/test-case

--- src/shellcheck.py
+++ src/shellcheck.py
@@ -4,12 +4,12 @@ Main program for shellcheck
 import sys
 
 from shellcheck_lib.cli.default.default_main_program import MainProgram
-from shellcheck_lib.cli.default import default_instructions_setup
-from shellcheck_lib.cli.default import instruction_name_and_argument_splitter
+from shellcheck_lib.cli.default.execution_mode.test_case import default_instructions_setup, \
+    instruction_name_and_argument_splitter
 from shellcheck_lib.general.output import StdOutputFiles
 
 program = MainProgram(StdOutputFiles(sys.stdout,
-                                                  sys.stderr),
+                                     sys.stderr),
                       instruction_name_and_argument_splitter.splitter,
                       default_instructions_setup.instructions_setup)
 exit_status = program.execute(sys.argv[1:])
