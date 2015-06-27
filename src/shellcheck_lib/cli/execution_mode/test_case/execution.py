efc903ea8aa3ea538cbe42fa089970efcef007e3
Emil Karlén <emil.karlen@fripost.org>
2015-06-27 22:52:24 +0200

    suite: Refactoring: Main program uses new Test Case Processor

--- src/shellcheck_lib/cli/execution_mode/test_case/execution.py
+++ src/shellcheck_lib/cli/execution_mode/test_case/execution.py
@@ -5,13 +5,12 @@ import shutil
 from shellcheck_lib.general.output import StdOutputFiles
 from shellcheck_lib.script_language.act_script_management import ScriptLanguageSetup
 from shellcheck_lib.script_language import python3
-from shellcheck_lib.general import line_source
-from shellcheck_lib.test_case import test_case_doc
 from shellcheck_lib.cli.execution_mode.test_case.argument_parsing import INTERPRETER_FOR_TEST
-from shellcheck_lib.default.execution_mode.test_case import test_case_parser
+from shellcheck_lib.default.execution_mode.test_case import processing
 from shellcheck_lib.default.execution_mode.test_case.instruction_setup import InstructionsSetup
 from shellcheck_lib.cli.execution_mode.test_case.settings import Output, TestCaseExecutionSettings
 from shellcheck_lib.execution import full_execution
+from shellcheck_lib.test_case import test_case_processing
 
 
 class Executor:
@@ -26,23 +25,18 @@ class Executor:
         self._settings = settings
 
     def execute(self) -> int:
-        test_case = self._parse_test_case_source()
-        script_language_setup = resolve_script_language(self._settings)
         if self._settings.output is Output.ACT_PHASE_OUTPUT:
-            return self._execute_act_phase(test_case,
-                                           script_language_setup)
+            return self._execute_act_phase()
         else:
-            full_result = full_execution.execute(script_language_setup,
-                                                 test_case,
-                                                 self._settings.initial_home_dir_path,
-                                                 self._settings.execution_directory_root_name_prefix,
-                                                 self._settings.is_keep_execution_directory_root)
-            self._print_output_to_stdout(full_result)
-            return full_result.status.value
+            return self._execute_normal()
 
-    def _execute_act_phase(self,
-                           test_case: test_case_doc.TestCase,
-                           script_language_setup: ScriptLanguageSetup) -> int:
+    def _execute_normal(self) -> int:
+        result = self._process(self._settings.is_keep_execution_directory_root)
+        full_result = result.execution_result
+        self._print_output_to_stdout(full_result)
+        return full_result.status.value
+
+    def _execute_act_phase(self) -> int:
         def copy_file(input_file_path: pathlib.Path,
                       output_file):
             with input_file_path.open() as f:
@@ -54,11 +48,9 @@ class Executor:
                 exit_code_string = f.read()
                 return int(exit_code_string)
 
-        full_result = full_execution.execute(script_language_setup,
-                                             test_case,
-                                             self._settings.initial_home_dir_path,
-                                             self._settings.execution_directory_root_name_prefix,
-                                             True)
+        result = self._process(True)
+        full_result = result.execution_result
+
         copy_file(full_result.execution_directory_structure.result.std.stdout_file, self._std.out)
         copy_file(full_result.execution_directory_structure.result.std.stderr_file, self._std.err)
         exit_code = act_phase_exit_code(full_result.execution_directory_structure.result.exitcode_file)
@@ -73,16 +65,18 @@ class Executor:
             self._std.out.write(str(the_full_result.execution_directory_structure.root_dir))
             self._std.out.write(os.linesep)
 
-    def _parse_test_case_source(self) -> test_case_doc.TestCase:
-        file_parser = test_case_parser.new_parser(self._split_line_into_name_and_argument_function,
-                                                  self._instruction_setup)
-        source = line_source.new_for_file(self._settings.file_path)
-        return file_parser.apply(source)
+    def _process(self,
+                 is_keep_eds: bool) -> test_case_processing.Result:
+        configuration = processing.Configuration(self._split_line_into_name_and_argument_function,
+                                                 self._instruction_setup,
+                                                 resolve_script_language(self._settings),
+                                                 is_keep_eds,
+                                                 self._settings.execution_directory_root_name_prefix)
+        processor = processing.new_processor(configuration)
+        return processor.apply(test_case_processing.TestCase(self._settings.file_path))
 
 
 def resolve_script_language(cl_arguments: TestCaseExecutionSettings) -> ScriptLanguageSetup:
     if cl_arguments.interpreter and cl_arguments.interpreter == INTERPRETER_FOR_TEST:
         return python3.new_script_language_setup()
     return python3.new_script_language_setup()
-
-
