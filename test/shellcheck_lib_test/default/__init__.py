1e143beee26b06eb2da9f34c09aa5a3b7512d2aa
Emil Karlén <emil.karlen@fripost.org>
2015-06-27 23:04:53 +0200

    suite: Refactoring: Moving default implementation of main program to CLI package

--- test/shellcheck_lib_test/default/__init__.py
+++ test/shellcheck_lib_test/default/__init__.py
@@ -1,11 +1,9 @@
 import unittest
 
-from shellcheck_lib_test.default import instruction_name_and_argument_splitter as splitter, main_program_default
-from shellcheck_lib_test.default import main_program_default
+from shellcheck_lib_test.default import instruction_name_and_argument_splitter as splitter
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(splitter.suite())
-    ret_val.addTest(main_program_default.suite())
     return ret_val
