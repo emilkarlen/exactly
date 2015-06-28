921c9064b0b9ac6cffc5912857c6e91b605570dc
Emil Karlén <emil.karlen@fripost.org>
2015-06-28 12:52:01 +0200

    Main Program: Running tests of default-main-program as tests of cli-main-program via shell

--- test/shellcheck_lib_test/util/cli_main_program_via_shell_utils/run.py
+++ test/shellcheck_lib_test/util/cli_main_program_via_shell_utils/run.py
@@ -4,7 +4,7 @@ import unittest
 
 from shellcheck_lib.execution.result import FullResultStatus
 from shellcheck_lib_test.util.with_tmp_file import ExpectedSubProcessResult, lines_content, SubProcessResultInfo, \
-    run_subprocess_with_file_arg__full
+    run_subprocess, run_subprocess_with_file_arg__full, SubProcessResult
 
 SRC_DIR_NAME = 'src'
 MAIN_PROGRAM_FILE_NAME = 'shellcheck.py'
@@ -19,16 +19,28 @@ SUCCESSFUL_RESULT = ExpectedSubProcessResult(exitcode=FullResultStatus.PASS.valu
                                              stderr='')
 
 
-def run_shellcheck_in_sub_process(puc: unittest.TestCase,
-                                  last_file_argument: str,
-                                  flags: list=()) -> SubProcessResultInfo:
+def run_shellcheck_in_sub_process_with_file_argument(puc: unittest.TestCase,
+                                                     file_contents: str,
+                                                     flags: list=()) -> SubProcessResultInfo:
     cwd = pathlib.Path.cwd()
     # print('# DEBUG: cwd: ' + str(cwd))
     if not sys.executable:
         puc.fail('Cannot execute test since the name of the Python 3 interpreter is not found in sys.executable.')
     shellcheck_path = shellcheck_src_path(cwd)
     args_without_file = [sys.executable, str(shellcheck_path)] + list(flags)
-    return run_subprocess_with_file_arg__full(args_without_file, last_file_argument)
+    return run_subprocess_with_file_arg__full(args_without_file, file_contents)
+
+
+def run_shellcheck_in_sub_process(puc: unittest.TestCase,
+                                  arguments: list,
+                                  stdin_contents: str='') -> SubProcessResult:
+    cwd = pathlib.Path.cwd()
+    # print('# DEBUG: cwd: ' + str(cwd))
+    if not sys.executable:
+        puc.fail('Cannot execute test since the name of the Python 3 interpreter is not found in sys.executable.')
+    shellcheck_path = shellcheck_src_path(cwd)
+    cmd_and_args = [sys.executable, str(shellcheck_path)] + list(arguments)
+    return run_subprocess(cmd_and_args, stdin_contents)
 
 
 def contents_of_file(path: pathlib.Path) -> str:
