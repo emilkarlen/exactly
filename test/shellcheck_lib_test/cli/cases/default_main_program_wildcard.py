caa002a2cb4f8e110eb708a283ba7d7778bf1190
Emil Karlén <emil@member.fsf.org>
2015-07-14 06:04:01 +0200

    Suite/wildcard-inclusion: New test: test_suite_with_wildcard_references_to_case_files_in_any_sub_dir (already implemented)

--- test/shellcheck_lib_test/cli/cases/default_main_program_wildcard.py
+++ test/shellcheck_lib_test/cli/cases/default_main_program_wildcard.py
@@ -88,6 +88,46 @@ class SuiteWithWildcardReferencesToCaseFilesInAnyDirectSubDir(check_suite.Setup)
         return 0
 
 
+class SuiteWithWildcardReferencesToCaseFilesInAnySubDir(check_suite.Setup):
+    def root_suite_file_based_at(self, root_path: pathlib.Path) -> pathlib.Path:
+        return root_path / 'main.suite'
+
+    def file_structure(self, root_path: pathlib.Path) -> DirContents:
+        return DirContents([
+            File('main.suite', lines_content(['[cases]',
+                                              '**/*.case'])),
+            Dir('sub-dir-2', [
+                empty_file('y.case'),
+                empty_file('x.case'),
+            ]),
+            Dir('sub-dir-1', [
+                empty_file('b.case'),
+                empty_file('a.case'),
+                Dir('sub-dir-1-1', [
+                    empty_file('1-1.case'),
+                ])
+            ]),
+            empty_file('1.case'),
+            empty_file('2.case'),
+        ])
+
+    def expected_stdout_lines(self, root_path: pathlib.Path) -> list:
+        return [
+            self.suite_begin(root_path / 'main.suite'),
+            self.case(root_path / '1.case', FullResultStatus.PASS.name),
+            self.case(root_path / '2.case', FullResultStatus.PASS.name),
+            self.case(root_path / 'sub-dir-1' / 'a.case', FullResultStatus.PASS.name),
+            self.case(root_path / 'sub-dir-1' / 'b.case', FullResultStatus.PASS.name),
+            self.case(root_path / 'sub-dir-1' / 'sub-dir-1-1' / '1-1.case', FullResultStatus.PASS.name),
+            self.case(root_path / 'sub-dir-2' / 'x.case', FullResultStatus.PASS.name),
+            self.case(root_path / 'sub-dir-2' / 'y.case', FullResultStatus.PASS.name),
+            self.suite_end(root_path / 'main.suite'),
+        ]
+
+    def expected_exit_code(self) -> int:
+        return 0
+
+
 class SuiteWithWildcardReferencesToCaseFilesThatAreDirectories(check_suite.Setup):
     def root_suite_file_based_at(self, root_path: pathlib.Path) -> pathlib.Path:
         return root_path / 'main.suite'
