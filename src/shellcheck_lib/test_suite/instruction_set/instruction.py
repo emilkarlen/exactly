ee1b78f1f31f448e317343730488fd869a64eb75
Emil Karlén <emil.karlen@fripost.org>
2015-09-20 19:01:43 +0200

    Preprocessing: Refactoring: Moves suite/suites-instruction base class to suites-related module

--- src/shellcheck_lib/test_suite/instruction_set/instruction.py
+++ src/shellcheck_lib/test_suite/instruction_set/instruction.py
@@ -1,7 +1,5 @@
 import pathlib
 
-from shellcheck_lib.document.model import Instruction
-
 
 class Environment:
     def __init__(self,
@@ -21,13 +19,3 @@ class FileNotAccessibleSimpleError(Exception):
     @property
     def file_path(self) -> pathlib.Path:
         return self._file_path
-
-
-class TestSuiteSectionInstruction(Instruction):
-    def resolve_paths(self,
-                      environment: Environment) -> list:
-        """
-        :raises FileNotAccessibleError: A referenced file is not accessible.
-        :return: [pathlib.Path]
-        """
-        raise NotImplementedError()
