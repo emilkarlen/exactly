6ddd32a4f79f8ce511f645a5d1f9f45e6cc80a97
Emil Karlén <emil@member.fsf.org>
2015-09-20 20:32:41 +0200

    Preprocessing/suite: Integration of anonymous-section-instructions into Test Suite Document

--- src/shellcheck_lib/test_suite/parser.py
+++ src/shellcheck_lib/test_suite/parser.py
@@ -1,6 +1,7 @@
 from shellcheck_lib.document import parse
 from shellcheck_lib.general import line_source
 from shellcheck_lib.test_suite import test_suite_doc
+from shellcheck_lib.test_suite.instruction_set.sections import anonymous
 from shellcheck_lib.test_suite.instruction_set.sections import suites
 from shellcheck_lib.test_suite.instruction_set.sections import cases
 
@@ -8,7 +9,7 @@ SECTION_NAME__SUITS = 'suites'
 SECTION_NAME__CASES = 'cases'
 
 PARSER_CONFIGURATION = parse.SectionsConfiguration(
-    None,
+    anonymous.new_parser(),
     (parse.SectionConfiguration(SECTION_NAME__SUITS, suites.new_parser()),
      parse.SectionConfiguration(SECTION_NAME__CASES, cases.new_parser()),
      )
@@ -23,6 +24,7 @@ class Parser:
               plain_test_case: line_source.LineSource) -> test_suite_doc.TestSuite:
         document = self.__plain_file_parser.apply(plain_test_case)
         return test_suite_doc.TestSuite(
+            document.elements_for_phase_or_empty_if_phase_not_present(None),
             document.elements_for_phase_or_empty_if_phase_not_present(SECTION_NAME__SUITS),
             document.elements_for_phase_or_empty_if_phase_not_present(SECTION_NAME__CASES),
         )
