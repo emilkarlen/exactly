1011f59f6f6f98351e541c35c409728843981abe
Emil Karlén <emil.karlen@fripost.org>
2015-09-20 20:32:41 +0200

    Preprocessing/suite: Integration of anonymous-section-instructions into Test Suite Document

--- src/shellcheck_lib/test_suite/test_suite_doc.py
+++ src/shellcheck_lib/test_suite/test_suite_doc.py
@@ -1,25 +1,34 @@
 from shellcheck_lib.document.model import PhaseContents, ElementType
+from shellcheck_lib.test_suite.instruction_set.sections.anonymous import AnonymousSectionInstruction
 from shellcheck_lib.test_suite.instruction_set.sections.cases import TestCaseSectionInstruction
 from shellcheck_lib.test_suite.instruction_set.sections.suites import TestSuiteSectionInstruction
 
 
 class TestSuite(tuple):
     def __new__(cls,
+                anonymous_section: PhaseContents,
                 suites_section: PhaseContents,
                 cases_section: PhaseContents):
+        TestSuite.__assert_instruction_class(anonymous_section,
+                                             AnonymousSectionInstruction)
         TestSuite.__assert_instruction_class(suites_section,
                                              TestSuiteSectionInstruction)
         TestSuite.__assert_instruction_class(cases_section, TestCaseSectionInstruction)
-        return tuple.__new__(cls, (suites_section,
+        return tuple.__new__(cls, (anonymous_section,
+                                   suites_section,
                                    cases_section))
 
     @property
-    def suites_section(self) -> PhaseContents:
+    def anonymous_section(self) -> PhaseContents:
         return self[0]
 
     @property
-    def cases_section(self) -> PhaseContents:
+    def suites_section(self) -> PhaseContents:
         return self[1]
+
+    @property
+    def cases_section(self) -> PhaseContents:
+        return self[2]
 
     @staticmethod
     def __assert_instruction_class(phase_contents: PhaseContents,
