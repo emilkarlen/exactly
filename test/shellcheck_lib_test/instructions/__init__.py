96b68bbf13d34ffb21b0e2c1ea0d46bb4216e6ab
Emil Karlén <emil@member.fsf.org>
2015-09-20 19:28:46 +0200

    Preprocessing/suite: Refactoring: Moves instruction_parser_using_argument_parser into document package (making it "available" for both test-case and test-suite)

--- test/shellcheck_lib_test/instructions/__init__.py
+++ test/shellcheck_lib_test/instructions/__init__.py
@@ -1,12 +1,10 @@
 import unittest
 
-from shellcheck_lib_test.instructions import instruction_parser_using_argument_parser
 from shellcheck_lib_test.instructions import assert_phase
 
 
 def suite():
     ret_val = unittest.TestSuite()
-    ret_val.addTest(instruction_parser_using_argument_parser.suite())
     ret_val.addTest(assert_phase.suite())
     return ret_val
 
