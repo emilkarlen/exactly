81389551167c4c249c950bfeddba430bfbeaf008
Emil Karlén <emil.karlen@fripost.org>
2015-09-25 14:02:17 +0200

    Instructions: Infrastructure for testing setup-phase instructions: test: test_fail_due_to_unexpected_pre_validation_result

--- test/shellcheck_lib_test/instructions/setup/test_resources/instruction_check_test.py
+++ test/shellcheck_lib_test/instructions/setup/test_resources/instruction_check_test.py
@@ -6,6 +6,7 @@ import unittest
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser
 
 from shellcheck_lib.general import line_source
+
 from shellcheck_lib.test_case.instruction.result import sh
 from shellcheck_lib.test_case.instruction.result import svh
 from shellcheck_lib.test_case.instruction.sections.setup import SetupPhaseInstruction
@@ -15,6 +16,7 @@ from shellcheck_lib_test.instructions.setup.test_resources import instruction_ch
 from shellcheck_lib_test.instructions import utils
 from shellcheck_lib_test.instructions.utils import SingleInstructionParserSource
 from shellcheck_lib_test.instructions.test_resources import svh_check
+from shellcheck_lib_test.instructions.test_resources import sh_check
 
 
 def suite():
@@ -37,6 +39,12 @@ class TestCases(instruction_check.TestCaseBase):
         self._check(instruction_check.Flow(ParserThatGivesAnInstructionThatJustSucceeds(),
                                            expected_pre_validation_result=svh_check.StatusIs(
                                                svh.SuccessOrValidationErrorOrHardErrorEnum.VALIDATION_ERROR)),
+                    single_line_source())
+
+    @unittest.expectedFailure
+    def test_fail_due_to_unexpected_pre_validation_result(self):
+        self._check(instruction_check.Flow(ParserThatGivesAnInstructionThatJustSucceeds(),
+                                           expected_main_result=sh_check.IsHardError()),
                     single_line_source())
 
 
