8efc4f0471d49a03da45a0283ba4e738117f20fa
Emil Karlén <emil@member.fsf.org>
2015-09-25 14:00:54 +0200

    Instructions: Infrastructure for testing setup-phase instructions: test: test_fail_due_to_unexpected_pre_validation_result

--- test/shellcheck_lib_test/instructions/setup/test_resources/instruction_check_test.py
+++ test/shellcheck_lib_test/instructions/setup/test_resources/instruction_check_test.py
@@ -4,6 +4,7 @@ Test of test-infrastructure: instruction_check.
 import unittest
 
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser
+
 from shellcheck_lib.general import line_source
 from shellcheck_lib.test_case.instruction.result import sh
 from shellcheck_lib.test_case.instruction.result import svh
@@ -13,6 +14,7 @@ from shellcheck_lib_test.execution.full_execution.util.instruction_test_resource
 from shellcheck_lib_test.instructions.setup.test_resources import instruction_check
 from shellcheck_lib_test.instructions import utils
 from shellcheck_lib_test.instructions.utils import SingleInstructionParserSource
+from shellcheck_lib_test.instructions.test_resources import svh_check
 
 
 def suite():
@@ -27,7 +29,14 @@ if __name__ == '__main__':
 
 class TestCases(instruction_check.TestCaseBase):
     def test_successful_flow(self):
-        self._check(instruction_check.Flow(ParserThatGivesAnInstructionThatDoesNothing()),
+        self._check(instruction_check.Flow(ParserThatGivesAnInstructionThatJustSucceeds()),
+                    single_line_source())
+
+    @unittest.expectedFailure
+    def test_fail_due_to_unexpected_pre_validation_result(self):
+        self._check(instruction_check.Flow(ParserThatGivesAnInstructionThatJustSucceeds(),
+                                           expected_pre_validation_result=svh_check.StatusIs(
+                                               svh.SuccessOrValidationErrorOrHardErrorEnum.VALIDATION_ERROR)),
                     single_line_source())
 
 
@@ -35,7 +44,7 @@ def single_line_source() -> SingleInstructionParserSource:
     return utils.new_source('instruction name', 'instruction arguments')
 
 
-class ParserThatGivesAnInstructionThatDoesNothing(SingleInstructionParser):
+class ParserThatGivesAnInstructionThatJustSucceeds(SingleInstructionParser):
     def apply(self, source: line_source.LineSequenceBuilder, instruction_argument: str) -> SetupPhaseInstruction:
         return SetupPhaseInstructionThatReturns(svh.new_svh_success(),
                                                 sh.new_sh_success(),
