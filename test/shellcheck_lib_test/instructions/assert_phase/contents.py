e5b833a5ada7835e59b71e03488753654f33f3d7
Emil Karlén <emil.karlen@fripost.org>
2015-09-28 20:12:57 +0200

    Instructions:assert/contents: Refactoring of tests: Using new test-infrastructure

--- test/shellcheck_lib_test/instructions/assert_phase/contents.py
+++ test/shellcheck_lib_test/instructions/assert_phase/contents.py
@@ -1,12 +1,14 @@
 import unittest
 
 from shellcheck_lib.instructions.assert_phase import contents as sut
+
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
-from shellcheck_lib.test_case.instruction.result import pfh
-from shellcheck_lib.test_case.instruction.result import svh
-from shellcheck_lib_test.instructions import utils
-from shellcheck_lib_test.instructions.assert_phase.utils import AssertInstructionTest
+from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import Flow
+from shellcheck_lib_test.instructions.assert_phase.test_resources import instruction_check
+from shellcheck_lib_test.instructions.test_resources import pfh_check
+from shellcheck_lib_test.instructions.test_resources import svh_check
+from shellcheck_lib_test.instructions.test_resources.eds_populator import FilesInActDir
 from shellcheck_lib_test.instructions.utils import new_source
 from shellcheck_lib_test.util.file_structure import DirContents, empty_file, empty_dir, File
 
@@ -30,233 +32,205 @@ class TestFileContentsEmptyInvalidSyntax(unittest.TestCase):
                                      arguments))
 
 
-class TestFileContentsEmptyValidSyntax(unittest.TestCase):
+class TestFileContentsEmptyValidSyntax(instruction_check.TestCaseBase):
     def test_fail__when__file_do_not_exist(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult())
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'name-of-non-existing-file empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 expected_main_result=pfh_check.is_fail()),
+            new_source('instruction-name', 'name-of-non-existing-file empty'))
 
     def test_fail__when__file_is_directory(self):
         file_name = 'name-of-existing-directory'
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_dir(file_name)]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', file_name + ' empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_dir(file_name)])),
+                 expected_main_result=pfh_check.is_fail()),
+            new_source('instruction-name',
+                       file_name + ' empty'))
 
     def test_fail__when__file_exists_but_is_non_empty(self):
         file_name = 'name-of-existing-file'
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([File(file_name, 'contents')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', file_name + ' empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([File(file_name, 'contents')])),
+                 expected_main_result=pfh_check.is_fail()
+                 ),
+            new_source('instruction-name',
+                       file_name + ' empty'))
 
     def test_pass__when__file_exists_and_is_empty(self):
         file_name = 'name-of-existing-file'
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.PASS,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_file(file_name)]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', file_name + ' empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_file(file_name)])),
+                 ),
+            new_source('instruction-name',
+                       file_name + ' empty'))
 
 
-class TestFileContentsNonEmptyInvalidSyntax(unittest.TestCase):
+class TestFileContentsNonEmptyInvalidSyntax(instruction_check.TestCaseBase):
     def test_that_when_no_arguments_then_exception_is_raised(self):
         arguments = 'file-name ! empty superfluous-argument'
-        parser = sut.Parser()
-        self.assertRaises(SingleInstructionInvalidArgumentException,
-                          parser.apply,
-                          new_source('instruction-name',
-                                     arguments))
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            self._check(
+                Flow(sut.Parser()),
+                new_source('instruction-name',
+                           arguments))
 
 
-class TestFileContentsNonEmptyValidSyntax(unittest.TestCase):
+class TestFileContentsNonEmptyValidSyntax(instruction_check.TestCaseBase):
     def test_fail__when__file_do_not_exist(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult())
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'name-of-non-existing-file ! empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'name-of-non-existing-file ! empty'))
 
     def test_fail__when__file_is_directory(self):
         file_name = 'name-of-existing-directory'
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_dir(file_name)]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', file_name + ' ! empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_dir(file_name)])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       file_name + ' ! empty'))
 
     def test_fail__when__file_exists_but_is_empty(self):
         file_name = 'name-of-existing-file'
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_file(file_name)]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', file_name + ' ! empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_file(file_name)])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       file_name + ' ! empty'))
 
     def test_pass__when__file_exists_and_is_non_empty(self):
         file_name = 'name-of-existing-file'
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.PASS,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([File(file_name, 'contents')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', file_name + ' ! empty'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([File(file_name, 'contents')])),
+                 ),
+            new_source('instruction-name',
+                       file_name + ' ! empty'))
 
 
-class TestFileContentsFileRelHome(unittest.TestCase):
+class TestFileContentsFileRelHome(instruction_check.TestCaseBase):
     def test_validation_error__when__comparison_file_does_not_exist(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.VALIDATION_ERROR,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            home_dir_contents=DirContents([empty_file('f')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'name-of-non-existing-file --rel-home f.txt'))
+        self._check(
+            Flow(sut.Parser(),
+                 expected_validation_result=svh_check.is_validation_error(),
+                 ),
+            new_source('instruction-name',
+                       'name-of-non-existing-file --rel-home f.txt'))
 
     def test_validation_error__when__comparison_file_is_a_directory(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.VALIDATION_ERROR,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            home_dir_contents=DirContents([empty_dir('dir')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'name-of-non-existing-file --rel-home dir'))
+        self._check(
+            Flow(sut.Parser(),
+                 home_dir_contents=DirContents([empty_dir('dir')]),
+                 expected_validation_result=svh_check.is_validation_error(),
+                 ),
+            new_source('instruction-name',
+                       'name-of-non-existing-file --rel-home dir'))
 
     def test_fail__when__target_file_does_not_exist(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            home_dir_contents=DirContents([empty_file('f.txt')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'name-of-non-existing-file --rel-home f.txt'))
+        self._check(
+            Flow(sut.Parser(),
+                 home_dir_contents=DirContents([empty_file('f.txt')]),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'name-of-non-existing-file --rel-home f.txt'))
 
     def test_fail__when__target_file_is_a_directory(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            home_dir_contents=DirContents([empty_file('f.txt')]),
-            act_dir_contents_after_act=DirContents([empty_dir('dir')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'dir --rel-home f.txt'))
+        self._check(
+            Flow(sut.Parser(),
+                 home_dir_contents=DirContents([empty_file('f.txt')]),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_dir('dir')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'dir --rel-home f.txt'))
 
     def test_fail__when__contents_differ(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            home_dir_contents=DirContents([empty_file('f.txt')]),
-            act_dir_contents_after_act=DirContents([File('target.txt', 'non-empty')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target.txt --rel-home f.txt'))
+        self._check(
+            Flow(sut.Parser(),
+                 home_dir_contents=DirContents([empty_file('f.txt')]),
+                 eds_contents_before_main=FilesInActDir(DirContents([File('target.txt', 'non-empty')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'target.txt --rel-home f.txt'))
 
     def test_pass__when__contents_equals(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.PASS,
-            utils.ActResult(),
-            home_dir_contents=DirContents([File('f.txt', 'contents')]),
-            act_dir_contents_after_act=DirContents([File('target.txt', 'contents')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target.txt --rel-home f.txt'))
+        self._check(
+            Flow(sut.Parser(),
+                 home_dir_contents=DirContents([File('f.txt', 'contents')]),
+                 eds_contents_before_main=FilesInActDir(DirContents([File('target.txt', 'contents')])),
+                 ),
+            new_source('instruction-name',
+                       'target.txt --rel-home f.txt'))
 
 
-class TestFileContentsFileRelCwd(unittest.TestCase):
+class TestFileContentsFileRelCwd(instruction_check.TestCaseBase):
     def test_fail__when__comparison_file_does_not_exist(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_file('target')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target --rel-cwd comparison'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_file('target')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'target --rel-cwd comparison'))
 
     def test_fail__when__target_file_does_not_exist(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_file('comparison')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target --rel-cwd comparison'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_file('comparison')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'target --rel-cwd comparison'))
 
     def test_validation_error__when__comparison_file_is_a_directory(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_file('target'),
-                                                    empty_dir('comparison')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target --rel-cwd comparison'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_file('target'),
+                                                                     empty_dir('comparison')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'target --rel-cwd comparison'))
 
     def test_validation_error__when__target_file_is_a_directory(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([empty_dir('target'),
-                                                    empty_file('comparison')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target --rel-cwd comparison'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([empty_dir('target'),
+                                                                     empty_file('comparison')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'target --rel-cwd comparison'))
 
     def test_fail__when__contents_is_different(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.FAIL,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([File('target', 'target-contents'),
-                                                    File('comparison', 'cmp-contents')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target --rel-cwd comparison'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([File('target', 'target-contents'),
+                                                                     File('comparison', 'cmp-contents')])),
+                 expected_main_result=pfh_check.is_fail(),
+                 ),
+            new_source('instruction-name',
+                       'target --rel-cwd comparison'))
 
     def test_pass__when__contents_is_equal(self):
-        test = AssertInstructionTest(
-            svh.SuccessOrValidationErrorOrHardErrorEnum.SUCCESS,
-            pfh.PassOrFailOrHardErrorEnum.PASS,
-            utils.ActResult(),
-            act_dir_contents_after_act=DirContents([File('target', 'contents'),
-                                                    File('comparison', 'contents')]))
-        test.apply(self,
-                   sut.Parser(),
-                   new_source('instruction-name', 'target --rel-cwd comparison'))
+        self._check(
+            Flow(sut.Parser(),
+                 eds_contents_before_main=FilesInActDir(DirContents([File('target', 'contents'),
+                                                                     File('comparison', 'contents')])),
+                 ),
+            new_source('instruction-name',
+                       'target --rel-cwd comparison'))
 
 
 def suite():
