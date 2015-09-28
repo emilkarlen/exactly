1ed579c2a0d5e12c49990c488142840c6b6a6c2b
Emil Karlén <emil.karlen@fripost.org>
2015-09-28 15:49:33 +0200

    Instruction: assert/shell

--- src/shellcheck_lib/instructions/assert_phase/shell.py
+++ src/shellcheck_lib/instructions/assert_phase/shell.py
@@ -1,25 +1,28 @@
 import subprocess
 
 from shellcheck_lib.default.execution_mode.test_case.instruction_setup import Description
-
 from shellcheck_lib.default.execution_mode.test_case.instruction_setup import InvokationVariant
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser, \
     SingleInstructionParserSource, SingleInstructionInvalidArgumentException
 from shellcheck_lib.test_case.instruction.common import GlobalEnvironmentForPostEdsPhase
-from shellcheck_lib.test_case.instruction.result import sh
-from shellcheck_lib.test_case.instruction.sections.cleanup import CleanupPhaseInstruction
+from shellcheck_lib.test_case.instruction.result import svh
+from shellcheck_lib.test_case.instruction.result import pfh
+from shellcheck_lib.test_case.instruction.sections.assert_ import AssertPhaseInstruction
 from shellcheck_lib.test_case.os_services import OsServices
 
 DESCRIPTION = Description(
     "Executes the given program using the system's shell.",
-    'The instruction is successful if (and only if) the exit code from the command is 0.',
+    """The assertion PASSes if (and only if) the exit code from the command is 0.
+
+    All other exit codes makes the assertion FAIL.
+    """,
     [InvokationVariant('PROGRAM ARGUMENT...',
                        'A plain file.'),
      ])
 
 
 class Parser(SingleInstructionParser):
-    def apply(self, source: SingleInstructionParserSource) -> CleanupPhaseInstruction:
+    def apply(self, source: SingleInstructionParserSource) -> AssertPhaseInstruction:
         arguments = source.instruction_argument.strip()
         if not arguments:
             msg = 'Program to execute must be given as argument'
@@ -27,16 +30,20 @@ class Parser(SingleInstructionParser):
         return _ShellInstruction(arguments)
 
 
-class _ShellInstruction(CleanupPhaseInstruction):
+class _ShellInstruction(AssertPhaseInstruction):
     def __init__(self,
                  command: str):
         self.command = command
 
+    def validate(self,
+                 global_environment: GlobalEnvironmentForPostEdsPhase) -> svh.SuccessOrValidationErrorOrHardError:
+        return svh.new_svh_success()
+
     def main(self,
              environment: GlobalEnvironmentForPostEdsPhase,
-             os_services: OsServices) -> sh.SuccessOrHardError:
+             os_services: OsServices) -> pfh.PassOrFailOrHardError:
         exit_code = subprocess.call(self.command,
                                     shell=True)
         if exit_code != 0:
-            return sh.new_sh_hard_error('Program finished with non-zero exit code {}'.format(exit_code))
-        return sh.new_sh_success()
+            return pfh.new_pfh_fail('Program finished with non-zero exit code {}'.format(exit_code))
+        return pfh.new_pfh_pass()
