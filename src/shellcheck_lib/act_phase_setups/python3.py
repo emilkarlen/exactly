ca1c6e35f5277d56133a5551d797f51e1e29deea
Emil Karlén <emil.karlen@fripost.org>
2015-10-11 13:16:54 +0200

    Refactoring: Separate module for Python 3 Act Phase Setup

---
+++ src/shellcheck_lib/act_phase_setups/python3.py
@@ -0,0 +1,7 @@
+from shellcheck_lib.script_language import python3 as python3_language
+from shellcheck_lib.act_phase_setups.script_language_setup import new_for_script_language_setup
+from shellcheck_lib.test_case.sections.act.phase_setup import ActPhaseSetup
+
+
+def new_act_phase_setup() -> ActPhaseSetup:
+    return new_for_script_language_setup(python3_language.new_script_language_setup())
--- src/shellcheck_lib/cli/utils.py
+++ src/shellcheck_lib/cli/utils.py
@@ -1,16 +1,11 @@
-from shellcheck_lib.script_language import python3
-from shellcheck_lib.act_phase_setups.script_language_setup import new_for_script_language_setup
+from shellcheck_lib.act_phase_setups import python3
 from shellcheck_lib.test_case.sections.act.phase_setup import ActPhaseSetup
 
 
 def resolve_act_phase_setup(interpreter: str=None) -> ActPhaseSetup:
     if interpreter and interpreter == INTERPRETER_FOR_TEST:
-        return _py3_setup()
-    return _py3_setup()
-
-
-def _py3_setup() -> ActPhaseSetup:
-    return new_for_script_language_setup(python3.new_script_language_setup())
+        return python3.new_act_phase_setup()
+    return python3.new_act_phase_setup()
 
 
 INTERPRETER_FOR_TEST = 'test-language'
