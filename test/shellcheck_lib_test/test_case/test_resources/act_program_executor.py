47e2d2906c8795f2e7dbf359e7bfcd078235a0d7
Emil Karlén <emil@member.fsf.org>
2015-10-14 19:53:20 +0200

    Tests of ActProgramExecutorForScriptLanguage: Refactoring: program-making-functions are now context-managers (to be able to use context-managers in their impl)

--- test/shellcheck_lib_test/test_case/test_resources/act_program_executor.py
+++ test/shellcheck_lib_test/test_case/test_resources/act_program_executor.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import os
 import pathlib
 import random
@@ -17,21 +18,27 @@ class ActProgramExecutorTestSetup:
     def __init__(self, sut: ActProgramExecutor):
         self.sut = sut
 
+    @contextmanager
     def program_that_copes_stdin_to_stdout(self) -> ScriptSourceBuilder:
         raise NotImplementedError()
 
+    @contextmanager
     def program_that_prints_to_stdout(self, string_to_print: str) -> ScriptSourceBuilder:
         raise NotImplementedError()
 
+    @contextmanager
     def program_that_prints_to_stderr(self, string_to_print: str) -> ScriptSourceBuilder:
         raise NotImplementedError()
 
+    @contextmanager
     def program_that_exits_with_code(self, exit_code: int) -> ScriptSourceBuilder:
         raise NotImplementedError()
 
+    @contextmanager
     def program_that_prints_cwd_without_new_line_to_stdout(self) -> ScriptSourceBuilder:
         raise NotImplementedError()
 
+    @contextmanager
     def program_that_prints_value_of_environment_variable_to_stdout(self, var_name: str) -> ScriptSourceBuilder:
         raise NotImplementedError()
 
@@ -63,69 +70,69 @@ class Tests:
         self.test_setup = test_setup
 
     def test_stdout_is_connected_to_program(self):
-        program = self.test_setup.program_that_prints_to_stdout('expected output on stdout')
-        process_result = self.__execute(program)
-        self.put.assertEqual('expected output on stdout',
-                             process_result.stdout,
-                             'Contents of stdout')
+        with self.test_setup.program_that_prints_to_stdout('expected output on stdout') as program:
+            process_result = self.__execute(program)
+            self.put.assertEqual('expected output on stdout',
+                                 process_result.stdout,
+                                 'Contents of stdout')
 
     def test_stderr_is_connected_to_program(self):
-        program = self.test_setup.program_that_prints_to_stderr('expected output on stderr')
-        process_result = self.__execute(program)
-        self.put.assertEqual('expected output on stderr',
-                             process_result.stderr,
-                             'Contents of stderr')
+        with self.test_setup.program_that_prints_to_stderr('expected output on stderr') as program:
+            process_result = self.__execute(program)
+            self.put.assertEqual('expected output on stderr',
+                                 process_result.stderr,
+                                 'Contents of stderr')
 
     def test_stdin_and_stdout_are_connected_to_program(self):
-        program = self.test_setup.program_that_copes_stdin_to_stdout()
-        process_result = self.__execute(program,
-                                        stdin_contents='contents of stdin')
-        self.put.assertEqual('contents of stdin',
-                             process_result.stdout,
-                             'Contents of stdout is expected to be equal to stdin')
+        with self.test_setup.program_that_copes_stdin_to_stdout() as program:
+            process_result = self.__execute(program,
+                                            stdin_contents='contents of stdin')
+            self.put.assertEqual('contents of stdin',
+                                 process_result.stdout,
+                                 'Contents of stdout is expected to be equal to stdin')
 
     def test_exit_code_is_returned(self):
-        program = self.test_setup.program_that_exits_with_code(87)
-        process_result = self.__execute(program)
-        self.put.assertEqual(87,
-                             process_result.exitcode,
-                             'Exit Code')
+        with self.test_setup.program_that_exits_with_code(87) as program:
+            process_result = self.__execute(program)
+            self.put.assertEqual(87,
+                                 process_result.exitcode,
+                                 'Exit Code')
 
     def test_environment_variables_are_accessible_by_program(self):
         var_name = 'SHELLCHECK_TEST_VAR'
         var_value = str(random.getrandbits(32))
         os.environ[var_name] = var_value
-        program = self.test_setup.program_that_prints_value_of_environment_variable_to_stdout(var_name)
-        process_result = self.__execute(program)
-        self.put.assertEqual(var_value,
-                             process_result.stdout,
-                             'Contents of stdout should be value of environment variable')
+        with self.test_setup.program_that_prints_value_of_environment_variable_to_stdout(var_name) as program:
+            process_result = self.__execute(program)
+            self.put.assertEqual(var_value,
+                                 process_result.stdout,
+                                 'Contents of stdout should be value of environment variable')
 
     def test_initial_cwd_is_act_directory_and_that_cwd_is_restored_afterwards(self):
         cwd_before = os.getcwd()
-        source = self.test_setup.program_that_prints_cwd_without_new_line_to_stdout()
-        act_program_executor = self.test_setup.sut
-        validation_result = act_program_executor.validate(source)
-        self.put.assertEqual(svh.new_svh_success(),
-                             validation_result)
-        with execution_directory_structure() as eds:
-            program_setup = SourceSetup(source,
-                                        eds.test_case_dir,
-                                        'file-name-stem')
-            act_program_executor.prepare(program_setup, eds)
-            process_executor = _ProcessExecutorForProgramExecutor(program_setup,
-                                                                  eds,
-                                                                  act_program_executor)
-            process_result = capture_process_executor_result(process_executor,
-                                                             eds.result.root_dir,
-                                                             cwd=eds.act_dir)
-        self.put.assertEqual(str(eds.act_dir),
-                             process_result.stdout,
-                             'Current Working Directory for program should be act-directory')
+        with self.test_setup.program_that_prints_cwd_without_new_line_to_stdout() as source:
+            act_program_executor = self.test_setup.sut
+            validation_result = act_program_executor.validate(source)
+            self.put.assertEqual(svh.new_svh_success(),
+                                 validation_result)
+            with execution_directory_structure() as eds:
+                program_setup = SourceSetup(source,
+                                            eds.test_case_dir,
+                                            'file-name-stem')
+                act_program_executor.prepare(program_setup, eds)
+                process_executor = _ProcessExecutorForProgramExecutor(program_setup,
+                                                                      eds,
+                                                                      act_program_executor)
+                process_result = capture_process_executor_result(process_executor,
+                                                                 eds.result.root_dir,
+                                                                 cwd=eds.act_dir)
+            self.put.assertEqual(str(eds.act_dir),
+                                 process_result.stdout,
+                                 'Current Working Directory for program should be act-directory')
 
-        self.put.assertEqual(cwd_before,
-                             os.getcwd(),
-                             'Current Working Directory should be restored after program has finished')
+            self.put.assertEqual(cwd_before,
+                                 os.getcwd(),
+                                 'Current Working Directory should be restored after program has finished')
 
     def __execute(self,
                   source: ScriptSourceBuilder,
