148b1b674041486d37eb26fc85ffa223cd3cfed6
Emil Karlén <emil@member.fsf.org>
2015-11-02 19:51:14 +0100

    Structuring of types of error descriptions (for errors from test case)

---
+++ src/shellcheck_lib/test_case/error_description.py
@@ -0,0 +1,93 @@
+class ErrorDescription:
+    def __init__(self,
+                 message: str):
+        self.__message = message
+
+    @property
+    def message(self) -> str:
+        return self.__message
+
+
+def of_message(message: str) -> ErrorDescription:
+    return ErrorDescriptionOfMessage(message)
+
+
+def of_exception(exception: Exception,
+                 message: str=None) -> ErrorDescription:
+    return ErrorDescriptionOfException(exception, message)
+
+
+def of_external_process_error(exit_code: int,
+                              stderr_output: str,
+                              message: str=None) -> ErrorDescription:
+    return ErrorDescriptionOfExternalProcessError(ExternalProcessError(exit_code,
+                                                                       stderr_output),
+                                                  message)
+
+
+class ErrorDescriptionOfMessage(ErrorDescription):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class ErrorDescriptionOfException(ErrorDescription):
+    def __init__(self,
+                 exception: Exception,
+                 message: str=None):
+        super().__init__(message)
+        self.__exception = exception
+
+    @property
+    def exception(self) -> Exception:
+        return self.__exception
+
+
+class ExternalProcessError(tuple):
+    def __new__(cls,
+                exit_code: int,
+                stderr_output: str):
+        return tuple.__new__(cls, (exit_code, stderr_output))
+
+    @property
+    def exit_code(self) -> int:
+        return self[0]
+
+    @property
+    def stderr_output(self) -> str:
+        return self[1]
+
+
+class ErrorDescriptionOfExternalProcessError(ErrorDescription):
+    def __init__(self,
+                 external_process_error: ExternalProcessError,
+                 message: str=None):
+        super().__init__(message)
+        self.__external_process_error = external_process_error
+
+    @property
+    def external_process_error(self) -> ExternalProcessError:
+        return self.__external_process_error
+
+
+class ErrorDescriptionVisitor:
+    def visit(self, error_description: ErrorDescription):
+        """
+        :return: Return value from _visit... method
+        """
+        if isinstance(error_description, ErrorDescriptionOfMessage):
+            return self._visit_message(error_description)
+        elif isinstance(error_description, ErrorDescriptionOfException):
+            return self._visit_exception(error_description)
+        elif isinstance(error_description, ErrorDescriptionOfExternalProcessError):
+            return self._visit_external_process_error(error_description)
+        else:
+            raise ValueError('Unknown {}: {}'.format(ErrorDescription, str(error_description)))
+
+    def _visit_message(self, error_description: ErrorDescriptionOfMessage):
+        raise NotImplementedError()
+
+    def _visit_exception(self, error_description: ErrorDescriptionOfException):
+        raise NotImplementedError()
+
+    def _visit_external_process_error(self, error_description: ErrorDescriptionOfExternalProcessError):
+        raise NotImplementedError()
---
+++ test/shellcheck_lib_test/test_case/error_description.py
@@ -0,0 +1,56 @@
+import enum
+import unittest
+
+from shellcheck_lib.test_case import error_description as sut
+
+
+class ReturnValueEnum(enum.Enum):
+    MESSAGE = 1
+    EXCEPTION = 2
+    EXCEPTION_WITH_MESSAGE = 3
+    EXTERNAL_PROCESS = 4
+
+
+class Visitor(sut.ErrorDescriptionVisitor):
+    def _visit_message(self, x: sut.ErrorDescriptionOfMessage):
+        return ReturnValueEnum.MESSAGE
+
+    def _visit_exception(self, x: sut.ErrorDescriptionOfException):
+        return ReturnValueEnum.EXCEPTION
+
+    def _visit_external_process_error(self, x: sut.ErrorDescriptionOfExternalProcessError):
+        return ReturnValueEnum.EXTERNAL_PROCESS
+
+
+class TestVisitor(unittest.TestCase):
+    VISITOR = Visitor()
+
+    def testVisitMessage(self):
+        ret_val = self.VISITOR.visit(sut.of_message('message'))
+        self.assertIs(ReturnValueEnum.MESSAGE,
+                      ret_val)
+
+    def testVisitException(self):
+        ret_val = self.VISITOR.visit(sut.of_exception(ValueError()))
+        self.assertIs(ReturnValueEnum.EXCEPTION,
+                      ret_val)
+
+    def testVisitExternalProcessError(self):
+        error = sut.of_external_process_error(1, 'stderr output')
+        ret_val = self.VISITOR.visit(error)
+        self.assertIs(ReturnValueEnum.EXTERNAL_PROCESS,
+                      ret_val)
+
+    def testVisitNonSubClassObject(self):
+        with self.assertRaises(ValueError) as info:
+            self.VISITOR.visit('string is not a sub class of ErrorDescription')
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(unittest.makeSuite(TestVisitor))
+    return ret_val
+
+
+if __name__ == '__main__':
+    unittest.main()
