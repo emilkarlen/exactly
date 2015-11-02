660c2c8bf90c0cb60d217a3295247e9993a323f9
Emil Karlén <emil.karlen@fripost.org>
2015-11-02 22:47:07 +0100

    Moves default main program setup/conf to separate module

---
+++ src/shellcheck_lib/cli/default_main_program_setup.py
@@ -0,0 +1,15 @@
+import sys
+
+from shellcheck_lib.general.std import StdOutputFiles
+
+from shellcheck_lib.cli import main_program
+from shellcheck_lib.cli.default_main_program import MainProgram
+from shellcheck_lib.default.execution_mode.test_case import default_instructions_setup, \
+    instruction_name_and_argument_splitter
+
+
+def default_main_program() -> main_program.MainProgram:
+    return MainProgram(StdOutputFiles(sys.stdout,
+                                      sys.stderr),
+                       instruction_name_and_argument_splitter.splitter,
+                       default_instructions_setup.instructions_setup)
