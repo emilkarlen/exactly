20a0231db1a99037723e422937958c79d4721a70
Emil Karlén <emil@member.fsf.org>
2015-11-02 22:47:07 +0100

    Moves default main program setup/conf to separate module

--- src/shellcheck.py
+++ src/shellcheck.py
@@ -17,16 +17,7 @@
 # along with shellcheck.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-import sys
+from shellcheck_lib.cli.default_main_program_setup import *
 
-from shellcheck_lib.cli.default_main_program import MainProgram
-from shellcheck_lib.default.execution_mode.test_case import default_instructions_setup, \
-    instruction_name_and_argument_splitter
-from shellcheck_lib.general.std import StdOutputFiles
-
-program = MainProgram(StdOutputFiles(sys.stdout,
-                                     sys.stderr),
-                      instruction_name_and_argument_splitter.splitter,
-                      default_instructions_setup.instructions_setup)
-exit_status = program.execute(sys.argv[1:])
+exit_status = default_main_program().execute(sys.argv[1:])
 sys.exit(exit_status)
