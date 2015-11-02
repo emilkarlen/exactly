ba52f55528a50498970e0af6aa0ab4ad3f3616af
Emil Karlén <emil@member.fsf.org>
2015-11-02 22:50:51 +0100

    Bug fix: Executing preprocessor with cwd as same dir as location of source file.
    Bug fix: Avoids wrapping of ProcessError

--- src/shellcheck_lib/test_case/preprocessor.py
+++ src/shellcheck_lib/test_case/preprocessor.py
@@ -34,7 +34,7 @@ class PreprocessorViaExternalProgram(Preprocessor):
     def apply(self,
               test_case_file_path: pathlib.Path,
               test_case_source: str) -> str:
-        command_line = self.external_program + [str(test_case_file_path)]
+        command_line = self.external_program + [str(test_case_file_path.name)]
         try:
             with tempfile.TemporaryFile(prefix='shellcheck-stdout-',
                                         mode='w+') as stdout_file:
@@ -51,22 +51,24 @@ class PreprocessorViaExternalProgram(Preprocessor):
                     else:
                         stderr_file.seek(0)
                         stderr_contents = stderr_file.read()
-                        raise self.__new_process_error(
+                        raise ProcessError(self.__error_info(
                             error_description.of_external_process_error(exitcode,
                                                                         stderr_contents,
                                                                         self.__error_message()),
-                            test_case_file_path)
+                            test_case_file_path))
+        except ProcessError as ex:
+            raise ex
         except Exception as ex:
-            raise self.__new_process_error(
+            raise ProcessError(self.__error_info(
                 error_description.of_exception(ex,
                                                self.__error_message()),
-                test_case_file_path)
+                test_case_file_path))
 
     @staticmethod
-    def __new_process_error(ed: error_description.ErrorDescription,
-                            test_case_file_path: pathlib.Path) -> ProcessError:
-        return ProcessError(ErrorInfo(ed,
-                                      test_case_file_path))
+    def __error_info(ed: error_description.ErrorDescription,
+                     test_case_file_path: pathlib.Path) -> ErrorInfo:
+        return ErrorInfo(ed,
+                         test_case_file_path)
 
     def __error_message(self) -> str:
         return 'Error from preprocessing by: ' + str(self.external_program)
