c1d0a2428b33b31b88444b80e93621bb51d1b1b5
Emil Karlén <emil.karlen@fripost.org>
2015-11-02 19:51:14 +0100

    Structuring of types of error descriptions (for errors from test case)

--- test/shellcheck_lib_test/test_case/__init__.py
+++ test/shellcheck_lib_test/test_case/__init__.py
@@ -1,11 +1,13 @@
 import unittest
 
+from shellcheck_lib_test.test_case import error_description
 from shellcheck_lib_test.test_case import processing_utils
 from shellcheck_lib_test.test_case import preprocessor
 
 
 def suite():
     ret_val = unittest.TestSuite()
+    ret_val.addTest(error_description.suite())
     ret_val.addTest(processing_utils.suite())
     ret_val.addTest(preprocessor.suite())
     return ret_val
