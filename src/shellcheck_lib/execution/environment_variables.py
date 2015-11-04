761c6e3f9cc787e8a577fcc0405185dcb014fd31
Emil Karlén <emil@member.fsf.org>
2015-11-04 05:38:39 +0100

    Methods for deriving all shellcheck environment variables

--- src/shellcheck_lib/execution/environment_variables.py
+++ src/shellcheck_lib/execution/environment_variables.py
@@ -1,6 +1,30 @@
+import pathlib
+
+from shellcheck_lib.execution.execution_directory_structure import ExecutionDirectoryStructure
+
 ENV_VAR_HOME = 'SHELLCHECK_HOME'
 ENV_VAR_ACT = 'SHELLCHECK_ACT'
 ENV_VAR_TMP = 'SHELLCHECK_TMP'
 ALL_ENV_VARS = [ENV_VAR_HOME,
                 ENV_VAR_ACT,
                 ENV_VAR_TMP]
+
+
+def pre_eds_environment_variables(home_dir_path: pathlib.Path) -> dict:
+    return {
+        ENV_VAR_HOME: str(home_dir_path)
+    }
+
+
+def post_eds_environment_variables(eds: ExecutionDirectoryStructure) -> dict:
+    return {
+        ENV_VAR_ACT: str(eds.act_dir),
+        ENV_VAR_TMP: str(eds.tmp_dir)
+    }
+
+
+def all_environment_variables(home_dir_path: pathlib.Path,
+                              eds: ExecutionDirectoryStructure) -> dict:
+    ret_val = pre_eds_environment_variables(home_dir_path)
+    ret_val.update(post_eds_environment_variables(eds))
+    return ret_val
--- src/shellcheck_lib/execution/partial_execution.py
+++ src/shellcheck_lib/execution/partial_execution.py
@@ -314,11 +314,10 @@ class PartialExecutor:
                 self._store_exit_code(exitcode)
 
     def __set_pre_eds_environment_variables(self):
-        os.environ[environment_variables.ENV_VAR_HOME] = str(self.configuration.home_dir)
+        os.environ.update(environment_variables.pre_eds_environment_variables(self.configuration.home_dir))
 
     def __set_post_eds_environment_variables(self):
-        os.environ[environment_variables.ENV_VAR_ACT] = str(self.execution_directory_structure.act_dir)
-        os.environ[environment_variables.ENV_VAR_TMP] = str(self.execution_directory_structure.tmp_dir)
+        os.environ.update(environment_variables.post_eds_environment_variables(self.execution_directory_structure))
 
     def __run_internal_instructions_phase_step(self,
                                                phase: phases.Phase,
