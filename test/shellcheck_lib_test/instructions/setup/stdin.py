a287fdcccdd815cf4022c79a754e39e0b3917fcf
Emil Karlén <emil@member.fsf.org>
2015-11-07 14:58:42 +0100

    setup/stdin: Implementation v 1

--- test/shellcheck_lib_test/instructions/setup/stdin.py
+++ test/shellcheck_lib_test/instructions/setup/stdin.py
@@ -2,8 +2,15 @@ import unittest
 
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
+
 from shellcheck_lib.instructions.setup import stdin as sut
+from shellcheck_lib.instructions.utils import file_ref
+from shellcheck_lib.test_case.sections.setup import SetupSettingsBuilder
+from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Flow, TestCaseBase
+from shellcheck_lib_test.instructions.setup.test_resources.settings_check import Assertion
 from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib.test_case.sections import common
+from shellcheck_lib_test.util.file_structure import DirContents, empty_file
 
 
 class TestParseSet(unittest.TestCase):
@@ -34,9 +41,39 @@ class TestParseSet(unittest.TestCase):
         sut.Parser().apply(source)
 
 
+class TestInstructionExecution(TestCaseBase):
+    def test_file_rel_home(self):
+        self._check(
+            Flow(sut.Parser(),
+                 home_dir_contents=DirContents([
+                     empty_file('file-in-home-dir.txt')]),
+                 expected_main_side_effects_on_environment=AssertStdinFileIsSetToFile(
+                     file_ref.rel_home('file-in-home-dir.txt'))
+                 ),
+            new_source('instruction-name',
+                       '--rel-home file-in-home-dir.txt'))
+
+
+class AssertStdinFileIsSetToFile(Assertion):
+    def __init__(self,
+                 file_reference: file_ref.FileRef):
+        self._file_reference = file_reference
+
+    def apply(self,
+              put: unittest.TestCase,
+              environment: common.GlobalEnvironmentForPostEdsPhase,
+              initial: SetupSettingsBuilder,
+              actual_result: SetupSettingsBuilder):
+        file_path = self._file_reference.file_path(environment)
+        put.assertEqual(str(file_path),
+                        actual_result.stdin_file_name,
+                        'Name of stdin file in Setup Settings')
+
+
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(unittest.makeSuite(TestParseSet))
+    ret_val.addTest(unittest.makeSuite(TestInstructionExecution))
     return ret_val
 
 
