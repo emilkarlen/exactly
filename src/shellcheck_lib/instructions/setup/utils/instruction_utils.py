3f6366d3ac4d2d44a7943db4c7d4bc7ccc68a21d
Emil Karlén <emil@member.fsf.org>
2015-11-12 00:31:24 +0100

    Instructions render result of file-ref-check

--- src/shellcheck_lib/instructions/setup/utils/instruction_utils.py
+++ src/shellcheck_lib/instructions/setup/utils/instruction_utils.py
@@ -1,3 +1,4 @@
+from shellcheck_lib.instructions.utils.file_properties import render_failure
 from shellcheck_lib.instructions.utils.file_ref_check import FileRefCheck
 from shellcheck_lib.test_case.sections.common import GlobalEnvironmentForPostEdsPhase, GlobalEnvironmentForPreEdsStep
 from shellcheck_lib.test_case.sections.result import svh
@@ -15,18 +16,20 @@ class InstructionWithFileRefsBase(SetupPhaseInstruction):
     def pre_validate(self, environment: GlobalEnvironmentForPreEdsStep) -> svh.SuccessOrValidationErrorOrHardError:
         for file_ref_check in self.file_ref_check_list_tuple:
             assert isinstance(file_ref_check, FileRefCheck)
-            validation_result = file_ref_check.satisfies_pre_eds_condition(environment.home_directory)
-            if not validation_result:
+            validation_result = file_ref_check.pre_eds_condition_result(environment.home_directory)
+            if validation_result and not validation_result.is_success:
                 file_path = file_ref_check.file_reference.file_path_pre_eds(environment.home_directory)
-                return svh.new_svh_validation_error('Invalid file reference: ' + str(file_path))
+                return svh.new_svh_validation_error(render_failure(validation_result.cause,
+                                                                   file_path))
         return svh.new_svh_success()
 
     def post_validate(self,
                       environment: GlobalEnvironmentForPostEdsPhase) -> svh.SuccessOrValidationErrorOrHardError:
         for file_ref_check in self.file_ref_check_list_tuple:
             assert isinstance(file_ref_check, FileRefCheck)
-            validation_result = file_ref_check.satisfies_post_eds_condition(environment.home_and_eds)
-            if not validation_result:
+            validation_result = file_ref_check.post_eds_condition_result(environment.home_and_eds)
+            if validation_result and not validation_result.is_success:
                 file_path = file_ref_check.file_reference.file_path_post_eds(environment.home_and_eds)
-                return svh.new_svh_validation_error('Invalid file reference: ' + str(file_path))
+                return svh.new_svh_validation_error(render_failure(validation_result.cause,
+                                                                   file_path))
         return svh.new_svh_success()
