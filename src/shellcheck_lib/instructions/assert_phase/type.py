4a7b02ce31a120401d91a5afd3dfb1856fb298eb
Emil Karlén <emil@member.fsf.org>
2015-11-12 23:21:12 +0100

    Refactorings: assert/type: Using FileRefCheck for type check

--- src/shellcheck_lib/instructions/assert_phase/type.py
+++ src/shellcheck_lib/instructions/assert_phase/type.py
@@ -1,8 +1,8 @@
-import pathlib
-
 from shellcheck_lib.default.execution_mode.test_case.instruction_setup import Description, InvokationVariant
-from shellcheck_lib.instructions.utils.file_properties import FileType
-from shellcheck_lib.instructions.utils.parse_utils import spit_arguments_list_string
+from shellcheck_lib.instructions.utils import file_ref
+from shellcheck_lib.instructions.utils.file_properties import FileType, must_exist_as, FilePropertiesCheck
+from shellcheck_lib.instructions.utils.file_ref_check import post_eds_failure_message_or_none, FileRefCheck
+from shellcheck_lib.instructions.utils.parse_utils import spit_arguments_list_string, ensure_is_not_option_argument
 from shellcheck_lib.test_case.sections import common as i
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser, \
     SingleInstructionInvalidArgumentException, SingleInstructionParserSource
@@ -38,43 +38,41 @@ class Parser(SingleInstructionParser):
         if len(arguments) != 2:
             raise SingleInstructionInvalidArgumentException('Invalid syntax')
         file_argument = arguments[0]
+        ensure_is_not_option_argument(file_argument)
+        file_reference = file_ref.rel_cwd(file_argument)
         del arguments[0]
-        return self._parse_type(file_argument, arguments)
+        expected_properties = self._parse_properties(arguments)
+        return _Instruction(file_reference, expected_properties)
 
     @staticmethod
-    def _parse_type(file_name: str,
-                    arguments: list) -> AssertPhaseInstruction:
+    def _parse_properties(arguments: list) -> FilePropertiesCheck:
         num_arguments = len(arguments)
         if num_arguments == 0:
-            raise SingleInstructionInvalidArgumentException('file/type expects a type argument')
+            raise SingleInstructionInvalidArgumentException('Missing type argument')
         if num_arguments > 1:
-            raise SingleInstructionInvalidArgumentException('file/type expects a single type argument')
+            raise SingleInstructionInvalidArgumentException('Expecting a single type argument')
         try:
-            return InstructionForFileType(file_name, FILE_TYPES[arguments[0]])
+            file_type = FILE_TYPES[arguments[0]]
+            follow_sym_links = file_type is not FileType.SYMLINK
+            return must_exist_as(file_type,
+                                 follow_sym_links)
         except KeyError:
             raise SingleInstructionInvalidArgumentException('Invalid file type: ' + arguments[0])
 
 
-class InstructionForFileType(instruction_utils.InstructionWithoutValidationBase):
+class _Instruction(instruction_utils.InstructionWithoutValidationBase):
     def __init__(self,
-                 file_name_relative_current_directory: str,
-                 expected_file_type: FileType):
-        self._file_name_relative_current_directory = file_name_relative_current_directory
-        self._expected_file_type = expected_file_type
+                 file_reference: file_ref.FileRef,
+                 expected_file_properties: FilePropertiesCheck):
+        self._file_reference = file_reference
+        self._expected_file_properties = expected_file_properties
 
     def main(self,
              environment: i.GlobalEnvironmentForPostEdsPhase,
              os_services: OsServices) -> pfh.PassOrFailOrHardError:
-        file_path = pathlib.Path(self._file_name_relative_current_directory)
-        if not file_path.exists():
-            return pfh.new_pfh_fail('File does not exist: ' + self._file_name_relative_current_directory)
-        if self._expected_file_type is FileType.REGULAR:
-            if not file_path.is_file():
-                return pfh.new_pfh_fail('Not a regular file: ' + self._file_name_relative_current_directory)
-        elif self._expected_file_type is FileType.DIRECTORY:
-            if not file_path.is_dir():
-                return pfh.new_pfh_fail('Not a directory: ' + self._file_name_relative_current_directory)
-        elif self._expected_file_type is FileType.SYMLINK:
-            if not file_path.is_symlink():
-                return pfh.new_pfh_fail('Not a symlink: ' + self._file_name_relative_current_directory)
+        failure_message = post_eds_failure_message_or_none(FileRefCheck(self._file_reference,
+                                                                        self._expected_file_properties),
+                                                           environment)
+        if failure_message is not None:
+            return pfh.new_pfh_fail(failure_message)
         return pfh.new_pfh_pass()
