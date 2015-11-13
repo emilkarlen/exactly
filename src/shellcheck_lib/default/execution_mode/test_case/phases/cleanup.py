6538a62e90b595e13cccd214d6797a5cc414c2c5
Emil Karlén <emil.karlen@fripost.org>
2015-11-14 00:24:46 +0100

    Adds pwd instruction to setup, assert, cleanup

--- src/shellcheck_lib/default/execution_mode/test_case/phases/cleanup.py
+++ src/shellcheck_lib/default/execution_mode/test_case/phases/cleanup.py
@@ -1,10 +1,15 @@
 from shellcheck_lib.default.execution_mode.test_case.instruction_setup import SingleInstructionSetup
-from shellcheck_lib.instructions.cleanup import shell as cleanup_shell
+from shellcheck_lib.instructions.cleanup import shell
+from shellcheck_lib.instructions.cleanup import change_dir
 
 INSTRUCTIONS = {
+    'pwd':
+        SingleInstructionSetup(
+            change_dir.Parser(),
+            change_dir.DESCRIPTION),
     'shell':
         SingleInstructionSetup(
-            cleanup_shell.Parser(),
-            cleanup_shell.DESCRIPTION),
+            shell.Parser(),
+            shell.DESCRIPTION),
 
 }
---
+++ src/shellcheck_lib/instructions/cleanup/change_dir.py
@@ -0,0 +1,27 @@
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser, \
+    SingleInstructionParserSource
+from shellcheck_lib.instructions.multi_phase_instructions import change_dir as cd_utils
+from shellcheck_lib.test_case.sections.cleanup import CleanupPhaseInstruction
+from shellcheck_lib.test_case.sections.common import GlobalEnvironmentForPostEdsPhase
+from shellcheck_lib.test_case.sections.result import sh
+from shellcheck_lib.test_case.os_services import OsServices
+
+DESCRIPTION = cd_utils.DESCRIPTION
+
+
+class Parser(SingleInstructionParser):
+    def apply(self, source: SingleInstructionParserSource) -> CleanupPhaseInstruction:
+        destination_directory = cd_utils.parse(source.instruction_argument)
+        return _Instruction(destination_directory)
+
+
+class _Instruction(CleanupPhaseInstruction):
+    def __init__(self, destination_directory: cd_utils.DestinationDirectory):
+        self.destination_directory = destination_directory
+
+    def main(self,
+             environment: GlobalEnvironmentForPostEdsPhase,
+             os_services: OsServices) -> sh.SuccessOrHardError:
+        error_message = cd_utils.change_dir(self.destination_directory,
+                                            environment.eds)
+        return sh.new_sh_success() if error_message is None else sh.new_sh_hard_error(error_message)
