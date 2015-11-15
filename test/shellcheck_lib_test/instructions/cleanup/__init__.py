2aaccce5b742670bc46f13c57d1402df035775fb
Emil Karlén <emil@member.fsf.org>
2015-11-15 18:56:31 +0100

    Test of assert/change_dir, cleanup/change_dir

--- test/shellcheck_lib_test/instructions/cleanup/__init__.py
+++ test/shellcheck_lib_test/instructions/cleanup/__init__.py
@@ -2,12 +2,14 @@ import unittest
 
 from shellcheck_lib_test.instructions.cleanup import test_resources
 from . import shell
+from . import change_dir
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources.suite())
     ret_val.addTest(shell.suite())
+    ret_val.addTest(change_dir.suite())
     return ret_val
 
 
