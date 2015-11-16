1d9ab6593ac460e67fd86bd667dd175f4e8e89d5
Emil Karlén <emil@member.fsf.org>
2015-11-16 20:54:58 +0100

    Refactoring: Adapting FileRef (a bit) to previously extracted functionality

--- src/shellcheck_lib/instructions/utils/file_ref.py
+++ src/shellcheck_lib/instructions/utils/file_ref.py
@@ -1,5 +1,6 @@
 import pathlib
 
+from shellcheck_lib.execution.execution_directory_structure import ExecutionDirectoryStructure
 from shellcheck_lib.test_case.sections.common import HomeAndEds
 
 
@@ -28,19 +29,36 @@ class FileRef:
         return self.__exists_pre_eds
 
 
+class FileRefRelEds(FileRef):
+    def __init__(self, file_name: str):
+        super().__init__(False, file_name)
+
+    def file_path_rel_eds(self, eds: ExecutionDirectoryStructure) -> pathlib.Path:
+        raise NotImplementedError()
+
+    def file_path_post_eds(self, home_and_eds: HomeAndEds) -> pathlib.Path:
+        return self.file_path_rel_eds(home_and_eds.eds)
+
+    def file_path_pre_eds(self, home_dir_path: pathlib.Path) -> pathlib.Path:
+        """
+        Can only be used if the files exists pre-EDS.
+        """
+        raise ValueError('This file does not exist before EDS is constructed')
+
+
 def rel_home(file_name: str) -> FileRef:
     return _FileRefRelHome(file_name)
 
 
-def rel_cwd(file_name: str) -> FileRef:
+def rel_cwd(file_name: str) -> FileRefRelEds:
     return _FileRefRelCwd(file_name)
 
 
-def rel_tmp_internal(file_name: str) -> FileRef:
+def rel_tmp_internal(file_name: str) -> FileRefRelEds:
     return _FileRefRelTmpInternal(file_name)
 
 
-def rel_tmp_user(file_name: str) -> FileRef:
+def rel_tmp_user(file_name: str) -> FileRefRelEds:
     return _FileRefRelTmpUser(file_name)
 
 
@@ -58,39 +76,25 @@ class _FileRefRelHome(FileRef):
         return self.file_path_pre_eds(home_and_eds.home_dir_path)
 
 
-class _FileRefForFileThatDoesNotExistPreEdsBase(FileRef):
-    def __init__(self, file_name: str):
-        super().__init__(False, file_name)
-
-    def file_path_post_eds(self, home_and_eds: HomeAndEds) -> pathlib.Path:
-        raise NotImplementedError()
-
-    def file_path_pre_eds(self, home_dir_path: pathlib.Path) -> pathlib.Path:
-        """
-        Can only be used if the files exists pre-EDS.
-        """
-        raise ValueError('This file does not exist before EDS is constructed')
-
-
-class _FileRefRelCwd(_FileRefForFileThatDoesNotExistPreEdsBase):
+class _FileRefRelCwd(FileRefRelEds):
     def __init__(self, file_name: str):
         super().__init__(file_name)
 
-    def file_path_post_eds(self, home_and_eds: HomeAndEds) -> pathlib.Path:
+    def file_path_rel_eds(self, eds: ExecutionDirectoryStructure) -> pathlib.Path:
         return pathlib.Path.cwd() / self._file_name
 
 
-class _FileRefRelTmpUser(_FileRefForFileThatDoesNotExistPreEdsBase):
+class _FileRefRelTmpUser(FileRefRelEds):
     def __init__(self, file_name: str):
         super().__init__(file_name)
 
-    def file_path_post_eds(self, home_and_eds: HomeAndEds) -> pathlib.Path:
-        return home_and_eds.eds.tmp.user_dir / self._file_name
+    def file_path_rel_eds(self, eds: ExecutionDirectoryStructure) -> pathlib.Path:
+        return eds.tmp.user_dir / self._file_name
 
 
-class _FileRefRelTmpInternal(_FileRefForFileThatDoesNotExistPreEdsBase):
+class _FileRefRelTmpInternal(FileRefRelEds):
     def __init__(self, file_name: str):
         super().__init__(file_name)
 
-    def file_path_post_eds(self, home_and_eds: HomeAndEds) -> pathlib.Path:
-        return home_and_eds.eds.tmp.internal_dir / self._file_name
+    def file_path_rel_eds(self, eds: ExecutionDirectoryStructure) -> pathlib.Path:
+        return eds.tmp.internal_dir / self._file_name
