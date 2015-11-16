b0e34f3a6fc67a093b4bc79c574964d92452bd49
Emil Karlén <emil.karlen@fripost.org>
2015-11-16 23:49:46 +0100

    Tests of Here Document parser: Must match whole line

--- test/shellcheck_lib_test/instructions/utils/here_document_parser.py
+++ test/shellcheck_lib_test/instructions/utils/here_document_parser.py
@@ -1,10 +1,9 @@
-import shlex
 import unittest
 
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionInvalidArgumentException, SingleInstructionParserSource
+    SingleInstructionInvalidArgumentException
 from shellcheck_lib.instructions.utils import here_document_parser as sut
-from shellcheck_lib_test.instructions.test_resources.utils import multi_line_source
+from shellcheck_lib_test.instructions.test_resources.utils import argument_list_source
 
 
 class TestFailingScenarios(unittest.TestCase):
@@ -13,36 +12,52 @@ class TestFailingScenarios(unittest.TestCase):
             first_line_arguments = []
             sut.parse_as_last_argument(True,
                                        first_line_arguments,
-                                       new_source(first_line_arguments))
+                                       argument_list_source(first_line_arguments))
 
     def test_fail_when_superfluous_arguments__document_is_mandatory(self):
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             first_line_arguments = ['<<MARKER', 'superfluous argument']
             sut.parse_as_last_argument(True,
                                        first_line_arguments,
-                                       new_source(first_line_arguments))
+                                       argument_list_source(first_line_arguments))
 
     def test_fail_when_superfluous_arguments__document_is_not_mandatory(self):
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             first_line_arguments = ['<<MARKER', 'superfluous argument']
             sut.parse_as_last_argument(False,
                                        first_line_arguments,
-                                       new_source(first_line_arguments))
+                                       argument_list_source(first_line_arguments))
 
     def test_fail_when_marker_not_found(self):
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             first_line_arguments = ['<<MARKER']
             sut.parse_as_last_argument(False,
                                        first_line_arguments,
-                                       new_source(first_line_arguments,
-                                                  ['not marker',
-                                                   '[section]']))
+                                       argument_list_source(first_line_arguments,
+                                                            ['not marker',
+                                                             '[section]']))
+
+    def test_fail_when_marker_not_found__must_match_whole_line__space_before(self):
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            first_line_arguments = ['<<MARKER']
+            sut.parse_as_last_argument(False,
+                                       first_line_arguments,
+                                       argument_list_source(first_line_arguments,
+                                                            ['  MARKER']))
+
+    def test_fail_when_marker_not_found__must_match_whole_line__space_after(self):
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            first_line_arguments = ['<<MARKER']
+            sut.parse_as_last_argument(False,
+                                       first_line_arguments,
+                                       argument_list_source(first_line_arguments,
+                                                            ['MARKER  ']))
 
 
 class TestSuccessfulScenarios(unittest.TestCase):
     def test_empty_list_of_lines__not_mandatory(self):
         first_line_arguments = ['<<MARKER']
-        source = new_source(first_line_arguments, ['MARKER'])
+        source = argument_list_source(first_line_arguments, ['MARKER'])
         actual = sut.parse_as_last_argument(False,
                                             first_line_arguments,
                                             source)
@@ -52,7 +67,7 @@ class TestSuccessfulScenarios(unittest.TestCase):
 
     def test_empty_list_of_lines__mandatory(self):
         first_line_arguments = ['<<___']
-        source = new_source(first_line_arguments, ['___'])
+        source = argument_list_source(first_line_arguments, ['___'])
         actual = sut.parse_as_last_argument(True,
                                             first_line_arguments,
                                             source)
@@ -62,10 +77,10 @@ class TestSuccessfulScenarios(unittest.TestCase):
 
     def test_single_line__not_mandatory(self):
         first_line_arguments = ['<<MARKER']
-        source = new_source(first_line_arguments,
-                            ['only line',
-                             'MARKER',
-                             'following line'])
+        source = argument_list_source(first_line_arguments,
+                                      ['only line',
+                                       'MARKER',
+                                       'following line'])
         actual = sut.parse_as_last_argument(False,
                                             first_line_arguments,
                                             source)
@@ -77,19 +92,13 @@ class TestSuccessfulScenarios(unittest.TestCase):
 
     def test_single_line__mandatory(self):
         first_line_arguments = ['<<123']
-        source = new_source(first_line_arguments, ['only line', '123'])
+        source = argument_list_source(first_line_arguments, ['only line', '123'])
         actual = sut.parse_as_last_argument(True,
                                             first_line_arguments,
                                             source)
         self.assertEquals(['only line'],
                           actual)
         self.assertFalse(source.line_sequence.has_next())
-
-
-def new_source(arguments: list,
-               following_lines: iter=()) -> SingleInstructionParserSource:
-    return multi_line_source(' '.join(map(shlex.quote, arguments)),
-                             following_lines)
 
 
 def suite():
