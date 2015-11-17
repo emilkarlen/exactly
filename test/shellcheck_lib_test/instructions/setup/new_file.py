4bdb6e94fca2ae57941653f445d3119d55348ebc
Emil Karlén <emil@member.fsf.org>
2015-11-17 20:55:39 +0100

    setup/file: New test that involves Here Document

--- test/shellcheck_lib_test/instructions/setup/new_file.py
+++ test/shellcheck_lib_test/instructions/setup/new_file.py
@@ -1,11 +1,12 @@
 import unittest
 
+from shellcheck_lib.general.string import lines_content
 from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Flow, TestCaseBase
 from shellcheck_lib_test.instructions.test_resources import sh_check
 from shellcheck_lib_test.instructions.test_resources.eds_contents_check import ActRootContainsExactly
 from shellcheck_lib_test.instructions.test_resources.eds_populator import act_dir_contents
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
-from shellcheck_lib_test.util.file_structure import DirContents, empty_dir, Dir, empty_file
+from shellcheck_lib_test.instructions.test_resources.utils import new_source, argument_list_source
+from shellcheck_lib_test.util.file_structure import DirContents, empty_dir, Dir, empty_file, File
 from shellcheck_lib.instructions.setup import new_file as sut
 
 
@@ -23,6 +24,19 @@ class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBase):
                 ]))),
             new_source('instruction-name',
                        'existing-directory/file-name.txt')
+        )
+
+    def test_single_line_contents(self):
+        self._check(
+            Flow(
+                sut.Parser(),
+                expected_main_side_effects_on_files=ActRootContainsExactly(DirContents([
+                    File('file name',
+                         lines_content(['single line']))
+                ]))),
+            argument_list_source(['file name', '<<MARKER'],
+                                 ['single line',
+                                  'MARKER'])
         )
 
     def test_argument_is_existing_file(self):
