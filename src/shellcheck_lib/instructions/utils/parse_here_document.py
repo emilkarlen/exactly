5a5d964bc809518ed796483ad0edc051ac201ca5
Emil Karlén <emil.karlen@fripost.org>
2015-11-17 22:51:03 +0100

    Here Document parsing: Utility method to parse as non-last argument

--- src/shellcheck_lib/instructions/utils/parse_here_document.py
+++ src/shellcheck_lib/instructions/utils/parse_here_document.py
@@ -6,25 +6,43 @@ from shellcheck_lib.document.parser_implementations.instruction_parser_for_singl
 DOCUMENT_TOKEN_RE = re.compile('(<<)([0-9a-zA-Z_-]+)')
 
 
-def parse_as_last_argument(here_document_is_mandatory: bool,
-                           first_line_arguments: list,
-                           source: SingleInstructionParserSource) -> list:
+class HereDocumentContentsParsingException(SingleInstructionInvalidArgumentException):
+    def __init__(self, error_message: str):
+        super().__init__(error_message)
+
+
+def parse(first_line_arguments: list,
+          source: SingleInstructionParserSource) -> (list, list):
     if not first_line_arguments:
-        if here_document_is_mandatory:
-            raise SingleInstructionInvalidArgumentException('Missing here-document')
-        return []
+        raise SingleInstructionInvalidArgumentException('Missing here-document')
     first_argument = first_line_arguments[0]
     marker_match = re.fullmatch(DOCUMENT_TOKEN_RE, first_argument)
     if not marker_match:
         raise SingleInstructionInvalidArgumentException('Not a Here Document token: {}'.format(first_argument))
-    if len(first_line_arguments) > 1:
-        raise SingleInstructionInvalidArgumentException('Superfluous arguments: {}'.format(first_line_arguments[1:]))
     marker = marker_match.group(2)
-    ret_val = []
+    here_doc = []
     line_sequence = source.line_sequence
     while line_sequence.has_next():
         line = line_sequence.next_line()
         if line == marker:
-            return ret_val
-        ret_val.append(line)
-    raise SingleInstructionInvalidArgumentException('End Of File reached without finding Marker: {}'.format(marker))
+            return here_doc, first_line_arguments[1:]
+        here_doc.append(line)
+    raise HereDocumentContentsParsingException('End Of File reached without finding Marker: {}'.format(marker))
+
+
+def parse_as_last_argument(here_document_is_mandatory: bool,
+                           first_line_arguments: list,
+                           source: SingleInstructionParserSource) -> list:
+    try:
+        (here_doc, remaining_arguments) = parse(first_line_arguments, source)
+        if remaining_arguments:
+            raise SingleInstructionInvalidArgumentException('Superfluous arguments: {}'.format(remaining_arguments))
+        return here_doc
+    except HereDocumentContentsParsingException as ex:
+        raise ex
+    except SingleInstructionInvalidArgumentException:
+        if here_document_is_mandatory:
+            raise SingleInstructionInvalidArgumentException('Missing here-document')
+        if first_line_arguments:
+            raise SingleInstructionInvalidArgumentException('Superfluous arguments: {}'.format(first_line_arguments))
+        return []
