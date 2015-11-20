573d1885f57ea139d4f453c943f6086502f9944c
Emil Karlén <emil.karlen@fripost.org>
2015-11-20 14:17:45 +0100

    Default impl of AssertPhaseInstruction/validate (and removes unused module)

--- src/shellcheck_lib/test_case/sections/assert_.py
+++ src/shellcheck_lib/test_case/sections/assert_.py
@@ -1,6 +1,6 @@
 from shellcheck_lib.document.model import Instruction
-from shellcheck_lib.test_case.sections.result.pfh import PassOrFailOrHardError
-from shellcheck_lib.test_case.sections.result.svh import SuccessOrValidationErrorOrHardError
+from shellcheck_lib.test_case.sections.result import pfh
+from shellcheck_lib.test_case.sections.result import svh
 from shellcheck_lib.test_case.sections.common import GlobalEnvironmentForPostEdsPhase
 from shellcheck_lib.test_case.os_services import OsServices
 
@@ -11,10 +11,10 @@ class AssertPhaseInstruction(Instruction):
     """
 
     def validate(self,
-                 global_environment: GlobalEnvironmentForPostEdsPhase) -> SuccessOrValidationErrorOrHardError:
-        raise NotImplementedError()
+                 global_environment: GlobalEnvironmentForPostEdsPhase) -> svh.SuccessOrValidationErrorOrHardError:
+        return svh.new_svh_success()
 
     def main(self,
              environment: GlobalEnvironmentForPostEdsPhase,
-             os_services: OsServices) -> PassOrFailOrHardError:
+             os_services: OsServices) -> pfh.PassOrFailOrHardError:
         raise NotImplementedError()
