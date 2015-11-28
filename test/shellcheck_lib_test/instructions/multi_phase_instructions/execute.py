f879abf32c70254557bbf705e78fb71f5c8049f6
Emil Karlén <emil@member.fsf.org>
2015-11-28 11:55:11 +0100

    Refactoring: New value assertion combinator: And

--- test/shellcheck_lib_test/instructions/multi_phase_instructions/execute.py
+++ test/shellcheck_lib_test/instructions/multi_phase_instructions/execute.py
@@ -7,7 +7,7 @@ from shellcheck_lib.instructions.multi_phase_instructions import execute as sut
 from shellcheck_lib.test_case.sections.common import HomeAndEds
 from shellcheck_lib_test.instructions.test_resources.utils import single_line_source
 from shellcheck_lib_test.util import home_and_eds_test
-from shellcheck_lib_test.util.value_assertion import ValueAssertion
+from shellcheck_lib_test.util import value_assertion as va
 
 
 class ExecuteAction(home_and_eds_test.Action):
@@ -29,38 +29,50 @@ class TestCaseBase(home_and_eds_test.TestCaseBase):
                            check)
 
 
-class ExitCodeAndStderrContentsIs(ValueAssertion):
+class ExitCodeIs(va.ValueAssertion):
     def __init__(self,
-                 exit_code: int,
-                 stderr_contents: str):
+                 exit_code: int):
         self.exit_code = exit_code
-        self.stderr_contents = stderr_contents
 
     def apply(self, put: unittest.TestCase, value):
         put.assertEquals(self.exit_code,
                          value[0],
                          'Exit code')
+
+
+class StderrContentsIs(va.ValueAssertion):
+    def __init__(self,
+                 stderr_contents: str):
+        self.stderr_contents = stderr_contents
+
+    def apply(self, put: unittest.TestCase, value):
         put.assertEqual(self.stderr_contents,
                         value[1],
                         'Stderr contents')
 
 
+def result_is(exitcode: int,
+              stderr_contents: str) -> va.ValueAssertion:
+    return va.And([ExitCodeIs(exitcode),
+                   StderrContentsIs(stderr_contents)])
+
+
 class TestExecuteProgramWithShellArgumentList(TestCaseBase):
     def test_check_zero_exit_code(self):
         self._test_source(single_line_source(python_with_command_argument_on_command_line('exit(0)')),
-                          home_and_eds_test.Check(expected_action_result=ExitCodeAndStderrContentsIs(0,
-                                                                                                     None)))
+                          home_and_eds_test.Check(expected_action_result=result_is(0,
+                                                                                   None)))
 
     def test_check_non_zero_exit_code(self):
         self._test_source(single_line_source(python_with_command_argument_on_command_line('exit(1)')),
-                          home_and_eds_test.Check(expected_action_result=ExitCodeAndStderrContentsIs(1,
-                                                                                                     '')))
+                          home_and_eds_test.Check(expected_action_result=result_is(1,
+                                                                                   '')))
 
     def test_check_non_zero_exit_code_with_output_to_stderr(self):
         python_program = 'import sys; sys.stderr.write(\\"on stderr\\"); exit(2)'
         self._test_source(single_line_source(python_with_command_argument_on_command_line(python_program)),
-                          home_and_eds_test.Check(expected_action_result=ExitCodeAndStderrContentsIs(2,
-                                                                                                     'on stderr')))
+                          home_and_eds_test.Check(expected_action_result=result_is(2,
+                                                                                   'on stderr')))
 
 
 def python_with_command_argument_on_command_line(command_argument: str) -> str:
--- test/shellcheck_lib_test/util/value_assertion.py
+++ test/shellcheck_lib_test/util/value_assertion.py
@@ -15,15 +15,38 @@ class AnythingGoes(ValueAssertion):
         pass
 
 
-class ValueIsNone(ValueAssertion):
+class And(ValueAssertion):
+    def __init__(self,
+                 assertions: list):
+        self.assertions = assertions
+
     def apply(self,
               put: unittest.TestCase,
               value):
-        put.assertIsNone(value)
+        for assertion in self.assertions:
+            assert isinstance(assertion, ValueAssertion)
+            assertion.apply(put, value)
+
+
+class ValueIsNone(ValueAssertion):
+    def __init__(self,
+                 message: str = None):
+        self.message = message
+
+    def apply(self,
+              put: unittest.TestCase,
+              value):
+        put.assertIsNone(value,
+                         self.message)
 
 
 class ValueIsNotNone(ValueAssertion):
+    def __init__(self,
+                 message: str = None):
+        self.message = message
+
     def apply(self,
               put: unittest.TestCase,
               value):
-        put.assertIsNotNone(value)
+        put.assertIsNotNone(value,
+                            self.message)
