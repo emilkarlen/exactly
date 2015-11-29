f4350082cac5b6ee0065d2b516054bda948721c7
Emil Karlén <emil@member.fsf.org>
2015-11-29 01:26:27 +0100

    New instruction: cleanup/execute

--- test/shellcheck_lib_test/instructions/cleanup/__init__.py
+++ test/shellcheck_lib_test/instructions/cleanup/__init__.py
@@ -1,14 +1,16 @@
 import unittest
 
 from shellcheck_lib_test.instructions.cleanup import test_resources
-from . import shell
 from . import change_dir
+from . import execute
+from . import shell
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources.suite())
     ret_val.addTest(shell.suite())
+    ret_val.addTest(execute.suite())
     ret_val.addTest(change_dir.suite())
     return ret_val
 
