c4d56b89e98a3e8f0d87c4ff4c2e50fe29df52f7
Emil Karlén <emil.karlen@fripost.org>
2015-12-19 15:09:38 +0100

    Wrapping of text-blocks in Text-objects

--- src/shellcheck_lib/general/textformat/structure.py
+++ src/shellcheck_lib/general/textformat/structure.py
@@ -5,21 +5,31 @@ class ParagraphItem:
     pass
 
 
+class Text(tuple):
+    def __new__(cls,
+                value: str):
+        return tuple.__new__(cls, (value,))
+
+    @property
+    def value(self) -> str:
+        return self[0]
+
+
 class Paragraph(ParagraphItem):
     def __init__(self,
                  start_on_new_line_blocks: list):
         """
-        :param start_on_new_line_blocks: Each element is a text that should start
+        :param start_on_new_line_blocks: [Text] Each element is a text that should start
         on a new line.
         """
         self.start_on_new_line_blocks = start_on_new_line_blocks
 
 
 class ParagraphItemVisitor:
-    def visit(self, text_item: ParagraphItem):
-        if isinstance(text_item, Paragraph):
-            return self.visit_paragraph(text_item)
-        raise ValueError('Unknown TextItem: ' + str(type(text_item)))
+    def visit(self, item: ParagraphItem):
+        if isinstance(item, Paragraph):
+            return self.visit_paragraph(item)
+        raise ValueError('Unknown {}: {}'.format(ParagraphItem.__name__) + str(type(item)))
 
     def visit_paragraph(self, paragraph: Paragraph):
         raise NotImplemented()
