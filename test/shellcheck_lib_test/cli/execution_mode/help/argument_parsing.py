ea43c51467b554a8d140f74fb48632529d9396ea
Emil Karlén <emil.karlen@fripost.org>
2015-12-22 00:33:36 +0100

    help: New test: test_instruction_set

--- test/shellcheck_lib_test/cli/execution_mode/help/argument_parsing.py
+++ test/shellcheck_lib_test/cli/execution_mode/help/argument_parsing.py
@@ -4,16 +4,27 @@ from shellcheck_lib.cli.execution_mode.help import argument_parsing as sut
 from shellcheck_lib.test_case.instruction_setup import InstructionsSetup
 
 
-class TestCase(unittest.TestCase):
+class TestTestCaseHelp(unittest.TestCase):
     def test_program(self):
         actual = sut.parse(empty_instruction_set(), [])
         self.assertIsInstance(actual,
-                              sut.HelpSettings)
+                              sut.ProgramHelpSettings,
+                              'Expecting settings for Program')
+
+    def test_instruction_set(self):
+        actual = sut.parse(empty_instruction_set(), [sut.INSTRUCTIONS])
+        self.assertIsInstance(actual,
+                              sut.TestCaseHelpSettings,
+                              'Expecting settings for Test Case')
+        assert isinstance(actual, sut.TestCaseHelpSettings)
+        self.assertIs(sut.TestCaseHelpItem.INSTRUCTION_SET,
+                      actual.item,
+                      'Item should denote help for Instruction Set')
 
 
 def suite():
     ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestCase))
+    ret_val.addTest(unittest.makeSuite(TestTestCaseHelp))
     return ret_val
 
 
