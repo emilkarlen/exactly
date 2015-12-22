78486e32e6731c5ba7e5ae56a0fe69358dd65245
Emil Karlén <emil@member.fsf.org>
2015-12-23 00:36:49 +0100

    help: Refactoring: Moves instruction help rendition to test_case (sub)package

--- src/shellcheck_lib/cli/execution_mode/help/contents/test_case/instruction_set.py
+++ src/shellcheck_lib/cli/execution_mode/help/contents/test_case/instruction_set.py
@@ -1,7 +1,7 @@
 from shellcheck_lib.general.textformat.structure import document as doc
 from shellcheck_lib.general.textformat.structure import lists
 from shellcheck_lib.general.textformat.structure.core import Text
-from shellcheck_lib.general.textformat.structure.paragraph import para
+from shellcheck_lib.test_case.help.render.instruction import instruction_set_list_item
 from shellcheck_lib.test_case.instruction_setup import InstructionsSetup
 
 
@@ -17,8 +17,8 @@ def instruction_set(instruction_setup: InstructionsSetup) -> doc.SectionContents
 def _instruction_list(instruction_set_dict):
     instruction_list_items = []
     for (instruction_name, instruction_setup) in instruction_set_dict.items():
-        description_para = para(instruction_setup.description.single_line_description())
-        instruction_list_items.append(lists.HeaderValueListItem(Text(instruction_name),
-                                                                [description_para]))
+        description = instruction_setup.description
+        list_item = instruction_set_list_item(description)
+        instruction_list_items.append(list_item)
     return lists.HeaderValueList(lists.ListType.VARIABLE_LIST,
                                  instruction_list_items)
