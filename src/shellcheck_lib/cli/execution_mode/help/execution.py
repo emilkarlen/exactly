05a772d4cf8170205eed514e5a2d9333311a7fa0
Emil Karlén <emil.karlen@fripost.org>
2015-12-22 15:00:59 +0100

    Removes unused code (old help func for printing instruction set)

--- src/shellcheck_lib/cli/execution_mode/help/execution.py
+++ src/shellcheck_lib/cli/execution_mode/help/execution.py
@@ -3,8 +3,6 @@ import shutil
 
 from shellcheck_lib.cli.execution_mode.help.settings import HelpSettings, TestCaseHelpSettings, TestCaseHelpItem, \
     TestSuiteHelpSettings, TestSuiteHelpItem, ProgramHelpSettings
-from shellcheck_lib.execution import phase_step
-from shellcheck_lib.general.std import StdOutputFiles
 from shellcheck_lib.general.textformat.formatting import section, paragraph_item
 from shellcheck_lib.general.textformat.formatting.wrapper import Wrapper
 from shellcheck_lib.general.textformat.structure import document as doc
@@ -72,28 +70,3 @@ def doc_for(instruction_setup: InstructionsSetup,
             return ts_help.section(settings.name)
         raise ValueError('Invalid %s: %s' % (str(TestSuiteHelpItem), str(item)))
     raise ValueError('Invalid %s: %s' % (str(HelpSettings), str(type(settings))))
-
-
-class PrintInstructionsPerPhase:
-    def __init__(self,
-                 output: StdOutputFiles):
-        self._std = output
-
-    def apply(self, instruction_setup: InstructionsSetup):
-        self._print_phase(phase_step.ANONYMOUS, instruction_setup.config_instruction_set)
-        self._print_phase(phase_step.SETUP, instruction_setup.setup_instruction_set)
-        self._print_phase(phase_step.ASSERT, instruction_setup.assert_instruction_set)
-        self._print_phase(phase_step.CLEANUP, instruction_setup.cleanup_instruction_set)
-
-    def _print_phase(self,
-                     phase_name: str,
-                     instruction_set: dict):
-        if not instruction_set:
-            return
-        self._writeln(phase_name)
-        for instruction_name, instruction_setup in instruction_set.items():
-            self._writeln(instruction_name + ' : ' + instruction_setup.description.single_line_description())
-
-    def _writeln(self, s: str):
-        self._std.out.write(s)
-        self._std.out.write(os.linesep)
