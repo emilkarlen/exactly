bc38e1498160e27d544509f4a9199657961f1f56
Emil Karlén <emil.karlen@fripost.org>
2015-12-25 12:19:00 +0100

    parse Paragraphs: First test

---
+++ test/shellcheck_lib_test/general/textformat/parse.py
@@ -0,0 +1,29 @@
+import unittest
+
+from shellcheck_lib.general.textformat import parse as sut
+from shellcheck_lib_test.general.textformat.test_resources import parse as test_resource
+
+
+class TestEmpty(unittest.TestCase):
+    def test_empty_input(self):
+        test_resource.check(self,
+                            [],
+                            sut.parse([]))
+
+
+class TestSingleParagraph(unittest.TestCase):
+    def test_single_text_block_on_single_line(self):
+        test_resource.check(self,
+                            [sut.Paragraph([sut.Text('the text block')])],
+                            sut.parse(['the text block']))
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(unittest.makeSuite(TestEmpty))
+    ret_val.addTest(unittest.makeSuite(TestSingleParagraph))
+    return ret_val
+
+
+if __name__ == '__main__':
+    unittest.main()
