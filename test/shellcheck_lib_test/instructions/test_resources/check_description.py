c3823cbfebf2ecd2061f3fcfdce5ed269c28d618
Emil Karlén <emil.karlen@fripost.org>
2015-12-25 19:45:02 +0100

    Improved test of concrete instruction descriptions (and bug fixes)

--- test/shellcheck_lib_test/instructions/test_resources/check_description.py
+++ test/shellcheck_lib_test/instructions/test_resources/check_description.py
@@ -3,9 +3,9 @@ import unittest
 from shellcheck_lib.general.textformat.formatting import paragraph_item
 from shellcheck_lib.general.textformat.formatting import section
 from shellcheck_lib.general.textformat.formatting.wrapper import Wrapper
-from shellcheck_lib.general.textformat.structure import lists
-from shellcheck_lib.test_case.help.instruction_description import Description
-from shellcheck_lib.test_case.help.render import instruction
+from shellcheck_lib.test_case.help.instruction_description import Description, InvokationVariant, \
+    SyntaxElementDescription
+from shellcheck_lib_test.general.textformat.test_resources import structure as struct_check
 
 
 class TestDescriptionBase(unittest.TestCase):
@@ -21,12 +21,61 @@ class TestDescriptionBase(unittest.TestCase):
         self.formatter = section.Formatter(paragraph_item.Formatter(Wrapper(page_width=100)))
         self.description = self._description()
 
-    def test_instruction_man_page(self):
-        section_contents = instruction.instruction_man_page(self.description)
-        self.formatter.format_section_contents(section_contents)
+    def test_instruction_name(self):
+        actual = self.description.instruction_name()
+        self.assertIsInstance(actual, str)
 
-    def test_instruction_set_list_item(self):
-        list_item = instruction.instruction_set_list_item(self.description)
-        the_list = lists.HeaderValueList(lists.ListType.VARIABLE_LIST,
-                                         [list_item])
-        self.formatter.paragraph_item_formatter.format_header_value_list(the_list)
+    def test_main_description_rest(self):
+        actual = self.description.main_description_rest()
+        struct_check.paragraph_item_list(self, actual)
+
+    def test_invokation_variants(self):
+        actual = self.description.invokation_variants()
+        struct_check.check_list(InvokationVariantChecker,
+                                struct_check.CheckerWithMsgPrefix(self),
+                                actual)
+
+    def test_syntax_element_descriptions(self):
+        actual = self.description.syntax_element_descriptions()
+        struct_check.check_list(SyntaxElementDescriptionChecker,
+                                struct_check.CheckerWithMsgPrefix(self),
+                                actual)
+
+
+class InvokationVariantChecker(struct_check.Assertion):
+    def __init__(self,
+                 checker: struct_check.CheckerWithMsgPrefix):
+        self.checker = checker
+
+    def apply(self, x):
+        self.checker.put.assertIsInstance(x,
+                                          InvokationVariant,
+                                          self.checker.msg('Must be instance of %s' % InvokationVariant))
+        assert isinstance(x, InvokationVariant)
+        self.checker.put.assertIsInstance(x.syntax,
+                                          str,
+                                          self.checker.msg('syntax be instance of %s' % str))
+        struct_check.check_list(struct_check.ParagraphItemChecker,
+                                struct_check.new_with_added_prefix('description_rest: ', self.checker),
+                                x.description_rest)
+
+
+class SyntaxElementDescriptionChecker(struct_check.Assertion):
+    def __init__(self,
+                 checker: struct_check.CheckerWithMsgPrefix):
+        self.checker = checker
+
+    def apply(self, x):
+        self.checker.put.assertIsInstance(x,
+                                          SyntaxElementDescription,
+                                          self.checker.msg('Must be instance of %s' % SyntaxElementDescription))
+        assert isinstance(x, SyntaxElementDescription)
+        self.checker.put.assertIsInstance(x.element_name,
+                                          str,
+                                          self.checker.msg('element_name be instance of %s' % str))
+        struct_check.check_list(struct_check.ParagraphItemChecker,
+                                struct_check.new_with_added_prefix('description_rest: ', self.checker),
+                                x.description_rest)
+        struct_check.check_list(InvokationVariantChecker,
+                                struct_check.new_with_added_prefix('invokation_variants: ', self.checker),
+                                x.invokation_variants)
