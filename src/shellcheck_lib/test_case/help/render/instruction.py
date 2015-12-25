a03ae98b6915df2d84329883eb678dcf48bfe6f8
Emil Karlén <emil.karlen@fripost.org>
2015-12-25 14:31:49 +0100

    help: Rendition of instruction help: inclusion of invokation variants

--- src/shellcheck_lib/test_case/help/render/instruction.py
+++ src/shellcheck_lib/test_case/help/render/instruction.py
@@ -1,7 +1,7 @@
 from shellcheck_lib.general.textformat.structure import document as doc, lists
 from shellcheck_lib.general.textformat.structure.core import Text
 from shellcheck_lib.general.textformat.structure.paragraph import para
-from shellcheck_lib.test_case.help.instruction_description import Description
+from shellcheck_lib.test_case.help.instruction_description import Description, InvokationVariant
 
 
 def instruction_man_page(description: Description) -> doc.SectionContents:
@@ -9,13 +9,31 @@ def instruction_man_page(description: Description) -> doc.SectionContents:
     single_line_description_para = para(description.single_line_description())
     main_description_rest = description.main_description_rest()
 
+    sections = _invokation_variants_sections(description.invokation_variants())
     return doc.SectionContents([todo_para,
                                 single_line_description_para] +
                                main_description_rest,
-                               [])
+                               sections)
 
 
 def instruction_set_list_item(description: Description) -> lists.HeaderValueListItem:
     description_para = para(description.single_line_description())
     return lists.HeaderValueListItem(Text(description.instruction_name()),
                                      [description_para])
+
+
+def _invokation_variants_sections(invokation_variants: list) -> list:
+    def variants_list():
+        items = []
+        for invokation_variant in invokation_variants:
+            assert isinstance(invokation_variant, InvokationVariant)
+            items.append(lists.HeaderValueListItem(Text(invokation_variant.syntax),
+                                                   invokation_variant.description_rest))
+        return lists.HeaderValueList(lists.ListType.VARIABLE_LIST,
+                                     items)
+
+    if not invokation_variants:
+        return []
+    return [doc.Section(Text('INVOKATION VARIANTS'),
+                        doc.SectionContents([variants_list()],
+                                            []))]
