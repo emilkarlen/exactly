12d3e37e6ffa29249ffc821b95db6d573f97025e
Emil Karlén <emil@member.fsf.org>
2015-12-26 20:04:07 +0100

    Refactoring: shell: cleanup/shell uses commn shell-functionality

--- src/shellcheck_lib/instructions/multi_phase_instructions/shell.py
+++ src/shellcheck_lib/instructions/multi_phase_instructions/shell.py
@@ -1,3 +1,8 @@
+import subprocess
+
+from shellcheck_lib.document.model import Instruction
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser, \
+    SingleInstructionParserSource, SingleInstructionInvalidArgumentException
 from shellcheck_lib.general.textformat.structure.paragraph import single_para
 from shellcheck_lib.test_case.help.instruction_description import Description, InvokationVariant
 
@@ -18,3 +23,30 @@ class TheDescriptionBase(Description):
                     'PROGRAM ARGUMENT...',
                     single_para('A plain file.')),
         ]
+
+
+class Parser(SingleInstructionParser):
+    def __init__(self,
+                 executor_2_instruction_function):
+        self.executor_2_instruction_function = executor_2_instruction_function
+
+    def apply(self, source: SingleInstructionParserSource) -> Instruction:
+        arguments = source.instruction_argument.strip()
+        if not arguments:
+            msg = 'Program to execute must be given as argument'
+            raise SingleInstructionInvalidArgumentException(msg)
+        executor = Executor(arguments)
+        return self.executor_2_instruction_function(executor)
+
+
+class Executor:
+    def __init__(self,
+                 command: str):
+        self.command = command
+
+    def run(self) -> int:
+        """
+        :return: exitcode
+        """
+        return subprocess.call(self.command,
+                               shell=True)
