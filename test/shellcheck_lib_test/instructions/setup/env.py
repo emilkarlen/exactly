e3ad4bc06bda79482cf23da47db31d5571ca9540
Emil Karlén <emil@member.fsf.org>
2015-12-26 22:07:40 +0100

    Refactoring of test: setup/env: using new_source2

--- test/shellcheck_lib_test/instructions/setup/env.py
+++ test/shellcheck_lib_test/instructions/setup/env.py
@@ -7,46 +7,46 @@ from shellcheck_lib.test_case.help.instruction_description import Description
 from shellcheck_lib.test_case.os_services import new_with_environ
 from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Flow, TestCaseBase
 from shellcheck_lib_test.instructions.test_resources.check_description import TestDescriptionBase
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib_test.instructions.test_resources.utils import new_source, new_source2
 
 
 class TestParseSet(unittest.TestCase):
     def test_fail_when_there_is_no_arguments(self):
-        source = new_source('instruction-name', '')
+        source = new_source2('')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.Parser().apply(source)
 
     def test_fail_when_there_is_more_than_three_argument(self):
-        source = new_source('instruction-name', 'argument1 = argument3 argument4')
+        source = new_source2('argument1 = argument3 argument4')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.Parser().apply(source)
 
     def test_succeed_when_there_is_exactly_one_assignment(self):
-        source = new_source('instruction-name', 'name = value')
+        source = new_source2('name = value')
         sut.Parser().apply(source)
 
     def test_both_name_and_value_can_be_shell_quoted(self):
-        source = new_source('instruction-name', "'long name' = 'long value'")
+        source = new_source2("'long name' = 'long value'")
         sut.Parser().apply(source)
 
 
 class TestParseUnset(unittest.TestCase):
     def test_fail_when_there_is_no_arguments(self):
-        source = new_source('instruction-name', 'unset')
+        source = new_source2('unset')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.Parser().apply(source)
 
     def test_fail_when_there_is_more_than_one_argument(self):
-        source = new_source('instruction-name', 'unset name superfluous')
+        source = new_source2('unset name superfluous')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.Parser().apply(source)
 
     def test_succeed_when_there_is_exactly_one_argument(self):
-        source = new_source('instruction-name', 'unset name')
+        source = new_source2('unset name')
         sut.Parser().apply(source)
 
     def test_all_parts_may_be_shell_quoted(self):
-        source = new_source('instruction-name', "'unset' 'long name'")
+        source = new_source2("'unset' 'long name'")
         sut.Parser().apply(source)
 
 
@@ -55,10 +55,10 @@ class TestSet(TestCaseBase):
         environ = {}
         os_services = new_with_environ(environ)
         self._check(
-            Flow(sut.Parser(),
-                 os_services=os_services),
-            new_source('instruction-name',
-                       'name = value'))
+                Flow(sut.Parser(),
+                     os_services=os_services),
+                new_source('instruction-name',
+                           'name = value'))
         self.assertEqual(environ,
                          {'name': 'value'})
 
@@ -68,10 +68,10 @@ class TestUnset(TestCaseBase):
         environ = {'a': 'A', 'b': 'B'}
         os_services = new_with_environ(environ)
         self._check(
-            Flow(sut.Parser(),
-                 os_services=os_services),
-            new_source('instruction-name',
-                       'unset a'))
+                Flow(sut.Parser(),
+                     os_services=os_services),
+                new_source('instruction-name',
+                           'unset a'))
         self.assertEqual(environ,
                          {'b': 'B'})
 
