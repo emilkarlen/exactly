af5940df69fa4f73686f2f7d68543a814a4bd029
Emil Karlén <emil.karlen@fripost.org>
2015-12-27 00:36:21 +0100

    Refactoring of test: cleanup: new_dir

--- test/shellcheck_lib_test/instructions/cleanup/new_dir.py
+++ test/shellcheck_lib_test/instructions/cleanup/new_dir.py
@@ -1,35 +1,43 @@
 import unittest
 
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParserSource
 from shellcheck_lib.instructions.cleanup import new_dir as sut
 from shellcheck_lib_test.instructions.cleanup.test_resources.instruction_check import TestCaseBase, Arrangement, \
     Expectation
 from shellcheck_lib_test.instructions.test_resources import sh_check
 from shellcheck_lib_test.instructions.test_resources.eds_contents_check import ActRootContainsExactly
 from shellcheck_lib_test.instructions.test_resources.eds_populator import act_dir_contents
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib_test.instructions.test_resources.utils import new_source2
 from shellcheck_lib_test.util.file_structure import DirContents, empty_dir, Dir, empty_file
 
 
-class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBase):
+class TestCaseBaseForParser(TestCaseBase):
+    def _run(self,
+             source: SingleInstructionParserSource,
+             arrangement: Arrangement,
+             expectation: Expectation):
+        self._check(sut.Parser(), source, arrangement, expectation)
+
+
+class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
     def test_creation_of_directory_with_multiple_path_components(self):
-        self._check(sut.Parser(),
-                    Arrangement(),
-                    Expectation(main_side_effects_on_files=ActRootContainsExactly(DirContents([
-                        Dir('first-component', [
-                            empty_dir('second-component')
-                        ])
-                    ]))),
-                    new_source('instruction-name',
-                               'first-component/second-component'))
+        self._run(new_source2('first-component/second-component'),
+                  Arrangement(),
+                  Expectation(main_side_effects_on_files=ActRootContainsExactly(DirContents([
+                      Dir('first-component', [
+                          empty_dir('second-component')
+                      ])
+                  ]))),
+                  )
 
     def test_argument_exists_as_non_directory__single_path_component(self):
-        self._check(sut.Parser(),
-                    Arrangement(eds_contents_before_main=act_dir_contents(DirContents([
-                        empty_file('file')
-                    ]))),
-                    Expectation(main_result=sh_check.IsHardError()),
-                    new_source('instruction-name',
-                               'file'))
+        self._run(new_source2('file'),
+                  Arrangement(eds_contents_before_main=act_dir_contents(DirContents([
+                      empty_file('file')
+                  ]))),
+                  Expectation(main_result=sh_check.IsHardError()),
+                  )
 
 
 def suite():
