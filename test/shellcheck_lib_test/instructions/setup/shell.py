730961a94e7e738fa597816617546bc8674d6a42
Emil Karlén <emil.karlen@fripost.org>
2015-12-26 23:34:36 +0100

    Refactoring of test: setup/env: shell: TestExecution

--- test/shellcheck_lib_test/instructions/setup/shell.py
+++ test/shellcheck_lib_test/instructions/setup/shell.py
@@ -1,10 +1,11 @@
 import unittest
 
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionInvalidArgumentException
+    SingleInstructionInvalidArgumentException, SingleInstructionParserSource
 from shellcheck_lib.instructions.setup import shell as sut
 from shellcheck_lib.test_case.help.instruction_description import Description
-from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Flow, TestCaseBase
+from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import TestCaseBase, Arrangement, \
+    Expectation, success
 from shellcheck_lib_test.instructions.test_resources import sh_check
 from shellcheck_lib_test.instructions.test_resources.check_description import TestDescriptionBase
 from shellcheck_lib_test.instructions.test_resources.utils import new_source2
@@ -19,7 +20,15 @@ class TestParse(unittest.TestCase):
             sut.parser().apply(source)
 
 
-class TestExecution(TestCaseBase):
+class TestCaseBaseForParser(TestCaseBase):
+    def _run(self,
+             source: SingleInstructionParserSource,
+             arrangement: Arrangement,
+             expectation: Expectation):
+        self._check2(sut.parser(), source, arrangement, expectation)
+
+
+class TestExecution(TestCaseBaseForParser):
     def test_instruction_is_successful_when_exit_status_from_command_is_0(self):
         script_that_exists_with_status_0 = """
 import sys
@@ -27,9 +36,10 @@ sys.exit(0)
 """
         with tmp_file_containing(script_that_exists_with_status_0,
                                  suffix='.py') as script_file_path:
-            self._check(
-                    Flow(sut.parser()),
-                    new_source2(py_exe.command_line_for_interpreting(script_file_path)))
+            self._run(new_source2(py_exe.command_line_for_interpreting(script_file_path)),
+                      Arrangement(),
+                      success()
+                      )
 
     def test_instruction_is_hard_error_WHEN_exit_status_from_command_is_not_0(self):
         script_that_exists_with_status_0 = """
@@ -38,10 +48,10 @@ sys.exit(1)
 """
         with tmp_file_containing(script_that_exists_with_status_0,
                                  suffix='.py') as script_file_path:
-            self._check(
-                    Flow(sut.parser(),
-                         expected_main_result=sh_check.IsHardError()),
-                    new_source2(py_exe.command_line_for_interpreting(script_file_path)))
+            self._run(new_source2(py_exe.command_line_for_interpreting(script_file_path)),
+                      Arrangement(),
+                      Expectation(main_result=sh_check.IsHardError())
+                      )
 
 
 class TestDescription(TestDescriptionBase):
