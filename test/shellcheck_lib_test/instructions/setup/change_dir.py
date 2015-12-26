f7875297e273c4c7011e1a90875362b1f2eb8ea9
Emil Karlén <emil.karlen@fripost.org>
2015-12-26 22:02:31 +0100

    Refactoring of test: setup/change_dir: test_creation_of_directory_with_multiple_path_components

--- test/shellcheck_lib_test/instructions/setup/change_dir.py
+++ test/shellcheck_lib_test/instructions/setup/change_dir.py
@@ -1,38 +1,48 @@
 import pathlib
 import unittest
 
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParserSource
+from shellcheck_lib.instructions.setup import change_dir as sut
 from shellcheck_lib_test.instructions.multi_phase_instructions.change_dir import AssertCwdIsSubDirOfEds
-from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Flow, TestCaseBase
+from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Flow, TestCaseBase, Arrangement, \
+    Expectation
 from shellcheck_lib_test.instructions.test_resources import sh_check
 from shellcheck_lib_test.instructions.test_resources.eds_populator import act_dir_contents
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib_test.instructions.test_resources.utils import new_source, new_source2
 from shellcheck_lib_test.util.file_structure import DirContents, empty_dir, Dir, empty_file
-from shellcheck_lib.instructions.setup import change_dir as sut
 
 
-class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBase):
+class TestCaseBaseForParser(TestCaseBase):
+    def _run(self,
+             source: SingleInstructionParserSource,
+             arrangement: Arrangement,
+             expectation: Expectation):
+        self._check2(sut.Parser(), source, arrangement, expectation)
+
+
+class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
     def test_creation_of_directory_with_multiple_path_components(self):
-        self._check(
-            Flow(sut.Parser(),
-                 eds_contents_before_main=act_dir_contents(DirContents([
-                     Dir('first-component', [
-                         empty_dir('second-component')
-                     ])])),
-                 side_effects_check=AssertCwdIsSubDirOfEds(pathlib.Path('first-component') / 'second-component')
-                 ),
-            new_source('instruction-name',
-                       'first-component/second-component'))
+        self._run(new_source2('first-component/second-component'),
+                  Arrangement(
+                          eds_contents_before_main=act_dir_contents(DirContents([
+                              Dir('first-component', [
+                                  empty_dir('second-component')
+                              ])]))),
+                  Expectation(
+                          side_effects_check=AssertCwdIsSubDirOfEds(
+                                  pathlib.Path('first-component') / 'second-component')))
 
     def test_argument_exists_as_non_directory__single_path_component(self):
         self._check(
-            Flow(sut.Parser(),
-                 eds_contents_before_main=act_dir_contents(DirContents([
-                     empty_file('file')
-                 ])),
-                 expected_main_result=sh_check.IsHardError(),
-                 ),
-            new_source('instruction-name',
-                       'file'))
+                Flow(sut.Parser(),
+                     eds_contents_before_main=act_dir_contents(DirContents([
+                         empty_file('file')
+                     ])),
+                     expected_main_result=sh_check.IsHardError(),
+                     ),
+                new_source('instruction-name',
+                           'file'))
 
 
 def suite():
