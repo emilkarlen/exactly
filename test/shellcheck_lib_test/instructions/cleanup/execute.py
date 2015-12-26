4f4c68f9febe775fc1a67b62a19cd1503acd76f7
Emil Karlén <emil@member.fsf.org>
2015-12-27 00:34:27 +0100

    Refactoring of test: cleanup: execute

--- test/shellcheck_lib_test/instructions/cleanup/execute.py
+++ test/shellcheck_lib_test/instructions/cleanup/execute.py
@@ -1,5 +1,7 @@
 import unittest
 
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParserSource
 from shellcheck_lib.instructions.cleanup import execute as sut
 from shellcheck_lib_test.instructions.cleanup.test_resources.instruction_check import TestCaseBase, Arrangement, \
     Expectation
@@ -8,27 +10,32 @@ from shellcheck_lib_test.instructions.test_resources.utils import single_line_so
 from shellcheck_lib_test.test_resources import python_program_execution as py_exe
 
 
-class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBase):
+class TestCaseBaseForParser(TestCaseBase):
+    def _run(self,
+             source: SingleInstructionParserSource,
+             arrangement: Arrangement,
+             expectation: Expectation):
+        self._check(sut.parser('instruction-name'), source, arrangement, expectation)
+
+
+class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
     def test_successful_execution(self):
-        self._check(_PARSER,
-                    Arrangement(),
-                    Expectation(),
-                    single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(0)')))
+        self._run(single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(0)')),
+                  Arrangement(),
+                  Expectation(),
+                  )
 
     def test_failing_execution(self):
-        self._check(_PARSER,
-                    Arrangement(),
-                    Expectation(main_result=sh_check.IsHardError()),
-                    single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(1)')))
+        self._run(single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(1)')),
+                  Arrangement(),
+                  Expectation(main_result=sh_check.IsHardError()),
+                  )
 
     def test_failing_validation(self):
-        self._check(_PARSER,
-                    Arrangement(),
-                    Expectation(main_result=sh_check.IsHardError()),
-                    single_line_source('/absolute/path/to/program/that/does/not/exist'))
-
-
-_PARSER = sut.parser('instruction-name')
+        self._run(single_line_source('/absolute/path/to/program/that/does/not/exist'),
+                  Arrangement(),
+                  Expectation(main_result=sh_check.IsHardError()),
+                  )
 
 
 def suite():
