add33424cab57d4a8ade756de2a078cd7f136535
Emil Karlén <emil.karlen@fripost.org>
2015-12-27 00:39:06 +0100

    Refactoring of test: cleanup: shell

--- test/shellcheck_lib_test/instructions/cleanup/shell.py
+++ test/shellcheck_lib_test/instructions/cleanup/shell.py
@@ -1,26 +1,34 @@
 import unittest
 
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionInvalidArgumentException
+    SingleInstructionInvalidArgumentException, SingleInstructionParserSource
 from shellcheck_lib.instructions.cleanup import shell as sut
 from shellcheck_lib.test_case.help.instruction_description import Description
 from shellcheck_lib_test.instructions.cleanup.test_resources.instruction_check import TestCaseBase, Arrangement, \
     Expectation
 from shellcheck_lib_test.instructions.test_resources import sh_check
 from shellcheck_lib_test.instructions.test_resources.check_description import TestDescriptionBase
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib_test.instructions.test_resources.utils import new_source2
 from shellcheck_lib_test.test_resources import python_program_execution as py_exe
 from shellcheck_lib_test.util.file_utils import tmp_file_containing
 
 
+class TestCaseBaseForParser(TestCaseBase):
+    def _run(self,
+             source: SingleInstructionParserSource,
+             arrangement: Arrangement,
+             expectation: Expectation):
+        self._check(sut.parser(), source, arrangement, expectation)
+
+
 class TestParse(unittest.TestCase):
     def test_fail_when_there_is_no_arguments(self):
-        source = new_source('instruction-name', '   ')
+        source = new_source2('   ')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.parser().apply(source)
 
 
-class TestExecution(TestCaseBase):
+class TestExecution(TestCaseBaseForParser):
     def test_instruction_is_successful_when_exit_status_from_command_is_0(self):
         script_that_exists_with_status_0 = """
 import sys
@@ -28,12 +36,11 @@ sys.exit(0)
 """
         with tmp_file_containing(script_that_exists_with_status_0,
                                  suffix='.py') as script_file_path:
-            self._check(
-                    sut.parser(),
+            self._run(
+                    new_source2(py_exe.command_line_for_interpreting(script_file_path)),
                     Arrangement(),
                     Expectation(),
-                    new_source('instruction-name',
-                               py_exe.command_line_for_interpreting(script_file_path)))
+            )
 
     def test_instruction_is_hard_error_WHEN_exit_status_from_command_is_not_0(self):
         script_that_exists_with_status_0 = """
@@ -42,11 +49,10 @@ sys.exit(1)
 """
         with tmp_file_containing(script_that_exists_with_status_0,
                                  suffix='.py') as script_file_path:
-            self._check(sut.parser(),
-                        Arrangement(),
-                        Expectation(main_result=sh_check.IsHardError()),
-                        new_source('instruction-name',
-                                   py_exe.command_line_for_interpreting(script_file_path)))
+            self._run(new_source2(py_exe.command_line_for_interpreting(script_file_path)),
+                      Arrangement(),
+                      Expectation(main_result=sh_check.IsHardError()),
+                      )
 
 
 class TestDescription(TestDescriptionBase):
