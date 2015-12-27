9dda1106225dddd569d4936233c5a9a91728b89a
Emil Karlén <emil.karlen@fripost.org>
2015-12-27 01:34:41 +0100

    Refactoring of test: assert: new_dir: Using new_source2

--- test/shellcheck_lib_test/instructions/assert_phase/new_dir.py
+++ test/shellcheck_lib_test/instructions/assert_phase/new_dir.py
@@ -5,33 +5,31 @@ from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_ch
 from shellcheck_lib_test.instructions.test_resources import pfh_check
 from shellcheck_lib_test.instructions.test_resources.eds_contents_check import ActRootContainsExactly
 from shellcheck_lib_test.instructions.test_resources.eds_populator import act_dir_contents
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib_test.instructions.test_resources.utils import new_source2
 from shellcheck_lib_test.util.file_structure import DirContents, empty_dir, Dir, empty_file
 
 
 class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBase):
     def test_creation_of_directory_with_multiple_path_components(self):
         self._chekk(
-            Flow(sut.Parser(),
-                 expected_main_side_effects_on_files=ActRootContainsExactly(DirContents([
-                     Dir('first-component', [
-                         empty_dir('second-component')
-                     ])
-                 ]))
-                 ),
-            new_source('instruction-name',
-                       'first-component/second-component'))
+                Flow(sut.Parser(),
+                     expected_main_side_effects_on_files=ActRootContainsExactly(DirContents([
+                         Dir('first-component', [
+                             empty_dir('second-component')
+                         ])
+                     ]))
+                     ),
+                new_source2('first-component/second-component'))
 
     def test_argument_exists_as_non_directory__single_path_component(self):
         self._chekk(
-            Flow(sut.Parser(),
-                 eds_contents_before_main=act_dir_contents(DirContents([
-                     empty_file('file')
-                 ])),
-                 expected_main_result=pfh_check.is_hard_error(),
-                 ),
-            new_source('instruction-name',
-                       'file'))
+                Flow(sut.Parser(),
+                     eds_contents_before_main=act_dir_contents(DirContents([
+                         empty_file('file')
+                     ])),
+                     expected_main_result=pfh_check.is_hard_error(),
+                     ),
+                new_source2('file'))
 
 
 def suite():
