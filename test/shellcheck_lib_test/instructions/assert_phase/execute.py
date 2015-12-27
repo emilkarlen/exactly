ebc59cbce2e77495a25d6dcbb9f834ace82ff8b7
Emil Karlén <emil@member.fsf.org>
2015-12-27 13:06:36 +0100

    Refactoring of test: assert: execute: TestCasesThatTestIntegrationByAFewRandomTests

--- test/shellcheck_lib_test/instructions/assert_phase/execute.py
+++ test/shellcheck_lib_test/instructions/assert_phase/execute.py
@@ -1,30 +1,44 @@
 import unittest
 
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParserSource
 from shellcheck_lib.instructions.assert_phase import execute as sut
-from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import Flow, TestCaseBase
+from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import TestCaseBase, \
+    Arrangement, Expectation, success
 from shellcheck_lib_test.instructions.test_resources import pfh_check
 from shellcheck_lib_test.instructions.test_resources import svh_check
 from shellcheck_lib_test.instructions.test_resources.utils import single_line_source
 from shellcheck_lib_test.test_resources import python_program_execution as py_exe
 
 
-class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBase):
+class TestCaseBaseForParser(TestCaseBase):
+    def _run(self,
+             source: SingleInstructionParserSource,
+             arrangement: Arrangement,
+             expectation: Expectation):
+        self._check(sut.parser('instruction-name'), source, arrangement, expectation)
+
+
+class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
     def test_successful_execution(self):
-        self._chekk(Flow(_PARSER),
-                    single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(0)')))
+        self._run(single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(0)')),
+                  Arrangement(),
+                  success(),
+                  )
 
     def test_failing_execution(self):
-        self._chekk(Flow(_PARSER,
-                         expected_main_result=pfh_check.is_fail()),
-                    single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(1)')))
+        self._run(
+                single_line_source(py_exe.command_line_for_executing_program_via_command_line('exit(1)')),
+                Arrangement(),
+                Expectation(expected_main_result=pfh_check.is_fail()),
+        )
 
     def test_failing_validation(self):
-        self._chekk(Flow(_PARSER,
-                         expected_validation_result=svh_check.is_validation_error()),
-                    single_line_source('/absolute/path/to/program/that/does/not/exist'))
-
-
-_PARSER = sut.parser('instruction-name')
+        self._run(
+                single_line_source('/absolute/path/to/program/that/does/not/exist'),
+                Arrangement(),
+                Expectation(expected_validation_result=svh_check.is_validation_error()),
+        )
 
 
 def suite():
