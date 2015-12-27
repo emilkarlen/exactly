b8762f2817a510673baa8efdcd17f6de5d5a6c5f
Emil Karlén <emil.karlen@fripost.org>
2015-12-27 01:35:36 +0100

    Refactoring of test: assert: shell: Using new_source2

--- test/shellcheck_lib_test/instructions/assert_phase/shell.py
+++ test/shellcheck_lib_test/instructions/assert_phase/shell.py
@@ -7,14 +7,14 @@ from shellcheck_lib.test_case.help.instruction_description import Description
 from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import Flow, TestCaseBase
 from shellcheck_lib_test.instructions.test_resources import pfh_check
 from shellcheck_lib_test.instructions.test_resources.check_description import TestDescriptionBase
-from shellcheck_lib_test.instructions.test_resources.utils import new_source
+from shellcheck_lib_test.instructions.test_resources.utils import new_source2
 from shellcheck_lib_test.test_resources import python_program_execution as py_exe
 from shellcheck_lib_test.util.file_utils import tmp_file_containing
 
 
 class TestParse(unittest.TestCase):
     def test_fail_when_there_is_no_arguments(self):
-        source = new_source('instruction-name', '   ')
+        source = new_source2('   ')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.parser().apply(source)
 
@@ -29,8 +29,7 @@ sys.exit(0)
                                  suffix='.py') as script_file_path:
             self._chekk(
                     Flow(sut.parser()),
-                    new_source('instruction-name',
-                               py_exe.command_line_for_interpreting(script_file_path)))
+                    new_source2(py_exe.command_line_for_interpreting(script_file_path)))
 
     def test_instruction_is_hard_error_WHEN_exit_status_from_command_is_not_0(self):
         script_that_exists_with_status_0 = """
@@ -42,8 +41,7 @@ sys.exit(1)
             self._chekk(
                     Flow(sut.parser(),
                          expected_main_result=pfh_check.is_fail()),
-                    new_source('instruction-name',
-                               py_exe.command_line_for_interpreting(script_file_path)))
+                    new_source2(py_exe.command_line_for_interpreting(script_file_path)))
 
 
 class TestDescription(TestDescriptionBase):
