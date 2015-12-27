343540b3d80a67c337b3b791ad62b0e2af349a24
Emil Karlén <emil.karlen@fripost.org>
2015-12-27 01:44:41 +0100

    Refactoring of test: assert: instruction_check_test

--- test/shellcheck_lib_test/instructions/assert_phase/test_resources/instruction_check_test.py
+++ test/shellcheck_lib_test/instructions/assert_phase/test_resources/instruction_check_test.py
@@ -11,61 +11,64 @@ from shellcheck_lib.test_case.sections.result import svh
 from shellcheck_lib_test.execution.full_execution.util.instruction_test_resources import \
     AssertPhaseInstructionThatReturns
 from shellcheck_lib_test.instructions.assert_phase.test_resources import instruction_check
+from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import Arrangement, success, \
+    Expectation
 from shellcheck_lib_test.instructions.test_resources import test_of_test_framework_utils as test_misc
-
-
-def suite():
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestCases))
-    return ret_val
-
-
-if __name__ == '__main__':
-    unittest.main()
 
 
 class TestCases(instruction_check.TestCaseBase):
     def test_successful_flow(self):
-        self._chekk(
-            instruction_check.Flow(test_misc.ParserThatGives(SUCCESSFUL_INSTRUCTION)),
-            test_misc.single_line_source())
+        self._check(
+                test_misc.ParserThatGives(_SUCCESSFUL_INSTRUCTION),
+                test_misc.single_line_source(),
+                Arrangement(),
+                success())
 
     def test_fail_due_to_unexpected_result_from_pre_validation(self):
         with self.assertRaises(test_misc.TestError):
-            self._chekk(
-                instruction_check.Flow(test_misc.ParserThatGives(SUCCESSFUL_INSTRUCTION),
-                                       expected_validation_result=test_misc.SvhRaisesTestError()),
-                test_misc.single_line_source())
+            self._check(test_misc.ParserThatGives(_SUCCESSFUL_INSTRUCTION),
+                        test_misc.single_line_source(),
+                        Arrangement(),
+                        Expectation(expected_validation_result=test_misc.SvhRaisesTestError()),
+                        )
 
     def test_fail_due_to_unexpected_result_from_main(self):
         with self.assertRaises(test_misc.TestError):
-            self._chekk(
-                instruction_check.Flow(test_misc.ParserThatGives(SUCCESSFUL_INSTRUCTION),
-                                       expected_main_result=test_misc.PfhRaisesTestError()),
-                test_misc.single_line_source())
+            self._check(
+                    test_misc.ParserThatGives(_SUCCESSFUL_INSTRUCTION),
+                    test_misc.single_line_source(),
+                    Arrangement(),
+                    Expectation(expected_main_result=test_misc.PfhRaisesTestError()),
+            )
 
     def test_fail_due_to_fail_of_side_effects_on_files(self):
         with self.assertRaises(test_misc.TestError):
-            self._chekk(
-                instruction_check.Flow(test_misc.ParserThatGives(SUCCESSFUL_INSTRUCTION),
-                                       expected_main_side_effects_on_files=test_misc.EdsContentsRaisesTestError()),
-                test_misc.single_line_source())
+            self._check(
+                    test_misc.ParserThatGives(_SUCCESSFUL_INSTRUCTION),
+                    test_misc.single_line_source(),
+                    Arrangement(),
+                    Expectation(expected_main_side_effects_on_files=test_misc.EdsContentsRaisesTestError()),
+            )
 
     def test_that_cwd_for_main_and_post_validation_is_test_root(self):
-        self._chekk(
-            instruction_check.Flow(test_misc.ParserThatGives(InstructionThatRaisesTestErrorIfCwdIsIsNotTestRoot())),
-            test_misc.single_line_source())
+        self._check(
+                test_misc.ParserThatGives(InstructionThatRaisesTestErrorIfCwdIsIsNotTestRoot()),
+                test_misc.single_line_source(),
+                Arrangement(),
+                success())
 
     def test_fail_due_to_side_effects_check(self):
         with self.assertRaises(test_misc.TestError):
-            self._chekk(
-                instruction_check.Flow(test_misc.ParserThatGives(SUCCESSFUL_INSTRUCTION),
-                                       side_effects_check=test_misc.SideEffectsCheckThatRaisesTestError()),
-                test_misc.single_line_source())
+            self._check(
+                    test_misc.ParserThatGives(_SUCCESSFUL_INSTRUCTION),
+                    test_misc.single_line_source(),
+                    Arrangement(),
+                    Expectation(side_effects_check=test_misc.SideEffectsCheckThatRaisesTestError()),
+            )
 
 
-SUCCESSFUL_INSTRUCTION = AssertPhaseInstructionThatReturns(svh.new_svh_success(),
-                                                           pfh.new_pfh_pass())
+_SUCCESSFUL_INSTRUCTION = AssertPhaseInstructionThatReturns(svh.new_svh_success(),
+                                                            pfh.new_pfh_pass())
 
 
 class InstructionThatRaisesTestErrorIfCwdIsIsNotTestRoot(AssertPhaseInstruction):
@@ -76,3 +79,13 @@ class InstructionThatRaisesTestErrorIfCwdIsIsNotTestRoot(AssertPhaseInstruction)
     def main(self, environment: GlobalEnvironmentForPostEdsPhase, os_services: OsServices) -> pfh.PassOrFailOrHardError:
         test_misc.raise_test_error_if_cwd_is_not_test_root(environment.eds)
         return pfh.new_pfh_pass()
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(unittest.makeSuite(TestCases))
+    return ret_val
+
+
+if __name__ == '__main__':
+    unittest.main()
