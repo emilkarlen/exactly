cedc1b6c79d52d3417da47162e4c26606ff372fe
Emil Karlén <emil@member.fsf.org>
2015-12-31 16:52:57 +0100

    Refactoring: test resources: full execution: Moves recording-related functionality to separate package

--- test/shellcheck_lib_test/execution/full_execution/test_execution_mode__normal.py
+++ test/shellcheck_lib_test/execution/full_execution/test_execution_mode__normal.py
@@ -6,7 +6,7 @@ from shellcheck_lib.execution.result import FullResultStatus
 from shellcheck_lib.test_case.sections.result import pfh
 from shellcheck_lib.test_case.sections.result import sh
 from shellcheck_lib.test_case.sections.result import svh
-from shellcheck_lib_test.execution.full_execution.test_resources.test_case_that_records_phase_execution import \
+from shellcheck_lib_test.execution.full_execution.test_resources.recording.test_case_that_records_phase_execution import \
     Expectation, Arrangement, TestCaseBase, one_successful_instruction_in_each_phase
 from shellcheck_lib_test.execution.test_resources import instruction_test_resources as test
 from shellcheck_lib_test.execution.test_resources.test_actions import validate_action_that_returns, \
