982868dd419be435e69c2544544a2c186a222c0f
Emil Karlén <emil.karlen@fripost.org>
2015-12-31 16:38:54 +0100

    Refactoring: test resources: Moves act-program-executor to separate module

--- test/shellcheck_lib_test/execution/full_execution/test_resources/test_case_that_records_phase_execution.py
+++ test/shellcheck_lib_test/execution/full_execution/test_resources/test_case_that_records_phase_execution.py
@@ -1,21 +1,17 @@
-import pathlib
 import types
 import unittest
 
 from shellcheck_lib.act_phase_setups import python3
 from shellcheck_lib.default.execution_mode.test_case.processing import script_handling_for_setup
-from shellcheck_lib.execution import phase_step
-from shellcheck_lib.execution.execution_directory_structure import ExecutionDirectoryStructure
 from shellcheck_lib.execution.partial_execution import ScriptHandling
 from shellcheck_lib.execution.result import FullResultStatus
-from shellcheck_lib.general.std import StdFiles
 from shellcheck_lib.test_case import test_case_doc
-from shellcheck_lib.test_case.sections.act.phase_setup import ActProgramExecutor, SourceSetup
-from shellcheck_lib.test_case.sections.act.script_source import ScriptSourceBuilder
 from shellcheck_lib.test_case.sections.result import svh
 from shellcheck_lib_test.execution.full_execution.test_resources.test_case_base import FullExecutionTestCaseBase
 from shellcheck_lib_test.execution.full_execution.test_resources.test_case_generation_for_sequence_tests import \
     TestCaseGeneratorForExecutionRecording, TestCaseGeneratorThatRecordsExecutionWithExtraInstructionList
+from shellcheck_lib_test.execution.test_resources.execution_recording.act_program_executor import \
+    ActProgramExecutorWrapperThatRecordsSteps
 from shellcheck_lib_test.execution.test_resources.execution_recording.recorder import \
     ListRecorder
 from shellcheck_lib_test.execution.test_resources.test_actions import validate_action_that_returns, \
@@ -100,49 +96,10 @@ def with_recording_act_program_executor(recorder: ListRecorder,
                                         validate_test_action,
                                         execute_test_action) -> ScriptHandling:
     return ScriptHandling(script_handling.builder,
-                          _ActProgramExecutorWrapperThatRecordsSteps(recorder,
-                                                                     script_handling.executor,
-                                                                     validate_test_action,
-                                                                     execute_test_action))
-
-
-class _ActProgramExecutorWrapperThatRecordsSteps(ActProgramExecutor):
-    def __init__(self,
-                 recorder: ListRecorder,
-                 wrapped: ActProgramExecutor,
-                 validate_test_action,
-                 execute_test_action):
-        self.__recorder = recorder
-        self.__wrapped = wrapped
-        self.__validate_test_action = validate_test_action
-        self.__execute_test_action = execute_test_action
-
-    def validate(self,
-                 home_dir: pathlib.Path(),
-                 source: ScriptSourceBuilder) -> svh.SuccessOrValidationErrorOrHardError:
-        self.__recorder.recording_of(phase_step.ACT__SCRIPT_VALIDATE).record()
-        test_action_result = self.__validate_test_action()
-        if not test_action_result.is_success:
-            return test_action_result
-        return self.__wrapped.validate(home_dir, source)
-
-    def prepare(self,
-                source_setup: SourceSetup,
-                home_dir_path: pathlib.Path,
-                eds: ExecutionDirectoryStructure):
-        self.__wrapped.prepare(source_setup, home_dir_path, eds)
-
-    def execute(self,
-                source_setup: SourceSetup,
-                home_dir: pathlib.Path,
-                eds: ExecutionDirectoryStructure,
-                std_files: StdFiles) -> int:
-        self.__recorder.recording_of(phase_step.ACT__SCRIPT_EXECUTE).record()
-        self.__execute_test_action()
-        return self.__wrapped.execute(source_setup,
-                                      home_dir,
-                                      eds,
-                                      std_files)
+                          ActProgramExecutorWrapperThatRecordsSteps(recorder,
+                                                                    script_handling.executor,
+                                                                    validate_test_action,
+                                                                    execute_test_action))
 
 
 class Arrangement(tuple):
