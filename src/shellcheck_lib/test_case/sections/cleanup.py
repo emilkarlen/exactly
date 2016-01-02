1b06b340477f2dfcd0f0c9c1e48bc0c16983598c
Emil Karlén <emil.karlen@fripost.org>
2016-01-02 13:42:17 +0100

    pre-eds-validation: cleanup: Adds pre-eds-validation method to CleanupPhaseInstruction

--- src/shellcheck_lib/test_case/sections/cleanup.py
+++ src/shellcheck_lib/test_case/sections/cleanup.py
@@ -1,5 +1,7 @@
 from shellcheck_lib.test_case.os_services import OsServices
-from shellcheck_lib.test_case.sections.common import GlobalEnvironmentForPostEdsPhase, TestCaseInstruction
+from shellcheck_lib.test_case.sections.common import GlobalEnvironmentForPostEdsPhase, TestCaseInstruction, \
+    GlobalEnvironmentForPreEdsStep
+from shellcheck_lib.test_case.sections.result import svh
 from shellcheck_lib.test_case.sections.result.sh import SuccessOrHardError
 
 
@@ -7,6 +9,10 @@ class CleanupPhaseInstruction(TestCaseInstruction):
     """
     Abstract base class for instructions of the CLEANUP phase.
     """
+
+    def validate_pre_eds(self,
+                         environment: GlobalEnvironmentForPreEdsStep) -> svh.SuccessOrValidationErrorOrHardError:
+        return svh.new_svh_success()
 
     def main(self,
              environment: GlobalEnvironmentForPostEdsPhase,
