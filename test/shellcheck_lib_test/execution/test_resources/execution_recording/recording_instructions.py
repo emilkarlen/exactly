1221b409ceec41a4398638caccce9810ff1a9e07
Emil Karlén <emil@member.fsf.org>
2016-01-03 19:49:24 +0100

    Refactoring: test: Improves instruction for recording step execution: for phase setup

--- test/shellcheck_lib_test/execution/test_resources/execution_recording/recording_instructions.py
+++ test/shellcheck_lib_test/execution/test_resources/execution_recording/recording_instructions.py
@@ -1,4 +1,3 @@
-from shellcheck_lib.test_case.os_services import OsServices
 from shellcheck_lib.test_case.sections import common
 from shellcheck_lib.test_case.sections.act.instruction import ActPhaseInstruction, PhaseEnvironmentForScriptGeneration
 from shellcheck_lib.test_case.sections.anonymous import AnonymousPhaseInstruction, \
@@ -8,11 +7,10 @@ from shellcheck_lib.test_case.sections.cleanup import CleanupPhaseInstruction
 from shellcheck_lib.test_case.sections.result import pfh
 from shellcheck_lib.test_case.sections.result import sh
 from shellcheck_lib.test_case.sections.result import svh
-from shellcheck_lib.test_case.sections.setup import SetupPhaseInstruction, SetupSettingsBuilder
+from shellcheck_lib.test_case.sections.setup import SetupPhaseInstruction
 from shellcheck_lib_test.execution.test_resources.execution_recording.recorder import ListElementRecorder, ListRecorder
-from shellcheck_lib_test.execution.test_resources.instruction_adapter import InternalInstruction
 from shellcheck_lib_test.execution.test_resources.instruction_test_resources import cleanup_phase_instruction_that, \
-    assert_phase_instruction_that
+    assert_phase_instruction_that, setup_phase_instruction_that
 
 
 class RecordingInstructions:
@@ -26,9 +24,9 @@ class RecordingInstructions:
                               value_for_pre_validate,
                               value_for_execute,
                               value_for_post_validate) -> SetupPhaseInstruction:
-        return SetupInstructionThatRecordsStringInList(self.__recorder_of(value_for_pre_validate),
-                                                       self.__recorder_of(value_for_execute),
-                                                       self.__recorder_of(value_for_post_validate))
+        return setup_instruction_that_records(self.__recorder_of(value_for_pre_validate),
+                                              self.__recorder_of(value_for_execute),
+                                              self.__recorder_of(value_for_post_validate))
 
     def new_act_instruction(self,
                             value_for_validate_pre_eds,
@@ -94,44 +92,12 @@ class AnonymousInternalInstructionThatRecordsStringInList(AnonymousPhaseInstruct
         return sh.new_sh_success()
 
 
-class SetupInstructionThatRecordsStringInList(SetupPhaseInstruction):
-    def __init__(self,
-                 recorder_for_pre_validate: ListElementRecorder,
-                 recorder_for_execute: ListElementRecorder,
-                 recorder_for_post_validate: ListElementRecorder):
-        self.__recorder_for_pre_validate = recorder_for_pre_validate
-        self.__recorder_for_execute = recorder_for_execute
-        self.__recorder_for_post_validate = recorder_for_post_validate
-
-    def pre_validate(self,
-                     global_environment: common.GlobalEnvironmentForPreEdsStep) \
-            -> svh.SuccessOrValidationErrorOrHardError:
-        self.__recorder_for_pre_validate.record()
-        return sh.new_sh_success()
-
-    def main(self,
-             os_services: OsServices,
-             environment: common.GlobalEnvironmentForPostEdsPhase,
-             settings_builder: SetupSettingsBuilder) -> sh.SuccessOrHardError:
-        self.__recorder_for_execute.record()
-        return sh.new_sh_success()
-
-    def post_validate(self,
-                      global_environment: common.GlobalEnvironmentForPostEdsPhase) \
-            -> svh.SuccessOrValidationErrorOrHardError:
-        self.__recorder_for_post_validate.record()
-        return sh.new_sh_success()
-
-
-class InternalInstructionThatRecordsStringInList(InternalInstruction):
-    def __init__(self,
-                 recorder: ListElementRecorder):
-        self.__recorder = recorder
-
-    def execute(self, phase_name,
-                environment: common.GlobalEnvironmentForPostEdsPhase,
-                os_services: OsServices):
-        self.__recorder.record()
+def setup_instruction_that_records(recorder_for_pre_validate: ListElementRecorder,
+                                   recorder_for_main: ListElementRecorder,
+                                   recorder_for_post_validate: ListElementRecorder) -> SetupPhaseInstruction:
+    return setup_phase_instruction_that(pre_validate=_do_record_and_return_svh(recorder_for_pre_validate),
+                                        post_validate=_do_record_and_return_svh(recorder_for_post_validate),
+                                        main=_do_record_and_return_sh(recorder_for_main))
 
 
 def assert_instruction_that_records(recorder_for_validate_pre_eds: ListElementRecorder,
