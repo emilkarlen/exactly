dcd10b69600ae307db52e589b8cea5b1c0f30b33
Emil Karlén <emil.karlen@fripost.org>
2016-01-07 00:04:01 +0100

    assert/execute Adds validation/pre-eds

--- test/shellcheck_lib_test/instructions/assert_phase/execute.py
+++ test/shellcheck_lib_test/instructions/assert_phase/execute.py
@@ -3,12 +3,16 @@ import unittest
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionParserSource
 from shellcheck_lib.instructions.assert_phase import execute as sut
+from shellcheck_lib.instructions.utils import relative_path_options as options
 from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import TestCaseBase, \
     Arrangement, Expectation, is_pass
+from shellcheck_lib_test.instructions.test_resources import eds_populator
 from shellcheck_lib_test.instructions.test_resources import pfh_check
 from shellcheck_lib_test.instructions.test_resources import svh_check
+from shellcheck_lib_test.instructions.test_resources.execute_utils import source_for_interpreting
 from shellcheck_lib_test.instructions.test_resources.utils import single_line_source
 from shellcheck_lib_test.test_resources import python_program_execution as py_exe
+from shellcheck_lib_test.test_resources.file_structure import DirContents, empty_file
 
 
 class TestCaseBaseForParser(TestCaseBase):
@@ -33,12 +37,36 @@ class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
                 Expectation(main_result=pfh_check.is_fail()),
         )
 
-    def test_failing_validation(self):
+
+class TestValidation(TestCaseBaseForParser):
+    def test_failing_validation_of_absolute_path(self):
         self._run(
                 single_line_source('/absolute/path/to/program/that/does/not/exist'),
                 Arrangement(),
-                Expectation(validation_post_eds=svh_check.is_validation_error()),
+                Expectation(validation_pre_eds=svh_check.is_validation_error()),
         )
+
+    def test_failing_validation_of__rel_home_path(self):
+        self._run(source_for_interpreting(options.REL_HOME_OPTION, 'non-existing-file.py'),
+                  Arrangement(),
+                  Expectation(validation_pre_eds=svh_check.is_validation_error()),
+                  )
+
+    def test_failing_validation_of__rel_tmp_path(self):
+        self._run(source_for_interpreting(options.REL_TMP_OPTION, 'non-existing-file.py'),
+                  Arrangement(),
+                  Expectation(validation_post_eds=svh_check.is_validation_error()),
+                  )
+
+    def test_successful_validation(self):
+        self._run(source_for_interpreting(options.REL_TMP_OPTION, 'existing-file.py'),
+                  Arrangement(eds_contents_before_main=eds_populator.tmp_user_dir_contents(
+                          DirContents([empty_file('existing-file.py')]))),
+                  is_pass(),
+                  )
+
+
+_NO_OPTION = ''
 
 
 def suite():
