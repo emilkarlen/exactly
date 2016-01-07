01a25a7b361f0076fc2a19b12f2aa98cbcc8823d
Emil Karlén <emil.karlen@fripost.org>
2016-01-07 16:51:16 +0100

    before-assert: Packages for test

---
+++ test/shellcheck_lib_test/instructions/before_assert/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+from shellcheck_lib_test.instructions.before_assert import test_resources
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTests(test_resources.suite())
+    return ret_val
+
+
+def run_suite():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+
+if __name__ == '__main__':
+    run_suite()
--- test/shellcheck_lib_test/instructions/cleanup/test_resources/instruction_check_test.py
+++ test/shellcheck_lib_test/instructions/cleanup/test_resources/instruction_check_test.py
@@ -80,5 +80,10 @@ def suite():
     return ret_val
 
 
+def run_suite():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+
 if __name__ == '__main__':
-    unittest.main()
+    run_suite()
