40b5cf23029ae27f2c8ef3b568ca67dd172ee63b
Emil Karlén <emil.karlen@fripost.org>
2016-01-07 18:42:15 +0100

    test main executes run_main

--- test/shellcheck_lib_test/instructions/assert_phase/test_resources/instruction_check_test.py
+++ test/shellcheck_lib_test/instructions/assert_phase/test_resources/instruction_check_test.py
@@ -95,5 +95,10 @@ def suite():
     return ret_val
 
 
+def run_suite():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+
 if __name__ == '__main__':
-    unittest.main()
+    run_suite()
