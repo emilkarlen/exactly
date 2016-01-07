b64c6d3337488ebddab7a2f5a5efcbf8d6fa6a1a
Emil Karlén <emil@member.fsf.org>
2016-01-07 16:51:16 +0100

    before-assert: Packages for test

--- test/shellcheck_lib_test/instructions/__init__.py
+++ test/shellcheck_lib_test/instructions/__init__.py
@@ -1,11 +1,12 @@
 import unittest
 
-from shellcheck_lib_test.instructions import utils
 from shellcheck_lib_test.instructions import assert_phase
-from shellcheck_lib_test.instructions import setup
+from shellcheck_lib_test.instructions import before_assert
 from shellcheck_lib_test.instructions import cleanup
 from shellcheck_lib_test.instructions import configuration
 from shellcheck_lib_test.instructions import multi_phase_instructions
+from shellcheck_lib_test.instructions import setup
+from shellcheck_lib_test.instructions import utils
 
 
 def suite():
@@ -14,6 +15,7 @@ def suite():
     ret_val.addTest(multi_phase_instructions.suite())
     ret_val.addTest(configuration.suite())
     ret_val.addTest(setup.suite())
+    ret_val.addTest(before_assert.suite())
     ret_val.addTest(assert_phase.suite())
     ret_val.addTest(cleanup.suite())
     return ret_val
