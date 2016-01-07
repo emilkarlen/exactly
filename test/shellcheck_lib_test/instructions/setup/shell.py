1374a7921b3b5fa685678f1b8ed4d24c0c1fa949
Emil Karlén <emil.karlen@fripost.org>
2016-01-07 23:17:26 +0100

    setup/shell: Tests uses common test utilities

--- test/shellcheck_lib_test/instructions/setup/shell.py
+++ test/shellcheck_lib_test/instructions/setup/shell.py
@@ -1,57 +1,37 @@
 import unittest
 
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionInvalidArgumentException, SingleInstructionParserSource
+    SingleInstructionParserSource, SingleInstructionParser
 from shellcheck_lib.instructions.setup import shell as sut
 from shellcheck_lib.test_case.instruction_description import Description
-from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import TestCaseBase, Arrangement, \
-    Expectation, is_success
+from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.shell_instruction_test import \
+    Configuration, suite_for
+from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import arrangement, Expectation, \
+    check
 from shellcheck_lib_test.instructions.test_resources import sh_check
+from shellcheck_lib_test.instructions.test_resources.arrangement import ArrangementPostAct
 from shellcheck_lib_test.instructions.test_resources.check_description import TestDescriptionBase
-from shellcheck_lib_test.instructions.test_resources.utils import new_source2
-from shellcheck_lib_test.test_resources import python_program_execution as py_exe
-from shellcheck_lib_test.test_resources.file_utils import tmp_file_containing
 
 
-class TestParse(unittest.TestCase):
-    def test_fail_when_there_is_no_arguments(self):
-        source = new_source2('   ')
-        with self.assertRaises(SingleInstructionInvalidArgumentException):
-            sut.parser().apply(source)
+class TheConfiguration(Configuration):
+    def run_test(self,
+                 put: unittest.TestCase,
+                 source: SingleInstructionParserSource,
+                 arrangement,
+                 expectation):
+        check(put, self.parser(), source, arrangement, expectation)
 
+    def parser(self) -> SingleInstructionParser:
+        return sut.parser()
 
-class TestCaseBaseForParser(TestCaseBase):
-    def _run(self,
-             source: SingleInstructionParserSource,
-             arrangement: Arrangement,
-             expectation: Expectation):
-        self._check(sut.parser(), source, arrangement, expectation)
+    def empty_arrangement(self) -> ArrangementPostAct:
+        return arrangement()
 
+    def expectation_of_non_zero_exitcode(self) -> Expectation:
+        return Expectation(main_result=sh_check.IsHardError())
 
-class TestExecution(TestCaseBaseForParser):
-    def test_instruction_is_successful_when_exit_status_from_command_is_0(self):
-        script_that_exists_with_status_0 = """
-import sys
-sys.exit(0)
-"""
-        with tmp_file_containing(script_that_exists_with_status_0,
-                                 suffix='.py') as script_file_path:
-            self._run(new_source2(py_exe.command_line_for_interpreting(script_file_path)),
-                      Arrangement(),
-                      is_success()
-                      )
-
-    def test_instruction_is_hard_error_WHEN_exit_status_from_command_is_not_0(self):
-        script_that_exists_with_status_0 = """
-import sys
-sys.exit(1)
-"""
-        with tmp_file_containing(script_that_exists_with_status_0,
-                                 suffix='.py') as script_file_path:
-            self._run(new_source2(py_exe.command_line_for_interpreting(script_file_path)),
-                      Arrangement(),
-                      Expectation(main_result=sh_check.IsHardError())
-                      )
+    def expectation_of_zero_exitcode(self) -> Expectation:
+        return Expectation()
 
 
 class TestDescription(TestDescriptionBase):
@@ -61,8 +41,7 @@ class TestDescription(TestDescriptionBase):
 
 def suite():
     ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestParse))
-    ret_val.addTest(unittest.makeSuite(TestExecution))
+    ret_val.addTest(suite_for(TheConfiguration()))
     ret_val.addTest(unittest.makeSuite(TestDescription))
     return ret_val
 
