ebe450c57e3db7a103eb95bc9647c1fe11b6d6f3
Emil Karlén <emil.karlen@fripost.org>
2016-01-08 16:06:52 +0100

    Refactoring of tests: using common code for test of "change_dir": cleanup

--- test/shellcheck_lib_test/instructions/cleanup/change_dir.py
+++ test/shellcheck_lib_test/instructions/cleanup/change_dir.py
@@ -1,51 +1,25 @@
-import pathlib
-import unittest
-
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionParserSource
+    SingleInstructionParser
 from shellcheck_lib.instructions.cleanup import change_dir as sut
-from shellcheck_lib_test.instructions.cleanup.test_resources.instruction_check import TestCaseBase, arrangement, \
-    Expectation
-from shellcheck_lib_test.instructions.multi_phase_instructions.change_dir import AssertCwdIsSubDirOfEds
+from shellcheck_lib_test.instructions.cleanup.test_resources.configuration import CleanupConfigurationBase
+from shellcheck_lib_test.instructions.cleanup.test_resources.instruction_check import Expectation
+from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.change_dir_instruction_test import \
+    Configuration, suite_for
 from shellcheck_lib_test.instructions.test_resources import sh_check
-from shellcheck_lib_test.instructions.test_resources.eds_populator import act_dir_contents
-from shellcheck_lib_test.instructions.test_resources.utils import new_source2
-from shellcheck_lib_test.test_resources.file_structure import DirContents, empty_dir, Dir, empty_file
+from shellcheck_lib_test.instructions.test_resources.utils import SideEffectsCheck
 
 
-class TestCaseBaseForParser(TestCaseBase):
-    def _run(self,
-             source: SingleInstructionParserSource,
-             arrangement: arrangement,
-             expectation: Expectation):
-        self._check(sut.Parser(), source, arrangement, expectation)
+class TheConfiguration(CleanupConfigurationBase, Configuration):
+    def parser(self) -> SingleInstructionParser:
+        return sut.Parser()
 
+    def expect_successful_execution_with_side_effect(self,
+                                                     side_effects_check: SideEffectsCheck):
+        return Expectation(side_effects_check=side_effects_check)
 
-class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
-    def test_creation_of_directory_with_multiple_path_components(self):
-        self._run(new_source2('first-component/second-component'),
-                  arrangement(eds_contents_before_main=act_dir_contents(DirContents([
-                      Dir('first-component', [
-                          empty_dir('second-component')
-                      ])]))),
-                  Expectation(side_effects_check=AssertCwdIsSubDirOfEds(
-                          pathlib.Path('first-component') / 'second-component'))
-                  )
-
-    def test_argument_exists_as_non_directory__single_path_component(self):
-        self._run(new_source2('file'),
-                  arrangement(eds_contents_before_main=act_dir_contents(DirContents([
-                      empty_file('file')
-                  ]))),
-                  Expectation(main_result=sh_check.IsHardError()),
-                  )
+    def expect_target_is_not_a_directory(self):
+        return Expectation(main_result=sh_check.IsHardError())
 
 
 def suite():
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestCasesThatTestIntegrationByAFewRandomTests))
-    return ret_val
-
-
-if __name__ == '__main__':
-    unittest.main()
+    return suite_for(TheConfiguration())
