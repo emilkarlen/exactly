df8a740961db40d4541e187440f641beece8bd35
Emil Karlén <emil@member.fsf.org>
2016-01-08 17:27:03 +0100

    Refactoring: Test of change_dir: Changed to use new ValueAssertion

--- test/shellcheck_lib_test/instructions/assert_phase/change_dir.py
+++ test/shellcheck_lib_test/instructions/assert_phase/change_dir.py
@@ -7,7 +7,8 @@ from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_ch
 from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.change_dir_instruction_test import \
     Configuration, suite_for
 from shellcheck_lib_test.instructions.test_resources import pfh_check
-from shellcheck_lib_test.instructions.test_resources.assertion_utils.side_effects import SideEffectsCheck
+from shellcheck_lib_test.instructions.test_resources.assertion_utils.side_effects import AdaptVa
+from shellcheck_lib_test.test_resources.value_assertion import ValueAssertion
 
 
 class TheConfiguration(AssertConfigurationBase, Configuration):
@@ -18,8 +19,8 @@ class TheConfiguration(AssertConfigurationBase, Configuration):
         return sut.Parser()
 
     def expect_successful_execution_with_side_effect(self,
-                                                     side_effects_check: SideEffectsCheck):
-        return Expectation(side_effects_check=side_effects_check)
+                                                     side_effects_check: ValueAssertion):
+        return Expectation(side_effects_check=AdaptVa(side_effects_check))
 
     def expect_target_is_not_a_directory(self):
         return Expectation(main_result=pfh_check.is_hard_error())
--- test/shellcheck_lib_test/instructions/setup/change_dir.py
+++ test/shellcheck_lib_test/instructions/setup/change_dir.py
@@ -9,7 +9,8 @@ from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.ch
 from shellcheck_lib_test.instructions.setup.test_resources.configuration import SetupConfigurationBase
 from shellcheck_lib_test.instructions.setup.test_resources.instruction_check import Expectation
 from shellcheck_lib_test.instructions.test_resources import sh_check
-from shellcheck_lib_test.instructions.test_resources.assertion_utils.side_effects import SideEffectsCheck
+from shellcheck_lib_test.instructions.test_resources.assertion_utils.side_effects import AdaptVa
+from shellcheck_lib_test.test_resources.value_assertion import ValueAssertion
 
 
 class TheConfiguration(SetupConfigurationBase, Configuration):
@@ -20,8 +21,8 @@ class TheConfiguration(SetupConfigurationBase, Configuration):
         return sut.Parser()
 
     def expect_successful_execution_with_side_effect(self,
-                                                     side_effects_check: SideEffectsCheck):
-        return Expectation(side_effects_check=side_effects_check)
+                                                     side_effects_check: ValueAssertion):
+        return Expectation(side_effects_check=AdaptVa(side_effects_check))
 
     def expect_target_is_not_a_directory(self):
         return Expectation(main_result=sh_check.IsHardError())
