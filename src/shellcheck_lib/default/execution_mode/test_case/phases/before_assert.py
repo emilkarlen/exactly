a7188a8b29a3e80038ac0f60c5094ede4f81523c
Emil Karlén <emil@member.fsf.org>
2016-01-08 02:27:03 +0100

    before-assert: Adds instructions to default instruction set

--- src/shellcheck_lib/default/execution_mode/test_case/phases/before_assert.py
+++ src/shellcheck_lib/default/execution_mode/test_case/phases/before_assert.py
@@ -1,1 +1,13 @@
-INSTRUCTIONS = {}
+from shellcheck_lib.instructions.cleanup import execute, shell
+from shellcheck_lib.test_case.instruction_setup import SingleInstructionSetup
+
+INSTRUCTIONS = {
+    'execute':
+        SingleInstructionSetup(
+                execute.parser('execute'),
+                execute.description('execute')),
+    'shell':
+        SingleInstructionSetup(
+                shell.parser(),
+                shell.description('shell')),
+}
