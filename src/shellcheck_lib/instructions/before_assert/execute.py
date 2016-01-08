3edf2657e4d9f34169ccf90298cd40561aeefee6
Emil Karlén <emil@member.fsf.org>
2016-01-08 02:25:20 +0100

    before-assert: Instruction for 'execute'

--- src/shellcheck_lib/instructions/before_assert/execute.py
+++ src/shellcheck_lib/instructions/before_assert/execute.py
@@ -1,14 +1,13 @@
 from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser
-from shellcheck_lib.execution.phases import CLEANUP
+from shellcheck_lib.execution.phases import ASSERT
 from shellcheck_lib.instructions.multi_phase_instructions import execute
 from shellcheck_lib.instructions.utils import sub_process_execution
-from shellcheck_lib.instructions.utils.pre_or_post_validation import PreOrPostEdsSvhValidationForSuccessOrHardError, \
-    PreOrPostEdsSvhValidationErrorValidator
+from shellcheck_lib.instructions.utils.pre_or_post_validation import PreOrPostEdsSvhValidationErrorValidator
 from shellcheck_lib.test_case.instruction_description import Description
 from shellcheck_lib.test_case.os_services import OsServices
-from shellcheck_lib.test_case.sections.cleanup import CleanupPhaseInstruction
+from shellcheck_lib.test_case.sections.before_assert import BeforeAssertPhaseInstruction
 from shellcheck_lib.test_case.sections.common import GlobalEnvironmentForPostEdsPhase, GlobalEnvironmentForPreEdsStep
-from shellcheck_lib.test_case.sections.result import sh
+from shellcheck_lib.test_case.sections.result import pfh
 from shellcheck_lib.test_case.sections.result import svh
 
 
@@ -18,31 +17,26 @@ def description(instruction_name: str) -> Description:
 
 
 def parser(instruction_name: str) -> SingleInstructionParser:
-    return execute.InstructionParser(sub_process_execution.InstructionMetaInfo(CLEANUP.identifier,
+    return execute.InstructionParser(sub_process_execution.InstructionMetaInfo(ASSERT.identifier,
                                                                                instruction_name),
                                      lambda setup: _Instruction(setup))
 
 
-class _Instruction(CleanupPhaseInstruction):
+class _Instruction(BeforeAssertPhaseInstruction):
     def __init__(self,
                  setup: execute.SetupForExecutableWithArguments):
         self.setup = setup
+        self.validator = PreOrPostEdsSvhValidationErrorValidator(setup.validator)
 
     def validate_pre_eds(self,
                          environment: GlobalEnvironmentForPreEdsStep) -> svh.SuccessOrValidationErrorOrHardError:
-        validator = PreOrPostEdsSvhValidationErrorValidator(self.setup.validator)
-        return validator.validate_pre_eds_if_applicable(environment.home_directory)
+        return self.validator.validate_pre_eds_if_applicable(environment.home_directory)
 
-    def _validate_from_main(
-            self,
-            environment: GlobalEnvironmentForPostEdsPhase) -> sh.SuccessOrHardError:
-        validator = PreOrPostEdsSvhValidationForSuccessOrHardError(self.setup.validator)
-        return validator.validate_pre_or_post_eds(environment.home_and_eds)
+    def validate_post_setup(self,
+                            environment: GlobalEnvironmentForPostEdsPhase) -> svh.SuccessOrValidationErrorOrHardError:
+        return self.validator.validate_post_eds_if_applicable(environment.eds)
 
     def main(self,
-             environment: GlobalEnvironmentForPostEdsPhase,
-             os_services: OsServices) -> sh.SuccessOrHardError:
-        validation_result = self._validate_from_main(environment)
-        if validation_result.is_hard_error:
-            return validation_result
+             os_services: OsServices,
+             environment: GlobalEnvironmentForPostEdsPhase) -> pfh.PassOrFailOrHardError:
         return execute.execute_and_return_sh(self.setup, environment.home_and_eds)
---
+++ test/shellcheck_lib_test/instructions/before_assert/execute.py
@@ -0,0 +1,26 @@
+import unittest
+
+from shellcheck_lib.document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParser
+from shellcheck_lib.instructions.before_assert import execute as sut
+from shellcheck_lib_test.instructions.before_assert.test_resources.configuration import BeforeAssertConfigurationBase
+from shellcheck_lib_test.instructions.before_assert.test_resources.instruction_check import Expectation
+from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.execute_instruction_test import \
+    suite_for, Configuration
+from shellcheck_lib_test.instructions.test_resources import svh_check__va
+
+
+class TheConfiguration(BeforeAssertConfigurationBase, Configuration):
+    def expectation_of_failure_because_specified_file_under_eds_is_missing(self):
+        return Expectation(validation_post_setup=svh_check__va.is_validation_error())
+
+    def parser(self) -> SingleInstructionParser:
+        return sut.parser('instruction name')
+
+
+def suite():
+    return suite_for(TheConfiguration())
+
+
+if __name__ == '__main__':
+    unittest.main()
