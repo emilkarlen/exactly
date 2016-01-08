e2051bf130f343b8ad356d9f8ff61f50c63426e3
Emil Karlén <emil.karlen@fripost.org>
2016-01-08 17:27:03 +0100

    Refactoring: Test of change_dir: Changed to use new ValueAssertion

--- test/shellcheck_lib_test/instructions/multi_phase_instructions/test_resources/change_dir_instruction_test.py
+++ test/shellcheck_lib_test/instructions/multi_phase_instructions/test_resources/change_dir_instruction_test.py
@@ -2,16 +2,16 @@ import pathlib
 import unittest
 
 from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.configuration import ConfigurationBase
-from shellcheck_lib_test.instructions.test_resources.assertion_utils.side_effects import SideEffectsCheck, \
-    AssertCwdIsSubDirOfActDir
+from shellcheck_lib_test.instructions.test_resources.assertion_utils.side_effects__va import AssertCwdIsSubDirOfActDir
 from shellcheck_lib_test.instructions.test_resources.eds_populator import act_dir_contents
 from shellcheck_lib_test.instructions.test_resources.utils import new_source2
 from shellcheck_lib_test.test_resources.file_structure import DirContents, Dir, empty_dir, empty_file
+from shellcheck_lib_test.test_resources.value_assertion import ValueAssertion
 
 
 class Configuration(ConfigurationBase):
     def expect_successful_execution_with_side_effect(self,
-                                                     side_effects_check: SideEffectsCheck):
+                                                     side_effects_check: ValueAssertion):
         raise NotImplementedError()
 
     def expect_target_is_not_a_directory(self):
