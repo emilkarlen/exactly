4bb078efb3b4f8abd27a9a5eab3631b246f4a4b7
Emil Karlén <emil@member.fsf.org>
2016-01-09 11:26:01 +0100

    before-assert: Instruction for 'new_dir'

--- test/shellcheck_lib_test/instructions/before_assert/__init__.py
+++ test/shellcheck_lib_test/instructions/before_assert/__init__.py
@@ -1,12 +1,13 @@
 import unittest
 
-from shellcheck_lib_test.instructions.before_assert import test_resources, change_dir, execute, shell
+from shellcheck_lib_test.instructions.before_assert import test_resources, change_dir, execute, shell, new_dir
 
 
 def suite():
     return unittest.TestSuite([
         test_resources.suite(),
         change_dir.suite(),
+        new_dir.suite(),
         shell.suite(),
         execute.suite(),
     ])
