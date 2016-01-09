26f4ffbb3d468d4c0db744f2b787a2b7da00aad0
Emil Karlén <emil.karlen@fripost.org>
2016-01-09 16:37:57 +0100

    debug execution

--- test/shellcheck_lib_test/tmp/debug_execution.py
+++ test/shellcheck_lib_test/tmp/debug_execution.py
@@ -5,5 +5,5 @@ from shellcheck_lib.default.default_main_program_setup import *
 
 class TestCase(unittest.TestCase):
     def test(self):
-        exit_status = default_main_program().execute(['/home/karlen/tmp/shellcheck/home-bugg/tc.testcase'])
+        exit_status = default_main_program().execute(['a.testcase'])
         print(exit_status)
