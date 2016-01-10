ddf3c997d1c388f26e89dba4c2d8a6518ac4eb99
Emil Karlén <emil.karlen@fripost.org>
2016-01-10 19:27:37 +0100

    act/executable_file: Tests of generation of relative paths

--- test/shellcheck_lib_test/instructions/act/executable_file.py
+++ test/shellcheck_lib_test/instructions/act/executable_file.py
@@ -1,3 +1,5 @@
+import os
+
 from shellcheck_lib.instructions.act import executable_file as sut
 from shellcheck_lib_test.instructions.act.test_resources.instruction_check import *
 from shellcheck_lib_test.instructions.test_resources import svh_check__va
@@ -63,19 +65,62 @@ class TestValidation(TestBase):
 
 
 class TestStatementGeneration(TestBase):
-    def test_absolute_path_is_unmodified__without_arguments(self):
+    def test_absolute_path_is_unmodified__sans_arguments(self):
         with absolute_path_toexecutable_file() as absolute_path:
             source = str(absolute_path)
             self._check(source,
                         Arrangement(),
                         Expectation(main_side_effects_on_script_source=_contains_single_line(source)))
 
-    def test_that_relative_path_is_transformed_to_absolute_path(self):
-        self.fail('TODO')
+    def test_absolute_path_is_unmodified__with_arguments(self):
+        with absolute_path_toexecutable_file() as absolute_path:
+            source = '%s argument1 argument2' % str(absolute_path)
+            self._check(source,
+                        Arrangement(),
+                        Expectation(main_side_effects_on_script_source=_contains_single_line(source)))
+
+    def test_that_relative_path_is_transformed_to_absolute_path__sans_arguments(self):
+        relative_path = 'relative-path-of-existing-program'
+        source = relative_path
+        self._check(source,
+                    Arrangement(home_dir_contents=DirContents([
+                        executable_file(relative_path, '')])),
+                    Expectation(main_side_effects_on_script_source=_contains_single_line__with_home_prepended(source)))
+
+    def test_that_relative_path_is_transformed_to_absolute_path__with_arguments(self):
+        relative_path = 'relative-path-of-existing-program'
+        source = '%s argument-to-program' % relative_path
+        self._check(source,
+                    Arrangement(home_dir_contents=DirContents([
+                        executable_file(relative_path, '')])),
+                    Expectation(main_side_effects_on_script_source=_contains_single_line__with_home_prepended(source)))
+
+
+class LineCheckInfo(tuple):
+    def __new__(cls,
+                home_and_eds: HomeAndEds,
+                source_line: str):
+        return tuple.__new__(cls, (home_and_eds,
+                                   source_line))
+
+    @property
+    def home_and_eds(self) -> HomeAndEds:
+        return self[0]
+
+    @property
+    def source_line(self) -> str:
+        return self[1]
 
 
 def _contains_single_line(line: str) -> va.ValueAssertion:
-    return ContainsSingleLine(va.Equals(line))
+    return ContainsSingleLine(LineChecker(lambda x: line))
+
+
+def _contains_single_line__with_home_prepended(line: str) -> va.ValueAssertion:
+    def prepend_home(home_and_eds: HomeAndEds) -> str:
+        return str(home_and_eds.home_dir_path) + os.path.sep + line
+
+    return ContainsSingleLine(LineChecker(prepend_home))
 
 
 class ContainsSingleLine(va.ValueAssertion):
@@ -85,12 +130,26 @@ class ContainsSingleLine(va.ValueAssertion):
 
     def apply(self,
               put: unittest.TestCase,
-              value: ScriptSourceBuilder,
+              value: SourceBuilderCheckInfo,
               message_builder: va.MessageBuilder = va.MessageBuilder()):
+        source_builder = value.source_builder
         put.assertEqual(1,
-                        len(value.source_lines),
+                        len(source_builder.source_lines),
                         'Number of source lines')
         self.line_assertion.apply(put,
-                                  value.source_lines[0],
+                                  LineCheckInfo(value.home_and_eds,
+                                                source_builder.source_lines[0]),
                                   va.sub_component_builder('line[0]',
                                                            message_builder))
+
+
+class LineChecker(va.ValueAssertion):
+    def __init__(self,
+                 get_expected: types.FunctionType):
+        self.get_expected = get_expected
+
+    def apply(self,
+              put: unittest.TestCase,
+              value: LineCheckInfo,
+              message_builder: va.MessageBuilder = va.MessageBuilder()):
+        va.Equals(self.get_expected(value.home_and_eds)).apply(put, value.source_line)
