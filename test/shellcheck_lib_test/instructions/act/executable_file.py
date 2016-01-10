ecbe4cdc82d0b6d4a81af693c91561518bcbbfe6
Emil Karlén <emil.karlen@fripost.org>
2016-01-10 19:04:03 +0100

    act/executable_file: Tests of generation of absolute path program

--- test/shellcheck_lib_test/instructions/act/executable_file.py
+++ test/shellcheck_lib_test/instructions/act/executable_file.py
@@ -5,63 +5,92 @@ from shellcheck_lib_test.test_resources.file_structure import DirContents, execu
 from shellcheck_lib_test.test_resources.file_utils import absolute_path_toexecutable_file
 
 
-class TestBase(TestCaseBase):
-    def __check(self,
-                source: str,
-                arrangement: Arrangement,
-                expectation: Expectation):
-        self._check(sut.ExecutableFileInstruction,
-                    source,
-                    arrangement,
-                    expectation)
-
-
-class TestValidation(TestBase):
-    def test_fails_when_command_is_relative_but_does_not_exist_relative_home__no_arguments(self):
-        self.__check('relative-path-of-existing-program',
-                     Arrangement(),
-                     Expectation(validation_pre_eds=svh_check__va.is_validation_error()))
-
-    def test_succeeds_when_command_is_relative_and_does_exist_relative_home__no_arguments(self):
-        relative_path = 'relative-path-of-existing-program'
-        self.__check(relative_path,
-                     Arrangement(home_dir_contents=DirContents([
-                         executable_file(relative_path, '')])),
-                     Expectation())
-
-    def test_fails_when_command_is_relative_but_does_not_exist_relative_home__with_arguments(self):
-        relative_path = 'relative-path-of-existing-program'
-        self.__check('%s argument-to-program' % relative_path,
-                     Arrangement(),
-                     Expectation(validation_pre_eds=svh_check__va.is_validation_error()))
-
-    def test_succeeds_when_command_is_relative_and_does_exist_relative_home__with_arguments(self):
-        relative_path = 'relative-path-of-existing-program'
-        self.__check('%s argument-to-program' % relative_path,
-                     Arrangement(home_dir_contents=DirContents([
-                         executable_file(relative_path, '')])),
-                     Expectation())
-
-    def test_succeeds_when_command_is_absolute_without_arguments(self):
-        with absolute_path_toexecutable_file() as absolute_path:
-            self.__check(str(absolute_path),
-                         Arrangement(),
-                         Expectation())
-
-    def test_succeeds_when_command_is_absolute_with_arguments(self):
-        with absolute_path_toexecutable_file() as absolute_path:
-            self.__check('%s argument-to-program' % str(absolute_path),
-                         Arrangement(),
-                         Expectation())
-
-
-class TestStatementGeneration(TestBase):
-    def test_that_relative_path_is_transformed_to_absolute_path(self):
-        self.fail('TODO')
-
-
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(TestValidation),
         unittest.makeSuite(TestStatementGeneration),
     ])
+
+
+class TestBase(TestCaseBase):
+    def _check(self,
+               source: str,
+               arrangement: Arrangement,
+               expectation: Expectation):
+        super()._check(sut.ExecutableFileInstruction,
+                       source,
+                       arrangement,
+                       expectation)
+
+
+class TestValidation(TestBase):
+    def test_fails_when_command_is_relative_but_does_not_exist_relative_home__no_arguments(self):
+        self._check('relative-path-of-existing-program',
+                    Arrangement(),
+                    Expectation(validation_pre_eds=svh_check__va.is_validation_error()))
+
+    def test_succeeds_when_command_is_relative_and_does_exist_relative_home__no_arguments(self):
+        relative_path = 'relative-path-of-existing-program'
+        self._check(relative_path,
+                    Arrangement(home_dir_contents=DirContents([
+                        executable_file(relative_path, '')])),
+                    Expectation())
+
+    def test_fails_when_command_is_relative_but_does_not_exist_relative_home__with_arguments(self):
+        relative_path = 'relative-path-of-existing-program'
+        self._check('%s argument-to-program' % relative_path,
+                    Arrangement(),
+                    Expectation(validation_pre_eds=svh_check__va.is_validation_error()))
+
+    def test_succeeds_when_command_is_relative_and_does_exist_relative_home__with_arguments(self):
+        relative_path = 'relative-path-of-existing-program'
+        self._check('%s argument-to-program' % relative_path,
+                    Arrangement(home_dir_contents=DirContents([
+                        executable_file(relative_path, '')])),
+                    Expectation())
+
+    def test_succeeds_when_command_is_absolute_without_arguments(self):
+        with absolute_path_toexecutable_file() as absolute_path:
+            self._check(str(absolute_path),
+                        Arrangement(),
+                        Expectation())
+
+    def test_succeeds_when_command_is_absolute_with_arguments(self):
+        with absolute_path_toexecutable_file() as absolute_path:
+            self._check('%s argument-to-program' % str(absolute_path),
+                        Arrangement(),
+                        Expectation())
+
+
+class TestStatementGeneration(TestBase):
+    def test_absolute_path_is_unmodified__without_arguments(self):
+        with absolute_path_toexecutable_file() as absolute_path:
+            source = str(absolute_path)
+            self._check(source,
+                        Arrangement(),
+                        Expectation(main_side_effects_on_script_source=_contains_single_line(source)))
+
+    def test_that_relative_path_is_transformed_to_absolute_path(self):
+        self.fail('TODO')
+
+
+def _contains_single_line(line: str) -> va.ValueAssertion:
+    return ContainsSingleLine(va.Equals(line))
+
+
+class ContainsSingleLine(va.ValueAssertion):
+    def __init__(self,
+                 line_assertion: va.ValueAssertion):
+        self.line_assertion = line_assertion
+
+    def apply(self,
+              put: unittest.TestCase,
+              value: ScriptSourceBuilder,
+              message_builder: va.MessageBuilder = va.MessageBuilder()):
+        put.assertEqual(1,
+                        len(value.source_lines),
+                        'Number of source lines')
+        self.line_assertion.apply(put,
+                                  value.source_lines[0],
+                                  va.sub_component_builder('line[0]',
+                                                           message_builder))
