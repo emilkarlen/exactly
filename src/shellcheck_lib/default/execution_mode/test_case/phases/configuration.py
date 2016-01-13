666ee90a2da9ba559b37050dd9dc94bb2c07eb3e
Emil Karlén <emil.karlen@fripost.org>
2016-01-13 18:45:58 +0100

    New instruction in default instruction set: configuration/mode

--- src/shellcheck_lib/default/execution_mode/test_case/phases/configuration.py
+++ src/shellcheck_lib/default/execution_mode/test_case/phases/configuration.py
@@ -1,4 +1,4 @@
-from shellcheck_lib.instructions.configuration import home
+from shellcheck_lib.instructions.configuration import home, execution_mode
 from shellcheck_lib.test_case.instruction_setup import SingleInstructionSetup
 
 INSTRUCTIONS = {
@@ -6,4 +6,8 @@ INSTRUCTIONS = {
         SingleInstructionSetup(
                 home.Parser(),
                 home.TheDescription('home')),
+    'mode':
+        SingleInstructionSetup(
+                execution_mode.Parser(),
+                execution_mode.TheDescription('mode')),
 }
