01d6ffe85a39276c9bcd159fadab878a23b4ffe1
Emil Karlén <emil.karlen@fripost.org>
2016-01-19 21:50:37 +0100

    Instruction Set: simplifications of assert phase

--- src/shellcheck_lib/default/execution_mode/test_case/phases/assert_.py
+++ src/shellcheck_lib/default/execution_mode/test_case/phases/assert_.py
@@ -7,47 +7,19 @@ from shellcheck_lib.instructions.assert_phase import new_dir
 from shellcheck_lib.instructions.assert_phase import shell
 from shellcheck_lib.instructions.assert_phase import stdout_stderr as stdout_stderr_instruction
 from shellcheck_lib.instructions.assert_phase import type
-from shellcheck_lib.test_case.instruction_setup import SingleInstructionSetup
+from shellcheck_lib.test_case.instruction_setup import instruction_set_from_name_and_setup_constructor_list
 
-INSTRUCTIONS = {
-    'contents':
-        SingleInstructionSetup(
-                contents_instruction.Parser(),
-                contents_instruction.TheDescription('contents')),
-    'dir':
-        SingleInstructionSetup(
-                new_dir.Parser(),
-                new_dir.description('dir')),
-    'env':
-        SingleInstructionSetup(
-                env.PARSER,
-                env.description('env')),
-    'execute':
-        SingleInstructionSetup(
-                execute.parser('execute'),
-                execute.description('execute')),
-    'exitcode':
-        SingleInstructionSetup(
-                exitcode_instruction.Parser(),
-                exitcode_instruction.TheDescription('exitcode')),
-    'pwd':
-        SingleInstructionSetup(
-                change_dir.Parser(),
-                change_dir.description('pwd')),
-    'shell':
-        SingleInstructionSetup(
-                shell.parser(),
-                shell.TheDescription('shell')),
-    'stderr':
-        SingleInstructionSetup(
-                stdout_stderr_instruction.ParserForContentsForStderr(),
-                stdout_stderr_instruction.TheDescription('stderr', 'stderr')),
-    'stdout':
-        SingleInstructionSetup(
-                stdout_stderr_instruction.ParserForContentsForStdout(),
-                stdout_stderr_instruction.TheDescription('stdout', 'stdout')),
-    'type':
-        SingleInstructionSetup(
-                type.Parser(),
-                type.TheDescription('type')),
-}
+INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
+        [
+            ('contents', contents_instruction.setup),
+            ('dir', new_dir.setup),
+            ('env', env.setup),
+            ('execute', execute.setup),
+            ('exitcode', exitcode_instruction.setup),
+            ('pwd', change_dir.setup),
+            ('shell', shell.setup),
+            ('stderr', stdout_stderr_instruction.setup_for_stderr),
+            ('stdout', stdout_stderr_instruction.setup_for_stdout),
+            ('type', type.setup),
+        ]
+)
--- test/shellcheck_lib_test/instructions/assert_phase/execute.py
+++ test/shellcheck_lib_test/instructions/assert_phase/execute.py
@@ -4,6 +4,7 @@ from shellcheck_lib.document.parser_implementations.instruction_parser_for_singl
     SingleInstructionParser
 from shellcheck_lib.instructions.assert_phase import execute as sut
 from shellcheck_lib.test_case.instruction_description import Description
+from shellcheck_lib.test_case.instruction_setup import SingleInstructionSetup
 from shellcheck_lib_test.instructions.assert_phase.test_resources.configuration import AssertConfigurationBase
 from shellcheck_lib_test.instructions.assert_phase.test_resources.instruction_check import Expectation
 from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.execute_instruction_test import \
@@ -12,14 +13,17 @@ from shellcheck_lib_test.instructions.test_resources import svh_check
 
 
 class TheConfiguration(AssertConfigurationBase, Configuration):
+    def instruction_setup(self) -> SingleInstructionSetup:
+        return sut.setup('instruction name')
+
+    def parser(self) -> SingleInstructionParser:
+        return self.instruction_setup()
+
     def description(self) -> Description:
-        return sut.description('instruction name')
+        return self.instruction_setup().description
 
     def expect_failure_because_specified_file_under_eds_is_missing(self):
         return Expectation(validation_post_eds=svh_check.is_validation_error())
-
-    def parser(self) -> SingleInstructionParser:
-        return sut.parser('instruction name')
 
 
 
