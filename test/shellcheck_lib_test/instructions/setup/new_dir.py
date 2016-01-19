8d8d74ff83a55d9788741fd82fe5623ac12c490b
Emil Karlén <emil@member.fsf.org>
2016-01-19 22:58:19 +0100

    Instruction Set: simplifications of setup phase

--- test/shellcheck_lib_test/instructions/setup/new_dir.py
+++ test/shellcheck_lib_test/instructions/setup/new_dir.py
@@ -2,6 +2,7 @@ from shellcheck_lib.document.parser_implementations.instruction_parser_for_singl
     SingleInstructionParser
 from shellcheck_lib.instructions.setup import new_dir as sut
 from shellcheck_lib.test_case.instruction_description import Description
+from shellcheck_lib.test_case.instruction_setup import SingleInstructionSetup
 from shellcheck_lib_test.instructions.multi_phase_instructions.test_resources.new_dir_instruction_test import \
     Configuration, suite_for
 from shellcheck_lib_test.instructions.setup.test_resources.configuration import SetupConfigurationBase
@@ -10,11 +11,14 @@ from shellcheck_lib_test.instructions.test_resources import sh_check
 
 
 class TheConfiguration(SetupConfigurationBase, Configuration):
+    def instruction_setup(self) -> SingleInstructionSetup:
+        return sut.setup('instruction name')
+
     def parser(self) -> SingleInstructionParser:
-        return sut.Parser()
+        return self.instruction_setup()
 
     def description(self) -> Description:
-        return sut.description('instruction name')
+        return self.instruction_setup().description
 
     def expect_failure_to_create_dir(self):
         return Expectation(main_result=sh_check.IsHardError())
