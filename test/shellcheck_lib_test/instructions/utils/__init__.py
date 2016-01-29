808dac11834b228c505328d50536d6530dc76cfd
Emil Karlén <emil.karlen@fripost.org>
2016-01-30 00:00:49 +0100

    Include accidentaly excluded tests

--- test/shellcheck_lib_test/instructions/utils/__init__.py
+++ test/shellcheck_lib_test/instructions/utils/__init__.py
@@ -1,19 +1,19 @@
 import unittest
 
-from shellcheck_lib_test.instructions.utils import executable_file
-from shellcheck_lib_test.instructions.utils import file_ref
-from shellcheck_lib_test.instructions.utils import parse_file_ref
-from shellcheck_lib_test.instructions.utils import parse_here_document
-from shellcheck_lib_test.instructions.utils import parse_utils
+from shellcheck_lib_test.instructions.utils import executable_file, sub_process_execution
+from shellcheck_lib_test.instructions.utils import file_properties
+from shellcheck_lib_test.instructions.utils import file_ref, parse_file_ref, parse_here_document, parse_utils
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(parse_utils.suite())
+    ret_val.addTest(file_properties.suite())
     ret_val.addTest(file_ref.suite())
     ret_val.addTest(parse_file_ref.suite())
     ret_val.addTest(parse_here_document.suite())
     ret_val.addTest(executable_file.suite())
+    ret_val.addTest(sub_process_execution.suite())
     return ret_val
 
 
