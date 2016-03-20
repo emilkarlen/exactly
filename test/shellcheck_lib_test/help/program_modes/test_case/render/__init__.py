c382778847fceda7568e4b968214ef550c878777
Emil Karlén <emil@member.fsf.org>
2016-03-20 10:33:31 +0100

    Help text: First test of rendition of test-case phase help

--- test/shellcheck_lib_test/help/program_modes/test_case/render/__init__.py
+++ test/shellcheck_lib_test/help/program_modes/test_case/render/__init__.py
@@ -1,11 +1,12 @@
 import unittest
 
-from shellcheck_lib_test.help.program_modes.test_case.render import render_instruction
+from shellcheck_lib_test.help.program_modes.test_case.render import render_instruction, test_case_phase
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(render_instruction.suite())
+    ret_val.addTest(test_case_phase.suite())
     return ret_val
 
 
