4fbb8f4ce80a7017d1260dbd7f831504d18f4f0b
Emil Karlén <emil.karlen@fripost.org>
2016-03-20 11:01:58 +0100

    Help text: Test of TestCasePhaseHelp

--- test/shellcheck_lib_test/help/program_modes/test_case/render/test_case_phase.py
+++ test/shellcheck_lib_test/help/program_modes/test_case/render/test_case_phase.py
@@ -1,8 +1,11 @@
 import unittest
 
-from shellcheck_lib.help.program_modes.test_case.contents_structure import TestCasePhaseReference, TestCasePhaseHelp
+from shellcheck_lib.help.program_modes.test_case.contents_structure import TestCasePhaseReference, TestCasePhaseHelp, \
+    TestCasePhaseInstructionSet
 from shellcheck_lib.help.program_modes.test_case.render import test_case_phase as sut
-from shellcheck_lib.help.utils.description import single_line_description
+from shellcheck_lib.help.utils.description import single_line_description, Description
+from shellcheck_lib.util.textformat.structure.paragraph import para
+from shellcheck_lib_test.help.test_resources import test_case_phase_instruction_set
 from shellcheck_lib_test.util.textformat.test_resources import structure as struct_check
 
 
@@ -15,7 +18,37 @@ class TestRender(unittest.TestCase):
                                      None)
         # ACT
         actual = sut.render_test_case_phase_overview(tcp_help)
+        # ASSERT
         struct_check.is_section_contents.apply(self, actual)
+
+    def test_with_instruction_set(self):
+        # ARRANGE
+        phase_ref = TestCasePhaseReference(single_line_description('single line'))
+        tcp_help = TestCasePhaseHelp('phase name',
+                                     phase_ref,
+                                     non_empty_instruction_set('phase name'))
+        # ACT
+        actual = sut.render_test_case_phase_overview(tcp_help)
+        # ASSERT
+        struct_check.is_section_contents.apply(self, actual)
+
+    def test_with_description_that_has_a_rest_part(self):
+        # ARRANGE
+        phase_ref = TestCasePhaseReference(Description('single line',
+                                                       [para('paragraph 1'),
+                                                        para('paragraph 2')]))
+        tcp_help = TestCasePhaseHelp('phase name',
+                                     phase_ref,
+                                     non_empty_instruction_set('phase name'))
+        # ACT
+        actual = sut.render_test_case_phase_overview(tcp_help)
+        # ASSERT
+        struct_check.is_section_contents.apply(self, actual)
+
+
+def non_empty_instruction_set(phase_name: str) -> TestCasePhaseInstructionSet:
+    return test_case_phase_instruction_set(phase_name,
+                                           ['instr1'])
 
 
 def suite() -> unittest.TestSuite:
