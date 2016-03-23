8b0672c5581dc66dd81b9b3db0e254d6d933d40c
Emil Karlén <emil.karlen@fripost.org>
2016-03-23 18:32:42 +0100

    Refactoring of tests: Splitting some tests into separate modules

--- test/shellcheck_lib_test/util/textformat/formatting/formatting_test_impls/paragraph.py
+++ test/shellcheck_lib_test/util/textformat/formatting/formatting_test_impls/paragraph.py
@@ -1,7 +1,7 @@
 import unittest
 
 from shellcheck_lib.util.textformat.formatting import paragraph_item as sut
-from shellcheck_lib_test.util.textformat.test_resources.constr import text, para, BLANK_LINE
+from shellcheck_lib_test.util.textformat.test_resources.constr import text, para
 
 
 class TestParagraph(unittest.TestCase):
@@ -33,57 +33,12 @@ class TestParagraph(unittest.TestCase):
                          actual)
 
 
-class TestParagraphItems(unittest.TestCase):
-    def test_no_item(self):
-        formatter = sut.Formatter(sut.Wrapper(5),
-                                  num_item_separator_lines=1)
-        actual = formatter.format_paragraph_items([])
-        self.assertEqual([],
-                         actual)
-
-    def test_single_item(self):
-        p = para([text('1234 12 34')])
-        formatter = sut.Formatter(sut.Wrapper(5),
-                                  num_item_separator_lines=1)
-        actual = formatter.format_paragraph_items([p])
-        self.assertEqual(['1234',
-                          '12 34'],
-                         actual)
-
-    def test_multiple_items_with_zero_separator_lines(self):
-        p1 = para([text('1234 12 34')])
-        p2 = para([text('abc')])
-        formatter = sut.Formatter(sut.Wrapper(5),
-                                  num_item_separator_lines=0)
-        actual = formatter.format_paragraph_items([p1, p2])
-        self.assertEqual(['1234',
-                          '12 34',
-                          'abc'],
-                         actual)
-
-    def test_multiple_items_with_non_zero_separator_lines(self):
-        p1 = para([text('1234 12 34')])
-        p2 = para([text('abc')])
-        formatter = sut.Formatter(sut.Wrapper(page_width=5),
-                                  num_item_separator_lines=2)
-        actual = formatter.format_paragraph_items([p1, p2])
-        self.assertEqual(['1234',
-                          '12 34',
-                          BLANK_LINE,
-                          BLANK_LINE,
-                          'abc'],
-                         actual)
-
-
 def formatter_with_page_width(page_width: int) -> sut.Formatter:
     return sut.Formatter(sut.Wrapper(page_width=page_width))
 
 
-def suite():
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestParagraph))
-    ret_val.addTest(unittest.makeSuite(TestParagraphItems))
-    return ret_val
+def suite() -> unittest.TestSuite:
+    return unittest.makeSuite(TestParagraph)
 
 
 def run_suite():
