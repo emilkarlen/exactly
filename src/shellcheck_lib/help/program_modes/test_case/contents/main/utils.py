dee888f0b19e8fc6f44cbec545622dcbf8e9d3e8
Emil Karlén <emil@member.fsf.org>
2016-04-03 17:44:52 +0200

    Help text: concepts

--- src/shellcheck_lib/help/program_modes/test_case/contents/main/utils.py
+++ src/shellcheck_lib/help/program_modes/test_case/contents/main/utils.py
@@ -1,5 +1,6 @@
 from shellcheck_lib.cli.cli_environment import exit_values
 from shellcheck_lib.help.program_modes.test_case.contents_structure import TestCaseHelp
+from shellcheck_lib.help.utils.phase_names import phase_name_dictionary
 from shellcheck_lib.util.textformat.structure.core import ParagraphItem
 from shellcheck_lib.util.textformat.structure.structures import first_column_is_header_table, para
 
@@ -7,10 +8,7 @@ from shellcheck_lib.util.textformat.structure.structures import first_column_is_
 class Setup(tuple):
     def __new__(cls,
                 test_case_help: TestCaseHelp):
-        phase_names = {}
-        for phase in test_case_help.phase_helps_in_order_of_execution:
-            phase_names[phase.name.plain] = phase.name
-        return tuple.__new__(cls, (test_case_help, phase_names))
+        return tuple.__new__(cls, (test_case_help, phase_name_dictionary()))
 
     @property
     def test_case_help(self) -> TestCaseHelp:
--- src/shellcheck_lib/help/utils/phase_names.py
+++ src/shellcheck_lib/help/utils/phase_names.py
@@ -7,3 +7,19 @@ ACT_PHASE_NAME = SectionName(phases.ACT.section_name)
 BEFORE_ASSERT_PHASE_NAME = SectionName(phases.BEFORE_ASSERT.section_name)
 ASSERT_PHASE_NAME = SectionName(phases.ASSERT.section_name)
 CLEANUP_PHASE_NAME = SectionName(phases.CLEANUP.section_name)
+
+ALL = (
+    CONFIGURATION_PHASE_NAME,
+    SETUP_PHASE_NAME,
+    ACT_PHASE_NAME,
+    BEFORE_ASSERT_PHASE_NAME,
+    ASSERT_PHASE_NAME,
+    CLEANUP_PHASE_NAME,
+)
+
+
+def phase_name_dictionary() -> dict:
+    phase_names = {}
+    for phase in ALL:
+        phase_names[phase.plain.replace('-', '_')] = phase
+    return phase_names
