4b97e7dd1cc1ff3597aad3c98b3f4594ea1d6cac
Emil Karlén <emil@member.fsf.org>
2016-04-05 00:01:41 +0200

    textformat/html: Rendering of lists

---
+++ src/shellcheck_lib/util/textformat/formatting/html/paragraph_item.py
@@ -0,0 +1,41 @@
+from xml.etree.ElementTree import Element
+
+from shellcheck_lib.util.textformat.formatting.html import lists as list_rendering
+from shellcheck_lib.util.textformat.formatting.html import literal_layout, paragraph
+from shellcheck_lib.util.textformat.formatting.html import text
+from shellcheck_lib.util.textformat.formatting.html.interfaces import AnyParagraphItemRenderer
+from shellcheck_lib.util.textformat.structure import lists
+from shellcheck_lib.util.textformat.structure.core import ParagraphItem
+from shellcheck_lib.util.textformat.structure.literal_layout import LiteralLayout
+from shellcheck_lib.util.textformat.structure.paragraph import Paragraph
+from shellcheck_lib.util.textformat.structure.table import Table
+
+
+class ParagraphItemRenderer(AnyParagraphItemRenderer):
+    def __init__(self,
+                 text_renderer: text.TextRenderer):
+        self.text_renderer = text_renderer
+
+    def apply(self, parent: Element, x: ParagraphItem) -> Element:
+        if isinstance(x, Paragraph):
+            return self.render_paragraph(parent, x)
+        if isinstance(x, lists.HeaderContentList):
+            return self.render_header_value_list(parent, x)
+        if isinstance(x, LiteralLayout):
+            return self.render_literal_layout(parent, x)
+        if isinstance(x, Table):
+            return self.render_table(parent, x)
+        raise TypeError('Unknown {}: {}'.format(str(ParagraphItem),
+                                                str(type(x))))
+
+    def render_literal_layout(self, parent: Element, x: LiteralLayout):
+        return literal_layout.render(parent, x)
+
+    def render_paragraph(self, parent: Element, x: Paragraph):
+        return paragraph.render(self.text_renderer, parent, x)
+
+    def render_header_value_list(self, parent: Element, x: lists.HeaderContentList):
+        return list_rendering.render(self.text_renderer, self, parent, x)
+
+    def render_table(self, parent: Element, x: Table):
+        raise NotImplementedError()
---
+++ test/shellcheck_lib_test/util/textformat/formatting/html/lists.py
@@ -0,0 +1,206 @@
+import unittest
+from xml.etree.ElementTree import Element, SubElement
+
+from shellcheck_lib.util.textformat.formatting.html import lists as sut
+from shellcheck_lib.util.textformat.formatting.html.interfaces import AnyParagraphItemRenderer
+from shellcheck_lib.util.textformat.formatting.html.text import TextRenderer
+from shellcheck_lib.util.textformat.structure import core
+from shellcheck_lib.util.textformat.structure import lists
+from shellcheck_lib.util.textformat.structure.structures import text, paras, para
+from shellcheck_lib_test.util.textformat.formatting.html.test_resources import as_unicode_str, TARGET_RENDERER
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(TestItemizedListType),
+        unittest.makeSuite(TestOrderedListType),
+        unittest.makeSuite(TestVariableListType),
+    ])
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
+
+
+class TestItemizedListType(unittest.TestCase):
+    def test_empty(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([],
+                                    lists.Format(lists.ListType.ITEMIZED_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root />',
+                         xml_string)
+        self.assertIs(root,
+                      ret_val)
+
+    def test_singleton_element_without_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header'))],
+                                    lists.Format(lists.ListType.ITEMIZED_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<ul><li>header</li></ul>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+    def test_multiple_element_without_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header 1')),
+                                     lists.HeaderContentListItem(text('header 2'))],
+                                    lists.Format(lists.ListType.ITEMIZED_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<ul>'
+                         '<li>header 1</li>'
+                         '<li>header 2</li>'
+                         '</ul>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+    def test_single_element_with_single_para_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header'),
+                                                                 paras('ignored'))],
+                                    lists.Format(lists.ListType.ITEMIZED_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('every para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<ul>'
+                         '<li>header<p>every para text</p></li>'
+                         '</ul>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+    def test_single_element_with_multiple_para_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header'),
+                                                                 [para('ignored'),
+                                                                  para('ignored')])],
+                                    lists.Format(lists.ListType.ITEMIZED_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('every para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<ul>'
+                         '<li>header'
+                         '<p>every para text</p>'
+                         '<p>every para text</p>'
+                         '</li>'
+                         '</ul>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+
+class TestOrderedListType(unittest.TestCase):
+    def test_singleton_element_without_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header'))],
+                                    lists.Format(lists.ListType.ORDERED_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<ol>'
+                         '<li>header</li>'
+                         '</ol>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+
+class TestVariableListType(unittest.TestCase):
+    def test_singleton_element_without_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header'))],
+                                    lists.Format(lists.ListType.VARIABLE_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<dl>'
+                         '<dt>header</dt>'
+                         '</dl>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+    def test_singleton_element_with_multiple_content_paragraphs(self):
+        # ARRANGE #
+        root = Element('root')
+        l = lists.HeaderContentList([lists.HeaderContentListItem(text('header'),
+                                                                 [para('ignored'),
+                                                                  para('ignored')])],
+                                    lists.Format(lists.ListType.VARIABLE_LIST))
+        # ACT #
+        ret_val = sut.render(TextRenderer(TARGET_RENDERER),
+                             ConstantPRenderer('para text'),
+                             root, l)
+        # ASSERT #
+        xml_string = as_unicode_str(root)
+        self.assertEqual('<root>'
+                         '<dl>'
+                         '<dt>header</dt>'
+                         '<dd>'
+                         '<p>para text</p>'
+                         '<p>para text</p>'
+                         '</dd>'
+                         '</dl>'
+                         '</root>',
+                         xml_string)
+        self.assertIs(list(root)[0],
+                      ret_val)
+
+
+class ConstantPRenderer(AnyParagraphItemRenderer):
+    def __init__(self, paragraph_contents: str):
+        self.paragraph_contents = paragraph_contents
+
+    def apply(self, parent: Element, x: core.ParagraphItem) -> Element:
+        p = SubElement(parent, 'p')
+        p.text = self.paragraph_contents
+        return p
