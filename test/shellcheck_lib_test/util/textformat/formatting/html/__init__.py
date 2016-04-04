a3a3161e4b9ef03c02e4377be85f16ced5ff3fa5
Emil Karlén <emil.karlen@fripost.org>
2016-04-04 20:43:59 +0200

    textformat/html: Rendering of LIteralLayout

--- test/shellcheck_lib_test/util/textformat/formatting/html/__init__.py
+++ test/shellcheck_lib_test/util/textformat/formatting/html/__init__.py
@@ -1,10 +1,13 @@
 import unittest
 
-from shellcheck_lib_test.util.textformat.formatting.html import paragraph
+from shellcheck_lib_test.util.textformat.formatting.html import paragraph, literal_layout
 
 
 def suite() -> unittest.TestSuite:
-    return paragraph.suite()
+    return unittest.TestSuite([
+        paragraph.suite(),
+        literal_layout.suite(),
+    ])
 
 
 if __name__ == '__main__':
