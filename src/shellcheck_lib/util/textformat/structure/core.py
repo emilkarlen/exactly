7b444dd5062783bb2caefa9bc8858e23280287f0
Emil Karlén <emil.karlen@fripost.org>
2016-04-04 18:29:55 +0200

    textformat: New visitor: TextVisitor

--- src/shellcheck_lib/util/textformat/structure/core.py
+++ src/shellcheck_lib/util/textformat/structure/core.py
@@ -53,6 +53,26 @@ class AnchorText(Text):
         self.concrete_text = concrete_text
 
 
+class TextVisitor:
+    def visit(self, text: Text):
+        if isinstance(text, StringText):
+            return self.visit_string(text)
+        if isinstance(text, CrossReferenceText):
+            return self.visit_cross_reference(text)
+        if isinstance(text, AnchorText):
+            return self.visit_anchor(text)
+        raise ValueError('Not a value of sub type of %s: %s' % (str(Text), str(text)))
+
+    def visit_string(self, text: StringText):
+        raise NotImplementedError()
+
+    def visit_cross_reference(self, text: CrossReferenceText):
+        raise NotImplementedError()
+
+    def visit_anchor(self, text: AnchorText):
+        raise NotImplementedError()
+
+
 class ParagraphItem:
     """
     An item that is an element of a paragraph-level sequence of elements.
