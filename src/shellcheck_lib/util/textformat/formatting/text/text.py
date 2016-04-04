9f5ce4f4dd2236ae343e91b137267baf74119934
Emil Karlén <emil@member.fsf.org>
2016-04-04 18:34:54 +0200

    Refactoring: TextFormatter: using new TextVisitor

--- src/shellcheck_lib/util/textformat/formatting/text/text.py
+++ src/shellcheck_lib/util/textformat/formatting/text/text.py
@@ -6,19 +6,19 @@ class CrossReferenceFormatter:
         raise NotImplementedError()
 
 
-class TextFormatter:
+class TextFormatter(core.TextVisitor):
     def __init__(self,
                  cross_reference_formatter: CrossReferenceFormatter):
         self._cross_reference_formatter = cross_reference_formatter
 
     def apply(self, text: core.Text) -> str:
-        if isinstance(text, core.StringText):
-            return text.value
-        elif isinstance(text, core.CrossReferenceText):
-            return self._cross_reference_formatter.apply(text)
-        elif isinstance(text, core.AnchorText):
-            return self.apply(text.concrete_text)
-        else:
-            raise ValueError('Text is neither a %s nor a %s: %s' % (str(core.Text),
-                                                                    str(core.CrossReferenceText),
-                                                                    str(text)))
+        return self.visit(text)
+
+    def visit_cross_reference(self, text: core.CrossReferenceText):
+        return self._cross_reference_formatter.apply(text)
+
+    def visit_anchor(self, text: core.AnchorText):
+        return self.apply(text.concrete_text)
+
+    def visit_string(self, text: core.StringText):
+        return text.value
