c3099e9f341940e734475e0f99aaaaa30ca021c2
Emil Karlén <emil@member.fsf.org>
2016-04-05 21:59:58 +0200

    Help system: Refactoring: Cleanup of handle_help_request by getting rid of superfluous argument (which previous commit fixes to passed in different way)

--- src/shellcheck_lib/cli/program_modes/help/program_modes/test_case/request_rendering.py
+++ src/shellcheck_lib/cli/program_modes/help/program_modes/test_case/request_rendering.py
@@ -4,7 +4,6 @@ from shellcheck_lib.help.program_modes.test_case.contents.main.overview import O
 from shellcheck_lib.help.program_modes.test_case.contents_structure import TestCaseHelp
 from shellcheck_lib.help.program_modes.test_case.render import instruction_set, render_instruction
 from shellcheck_lib.help.program_modes.test_case.render.test_case_phase import TestCasePhaseOverviewRenderer
-from shellcheck_lib.help.utils.cross_reference import CrossReferenceTextConstructor
 from shellcheck_lib.help.utils.render import SectionContentsRenderer, RenderingEnvironment
 from shellcheck_lib.util.textformat.structure import document as doc
 from shellcheck_lib.util.textformat.structure.structures import para, text
@@ -12,9 +11,7 @@ from shellcheck_lib.util.textformat.structure.structures import para, text
 
 class TestCaseHelpRendererResolver:
     def __init__(self,
-                 cross_ref_text_constructor: CrossReferenceTextConstructor,
                  contents: TestCaseHelp):
-        self.cross_ref_text_constructor = cross_ref_text_constructor
         self._contents = contents
 
     def resolve(self, request: TestCaseHelpRequest) -> SectionContentsRenderer:
@@ -30,18 +27,15 @@ class TestCaseHelpRendererResolver:
         if item is TestCaseHelpItem.INSTRUCTION:
             return render_instruction.InstructionManPageRenderer(request.data)
         if item is TestCaseHelpItem.INSTRUCTION_SEARCH:
-            return InstructionSearchRenderer(self.cross_ref_text_constructor,
-                                             request.name,
+            return InstructionSearchRenderer(request.name,
                                              request.data)
         raise ValueError('Invalid %s: %s' % (str(TestCaseHelpItem), str(item)))
 
 
 class InstructionSearchRenderer(SectionContentsRenderer):
     def __init__(self,
-                 cross_ref_text_constructor: CrossReferenceTextConstructor,
                  instruction_name: str,
                  phase_and_instruction_description_list: list):
-        self.cross_ref_text_constructor = cross_ref_text_constructor
         self.instruction_name = instruction_name
         self.phase_and_instruction_description_list = phase_and_instruction_description_list
 
