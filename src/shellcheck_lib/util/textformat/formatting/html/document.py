bc198fa4fb1d53cd533bb870fd305281baf89bdc
Emil Karlén <emil@member.fsf.org>
2016-04-05 20:11:24 +0200

    textformat/html: Rendering of document v 1.0

---
+++ src/shellcheck_lib/util/textformat/formatting/html/document.py
@@ -0,0 +1,42 @@
+from xml.etree.ElementTree import Element, SubElement, ElementTree
+
+from shellcheck_lib.util.textformat.formatting.html.section import SectionRenderer, Environment
+from shellcheck_lib.util.textformat.structure.document import SectionContents
+
+
+class DocumentSetup:
+    def __init__(self, page_title: str):
+        self.page_title = page_title
+
+
+class DocumentRenderer:
+    """
+    Renders a XHTML 1.0 document.
+    """
+
+    def __init__(self,
+                 section_renderer: SectionRenderer):
+        self.section_renderer = section_renderer
+
+    def apply(self,
+              output_file,
+              document_setup: DocumentSetup,
+              section_contents: SectionContents):
+        html = Element('html')
+        html.append(self._head_element(document_setup))
+        body = SubElement(html, 'body')
+        self.section_renderer.render_section_contents(Environment(0),
+                                                      body,
+                                                      section_contents)
+        output_file.write(DOCTYPE_XHTML1_0)
+        ElementTree(html).write(output_file, encoding="unicode")
+
+    def _head_element(self, document_setup: DocumentSetup) -> Element:
+        ret_val = Element('head')
+        title = SubElement(ret_val, 'title')
+        title.text = document_setup.page_title
+        return ret_val
+
+
+DOCTYPE_XHTML1_0 = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" ' \
+                   '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
