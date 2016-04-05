0dfc901af20e11861fa69d9fbca7833f0dc1fc9a
Emil Karlén <emil.karlen@fripost.org>
2016-04-05 21:59:58 +0200

    Help system: Refactoring: Cleanup of handle_help_request by getting rid of superfluous argument (which previous commit fixes to passed in different way)

--- src/shellcheck_lib/cli/program_modes/help/request_handling.py
+++ src/shellcheck_lib/cli/program_modes/help/request_handling.py
@@ -1,4 +1,3 @@
-from shellcheck_lib.cli.program_modes.help import arguments_for
 from shellcheck_lib.cli.program_modes.help.concepts.help_request import ConceptHelpRequest
 from shellcheck_lib.cli.program_modes.help.concepts.request_rendering import ConceptHelpRequestRendererResolver
 from shellcheck_lib.cli.program_modes.help.html_page.help_request import XHtmlHelpRequest
@@ -14,60 +13,29 @@ from shellcheck_lib.cli.program_modes.help.program_modes.test_suite.request_rend
 from shellcheck_lib.cli.program_modes.help.request_handling_.console_help import ConsoleHelpRequestHandler
 from shellcheck_lib.cli.program_modes.help.request_handling_.html_generation import HtmlGenerationRequestHandler
 from shellcheck_lib.cli.program_modes.help.request_handling_.request_handler import RequestHandler
-from shellcheck_lib.help import cross_reference_id
 from shellcheck_lib.help.contents_structure import ApplicationHelp
-from shellcheck_lib.help.utils.cross_reference import CrossReferenceTextConstructor
 from shellcheck_lib.help.utils.render import SectionContentsRenderer
 from shellcheck_lib.util.std import StdOutputFiles
-from shellcheck_lib.util.textformat.formatting.text import section, paragraph_item
-from shellcheck_lib.util.textformat.formatting.text import text
-from shellcheck_lib.util.textformat.formatting.text.lists import list_formats_with
-from shellcheck_lib.util.textformat.formatting.text.wrapper import Wrapper
-from shellcheck_lib.util.textformat.structure import core
 
 
 def handle_help_request(output: StdOutputFiles,
                         application_help: ApplicationHelp,
                         help_request: HelpRequest):
-    handler = _resolve_handler(_cross_ref_text_constructor(),
-                               application_help,
+    handler = _resolve_handler(application_help,
                                help_request)
     handler.handle(output)
 
 
-def _formatter(page_width):
-    text_formatter = text.TextFormatter(HelpCrossReferenceFormatter())
-    return section.Formatter(paragraph_item.Formatter(text_formatter,
-                                                      Wrapper(page_width=page_width),
-                                                      list_formats=list_formats_with(indent_str='  ')),
-                             section_content_indent_str='   ')
-
-
-class HelpCrossReferenceFormatter(text.CrossReferenceFormatter):
-    def __init__(self):
-        self.command_line_getter = _HelpCommandLineGetterVisitor()
-
-    def apply(self, cross_reference: core.CrossReferenceText) -> str:
-        command_line = self.command_line_getter.visit(cross_reference.target)
-        return cross_reference.title + ' (' + command_line + ')'
-
-
-def _cross_ref_text_constructor() -> CrossReferenceTextConstructor:
-    return CrossReferenceTextConstructor()
-
-
-def _resolve_handler(cross_ref_text_constructor: CrossReferenceTextConstructor,
-                     application_help: ApplicationHelp,
+def _resolve_handler(application_help: ApplicationHelp,
                      request: HelpRequest) -> RequestHandler:
     if isinstance(request, XHtmlHelpRequest):
         return HtmlGenerationRequestHandler(application_help)
     else:
-        renderer = _renderer(cross_ref_text_constructor, application_help, request)
+        renderer = _renderer(application_help, request)
         return ConsoleHelpRequestHandler(application_help, renderer)
 
 
-def _renderer(cross_ref_text_constructor: CrossReferenceTextConstructor,
-              application_help: ApplicationHelp,
+def _renderer(application_help: ApplicationHelp,
               request: HelpRequest) -> SectionContentsRenderer:
     if isinstance(request, MainProgramHelpRequest):
         resolver = MainProgramHelpRendererResolver(application_help.main_program_help)
@@ -76,19 +44,9 @@ def _renderer(cross_ref_text_constructor: CrossReferenceTextConstructor,
         resolver = ConceptHelpRequestRendererResolver(application_help.concepts_help)
         return resolver.renderer_for(request)
     if isinstance(request, TestCaseHelpRequest):
-        resolver = TestCaseHelpRendererResolver(cross_ref_text_constructor,
-                                                application_help.test_case_help)
+        resolver = TestCaseHelpRendererResolver(application_help.test_case_help)
         return resolver.resolve(request)
     if isinstance(request, TestSuiteHelpRequest):
         resolver = TestSuiteHelpRendererResolver(application_help.test_suite_help)
         return resolver.resolve(request)
     raise ValueError('Invalid %s: %s' % (str(HelpRequest), str(type(request))))
-
-
-class _HelpCommandLineGetterVisitor(cross_reference_id.CrossReferenceIdVisitor):
-    def visit_concept(self, x: cross_reference_id.ConceptCrossReferenceId):
-        return _command_line_display_for_help_arguments(arguments_for.individual_concept(x.concept_name))
-
-
-def _command_line_display_for_help_arguments(arguments: list) -> str:
-    return '>' + arguments_for.HELP + ' ' + ' '.join(arguments)
