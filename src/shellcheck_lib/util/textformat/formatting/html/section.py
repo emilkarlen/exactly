618dbeec93de015b9441f8bd97811e63bf040aa8
Emil Karlén <emil.karlen@fripost.org>
2016-04-05 19:08:15 +0200

    textformat/html: Rendering of section/section content: v 1.0

--- src/shellcheck_lib/util/textformat/formatting/html/section.py
+++ src/shellcheck_lib/util/textformat/formatting/html/section.py
@@ -1,7 +1,9 @@
-from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, SubElement
 
 from shellcheck_lib.util.textformat.formatting.html import text
 from shellcheck_lib.util.textformat.formatting.html.paragraph_item.interfaces import ParagraphItemRenderer
+from shellcheck_lib.util.textformat.formatting.html.text import Position
+from shellcheck_lib.util.textformat.structure import core
 from shellcheck_lib.util.textformat.structure.document import SectionContents, Section
 
 
@@ -10,11 +12,43 @@ class Environment:
         self.section_level = section_level
 
 
+def increased_level(environment: Environment) -> Environment:
+    return Environment(1 + environment.section_level)
+
+
+class SectionHeaderRenderer:
+    def apply(self,
+              environment: Environment,
+              parent: Element,
+              header_text: core.Text) -> Element:
+        raise NotImplementedError()
+
+
+class HnSectionHeaderRenderer(SectionHeaderRenderer):
+    def __init__(self,
+                 text_renderer: text.TextRenderer):
+        self.text_renderer = text_renderer
+
+    def apply(self,
+              environment: Environment,
+              parent: Element,
+              header_text: core.Text) -> Element:
+        ret_val = SubElement(parent, self._resolve_header_element_tag(environment))
+        self.text_renderer.apply(ret_val, ret_val, Position.INSIDE, header_text)
+        return ret_val
+
+    @staticmethod
+    def _resolve_header_element_tag(environment: Environment) -> str:
+        if environment.section_level <= 5:
+            return 'h' + str(1 + environment.section_level)
+        return 'h6'
+
+
 class SectionRenderer:
     def __init__(self,
-                 text_renderer: text.TextRenderer,
+                 section_header_renderer: SectionHeaderRenderer,
                  paragraph_item_renderer: ParagraphItemRenderer):
-        self.text_renderer = text_renderer
+        self.section_header_renderer = section_header_renderer
         self.paragraph_item_renderer = paragraph_item_renderer
 
     def render_section_contents(self,
@@ -27,7 +61,17 @@ class SectionRenderer:
         ret_val = parent
         for paragraph_item in section_contents.initial_paragraphs:
             ret_val = self.paragraph_item_renderer.apply(parent, paragraph_item)
+        for section in section_contents.sections:
+            ret_val = self.render_section(environment, parent, section)
         return ret_val
 
-    def render_section(self, environment: Environment, parent: Element, section: Section):
-        raise NotImplementedError()
+    def render_section(self,
+                       environment: Environment,
+                       parent: Element,
+                       section: Section) -> Element:
+        """
+        :return: The last rendered element, or parent, if no element was rendered.
+        """
+        last_header_element = self.section_header_renderer.apply(environment, parent, section.header)
+        last_sc_elem = self.render_section_contents(increased_level(environment), parent, section.contents)
+        return last_header_element if last_sc_elem is parent else last_sc_elem
--- test/shellcheck_lib_test/util/textformat/formatting/html/section.py
+++ test/shellcheck_lib_test/util/textformat/formatting/html/section.py
@@ -2,22 +2,74 @@ import unittest
 from xml.etree.ElementTree import Element
 
 from shellcheck_lib.util.textformat.formatting.html import section as sut
+from shellcheck_lib.util.textformat.formatting.html.section import HnSectionHeaderRenderer
 from shellcheck_lib.util.textformat.formatting.html.text import TextRenderer
-from shellcheck_lib.util.textformat.structure.document import SectionContents
+from shellcheck_lib.util.textformat.structure.core import StringText
+from shellcheck_lib.util.textformat.structure.document import SectionContents, Section
 from shellcheck_lib.util.textformat.structure.structures import para
 from shellcheck_lib_test.util.textformat.formatting.html.paragraph_item.test_resources import as_unicode_str, \
-    TargetRendererTestImpl, ParaWithSingleStrTextRenderer
+    TargetRendererTestImpl, ParaWithSingleStrTextRenderer, assert_contents_and_that_last_child_is_returned
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
+        unittest.makeSuite(TestHnSectionHeaderRenderer),
         unittest.makeSuite(TestSectionContentsEmpty),
         unittest.makeSuite(TestSectionContentsWithoutSections),
+        unittest.makeSuite(TestSectionContentsWithSingleSection),
+        unittest.makeSuite(TestSectionContentsWithMultipleSections),
+        unittest.makeSuite(TestSection),
     ])
 
 
 if __name__ == '__main__':
     unittest.TextTestRunner().run(suite())
+
+
+class TestHnSectionHeaderRenderer(unittest.TestCase):
+    HN_SECTION_HEADER_RENDERER = HnSectionHeaderRenderer(TextRenderer(TargetRendererTestImpl()))
+
+    def test_level_0(self):
+        # ARRANGE #
+        root = Element('root')
+        # ACT #
+        ret_val = self.HN_SECTION_HEADER_RENDERER.apply(sut.Environment(0),
+                                                        root,
+                                                        StringText('text'))
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h1>text</h1>'
+            '</root>',
+            root, ret_val, self)
+
+    def test_highest_h_level(self):
+        # ARRANGE #
+        root = Element('root')
+        # ACT #
+        ret_val = self.HN_SECTION_HEADER_RENDERER.apply(sut.Environment(5),
+                                                        root,
+                                                        StringText('text'))
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h6>text</h6>'
+            '</root>',
+            root, ret_val, self)
+
+    def test_level_greater_than_highest_h_level(self):
+        # ARRANGE #
+        root = Element('root')
+        # ACT #
+        ret_val = self.HN_SECTION_HEADER_RENDERER.apply(sut.Environment(6),
+                                                        root,
+                                                        StringText('text'))
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h6>text</h6>'
+            '</root>',
+            root, ret_val, self)
 
 
 class TestSectionContentsEmpty(unittest.TestCase):
@@ -47,13 +99,11 @@ class TestSectionContentsWithoutSections(unittest.TestCase):
                                                         root,
                                                         sc)
         # ASSERT #
-        xml_string = as_unicode_str(root)
-        self.assertEqual('<root>'
-                         '<p>the only para</p>'
-                         '</root>',
-                         xml_string)
-        self.assertIs(list(root)[-1],
-                      ret_val)
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<p>the only para</p>'
+            '</root>',
+            root, ret_val, self)
 
     def test_multiple_paragraph_items(self):
         # ARRANGE #
@@ -66,15 +116,155 @@ class TestSectionContentsWithoutSections(unittest.TestCase):
                                                         root,
                                                         sc)
         # ASSERT #
-        xml_string = as_unicode_str(root)
-        self.assertEqual('<root>'
-                         '<p>para 1</p>'
-                         '<p>para 2</p>'
-                         '</root>',
-                         xml_string)
-        self.assertIs(list(root)[-1],
-                      ret_val)
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<p>para 1</p>'
+            '<p>para 2</p>'
+            '</root>'
+            ,
+            root, ret_val, self)
 
 
-TEST_RENDERER = sut.SectionRenderer(TextRenderer(TargetRendererTestImpl()),
+class TestSectionContentsWithSingleSection(unittest.TestCase):
+    def test_section_without_contents(self):
+        # ARRANGE #
+        root = Element('root')
+        sc = SectionContents([],
+                             [Section(StringText('section header'),
+                                      SectionContents([],
+                                                      []))])
+        # ACT #
+        ret_val = TEST_RENDERER.render_section_contents(sut.Environment(0),
+                                                        root,
+                                                        sc)
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h1>section header</h1>'
+            '</root>'
+            ,
+            root, ret_val, self)
+
+    def test_section_with_single_initial_para(self):
+        # ARRANGE #
+        root = Element('root')
+        sc = SectionContents([],
+                             [Section(StringText('section header'),
+                                      SectionContents([para('initial para')],
+                                                      []))])
+        # ACT #
+        ret_val = TEST_RENDERER.render_section_contents(sut.Environment(0),
+                                                        root,
+                                                        sc)
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h1>section header</h1>'
+            '<p>initial para</p>'
+            '</root>'
+            ,
+            root, ret_val, self)
+
+    def test_section_with_single_sub_section(self):
+        # ARRANGE #
+        root = Element('root')
+        sc = SectionContents(
+            [],
+            [Section(StringText('header 1'),
+                     SectionContents([],
+                                     [Section(StringText('header 1/1'),
+                                              SectionContents([], []))]))])
+        # ACT #
+        ret_val = TEST_RENDERER.render_section_contents(sut.Environment(0),
+                                                        root,
+                                                        sc)
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h1>header 1</h1>'
+            '<h2>header 1/1</h2>'
+            '</root>'
+            ,
+            root, ret_val, self)
+
+    def test_single_sub_section_with_initial_paras_everywhere(self):
+        # ARRANGE #
+        root = Element('root')
+        sc = SectionContents(
+            [para('para 0')],
+            [Section(StringText('header 1'),
+                     SectionContents([para('para 1')],
+                                     [Section(StringText('header 1/1'),
+                                              SectionContents([para('para 2')], []))]))])
+        # ACT #
+        ret_val = TEST_RENDERER.render_section_contents(sut.Environment(0),
+                                                        root,
+                                                        sc)
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<p>para 0</p>'
+            '<h1>header 1</h1>'
+            '<p>para 1</p>'
+            '<h2>header 1/1</h2>'
+            '<p>para 2</p>'
+            '</root>'
+            ,
+            root, ret_val, self)
+
+
+class TestSectionContentsWithMultipleSections(unittest.TestCase):
+    def test(self):
+        # ARRANGE #
+        root = Element('root')
+        sc = SectionContents([],
+                             [Section(StringText('header 1'),
+                                      SectionContents([], [])),
+                              Section(StringText('header 2'),
+                                      SectionContents([], [])),
+                              ])
+        # ACT #
+        ret_val = TEST_RENDERER.render_section_contents(sut.Environment(0),
+                                                        root,
+                                                        sc)
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h1>header 1</h1>'
+            '<h1>header 2</h1>'
+            '</root>'
+            ,
+            root, ret_val, self)
+
+
+class TestSection(unittest.TestCase):
+    def test(self):
+        # ARRANGE #
+        root = Element('root')
+        s = Section(
+            StringText('header 1'),
+            SectionContents(
+                [para('para 1')],
+                [Section(
+                    StringText('header 1/1'),
+                    SectionContents(
+                        [para('para 1/1')], []))])
+        )
+        # ACT #
+        ret_val = TEST_RENDERER.render_section(sut.Environment(0),
+                                               root,
+                                               s)
+        # ASSERT #
+        assert_contents_and_that_last_child_is_returned(
+            '<root>'
+            '<h1>header 1</h1>'
+            '<p>para 1</p>'
+            '<h2>header 1/1</h2>'
+            '<p>para 1/1</p>'
+            '</root>'
+            ,
+            root, ret_val, self)
+
+
+TEST_RENDERER = sut.SectionRenderer(HnSectionHeaderRenderer(TextRenderer(TargetRendererTestImpl())),
                                     ParaWithSingleStrTextRenderer())
