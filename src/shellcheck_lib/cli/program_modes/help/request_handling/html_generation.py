9c00f02d18d57bed21a0cd7f6b72a318635bfbe7
Emil Karlén <emil@member.fsf.org>
2016-04-06 04:43:05 +0200

    html-generation: Adds TOC header

--- src/shellcheck_lib/cli/program_modes/help/request_handling/html_generation.py
+++ src/shellcheck_lib/cli/program_modes/help/request_handling/html_generation.py
@@ -14,15 +14,27 @@ from shellcheck_lib.util.textformat.formatting.html import document as doc_rende
 from shellcheck_lib.util.textformat.formatting.html import text
 from shellcheck_lib.util.textformat.formatting.html.paragraph_item.full_paragraph_item import FullParagraphItemRenderer
 from shellcheck_lib.util.textformat.formatting.html.section import HnSectionHeaderRenderer
-from shellcheck_lib.util.textformat.formatting.html.utils import ElementPopulator
+from shellcheck_lib.util.textformat.formatting.html.utils import ElementPopulator, ComplexElementPopulator
 from shellcheck_lib.util.textformat.structure import core as doc
 from shellcheck_lib.util.textformat.structure.document import SectionContents
 from shellcheck_lib.util.textformat.structure.lists import ListType
 
+PAGE_TITLE = '%s Reference Manual' % program_info.PROGRAM_NAME
+TOC_TITLE = 'Table of Contents'
+
 ELEMENT_STYLES = """\
 pre {
-background: lightgray;
+background-color : #EEE8AA;
+padding: 7px;
+border-radius     : 2px;
 }
+
+h1 {
+background-color: #E6E6FA;
+border-radius     : 4px;
+padding: 5px;
+}
+
 """
 
 TITLE_STYLE = """\
@@ -30,7 +42,10 @@ font-size: 250%;
 font-weight: bold;
 """
 
-PAGE_TITLE = '%s Reference Manual' % program_info.PROGRAM_NAME
+TOC_TITLE_STYLE = """\
+font-size: 200%;
+font-weight: bold;
+"""
 
 
 class HtmlGenerationRequestHandler(RequestHandler):
@@ -48,10 +63,9 @@ class HtmlGenerationRequestHandler(RequestHandler):
 
     def _setup(self) -> doc_rendering.DocumentSetup:
         head_populator = StylePopulator(ELEMENT_STYLES)
-        header_populator = TitleAndVersionPopulator()
         setup = doc_rendering.DocumentSetup(PAGE_TITLE,
                                             head_populator=head_populator,
-                                            header_populator=header_populator)
+                                            header_populator=HEADER_POPULATOR)
         return setup
 
     def _contents(self) -> SectionContents:
@@ -99,11 +113,28 @@ class StylePopulator(ElementPopulator):
         SubElement(parent, 'style').text = self.style
 
 
-class TitleAndVersionPopulator(ElementPopulator):
+class DivWithTextAndStylePopulator(ElementPopulator):
+    def __init__(self,
+                 contents: str,
+                 style: str):
+        self.contents = contents
+        self.style = style
+
     def apply(self, parent: Element):
         div = SubElement(parent, 'div')
-        div.text = PAGE_TITLE
-        div.set('style', TITLE_STYLE)
+        div.text = self.contents
+        div.set('style', self.style)
+
+
+class VersionPopulator(ElementPopulator):
+    def apply(self, parent: Element):
         version = SubElement(parent, 'p')
         version.text = program_info.PROGRAM_NAME + ' version ' + program_info.VERSION
         SubElement(parent, 'hr')
+
+
+HEADER_POPULATOR = ComplexElementPopulator([
+    DivWithTextAndStylePopulator(PAGE_TITLE, TITLE_STYLE),
+    VersionPopulator(),
+    DivWithTextAndStylePopulator(TOC_TITLE, TOC_TITLE_STYLE),
+])
