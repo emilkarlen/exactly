224154e879a0a407d8577fee2ef340a6f3dea14f
emil <emilkarlen@aim.com>
2016-04-10 22:34:30 +0200

    windows port: Using Pythons universion-newline functionality to split actual output into lines

--- test/shellcheck_lib_test/test_suite/__init__.py
+++ test/shellcheck_lib_test/test_suite/__init__.py
@@ -1,13 +1,15 @@
 import unittest
 
-from . import enumeration
-from . import suite_hierarchy_reading
-from . import execution
+from shellcheck_lib_test.test_suite import enumeration, suite_hierarchy_reading, execution
 
 
-def suite():
+def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(enumeration.suite())
     ret_val.addTest(suite_hierarchy_reading.suite())
     ret_val.addTest(execution.suite())
     return ret_val
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
