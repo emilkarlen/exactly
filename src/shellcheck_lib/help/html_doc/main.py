2d614b30d80276eb96dcd7b3994a5fc6aade4c4c
emil <emilkarlen@aim.com>
2016-04-11 17:36:13 +0200

    html-doc: Adds "test phases" to help (but invalid cross-ref targets)

--- src/shellcheck_lib/help/html_doc/main.py
+++ src/shellcheck_lib/help/html_doc/main.py
@@ -1,5 +1,4 @@
 from shellcheck_lib.help import cross_reference_id as cross_ref
-from shellcheck_lib.help.concepts.all_concepts import all_concepts
 from shellcheck_lib.help.concepts.concept_structure import ConceptDocumentation
 from shellcheck_lib.help.concepts.render import IndividualConceptRenderer
 from shellcheck_lib.help.contents_structure import ApplicationHelp
@@ -7,6 +6,7 @@ from shellcheck_lib.help.cross_reference_id import CustomTargetInfoFactory
 from shellcheck_lib.help.html_doc import page_setup
 from shellcheck_lib.help.html_doc.cross_ref_target_renderer import HtmlTargetRenderer
 from shellcheck_lib.help.program_modes.test_case.contents.main import overview as test_case_overview_rendering
+from shellcheck_lib.help.program_modes.test_case.contents_structure import TestCasePhaseDocumentation
 from shellcheck_lib.help.program_modes.test_suite import render as test_suite_rendering
 from shellcheck_lib.help.utils.cross_reference import CrossReferenceTextConstructor
 from shellcheck_lib.help.utils.render import RenderingEnvironment
@@ -53,7 +53,7 @@ class HtmlDocGenerator:
     def _main_contents(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
         test_case_targets_factory = cross_ref.sub_component_factory('test-case',
                                                                     targets_factory)
-        test_case_target = test_case_targets_factory.root('Test Case')
+        test_case_target = test_case_targets_factory.root('Test Cases')
         test_case_sub_targets, test_case_contents = self._test_case_contents(test_case_targets_factory)
 
         test_suite_targets_factory = cross_ref.sub_component_factory('test-suite',
@@ -88,10 +88,57 @@ class HtmlDocGenerator:
         return ret_val_targets, ret_val_contents
 
     def _test_case_contents(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
+        overview_targets_factory = cross_ref.sub_component_factory('overview',
+                                                                   targets_factory)
+        overview_target = overview_targets_factory.root('Test Case Overview')
+        overview_sub_targets, overview_contents = self._test_case_overview_contents(overview_targets_factory)
+
+        phases_targets_factory = cross_ref.sub_component_factory('phases',
+                                                                 targets_factory)
+        phases_target = phases_targets_factory.root('Phases')
+        phases_sub_targets, phases_contents = self._test_case_phases_contents(phases_targets_factory)
+
+        ret_val_contents = doc.SectionContents(
+            [],
+            [
+                doc.Section(overview_target.anchor_text(),
+                            overview_contents),
+                doc.Section(phases_target.anchor_text(),
+                            phases_contents)
+            ]
+        )
+        ret_val_targets = [
+            cross_ref.TargetInfoNode(overview_target,
+                                     overview_sub_targets),
+            cross_ref.TargetInfoNode(phases_target,
+                                     phases_sub_targets),
+        ]
+        return ret_val_targets, ret_val_contents
+
+    def _test_case_overview_contents(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
         generator = test_case_overview_rendering.OverviewRenderer(self.application_help.test_case_help,
                                                                   targets_factory)
         section_contents = generator.apply(self.rendering_environment)
         return generator.target_info_hierarchy(), section_contents
+
+    def _test_case_phases_contents(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
+        ret_val_sections = []
+        ret_val_targets = []
+        for phase in self.application_help.test_case_help.phase_helps_in_order_of_execution:
+            assert isinstance(phase, TestCasePhaseDocumentation)
+            phase_presentation_str = phase.name.syntax
+            # cross_reference_target = cross_ref.TestCasePhaseCrossReference(phase.name.plain)
+            cross_reference_target = targets_factory.sub(phase.name.plain, phase.name.plain).target
+            header = docs.anchor_text(docs.text(phase_presentation_str),
+                                      cross_reference_target)
+            section = doc.Section(header,
+                                  phase.render())
+            target_info_node = cross_ref.TargetInfoNode(cross_ref.TargetInfo(phase_presentation_str,
+                                                                             cross_reference_target),
+                                                        [])
+            ret_val_sections.append(section)
+            ret_val_targets.append(target_info_node)
+        return ret_val_targets, doc.SectionContents([], ret_val_sections)
 
     def _test_suite_contents(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
         generator = test_suite_rendering.OverviewRenderer(self.application_help.test_suite_help)
@@ -101,7 +148,8 @@ class HtmlDocGenerator:
     def _concepts_contents(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
         ret_val_sections = []
         ret_val_targets = []
-        for concept in sorted(all_concepts(), key=lambda cn: cn.name().singular):
+        for concept in sorted(self.application_help.concepts_help.all_concepts,
+                              key=lambda cn: cn.name().singular):
             assert isinstance(concept, ConceptDocumentation)
             concept_presentation_str = concept.name().singular
             header = docs.anchor_text(docs.text(concept_presentation_str),
