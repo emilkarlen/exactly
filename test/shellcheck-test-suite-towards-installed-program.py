acd070a2621e9cf81cd1c0f5898dc698107b0b43
Emil Karlén <emil.karlen@fripost.org>
2016-04-15 01:32:42 +0200

    simple installation: Adds test-program to run tests against installed program

---
+++ test/shellcheck-test-suite-towards-installed-program.py
@@ -0,0 +1,21 @@
+import unittest
+
+import shellcheck_lib_test
+from shellcheck_lib_test.test_resources.main_program.main_program_runner import RunInstalledProgramViaOsInSubProcess
+
+import test_cli_main_program__test_case
+import test_cli_main_program__invalid_invokation_dynamic
+import test_cli_main_program__test_suite
+
+# This value is from setup.py
+EXECUTABLE_NAME = 'shellcheck'
+
+main_program_runner = RunInstalledProgramViaOsInSubProcess(EXECUTABLE_NAME)
+suite = unittest.TestSuite()
+suite.addTest(shellcheck_lib_test.suite())
+# suite.addTest(test_cli_main_program__test_case.suite())
+suite.addTest(test_cli_main_program__invalid_invokation_dynamic.suite_for(main_program_runner))
+# suite.addTest(test_cli_main_program__test_suite.suite())
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
