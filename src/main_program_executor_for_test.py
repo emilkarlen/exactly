2f6b52f47828db0a75096c807fd1df361761b7af
Emil Karlén <emil@member.fsf.org>
2016-04-14 20:42:19 +0200

    simple installation: Refactoring: Rename of file

---
+++ src/main_program_executor_for_test.py
@@ -0,0 +1,13 @@
+"""
+Executes the (default) main program.
+
+Tests use this file to execute the main program, which uses the fact that this file
+is located in the root directory of the source - making it possible to import modules
+from the library without having to set the sys.path.
+"""
+import sys
+
+from shellcheck_lib.default.default_main_program_setup import main
+
+exit_status = main()
+sys.exit(exit_status)
