bbdf82a3c1e4b326e5696c18de63d0f05624c8d6
Emil Karlén <emil@member.fsf.org>
2016-04-16 19:19:33 +0200

    tests-toward-installed-program: Reorganization of tests in "default" package: help, html_doc

--- test/shellcheck_lib_test/default/__init__.py
+++ test/shellcheck_lib_test/default/__init__.py
@@ -1,13 +1,14 @@
 import unittest
 
-from shellcheck_lib_test.default import default_main_program, html_doc
+from shellcheck_lib_test.default import default_main_program
 from shellcheck_lib_test.default import instruction_name_and_argument_splitter as splitter
+from shellcheck_lib_test.default import program_modes
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(splitter.suite())
-    ret_val.addTest(html_doc.suite())
+    ret_val.addTest(program_modes.suite())
     ret_val.addTest(default_main_program.suite())
     return ret_val
 
