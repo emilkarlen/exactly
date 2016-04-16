f78c30b688d6135eadb70c036f07baa26b55220c
Emil Karlén <emil.karlen@fripost.org>
2016-04-16 22:59:29 +0200

    tests-toward-installed-program: Refactors tests of suite run via main program so that they can be run towards installed program

--- test/shellcheck-test-suite-towards-installed-program.py
+++ test/shellcheck-test-suite-towards-installed-program.py
@@ -1,12 +1,10 @@
 import unittest
 
 import shellcheck_lib_test
+import test_cli_main_program__invalid_invokation_dynamic
+import test_cli_main_program__test_case
 from shellcheck_lib_test.test_resources.cli_main_program_via_shell_utils.main_program_runner import \
     RunInstalledProgramViaOsInSubProcess
-
-import test_cli_main_program__test_case
-import test_cli_main_program__invalid_invokation_dynamic
-import test_cli_main_program__test_suite
 
 # This value is from setup.py
 EXECUTABLE_NAME = 'shellcheck'
@@ -16,7 +14,7 @@ suite = unittest.TestSuite()
 suite.addTest(shellcheck_lib_test.suite())
 suite.addTest(test_cli_main_program__test_case.suite_for(main_program_runner))
 suite.addTest(test_cli_main_program__invalid_invokation_dynamic.suite_for(main_program_runner))
-suite.addTest(test_cli_main_program__test_suite.suite())
+suite.addTest(shellcheck_lib_test.default.program_modes.test_suite.suite_for(main_program_runner))
 
 runner = unittest.TextTestRunner()
 runner.run(suite)
--- test/shellcheck-test-suite.py
+++ test/shellcheck-test-suite.py
@@ -13,7 +13,6 @@ os.chdir(this_dir)
 
 import test_cli_main_program__test_case
 import test_cli_main_program__invalid_invokation_dynamic
-import test_cli_main_program__test_suite
 import shellcheck_lib_test
 from shellcheck_lib_test.test_resources.main_program.main_program_runner import RunViaOsInSubProcess
 
@@ -22,7 +21,7 @@ suite = unittest.TestSuite()
 suite.addTest(shellcheck_lib_test.suite())
 suite.addTest(test_cli_main_program__test_case.suite_for(main_program_runner))
 suite.addTest(test_cli_main_program__invalid_invokation_dynamic.suite_for(main_program_runner))
-suite.addTest(test_cli_main_program__test_suite.suite())
+suite.addTest(shellcheck_lib_test.default.program_modes.test_suite.suite_for(main_program_runner))
 
 runner = unittest.TextTestRunner()
 runner.run(suite)
