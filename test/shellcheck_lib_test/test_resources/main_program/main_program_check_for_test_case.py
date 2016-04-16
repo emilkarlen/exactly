bf6f36ec60a351bb2a4d3d792ce9c9d37a689912
Emil Karlén <emil.karlen@fripost.org>
2016-04-16 19:02:38 +0200

    tests-toward-installed-program: Making test-case tests applicable towards installed program: suite_for_test_case_preprocessing

--- test/shellcheck_lib_test/test_resources/main_program/main_program_check_for_test_case.py
+++ test/shellcheck_lib_test/test_resources/main_program/main_program_check_for_test_case.py
@@ -5,6 +5,8 @@ from shellcheck_lib.cli.cli_environment.command_line_options import OPTION_FOR_P
 from shellcheck_lib_test.test_resources import quoting
 from shellcheck_lib_test.test_resources.file_structure import File, DirContents
 from shellcheck_lib_test.test_resources.main_program import main_program_check_base
+from shellcheck_lib_test.test_resources.main_program.main_program_check_base import check_with_pre_proc
+from shellcheck_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 from shellcheck_lib_test.test_resources.process import SubProcessResult, ExpectedSubProcessResult
 
 
@@ -45,6 +47,59 @@ class SetupWithPreprocessor(main_program_check_base.SetupWithPreprocessor):
               root_path: pathlib.Path,
               actual_result: SubProcessResult):
         self.expected_result().assert_matches(put, actual_result)
+
+
+class TestsForSetupWithPreprocessorBase(unittest.TestCase):
+    def __init__(self, main_program_runner: MainProgramRunner):
+        super().__init__()
+        self.main_program_runner = main_program_runner
+
+    def setup(self) -> SetupWithPreprocessor:
+        raise NotImplementedError()
+
+    def runTest(self):
+        setup = self.setup()
+        check_with_pre_proc([],
+                            setup,
+                            self,
+                            self.main_program_runner)
+
+
+class TestForSetupWithPreprocessor(unittest.TestCase):
+    def __init__(self,
+                 setup: SetupWithPreprocessor,
+                 main_program_runner: MainProgramRunner):
+        super().__init__()
+        self.setup = setup
+        self.main_program_runner = main_program_runner
+
+    def runTest(self):
+        check_with_pre_proc([],
+                            self.setup,
+                            self,
+                            self.main_program_runner)
+        # with tempfile.TemporaryDirectory(prefix='shellcheck-suite-test-preprocessor-') as pre_proc_dir:
+        #     preprocessor_file_path = resolved_path(pre_proc_dir) / 'preprocessor.py'
+        #     with preprocessor_file_path.open('w') as f:
+        #         f.write(setup.preprocessor_source())
+        #     with tempfile.TemporaryDirectory(prefix='shellcheck-suite-test-dir-contents-') as tmp_dir:
+        #         tmp_dir_path = resolved_path(tmp_dir)
+        #         file_structure = setup.file_structure(tmp_dir_path,
+        #                                               sys.executable,
+        #                                               str(preprocessor_file_path))
+        #         file_structure.write_to(tmp_dir_path)
+        #         file_argument = str(setup.file_argument_based_at(tmp_dir_path))
+        #         first_arguments = setup.first_arguments(tmp_dir_path,
+        #                                                 sys.executable,
+        #                                                 str(preprocessor_file_path))
+        #         arguments = first_arguments + ARGUMENTS_FOR_TEST_INTERPRETER + [file_argument]
+        #         sub_process_result = self.main_program_runner.run(self, arguments)
+        #         setup.check(self,
+        #                     tmp_dir_path,
+        #                     sub_process_result)
+
+    def shortDescription(self):
+        return str(type(self.setup)) + '/' + self.main_program_runner.description_for_test_name()
 
 
 class TestsForSetupWithPreprocessorInternally(unittest.TestCase):
