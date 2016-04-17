e657804ecdd231c8e84dff137b7c3d3637d6e99a
Emil Karlén <emil@member.fsf.org>
2016-04-17 20:06:22 +0200

    tests-toward-installed-program: Refactoring of main-program tests (mode "test case"): now uses ValueAssertion

--- test/shellcheck_lib_test/default/program_modes/test_case.py
+++ test/shellcheck_lib_test/default/program_modes/test_case.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from shellcheck_lib.cli import main_program
@@ -7,54 +8,85 @@ from shellcheck_lib.execution import phases
 from shellcheck_lib.util.string import lines_content
 from shellcheck_lib_test.default.test_resources import default_main_program_case_preprocessing
 from shellcheck_lib_test.default.test_resources.test_case_file_elements import phase_header_line
+from shellcheck_lib_test.test_resources import value_assertion as va
 from shellcheck_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_with_preprocessor, \
     tests_for_setup_without_preprocessor
 from shellcheck_lib_test.test_resources.main_program.main_program_check_for_test_case import SetupWithoutPreprocessor
 from shellcheck_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 from shellcheck_lib_test.test_resources.main_program.main_program_runners import RunViaMainProgramInternally
-from shellcheck_lib_test.test_resources.main_program.main_program_test_utils import process_result_for
-from shellcheck_lib_test.test_resources.process import ExpectedSubProcessResult
+from shellcheck_lib_test.test_resources.process import SubProcessResultInfo, SubProcessResult
 
 
 class InvalidCommandLineOptionShouldExitWithInvalidUsageStatus(SetupWithoutPreprocessor):
     def additional_arguments(self) -> list:
         return ['--invalid-option-that-should-cause-failure']
 
-    def expected_result(self) -> ExpectedSubProcessResult:
-        return ExpectedSubProcessResult(exitcode=main_program.EXIT_INVALID_USAGE,
-                                        stdout='')
+    def expected_result(self) -> va.ValueAssertion:
+        return process_result_for_exit_code(main_program.EXIT_INVALID_USAGE)
 
     def test_case(self) -> str:
         return ''
 
 
 class EmptyTestCaseShouldPass(SetupWithoutPreprocessor):
-    def expected_result(self) -> ExpectedSubProcessResult:
-        return process_result_for(exit_values.EXECUTION__PASS)
+    def expected_result(self) -> va.ValueAssertion:
+        return process_result_for_exit_value(exit_values.EXECUTION__PASS)
 
     def test_case(self) -> str:
         return ''
 
 
-class AllPhasesEmptyShouldPass(SetupWithoutPreprocessor):
-    def expected_result(self) -> ExpectedSubProcessResult:
-        return process_result_for(exit_values.EXECUTION__PASS)
+def process_result_for_exit_value(exit_value: exit_values.ExitValue) -> va.ValueAssertion:
+    return assertion_on_process_result(
+        va.And([
+            va.sub_component('exit code',
+                             SubProcessResult.exitcode.fget,
+                             va.Equals(exit_value.exit_code)),
+            va.sub_component('stdout',
+                             SubProcessResult.stdout.fget,
+                             va.Equals(exit_value.exit_identifier + os.linesep)),
+        ])
+    )
 
+
+def process_result_for_exit_code(exit_code: int) -> va.ValueAssertion:
+    return assertion_on_process_result(
+        va.sub_component('exit code',
+                         SubProcessResult.exitcode.fget,
+                         va.Equals(exit_code)),
+    )
+
+
+def assertion_on_process_result(assertion: va.ValueAssertion) -> va.ValueAssertion:
+    """
+    :type assertion: A ValueAssertion on a SubProcessResult.
+    """
+    return va.sub_component(
+        'sub process result',
+        SubProcessResultInfo.sub_process_result.fget,
+        assertion,
+    )
+
+
+class AllPhasesEmptyShouldPass(SetupWithoutPreprocessor):
     def test_case(self) -> str:
         test_case_lines = [phase_header_line(phase)
                            for phase in phases.ALL]
         return lines_content(test_case_lines)
 
+    def expected_result(self) -> va.ValueAssertion:
+        return process_result_for_exit_value(exit_values.EXECUTION__PASS)
+
 
 class WhenAPhaseHasInvalidPhaseNameThenExitStatusShouldIndicateThis(SetupWithoutPreprocessor):
-    def expected_result(self) -> ExpectedSubProcessResult:
-        return process_result_for(exit_values.NO_EXECUTION__PARSE_ERROR)
-
     def test_case(self) -> str:
         test_case_lines = [
             section_header('invalid phase name'),
         ]
         return lines_content(test_case_lines)
+
+    def expected_result(self) -> va.ValueAssertion:
+        return process_result_for_exit_value(exit_values.NO_EXECUTION__PARSE_ERROR)
 
 
 MISC = [
