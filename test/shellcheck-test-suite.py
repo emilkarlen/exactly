a9f1ab81def3cdfed3a84b8743cdb8c0a3de3334
Emil Karlén <emil@member.fsf.org>
2016-04-17 22:43:54 +0200

    tests-toward-installed-program: Removes unused code

--- test/shellcheck-test-suite.py
+++ test/shellcheck-test-suite.py
@@ -11,14 +11,12 @@ sys.path.insert(0, src_dir)
 
 os.chdir(this_dir)
 
-import test_cli_main_program__invalid_invokation_dynamic
 import shellcheck_lib_test
 from shellcheck_lib_test.test_resources.main_program.main_program_runners import RunViaOsInSubProcess
 
 main_program_runner = RunViaOsInSubProcess()
 suite = unittest.TestSuite()
 suite.addTest(shellcheck_lib_test.suite())
-suite.addTest(test_cli_main_program__invalid_invokation_dynamic.suite_for(main_program_runner))
 suite.addTest(shellcheck_lib_test.default.program_modes.test_case.suite_for(main_program_runner))
 suite.addTest(shellcheck_lib_test.default.program_modes.test_suite.suite_for(main_program_runner))
 
