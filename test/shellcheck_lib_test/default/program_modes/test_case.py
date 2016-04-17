afddbef05400477974ea20a54c5254b58c5f61d0
Emil Karlén <emil.karlen@fripost.org>
2016-04-17 20:06:31 +0200

    tests-toward-installed-program: Refactoring of main-program tests (mode "test case"): now uses ValueAssertion

--- test/shellcheck_lib_test/default/program_modes/test_case.py
+++ test/shellcheck_lib_test/default/program_modes/test_case.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 
 from shellcheck_lib.cli import main_program
@@ -7,6 +6,8 @@ from shellcheck_lib.document.syntax import section_header
 from shellcheck_lib.execution import phases
 from shellcheck_lib.util.string import lines_content
 from shellcheck_lib_test.default.test_resources import default_main_program_case_preprocessing
+from shellcheck_lib_test.default.test_resources.assertions import process_result_for_exit_value, \
+    process_result_for_exit_code
 from shellcheck_lib_test.default.test_resources.test_case_file_elements import phase_header_line
 from shellcheck_lib_test.test_resources import value_assertion as va
 from shellcheck_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_with_preprocessor, \
@@ -14,7 +15,6 @@ from shellcheck_lib_test.test_resources.main_program.main_program_check_base imp
 from shellcheck_lib_test.test_resources.main_program.main_program_check_for_test_case import SetupWithoutPreprocessor
 from shellcheck_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 from shellcheck_lib_test.test_resources.main_program.main_program_runners import RunViaMainProgramInternally
-from shellcheck_lib_test.test_resources.process import SubProcessResultInfo, SubProcessResult
 
 
 class InvalidCommandLineOptionShouldExitWithInvalidUsageStatus(SetupWithoutPreprocessor):
@@ -34,38 +34,6 @@ class EmptyTestCaseShouldPass(SetupWithoutPreprocessor):
 
     def test_case(self) -> str:
         return ''
-
-
-def process_result_for_exit_value(exit_value: exit_values.ExitValue) -> va.ValueAssertion:
-    return assertion_on_process_result(
-        va.And([
-            va.sub_component('exit code',
-                             SubProcessResult.exitcode.fget,
-                             va.Equals(exit_value.exit_code)),
-            va.sub_component('stdout',
-                             SubProcessResult.stdout.fget,
-                             va.Equals(exit_value.exit_identifier + os.linesep)),
-        ])
-    )
-
-
-def process_result_for_exit_code(exit_code: int) -> va.ValueAssertion:
-    return assertion_on_process_result(
-        va.sub_component('exit code',
-                         SubProcessResult.exitcode.fget,
-                         va.Equals(exit_code)),
-    )
-
-
-def assertion_on_process_result(assertion: va.ValueAssertion) -> va.ValueAssertion:
-    """
-    :type assertion: A ValueAssertion on a SubProcessResult.
-    """
-    return va.sub_component(
-        'sub process result',
-        SubProcessResultInfo.sub_process_result.fget,
-        assertion,
-    )
 
 
 class AllPhasesEmptyShouldPass(SetupWithoutPreprocessor):
@@ -102,15 +70,10 @@ PREPROCESSING_TESTS = [
 ]
 
 
-def suite_for_test_case_preprocessing(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
-    return tests_for_setup_with_preprocessor(PREPROCESSING_TESTS,
-                                             main_program_runner)
-
-
 def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(tests_for_setup_without_preprocessor(MISC, main_program_runner))
-    ret_val.addTest(suite_for_test_case_preprocessing(main_program_runner))
+    ret_val.addTest(tests_for_setup_with_preprocessor(PREPROCESSING_TESTS, main_program_runner))
     return ret_val
 
 
