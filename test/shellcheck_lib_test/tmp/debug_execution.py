b6dbe18384550ea7dd2e3280d94d6bd72000fb12
Emil Karlén <emil.karlen@fripost.org>
2016-04-24 17:27:47 +0200

    Rename (to make it clear that the file is not part of the library): main_program_executor_for_test.py -> main-program-executor-for-test.py

--- test/shellcheck_lib_test/tmp/debug_execution.py
+++ test/shellcheck_lib_test/tmp/debug_execution.py
@@ -5,5 +5,7 @@ from shellcheck_lib.default.default_main_program_setup import *
 
 class TestCase(unittest.TestCase):
     def test(self):
-        exit_status = default_main_program().execute(['a.testcase'])
+        exit_status = default_main_program().execute(['--actor',
+                                                      '/usr/bin/python3',
+                                                      '/home/karlen/vcs/shellcheck/github/tmp/bug.case'])
         print(exit_status)
