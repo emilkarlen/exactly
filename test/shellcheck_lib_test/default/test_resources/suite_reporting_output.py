b5222d0fc42c0a6d9465da77fe95fd76f507022a
Emil Karlén <emil.karlen@fripost.org>
2016-04-24 15:01:49 +0200

    suite-output: Refactoring of integration tests for test suite

--- test/shellcheck_lib_test/default/test_resources/suite_reporting_output.py
+++ test/shellcheck_lib_test/default/test_resources/suite_reporting_output.py
@@ -3,25 +3,28 @@ import pathlib
 from shellcheck_lib.cli.cli_environment.exit_value import ExitValue
 
 
-def suite_begin(file_path: pathlib.Path) -> str:
-    return 'suite ' + str(file_path) + ': begin'
+class ExpectedLine:
+    def __init__(self, suite_file_abs_path: pathlib.Path):
+        self.suite_file_abs_path = suite_file_abs_path
 
+    def suite_begin(self, file_path: pathlib.Path) -> str:
+        return 'suite ' + self._sub_file_path(file_path) + ': begin'
 
-def suite_end(file_path: pathlib.Path) -> str:
-    return 'suite ' + str(file_path) + ': end'
+    def suite_end(self, file_path: pathlib.Path) -> str:
+        return 'suite ' + self._sub_file_path(file_path) + ': end'
 
+    def case(self, file_path: pathlib.Path, status: str) -> str:
+        return 'case  ' + self._sub_file_path(file_path) + ': ' + status
 
-def case(file_path: pathlib.Path, status: str) -> str:
-    return 'case  ' + str(file_path) + ': ' + status
+    def summary_for_invalid_suite(self,
+                                  file_path: pathlib.Path,
+                                  exit_value: ExitValue) -> list:
+        return [exit_value.exit_identifier]
 
+    def summary_for_valid_suite(self,
+                                file_path: pathlib.Path,
+                                exit_value: ExitValue) -> list:
+        return [exit_value.exit_identifier]
 
-def summary_for_invalid_suite(file_path: pathlib.Path, exit_value: ExitValue) -> list:
-    return [exit_value.exit_identifier]
-
-
-def summary_for_valid_suite(file_path: pathlib.Path,
-                            exit_value: ExitValue) -> list:
-    """
-    :type errors: dict ExitValue -> int
-    """
-    return [exit_value.exit_identifier]
+    def _sub_file_path(self, file_path_abs_path: pathlib.Path):
+        return str(file_path_abs_path)
