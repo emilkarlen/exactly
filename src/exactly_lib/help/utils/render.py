96993cf2963b4e29edc11ca82dc31bd4273fef20
Emil Karlén <emil.karlen@fripost.org>
2016-04-30 18:33:45 +0200

    Makes it possible to render simple header-value lists as either lists or tables (and makes htmldoc to render as tables)

--- src/exactly_lib/help/utils/render.py
+++ src/exactly_lib/help/utils/render.py
@@ -1,16 +1,23 @@
 from exactly_lib.help.utils.cross_reference import CrossReferenceTextConstructor
-from exactly_lib.util.textformat.structure import document as doc, lists
+from exactly_lib.util.textformat.structure import document as doc, lists, table
 from exactly_lib.util.textformat.structure.core import ParagraphItem
+from exactly_lib.util.textformat.structure.paragraph import Paragraph
 from exactly_lib.util.textformat.structure.structures import simple_header_only_list
 
 
 class RenderingEnvironment(tuple):
-    def __new__(cls, cross_ref_text_constructor: CrossReferenceTextConstructor):
-        return tuple.__new__(cls, (cross_ref_text_constructor,))
+    def __new__(cls, cross_ref_text_constructor: CrossReferenceTextConstructor,
+                render_simple_header_value_lists_as_tables: bool=False):
+        return tuple.__new__(cls, (cross_ref_text_constructor,
+                                   render_simple_header_value_lists_as_tables))
 
     @property
     def cross_ref_text_constructor(self) -> CrossReferenceTextConstructor:
         return self[0]
+
+    @property
+    def render_simple_header_value_lists_as_tables(self) -> bool:
+        return self[1]
 
 
 class SectionContentsRenderer:
@@ -27,3 +34,14 @@ def cross_reference_list(cross_references: list,
     return simple_header_only_list([environment.cross_ref_text_constructor.apply(cross_ref)
                                     for cross_ref in cross_references],
                                    lists.ListType.ITEMIZED_LIST)
+
+
+def transform_list_to_table(l: lists.HeaderContentList) -> table.Table:
+    rows = []
+    for item in l.items:
+        assert isinstance(item, lists.HeaderContentListItem)
+        header_cell = [Paragraph([item.header])]
+        value_cell = item.content_paragraph_items
+        rows.append([header_cell, value_cell])
+    return table.Table(table.TableFormat(),
+                       rows)
