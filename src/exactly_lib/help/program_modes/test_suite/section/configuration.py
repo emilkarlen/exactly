432863e05f74e4062bf511bd4554a73bbe50715b
Emil Karlén <emil.karlen@fripost.org>
2016-05-02 00:23:49 +0200

    suite help: htmldoc includes suite help:  adds help for instructions for section "conf"

--- src/exactly_lib/help/program_modes/test_suite/section/configuration.py
+++ src/exactly_lib/help/program_modes/test_suite/section/configuration.py
@@ -1,10 +1,19 @@
+from exactly_lib.help.program_modes.common.contents_structure import SectionInstructionSet
 from exactly_lib.help.program_modes.test_suite.section.common import \
-    TestSuiteSectionDocumentationBaseForSectionWithoutInstructions
+    TestSuiteSectionDocumentationForSectionWithInstructions
 from exactly_lib.help.utils.description import Description
 from exactly_lib.util.textformat.structure import structures as docs
 
 
-class ConfigurationSectionDocumentation(TestSuiteSectionDocumentationBaseForSectionWithoutInstructions):
+class ConfigurationSectionDocumentation(TestSuiteSectionDocumentationForSectionWithInstructions):
+    def __init__(self,
+                 name: str,
+                 instruction_set: SectionInstructionSet):
+        super().__init__(name, instruction_set)
+
+    def instruction_purpose_description(self) -> list:
+        return [docs.para('TODO conf section instruction purpose')]
+
     def is_mandatory(self) -> bool:
         return False
 
