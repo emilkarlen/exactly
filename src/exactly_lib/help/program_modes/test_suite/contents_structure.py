6efa575609a18b2083581c3216ab1bfd73e478c2
Emil Karlén <emil.karlen@fripost.org>
2016-05-01 22:56:26 +0200

    suite help: Refactoring: Replaces TestSuiteSectionHelp with SectionDocumentation

--- src/exactly_lib/help/program_modes/test_suite/contents_structure.py
+++ src/exactly_lib/help/program_modes/test_suite/contents_structure.py
@@ -1,13 +1,3 @@
-class TestSuiteSectionHelp(tuple):
-    def __new__(cls,
-                name: str):
-        return tuple.__new__(cls, (name,))
-
-    @property
-    def name(self) -> str:
-        return self[0]
-
-
 class TestSuiteHelp(tuple):
     def __new__(cls,
                 test_suite_section_helps: iter):
