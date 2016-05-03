7b6116a64b81078a5db24cf49dc4de7c913e38ac
Emil Karlén <emil@member.fsf.org>
2016-05-03 19:34:16 +0200

    help text: Removes redundant code (help request resolver for test-suite)

--- src/exactly_lib/cli/program_modes/help/program_modes/test_suite/request_rendering.py
+++ src/exactly_lib/cli/program_modes/help/program_modes/test_suite/request_rendering.py
@@ -1,10 +1,10 @@
 from exactly_lib.cli.program_modes.help.program_modes.test_suite.help_request import TestSuiteHelpItem, \
     TestSuiteHelpRequest
 from exactly_lib.help.program_modes.common.contents_structure import SectionDocumentation
+from exactly_lib.help.program_modes.common.renderers import SectionDocumentationRenderer
 from exactly_lib.help.program_modes.test_suite.contents.overview import OverviewRenderer
 from exactly_lib.help.program_modes.test_suite.contents_structure import TestSuiteHelp
 from exactly_lib.help.utils.render import SectionContentsRenderer, RenderingEnvironment
-from exactly_lib.help.program_modes.common.renderers import SectionDocumentationRenderer
 from exactly_lib.util.textformat.structure import document as doc
 
 
@@ -23,10 +23,4 @@ class TestSuiteHelpRendererResolver:
 
     def render(self, request: TestSuiteHelpRequest,
                environment: RenderingEnvironment) -> doc.SectionContents:
-        item = request.item
-        if item is TestSuiteHelpItem.OVERVIEW:
-            return OverviewRenderer(self._contents).apply(environment)
-        if item is TestSuiteHelpItem.SECTION:
-            assert isinstance(request.data, SectionDocumentation)
-            return SectionDocumentationRenderer(request.data).apply(environment)
-        raise ValueError('Invalid %s: %s' % (str(TestSuiteHelpItem), str(item)))
+        return self.resolve(request).apply(environment)
