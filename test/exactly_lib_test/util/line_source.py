b3435ce53dcd312e32e3096f34cddacb03531ec1
Emil Karlén <emil@member.fsf.org>
2016-05-05 23:53:44 +0200

    LineSource: Ports tests to windows

--- test/exactly_lib_test/util/line_source.py
+++ test/exactly_lib_test/util/line_source.py
@@ -1,12 +1,26 @@
-import os
 import unittest
 
 from exactly_lib.util import line_source
+from exactly_lib.util.string import lines_content
 from exactly_lib_test.test_resources.file_utils import tmp_file_containing
+
+
+def suite():
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(unittest.makeSuite(TestStringLineSource))
+    ret_val.addTest(unittest.makeSuite(TestFileLineSource))
+    return ret_val
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
 
 
 def line(line_number: int, text: str) -> line_source.Line:
     return line_source.Line(line_number, text)
+
+
+NL = '\n'
 
 
 class TestStringLineSource(unittest.TestCase):
@@ -22,21 +36,21 @@ class TestStringLineSource(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_single_linesep_should_give_two_lines(self):
-        two_lines = os.linesep.join(['first', 'second'])
+        two_lines = NL.join(['first', 'second'])
         line_source_for_two_lines = line_source.new_for_string(two_lines)
         actual = list(iter(line_source_for_two_lines))
         expected = [line(1, 'first'), line(2, 'second')]
         self.assertEqual(expected, actual)
 
     def test_trailing_linesep_should_produce_no_line(self):
-        single_line_with_linesep = 'first' + os.linesep
+        single_line_with_linesep = 'first' + NL
         line_source_for_two_lines = line_source.new_for_string(single_line_with_linesep)
         actual = list(iter(line_source_for_two_lines))
         expected = [line(1, 'first')]
         self.assertEqual(expected, actual)
 
     def test_empty_line(self):
-        contents = 'first' + os.linesep + os.linesep
+        contents = 'first' + NL + NL
         line_source_for_two_lines = line_source.new_for_string(contents)
         actual = list(iter(line_source_for_two_lines))
         expected = [line(1, 'first'),
@@ -52,8 +66,8 @@ class TestFileLineSource(unittest.TestCase):
         self.assertEqual([], actual)
 
     def test_single_linesep_should_give_two_lines(self):
-        two_lines = os.linesep.join(['first',
-                                     'second'])
+        two_lines = lines_content(['first',
+                                   'second'])
         with tmp_file_containing(two_lines) as file_path:
             ls = line_source.new_for_file(file_path)
             actual = list(iter(ls))
@@ -62,7 +76,7 @@ class TestFileLineSource(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_trailing_linesep_should_produce_no_line(self):
-        single_line_with_linesep = 'first' + os.linesep
+        single_line_with_linesep = lines_content(['first'])
         with tmp_file_containing(single_line_with_linesep) as file_path:
             ls = line_source.new_for_file(file_path)
             actual = list(iter(ls))
@@ -70,21 +84,10 @@ class TestFileLineSource(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_empty_line(self):
-        contents = 'first' + os.linesep + os.linesep
+        contents = lines_content(['first', ''])
         with tmp_file_containing(contents) as file_path:
             ls = line_source.new_for_file(file_path)
             actual = list(iter(ls))
         expected = [line(1, 'first'),
                     line(2, '')]
         self.assertEqual(expected, actual)
-
-
-def suite():
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestStringLineSource))
-    ret_val.addTest(unittest.makeSuite(TestFileLineSource))
-    return ret_val
-
-
-if __name__ == '__main__':
-    unittest.main()
