0707e4a5ba4d2dded46ba78d41c034de871517a7
Emil Karlén <emil@member.fsf.org>
2016-05-05 22:51:35 +0200

    Test of ArgumentUsageVisitor

--- test/exactly_lib_test/util/cli_syntax/__init__.py
+++ test/exactly_lib_test/util/cli_syntax/__init__.py
@@ -1,11 +1,13 @@
 import unittest
 
 from exactly_lib_test.util.cli_syntax.elements import argument
+from exactly_lib_test.util.cli_syntax.render import cli_program_syntax
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(argument.suite())
+    ret_val.addTest(cli_program_syntax.suite())
     return ret_val
 
 
---
+++ test/exactly_lib_test/util/cli_syntax/render/cli_program_syntax.py
@@ -0,0 +1,59 @@
+import unittest
+
+from exactly_lib.util.cli_syntax.render import cli_program_syntax as sut
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(ArgumentRecordingArgumentVisitor),
+        unittest.makeSuite(ArgumentVisitorTest),
+    ])
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
+
+
+class ArgumentRecordingArgumentVisitor(sut.ArgumentVisitor):
+    def __init__(self):
+        self.visited_classes = []
+
+    def visit_constant(self, x: sut.Constant):
+        self.visited_classes.append(sut.Constant)
+        return x
+
+    def visit_named(self, x: sut.Named):
+        self.visited_classes.append(sut.Named)
+        return x
+
+    def visit_option(self, x: sut.Option):
+        self.visited_classes.append(sut.Option)
+        return x
+
+
+class ArgumentVisitorTest(unittest.TestCase):
+    def test_as_is(self):
+        self._check(sut.Constant('name'), sut.Constant)
+
+    def test_positional(self):
+        self._check(sut.Named('value-element'), sut.Named)
+
+    def test_plain_option(self):
+        self._check(sut.Option('n'), sut.Option)
+
+    def test_visit_SHOULD_raise_TypeError_WHEN_argument_is_not_a_sub_class_of_argument(self):
+        visitor = ArgumentRecordingArgumentVisitor()
+        with self.assertRaises(TypeError):
+            visitor.visit('not an argument')
+
+    def _check(self, x: sut.Argument, expected_class):
+        # ARRANGE #
+        visitor = ArgumentRecordingArgumentVisitor()
+        # ACT #
+        returned = visitor.visit(x)
+        # ASSERT #
+        self.assertListEqual([expected_class],
+                             visitor.visited_classes)
+        self.assertIs(x,
+                      returned,
+                      'Visitor should return the return-value of the visited method')
