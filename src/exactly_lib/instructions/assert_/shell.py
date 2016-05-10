982dc73108813a7cb5a74ad73c097abb9bf42b6d
Emil Karlén <emil.karlen@fripost.org>
2016-05-10 23:30:50 +0200

    Improves help text for shell

--- src/exactly_lib/instructions/assert_/shell.py
+++ src/exactly_lib/instructions/assert_/shell.py
@@ -1,32 +1,32 @@
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser
+from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.instructions.multi_phase_instructions import shell as shell_common
 from exactly_lib.instructions.multi_phase_instructions.shell import TheInstructionDocumentationBase
 from exactly_lib.instructions.utils.sub_process_execution import ExecuteInfo
-from exactly_lib.common.instruction_setup import SingleInstructionSetup
+from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParser
 from exactly_lib.test_case.os_services import OsServices
 from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
 from exactly_lib.test_case.phases.common import GlobalEnvironmentForPostEdsPhase
 from exactly_lib.test_case.phases.result import pfh
-from exactly_lib.util.textformat import parse as text_parse
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-            parser(),
-            TheDescription(instruction_name))
+        parser(),
+        TheDescription(instruction_name))
 
 
 class TheDescription(TheInstructionDocumentationBase):
     def __init__(self, name: str):
         super().__init__(name)
 
-    def main_description_rest(self) -> list:
+    def _main_description_rest_prefix(self) -> list:
         text = """\
             The assertion PASSes if (and only if) the exit code from the command is 0.
 
             All other exit codes makes the assertion FAIL.
             """
-        return text_parse.normalize_and_parse(text)
+        return self._paragraphs(text)
 
 
 def parser() -> SingleInstructionParser:
