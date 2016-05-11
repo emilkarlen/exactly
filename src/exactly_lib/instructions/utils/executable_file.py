ae27e988712fc4c0760c8057c32f19ba04c43eb3
Emil Karlén <emil@member.fsf.org>
2016-05-11 21:13:23 +0200

    help text: Refactoring: Moves utility for parsing an "executable file" to package for parsing functionality

--- src/exactly_lib/instructions/utils/executable_file.py
+++ src/exactly_lib/instructions/utils/executable_file.py
@@ -2,19 +2,9 @@ import pathlib
 import stat
 
 from exactly_lib.instructions.utils import file_ref
-from exactly_lib.instructions.utils.arg_parse import parse_file_ref
-from exactly_lib.instructions.utils.arg_parse.parse_utils import TokenStream
 from exactly_lib.instructions.utils.file_ref import FileRefValidatorBase
 from exactly_lib.instructions.utils.pre_or_post_validation import PreOrPostEdsValidator
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionInvalidArgumentException
 from exactly_lib.test_case.phases.common import HomeAndEds
-
-LIST_DELIMITER_START = '('
-LIST_DELIMITER_END = ')'
-
-
-PARSE_FILE_REF_CONFIGURATION = parse_file_ref.DEFAULT_CONFIG
 
 
 class ExecutableFile:
@@ -46,36 +36,6 @@ class ExecutableFile:
     @property
     def validator(self) -> PreOrPostEdsValidator:
         return self._validator
-
-
-def parse(tokens: TokenStream) -> (ExecutableFile, TokenStream):
-    """
-    :param tokens: instruction argument
-    :raise SingleInstructionInvalidArgumentException: Invalid file syntax
-    """
-    if tokens.is_null:
-        parse_file_ref.parse_file_ref(tokens, conf=PARSE_FILE_REF_CONFIGURATION)  # will raise exception
-    if tokens.head == LIST_DELIMITER_START:
-        (the_file_ref, remaining_tokens) = parse_file_ref.parse_file_ref(tokens.tail,
-                                                                         conf=PARSE_FILE_REF_CONFIGURATION)
-        (exe_argument_list, tail_tokens) = _parse_arguments_and_end_delimiter(remaining_tokens)
-        return ExecutableFile(the_file_ref, exe_argument_list), tail_tokens
-    else:
-        (the_file_ref, remaining_tokens) = parse_file_ref.parse_file_ref(tokens,
-                                                                         conf=PARSE_FILE_REF_CONFIGURATION)
-        return ExecutableFile(the_file_ref, []), remaining_tokens
-
-
-def _parse_arguments_and_end_delimiter(tokens: TokenStream) -> (list, TokenStream):
-    arguments = []
-    while True:
-        if tokens.is_null:
-            msg = 'Missing end delimiter surrounding executable: %s' % LIST_DELIMITER_END
-            raise SingleInstructionInvalidArgumentException(msg)
-        if tokens.head == LIST_DELIMITER_END:
-            return arguments, tokens.tail
-        arguments.append(tokens.head)
-        tokens = tokens.tail
 
 
 class ExistingExecutableFile(FileRefValidatorBase):
--- test/exactly_lib_test/instructions/utils/__init__.py
+++ test/exactly_lib_test/instructions/utils/__init__.py
@@ -1,16 +1,15 @@
 import unittest
 
 from exactly_lib_test.instructions.utils import arg_parse
-from exactly_lib_test.instructions.utils import executable_file, sub_process_execution
 from exactly_lib_test.instructions.utils import file_properties
 from exactly_lib_test.instructions.utils import file_ref
+from exactly_lib_test.instructions.utils import sub_process_execution
 
 
-def suite():
+def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(file_properties.suite())
     ret_val.addTest(file_ref.suite())
-    ret_val.addTest(executable_file.suite())
     ret_val.addTest(sub_process_execution.suite())
     ret_val.addTest(arg_parse.suite())
     return ret_val
