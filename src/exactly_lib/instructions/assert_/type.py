2bbf692d4c9716604be5486b03b8920601bca098
Emil Karlén <emil.karlen@fripost.org>
2016-05-12 21:41:17 +0200

    help text: Improves help text for assert/type

--- src/exactly_lib/instructions/assert_/type.py
+++ src/exactly_lib/instructions/assert_/type.py
@@ -1,21 +1,26 @@
 from exactly_lib.common.instruction_documentation import InvokationVariant, \
-    InstructionDocumentation, \
     SyntaxElementDescription
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
+from exactly_lib.help.concepts.plain_concepts.present_working_directory import PRESENT_WORKING_DIRECTORY_CONCEPT
+from exactly_lib.help.utils import formatting
 from exactly_lib.instructions.utils import file_ref
 from exactly_lib.instructions.utils.arg_parse.parse_utils import split_arguments_list_string, \
     ensure_is_not_option_argument
+from exactly_lib.instructions.utils.documentation import documentation_text as dt
+from exactly_lib.instructions.utils.documentation.instruction_documentation_with_text_parser import \
+    InstructionDocumentationWithCommandLineRenderingBase
 from exactly_lib.instructions.utils.file_properties import FileType, must_exist_as, FilePropertiesCheck, type_name
 from exactly_lib.instructions.utils.file_ref_check import pre_or_post_eds_failure_message_or_none, FileRefCheck
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser, \
+from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParser, \
     SingleInstructionInvalidArgumentException, SingleInstructionParserSource
 from exactly_lib.test_case.os_services import OsServices
 from exactly_lib.test_case.phases import common as i
 from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
 from exactly_lib.test_case.phases.result import pfh
+from exactly_lib.util.cli_syntax.elements import argument as a
 from exactly_lib.util.textformat.structure import core
 from exactly_lib.util.textformat.structure import lists
-from exactly_lib.util.textformat.structure.structures import paras, text
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
@@ -30,41 +35,66 @@ FILE_TYPES = {
     "directory": FileType.DIRECTORY
 }
 
+_TYPE_ARGUMENT_STR = 'TYPE'
 
-class TheInstructionDocumentation(InstructionDocumentation):
+
+class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderingBase):
     def __init__(self, name: str):
-        super().__init__(name)
+        self.type_argument = a.Named(_TYPE_ARGUMENT_STR)
+        self.path_argument = dt.PATH_ARGUMENT
+        super().__init__(name, {
+            'PATH': self.path_argument.name,
+            'pwd': formatting.concept(PRESENT_WORKING_DIRECTORY_CONCEPT.name().singular),
+        })
 
     def single_line_description(self) -> str:
         return 'Tests the type of a file.'
 
     def main_description_rest(self) -> list:
-        return paras('All tests fails if FILENAME does not exist. (TODO symlink options/behavior)')
+        text = """\
+        PASS if, and only if, {PATH} exists, and is a file of the given type.
+
+
+        {PATH} is relative the {pwd}.
+        """
+        return self._paragraphs(text)
 
     def invokation_variants(self) -> list:
         return [
-            InvokationVariant(
-                'FILENAME type TYPE',
-                paras('File exists and has given type.')),
+            InvokationVariant(self._cl_syntax_for_args([
+                a.Single(a.Multiplicity.MANDATORY,
+                         self.path_argument),
+                a.Single(a.Multiplicity.MANDATORY,
+                         self.type_argument),
+            ]),
+                []),
         ]
 
     def syntax_element_descriptions(self) -> list:
         def type_description(k: str) -> str:
             tn = type_name[FILE_TYPES[k]]
-            return paras('Tests if FILENAME is a %s or, a sym-link to a %s.' % (tn, tn))
+            text = 'Tests if {PATH} is a %s, or a symbolic link to a %s.' % (tn, tn)
+            if FILE_TYPES[k] is FileType.SYMLINK:
+                text = 'Tests if {PATH} is a %s.' % tn
+            return self._paragraphs(text)
 
         def file_type_list() -> core.ParagraphItem:
             list_items = [
-                lists.HeaderContentListItem(text(k),
+                lists.HeaderContentListItem(self._text(k),
                                             type_description(k))
                 for k in sorted(FILE_TYPES.keys())]
             return lists.HeaderContentList(list_items,
                                            lists.Format(lists.ListType.VARIABLE_LIST))
 
         return [
-            SyntaxElementDescription('TYPE',
+            SyntaxElementDescription(self.type_argument.name,
                                      [file_type_list()],
                                      [])
+        ]
+
+    def see_also(self) -> list:
+        return [
+            PRESENT_WORKING_DIRECTORY_CONCEPT.cross_reference_target(),
         ]
 
 
@@ -72,7 +102,7 @@ class Parser(SingleInstructionParser):
     def apply(self, source: SingleInstructionParserSource) -> AssertPhaseInstruction:
         arguments = split_arguments_list_string(source.instruction_argument)
         if len(arguments) != 2:
-            raise SingleInstructionInvalidArgumentException('Invalid syntax')
+            raise SingleInstructionInvalidArgumentException('Expecting exactly two arguments.')
         file_argument = arguments[0]
         ensure_is_not_option_argument(file_argument)
         file_reference = file_ref.rel_cwd(file_argument)
@@ -84,16 +114,19 @@ class Parser(SingleInstructionParser):
     def _parse_properties(arguments: list) -> FilePropertiesCheck:
         num_arguments = len(arguments)
         if num_arguments == 0:
-            raise SingleInstructionInvalidArgumentException('Missing type argument')
+            msg = 'Missing {TYPE} argument.'.format(TYPE=_TYPE_ARGUMENT_STR)
+            raise SingleInstructionInvalidArgumentException(msg)
         if num_arguments > 1:
-            raise SingleInstructionInvalidArgumentException('Expecting a single type argument')
+            msg = 'Expecting a single {TYPE} argument.'.format(TYPE=_TYPE_ARGUMENT_STR)
+            raise SingleInstructionInvalidArgumentException(msg)
         try:
             file_type = FILE_TYPES[arguments[0]]
             follow_sym_links = file_type is not FileType.SYMLINK
             return must_exist_as(file_type,
                                  follow_sym_links)
         except KeyError:
-            raise SingleInstructionInvalidArgumentException('Invalid file type: ' + arguments[0])
+            msg = 'Invalid {TYPE}: "{arg}".'.format(TYPE=_TYPE_ARGUMENT_STR, arg=arguments[0])
+            raise SingleInstructionInvalidArgumentException(msg)
 
 
 class _Instruction(AssertPhaseInstruction):
