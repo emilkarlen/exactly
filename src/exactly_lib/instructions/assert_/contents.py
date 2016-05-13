ecf8fa9ef98c2735430a0bb39219159e9379a422
Emil Karlén <emil@member.fsf.org>
2016-05-13 19:27:22 +0200

    help text: Improves help for assert/contents

--- src/exactly_lib/instructions/assert_/contents.py
+++ src/exactly_lib/instructions/assert_/contents.py
@@ -1,22 +1,26 @@
 import pathlib
 
-from exactly_lib.common.instruction_documentation import InvokationVariant, \
-    InstructionDocumentation
+from exactly_lib.common.instruction_documentation import InvokationVariant
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.execution.execution_directory_structure import \
     root_dir_for_non_stdout_or_stderr_files_with_replaced_env_vars, SUB_DIR_FOR_REPLACEMENT_SOURCES_UNDER_ACT_DIR, \
-    SUB_DIR_FOR_REPLACEMENT_SOURCES_NOT_UNDER_ACT_DIR, PATH__TMP_USER
-from exactly_lib.instructions.assert_.utils.contents_utils import ActualFileTransformer, EMPTY_ARGUMENT, \
-    WITH_REPLACED_ENV_VARS_OPTION, parse_actual_file_argument, with_replaced_env_vars_help
+    SUB_DIR_FOR_REPLACEMENT_SOURCES_NOT_UNDER_ACT_DIR
+from exactly_lib.help.concepts.contents_structure import ConceptDocumentation
+from exactly_lib.help.concepts.plain_concepts.environment_variable import ENVIRONMENT_VARIABLE_CONCEPT
+from exactly_lib.instructions.assert_.utils.contents_utils import ActualFileTransformer, parse_actual_file_argument, \
+    with_replaced_env_vars_help
 from exactly_lib.instructions.utils.arg_parse.parse_utils import split_arguments_list_string
-from exactly_lib.instructions.utils.arg_parse.relative_path_options import REL_HOME_OPTION, REL_TMP_OPTION, \
-    REL_CWD_OPTION
+from exactly_lib.instructions.utils.documentation import documentation_text as dt
+from exactly_lib.instructions.utils.documentation import relative_path_options_documentation as rel_opts
+from exactly_lib.instructions.utils.documentation.instruction_documentation_with_text_parser import \
+    InstructionDocumentationWithCommandLineRenderingBase
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import SingleInstructionParser, \
     SingleInstructionInvalidArgumentException, SingleInstructionParserSource
 from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
 from exactly_lib.test_case.phases.common import GlobalEnvironmentForPostEdsPhase
-from exactly_lib.util.textformat.structure.structures import paras
+from exactly_lib.util.cli_syntax.elements import argument as a
 from .utils import contents_utils
+from .utils import contents_utils_for_instr_doc as doc_utils
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
@@ -24,41 +28,65 @@ def setup(instruction_name: str) -> SingleInstructionSetup:
                                   TheInstructionDocumentation(instruction_name))
 
 
-class TheInstructionDocumentation(InstructionDocumentation):
+class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderingBase):
     def __init__(self, name: str):
-        super().__init__(name)
+        self.expected_file_arg = a.Named(contents_utils.RELATIVITY_CONFIGURATION.argument_syntax_name)
+        self.actual_file_arg = dt.PATH_ARGUMENT
+        super().__init__(name, {
+            'checked_file': self.actual_file_arg.name,
+            'EXPECTED_FILE_ARG': self.expected_file_arg.name,
+        })
+        self.with_replaced_env_vars_option = a.Option(contents_utils.WITH_REPLACED_ENV_VARS_OPTION_NAME)
 
     def single_line_description(self) -> str:
         return 'Tests the contents of a file.'
 
     def main_description_rest(self) -> list:
-        return with_replaced_env_vars_help('FILENAME')
+        return self._paragraphs("""\
+        FAILs if {checked_file} is not an existing file.""")
 
     def invokation_variants(self) -> list:
+        actual_file = a.Single(a.Multiplicity.MANDATORY,
+                               self.actual_file_arg)
+        mandatory_empty_arg = a.Single(a.Multiplicity.MANDATORY,
+                                       doc_utils.EMPTY_ARGUMENT_CONSTANT)
+        mandatory_not_arg = a.Single(a.Multiplicity.MANDATORY,
+                                     doc_utils.NOT_ARGUMENT_CONSTANT)
+        mandatory_file_arg = a.Single(a.Multiplicity.MANDATORY,
+                                      self.expected_file_arg)
+        optional_replace_env_vars_option = a.Single(a.Multiplicity.OPTIONAL,
+                                                    self.with_replaced_env_vars_option)
         return [
-            InvokationVariant(
-                'FILENAME {}'.format(EMPTY_ARGUMENT),
-                paras('File exists, is a regular file, and is empty')),
-            InvokationVariant(
-                'FILENAME ! {}'.format(EMPTY_ARGUMENT),
-                paras('File exists, is a regular file, and is not empty')),
-            InvokationVariant(
-                'FILENAME {} {} FILE'.format(WITH_REPLACED_ENV_VARS_OPTION,
-                                             REL_HOME_OPTION),
-                paras('Compares contents of FILENAME to contents of FILE '
-                      '(which is a path relative home)')),
-            InvokationVariant(
-                'FILENAME {} {} FILE'.format(WITH_REPLACED_ENV_VARS_OPTION,
-                                             REL_TMP_OPTION),
-                paras('Compares contents of FILENAME to contents of FILE '
-                      '(which is a path relative the {user_tmp_dir} directory inside of the sandbox)'
-                      .format(user_tmp_dir=PATH__TMP_USER))),
-            InvokationVariant(
-                'FILENAME {} {} FILE'.format(WITH_REPLACED_ENV_VARS_OPTION,
-                                             REL_CWD_OPTION),
-                paras('Compares contents of FILENAME to contents of FILE '
-                      '(which is a path relative current working directory)')),
+            InvokationVariant(self._cl_syntax_for_args([actual_file, mandatory_empty_arg]),
+                              self._paragraphs('Asserts that {checked_file} is an empty file.')),
+            InvokationVariant(self._cl_syntax_for_args([actual_file, mandatory_not_arg, mandatory_empty_arg]),
+                              self._paragraphs('Asserts that {checked_file} is a non-empty file.')),
+            InvokationVariant(self._cl_syntax_for_args([actual_file,
+                                                        optional_replace_env_vars_option,
+                                                        rel_opts.OPTIONAL_RELATIVITY_ARGUMENT_USAGE,
+                                                        mandatory_file_arg,
+                                                        ]),
+                              self._paragraphs("""\
+                              Asserts that the contents of {checked_file}
+                              is equal to the contents of {EXPECTED_FILE_ARG}.
+                              """)),
         ]
+
+    def syntax_element_descriptions(self) -> list:
+        return [
+            rel_opts.relativity_syntax_element_description(self.expected_file_arg,
+                                                           contents_utils.RELATIVITY_CONFIGURATION.accepted_options,
+                                                           rel_opts.RELATIVITY_ARGUMENT),
+            self._cli_argument_syntax_element_description(self.with_replaced_env_vars_option,
+                                                          with_replaced_env_vars_help('the contents of ' +
+                                                                                      self.actual_file_arg.name))
+        ]
+
+    def see_also(self) -> list:
+        concepts = rel_opts.see_also_concepts(contents_utils.RELATIVITY_CONFIGURATION.accepted_options)
+        if not ENVIRONMENT_VARIABLE_CONCEPT in concepts:
+            concepts.append(ENVIRONMENT_VARIABLE_CONCEPT)
+        return list(map(ConceptDocumentation.cross_reference_target, concepts))
 
 
 class Parser(SingleInstructionParser):
