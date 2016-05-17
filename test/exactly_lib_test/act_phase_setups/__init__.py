e629b7eb60cc4095b238b6bbc602e373d775e956
Emil Karlén <emil.karlen@fripost.org>
2016-05-17 23:45:24 +0200

    act phase/script: Test of script source parser

--- test/exactly_lib_test/act_phase_setups/__init__.py
+++ test/exactly_lib_test/act_phase_setups/__init__.py
@@ -1,19 +1,16 @@
 import unittest
 
-from exactly_lib_test.act_phase_setups import script_language_setup, single_command_setup
+from exactly_lib_test.act_phase_setups import script_language_setup, single_command_setup, \
+    source_parser_and_instruction
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(script_language_setup.suite())
     ret_val.addTest(single_command_setup.suite())
+    ret_val.addTest(source_parser_and_instruction.suite())
     return ret_val
 
 
-def run_suite():
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
-
-
 if __name__ == '__main__':
-    run_suite()
+    unittest.TextTestRunner().run(suite())
