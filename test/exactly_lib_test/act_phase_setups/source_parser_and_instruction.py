ec41914092097c27846462cd12e415248d509191
Emil Karlén <emil@member.fsf.org>
2016-05-18 01:10:01 +0200

    act phase/script: Script source parser now parses all lines as a single instruction, and includes empty lines and comment lines in the source

--- test/exactly_lib_test/act_phase_setups/source_parser_and_instruction.py
+++ test/exactly_lib_test/act_phase_setups/source_parser_and_instruction.py
@@ -4,6 +4,7 @@ from exactly_lib.act_phase_setups import source_parser_and_instruction as sut
 from exactly_lib.section_document import model
 from exactly_lib.section_document.parse import LineSequenceSourceFromListOfLines, ListOfLines
 from exactly_lib.util import line_source
+from exactly_lib.util.string import line_separated
 
 
 def suite() -> unittest.TestSuite:
@@ -31,7 +32,7 @@ class TestParse(unittest.TestCase):
         self.assertFalse(source.has_next(),
                          'There should be no remaining lines in the source')
 
-    def test_parse_SHOULD_only_read_a_single_line_even_if_there_are_multiple_lines(self):
+    def test_parse_SHOULD_read_all_remaining_lines_if_no_section_header_line_follows(self):
         # ARRANGE #
         following_lines = ListOfLines(['second line',
                                        'third line'])
@@ -42,10 +43,28 @@ class TestParse(unittest.TestCase):
         # ASSERT #
         instruction = self._assert_is_instruction_element_with_correct_type_of_instruction(element)
         self.assertEqual(instruction.source_code,
-                         'first line')
+                         line_separated(['first line',
+                                         'second line',
+                                         'third line']))
+        self.assertFalse(source.has_next(),
+                         'There should be no remaining lines in the source')
+
+    def test_parse_SHOULD_read_all_lines_until_but_not_including_next_section_header_line(self):
+        # ARRANGE #
+        following_lines = ListOfLines(['second line',
+                                       '[section-header]'])
+        source = LineSequenceSourceFromListOfLines(following_lines)
+        source_builder = line_source.LineSequenceBuilder(source, 1, 'first line')
+        # ACT #
+        element = sut.PlainSourceActPhaseParser().apply(source_builder)
+        # ASSERT #
+        instruction = self._assert_is_instruction_element_with_correct_type_of_instruction(element)
+        self.assertEqual(instruction.source_code,
+                         line_separated(['first line',
+                                         'second line']))
         self.assertTrue(source.has_next(),
                         'There should be remaining lines in the source')
-        self.assertEqual('second line',
+        self.assertEqual('[section-header]',
                          source.next_line(),
                          'All following lines should remain in in the source')
 
--- test/exactly_lib_test/section_document/test_syntax.py
+++ test/exactly_lib_test/section_document/test_syntax.py
@@ -3,65 +3,65 @@ import unittest
 from exactly_lib.section_document import syntax
 
 
-class TestExtractPhaseNameFromPhaseLine(unittest.TestCase):
+class TestExtractSectionNameFromSectionLine(unittest.TestCase):
     def test_valid_phase_line_without_initial_space(self):
         name = 'name'
-        actual = syntax.extract_phase_name_from_phase_line(syntax.section_header(name))
+        actual = syntax.extract_section_name_from_section_line(syntax.section_header(name))
         self.assertEqual(name, actual)
 
     def test_valid_special_characters(self):
         name = 'begin -_.0123456789'
-        actual = syntax.extract_phase_name_from_phase_line(syntax.section_header(name))
+        actual = syntax.extract_section_name_from_section_line(syntax.section_header(name))
         self.assertEqual(name, actual)
 
     def test_valid_phase_line_with_initial_space(self):
-        actual = syntax.extract_phase_name_from_phase_line('       [name]')
+        actual = syntax.extract_section_name_from_section_line('       [name]')
         expected = 'name'
         self.assertEqual(expected, actual)
 
     def test_valid_phase_line_with_trailing_space(self):
-        actual = syntax.extract_phase_name_from_phase_line('[name]         ')
+        actual = syntax.extract_section_name_from_section_line('[name]         ')
         expected = 'name'
         self.assertEqual(expected, actual)
 
     def test_invalid_characters_in_name(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[! is not allowed in names]')
 
     def test_name_must_not_be_empty(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[]')
 
     def test_name_must_not_begin_with_space(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[ bad]')
 
     def test_name_must_not_end_with_space(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[bad ]')
 
     def test_name_must_be_surrounded_by_markers(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[missing_right_bracket')
 
     def test_right_bracket_can_only_be_followed_by_space(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[invalid-content-after-header] only space is allowed')
 
     def test_right_bracket_can_not_be_followed_by_a_comment(self):
-        self.assertRaises(syntax.GeneralError,
-                          syntax.extract_phase_name_from_phase_line,
+        self.assertRaises(ValueError,
+                          syntax.extract_section_name_from_section_line,
                           '[invalid-content-after-header]#not even a comment is allowed')
 
 
 def suite() -> unittest.TestSuite:
-    return unittest.makeSuite(TestExtractPhaseNameFromPhaseLine)
+    return unittest.makeSuite(TestExtractSectionNameFromSectionLine)
 
 
 if __name__ == '__main__':
