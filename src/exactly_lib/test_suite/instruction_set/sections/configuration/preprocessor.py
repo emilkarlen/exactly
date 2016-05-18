141e1d4f0de5ab80e15f57b316f6c872615680ff
Emil Karlén <emil@member.fsf.org>
2016-05-18 21:28:56 +0200

    help text: Improves documentation of suite/conf/preprocessor

--- src/exactly_lib/test_suite/instruction_set/sections/configuration/preprocessor.py
+++ src/exactly_lib/test_suite/instruction_set/sections/configuration/preprocessor.py
@@ -1,14 +1,16 @@
 import shlex
 
-from exactly_lib.common.instruction_documentation import InstructionDocumentation, InvokationVariant
+from exactly_lib.common.instruction_documentation import InvokationVariant, SyntaxElementDescription
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.help.concepts.plain_concepts.preprocessor import PREPROCESSOR_CONCEPT
+from exactly_lib.instructions.utils.documentation.instruction_documentation_with_text_parser import \
+    InstructionDocumentationWithCommandLineRenderingBase
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionParser, SingleInstructionParserSource, SingleInstructionInvalidArgumentException
 from exactly_lib.test_case.preprocessor import PreprocessorViaExternalProgram
 from exactly_lib.test_suite.instruction_set.sections.configuration.instruction_definition import \
     ConfigurationSectionInstruction, ConfigurationSectionEnvironment
-from exactly_lib.util.textformat.structure.structures import paras
+from exactly_lib.util.cli_syntax.elements import argument as a
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
@@ -16,24 +18,45 @@ def setup(instruction_name: str) -> SingleInstructionSetup:
                                   _TheInstructionDocumentation(instruction_name))
 
 
-class _TheInstructionDocumentation(InstructionDocumentation):
+class _TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderingBase):
     def __init__(self, name: str):
-        super().__init__(name)
+        super().__init__(name, {})
+        self.executable = a.Named('EXECUTABLE')
 
     def single_line_description(self) -> str:
         return 'Sets a preprocessor to use for each test case in the suite'
 
     def invokation_variants(self) -> list:
+        executable_arg = a.Single(a.Multiplicity.MANDATORY,
+                                  self.executable)
+        optional_arguments_arg = a.Single(a.Multiplicity.ZERO_OR_MORE,
+                                          a.Named('ARGUMENT'))
         return [
-            InvokationVariant(
-                'EXECUTABLE [ARGUMENT...]',
-                paras('An executable program that will be given the test case file as single argument')),
+            InvokationVariant(self._cl_syntax_for_args([executable_arg,
+                                                        optional_arguments_arg])),
         ]
+
+    def syntax_element_descriptions(self) -> list:
+        return [
+            SyntaxElementDescription(self.executable.name,
+                                     self._paragraphs('The path of an existing executable file.'))
+        ]
+
+    def main_description_rest(self) -> list:
+        return self._paragraphs(_DESCRIPTION)
 
     def see_also(self) -> list:
         return [
             PREPROCESSOR_CONCEPT.cross_reference_target(),
         ]
+
+
+_DESCRIPTION = """\
+An executable program that will be given the test case file as single (additional) argument.
+
+
+The command line uses shell syntax.
+"""
 
 
 class _InstructionParser(SingleInstructionParser):
