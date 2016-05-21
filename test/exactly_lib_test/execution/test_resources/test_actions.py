414f71b87c1fbec05f5c384206aaaf3b9e13fc11
Emil Karlén <emil@member.fsf.org>
2016-05-21 04:00:02 +0200

    error handling in act phase: partial execution: Handling of HARD ERROR from ActPhaseExecutor#execute

--- test/exactly_lib_test/execution/test_resources/test_actions.py
+++ test/exactly_lib_test/execution/test_resources/test_actions.py
@@ -1,4 +1,5 @@
-from exactly_lib.execution.act_phase import ExitCodeOrHardError, new_eh_exit_code
+from exactly_lib.execution.act_phase import ExitCodeOrHardError, new_eh_exit_code, new_eh_hard_error
+from exactly_lib.execution.result import new_failure_details_from_message
 from exactly_lib.test_case.phases.result import svh
 
 
@@ -19,6 +20,13 @@ def validate_action_that_raises(ex: Exception):
 def execute_action_that_returns_exit_code(exit_code: int = 0):
     def f() -> ExitCodeOrHardError:
         return new_eh_exit_code(exit_code)
+
+    return f
+
+
+def execute_action_that_returns_hard_error_with_message(message: str):
+    def f() -> ExitCodeOrHardError:
+        return new_eh_hard_error(new_failure_details_from_message(message))
 
     return f
 
