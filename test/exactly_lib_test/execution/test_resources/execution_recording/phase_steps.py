fa462b14023cea90ec9bbbb9ee48d0ceca30103e
Emil Karlén <emil.karlen@fripost.org>
2016-10-04 18:49:22 +0200

    act-phase-handling: Updates tests to include (previously excluded step) ACT/validate-pre-eds

--- test/exactly_lib_test/execution/test_resources/execution_recording/phase_steps.py
+++ test/exactly_lib_test/execution/test_resources/execution_recording/phase_steps.py
@@ -3,7 +3,7 @@ from exactly_lib.execution import phase_step_simple as phase_step
 PRE_EDS_VALIDATION_STEPS__TWICE = [
     phase_step.SETUP__VALIDATE_PRE_EDS,
     phase_step.SETUP__VALIDATE_PRE_EDS,
-    # phase_step.ACT__VALIDATE_PRE_EDS,
+    phase_step.ACT__VALIDATE_PRE_EDS,
     phase_step.BEFORE_ASSERT__VALIDATE_PRE_EDS,
     phase_step.BEFORE_ASSERT__VALIDATE_PRE_EDS,
     phase_step.ASSERT__VALIDATE_PRE_EDS,
