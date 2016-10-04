394d0c9dfe111cc94bf3ae4c0bbd46b2ce233e89
Emil Karlén <emil@member.fsf.org>
2016-10-04 19:54:41 +0200

    act-phase-handling: partial execution: Failing test: test_hard_error_in_validate_pre_eds

--- test/exactly_lib_test/execution/partial_execution/phase_step_sequence/act_phase_handling_failure.py
+++ test/exactly_lib_test/execution/partial_execution/phase_step_sequence/act_phase_handling_failure.py
@@ -3,6 +3,7 @@ import unittest
 from exactly_lib.execution import phase_step_simple as phase_step
 from exactly_lib.execution.result import PartialResultStatus
 from exactly_lib.test_case.phases.cleanup import PreviousPhase
+from exactly_lib.test_case.phases.result import svh
 from exactly_lib_test.execution.partial_execution.test_resources.recording.test_case_generation_for_sequence_tests import \
     TestCaseGeneratorThatRecordsExecutionWithExtraInstructionList, \
     TestCaseGeneratorForExecutionRecording
@@ -13,7 +14,7 @@ from exactly_lib_test.execution.test_resources.execution_recording.phase_steps i
     PRE_EDS_VALIDATION_STEPS__ONCE
 from exactly_lib_test.execution.test_resources.test_actions import execute_action_that_raises, \
     execute_action_that_returns_hard_error_with_message, \
-    prepare_action_that_returns_hard_error_with_message
+    prepare_action_that_returns_hard_error_with_message, validate_action_that_returns
 from exactly_lib_test.test_resources.expected_instruction_failure import ExpectedFailureForPhaseFailure
 
 
@@ -26,6 +27,22 @@ if __name__ == '__main__':
 
 
 class Test(TestCaseBase):
+    def test_hard_error_in_validate_pre_eds(self):
+        test_case = _single_successful_instruction_in_each_phase()
+        self._check(
+            Arrangement(test_case,
+                        act_executor_validate_pre_eds=validate_action_that_returns(
+                            svh.new_svh_hard_error('error in act/validate-pre-eds'))),
+            Expectation(PartialResultStatus.HARD_ERROR,
+                        ExpectedFailureForPhaseFailure.new_with_message(
+                            phase_step.ACT__VALIDATE_PRE_EDS,
+                            'error in act/validate-pre-eds'),
+                        [
+                            phase_step.SETUP__VALIDATE_PRE_EDS,
+                            phase_step.ACT__VALIDATE_PRE_EDS,
+                        ],
+                        True))
+
     def test_hard_error_in_prepare(self):
         test_case = _single_successful_instruction_in_each_phase()
         self._check(
