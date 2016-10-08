8fae1debfb68536856ed29af013ef24043fd5f90
Emil Karlén <emil.karlen@fripost.org>
2016-10-08 10:47:51 +0200

    Moves SourceCodeInstruction to module "act_phase_source_parser"

--- src/exactly_lib/processing/parse/act_phase_source_parser.py
+++ src/exactly_lib/processing/parse/act_phase_source_parser.py
@@ -1,8 +1,11 @@
 from exactly_lib.section_document import model
 from exactly_lib.section_document import parse
 from exactly_lib.section_document import syntax
-from exactly_lib.test_case.phases.act.instruction import SourceCodeInstruction
+from exactly_lib.test_case.phases import common
+from exactly_lib.test_case.phases.act.instruction import ActPhaseInstruction, PhaseEnvironmentForScriptGeneration
+from exactly_lib.test_case.phases.result import sh
 from exactly_lib.util import line_source
+from exactly_lib.util.line_source import LineSequence
 
 
 class PlainSourceActPhaseParser(parse.SectionElementParser):
@@ -16,3 +19,17 @@ class PlainSourceActPhaseParser(parse.SectionElementParser):
         return model.SectionContentElement(model.ElementType.INSTRUCTION,
                                            line_sequence,
                                            SourceCodeInstruction(line_sequence))
+
+
+class SourceCodeInstruction(ActPhaseInstruction):
+    def __init__(self,
+                 source_code: LineSequence):
+        self._source_code = source_code
+
+    def source_code(self) -> LineSequence:
+        return self._source_code
+
+    def main(self, global_environment: common.GlobalEnvironmentForPostEdsPhase,
+             script_generator: PhaseEnvironmentForScriptGeneration) -> sh.SuccessOrHardError:
+        script_generator.append.raw_script_statement(self._source_code.text)
+        return sh.new_sh_success()
