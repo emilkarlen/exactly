b183b2063c83a00ff0e4f7c8b21919bf6ea170e7
Emil Karlén <emil@member.fsf.org>
2016-10-16 22:18:13 +0200

    act-phase-handling: Makes some parts of python3 act phase handling private

--- src/exactly_lib/act_phase_setups/script_interpretation/python3.py
+++ src/exactly_lib/act_phase_setups/script_interpretation/python3.py
@@ -1,34 +1,30 @@
 import sys
 
-import exactly_lib.act_phase_setups.script_interpretation.script_language_management
 from exactly_lib.act_phase_setups.script_interpretation import script_language_management
 from exactly_lib.act_phase_setups.script_interpretation.script_language_management import ScriptLanguage
+from exactly_lib.act_phase_setups.script_interpretation.script_language_management import StandardScriptFileManager
 from exactly_lib.act_phase_setups.script_interpretation.script_language_setup import new_for_script_language_setup
 from exactly_lib.processing.act_phase import ActPhaseSetup
 
 
 def script_language_setup() -> script_language_management.ScriptLanguageSetup:
-    return script_language_management.ScriptLanguageSetup(file_manager(),
-                                                          language())
+    return script_language_management.ScriptLanguageSetup(_file_manager(),
+                                                          _PythonScriptLanguage())
 
 
-def language() -> ScriptLanguage:
-    return PythonScriptLanguage()
-
-
-def file_manager() -> script_language_management.ScriptFileManager:
+def _file_manager() -> script_language_management.ScriptFileManager:
     if not sys.executable:
         raise ValueError('Cannot execute since name of executable not found in sys.executable.')
-    return exactly_lib.act_phase_setups.script_interpretation.script_language_management.StandardScriptFileManager('py',
-                                                                                                                   sys.executable,
-                                                                                                                   [])
+    return StandardScriptFileManager('py',
+                                     sys.executable,
+                                     [])
 
 
 def new_act_phase_setup() -> ActPhaseSetup:
     return new_for_script_language_setup(script_language_setup())
 
 
-class PythonScriptLanguage(ScriptLanguage):
+class _PythonScriptLanguage(ScriptLanguage):
     def comment_line(self, comment: str) -> list:
         return ['# ' + comment]
 
