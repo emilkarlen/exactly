ef51dbef5dd643b916ff3b4505238951ae446fd6
Emil Karlén <emil.karlen@fripost.org>
2016-10-16 19:02:13 +0200

    act-phase-handling: single_command_setup: Removes deprecated code

--- src/exactly_lib/act_phase_setups/single_command_setup.py
+++ src/exactly_lib/act_phase_setups/single_command_setup.py
@@ -1,37 +1,19 @@
-import os
 import pathlib
-import shlex
 
 from exactly_lib.act_phase_setups import utils
 from exactly_lib.act_phase_setups.util.executor_made_of_parts import main as executor_made_of_parts
 from exactly_lib.act_phase_setups.util.executor_made_of_parts.parser_for_single_line import \
     ParserForSingleLineUsingStandardSyntaxSplitAccordingToShellSyntax
-from exactly_lib.execution.act_phase import SourceSetup, ActSourceExecutor, ExitCodeOrHardError
-from exactly_lib.execution.execution_directory_structure import ExecutionDirectoryStructure
-from exactly_lib.instructions.act.executable_file import ExecutableFileInstruction
+from exactly_lib.execution.act_phase import ExitCodeOrHardError
 from exactly_lib.processing.act_phase import ActPhaseSetup
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SectionElementParserForStandardCommentAndEmptyLines
-from exactly_lib.test_case.phases.act.instruction import ActPhaseInstruction
-from exactly_lib.test_case.phases.act.program_source import ActSourceBuilder, ActSourceBuilderForStatementLines
 from exactly_lib.test_case.phases.common import HomeAndEds
 from exactly_lib.test_case.phases.result import sh
 from exactly_lib.test_case.phases.result import svh
-from exactly_lib.util import line_source
 from exactly_lib.util.std import StdFiles
-
-
-class _ActPhaseParser(SectionElementParserForStandardCommentAndEmptyLines):
-    def _parse_instruction(self, source: line_source.LineSequenceBuilder) -> ActPhaseInstruction:
-        return ExecutableFileInstruction(source.first_line.text)
 
 
 def act_phase_setup() -> ActPhaseSetup:
     return ActPhaseSetup(Constructor())
-
-
-def _script_source_builder() -> ActSourceBuilder:
-    return ActSourceBuilderForStatementLines()
 
 
 class Constructor(executor_made_of_parts.Constructor):
@@ -76,42 +58,4 @@ class Executor(executor_made_of_parts.Executor):
             cmd_path = home_and_eds.home_dir_path / cmd_path
             self.cmd_and_args[0] = str(cmd_path)
         return utils.execute_cmd_and_args(self.cmd_and_args,
-                                          std_files)
-
-
-class ActSourceExecutorForSingleCommand(ActSourceExecutor):
-    def validate_pre_eds(self,
-                         script_builder: ActSourceBuilder,
-                         home_dir_path: pathlib.Path) -> svh.SuccessOrValidationErrorOrHardError:
-        return svh.new_svh_success()
-
-    def validate(self,
-                 home_dir: pathlib.Path,
-                 source: ActSourceBuilder) -> svh.SuccessOrValidationErrorOrHardError:
-        num_source_lines = len(source.source_lines)
-        if num_source_lines != 1:
-            header = 'There must be a single command. Found {}'.format(num_source_lines)
-            msg = header
-            if num_source_lines > 0:
-                msg = os.linesep.join([header + ':'] + source.source_lines)
-            return svh.new_svh_validation_error(msg)
-        if source.source_lines[0].isspace():
-            msg = 'command is only white space'
-            return svh.new_svh_validation_error(msg)
-        return svh.new_svh_success()
-
-    def prepare(self,
-                source_setup: SourceSetup,
-                home_dir_path: pathlib.Path,
-                eds: ExecutionDirectoryStructure) -> sh.SuccessOrHardError:
-        return sh.new_sh_success()
-
-    def execute(self,
-                source_setup: SourceSetup,
-                home_dir: pathlib.Path,
-                eds: ExecutionDirectoryStructure,
-                std_files: StdFiles) -> ExitCodeOrHardError:
-        command_string = source_setup.script_builder.source_lines[0]
-        cmd_and_args = shlex.split(command_string)
-        return utils.execute_cmd_and_args(cmd_and_args,
                                           std_files)
