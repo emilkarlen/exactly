03563842622329c1b0f9041fb4984594f81ee99d
Emil Karlén <emil@member.fsf.org>
2016-10-16 12:23:22 +0200

    act-phase-handling: Organizes some tests into new package structure

---
+++ test/exactly_lib_test/default/program_modes/test_case/act_phase.py
@@ -0,0 +1,51 @@
+import unittest
+
+from exactly_lib.cli.cli_environment.program_modes.test_case import exit_values
+from exactly_lib.execution import phases
+from exactly_lib.util.string import lines_content
+from exactly_lib_test.default.test_resources.internal_main_program_runner import RunViaMainProgramInternally
+from exactly_lib_test.default.test_resources.test_case_file_elements import phase_header_line
+from exactly_lib_test.test_resources import value_assertion as va
+from exactly_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_without_preprocessor
+from exactly_lib_test.test_resources.main_program.main_program_check_for_test_case import \
+    SetupWithoutPreprocessorAndTestActor
+from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
+from exactly_lib_test.test_resources.process_result_info_assertions import process_result_for_exit_value
+
+
+def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(tests_for_setup_without_preprocessor(TESTS, main_program_runner))
+    return ret_val
+
+
+def suite() -> unittest.TestSuite:
+    return suite_for(RunViaMainProgramInternally())
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
+
+
+class EmptyTestCaseShouldPass(SetupWithoutPreprocessorAndTestActor):
+    def expected_result(self) -> va.ValueAssertion:
+        return process_result_for_exit_value(exit_values.EXECUTION__PASS)
+
+    def test_case(self) -> str:
+        return ''
+
+
+class AllPhasesEmptyShouldPass(SetupWithoutPreprocessorAndTestActor):
+    def test_case(self) -> str:
+        test_case_lines = [phase_header_line(phase)
+                           for phase in phases.ALL]
+        return lines_content(test_case_lines)
+
+    def expected_result(self) -> va.ValueAssertion:
+        return process_result_for_exit_value(exit_values.EXECUTION__PASS)
+
+
+TESTS = [
+    EmptyTestCaseShouldPass(),
+    AllPhasesEmptyShouldPass(),
+]
---
+++ test/exactly_lib_test/default/program_modes/test_case/preprocessing.py
@@ -0,0 +1,25 @@
+import unittest
+
+from exactly_lib_test.default.test_resources import default_main_program_case_preprocessing
+from exactly_lib_test.default.test_resources.internal_main_program_runner import RunViaMainProgramInternally
+from exactly_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_with_preprocessor
+from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
+
+
+def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(tests_for_setup_with_preprocessor(TESTS, main_program_runner))
+    return ret_val
+
+
+def suite() -> unittest.TestSuite:
+    return suite_for(RunViaMainProgramInternally())
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
+
+TESTS = [
+    default_main_program_case_preprocessing.TransformationIntoTestCaseThatPass(),
+    default_main_program_case_preprocessing.TransformationIntoTestCaseThatParserError(),
+]
