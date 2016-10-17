92fda17ea26f18809a8dec7485a6f8d0f591f88b
Emil Karlén <emil@member.fsf.org>
2016-10-17 18:41:28 +0200

    act-phase-handling: python3: Removes unused class _ScriptLanguage

--- src/exactly_lib/act_phase_setups/script_interpretation/python3.py
+++ src/exactly_lib/act_phase_setups/script_interpretation/python3.py
@@ -1,7 +1,6 @@
 import sys
 
 from exactly_lib.act_phase_setups.script_interpretation import script_language_management
-from exactly_lib.act_phase_setups.script_interpretation.script_language_management import ScriptLanguage
 from exactly_lib.act_phase_setups.script_interpretation.script_language_management import StandardScriptFileManager
 from exactly_lib.act_phase_setups.script_interpretation.script_language_setup import new_for_script_language_setup
 from exactly_lib.processing.act_phase import ActPhaseSetup
@@ -21,11 +20,3 @@ def _file_manager() -> script_language_management.ScriptFileManager:
 
 def new_act_phase_setup() -> ActPhaseSetup:
     return new_for_script_language_setup(script_language_setup())
-
-
-class _PythonScriptLanguage(ScriptLanguage):
-    def comment_line(self, comment: str) -> list:
-        return ['# ' + comment]
-
-    def raw_script_statement(self, statement: str) -> list:
-        return [statement]
