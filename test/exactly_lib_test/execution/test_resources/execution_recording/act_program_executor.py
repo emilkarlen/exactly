162e5313371a65fca6e613d45197f3a845a1475b
Emil Karlén <emil.karlen@fripost.org>
2016-10-17 19:23:21 +0200

    act-phase-handling: Removes unused class: ActSourceExecutorWrapperThatRecordsSteps

--- test/exactly_lib_test/execution/test_resources/execution_recording/act_program_executor.py
+++ test/exactly_lib_test/execution/test_resources/execution_recording/act_program_executor.py
@@ -12,44 +12,6 @@ from exactly_lib.util.std import StdFiles
 from exactly_lib_test.execution.test_resources.execution_recording.recorder import ListRecorder
 
 
-class ActSourceExecutorWrapperThatRecordsSteps(ActSourceExecutor):
-    def __init__(self,
-                 recorder: ListRecorder,
-                 wrapped: ActSourceExecutor):
-        self.__recorder = recorder
-        self.__wrapped = wrapped
-
-    def validate_pre_eds(self,
-                         script_builder: ActSourceBuilder,
-                         home_dir_path: pathlib.Path) -> svh.SuccessOrValidationErrorOrHardError:
-        self.__recorder.recording_of(phase_step.ACT__VALIDATE_PRE_EDS).record()
-        return self.__wrapped.validate_pre_eds(script_builder, home_dir_path)
-
-    def validate(self,
-                 home_dir: pathlib.Path,
-                 source: ActSourceBuilder) -> svh.SuccessOrValidationErrorOrHardError:
-        self.__recorder.recording_of(phase_step.ACT__VALIDATE_POST_SETUP).record()
-        return self.__wrapped.validate(home_dir, source)
-
-    def prepare(self,
-                source_setup: SourceSetup,
-                home_dir_path: pathlib.Path,
-                eds: ExecutionDirectoryStructure) -> sh.SuccessOrHardError:
-        self.__recorder.recording_of(phase_step.ACT__PREPARE).record()
-        return self.__wrapped.prepare(source_setup, home_dir_path, eds)
-
-    def execute(self,
-                source_setup: SourceSetup,
-                home_dir: pathlib.Path,
-                eds: ExecutionDirectoryStructure,
-                std_files: StdFiles) -> ExitCodeOrHardError:
-        self.__recorder.recording_of(phase_step.ACT__EXECUTE).record()
-        return self.__wrapped.execute(source_setup,
-                                      home_dir,
-                                      eds,
-                                      std_files)
-
-
 def do_nothing():
     pass
 
