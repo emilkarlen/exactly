e9f87c16426bec60b50a6daa0459a9ac339faf83
Emil Karlén <emil@member.fsf.org>
2016-10-19 21:46:33 +0200

    act-phase-handling: Removes deprecated class: PhaseEnvironmentForScriptGeneration

--- src/exactly_lib/test_case/phases/act/instruction.py
+++ src/exactly_lib/test_case/phases/act/instruction.py
@@ -1,20 +1,5 @@
-from exactly_lib.test_case.phases.act.program_source import ScriptSourceAccumulator
 from exactly_lib.test_case.phases.common import TestCaseInstruction
 from exactly_lib.util.line_source import LineSequence
-
-
-class PhaseEnvironmentForScriptGeneration:
-    """
-    The phase-environment for phases that generate a script.
-    """
-
-    def __init__(self,
-                 script_source_accumulator: ScriptSourceAccumulator):
-        self.__script_source_accumulator = script_source_accumulator
-
-    @property
-    def append(self) -> ScriptSourceAccumulator:
-        return self.__script_source_accumulator
 
 
 class ActPhaseInstruction(TestCaseInstruction):
