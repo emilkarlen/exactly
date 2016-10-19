86c4d9d633432673ef6b5359c04c7b12c9e48f7f
Emil Karlén <emil@member.fsf.org>
2016-10-19 21:15:45 +0200

    act-phase-handling: Removes deprecated methods from ActPhaseInstruction

--- src/exactly_lib/test_case/phases/act/instruction.py
+++ src/exactly_lib/test_case/phases/act/instruction.py
@@ -1,8 +1,5 @@
 from exactly_lib.test_case.phases.act.program_source import ScriptSourceAccumulator
-from exactly_lib.test_case.phases.common import GlobalEnvironmentForPostEdsPhase, TestCaseInstruction, \
-    GlobalEnvironmentForPreEdsStep
-from exactly_lib.test_case.phases.result import svh
-from exactly_lib.test_case.phases.result.sh import SuccessOrHardError
+from exactly_lib.test_case.phases.common import TestCaseInstruction
 from exactly_lib.util.line_source import LineSequence
 
 
@@ -26,24 +23,4 @@ class ActPhaseInstruction(TestCaseInstruction):
     """
 
     def source_code(self) -> LineSequence:
-        raise NotImplementedError()
-
-    def validate_pre_eds(self,
-                         environment: GlobalEnvironmentForPreEdsStep) -> svh.SuccessOrValidationErrorOrHardError:
-        return svh.new_svh_success()
-
-    def validate_post_setup(self,
-                            global_environment: GlobalEnvironmentForPostEdsPhase) -> svh.SuccessOrValidationErrorOrHardError:
-        return svh.new_svh_success()
-
-    def main(self,
-             global_environment: GlobalEnvironmentForPostEdsPhase,
-             phase_environment: PhaseEnvironmentForScriptGeneration) -> SuccessOrHardError:
-        """
-        Builds the script, and sets some execution premises (e.g. stdin),
-        by updating the phase environment.
-
-        :param global_environment An object passed to all instructions in the Document.
-        :param phase_environment An object passed to all instructions in the Phase.
-        """
         raise NotImplementedError()
