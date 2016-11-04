e8081cb5d982c6f409e1c8c42a499e2296ac8043
Emil Karlén <emil@member.fsf.org>
2016-11-04 21:40:44 +0100

    timeout: Moves test utilities to separate module

---
+++ test/exactly_lib_test/execution/test_resources/execution_recording/recording2.py
@@ -0,0 +1,82 @@
+"""
+Utilities to record an attribute associated to execution steps to a dictionary
+"""
+import types
+
+from exactly_lib.execution import phase_step_simple as step
+from exactly_lib.execution.act_phase import ActPhaseHandling
+from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
+from exactly_lib.test_case.phases.before_assert import BeforeAssertPhaseInstruction
+from exactly_lib.test_case.phases.cleanup import CleanupPhaseInstruction
+from exactly_lib.test_case.phases.setup import SetupPhaseInstruction
+from exactly_lib_test.execution.test_resources.act_source_executor import \
+    ActSourceAndExecutorConstructorThatRunsConstantActions
+from exactly_lib_test.execution.test_resources.instruction_test_resources import setup_phase_instruction_that, \
+    assert_phase_instruction_that, before_assert_phase_instruction_that, cleanup_phase_instruction_that
+from exactly_lib_test.test_case.test_resources.test_case_doc import TestCaseWithOnlyInstructionElementsBuilder
+
+
+class PropertyRecorderBuilder:
+    """
+    Builder for recording to a given recorder, a given property,
+    for different keys.
+    """
+
+    def __init__(self, property_getter: types.FunctionType, recorder: dict):
+        self.property_getter = property_getter
+        self.recorder = recorder
+
+    def of_first_arg(self, key) -> types.FunctionType:
+        def ret_val(first_arg, *args, **kwargs):
+            self.recorder[key] = self.property_getter(first_arg)
+
+        return ret_val
+
+
+def _test_case_that_records_for_each_step_of_partial_execution(
+        property_recorder: PropertyRecorderBuilder) -> TestCaseWithOnlyInstructionElementsBuilder:
+    builder = TestCaseWithOnlyInstructionElementsBuilder()
+    builder.setup_phase = [setup_phase_instruction_that_records_for_each_step(property_recorder)]
+    builder.before_assert_phase = [before_assert_phase_instruction_that_records_for_each_step(property_recorder)]
+    builder.assert_phase = [assert_phase_instruction_that_records_for_each_step(property_recorder)]
+    builder.cleanup_phase = [cleanup_phase_instruction_that_records_for_each_step(property_recorder)]
+    return builder
+
+
+def act_phase_handling_that_records_property_of_constructor_argument(
+        key: str,
+        property_recorder: PropertyRecorderBuilder) -> ActPhaseHandling:
+    return ActPhaseHandling(ActSourceAndExecutorConstructorThatRunsConstantActions(
+        apply_action_before_executor_is_constructed=property_recorder.of_first_arg(key)
+    ))
+
+
+def setup_phase_instruction_that_records_for_each_step(
+        property_recorder: PropertyRecorderBuilder) -> SetupPhaseInstruction:
+    return setup_phase_instruction_that(
+        validate_pre_eds_initial_action=property_recorder.of_first_arg(step.SETUP__VALIDATE_PRE_EDS),
+        validate_post_setup_initial_action=property_recorder.of_first_arg(step.SETUP__VALIDATE_POST_SETUP),
+        main_initial_action=property_recorder.of_first_arg(step.SETUP__MAIN))
+
+
+def before_assert_phase_instruction_that_records_for_each_step(
+        property_recorder: PropertyRecorderBuilder) -> BeforeAssertPhaseInstruction:
+    return before_assert_phase_instruction_that(
+        validate_pre_eds_initial_action=property_recorder.of_first_arg(step.BEFORE_ASSERT__VALIDATE_PRE_EDS),
+        validate_post_setup_initial_action=property_recorder.of_first_arg(step.BEFORE_ASSERT__VALIDATE_POST_SETUP),
+        main_initial_action=property_recorder.of_first_arg(step.BEFORE_ASSERT__MAIN))
+
+
+def assert_phase_instruction_that_records_for_each_step(
+        property_recorder: PropertyRecorderBuilder) -> AssertPhaseInstruction:
+    return assert_phase_instruction_that(
+        validate_pre_eds_initial_action=property_recorder.of_first_arg(step.ASSERT__VALIDATE_PRE_EDS),
+        validate_post_setup_initial_action=property_recorder.of_first_arg(step.ASSERT__VALIDATE_POST_SETUP),
+        main_initial_action=property_recorder.of_first_arg(step.ASSERT__MAIN))
+
+
+def cleanup_phase_instruction_that_records_for_each_step(
+        property_recorder: PropertyRecorderBuilder) -> CleanupPhaseInstruction:
+    return cleanup_phase_instruction_that(
+        validate_pre_eds_initial_action=property_recorder.of_first_arg(step.CLEANUP__VALIDATE_PRE_EDS),
+        main_initial_action=property_recorder.of_first_arg(step.CLEANUP__MAIN))
