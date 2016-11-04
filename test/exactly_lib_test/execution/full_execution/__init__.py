5ab3814fc592fb0b75e9e939b112894b4f440ce7
Emil Karlén <emil.karlen@fripost.org>
2016-11-04 21:40:58 +0100

    timeout: Adds new tests to suite

--- test/exactly_lib_test/execution/full_execution/__init__.py
+++ test/exactly_lib_test/execution/full_execution/__init__.py
@@ -3,7 +3,8 @@ import unittest
 from exactly_lib_test.execution.full_execution import execution_mode
 from exactly_lib_test.execution.full_execution import \
     test_translation_of_partial_result_to_full_result, \
-    test_environment
+    test_environment, \
+    propagation_of_timeout_to_phases
 
 
 def suite():
@@ -11,6 +12,7 @@ def suite():
     ret_val.addTest(unittest.makeSuite(test_translation_of_partial_result_to_full_result.Test))
     ret_val.addTest(unittest.makeSuite(test_environment.Test))
     ret_val.addTest(execution_mode.suite())
+    ret_val.addTest(propagation_of_timeout_to_phases.suite())
     return ret_val
 
 
