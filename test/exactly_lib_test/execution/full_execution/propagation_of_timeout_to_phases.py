37ac7c2b3b9fc0be4ed2f090075e5a282cab3e23
Emil Karlén <emil.karlen@fripost.org>
2016-11-04 21:24:44 +0100

    timeout: full_execution: test_WHEN_an_instruction_sets_timeout_THEN_the_that_timeout_SHOULD_be_propagated_to_instructions_and_act_phase

--- test/exactly_lib_test/execution/full_execution/propagation_of_timeout_to_phases.py
+++ test/exactly_lib_test/execution/full_execution/propagation_of_timeout_to_phases.py
@@ -5,12 +5,12 @@ import unittest
 from exactly_lib.execution import phase_step_simple as step
 from exactly_lib.execution.act_phase import ActPhaseHandling
 from exactly_lib.execution.phase_step_simple import ALL_SETUP, ALL_ASSERT, ALL_BEFORE_ASSERT, ALL_CLEANUP
-from exactly_lib.test_case import test_case_doc
 from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
 from exactly_lib.test_case.phases.before_assert import BeforeAssertPhaseInstruction
 from exactly_lib.test_case.phases.cleanup import CleanupPhaseInstruction
 from exactly_lib.test_case.phases.common import GlobalEnvironmentForPreEdsStep
-from exactly_lib.test_case.phases.configuration import ConfigurationBuilder
+from exactly_lib.test_case.phases.configuration import ConfigurationBuilder, ConfigurationPhaseInstruction
+from exactly_lib.test_case.phases.result import sh
 from exactly_lib.test_case.phases.setup import SetupPhaseInstruction
 from exactly_lib_test.execution.full_execution.test_resources import execution_check
 from exactly_lib_test.execution.test_resources import full_result_assertions
@@ -37,7 +37,7 @@ class Test(unittest.TestCase):
         recorder_builder = PropertyRecorderBuilder(
             GlobalEnvironmentForPreEdsStep.timeout_in_seconds.fget,
             actual_recordings)
-        test_case = _test_case_that_records_for_each_step(recorder_builder)
+        test_case = _test_case_that_records_for_each_step_of_partial_execution(recorder_builder).build()
         act_phase_handling = _act_phase_handling_that_records_property_of_constructor_argument(ACT_EXE_CONSTRUCTOR,
                                                                                                recorder_builder)
         configuration_builder_with_default_timeout = ConfigurationBuilder(_current_dir(),
@@ -57,6 +57,45 @@ class Test(unittest.TestCase):
                                             default_timeout)
         self.assertDictEqual(expected_recordings, actual_recordings)
 
+    def test_WHEN_an_instruction_sets_timeout_THEN_the_that_timeout_SHOULD_be_propagated_to_instructions_and_act_phase(
+            self):
+        # ARRANGE #
+        expected_timeout = 87
+        actual_recordings = {}
+        recorder_builder = PropertyRecorderBuilder(
+            GlobalEnvironmentForPreEdsStep.timeout_in_seconds.fget,
+            actual_recordings)
+        test_case_builder = _test_case_that_records_for_each_step_of_partial_execution(recorder_builder)
+        test_case_builder.configuration_phase = [_ConfigurationPhaseInstructionThatSetsTimeoutTo(expected_timeout)]
+        test_case = test_case_builder.build()
+        act_phase_handling = _act_phase_handling_that_records_property_of_constructor_argument(ACT_EXE_CONSTRUCTOR,
+                                                                                               recorder_builder)
+        configuration_builder_with_default_timeout = ConfigurationBuilder(_current_dir(),
+                                                                          act_phase_handling,
+                                                                          expected_timeout)
+        arrangement = execution_check.Arrangement(test_case,
+                                                  configuration_builder_with_default_timeout)
+        expectation = execution_check.Expectation(full_result=full_result_assertions.is_pass)
+        # ACT & ASSERT #
+        execution_check.check(self, arrangement, expectation)
+        # ASSERT #
+        expected_recordings = dict.fromkeys(ALL_SETUP +
+                                            (ACT_EXE_CONSTRUCTOR,) +
+                                            ALL_BEFORE_ASSERT +
+                                            ALL_ASSERT +
+                                            ALL_CLEANUP,
+                                            expected_timeout)
+        self.assertDictEqual(expected_recordings, actual_recordings)
+
+
+class _ConfigurationPhaseInstructionThatSetsTimeoutTo(ConfigurationPhaseInstruction):
+    def __init__(self, timeout):
+        self.timeout = timeout
+
+    def main(self, configuration_builder: ConfigurationBuilder) -> sh.SuccessOrHardError:
+        configuration_builder.set_timeout_in_seconds(self.timeout)
+        return sh.new_sh_success()
+
 
 class PropertyRecorderBuilder:
     """
@@ -75,13 +114,14 @@ class PropertyRecorderBuilder:
         return ret_val
 
 
-def _test_case_that_records_for_each_step(property_recorder: PropertyRecorderBuilder) -> test_case_doc.TestCase:
+def _test_case_that_records_for_each_step_of_partial_execution(
+        property_recorder: PropertyRecorderBuilder) -> TestCaseWithOnlyInstructionElementsBuilder:
     builder = TestCaseWithOnlyInstructionElementsBuilder()
     builder.setup_phase = [setup_phase_instruction_that_records_for_each_step(property_recorder)]
     builder.before_assert_phase = [before_assert_phase_instruction_that_records_for_each_step(property_recorder)]
     builder.assert_phase = [assert_phase_instruction_that_records_for_each_step(property_recorder)]
     builder.cleanup_phase = [cleanup_phase_instruction_that_records_for_each_step(property_recorder)]
-    return builder.build()
+    return builder
 
 
 def _act_phase_handling_that_records_property_of_constructor_argument(
