99ac78e3ef5bcc21a4a572af2c0ba371aa28a933
Emil Karlén <emil@member.fsf.org>
2016-11-05 19:46:21 +0100

    timeout: Rewrite of assert/run using new infrastructure for instruction from validator and executor

---
+++ src/exactly_lib/instructions/assert_/utils/instruction_from_parts.py
@@ -0,0 +1,30 @@
+from exactly_lib.instructions.utils.main_step_executor import InstructionParts
+from exactly_lib.instructions.utils.pre_or_post_validation import PreOrPostEdsSvhValidationErrorValidator
+from exactly_lib.test_case.os_services import OsServices
+from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
+from exactly_lib.test_case.phases.common import InstructionEnvironmentForPreSdsStep, \
+    InstructionEnvironmentForPostSdsStep
+from exactly_lib.test_case.phases.result import pfh
+from exactly_lib.test_case.phases.result import svh
+
+
+class AssertPhaseInstructionFromValidatorAndExecutor(AssertPhaseInstruction):
+    def __init__(self,
+                 instruction_setup: InstructionParts):
+        self.setup = instruction_setup
+        self._validator = PreOrPostEdsSvhValidationErrorValidator(instruction_setup.validator)
+
+    def validate_pre_sds(self,
+                         environment: InstructionEnvironmentForPreSdsStep) -> svh.SuccessOrValidationErrorOrHardError:
+        return self._validator.validate_pre_eds_if_applicable(environment.home_directory)
+
+    def validate_post_setup(self,
+                            environment: InstructionEnvironmentForPostSdsStep) -> svh.SuccessOrValidationErrorOrHardError:
+        return self._validator.validate_post_eds_if_applicable(environment.sds)
+
+    def main(self,
+             environment: InstructionEnvironmentForPostSdsStep,
+             os_services: OsServices) -> pfh.PassOrFailOrHardError:
+        return self.setup.executor.apply_pfh(environment,
+                                             self.logging_paths(environment.sds),
+                                             os_services)
