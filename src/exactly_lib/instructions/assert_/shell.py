e391eeac7ab7eda8f58a1ea225015365abd8c9c2
Emil Karlén <emil@member.fsf.org>
2016-11-05 19:47:35 +0100

    timeout: Rewrite of assert/shell using new infrastructure for instruction from validator and executor

--- src/exactly_lib/instructions/assert_/shell.py
+++ src/exactly_lib/instructions/assert_/shell.py
@@ -1,18 +1,12 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
+from exactly_lib.instructions.assert_.utils.instruction_from_parts import AssertPhaseInstructionFromValidatorAndExecutor
 from exactly_lib.instructions.multi_phase_instructions import shell as shell_common
 from exactly_lib.instructions.multi_phase_instructions.shell import TheInstructionDocumentationBase
-from exactly_lib.instructions.utils.sub_process_execution import ExecuteInfo
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionParser
-from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
-from exactly_lib.test_case.phases.result import pfh
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        parser(instruction_name),
+        shell_common.Parser2(instruction_name, AssertPhaseInstructionFromValidatorAndExecutor),
         TheDescription(instruction_name))
 
 
@@ -27,19 +21,3 @@ class TheDescription(TheInstructionDocumentationBase):
             All other exit codes makes the assertion FAIL.
             """
         return self._paragraphs(text)
-
-
-def parser(instruction_name: str) -> SingleInstructionParser:
-    return shell_common.Parser(instruction_name,
-                               _ShellInstruction)
-
-
-class _ShellInstruction(AssertPhaseInstruction):
-    def __init__(self,
-                 execute_info: ExecuteInfo):
-        self.execute_info = execute_info
-
-    def main(self,
-             environment: InstructionEnvironmentForPostSdsStep,
-             os_services: OsServices) -> pfh.PassOrFailOrHardError:
-        return shell_common.run_and_return_pfh(self.execute_info, environment.phase_logging)
