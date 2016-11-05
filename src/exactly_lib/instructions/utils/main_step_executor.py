15126d4e78e73d1ff5c884637753f5c7d1e1f51e
Emil Karlén <emil.karlen@fripost.org>
2016-11-05 11:23:30 +0100

    instruction classes utils: Merge master

--- src/exactly_lib/instructions/utils/main_step_executor.py
+++ src/exactly_lib/instructions/utils/main_step_executor.py
@@ -1,5 +1,5 @@
 from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.common import GlobalEnvironmentForPostEdsPhase
+from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
 from exactly_lib.test_case.phases.result import pfh
 from exactly_lib.test_case.phases.result import sh
 
@@ -10,11 +10,11 @@ class MainStepExecutor:
     """
 
     def apply_sh(self,
-                 environment: GlobalEnvironmentForPostEdsPhase,
+                 environment: InstructionEnvironmentForPostSdsStep,
                  os_services: OsServices) -> sh.SuccessOrHardError:
         raise NotImplementedError()
 
     def apply_pfh(self,
-                  environment: GlobalEnvironmentForPostEdsPhase,
+                  environment: InstructionEnvironmentForPostSdsStep,
                   os_services: OsServices) -> pfh.PassOrFailOrHardError:
         raise NotImplementedError()
