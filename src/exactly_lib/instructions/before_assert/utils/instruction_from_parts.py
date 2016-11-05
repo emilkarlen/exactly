9fd26311722c84d41d4c5275c8b366d72c190a9f
Emil Karlén <emil@member.fsf.org>
2016-11-05 11:16:40 +0100

    instruction classes utils: before assert phase instruction from Validator and MainStepExecutor

---
+++ src/exactly_lib/instructions/before_assert/utils/instruction_from_parts.py
@@ -0,0 +1,29 @@
+from exactly_lib.instructions.utils.main_step_executor import MainStepExecutor
+from exactly_lib.instructions.utils.pre_or_post_validation import PreOrPostEdsSvhValidationErrorValidator
+from exactly_lib.test_case.os_services import OsServices
+from exactly_lib.test_case.phases.before_assert import BeforeAssertPhaseInstruction
+from exactly_lib.test_case.phases.common import GlobalEnvironmentForPostEdsPhase, \
+    GlobalEnvironmentForPreEdsStep
+from exactly_lib.test_case.phases.result import sh
+from exactly_lib.test_case.phases.result import svh
+
+
+class BeforeAssertPhaseInstructionFromParts(BeforeAssertPhaseInstruction):
+    def __init__(self,
+                 validator: PreOrPostEdsSvhValidationErrorValidator,
+                 executor: MainStepExecutor):
+        self.validator = validator
+        self.executor = executor
+
+    def validate_pre_eds(self,
+                         environment: GlobalEnvironmentForPreEdsStep) -> svh.SuccessOrValidationErrorOrHardError:
+        return self.validator.validate_pre_eds_if_applicable(environment.home_directory)
+
+    def validate_post_setup(self,
+                            environment: GlobalEnvironmentForPostEdsPhase) -> svh.SuccessOrValidationErrorOrHardError:
+        return self.validator.validate_post_eds_if_applicable(environment.eds)
+
+    def main(self,
+             environment: GlobalEnvironmentForPostEdsPhase,
+             os_services: OsServices) -> sh.SuccessOrHardError:
+        return self.executor.apply_sh(environment, os_services)
