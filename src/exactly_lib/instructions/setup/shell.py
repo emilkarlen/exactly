4fbe4a99b1d81916ea28e16b1f72f937c75b12ef
Emil Karlén <emil@member.fsf.org>
2016-11-05 19:58:54 +0100

    timeout: Rewrite of setup/shell,run using new infrastructure for instruction from validator and executor

--- src/exactly_lib/instructions/setup/shell.py
+++ src/exactly_lib/instructions/setup/shell.py
@@ -1,32 +1,9 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.instructions.multi_phase_instructions import shell as shell_common
-from exactly_lib.instructions.utils.sub_process_execution import ExecuteInfo
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionParser
-from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
-from exactly_lib.test_case.phases.result import sh
-from exactly_lib.test_case.phases.setup import SetupPhaseInstruction, SetupSettingsBuilder
+from exactly_lib.instructions.setup.utils.instruction_from_parts import SetupPhaseInstructionFromValidatorAndExecutor
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-            parser(instruction_name),
-            shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
-
-
-def parser(instruction_name: str) -> SingleInstructionParser:
-    return shell_common.Parser(instruction_name,
-                               _ShellInstruction)
-
-
-class _ShellInstruction(SetupPhaseInstruction):
-    def __init__(self,
-                 execute_info: ExecuteInfo):
-        self.execute_info = execute_info
-
-    def main(self,
-             environment: InstructionEnvironmentForPostSdsStep,
-             os_services: OsServices,
-             settings_builder: SetupSettingsBuilder) -> sh.SuccessOrHardError:
-        return shell_common.run_and_return_sh(self.execute_info, environment.phase_logging)
+        shell_common.Parser2(instruction_name, SetupPhaseInstructionFromValidatorAndExecutor),
+        shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
