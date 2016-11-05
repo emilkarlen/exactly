b4b0b8a587167aeb09d8aa78b3308a2fb50d47b2
Emil Karlén <emil@member.fsf.org>
2016-11-05 13:20:01 +0100

    Refactoring: Rename: ExecutionDirectoryStructure -> SandboxDirectoryStructure (module: sds_test)

--- test/exactly_lib_test/instructions/multi_phase_instructions/change_dir.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/change_dir.py
@@ -10,7 +10,7 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 from exactly_lib.test_case.sandbox_directory_structure import SandboxDirectoryStructure
 from exactly_lib_test.help.test_resources import CrossReferenceTextConstructorTestImpl
 from exactly_lib_test.instructions.test_resources.check_description import suite_for_instruction_documentation
-from exactly_lib_test.test_resources.execution import eds_test
+from exactly_lib_test.test_resources.execution import sds_test
 from exactly_lib_test.test_resources.execution.eds_populator import act_dir_contents, tmp_user_dir_contents
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir, Dir, empty_file
 from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion, ValueIsNotNone
@@ -83,7 +83,7 @@ class TestParseSet(unittest.TestCase):
             sut.parse(arguments)
 
 
-class ParseAndChangeDirAction(eds_test.Action):
+class ParseAndChangeDirAction(sds_test.Action):
     def __init__(self,
                  arguments: str):
         self.arguments = arguments
@@ -93,10 +93,10 @@ class ParseAndChangeDirAction(eds_test.Action):
         return sut.change_dir(destination_directory, eds)
 
 
-class TestCaseBase(eds_test.TestCaseBase):
+class TestCaseBase(sds_test.TestCaseBase):
     def _test_argument(self,
                        arguments: str,
-                       setup: eds_test.Check):
+                       setup: sds_test.Check):
         action = ParseAndChangeDirAction(arguments)
         self._check_action(action,
                            setup)
@@ -110,7 +110,7 @@ def is_failure() -> ValueAssertion:
     return ValueIsNotNone()
 
 
-class ChangeDirTo(eds_test.Action):
+class ChangeDirTo(sds_test.Action):
     def __init__(self, eds2dir_fun):
         self.eds2dir_fun = eds2dir_fun
 
@@ -118,14 +118,14 @@ class ChangeDirTo(eds_test.Action):
         os.chdir(str(self.eds2dir_fun(eds)))
 
 
-class CwdIsActDir(eds_test.PostActionCheck):
+class CwdIsActDir(sds_test.PostActionCheck):
     def apply(self, put: unittest.TestCase, eds: SandboxDirectoryStructure):
         put.assertEqual(str(eds.act_dir),
                         os.getcwd(),
                         'Current Working Directory')
 
 
-class CwdIsSubDirOfActDir(eds_test.PostActionCheck):
+class CwdIsSubDirOfActDir(sds_test.PostActionCheck):
     def __init__(self, sub_dir_name: str):
         self.sub_dir_name = sub_dir_name
 
@@ -135,7 +135,7 @@ class CwdIsSubDirOfActDir(eds_test.PostActionCheck):
                         'Current Working Directory')
 
 
-class CwdIs(eds_test.PostActionCheck):
+class CwdIs(sds_test.PostActionCheck):
     def __init__(self, eds2dir_fun):
         self.eds2dir_fun = eds2dir_fun
 
@@ -148,7 +148,7 @@ class CwdIs(eds_test.PostActionCheck):
 class TestSuccessfulScenarios(TestCaseBase):
     def test_relative_argument_should_change_dir_relative_to_cwd__from_act_dir(self):
         self._test_argument('existing-dir',
-                            eds_test.Check(expected_action_result=is_success(),
+                            sds_test.Check(expected_action_result=is_success(),
                                            eds_contents_before=act_dir_contents(DirContents([
                                                empty_dir('existing-dir')
                                            ])),
@@ -157,7 +157,7 @@ class TestSuccessfulScenarios(TestCaseBase):
 
     def test_relative_argument_should_change_dir_relative_to_cwd__from_tmp_dir(self):
         self._test_argument('sub1/sub2',
-                            eds_test.Check(expected_action_result=is_success(),
+                            sds_test.Check(expected_action_result=is_success(),
                                            eds_contents_before=tmp_user_dir_contents(DirContents([
                                                Dir('sub1', [
                                                    empty_dir('sub2')
@@ -169,20 +169,20 @@ class TestSuccessfulScenarios(TestCaseBase):
 
     def test_act_root_option_should_change_to_act_dir(self):
         self._test_argument('--rel-act',
-                            eds_test.Check(expected_action_result=is_success(),
+                            sds_test.Check(expected_action_result=is_success(),
                                            pre_action_action=ChangeDirTo(lambda eds: eds.root_dir),
                                            post_action_check=CwdIs(lambda eds: eds.act_dir)
                                            ))
 
     def test_relative_tmp__without_argument(self):
         self._test_argument('--rel-tmp',
-                            eds_test.Check(expected_action_result=is_success(),
+                            sds_test.Check(expected_action_result=is_success(),
                                            post_action_check=CwdIs(lambda eds: eds.tmp.user_dir)
                                            ))
 
     def test_relative_tmp__with_argument(self):
         self._test_argument('--rel-tmp sub1/sub2',
-                            eds_test.Check(expected_action_result=is_success(),
+                            sds_test.Check(expected_action_result=is_success(),
                                            eds_contents_before=tmp_user_dir_contents(DirContents([
                                                Dir('sub1', [
                                                    empty_dir('sub2')
@@ -195,7 +195,7 @@ class TestSuccessfulScenarios(TestCaseBase):
 class TestFailingScenarios(TestCaseBase):
     def test_argument_is_file(self):
         self._test_argument('existing-file',
-                            eds_test.Check(expected_action_result=is_failure(),
+                            sds_test.Check(expected_action_result=is_failure(),
                                            eds_contents_before=act_dir_contents(DirContents([
                                                empty_file('existing-file')
                                            ])),
