1334502b51fb94a8e6ca629cda963e794a8984ce
Emil Karlén <emil@member.fsf.org>
2016-11-05 19:36:58 +0100

    timeout: Inlines unnecessary method

--- src/exactly_lib/instructions/before_assert/run.py
+++ src/exactly_lib/instructions/before_assert/run.py
@@ -2,17 +2,10 @@ from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.instructions.before_assert.utils.instruction_from_parts import \
     BeforeAssertPhaseInstructionFromValidatorAndExecutor
 from exactly_lib.instructions.multi_phase_instructions import run
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionParser
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        parser(instruction_name),
+        run.InstructionParser2(instruction_name, BeforeAssertPhaseInstructionFromValidatorAndExecutor),
         run.TheInstructionDocumentation(instruction_name,
                                         description_rest_text=run.NON_ASSERT_PHASE_DESCRIPTION_REST))
-
-
-def parser(instruction_name: str) -> SingleInstructionParser:
-    return run.InstructionParser2(instruction_name,
-                                  BeforeAssertPhaseInstructionFromValidatorAndExecutor)
--- src/exactly_lib/instructions/before_assert/shell.py
+++ src/exactly_lib/instructions/before_assert/shell.py
@@ -2,16 +2,9 @@ from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.instructions.before_assert.utils.instruction_from_parts import \
     BeforeAssertPhaseInstructionFromValidatorAndExecutor
 from exactly_lib.instructions.multi_phase_instructions import shell as shell_common
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionParser
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        parser(instruction_name),
+        shell_common.Parser2(instruction_name, BeforeAssertPhaseInstructionFromValidatorAndExecutor),
         shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
-
-
-def parser(instruction_name: str) -> SingleInstructionParser:
-    return shell_common.Parser2(instruction_name,
-                                BeforeAssertPhaseInstructionFromValidatorAndExecutor)
