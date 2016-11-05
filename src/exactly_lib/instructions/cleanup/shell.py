40505ccbdae4e34e542c78acfe5b258b056ccc4c
Emil Karlén <emil@member.fsf.org>
2016-11-05 19:17:52 +0100

    timeout: Rewrite of cleanup/shell using new infrastructure for instruction from validator and executor

--- src/exactly_lib/instructions/cleanup/shell.py
+++ src/exactly_lib/instructions/cleanup/shell.py
@@ -1,32 +1,17 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
+from exactly_lib.instructions.cleanup.utils.instruction_from_parts import \
+    CleanupPhaseInstructionFromValidatorAndExecutor
 from exactly_lib.instructions.multi_phase_instructions import shell as shell_common
-from exactly_lib.instructions.utils.sub_process_execution import ExecuteInfo
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionParser
-from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.cleanup import CleanupPhaseInstruction, PreviousPhase
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
-from exactly_lib.test_case.phases.result import sh
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-            parser(instruction_name),
-            shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
+        parser(instruction_name),
+        shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
 
 
 def parser(instruction_name: str) -> SingleInstructionParser:
-    return shell_common.Parser(instruction_name,
-                               _ShellInstruction)
-
-
-class _ShellInstruction(CleanupPhaseInstruction):
-    def __init__(self,
-                 execute_info: ExecuteInfo):
-        self.execute_info = execute_info
-
-    def main(self,
-             environment: InstructionEnvironmentForPostSdsStep,
-             previous_phase: PreviousPhase,
-             os_services: OsServices) -> sh.SuccessOrHardError:
-        return shell_common.run_and_return_sh(self.execute_info, environment.phase_logging)
+    return shell_common.Parser2(instruction_name,
+                                CleanupPhaseInstructionFromValidatorAndExecutor)
