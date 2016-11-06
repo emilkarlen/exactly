9c5076632c500303f89add72f020947887f1cc7b
Emil Karlén <emil.karlen@fripost.org>
2016-11-06 11:03:00 +0100

    sub process execution: "shell" instruction now uses new generic InstructionParser

--- src/exactly_lib/instructions/before_assert/shell.py
+++ src/exactly_lib/instructions/before_assert/shell.py
@@ -6,5 +6,5 @@ from exactly_lib.instructions.multi_phase_instructions import shell as shell_com
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        shell_common.Parser(instruction_name, BeforeAssertPhaseInstructionFromValidatorAndExecutor),
+        shell_common.instruction_parser(instruction_name, BeforeAssertPhaseInstructionFromValidatorAndExecutor),
         shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
