728a90158fe30cb6a7fa7324f1ec33bf4d124452
Emil Karlén <emil@member.fsf.org>
2016-11-06 10:50:25 +0100

    sub process execution: New generic InstructionParser that uses new ValidationAndSubProcessExecutionSetupParser
    This parser is integrated into the "run" instruction

--- src/exactly_lib/instructions/multi_phase_instructions/run.py
+++ src/exactly_lib/instructions/multi_phase_instructions/run.py
@@ -3,7 +3,7 @@ import shlex
 from exactly_lib.common.instruction_documentation import InvokationVariant, \
     SyntaxElementDescription
 from exactly_lib.instructions.utils import file_properties
-from exactly_lib.instructions.utils import sub_process_execution
+from exactly_lib.instructions.utils import instruction_from_parts_for_executing_sub_process as spe_parts
 from exactly_lib.instructions.utils.arg_parse import parse_executable_file
 from exactly_lib.instructions.utils.arg_parse import parse_file_ref
 from exactly_lib.instructions.utils.arg_parse.parse_executable_file import PARSE_FILE_REF_CONFIGURATION
@@ -16,15 +16,20 @@ from exactly_lib.instructions.utils.documentation.instruction_documentation_with
 from exactly_lib.instructions.utils.executable_file import ExecutableFile
 from exactly_lib.instructions.utils.file_ref import FileRef
 from exactly_lib.instructions.utils.file_ref_check import FileRefCheckValidator, FileRefCheck
-from exactly_lib.instructions.utils.instruction_from_parts_for_executing_sub_process import \
-    MainStepExecutorForSubProcess, ValidationAndSubProcessExecutionSetup
-from exactly_lib.instructions.utils.instruction_parts import InstructionParts
 from exactly_lib.instructions.utils.pre_or_post_validation import AndValidator
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionParser, SingleInstructionParserSource, SingleInstructionInvalidArgumentException
-from exactly_lib.test_case.phases.common import HomeAndSds, TestCaseInstruction
+from exactly_lib.test_case.phases.common import HomeAndSds
 from exactly_lib.util.cli_syntax.elements import argument as a
 from exactly_lib.util.cli_syntax.option_syntax import long_option_syntax
+
+
+def instruction_parser(instruction_name: str,
+                       instruction_parts2instruction_function) -> SingleInstructionParser:
+    return spe_parts.InstructionParser(instruction_name,
+                                       SetupParser(),
+                                       instruction_parts2instruction_function)
+
 
 INTERPRET_OPTION_NAME = a.OptionName(long_name='interpret')
 INTERPRET_OPTION = long_option_syntax(INTERPRET_OPTION_NAME.long)
@@ -37,6 +42,8 @@ OPTIONS_SEPARATOR_ARGUMENT = '--'
 NON_ASSERT_PHASE_DESCRIPTION_REST = """\
 It is considered an error if the program exits with a non-zero exit code.
 """
+
+_SOURCE_SYNTAX_ELEMENT_NAME = 'SOURCE'
 
 
 class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderingBase):
@@ -100,7 +107,7 @@ class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderi
             InvokationVariant(self._cl_syntax_for_args([
                 self.mandatory_executable,
                 a.Single(a.Multiplicity.MANDATORY, a.Option(SOURCE_OPTION_NAME)),
-                a.Single(a.Multiplicity.MANDATORY, a.Named('SOURCE'))]),
+                a.Single(a.Multiplicity.MANDATORY, a.Named(_SOURCE_SYNTAX_ELEMENT_NAME))]),
                 self._paragraphs(
                     """\
                     Interprets the given SOURCE using {EXECUTABLE}.
@@ -176,16 +183,12 @@ class CmdAndArgsResolverForSource(CmdAndArgsResolverForExecutableFileBase):
         return [self.source]
 
 
-class SetupParser:
-    def __init__(self,
-                 instruction_name: str):
-        self.instruction_name = instruction_name
-
-    def apply(self, source: SingleInstructionParserSource) -> ValidationAndSubProcessExecutionSetup:
+class SetupParser(spe_parts.ValidationAndSubProcessExecutionSetupParser):
+    def apply(self, source: SingleInstructionParserSource) -> spe_parts.ValidationAndSubProcessExecutionSetup:
         tokens = TokenStream(source.instruction_argument)
         (exe_file, arg_tokens) = parse_executable_file.parse(tokens)
         (validator, cmd_and_args_resolver) = self._validator__cmd_and_args_resolver(exe_file, arg_tokens)
-        return ValidationAndSubProcessExecutionSetup(validator, cmd_and_args_resolver, False)
+        return spe_parts.ValidationAndSubProcessExecutionSetup(validator, cmd_and_args_resolver, False)
 
     def _validator__cmd_and_args_resolver(self,
                                           exe_file: ExecutableFile,
@@ -222,23 +225,8 @@ class SetupParser:
     def _source(exe_file: ExecutableFile,
                 remaining_arguments_str: str):
         if not remaining_arguments_str:
-            raise SingleInstructionInvalidArgumentException('Missing SOURCE argument for option %s' % SOURCE_OPTION)
+            msg = 'Missing {SOURCE} argument for option {option}'.format(SOURCE=_SOURCE_SYNTAX_ELEMENT_NAME,
+                                                                         option=SOURCE_OPTION)
+            raise SingleInstructionInvalidArgumentException(msg)
         cmd_resolver = CmdAndArgsResolverForSource(exe_file, remaining_arguments_str)
         return exe_file.validator, cmd_resolver
-
-
-class InstructionParser(SingleInstructionParser):
-    def __init__(self,
-                 instruction_name: str,
-                 instruction_parts2instruction_function):
-        self.instruction_name = instruction_name
-        self._instruction_parts2instruction_function = instruction_parts2instruction_function
-        self.setup_parser = SetupParser(instruction_name)
-
-    def apply(self, source: SingleInstructionParserSource) -> TestCaseInstruction:
-        source_info = sub_process_execution.InstructionSourceInfo(source.line_sequence.first_line.line_number,
-                                                                  self.instruction_name)
-        setup = self.setup_parser.apply(source)
-        return self._instruction_parts2instruction_function(
-            InstructionParts(setup.validator,
-                             MainStepExecutorForSubProcess(source_info, setup)))
--- src/exactly_lib/instructions/utils/instruction_from_parts_for_executing_sub_process.py
+++ src/exactly_lib/instructions/utils/instruction_from_parts_for_executing_sub_process.py
@@ -1,8 +1,11 @@
 from exactly_lib.instructions.utils import sub_process_execution as spe
-from exactly_lib.instructions.utils.instruction_parts import MainStepExecutor
+from exactly_lib.instructions.utils.instruction_parts import MainStepExecutor, InstructionParts
 from exactly_lib.instructions.utils.pre_or_post_validation import PreOrPostEdsValidator
+from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParserSource, SingleInstructionParser
 from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep, PhaseLoggingPaths
+from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep, PhaseLoggingPaths, \
+    TestCaseInstructionExecutedInSandbox
 from exactly_lib.test_case.phases.result import pfh
 from exactly_lib.test_case.phases.result import sh
 
@@ -55,3 +58,26 @@ class MainStepExecutorForSubProcess(MainStepExecutor):
                   logging_paths: PhaseLoggingPaths,
                   os_services: OsServices) -> pfh.PassOrFailOrHardError:
         return spe.result_to_pfh(self.apply(environment, logging_paths, os_services))
+
+
+class ValidationAndSubProcessExecutionSetupParser:
+    def apply(self, source: SingleInstructionParserSource) -> ValidationAndSubProcessExecutionSetup:
+        raise NotImplementedError()
+
+
+class InstructionParser(SingleInstructionParser):
+    def __init__(self,
+                 instruction_name: str,
+                 setup_parser: ValidationAndSubProcessExecutionSetupParser,
+                 instruction_parts2instruction_function):
+        self.instruction_name = instruction_name
+        self._instruction_parts2instruction_function = instruction_parts2instruction_function
+        self.setup_parser = setup_parser
+
+    def apply(self, source: SingleInstructionParserSource) -> TestCaseInstructionExecutedInSandbox:
+        source_info = spe.InstructionSourceInfo(source.line_sequence.first_line.line_number,
+                                                self.instruction_name)
+        setup = self.setup_parser.apply(source)
+        return self._instruction_parts2instruction_function(
+            InstructionParts(setup.validator,
+                             MainStepExecutorForSubProcess(source_info, setup)))
