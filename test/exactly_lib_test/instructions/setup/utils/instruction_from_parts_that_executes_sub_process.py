ecb91cdd3d0bdcbca32fc9853f610a6ce677dab8
Emil Karlén <emil.karlen@fripost.org>
2016-11-07 18:15:59 +0100

    sub process execution: Test of instruction_from_parts_that_executes_sub_process (phase setup)

---
+++ test/exactly_lib_test/instructions/setup/utils/instruction_from_parts_that_executes_sub_process.py
@@ -0,0 +1,42 @@
+import unittest
+
+from exactly_lib.common.instruction_setup import SingleInstructionSetup
+from exactly_lib.instructions.setup.utils import instruction_from_parts
+from exactly_lib.instructions.utils.instruction_parts import InstructionInfoForConstructingAnInstructionFromParts
+from exactly_lib.test_case import phase_identifier
+from exactly_lib_test.instructions.multi_phase_instructions.test_resources import \
+    instruction_from_parts_that_executes_sub_process as test_impl
+from exactly_lib_test.instructions.setup.test_resources.configuration import SetupConfigurationBase
+from exactly_lib_test.instructions.setup.test_resources.instruction_check import Expectation
+from exactly_lib_test.instructions.test_resources.assertion_utils import sh_check
+from exactly_lib_test.instructions.test_resources.assertion_utils import svh_check
+from exactly_lib_test.test_resources.value_assertions import value_assertion as va
+
+
+class ConfigurationForTheSetupPhase(SetupConfigurationBase, test_impl.Configuration):
+    def phase(self) -> phase_identifier.Phase:
+        return phase_identifier.SETUP
+
+    def instruction_setup(self) -> SingleInstructionSetup:
+        raise ValueError('this method is not used by these tests')
+
+    def instruction_info_for(self, instruction_name: str) -> InstructionInfoForConstructingAnInstructionFromParts:
+        return instruction_from_parts.instruction_info_for(instruction_name)
+
+    def expectation_for_zero_exitcode(self) -> Expectation:
+        return Expectation(main_result=sh_check.is_success())
+
+    def expectation_for_non_zero_exitcode(self) -> Expectation:
+        return Expectation(main_result=sh_check.is_hard_error())
+
+    def expect_failing_validation_post_setup(self,
+                                             assertion_on_error_message: va.ValueAssertion = va.anything_goes()):
+        return Expectation(post_validation_result=svh_check.is_validation_error(assertion_on_error_message))
+
+
+def suite() -> unittest.TestSuite:
+    return test_impl.suite_for(ConfigurationForTheSetupPhase())
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
