3428b36031e7336c61efefe40fce6b48c8b4c0bd
Emil Karlén <emil@member.fsf.org>
2016-11-07 18:11:05 +0100

    sub process execution: Test of instruction_from_parts_that_executes_sub_process (phase cleanup)

--- test/exactly_lib_test/instructions/cleanup/__init__.py
+++ test/exactly_lib_test/instructions/cleanup/__init__.py
@@ -1,12 +1,13 @@
 import unittest
 
 from exactly_lib_test.instructions.cleanup import change_dir, env, run, new_dir, shell
-from exactly_lib_test.instructions.cleanup import test_resources
+from exactly_lib_test.instructions.cleanup import test_resources, utils
 
 
 def suite():
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources.suite())
+    ret_val.addTest(utils.suite())
     ret_val.addTest(shell.suite())
     ret_val.addTest(run.suite())
     ret_val.addTest(env.suite())
