1051cf37e69e4f5263059ec55fa4e1bda302f177
Emil Karlén <emil.karlen@fripost.org>
2016-11-07 11:21:00 +0100

    sub process execution: TestResultIsValidationErrorWhenPreSdsValidationFails

--- test/exactly_lib_test/instructions/multi_phase_instructions/test_resources/instruction_from_parts_that_executes_sub_process.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/test_resources/instruction_from_parts_that_executes_sub_process.py
@@ -1,3 +1,4 @@
+import pathlib
 import unittest
 
 from exactly_lib.instructions.utils import instruction_from_parts_for_executing_sub_process as spe_parts
@@ -9,6 +10,7 @@ from exactly_lib.instructions.utils.instruction_from_parts_for_executing_sub_pro
 from exactly_lib.instructions.utils.instruction_parts import InstructionInfoForConstructingAnInstructionFromParts
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionParserSource, SingleInstructionParser
+from exactly_lib.test_case.sandbox_directory_structure import SandboxDirectoryStructure
 from exactly_lib_test.instructions.assert_.test_resources import instruction_check
 from exactly_lib_test.instructions.assert_.test_resources.instruction_check import Expectation
 from exactly_lib_test.instructions.multi_phase_instructions.test_resources.configuration import ConfigurationBase
@@ -58,7 +60,9 @@ class Configuration(ConfigurationBase):
 
 def suite_for(configuration: Configuration) -> unittest.TestSuite:
     test_case_classes = [TestInstructionIsSuccessfulWhenExitStatusFromCommandIsZero,
-                         TestInstructionIsErrorWhenExitStatusFromCommandIsNonZero]
+                         TestInstructionIsErrorWhenExitStatusFromCommandIsNonZero,
+                         TestResultIsValidationErrorWhenPreSdsValidationFails,
+                         TestResultIsValidationErrorWhenPostSetupValidationFails]
     return unittest.TestSuite(
         [tcc(configuration) for tcc in test_case_classes])
 
@@ -71,13 +75,11 @@ class TestCaseBase(TestCaseBaseWithShortDescriptionOfTestClassAndAnObjectType):
 
 class TestInstructionIsSuccessfulWhenExitStatusFromCommandIsZero(TestCaseBase):
     def runTest(self):
-        script_that_exists_with_status_0 = 'import sys; sys.exit(0)'
-
         execution_setup_parser = _SetupParserForExecutingPythonSourceFromInstructionArgumentOnCommandLine(
             pre_or_post_validation.ConstantSuccessValidator())
         self.conf.run_sub_process_test(
             self,
-            new_source2(script_that_exists_with_status_0),
+            new_source2(SCRIPT_THAT_EXISTS_WITH_STATUS_0),
             execution_setup_parser,
             self.conf.empty_arrangement(),
             self.conf.expectation_for_zero_exitcode())
@@ -97,6 +99,19 @@ class TestInstructionIsErrorWhenExitStatusFromCommandIsNonZero(TestCaseBase):
             self.conf.expectation_for_non_zero_exitcode())
 
 
+class TestResultIsValidationErrorWhenPreSdsValidationFails(TestCaseBase):
+    def runTest(self):
+        execution_setup_parser = _SetupParserForExecutingPythonSourceFromInstructionArgumentOnCommandLine(
+            ConstantResultValidator(pre_sds='validation error message')
+        )
+        self.conf.run_sub_process_test(
+            self,
+            new_source2(SCRIPT_THAT_EXISTS_WITH_STATUS_0),
+            execution_setup_parser,
+            self.conf.empty_arrangement(),
+            self.conf.expect_failing_validation_pre_eds())
+
+
 class _SetupParserForExecutingPythonSourceFromInstructionArgumentOnCommandLine(
     spe_parts.ValidationAndSubProcessExecutionSetupParser):
     def __init__(self,
@@ -112,3 +127,19 @@ class _SetupParserForExecutingPythonSourceFromInstructionArgumentOnCommandLine(
 
 def _resolver_for_execute_py_on_command_line(python_source: str) -> spe.CmdAndArgsResolver:
     return ConstantCmdAndArgsResolver(non_shell_args_for_that_executes_source_on_command_line(python_source))
+
+
+SCRIPT_THAT_EXISTS_WITH_STATUS_0 = 'import sys; sys.exit(0)'
+
+
+class ConstantResultValidator(pre_or_post_validation.PreOrPostEdsValidator):
+    def __init__(self, pre_sds=None,
+                 post_setup=None):
+        self.pre_sds = pre_sds
+        self.post_setup = post_setup
+
+    def validate_pre_eds_if_applicable(self, home_dir_path: pathlib.Path) -> str:
+        return self.pre_sds
+
+    def validate_post_eds_if_applicable(self, sds: SandboxDirectoryStructure) -> str:
+        return self.post_setup
