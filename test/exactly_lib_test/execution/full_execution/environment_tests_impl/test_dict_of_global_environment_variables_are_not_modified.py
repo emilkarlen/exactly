955abdfad0f5c2691a7f4a7b47e353b47c4e1d2a
Emil Karlén <emil@member.fsf.org>
2016-11-12 17:29:14 +0100

    envvars: Adds failing test of full_execution: test_dict_of_global_environment_variables_are_not_modified

---
+++ test/exactly_lib_test/execution/full_execution/environment_tests_impl/test_dict_of_global_environment_variables_are_not_modified.py
@@ -0,0 +1,30 @@
+import copy
+import os
+import unittest
+
+from exactly_lib.test_case import test_case_doc
+from exactly_lib.test_case.act_phase_handling import ActPhaseHandling
+from exactly_lib_test.execution.full_execution.test_resources.test_case_base import FullExecutionTestCaseBase
+from exactly_lib_test.execution.partial_execution.test_resources.basic import dummy_act_phase_handling
+from exactly_lib_test.execution.test_resources import recorder as instr_setup
+from exactly_lib_test.execution.test_resources.test_case_generation import full_test_case_with_instructions
+
+
+class Test(FullExecutionTestCaseBase):
+    def __init__(self,
+                 unittest_case: unittest.TestCase,
+                 dbg_do_not_delete_dir_structure=False):
+        super().__init__(unittest_case,
+                         dbg_do_not_delete_dir_structure)
+        self.recorder = instr_setup.Recorder()
+        self.original_set_of_env_vars = dict(os.environ)
+
+    def _act_phase_handling(self) -> ActPhaseHandling:
+        return dummy_act_phase_handling()
+
+    def _test_case(self) -> test_case_doc.TestCase:
+        return full_test_case_with_instructions()
+
+    def _assertions(self):
+        self.utc.assertDictEqual(self.original_set_of_env_vars,
+                                 dict(os.environ))
--- test/exactly_lib_test/execution/full_execution/test_environment.py
+++ test/exactly_lib_test/execution/full_execution/test_environment.py
@@ -2,7 +2,8 @@ import unittest
 
 from exactly_lib_test.execution.full_execution.environment_tests_impl import \
     test_environment_variables, \
-    test_current_directory
+    test_current_directory, \
+    test_dict_of_global_environment_variables_are_not_modified
 
 
 class Test(unittest.TestCase):
@@ -11,3 +12,6 @@ class Test(unittest.TestCase):
 
     def test_environment_variables_for_each_phase_step(self):
         test_environment_variables.Test(self).execute()
+
+    def test_dict_of_global_environment_variables_are_not_modified(self):
+        test_dict_of_global_environment_variables_are_not_modified.Test(self).execute()
