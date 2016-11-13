cb8584c4f325f1d3241e8c98a0196c2d5921ff76
Emil Karlén <emil@member.fsf.org>
2016-11-13 13:02:48 +0100

    suite reporting: Rename: Default... -> SimpleProgress...

--- test/exactly_lib_test/cli/test_resources/execute_main_program.py
+++ test/exactly_lib_test/cli/test_resources/execute_main_program.py
@@ -2,7 +2,7 @@ from exactly_lib.cli.argument_parsing_of_act_phase_setup import INTERPRETER_FOR_
 from exactly_lib.cli.cli_environment.program_modes.test_case.command_line_options import OPTION_FOR_ACTOR
 from exactly_lib.default import default_main_program as sut
 from exactly_lib.processing.instruction_setup import InstructionsSetup
-from exactly_lib.test_suite.reporters.simple_progress_reporter import DefaultRootSuiteReporterFactory
+from exactly_lib.test_suite.reporters.simple_progress_reporter import SimpleProgressRootSuiteReporterFactory
 from exactly_lib_test.cli.test_resources.test_case_handling_setup import test_case_handling_setup
 from exactly_lib_test.test_resources.process import SubProcessResult
 from exactly_lib_test.test_resources.str_std_out_files import StringStdOutFiles
@@ -36,7 +36,7 @@ def execute_main_program(arguments: list,
                               name_and_argument_splitter,
                               instructions_setup,
                               test_case_handling_setup(),
-                              DefaultRootSuiteReporterFactory())
+                              SimpleProgressRootSuiteReporterFactory())
     exit_status = program.execute(arguments)
     str_std_out_files.finish()
     return SubProcessResult(exit_status,
