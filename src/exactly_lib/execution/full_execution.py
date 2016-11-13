ba3add73050c2e81581bbc78df5c2ef6fe141446
Emil Karlén <emil@member.fsf.org>
2016-11-13 11:54:37 +0100

    envvars: full_execution: Removes exactly-environment variables that where inherited from the process that started exactly

--- src/exactly_lib/execution/full_execution.py
+++ src/exactly_lib/execution/full_execution.py
@@ -20,7 +20,8 @@ def execute(test_case: test_case_doc.TestCase,
     """
     The main method for executing a Test Case.
     """
-    _prepare_environment_variables()
+    environ = dict(os.environ)
+    _prepare_environment_variables(environ)
     partial_result = _execute_configuration_phase(configuration_builder,
                                                   test_case.configuration_phase)
     if partial_result.status is not PartialResultStatus.PASS:
@@ -28,7 +29,7 @@ def execute(test_case: test_case_doc.TestCase,
     if configuration_builder.execution_mode is ExecutionMode.SKIP:
         return result.new_skipped()
     partial_execution_configuration = partial_execution.Configuration(configuration_builder.home_dir_path,
-                                                                      dict(os.environ),
+                                                                      environ,
                                                                       configuration_builder.timeout_in_seconds)
     partial_result = partial_execution.execute(configuration_builder.act_phase_handling,
                                                partial_execution.TestCase(test_case.setup_phase,
@@ -73,10 +74,10 @@ def translate_status(execution_mode: ExecutionMode,
     return FullResultStatus(ps.value)
 
 
-def _prepare_environment_variables():
+def _prepare_environment_variables(environ: dict):
     for ev in environment_variables.ALL_ENV_VARS:
-        if ev in os.environ:
-            del os.environ[ev]
+        if ev in environ:
+            del environ[ev]
 
 
 def _execute_configuration_phase(phase_environment: ConfigurationBuilder,
