e270d8054f7acede5def144ffadae97bd37882ef
Emil Karlén <emil@member.fsf.org>
2016-11-13 20:23:44 +0100

    suite reporting: simple_progress_reporter: test_single_empty_suite_SHOULD_exit_with_success_and_print_suite_lines_and_exit_identifier

--- test/exactly_lib_test/test_suite/reporters/simple_progress_reporter.py
+++ test/exactly_lib_test/test_suite/reporters/simple_progress_reporter.py
@@ -2,21 +2,15 @@ import datetime
 import pathlib
 import unittest
 
-from exactly_lib.processing import processors as case_processing
 from exactly_lib.processing import test_case_processing
-from exactly_lib.processing.act_phase import ActPhaseSetup
-from exactly_lib.processing.instruction_setup import InstructionsSetup
-from exactly_lib.processing.preprocessor import IDENTITY_PREPROCESSOR
-from exactly_lib.processing.test_case_handling_setup import TestCaseHandlingSetup
 from exactly_lib.test_suite import execution
 from exactly_lib.test_suite import exit_values
+from exactly_lib.test_suite.execution import SuitesExecutor
 from exactly_lib.test_suite.reporters import simple_progress_reporter as sut
 from exactly_lib.util.string import lines_content
-from exactly_lib_test.execution.test_resources.act_source_executor import \
-    ActSourceAndExecutorConstructorThatRunsConstantActions
 from exactly_lib_test.test_resources.str_std_out_files import StringStdOutFiles
 from exactly_lib_test.test_suite.test_resources.execution_utils import TestCaseProcessorThatGivesConstant, \
-    FULL_RESULT_PASS
+    FULL_RESULT_PASS, new_test_suite, DUMMY_CASE_PROCESSING
 
 
 def suite() -> unittest.TestSuite:
@@ -26,23 +20,50 @@ def suite() -> unittest.TestSuite:
     ])
 
 
+def _suite_begin(file_name: str):
+    return 'suite ' + file_name + ': begin'
+
+
+def _suite_end(file_name: str):
+    return 'suite ' + file_name + ': end'
+
+
 class TestExecutionOfSuite(unittest.TestCase):
-    def test_empty_suite_SHOULD_exit_with_success_and_print_single_line_with_ok_identifier(self):
+    def test_no_suites_SHOULD_exit_with_success_and_print_single_line_with_ok_identifier(self):
         # ARRANGE #
-        factory = sut.SimpleProgressRootSuiteReporterFactory()
-        std_output_files = StringStdOutFiles()
-        root_file_path = pathlib.Path()
-        reporter = factory.new_reporter(std_output_files.stdout_files, root_file_path)
-        result = test_case_processing.new_executed(FULL_RESULT_PASS)
-        executor = execution.SuitesExecutor(reporter, DEFAULT_CASE_PROCESSING,
-                                            TestCaseProcessorThatGivesConstant(result))
         test_suites = []
+        std_output_files = StringStdOutFiles()
+        executor = _suite_executor_for_case_processing_that_unconditionally_passes(std_output_files,
+                                                                                   pathlib.Path())
         # ACT #
         exit_value = executor.execute_and_report(test_suites)
         # ASSERT #
         std_output_files.finish()
+
         self.assertEquals(exit_values.ALL_PASS, exit_value)
         self.assertEqual(lines_content([exit_values.ALL_PASS.exit_identifier]),
+                         std_output_files.stdout_contents)
+
+    def test_single_empty_suite_SHOULD_exit_with_success_and_print_suite_lines_and_exit_identifier(self):
+        # ARRANGE #
+        test_suites = [
+            new_test_suite('root-file-name', [], [])
+        ]
+        std_output_files = StringStdOutFiles()
+        executor = _suite_executor_for_case_processing_that_unconditionally_passes(std_output_files,
+                                                                                   pathlib.Path())
+        # ACT #
+        exit_value = executor.execute_and_report(test_suites)
+        # ASSERT #
+        std_output_files.finish()
+
+        self.assertEquals(exit_values.ALL_PASS, exit_value)
+        expected_output = lines_content([
+            _suite_begin('root-file-name'),
+            _suite_end('root-file-name'),
+            exit_values.ALL_PASS.exit_identifier
+        ])
+        self.assertEqual(expected_output,
                          std_output_files.stdout_contents)
 
 
@@ -91,9 +112,10 @@ class TestFinalResultFormatting(unittest.TestCase):
                          'Line that reports number of tests and elapsed time')
 
 
-DEFAULT_CASE_PROCESSING = case_processing.Configuration(
-    lambda x: ((), ()),
-    InstructionsSetup({}, {}, {}, {}, {}),
-    TestCaseHandlingSetup(ActPhaseSetup(ActSourceAndExecutorConstructorThatRunsConstantActions()),
-                          IDENTITY_PREPROCESSOR),
-    False)
+def _suite_executor_for_case_processing_that_unconditionally_passes(std_output_files: StringStdOutFiles,
+                                                                    root_file_path: pathlib.Path) -> SuitesExecutor:
+    factory = sut.SimpleProgressRootSuiteReporterFactory()
+    root_suite_reporter = factory.new_reporter(std_output_files.stdout_files, root_file_path)
+    result = test_case_processing.new_executed(FULL_RESULT_PASS)
+    return execution.SuitesExecutor(root_suite_reporter, DUMMY_CASE_PROCESSING,
+                                    lambda conf: TestCaseProcessorThatGivesConstant(result))
--- test/exactly_lib_test/test_suite/test_resources/execution_utils.py
+++ test/exactly_lib_test/test_suite/test_resources/execution_utils.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from exactly_lib.execution import result
 from exactly_lib.processing import processors as case_processing
 from exactly_lib.processing import test_case_processing
@@ -6,6 +8,7 @@ from exactly_lib.processing.instruction_setup import InstructionsSetup
 from exactly_lib.processing.preprocessor import IDENTITY_PREPROCESSOR
 from exactly_lib.processing.test_case_handling_setup import TestCaseHandlingSetup
 from exactly_lib.test_case.sandbox_directory_structure import SandboxDirectoryStructure
+from exactly_lib.test_suite import structure
 from exactly_lib_test.execution.test_resources.act_source_executor import \
     ActSourceAndExecutorConstructorThatRunsConstantActions
 
@@ -33,3 +36,15 @@ DUMMY_CASE_PROCESSING = case_processing.Configuration(
 DUMMY_SDS = SandboxDirectoryStructure('test-root-dir')
 
 FULL_RESULT_PASS = result.new_pass(DUMMY_SDS)
+
+T_C_H_S = test_case_handling_setup_with_identity_preprocessor()
+
+
+def new_test_suite(source_file_name: str,
+                   sub_test_suites: list,
+                   test_cases: list) -> structure.TestSuite:
+    return structure.TestSuite(pathlib.Path(source_file_name),
+                               [],
+                               T_C_H_S,
+                               sub_test_suites,
+                               test_cases)
