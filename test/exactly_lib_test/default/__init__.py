88cadaf2aef1816460bcfc66922b7a6dd11ba15e
Emil Karlén <emil.karlen@fripost.org>
2016-11-27 16:53:26 +0100

    Restructuring of tests: Moves test of suite/wilde card file references to separate module

--- test/exactly_lib_test/default/__init__.py
+++ test/exactly_lib_test/default/__init__.py
@@ -2,8 +2,6 @@ import unittest
 
 from exactly_lib_test.default import instruction_name_and_argument_splitter as splitter
 from exactly_lib_test.default import program_modes
-from exactly_lib_test.default.test_resources.internal_main_program_runner import \
-    run_via_main_program_internally_with_default_setup
 from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 
 
@@ -12,7 +10,7 @@ def suite_that_does_not_require_main_program_runner() -> unittest.TestSuite:
 
 
 def suite_that_does_require_main_program_runner(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
-    return program_modes.suite_for(main_program_runner)
+    return program_modes.suite_that_does_require_main_program_runner(main_program_runner)
 
 
 def complete_suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
@@ -22,4 +20,7 @@ def complete_suite_for(main_program_runner: MainProgramRunner) -> unittest.TestS
 
 
 if __name__ == '__main__':
+    from exactly_lib_test.default.test_resources.internal_main_program_runner import \
+        run_via_main_program_internally_with_default_setup
+
     unittest.TextTestRunner().run(complete_suite_for(run_via_main_program_internally_with_default_setup()))
--- test/exactly_lib_test/default/program_modes/__init__.py
+++ test/exactly_lib_test/default/program_modes/__init__.py
@@ -1,16 +1,14 @@
 import unittest
 
 from exactly_lib_test.default.program_modes import test_case, test_suite, help, html_doc
-from exactly_lib_test.default.test_resources.internal_main_program_runner import \
-    run_via_main_program_internally_with_default_setup
 from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 
 
-def suite() -> unittest.TestSuite:
-    return suite_for(run_via_main_program_internally_with_default_setup())
+def suite_that_does_not_require_main_program_runner() -> unittest.TestSuite:
+    return unittest.TestSuite()
 
 
-def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
+def suite_that_does_require_main_program_runner(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_case.suite_for(main_program_runner))
     ret_val.addTest(test_suite.suite_for(main_program_runner))
@@ -19,5 +17,14 @@ def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
     return ret_val
 
 
+def complete_suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
+    ret_val = suite_that_does_not_require_main_program_runner()
+    ret_val.addTests(suite_that_does_require_main_program_runner(main_program_runner))
+    return ret_val
+
+
 if __name__ == '__main__':
-    unittest.TextTestRunner().run(suite())
+    from exactly_lib_test.default.test_resources.internal_main_program_runner import \
+        run_via_main_program_internally_with_default_setup
+
+    unittest.TextTestRunner().run(complete_suite_for(run_via_main_program_internally_with_default_setup()))
