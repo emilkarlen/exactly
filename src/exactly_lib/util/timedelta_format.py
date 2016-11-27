f9dc0dd9d3eef3d1390f19f7979e5017d6d6f353
Emil Karlén <emil@member.fsf.org>
2016-11-27 17:11:33 +0100

    suite reporting: simple progress reporter: Change of total time format (removes space between number and 's')

--- src/exactly_lib/util/timedelta_format.py
+++ src/exactly_lib/util/timedelta_format.py
@@ -2,14 +2,5 @@ import datetime
 
 
 def elapsed_time_value_and_unit(td: datetime.timedelta) -> (str, str):
-    unit = 'days'
-    s = str(td)
-    if s[:2] == '0:':
-        s = s[2:]
-        unit = 'min'
-        if s[:3] == '00:':
-            s = s[3:]
-            unit = 's'
-            if s[0] == '0':
-                s = s[1:]
-    return (s, unit)
+    s = '%f' % td.total_seconds()
+    return (s, 's')
