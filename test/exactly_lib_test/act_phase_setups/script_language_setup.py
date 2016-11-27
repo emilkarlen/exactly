c63fcb4516d954a1dc031890c24f99d7069e1efa
Emil Karlén <emil@member.fsf.org>
2016-11-27 17:27:54 +0100

    Refactoring of tests: Simple improvement of method name

--- test/exactly_lib_test/act_phase_setups/script_language_setup.py
+++ test/exactly_lib_test/act_phase_setups/script_language_setup.py
@@ -24,35 +24,36 @@ class TheConfiguration(Configuration):
 
     @contextmanager
     def program_that_copes_stdin_to_stdout(self) -> list:
-        yield self._builder_for(py_program.copy_stdin_to_stdout())
+        yield _instructions_for(py_program.copy_stdin_to_stdout())
 
     @contextmanager
     def program_that_prints_to_stderr(self, string_to_print: str) -> list:
-        yield self._builder_for(py_program.write_string_to_stderr(string_to_print))
+        yield _instructions_for(py_program.write_string_to_stderr(string_to_print))
 
     @contextmanager
     def program_that_prints_to_stdout(self, string_to_print: str) -> list:
-        yield self._builder_for(py_program.write_string_to_stdout(string_to_print))
+        yield _instructions_for(py_program.write_string_to_stdout(string_to_print))
 
     @contextmanager
     def program_that_exits_with_code(self, exit_code: int) -> list:
-        yield self._builder_for(py_program.exit_with_code(exit_code))
+        yield _instructions_for(py_program.exit_with_code(exit_code))
 
     @contextmanager
     def program_that_prints_cwd_without_new_line_to_stdout(self) -> list:
-        yield self._builder_for(py_program.write_cwd_to_stdout())
+        yield _instructions_for(py_program.write_cwd_to_stdout())
 
     @contextmanager
     def program_that_prints_value_of_environment_variable_to_stdout(self, var_name: str) -> list:
-        yield self._builder_for(py_program.write_value_of_environment_variable_to_stdout(var_name))
+        yield _instructions_for(py_program.write_value_of_environment_variable_to_stdout(var_name))
 
     @contextmanager
     def program_that_sleeps_at_least(self, number_of_seconds: int) -> list:
-        yield self._builder_for(
+        yield _instructions_for(
             py_program.program_that_sleeps_at_least_and_then_exists_with_zero_exit_status(number_of_seconds))
 
-    def _builder_for(self, statements: list) -> list:
-        return list(map(lambda stmt: instr([stmt]), statements))
+
+def _instructions_for(statements: list) -> list:
+    return list(map(lambda stmt: instr([stmt]), statements))
 
 
 class TestWhenInterpreterDoesNotExistThanExecuteShouldGiveHardError(unittest.TestCase):
