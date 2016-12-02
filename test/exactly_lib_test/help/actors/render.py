47ab2c629e712c8448c21964e7c2ca48c85ba0f5
Emil Karlén <emil@member.fsf.org>
2016-12-02 23:51:31 +0100

    actor doc: Test of rendering of actor help: single actor and list of actors

---
+++ test/exactly_lib_test/help/actors/render.py
@@ -0,0 +1,44 @@
+import unittest
+
+from exactly_lib.help.actors import render as sut
+from exactly_lib.help.actors.contents_structure import ActorsHelp
+from exactly_lib.help.utils.render import RenderingEnvironment
+from exactly_lib_test.help.actors.test_resources.documentation import ActorTestImpl
+from exactly_lib_test.help.test_resources import CrossReferenceTextConstructorTestImpl
+from exactly_lib_test.util.textformat.test_resources import structure as struct_check
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(TestIndividualActor),
+        unittest.makeSuite(TestAllActorsList),
+    ])
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
+
+
+class TestAllActorsList(unittest.TestCase):
+    def runTest(self):
+        # ARRANGE #
+        renderer = sut.AllActorsListRenderer(ActorsHelp([ActorTestImpl('actor 1'),
+                                                         ActorTestImpl('actor 2')]))
+        # ACT #
+        actual = renderer.apply(RENDERING_ENVIRONMENT)
+        # ASSERT #
+        struct_check.is_section_contents.apply(self, actual)
+
+
+class TestIndividualActor(unittest.TestCase):
+    def test_render(self):
+        # ARRANGE #
+        actor = ActorTestImpl('name')
+        renderer = sut.IndividualActorRenderer(actor)
+        # ACT #
+        actual = renderer.apply(RENDERING_ENVIRONMENT)
+        # ASSERT #
+        struct_check.is_section_contents.apply(self, actual)
+
+
+RENDERING_ENVIRONMENT = RenderingEnvironment(CrossReferenceTextConstructorTestImpl())
--- test/exactly_lib_test/help/actors/test_resources/documentation.py
+++ test/exactly_lib_test/help/actors/test_resources/documentation.py
@@ -1,4 +1,6 @@
 from exactly_lib.help.actors.contents_structure import ActorDocumentation
+from exactly_lib.help.cross_reference_id import CustomCrossReferenceId
+from exactly_lib.util.textformat.structure import structures as docs
 
 
 class ActorTestImpl(ActorDocumentation):
@@ -6,10 +8,13 @@ class ActorTestImpl(ActorDocumentation):
         super().__init__(singular_name)
 
     def single_line_description_str(self) -> str:
-        return 'single line description str'
+        return 'single_line_description_str'
 
     def act_phase_contents(self) -> list:
-        return []
+        return docs.paras('act_phase_contents')
 
     def act_phase_contents_syntax(self) -> list:
-        return []
+        return docs.paras('act_phase_contents_syntax')
+
+    def _see_also_specific(self) -> list:
+        return [CustomCrossReferenceId('custom-cross-reference-target')]
