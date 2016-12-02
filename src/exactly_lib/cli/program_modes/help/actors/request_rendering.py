5ae140839be83855d1a49ffd27ef2f0587980bdf
Emil Karlén <emil.karlen@fripost.org>
2016-12-03 00:07:13 +0100

    actor doc: cli: Help request for "actor" and "actors"

---
+++ src/exactly_lib/cli/program_modes/help/actors/request_rendering.py
@@ -0,0 +1,17 @@
+from exactly_lib.cli.program_modes.help.actors.help_request import ActorHelpRequest, ActorHelpItem
+from exactly_lib.help.actors.contents_structure import ActorsHelp
+from exactly_lib.help.utils.render import SectionContentsRenderer
+
+
+class ActorHelpRequestRendererResolver:
+    def __init__(self, actors_help: ActorsHelp):
+        self.actors_help = actors_help
+
+    def renderer_for(self, request: ActorHelpRequest) -> SectionContentsRenderer:
+        from exactly_lib.help.actors import render
+        item = request.item
+        if item is ActorHelpItem.ALL_ACTORS_LIST:
+            return render.AllActorsListRenderer(self.actors_help)
+        if item is ActorHelpItem.INDIVIDUAL_ACTOR:
+            return render.IndividualActorRenderer(request.individual_actor)
+        raise ValueError('Invalid %s: %s' % (str(ActorHelpItem), str(item)))
