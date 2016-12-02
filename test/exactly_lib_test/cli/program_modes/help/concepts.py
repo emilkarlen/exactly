8ab189835efc8d10549991361e6274f47cb3b04e
Emil Karlén <emil.karlen@fripost.org>
2016-12-02 16:20:11 +0100

    actor doc: Move test impl of ConceptDoc to separate module

--- test/exactly_lib_test/cli/program_modes/help/concepts.py
+++ test/exactly_lib_test/cli/program_modes/help/concepts.py
@@ -4,7 +4,7 @@ from exactly_lib.cli.program_modes.help.concepts import request_rendering
 from exactly_lib.cli.program_modes.help.concepts.help_request import ConceptHelpRequest, ConceptHelpItem
 from exactly_lib.help.concepts.contents_structure import ConceptsHelp
 from exactly_lib.help.utils.render import SectionContentsRenderer
-from exactly_lib_test.cli.program_modes.help.argument_parsing import ConceptTestImpl
+from exactly_lib_test.help.concepts.test_resources.documentation import ConceptTestImpl
 
 
 class TestConceptHelpRequestRendererResolver(unittest.TestCase):
