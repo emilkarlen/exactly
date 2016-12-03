7f918a99433543a27941b5d35b7a37b764f93eb6
Emil Karlén <emil@member.fsf.org>
2016-12-03 20:59:30 +0100

    doc:  cli: Remove unnecessary helper method for creating EntityHelpRequest for individual entity types

--- src/exactly_lib/cli/program_modes/help/actors/help_request.py
+++
@@ -1,8 +0,0 @@
-from exactly_lib.cli.program_modes.help.entities_requests import EntityHelpRequest, EntityHelpItem
-from exactly_lib.help.actors.contents_structure import ActorDocumentation
-from exactly_lib.help.entity_names import ACTOR_ENTITY_TYPE_NAME
-
-
-def actor_help_request(item: EntityHelpItem,
-                       individual_actor: ActorDocumentation = None) -> EntityHelpRequest:
-    return EntityHelpRequest(ACTOR_ENTITY_TYPE_NAME, item, individual_actor)
--- src/exactly_lib/cli/program_modes/help/concepts/help_request.py
+++
@@ -1,8 +0,0 @@
-from exactly_lib.cli.program_modes.help.entities_requests import EntityHelpRequest, EntityHelpItem
-from exactly_lib.help.concepts.contents_structure import ConceptDocumentation
-from exactly_lib.help.entity_names import CONCEPT_ENTITY_TYPE_NAME
-
-
-def concept_help_request(item: EntityHelpItem,
-                         individual_concept: ConceptDocumentation = None) -> EntityHelpRequest:
-    return EntityHelpRequest(CONCEPT_ENTITY_TYPE_NAME, item, individual_concept)
