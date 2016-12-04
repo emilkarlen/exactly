10f24965e7356373688cf72d77f22f28761e15a6
Emil Karlén <emil.karlen@fripost.org>
2016-12-04 20:09:23 +0100

    suite reporter doc: Adds "junit" reporter

--- src/exactly_lib/help/suite_reporters/suite_reporter/all_suite_reporters.py
+++ src/exactly_lib/help/suite_reporters/suite_reporter/all_suite_reporters.py
@@ -1,5 +1,7 @@
+from exactly_lib.help.suite_reporters.suite_reporter import junit
 from exactly_lib.help.suite_reporters.suite_reporter.progress_reporter import DOCUMENTATION
 
 ALL_SUITE_REPORTERS = [
     DOCUMENTATION,
+    junit.JunitSuiteReporterDocumentation(),
 ]
---
+++ src/exactly_lib/help/suite_reporters/suite_reporter/junit.py
@@ -0,0 +1,14 @@
+from exactly_lib.help.suite_reporters.contents_structure import SuiteReporterDocumentation
+from exactly_lib.help.suite_reporters.names_and_cross_references import JUNIT_REPORTER__NAME
+from exactly_lib.help.utils.textformat_parse import TextParser
+
+
+class JunitSuiteReporterDocumentation(SuiteReporterDocumentation):
+    def __init__(self):
+        super().__init__(JUNIT_REPORTER__NAME)
+        format_map = {
+        }
+        self._parser = TextParser(format_map)
+
+    def single_line_description_str(self) -> str:
+        return self._parser.format('Prints JUnit xml.')
