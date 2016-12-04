461e979aca3a77c302af75c56e416d8285fe2aab
Emil Karlén <emil.karlen@fripost.org>
2016-12-04 23:15:52 +0100

    doc: lookup method for key_patterns
    To be used for looking up entities and other command line elements

--- test/exactly_lib_test/cli/__init__.py
+++ test/exactly_lib_test/cli/__init__.py
@@ -1,11 +1,15 @@
 import unittest
 
 from exactly_lib_test.cli import program_modes
+from exactly_lib_test.cli.util import value_lookup
 from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 
 
 def suite_that_does_not_require_main_program_runner() -> unittest.TestSuite:
-    return program_modes.suite_that_does_not_require_main_program_runner()
+    return unittest.TestSuite([
+        value_lookup.suite(),
+        program_modes.suite_that_does_not_require_main_program_runner(),
+    ])
 
 
 def suite_that_does_require_main_program_runner(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
@@ -21,4 +25,5 @@ def complete_suite_for(main_program_runner: MainProgramRunner) -> unittest.TestS
 if __name__ == '__main__':
     from exactly_lib_test.default.test_resources.internal_main_program_runner import \
         run_via_main_program_internally_with_default_setup
+
     unittest.TextTestRunner().run(complete_suite_for(run_via_main_program_internally_with_default_setup()))
