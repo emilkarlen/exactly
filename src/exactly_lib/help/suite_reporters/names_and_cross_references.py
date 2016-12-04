7bff1b9d1c61a23057cd212c836c1fe2ddc10489
Emil Karlén <emil@member.fsf.org>
2016-12-04 20:09:23 +0100

    suite reporter doc: Adds "junit" reporter

--- src/exactly_lib/help/suite_reporters/names_and_cross_references.py
+++ src/exactly_lib/help/suite_reporters/names_and_cross_references.py
@@ -7,8 +7,11 @@ def suite_reporter_cross_ref(reporter_name: str) -> EntityCrossReferenceId:
 
 
 PROGRESS_REPORTER__NAME = 'progress'
+JUNIT_REPORTER__NAME = 'junit'
 PROGRESS_REPORTER__CROSS_REF = suite_reporter_cross_ref(PROGRESS_REPORTER__NAME)
+JUNIT_REPORTER__CROSS_REF = suite_reporter_cross_ref(JUNIT_REPORTER__NAME)
 
 ALL_SUITE_REPORTERS__CROSS_REFS = [
-    PROGRESS_REPORTER__CROSS_REF
+    PROGRESS_REPORTER__CROSS_REF,
+    JUNIT_REPORTER__CROSS_REF,
 ]
