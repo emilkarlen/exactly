2322a7a4a8b5da8e35fea4b8324d6bee9b3c993b
Emil Karlén <emil@member.fsf.org>
2016-12-04 14:12:45 +0100

    doc: Refactors tests for "htmldoc"

--- test/exactly_lib_test/default/program_modes/__init__.py
+++ test/exactly_lib_test/default/program_modes/__init__.py
@@ -1,19 +1,21 @@
 import unittest
 
-from exactly_lib_test.default.program_modes import test_case, test_suite, help, html_doc
+from exactly_lib_test.default.program_modes import test_case, test_suite, non_html_doc, html_doc
 from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
 
 
 def suite_that_does_not_require_main_program_runner() -> unittest.TestSuite:
-    return unittest.TestSuite()
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(html_doc.suite_that_does_not_require_main_program_runner())
+    return ret_val
 
 
 def suite_that_does_require_main_program_runner(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_case.suite_for(main_program_runner))
     ret_val.addTest(test_suite.suite_for(main_program_runner))
-    ret_val.addTest(help.suite_for(main_program_runner))
-    ret_val.addTest(html_doc.suite_for(main_program_runner))
+    ret_val.addTest(non_html_doc.suite_for(main_program_runner))
+    ret_val.addTest(html_doc.suite_that_does_require_main_program_runner(main_program_runner))
     return ret_val
 
 
