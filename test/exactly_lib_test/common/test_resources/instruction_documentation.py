958884fc17c44ba83726d8d67dcaadba52c10e60
Emil Karlén <emil.karlen@fripost.org>
2016-12-04 12:51:09 +0100

    reorganization of test resources: Moves InstructionDocumentationWithConstantValues to more suitable place

--- test/exactly_lib_test/common/test_resources/instruction_documentation.py
+++ test/exactly_lib_test/common/test_resources/instruction_documentation.py
@@ -1,7 +1,38 @@
 from exactly_lib.common.instruction_documentation import InstructionDocumentation, SyntaxElementDescription, \
     InvokationVariant
+from exactly_lib.util.textformat import parse as text_parse
 from exactly_lib.util.textformat.structure import structures as docs
-from exactly_lib_test.test_resources.instruction_description import InstructionDocumentationWithConstantValues
+
+
+class InstructionDocumentationWithConstantValues(InstructionDocumentation):
+    def __init__(self,
+                 instruction_name: str,
+                 single_line_description: str,
+                 main_description_rest: str,
+                 invokation_variants: list,
+                 syntax_element_descriptions: iter = ()):
+        """
+        :param invokation_variants: [InvokationVariant]
+        :param syntax_element_descriptions: [SyntaxElementDescription]
+        """
+        super().__init__(instruction_name)
+        self.__single_line_description = single_line_description
+        self.__main_description_rest = main_description_rest
+        self.__invokation_variants = invokation_variants
+        self.__syntax_element_descriptions = list(syntax_element_descriptions)
+
+    def single_line_description(self) -> str:
+        return self.__single_line_description
+
+    def main_description_rest(self) -> list:
+        return text_parse.normalize_and_parse(self.__main_description_rest)
+
+    def invokation_variants(self) -> list:
+        return self.__invokation_variants
+
+    def syntax_element_descriptions(self) -> iter:
+        return self.__syntax_element_descriptions
+
 
 SYNTAX_ELEMENT_DESCRIPTIONS = [
     SyntaxElementDescription('syntax element name',
