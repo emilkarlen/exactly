86b67317022071a03f219e26c4b133869fad18f0
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 11:42:16 +0100

    suite reporter doc: junit: Improves doc of syntax

--- src/exactly_lib/help/suite_reporters/suite_reporter/junit.py
+++ src/exactly_lib/help/suite_reporters/suite_reporter/junit.py
@@ -1,14 +1,16 @@
 from exactly_lib.help.suite_reporters.contents_structure import SuiteReporterDocumentation
 from exactly_lib.help.suite_reporters.names_and_cross_references import JUNIT_REPORTER__NAME
 from exactly_lib.help.utils.textformat_parser import TextParser
-from exactly_lib.test_suite.reporters.junit import UNCONDITIONAL_EXIT_CODE
+from exactly_lib.test_suite.reporters import junit
 
 
 class JunitSuiteReporterDocumentation(SuiteReporterDocumentation):
     def __init__(self):
         super().__init__(JUNIT_REPORTER__NAME)
         format_map = {
-            'EXIT_CODE': str(UNCONDITIONAL_EXIT_CODE),
+            'EXIT_CODE': str(junit.UNCONDITIONAL_EXIT_CODE),
+            'test_suite_element': junit.TEST_SUITE_ELEMENT_NAME,
+            'test_suites_element': junit.TEST_SUITES_ELEMENT_NAME,
         }
         self._parser = TextParser(format_map)
 
@@ -25,11 +27,13 @@ class JunitSuiteReporterDocumentation(SuiteReporterDocumentation):
 _SYNTAX_OF_OUTPUT = """\
 https://windyroad.com.au/dl/Open Source/JUnit.xsd
 
-
 as of 2016-11-14.
 
 
-A suite without sub suites is reported as a
+A suite with sub suites is reported as a '{test_suites_element}' element.
+
+
+A suite without sub suites is reported as a '{test_suite_element}' element.
 """
 
 _EXIT_CODE_DESCRIPTION = """\
