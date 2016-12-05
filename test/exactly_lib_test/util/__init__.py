a1d26e1a66d78a7d84b526f56d66d9329819d415
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 01:50:53 +0100

    Removes unused module

--- test/exactly_lib_test/util/__init__.py
+++ test/exactly_lib_test/util/__init__.py
@@ -1,12 +1,11 @@
 import unittest
 
-from exactly_lib_test.util import functional, line_source, monad, textformat, tables, cli_syntax
+from exactly_lib_test.util import functional, line_source, textformat, tables, cli_syntax
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(functional.suite())
-    ret_val.addTest(monad.suite())
     ret_val.addTest(tables.suite())
     ret_val.addTest(line_source.suite())
     ret_val.addTest(textformat.suite())
