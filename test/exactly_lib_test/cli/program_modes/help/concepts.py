a4215c4f1038154894df103243d9a930388fea01
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 01:37:39 +0100

    doc: cli help command: Includes entity name in help output if it was find via a sub string

--- test/exactly_lib_test/cli/program_modes/help/concepts.py
+++ test/exactly_lib_test/cli/program_modes/help/concepts.py
@@ -6,6 +6,12 @@ from exactly_lib.help.concepts.contents_structure import concepts_help, ConceptD
 from exactly_lib.help.entity_names import CONCEPT_ENTITY_TYPE_NAME
 from exactly_lib.help.utils.render import SectionContentsRenderer
 from exactly_lib_test.help.concepts.test_resources.documentation import ConceptTestImpl
+from exactly_lib_test.help.test_resources.rendering_environment import RENDERING_ENVIRONMENT
+from exactly_lib_test.util.textformat.test_resources import structure as struct_check
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.makeSuite(TestConceptHelpRequestRendererResolver)
 
 
 class TestConceptHelpRequestRendererResolver(unittest.TestCase):
@@ -20,6 +26,9 @@ class TestConceptHelpRequestRendererResolver(unittest.TestCase):
         actual = resolver.renderer_for(_concept_help_request(EntityHelpItem.ALL_ENTITIES_LIST, None))
         # ASSERT #
         self.assertIsInstance(actual, SectionContentsRenderer)
+        section_contents = actual.apply(RENDERING_ENVIRONMENT)
+        struct_check.is_section_contents.apply_with_message(self, section_contents,
+                                                            'output from renderer')
 
     def test_individual_concept(self):
         # ARRANGE #
@@ -29,15 +38,31 @@ class TestConceptHelpRequestRendererResolver(unittest.TestCase):
         ]
         resolver = sut.concept_help_request_renderer_resolver(concepts_help(concepts))
         # ACT #
-        actual = resolver.renderer_for(_concept_help_request(EntityHelpItem.INDIVIDUAL_ENTITY, first_concept))
+        actual = resolver.renderer_for(_concept_help_request(EntityHelpItem.INDIVIDUAL_ENTITY,
+                                                             first_concept,
+                                                             do_include_name_in_output=False))
         # ASSERT #
         self.assertIsInstance(actual, SectionContentsRenderer)
+        section_contents = actual.apply(RENDERING_ENVIRONMENT)
+        struct_check.is_section_contents.apply_with_message(self, section_contents,
+                                                            'output from renderer')
 
-
-def suite() -> unittest.TestSuite:
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestConceptHelpRequestRendererResolver))
-    return ret_val
+    def test_individual_concept_and_include_name_in_output(self):
+        # ARRANGE #
+        first_concept = ConceptTestImpl('first concept')
+        concepts = [
+            first_concept,
+        ]
+        resolver = sut.concept_help_request_renderer_resolver(concepts_help(concepts))
+        # ACT #
+        actual = resolver.renderer_for(_concept_help_request(EntityHelpItem.INDIVIDUAL_ENTITY,
+                                                             first_concept,
+                                                             do_include_name_in_output=True))
+        # ASSERT #
+        self.assertIsInstance(actual, SectionContentsRenderer)
+        section_contents = actual.apply(RENDERING_ENVIRONMENT)
+        struct_check.is_section_contents.apply_with_message(self, section_contents,
+                                                            'output from renderer')
 
 
 if __name__ == '__main__':
@@ -45,5 +70,7 @@ if __name__ == '__main__':
 
 
 def _concept_help_request(item: EntityHelpItem,
-                          individual_concept: ConceptDocumentation = None) -> EntityHelpRequest:
-    return EntityHelpRequest(CONCEPT_ENTITY_TYPE_NAME, item, individual_concept)
+                          individual_concept: ConceptDocumentation = None,
+                          do_include_name_in_output: bool = False) -> EntityHelpRequest:
+    return EntityHelpRequest(CONCEPT_ENTITY_TYPE_NAME, item, individual_concept,
+                             do_include_name_in_output)
