be8cfc06b9f5a6c3f68edbdf9a6ab58855861c70
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 10:50:23 +0100

    Rename of module

--- src/exactly_lib/help/concepts/configuration_parameters/timeout.py
+++ src/exactly_lib/help/concepts/configuration_parameters/timeout.py
@@ -2,7 +2,7 @@ from exactly_lib.default.program_modes.test_case.default_instruction_names impor
 from exactly_lib.help.concepts.contents_structure import ConfigurationParameterDocumentation, Name
 from exactly_lib.help.cross_reference_id import TestCasePhaseInstructionCrossReference
 from exactly_lib.help.utils.phase_names import phase_name_dictionary, CONFIGURATION_PHASE_NAME
-from exactly_lib.help.utils.textformat_parse import TextParser
+from exactly_lib.help.utils.textformat_parser import TextParser
 from exactly_lib.util.description import Description, DescriptionWithSubSections, from_simple_description
 
 
--- src/exactly_lib/help/program_modes/test_case/contents/cli_syntax.py
+++ src/exactly_lib/help/program_modes/test_case/contents/cli_syntax.py
@@ -6,7 +6,7 @@ from exactly_lib.help.concepts.plain_concepts.sandbox import SANDBOX_CONCEPT
 from exactly_lib.help.utils import formatting
 from exactly_lib.help.utils.cli_program_documentation import CliProgramSyntaxDocumentation
 from exactly_lib.help.utils.phase_names import phase_name_dictionary
-from exactly_lib.help.utils.textformat_parse import TextParser
+from exactly_lib.help.utils.textformat_parser import TextParser
 from exactly_lib.util.cli_syntax.elements import argument as arg
 from exactly_lib.util.cli_syntax.elements import cli_program_syntax as cli_syntax
 from exactly_lib.util.description import DescriptionWithSubSections
