ac659f1920afabd8ee2d399559afbde05b260ce0
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 11:18:30 +0100

    suite reporter doc: More doc of "junit" reporter

--- src/exactly_lib/help/suite_reporters/suite_reporter/junit.py
+++ src/exactly_lib/help/suite_reporters/suite_reporter/junit.py
@@ -1,6 +1,8 @@
 from exactly_lib.help.suite_reporters.contents_structure import SuiteReporterDocumentation
 from exactly_lib.help.suite_reporters.names_and_cross_references import JUNIT_REPORTER__NAME
 from exactly_lib.help.utils.textformat_parser import TextParser
+from exactly_lib.util.textformat.parse import normalize_and_parse
+from exactly_lib.util.textformat.structure import structures as docs
 
 
 class JunitSuiteReporterDocumentation(SuiteReporterDocumentation):
@@ -11,4 +13,18 @@ class JunitSuiteReporterDocumentation(SuiteReporterDocumentation):
         self._parser = TextParser(format_map)
 
     def single_line_description_str(self) -> str:
-        return self._parser.format('Prints JUnit xml.')
+        return self._parser.format('Outputs JUnit XML.')
+
+    def syntax_of_output(self) -> list:
+        return normalize_and_parse(_SYNTAX_OF_OUTPUT)
+
+    def exit_code_description(self) -> list:
+        return docs.paras('Unconditionally 0.')
+
+
+_SYNTAX_OF_OUTPUT = """\
+https://windyroad.com.au/dl/Open Source/JUnit.xsd
+
+
+as of 2016-11-14.
+"""
