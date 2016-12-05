52da847cdf42859b0bae171c804d531f531a60f7
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 22:38:24 +0100

    actor doc: Individual actor renderer: Doc about default actor

--- src/exactly_lib/help/actors/render.py
+++ src/exactly_lib/help/actors/render.py
@@ -1,4 +1,5 @@
 from exactly_lib.help.actors.contents_structure import ActorDocumentation
+from exactly_lib.help.entity_names import ACTOR_ENTITY_TYPE_NAME
 from exactly_lib.help.utils.entity_documentation import AllEntitiesListRenderer
 from exactly_lib.help.utils.section_contents_renderer import RenderingEnvironment, SectionContentsRenderer
 from exactly_lib.util.textformat.structure import document as doc
@@ -18,5 +19,13 @@ class IndividualActorRenderer(SectionContentsRenderer):
     def apply(self, environment: RenderingEnvironment) -> doc.SectionContents:
         self.rendering_environment = environment
         initial_paragraphs = [docs.para(self.actor.single_line_description())]
+        initial_paragraphs.extend(self._default_reporter_info())
         sub_sections = []
         return doc.SectionContents(initial_paragraphs, sub_sections)
+
+    def _default_reporter_info(self) -> list:
+        from exactly_lib.help.actors.names_and_cross_references import DEFAULT_ACTOR
+        if self.actor.singular_name() == DEFAULT_ACTOR.singular_name:
+            return docs.paras('This is the default %s.' % ACTOR_ENTITY_TYPE_NAME)
+        else:
+            return []
