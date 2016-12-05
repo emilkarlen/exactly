d94f2a5835c67b7f50db6e667db2f580359b5e57
Emil Karlén <emil@member.fsf.org>
2016-12-05 01:50:53 +0100

    Removes unused module

--- src/exactly_lib/util/monad.py
+++
@@ -1,6 +0,0 @@
-def map_m(success_result, argument, functions: iter):
-    for function in functions:
-        result = function(argument)
-        if result != success_result:
-            return result
-    return success_result
