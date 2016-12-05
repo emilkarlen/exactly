2e654953ca12243a61658bda040b5b3b3713fc40
Emil Karlén <emil.karlen@fripost.org>
2016-12-05 10:49:21 +0100

    suite reporter doc: Rendering of new sections

--- test/exactly_lib_test/help/suite_reporters/test_resources/documentation.py
+++ test/exactly_lib_test/help/suite_reporters/test_resources/documentation.py
@@ -3,11 +3,26 @@ from exactly_lib.help.suite_reporters.contents_structure import SuiteReporterDoc
 
 
 class SuiteReporterDocTestImpl(SuiteReporterDocumentation):
-    def __init__(self, singular_name: str):
+    def __init__(self, singular_name: str,
+                 main_description_rest: list = (),
+                 syntax_of_output: list = (),
+                 exit_code_description: list = ()):
         super().__init__(singular_name)
+        self._main_description_rest = list(main_description_rest)
+        self._syntax_of_output = list(syntax_of_output)
+        self._exit_code_description = list(exit_code_description)
 
     def single_line_description_str(self) -> str:
         return 'single_line_description_str'
+
+    def main_description_rest(self) -> list:
+        return self._main_description_rest
+
+    def syntax_of_output(self) -> list:
+        return self._syntax_of_output
+
+    def exit_code_description(self) -> list:
+        return self._exit_code_description
 
     def _see_also_specific(self) -> list:
         return [CustomCrossReferenceId('custom-cross-reference-target')]
