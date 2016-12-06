984a209448b9308b6ff841546cf4ea81f70d6ec1
Emil Karlén <emil.karlen@fripost.org>
2016-12-06 20:05:35 +0100

    actor/command line: REFACT: Moves test of "executable program file" to new package for "command line" actor

--- test/exactly_lib_test/act_phase_setups/__init__.py
+++ test/exactly_lib_test/act_phase_setups/__init__.py
@@ -1,7 +1,8 @@
 import unittest
 
-from exactly_lib_test.act_phase_setups import source_interpreter, single_command_setup, shell_command, \
-    shell_command_source_interpreter
+from exactly_lib_test.act_phase_setups import source_interpreter, shell_command, \
+    shell_command_source_interpreter, command_line
+from exactly_lib_test.act_phase_setups.command_line import executable_program_file
 from exactly_lib_test.act_phase_setups.util import executor_made_of_parts
 
 
@@ -9,7 +10,7 @@ def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(executor_made_of_parts.suite())
     ret_val.addTest(source_interpreter.suite())
-    ret_val.addTest(single_command_setup.suite())
+    ret_val.addTest(command_line.suite())
     ret_val.addTest(shell_command.suite())
     ret_val.addTest(shell_command_source_interpreter.suite())
     return ret_val
