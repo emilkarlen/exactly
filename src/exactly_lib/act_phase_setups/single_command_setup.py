d3e4b39da4fd699e835faf0ea3a22ef7bbc0d0b8
Emil Karlén <emil@member.fsf.org>
2016-12-06 21:03:34 +0100

    actor/command line: Makes command line actor handle both executable files and shell commands

--- src/exactly_lib/act_phase_setups/single_command_setup.py
+++ src/exactly_lib/act_phase_setups/single_command_setup.py
@@ -1,8 +1,10 @@
 import pathlib
+import shlex
 
 from exactly_lib.act_phase_setups.util.executor_made_of_parts import parts
 from exactly_lib.act_phase_setups.util.executor_made_of_parts.parser_for_single_line import \
-    ParserForSingleLineUsingStandardSyntaxSplitAccordingToShellSyntax
+    ParserForSingleLineUsingStandardSyntax
+from exactly_lib.act_phase_setups.util.executor_made_of_parts.parts import Parser
 from exactly_lib.act_phase_setups.util.executor_made_of_parts.sub_process_executor import CommandExecutor
 from exactly_lib.processing.act_phase import ActPhaseSetup
 from exactly_lib.test_case.act_phase_handling import ActPhaseOsProcessExecutor
@@ -20,15 +22,53 @@ def act_phase_setup() -> ActPhaseSetup:
 
 class Constructor(parts.Constructor):
     def __init__(self):
-        super().__init__(ParserForSingleLineUsingStandardSyntaxSplitAccordingToShellSyntax(),
-                         Validator,
-                         Executor)
+        super().__init__(_Parser(),
+                         _validator,
+                         _executor)
 
 
-class Validator(parts.Validator):
-    def __init__(self,
-                 environment: InstructionEnvironmentForPreSdsStep,
-                 cmd_and_args: list):
+class _Parser(Parser):
+    def apply(self, act_phase_instructions: list) -> Command:
+        single_line_parser = ParserForSingleLineUsingStandardSyntax()
+        single_line = single_line_parser.apply(act_phase_instructions)
+        single_line = single_line.strip()
+        if single_line.startswith(SHELL_COMMAND_MARKER):
+            return self._parse_shell_command(single_line[len(SHELL_COMMAND_MARKER):])
+        else:
+            return self._parse_executable_file(single_line)
+
+    @staticmethod
+    def _parse_shell_command(argument: str) -> Command:
+        striped_argument = argument.strip()
+        if not striped_argument:
+            msg = SHELL_COMMAND_MARKER + ': command string is missing.'
+            raise parts.ParseException(svh.new_svh_validation_error(msg))
+        return Command(striped_argument, shell=True)
+
+    @staticmethod
+    def _parse_executable_file(argument: str) -> Command:
+        cmd_and_args = shlex.split(argument)
+        return Command(cmd_and_args, shell=False)
+
+
+def _validator(environment: InstructionEnvironmentForPreSdsStep, command: Command) -> parts.Validator:
+    if command.shell:
+        return parts.UnconditionallySuccessfulValidator()
+    else:
+        return _ExecutableFileValidator(command.args)
+
+
+def _executor(os_process_executor: ActPhaseOsProcessExecutor,
+              environment: InstructionEnvironmentForPreSdsStep,
+              command: Command) -> parts.Executor:
+    if command.shell:
+        return _ShellCommandExecutor(os_process_executor, command.args)
+    else:
+        return _ExecutableFileExecutor(os_process_executor, command.args)
+
+
+class _ExecutableFileValidator(parts.Validator):
+    def __init__(self, cmd_and_args: list):
         self.cmd_and_args = cmd_and_args
 
     def validate_pre_sds(self,
@@ -49,10 +89,9 @@ class Validator(parts.Validator):
         return svh.new_svh_success()
 
 
-class Executor(CommandExecutor):
+class _ExecutableFileExecutor(CommandExecutor):
     def __init__(self,
                  os_process_executor: ActPhaseOsProcessExecutor,
-                 environment: InstructionEnvironmentForPreSdsStep,
                  cmd_and_args: list):
         super().__init__(os_process_executor)
         self.cmd_and_args = cmd_and_args
@@ -66,3 +105,16 @@ class Executor(CommandExecutor):
             cmd_path = environment.home_directory / cmd_path
             self.cmd_and_args[0] = str(cmd_path)
         return Command(self.cmd_and_args, shell=False)
+
+
+class _ShellCommandExecutor(CommandExecutor):
+    def __init__(self,
+                 os_process_executor: ActPhaseOsProcessExecutor,
+                 command_line: str):
+        super().__init__(os_process_executor)
+        self.command_line = command_line
+
+    def _command_to_execute(self,
+                            environment: InstructionEnvironmentForPostSdsStep,
+                            script_output_dir_path: pathlib.Path) -> Command:
+        return Command(self.command_line, shell=True)
