4b7a6b37114d8573ffbf3756e9a12e3803a3f405
Emil Karlén <emil.karlen@fripost.org>
2016-12-07 23:50:05 +0100

    actor/interpretation: Adapts command line parsing of actor to new interpreter actor

--- src/exactly_lib/cli/argument_parsing_of_act_phase_setup.py
+++ src/exactly_lib/cli/argument_parsing_of_act_phase_setup.py
@@ -1,8 +1,8 @@
-from exactly_lib.act_phase_setups.interpreter import python3
-from exactly_lib.act_phase_setups.interpreter import source_file_management
-from exactly_lib.act_phase_setups.interpreter.executable_file import new_for_script_language_setup
-from exactly_lib.act_phase_setups.interpreter.source_file_management import SourceInterpreterSetup
+import shlex
+
+from exactly_lib.act_phase_setups.interpreter import python3, act_phase_setup
 from exactly_lib.processing.act_phase import ActPhaseSetup
+from exactly_lib.util.process_execution.os_process_execution import Command
 
 INTERPRETER_FOR_TEST = 'test-language'
 
@@ -21,13 +21,11 @@ def resolve_act_phase_setup(default_setup: ActPhaseSetup,
         if interpreter == INTERPRETER_FOR_TEST:
             return python3.new_act_phase_setup()
         else:
-            return new_for_generic_script_language_setup(interpreter)
+            return _new_for_generic_script_language_setup(interpreter)
     return default_setup
 
 
-def new_for_generic_script_language_setup(interpreter: str) -> ActPhaseSetup:
-    return new_for_script_language_setup(
-        SourceInterpreterSetup(source_file_management.StandardSourceFileManager(
-            'src',
-            interpreter,
-            [])))
+def _new_for_generic_script_language_setup(interpreter: str) -> ActPhaseSetup:
+    cmd_and_args = shlex.split(interpreter)
+    command = Command(cmd_and_args, shell=False)
+    return act_phase_setup(command)
