b9ce1ac514947895209f8757ead9b01d858dddfb
Emil Karlén <emil.karlen@fripost.org>
2016-12-07 23:57:11 +0100

    actor/interpretation: Combines interpreters for shell/executable into single actor (tests)

--- test/exactly_lib_test/act_phase_setups/__init__.py
+++ test/exactly_lib_test/act_phase_setups/__init__.py
@@ -1,16 +1,14 @@
 import unittest
 
-from exactly_lib_test.act_phase_setups import command_line, source_interpreter, shell_command_source_interpreter
-from exactly_lib_test.act_phase_setups.command_line import executable_program_file
+from exactly_lib_test.act_phase_setups import command_line, interpreter, util
 from exactly_lib_test.act_phase_setups.util import executor_made_of_parts
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(executor_made_of_parts.suite())
-    ret_val.addTest(source_interpreter.suite())
     ret_val.addTest(command_line.suite())
-    ret_val.addTest(shell_command_source_interpreter.suite())
+    ret_val.addTest(interpreter.suite())
     return ret_val
 
 
