6d57968de6f31e2d607c3cf75dda675303bb2307
Emil Karlén <emil.karlen@fripost.org>
2016-12-11 22:36:10 +0100

    assert/file contents: matches, version 0.1

--- test/exactly_lib_test/instructions/assert_/stdout_stderr.py
+++ test/exactly_lib_test/instructions/assert_/stdout_stderr.py
@@ -1,7 +1,7 @@
 import unittest
 
 from exactly_lib.instructions.assert_ import stdout_stderr as sut
-from exactly_lib.instructions.assert_.utils.contents_utils import EQUALS_ARGUMENT
+from exactly_lib.instructions.assert_.utils.contents_utils import EQUALS_ARGUMENT, MATCHES_ARGUMENT
 from exactly_lib.instructions.utils.arg_parse.relative_path_options import REL_HOME_OPTION
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException, SingleInstructionParser, SingleInstructionParserSource
@@ -19,6 +19,8 @@ from exactly_lib_test.test_resources.execution.sds_populator import act_dir_cont
 from exactly_lib_test.test_resources.execution.utils import ActResult
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir, File
 from exactly_lib_test.test_resources.parse import new_source2, argument_list_source
+from exactly_lib_test.test_resources.test_case_base_with_short_description import \
+    TestCaseBaseWithShortDescriptionOfTestClassAndAnObjectType
 
 
 class TestWithParserBase(instruction_check.TestCaseBase):
@@ -30,6 +32,64 @@ class TestWithParserBase(instruction_check.TestCaseBase):
              arrangement: ArrangementPostAct,
              expectation: Expectation):
         self._check(self._new_parser(), source, arrangement, expectation)
+
+
+class TestConfiguration:
+    def new_parser(self) -> SingleInstructionParser:
+        raise NotImplementedError()
+
+    def act_result(self,
+                   exit_code: int = 0,
+                   contents_of_tested_file: str = '',
+                   contents_of_other_file: str = '') -> ActResult:
+        raise NotImplementedError()
+
+
+class TestConfigurationForStdout(TestConfiguration):
+    def new_parser(self) -> SingleInstructionParser:
+        return sut.ParserForContentsForStdout()
+
+    def act_result(self,
+                   exit_code: int = 0,
+                   contents_of_tested_file: str = '',
+                   contents_of_other_file: str = '') -> ActResult:
+        return ActResult(exitcode=exit_code,
+                         stdout_contents=contents_of_tested_file,
+                         stderr_contents=contents_of_other_file)
+
+
+class TestConfigurationForStderr(TestConfiguration):
+    def new_parser(self) -> SingleInstructionParser:
+        return sut.ParserForContentsForStderr()
+
+    def act_result(self,
+                   exit_code: int = 0,
+                   contents_of_tested_file: str = '',
+                   contents_of_other_file: str = '') -> ActResult:
+        return ActResult(exitcode=exit_code,
+                         stderr_contents=contents_of_tested_file,
+                         stdout_contents=contents_of_other_file)
+
+
+class TestWithConfigurationBase(TestCaseBaseWithShortDescriptionOfTestClassAndAnObjectType):
+    def __init__(self, configuration: TestConfiguration):
+        super().__init__(configuration)
+        self.configuration = configuration
+
+    def _check(self,
+               source: SingleInstructionParserSource,
+               arrangement: ArrangementPostAct,
+               expectation: Expectation):
+        instruction_check.check(self, self.configuration.new_parser(), source, arrangement, expectation)
+
+    def _act_result_producer(self,
+                             exit_code: int = 0,
+                             contents_of_tested_file: str = '',
+                             contents_of_other_file: str = '') -> ActResultProducer:
+        act_result = self.configuration.act_result(exit_code=exit_code,
+                                                   contents_of_tested_file=contents_of_tested_file,
+                                                   contents_of_other_file=contents_of_other_file)
+        return ActResultProducer(act_result)
 
 
 class FileContentsEmptyInvalidSyntax(TestWithParserBase):
@@ -189,6 +249,78 @@ class TestFileContentsNonEmptyValidSyntaxFORStderr(FileContentsNonEmptyValidSynt
     def test_pass__when__file_exists_and_is_non_empty(self):
         self.pass__when__file_exists_and_is_non_empty(ActResult(stdout_contents='',
                                                                 stderr_contents='non-empty'))
+
+
+class TestParseMatchesWithMissingRegExArgument(TestWithConfigurationBase):
+    def runTest(self):
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            self.configuration.new_parser().apply(new_source2(MATCHES_ARGUMENT))
+
+
+class TestParseMatchesWithSuperfluousArgument(TestWithConfigurationBase):
+    def runTest(self):
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            self.configuration.new_parser().apply(new_source2('{} abc superfluous'.format(MATCHES_ARGUMENT)))
+
+
+class TestParseMatchesWithInvalidRegEx(TestWithConfigurationBase):
+    def runTest(self):
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            self.configuration.new_parser().apply(new_source2('{} **'.format(MATCHES_ARGUMENT)))
+
+
+class TestMatchesShouldFailWhenNoLineMatchesRegEx(TestWithConfigurationBase):
+    def runTest(self):
+        actual_contents = lines_content(['no match',
+                                         'NO MATCH',
+                                         'not match'])
+        act_result_producer = self._act_result_producer(contents_of_tested_file=actual_contents)
+        reg_ex = '123'
+        self._check(
+            new_source2("%s '%s'" % (MATCHES_ARGUMENT, reg_ex)),
+            arrangement(act_result_producer=act_result_producer),
+            Expectation(main_result=pfh_check.is_fail()),
+        )
+
+
+class TestMatchesShouldPassWhenALineMatchesRegEx(TestWithConfigurationBase):
+    def runTest(self):
+        actual_contents = lines_content(['no match',
+                                         'MATCH',
+                                         'not match'])
+        act_result_producer = self._act_result_producer(contents_of_tested_file=actual_contents)
+        reg_ex = 'ATC'
+        self._check(
+            new_source2("%s '%s'" % (MATCHES_ARGUMENT, reg_ex)),
+            arrangement(act_result_producer=act_result_producer),
+            Expectation(main_result=pfh_check.is_pass()),
+        )
+
+
+class TestMatchesShouldPassWhenAWholeLineMatchesRegEx(TestWithConfigurationBase):
+    def runTest(self):
+        actual_contents = lines_content(['no match',
+                                         'MATCH',
+                                         'not match'])
+        act_result_producer = self._act_result_producer(contents_of_tested_file=actual_contents)
+        reg_ex = '^MATCH$'
+        self._check(
+            new_source2("%s '%s'" % (MATCHES_ARGUMENT, reg_ex)),
+            arrangement(act_result_producer=act_result_producer),
+            Expectation(main_result=pfh_check.is_pass()),
+        )
+
+
+def suite_for_matches(configuration: TestConfiguration) -> unittest.TestSuite:
+    test_cases = [
+        TestParseMatchesWithMissingRegExArgument,
+        TestParseMatchesWithSuperfluousArgument,
+        TestParseMatchesWithInvalidRegEx,
+        TestMatchesShouldFailWhenNoLineMatchesRegEx,
+        TestMatchesShouldPassWhenALineMatchesRegEx,
+        TestMatchesShouldPassWhenAWholeLineMatchesRegEx,
+    ]
+    return unittest.TestSuite([tc(configuration) for tc in test_cases])
 
 
 class FileContentsFileRelHome(TestWithParserBase):
@@ -572,6 +704,9 @@ def suite() -> unittest.TestSuite:
 
         unittest.makeSuite(TestFileContentsNonEmptyValidSyntaxFORStdout),
         unittest.makeSuite(TestFileContentsNonEmptyValidSyntaxFORStderr),
+
+        suite_for_matches(TestConfigurationForStdout()),
+        suite_for_matches(TestConfigurationForStderr()),
 
         unittest.makeSuite(TestFileContentsFileRelHomeFORStdout),
         unittest.makeSuite(TestFileContentsFileRelHomeFORStderr),
