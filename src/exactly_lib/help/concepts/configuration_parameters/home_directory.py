97bbfb81d81927095b96da2813450e6b116ea648
Emil Karlén <emil.karlen@etraveli.com>
2016-12-11 16:11:33 +0100

    doc: Improves help text of "home directory".

--- src/exactly_lib/help/concepts/configuration_parameters/home_directory.py
+++ src/exactly_lib/help/concepts/configuration_parameters/home_directory.py
@@ -10,13 +10,14 @@ from exactly_lib.util.textformat.structure.structures import text
 
 class _HomeDirectoryConfigurationParameter(ConfigurationParameterDocumentation):
     def __init__(self):
-        super().__init__(Name('home directory', 'home directories'))
+        super().__init__(_NAME)
 
     def purpose(self) -> DescriptionWithSubSections:
         return from_simple_description(
             Description(text(_HOME_DIRECTORY_SINGLE_LINE_DESCRIPTION),
                         normalize_and_parse(_HOME_DIRECTORY_REST_DESCRIPTION
                                             .format(phase=phase_name_dictionary(),
+                                                    the_concept=_NAME.singular,
                                                     home_dir_env_var=ENV_VAR_HOME))))
 
     def default_value_str(self) -> str:
@@ -29,12 +30,13 @@ class _HomeDirectoryConfigurationParameter(ConfigurationParameterDocumentation):
         ]
 
 
+_NAME = Name('home directory', 'home directories')
+
 HOME_DIRECTORY_CONFIGURATION_PARAMETER = _HomeDirectoryConfigurationParameter()
 _HOME_DIRECTORY_SINGLE_LINE_DESCRIPTION = """\
 Default location of (external, existing) files referenced from the test case."""
-_HOME_DIRECTORY_REST_DESCRIPTION = """\
-TODO (improve description!)
 
+_HOME_DIRECTORY_REST_DESCRIPTION = """\
 Instructions and phases may use predefined input in terms of files
 that are supposed to exist before the test case is executed.
 
@@ -42,7 +44,7 @@ E.g., the {phase[act]} phase (by default) references an program that is expected
 to be an executable file.
 
 
-If the path to this file is relative, then it is relative the Home Directory.
+If the path to this file is relative, then it is relative the {the_concept}.
 
 
 Many instructions use exiting files. E.g. for installing them into the
