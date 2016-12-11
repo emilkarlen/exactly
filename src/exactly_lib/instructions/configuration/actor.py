265d7d17ce80f503e99ea51b3c5462075a09e323
Emil Karlén <emil.karlen@etraveli.com>
2016-12-11 19:32:43 +0100

    Documents precedence of different ways to specify the "actor"

--- src/exactly_lib/instructions/configuration/actor.py
+++ src/exactly_lib/instructions/configuration/actor.py
@@ -10,10 +10,16 @@ from exactly_lib.test_case.phases.result import sh
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(Parser(),
                                   actor_utils.InstructionDocumentation(instruction_name,
-                                                                       _SINGLE_LINE_DESCRIPTION_UNFORMATTED))
+                                                                       _SINGLE_LINE_DESCRIPTION_UNFORMATTED,
+                                                                       _MAIN_DESCRIPTION_REST_UNFORMATTED))
 
 
 _SINGLE_LINE_DESCRIPTION_UNFORMATTED = 'Specifies the {actor} that will execute the {act_phase} phase'
+
+_MAIN_DESCRIPTION_REST_UNFORMATTED = """\
+The {actor} specified by this instruction has precedence over all other ways
+to specify the actor.
+"""
 
 
 class Parser(SingleInstructionParser):
