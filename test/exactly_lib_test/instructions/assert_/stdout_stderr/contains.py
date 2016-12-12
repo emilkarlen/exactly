649b89d0278bb5a4eaeff7cf8b5ecca122ccb877
Emil Karlén <emil.karlen@fripost.org>
2016-12-12 20:33:47 +0100

    assert/stdout,stderr: REFACT: Improves tests of "matches"

--- test/exactly_lib_test/instructions/assert_/stdout_stderr/contains.py
+++ test/exactly_lib_test/instructions/assert_/stdout_stderr/contains.py
@@ -7,7 +7,7 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 from exactly_lib.util.string import lines_content
 from exactly_lib_test.instructions.assert_.test_resources import instruction_check
 from exactly_lib_test.instructions.assert_.test_resources.instruction_check import ActResultProducer, \
-    arrangement, Expectation
+    Expectation
 from exactly_lib_test.instructions.test_resources.arrangements import ArrangementPostAct
 from exactly_lib_test.instructions.test_resources.assertion_utils import pfh_check
 from exactly_lib_test.test_resources.execution.utils import ActResult
@@ -20,37 +20,40 @@ class TestConfiguration:
     def new_parser(self) -> SingleInstructionParser:
         raise NotImplementedError()
 
-    def act_result(self,
-                   exit_code: int = 0,
-                   contents_of_tested_file: str = '',
-                   contents_of_other_file: str = '') -> ActResult:
+    def source_for(self, argument_tail: str) -> SingleInstructionParserSource:
+        raise NotImplementedError()
+
+    def arrangement_for_contents(self, actual_contents: str) -> instruction_check.ArrangementPostAct:
         raise NotImplementedError()
 
 
-class TestConfigurationForStdout(TestConfiguration):
+class TestConfigurationForStdFile(TestConfiguration):
+    def source_for(self, argument_tail: str) -> SingleInstructionParserSource:
+        return new_source2(argument_tail)
+
+    def arrangement_for_contents(self, actual_contents: str) -> instruction_check.ArrangementPostAct:
+        act_result = self.act_result(actual_contents)
+        act_result_producer = ActResultProducer(act_result)
+        return instruction_check.ArrangementPostAct(act_result_producer=act_result_producer)
+
+    def act_result(self, contents_of_tested_file: str) -> ActResult:
+        raise NotImplementedError()
+
+
+class TestConfigurationForStdout(TestConfigurationForStdFile):
     def new_parser(self) -> SingleInstructionParser:
         return sut.ParserForContentsForStdout()
 
-    def act_result(self,
-                   exit_code: int = 0,
-                   contents_of_tested_file: str = '',
-                   contents_of_other_file: str = '') -> ActResult:
-        return ActResult(exitcode=exit_code,
-                         stdout_contents=contents_of_tested_file,
-                         stderr_contents=contents_of_other_file)
+    def act_result(self, contents_of_tested_file: str) -> ActResult:
+        return ActResult(stdout_contents=contents_of_tested_file)
 
 
-class TestConfigurationForStderr(TestConfiguration):
+class TestConfigurationForStderr(TestConfigurationForStdFile):
     def new_parser(self) -> SingleInstructionParser:
         return sut.ParserForContentsForStderr()
 
-    def act_result(self,
-                   exit_code: int = 0,
-                   contents_of_tested_file: str = '',
-                   contents_of_other_file: str = '') -> ActResult:
-        return ActResult(exitcode=exit_code,
-                         stderr_contents=contents_of_tested_file,
-                         stdout_contents=contents_of_other_file)
+    def act_result(self, contents_of_tested_file: str) -> ActResult:
+        return ActResult(stderr_contents=contents_of_tested_file)
 
 
 class TestWithConfigurationBase(TestCaseBaseWithShortDescriptionOfTestClassAndAnObjectType):
@@ -64,32 +67,26 @@ class TestWithConfigurationBase(TestCaseBaseWithShortDescriptionOfTestClassAndAn
                expectation: Expectation):
         instruction_check.check(self, self.configuration.new_parser(), source, arrangement, expectation)
 
-    def _act_result_producer(self,
-                             exit_code: int = 0,
-                             contents_of_tested_file: str = '',
-                             contents_of_other_file: str = '') -> ActResultProducer:
-        act_result = self.configuration.act_result(exit_code=exit_code,
-                                                   contents_of_tested_file=contents_of_tested_file,
-                                                   contents_of_other_file=contents_of_other_file)
-        return ActResultProducer(act_result)
-
 
 class TestParseMatchesWithMissingRegExArgument(TestWithConfigurationBase):
     def runTest(self):
         with self.assertRaises(SingleInstructionInvalidArgumentException):
-            self.configuration.new_parser().apply(new_source2(MATCHES_ARGUMENT))
+            self.configuration.new_parser().apply(
+                self.configuration.source_for(args('{matches}')))
 
 
 class TestParseMatchesWithSuperfluousArgument(TestWithConfigurationBase):
     def runTest(self):
         with self.assertRaises(SingleInstructionInvalidArgumentException):
-            self.configuration.new_parser().apply(new_source2('{} abc superfluous'.format(MATCHES_ARGUMENT)))
+            self.configuration.new_parser().apply(
+                self.configuration.source_for(args('{matches} abc superfluous')))
 
 
 class TestParseMatchesWithInvalidRegEx(TestWithConfigurationBase):
     def runTest(self):
         with self.assertRaises(SingleInstructionInvalidArgumentException):
-            self.configuration.new_parser().apply(new_source2('{} **'.format(MATCHES_ARGUMENT)))
+            self.configuration.new_parser().apply(
+                self.configuration.source_for('{matches} **'))
 
 
 class TestMatchesShouldFailWhenNoLineMatchesRegEx(TestWithConfigurationBase):
@@ -97,11 +94,10 @@ class TestMatchesShouldFailWhenNoLineMatchesRegEx(TestWithConfigurationBase):
         actual_contents = lines_content(['no match',
                                          'NO MATCH',
                                          'not match'])
-        act_result_producer = self._act_result_producer(contents_of_tested_file=actual_contents)
         reg_ex = '123'
         self._check(
-            new_source2("%s '%s'" % (MATCHES_ARGUMENT, reg_ex)),
-            arrangement(act_result_producer=act_result_producer),
+            self.configuration.source_for(args("{matches} '{reg_ex}'", reg_ex)),
+            self.configuration.arrangement_for_contents(actual_contents),
             Expectation(main_result=pfh_check.is_fail()),
         )
 
@@ -111,11 +107,10 @@ class TestMatchesShouldPassWhenALineMatchesRegEx(TestWithConfigurationBase):
         actual_contents = lines_content(['no match',
                                          'MATCH',
                                          'not match'])
-        act_result_producer = self._act_result_producer(contents_of_tested_file=actual_contents)
         reg_ex = 'ATC'
         self._check(
-            new_source2("%s '%s'" % (MATCHES_ARGUMENT, reg_ex)),
-            arrangement(act_result_producer=act_result_producer),
+            self.configuration.source_for(args("{matches} '{reg_ex}'", reg_ex)),
+            self.configuration.arrangement_for_contents(actual_contents),
             Expectation(main_result=pfh_check.is_pass()),
         )
 
@@ -125,11 +120,10 @@ class TestMatchesShouldPassWhenAWholeLineMatchesRegEx(TestWithConfigurationBase)
         actual_contents = lines_content(['no match',
                                          'MATCH',
                                          'not match'])
-        act_result_producer = self._act_result_producer(contents_of_tested_file=actual_contents)
         reg_ex = '^MATCH$'
         self._check(
-            new_source2("%s '%s'" % (MATCHES_ARGUMENT, reg_ex)),
-            arrangement(act_result_producer=act_result_producer),
+            self.configuration.source_for(args("{matches} '{reg_ex}'", reg_ex)),
+            self.configuration.arrangement_for_contents(actual_contents),
             Expectation(main_result=pfh_check.is_pass()),
         )
 
@@ -151,6 +145,14 @@ def suite() -> unittest.TestSuite:
         suite_for_matches(TestConfigurationForStdout()),
         suite_for_matches(TestConfigurationForStderr()),
     ])
+
+
+def args(pattern: str, reg_ex: str = None) -> str:
+    if reg_ex is None:
+        return pattern.format(matches=MATCHES_ARGUMENT)
+    else:
+        return pattern.format(matches=MATCHES_ARGUMENT,
+                              reg_ex=reg_ex)
 
 
 if __name__ == '__main__':
