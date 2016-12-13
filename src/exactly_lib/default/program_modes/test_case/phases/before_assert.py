8bcbe3bc317b8bda08d9786bd547442b984a03b6
Emil Karlén <emil.karlen@fripost.org>
2016-12-13 20:23:43 +0100

    shell instruction: Changes name "shell" -> "$"

--- src/exactly_lib/default/program_modes/test_case/phases/before_assert.py
+++ src/exactly_lib/default/program_modes/test_case/phases/before_assert.py
@@ -1,5 +1,6 @@
 from exactly_lib.common.instruction_setup import instruction_set_from_name_and_setup_constructor_list
-from exactly_lib.default.program_modes.test_case.default_instruction_names import CHANGE_DIR_INSTRUCTION_NAME
+from exactly_lib.default.program_modes.test_case.default_instruction_names import CHANGE_DIR_INSTRUCTION_NAME, \
+    SHELL_INSTRUCTION_NAME
 from exactly_lib.instructions.before_assert import change_dir, env, run, shell, new_dir
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
@@ -8,6 +9,6 @@ INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
         ('env', env.setup),
         ('run', run.setup),
         (CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
-        ('shell', shell.setup),
+        (SHELL_INSTRUCTION_NAME, shell.setup),
     ]
 )
--- src/exactly_lib/default/program_modes/test_case/phases/setup.py
+++ src/exactly_lib/default/program_modes/test_case/phases/setup.py
@@ -1,5 +1,6 @@
 from exactly_lib.common.instruction_setup import instruction_set_from_name_and_setup_constructor_list
-from exactly_lib.default.program_modes.test_case.default_instruction_names import CHANGE_DIR_INSTRUCTION_NAME
+from exactly_lib.default.program_modes.test_case.default_instruction_names import CHANGE_DIR_INSTRUCTION_NAME, \
+    SHELL_INSTRUCTION_NAME
 from exactly_lib.instructions.setup import change_dir, env, run, install, new_dir, new_file, shell, stdin
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
@@ -10,7 +11,7 @@ INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
         ('file', new_file.setup),
         ('install', install.setup),
         (CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
-        ('shell', shell.setup),
+        (SHELL_INSTRUCTION_NAME, shell.setup),
         ('stdin', stdin.setup),
     ]
 )
