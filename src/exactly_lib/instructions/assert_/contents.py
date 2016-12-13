d819fe88ced61a50aec5505f5c1fde024cbf5b5e
Emil Karlén <emil@member.fsf.org>
2016-12-13 19:11:18 +0100

    assert/file contents: REFACT: Extracts common parts of Instruction documentation

--- src/exactly_lib/instructions/assert_/contents.py
+++ src/exactly_lib/instructions/assert_/contents.py
@@ -2,17 +2,13 @@ import pathlib
 
 from exactly_lib.common.help.syntax_contents_structure import InvokationVariant, SyntaxElementDescription
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
-from exactly_lib.help.concepts.contents_structure import ConceptDocumentation
-from exactly_lib.help.concepts.plain_concepts.environment_variable import ENVIRONMENT_VARIABLE_CONCEPT
-from exactly_lib.instructions.assert_.utils.file_contents import contents_utils_for_instr_doc as doc_utils
 from exactly_lib.instructions.assert_.utils.file_contents import parsing
 from exactly_lib.instructions.assert_.utils.file_contents.actual_file_transformers import \
     ActualFileTransformerForEnvVarsReplacementBase
 from exactly_lib.instructions.assert_.utils.file_contents.actual_files import ComparisonActualFile, \
     ActComparisonActualFileForFileRef
-from exactly_lib.instructions.assert_.utils.file_contents.parsing import with_replaced_env_vars_help
+from exactly_lib.instructions.assert_.utils.file_contents.contents_utils_for_instr_doc import FileContentsHelpParts
 from exactly_lib.instructions.utils.arg_parse import parse_file_ref
-from exactly_lib.instructions.utils.arg_parse import parse_here_doc_or_file_ref
 from exactly_lib.instructions.utils.arg_parse.parse_utils import split_arguments_list_string
 from exactly_lib.instructions.utils.documentation import documentation_text as dt
 from exactly_lib.instructions.utils.documentation import relative_path_options_documentation as rel_opts
@@ -36,18 +32,18 @@ def setup(instruction_name: str) -> SingleInstructionSetup:
 
 class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderingBase):
     def __init__(self, name: str):
-        self.expected_file_arg = a.Named(parse_here_doc_or_file_ref.CONFIGURATION.argument_syntax_name)
         self.actual_file_arg = a.Named('ACTUAL-PATH')
-        self.expected_file_arg = a.Named('EXPECTED-FILE')
         super().__init__(name, {
             'checked_file': self.actual_file_arg.name,
-            'EXPECTED_FILE_ARG': self.expected_file_arg.name,
         })
+        self.actual_file = a.Single(a.Multiplicity.MANDATORY,
+                                    self.actual_file_arg)
+        self._help_parts = FileContentsHelpParts(name,
+                                                 self.actual_file_arg.name,
+                                                 [self.actual_file])
         self.with_replaced_env_vars_option = a.Option(parsing.WITH_REPLACED_ENV_VARS_OPTION_NAME)
         self.actual_file_relativity = a.Single(a.Multiplicity.OPTIONAL,
                                                a.Named('ACTUAL-REL'))
-        self.actual_file = a.Single(a.Multiplicity.MANDATORY,
-                                    self.actual_file_arg)
 
     def single_line_description(self) -> str:
         return 'Tests the contents of a file'
@@ -58,36 +54,7 @@ class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderi
         """)
 
     def invokation_variants(self) -> list:
-        mandatory_empty_arg = a.Single(a.Multiplicity.MANDATORY,
-                                       doc_utils.EMPTY_ARGUMENT_CONSTANT)
-        mandatory_not_arg = a.Single(a.Multiplicity.MANDATORY,
-                                     doc_utils.NOT_ARGUMENT_CONSTANT)
-        expected_file_arg = a.Single(a.Multiplicity.MANDATORY,
-                                     self.expected_file_arg)
-        optional_replace_env_vars_option = a.Single(a.Multiplicity.OPTIONAL,
-                                                    self.with_replaced_env_vars_option)
-        here_doc_arg = a.Single(a.Multiplicity.MANDATORY, dt.HERE_DOCUMENT)
-        return [
-            InvokationVariant(self._cls([mandatory_empty_arg]),
-                              self._paragraphs('Asserts that {checked_file} is an empty file.')),
-            InvokationVariant(self._cls([mandatory_not_arg, mandatory_empty_arg]),
-                              self._paragraphs('Asserts that {checked_file} is a non-empty file.')),
-            InvokationVariant(self._cls([optional_replace_env_vars_option,
-                                         here_doc_arg,
-                                         ]),
-                              self._paragraphs("""\
-                              Asserts that the contents of {checked_file}
-                              is equal to the contents of a "here document".
-                              """)),
-            InvokationVariant(self._cls([optional_replace_env_vars_option,
-                                         # rel_opts.OPTIONAL_RELATIVITY_ARGUMENT_USAGE,
-                                         expected_file_arg,
-                                         ]),
-                              self._paragraphs("""\
-                              Asserts that the contents of {checked_file}
-                              is equal to the contents of {EXPECTED_FILE_ARG}.
-                              """)),
-        ]
+        return self._help_parts.invokation_variants()
 
     def _cls(self, additional_argument_usages: list) -> str:
         return self._cl_syntax_for_args([self.actual_file] + additional_argument_usages)
@@ -98,10 +65,7 @@ class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderi
         relativity_of_actual_arg = a.Named('RELATIVITY-OF-ACTUAL-FILE')
         optional_relativity_of_actual = a.Single(a.Multiplicity.OPTIONAL,
                                                  relativity_of_actual_arg)
-        relativity_of_expected_arg = a.Named('RELATIVITY-OF-EXPECTED-FILE')
-        optional_relativity_of_expected = a.Single(a.Multiplicity.OPTIONAL,
-                                                   relativity_of_expected_arg)
-        return [
+        additional_syntax_element_descriptions = [
             SyntaxElementDescription(self.actual_file_arg.name,
                                      self._paragraphs("The file who's contents is checked."),
                                      [InvokationVariant(self._cl_syntax_for_args(
@@ -111,34 +75,14 @@ class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderi
                                              dt.PATH_ARGUMENT.name,
                                              _ACTUAL_RELATIVITY_CONFIGURATION.default_option))]
                                      ),
-            SyntaxElementDescription(self.expected_file_arg.name,
-                                     self._paragraphs("The file that contains the expected contents."),
-                                     [InvokationVariant(self._cl_syntax_for_args(
-                                         [optional_relativity_of_expected,
-                                          mandatory_path]),
-                                         rel_opts.default_relativity_for_rel_opt_type(
-                                             parse_here_doc_or_file_ref.CONFIGURATION.argument_syntax_name,
-                                             parse_here_doc_or_file_ref.CONFIGURATION.default_option)
-                                     )]
-                                     ),
-            rel_opts.relativity_syntax_element_description(dt.PATH_ARGUMENT,
-                                                           parse_here_doc_or_file_ref.CONFIGURATION.accepted_options,
-                                                           relativity_of_expected_arg),
             rel_opts.relativity_syntax_element_description(dt.PATH_ARGUMENT,
                                                            _ACTUAL_RELATIVITY_CONFIGURATION.accepted_options,
                                                            relativity_of_actual_arg),
-            self._cli_argument_syntax_element_description(self.with_replaced_env_vars_option,
-                                                          with_replaced_env_vars_help('the contents of ' +
-                                                                                      self.actual_file_arg.name)),
-            dt.here_document_syntax_element_description(self.instruction_name(),
-                                                        dt.HERE_DOCUMENT),
         ]
+        return additional_syntax_element_descriptions + self._help_parts.syntax_element_descriptions()
 
     def see_also(self) -> list:
-        concepts = rel_opts.see_also_concepts(parse_here_doc_or_file_ref.CONFIGURATION.accepted_options)
-        if ENVIRONMENT_VARIABLE_CONCEPT not in concepts:
-            concepts.append(ENVIRONMENT_VARIABLE_CONCEPT)
-        return list(map(ConceptDocumentation.cross_reference_target, concepts))
+        return self._help_parts.see_also()
 
 
 class Parser(SingleInstructionParser):
--- src/exactly_lib/instructions/utils/documentation/documentation_text.py
+++ src/exactly_lib/instructions/utils/documentation/documentation_text.py
@@ -1,9 +1,36 @@
 from exactly_lib.common.help.syntax_contents_structure import SyntaxElementDescription
 from exactly_lib.help.concepts.contents_structure import ConceptDocumentation
 from exactly_lib.help.utils import formatting
-from exactly_lib.util.cli_syntax.elements.argument import Named
+from exactly_lib.util.cli_syntax.elements import argument as a
+from exactly_lib.util.cli_syntax.render import cli_program_syntax
 from exactly_lib.util.textformat.parse import normalize_and_parse
 from exactly_lib.util.textformat.structure import structures as docs
+
+
+class CommandLineRenderingHelper:
+    """
+    Utility class for rendering command lines.
+    """
+
+    CL_SYNTAX_RENDERER = cli_program_syntax.CommandLineSyntaxRenderer()
+
+    ARG_SYNTAX_RENDERER = cli_program_syntax.ArgumentInArgumentDescriptionRenderer()
+
+    def cl_syntax_for_args(self, argument_usages: list) -> str:
+        cl = a.CommandLine(argument_usages)
+        return self.cl_syntax(cl)
+
+    def cl_syntax(self, command_line: a.CommandLine) -> str:
+        return self.CL_SYNTAX_RENDERER.as_str(command_line)
+
+    def arg_syntax(self, arg: a.Argument) -> str:
+        return self.ARG_SYNTAX_RENDERER.visit(arg)
+
+    def cli_argument_syntax_element_description(self,
+                                                argument: a.Argument,
+                                                description_rest: list) -> SyntaxElementDescription:
+        return SyntaxElementDescription(self.arg_syntax(argument),
+                                        description_rest)
 
 
 def paths_uses_posix_syntax() -> list:
@@ -11,19 +38,19 @@ def paths_uses_posix_syntax() -> list:
 
 
 def here_document_syntax_element_description(instruction_name: str,
-                                             here_document_argument: Named) -> SyntaxElementDescription:
+                                             here_document_argument: a.Named) -> SyntaxElementDescription:
     s = _HERE_DOCUMENT_DESCRIPTION.format(instruction_name=instruction_name)
     return SyntaxElementDescription(here_document_argument.name,
                                     normalize_and_parse(s))
 
 
-PATH_ARGUMENT = Named('PATH')
+PATH_ARGUMENT = a.Named('PATH')
 
-FILE_ARGUMENT = Named('FILE')
+FILE_ARGUMENT = a.Named('FILE')
 
-DIR_ARGUMENT = Named('DIR')
+DIR_ARGUMENT = a.Named('DIR')
 
-HERE_DOCUMENT = Named('HERE-DOCUMENT')
+HERE_DOCUMENT = a.Named('HERE-DOCUMENT')
 
 _HERE_DOCUMENT_DESCRIPTION = """\
 A sequence of lines, given using the shell "here document" syntax.
@@ -47,7 +74,7 @@ matches.
 def a_path_that_is_relative_the(syntax_element_name,
                                 relativity_root) -> SyntaxElementDescription:
     syntax_element_name_str = syntax_element_name
-    if isinstance(syntax_element_name, Named):
+    if isinstance(syntax_element_name, a.Named):
         syntax_element_name_str = syntax_element_name.name
     relativity_root_str = relativity_root
     if isinstance(relativity_root_str, ConceptDocumentation):
