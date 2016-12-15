8b4b4c855be25e54b5feb364c9b0787f7ff60e4f
Emil Karlén <emil@member.fsf.org>
2016-12-15 22:26:32 +0100

    file-contents: REFACT: Improves argument source generation

--- test/exactly_lib_test/instructions/assert_/contents/equals.py
+++ test/exactly_lib_test/instructions/assert_/contents/equals.py
@@ -1,7 +1,7 @@
 import unittest
 
-from exactly_lib.instructions.assert_.utils.file_contents.parsing import EQUALS_ARGUMENT
-from exactly_lib.instructions.utils.arg_parse import relative_path_options as options
+from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionParserSource
 from exactly_lib_test.instructions.assert_.contents.test_resources import TestCaseBaseForParser
 from exactly_lib_test.instructions.assert_.contents.test_resources import TestConfigurationForFile
 from exactly_lib_test.instructions.assert_.test_resources.contents_resources import \
@@ -9,35 +9,35 @@ from exactly_lib_test.instructions.assert_.test_resources.contents_resources imp
     StoreContentsInFileInCurrentDir, WriteFileToHomeDir, WriteFileToCurrentDir, \
     StoreContentsInFileInParentDirOfCwd
 from exactly_lib_test.instructions.assert_.test_resources.file_contents import equals
+from exactly_lib_test.instructions.assert_.test_resources.file_contents.instruction_test_configuration import args
 from exactly_lib_test.instructions.assert_.test_resources.instruction_check import arrangement, \
     Expectation, is_pass
 from exactly_lib_test.instructions.test_resources.assertion_utils import pfh_check, svh_check
 from exactly_lib_test.test_resources.execution.sds_populator import act_dir_contents, tmp_user_dir_contents
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_file, empty_dir, File
-from exactly_lib_test.test_resources.parse import new_source, new_source2
+from exactly_lib_test.test_resources.parse import new_source2
 
 
-class TestFileContentsFileRelHome(TestCaseBaseForParser):
+class TestFileExpectedFileRelHome(TestCaseBaseForParser):
     def test_validation_error__when__actual_file_does_not_exist(self):
         self._run(
-            new_source2('name-of-non-existing-file %s %s f.txt' %
-                        (EQUALS_ARGUMENT, options.REL_HOME_OPTION)),
+            new_source2(args('name-of-non-existing-file {equals} {rel_home_option} expected.txt')),
             arrangement(),
             Expectation(validation_pre_sds=svh_check.is_validation_error()),
         )
 
     def test_validation_error__when__actual_file_is_a_directory(self):
         self._run(
-            new_source2('name-of-non-existing-file %s --rel-home dir' % EQUALS_ARGUMENT),
+            new_source2(args('name-of-non-existing-file {equals} {rel_home_option} dir')),
             arrangement(home_dir_contents=DirContents([empty_dir('dir')])),
             Expectation(validation_pre_sds=svh_check.is_validation_error()),
         )
 
 
-class TestFileContentsFileRelCwd(TestCaseBaseForParser):
+class TestFileExpectedFileRelCwd(TestCaseBaseForParser):
     def test_fail__when__actual_file_does_not_exist(self):
         self._run(
-            new_source2('target %s --rel-cd comparison' % EQUALS_ARGUMENT),
+            new_source2(args('target {equals} {rel_cwd_option} expected.txt')),
             arrangement(sds_contents_before_main=act_dir_contents(
                 DirContents([empty_file('target')]))),
             Expectation(main_result=pfh_check.is_fail()),
@@ -45,7 +45,7 @@ class TestFileContentsFileRelCwd(TestCaseBaseForParser):
 
     def test_validation_error__when__actual_file_is_a_directory(self):
         self._run(
-            new_source2('target %s --rel-cd expected.txt' % EQUALS_ARGUMENT),
+            new_source2(args('target {equals} {rel_cwd_option} expected.txt')),
             arrangement(sds_contents_before_main=act_dir_contents(
                 DirContents([empty_dir('target'),
                              empty_file('expected.txt')]))),
@@ -56,7 +56,7 @@ class TestFileContentsFileRelCwd(TestCaseBaseForParser):
 class TestFileExpectedFileRelTmp(TestCaseBaseForParser):
     def test_fail__when__expected_file_does_not_exist(self):
         self._run(
-            new_source2('target %s --rel-tmp expected.txt' % EQUALS_ARGUMENT),
+            new_source2(args('target {equals} {rel_tmp_option} expected.txt')),
             arrangement(sds_contents_before_main=tmp_user_dir_contents(
                 DirContents([empty_file('expected.txt')]))),
             Expectation(main_result=pfh_check.is_fail()),
@@ -64,26 +64,32 @@ class TestFileExpectedFileRelTmp(TestCaseBaseForParser):
 
 
 class TestExpectedFileRelTmp(TestCaseBaseForParser):
-    def test_fail__when__expected_file_does_not_exist(self):
+    def test_fail__when__actual_file_does_not_exist(self):
         self._run(
-            new_source2('--rel-tmp target %s --rel-home expected.txt' % EQUALS_ARGUMENT),
+            new_source2(args('{rel_tmp_option} actual.txt {equals} {rel_home_option} expected.txt')),
             arrangement(home_dir_contents=DirContents([empty_file('expected.txt')])),
             Expectation(main_result=pfh_check.is_fail()),
         )
 
     def test_pass__when__contents_is_equal(self):
         self._run(
-            new_source2('--rel-tmp target %s --rel-home expected.txt' % EQUALS_ARGUMENT),
+            new_source2(args('{rel_tmp_option} actual.txt {equals} {rel_home_option} expected.txt')),
             arrangement(home_dir_contents=DirContents([File('expected.txt', 'contents')]),
                         sds_contents_before_main=tmp_user_dir_contents(
-                            DirContents([File('target', 'contents')]))),
+                            DirContents([File('actual.txt', 'contents')]))),
             is_pass(),
         )
 
 
 class TestReplacedEnvVars(TestCaseBaseForParser):
-    COMPARISON_SOURCE_FILE_NAME = 'with-replaced-env-vars.txt'
-    COMPARISON_expected_file_NAME = 'file-with-env-var-values-in-it-from-act-phase.txt'
+    COMPARISON_EXPECTED_FILE_NAME = 'with-replaced-env-vars.txt'
+    COMPARISON_ACTUAL_FILE_NAME = 'file-with-env-var-values-in-it-from-act-phase.txt'
+
+    def source(self, template: str) -> SingleInstructionParserSource:
+        _args = args(template,
+                     actual_file=self.COMPARISON_ACTUAL_FILE_NAME,
+                     expected_file=self.COMPARISON_EXPECTED_FILE_NAME)
+        return new_source2(_args)
 
     def __init__(self,
                  method_name):
@@ -91,66 +97,55 @@ class TestReplacedEnvVars(TestCaseBaseForParser):
 
     def test_pass__when__contents_equals__rel_home(self):
         act_result_producer = ActResultProducerForContentsWithAllReplacedEnvVars(
-            StoreContentsInFileInCurrentDir(self.COMPARISON_expected_file_NAME),
-            source_file_writer=WriteFileToHomeDir(self.COMPARISON_SOURCE_FILE_NAME),
+            StoreContentsInFileInCurrentDir(self.COMPARISON_ACTUAL_FILE_NAME),
+            source_file_writer=WriteFileToHomeDir(self.COMPARISON_EXPECTED_FILE_NAME),
             source_should_contain_expected_value=True)
         self._run(
-            new_source('instruction-name',
-                       '{} --with-replaced-env-vars {} --rel-home {}'.format(self.COMPARISON_expected_file_NAME,
-                                                                             EQUALS_ARGUMENT,
-                                                                             self.COMPARISON_SOURCE_FILE_NAME)),
+            self.source('{actual_file} {replace_env_vars_option} {equals} {rel_home_option} {expected_file}'),
             arrangement(act_result_producer=act_result_producer),
             is_pass(),
         )
 
     def test_fail__when__contents_not_equals__rel_home(self):
         act_result_producer = ActResultProducerForContentsWithAllReplacedEnvVars(
-            StoreContentsInFileInCurrentDir(self.COMPARISON_expected_file_NAME),
-            source_file_writer=WriteFileToHomeDir(self.COMPARISON_SOURCE_FILE_NAME),
+            StoreContentsInFileInCurrentDir(self.COMPARISON_ACTUAL_FILE_NAME),
+            source_file_writer=WriteFileToHomeDir(self.COMPARISON_EXPECTED_FILE_NAME),
             source_should_contain_expected_value=False)
         self._run(
-            new_source2('{} --with-replaced-env-vars {} --rel-home {}'.format(self.COMPARISON_expected_file_NAME,
-                                                                              EQUALS_ARGUMENT,
-                                                                              self.COMPARISON_SOURCE_FILE_NAME)),
+            self.source('{actual_file} {replace_env_vars_option} {equals} {rel_home_option} {expected_file}'),
             arrangement(act_result_producer=act_result_producer),
             Expectation(main_result=pfh_check.is_fail()),
         )
 
     def test_pass__when__contents_equals__rel_cwd(self):
         act_result_producer = ActResultProducerForContentsWithAllReplacedEnvVars(
-            StoreContentsInFileInCurrentDir(self.COMPARISON_expected_file_NAME),
-            source_file_writer=WriteFileToCurrentDir(self.COMPARISON_SOURCE_FILE_NAME),
+            StoreContentsInFileInCurrentDir(self.COMPARISON_ACTUAL_FILE_NAME),
+            source_file_writer=WriteFileToCurrentDir(self.COMPARISON_EXPECTED_FILE_NAME),
             source_should_contain_expected_value=True)
         self._run(
-            new_source2('{} --with-replaced-env-vars {} --rel-cd {}'.format(self.COMPARISON_expected_file_NAME,
-                                                                            EQUALS_ARGUMENT,
-                                                                            self.COMPARISON_SOURCE_FILE_NAME)),
+            self.source('{actual_file} {replace_env_vars_option} {equals} {rel_cwd_option} {expected_file}'),
             arrangement(act_result_producer=act_result_producer),
             is_pass(),
         )
 
     def test_fail__when__contents_not_equals__rel_cwd(self):
         act_result_producer = ActResultProducerForContentsWithAllReplacedEnvVars(
-            StoreContentsInFileInCurrentDir(self.COMPARISON_expected_file_NAME),
-            source_file_writer=WriteFileToCurrentDir(self.COMPARISON_SOURCE_FILE_NAME),
+            StoreContentsInFileInCurrentDir(self.COMPARISON_ACTUAL_FILE_NAME),
+            source_file_writer=WriteFileToCurrentDir(self.COMPARISON_EXPECTED_FILE_NAME),
             source_should_contain_expected_value=False)
         self._run(
-            new_source2('{} --with-replaced-env-vars {} --rel-cd {}'.format(self.COMPARISON_expected_file_NAME,
-                                                                            EQUALS_ARGUMENT,
-                                                                            self.COMPARISON_SOURCE_FILE_NAME)),
+            self.source('{actual_file} {replace_env_vars_option} {equals} {rel_cwd_option} {expected_file}'),
             arrangement(act_result_producer=act_result_producer),
             Expectation(main_result=pfh_check.is_fail()),
         )
 
     def test_pass__when__contents_equals_but_src_does_not_reside_inside_act_dir__rel_home(self):
         act_result_producer = ActResultProducerForContentsWithAllReplacedEnvVars(
-            StoreContentsInFileInParentDirOfCwd(self.COMPARISON_expected_file_NAME),
-            source_file_writer=WriteFileToHomeDir(self.COMPARISON_SOURCE_FILE_NAME),
+            StoreContentsInFileInParentDirOfCwd(self.COMPARISON_ACTUAL_FILE_NAME),
+            source_file_writer=WriteFileToHomeDir(self.COMPARISON_EXPECTED_FILE_NAME),
             source_should_contain_expected_value=True)
         self._run(
-            new_source2('../{} --with-replaced-env-vars {} --rel-home {}'.format(self.COMPARISON_expected_file_NAME,
-                                                                                 EQUALS_ARGUMENT,
-                                                                                 self.COMPARISON_SOURCE_FILE_NAME)),
+            self.source('../{actual_file} {replace_env_vars_option} {equals} {rel_home_option} {expected_file}'),
             arrangement(act_result_producer=act_result_producer),
             is_pass(),
         )
@@ -158,8 +153,8 @@ class TestReplacedEnvVars(TestCaseBaseForParser):
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        unittest.makeSuite(TestFileContentsFileRelHome),
-        unittest.makeSuite(TestFileContentsFileRelCwd),
+        unittest.makeSuite(TestFileExpectedFileRelHome),
+        unittest.makeSuite(TestFileExpectedFileRelCwd),
         unittest.makeSuite(TestFileExpectedFileRelTmp),
         unittest.makeSuite(TestExpectedFileRelTmp),
         unittest.makeSuite(TestReplacedEnvVars),
--- test/exactly_lib_test/test_resources/parse.py
+++ test/exactly_lib_test/test_resources/parse.py
@@ -10,31 +10,32 @@ from exactly_lib.util.line_source import LineSequenceBuilder
 def new_source(instruction_name: str, arguments: str) -> SingleInstructionParserSource:
     first_line = instruction_name + ' ' + arguments
     return SingleInstructionParserSource(
-            new_line_sequence(first_line),
-            arguments)
+        new_line_sequence(first_line),
+        arguments)
 
 
-def new_source2(arguments: str) -> SingleInstructionParserSource:
-    first_line = 'instruction name' + ' ' + arguments
+def new_source2(first_line_arguments: str,
+                following_lines: list = ()) -> SingleInstructionParserSource:
+    first_line = 'instruction-name' + ' ' + first_line_arguments
     return SingleInstructionParserSource(
-            new_line_sequence(first_line),
-            arguments)
+        new_line_sequence(first_line, tuple(following_lines)),
+        first_line_arguments)
 
 
 def single_line_source(arguments: str) -> SingleInstructionParserSource:
     first_line = 'instruction-name' + ' ' + arguments
     return SingleInstructionParserSource(
-            new_line_sequence(first_line),
-            arguments)
+        new_line_sequence(first_line),
+        arguments)
 
 
 def multi_line_source(first_line_arguments: str,
                       following_lines: list) -> SingleInstructionParserSource:
     first_line = 'instruction-name' + ' ' + first_line_arguments
     return SingleInstructionParserSource(
-            new_line_sequence(first_line,
-                              following_lines=tuple(following_lines)),
-            first_line_arguments)
+        new_line_sequence(first_line,
+                          following_lines=tuple(following_lines)),
+        first_line_arguments)
 
 
 def argument_list_source(arguments: list,
@@ -46,7 +47,7 @@ def argument_list_source(arguments: list,
 def new_line_sequence(first_line: str,
                       following_lines: tuple = ()) -> LineSequenceBuilder:
     return line_source.LineSequenceBuilder(
-            parse.LineSequenceSourceFromListOfLines(
-                    parse.ListOfLines(list(following_lines))),
-            1,
-            first_line)
+        parse.LineSequenceSourceFromListOfLines(
+            parse.ListOfLines(list(following_lines))),
+        1,
+        first_line)
