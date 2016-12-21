650c7d4b844b9494af0813f4328fc7d7ade2c711
Emil Karlén <emil@member.fsf.org>
2016-12-21 20:49:29 +0100

    REFACT: Test resources: Moves test resources related to tmp-dir to correct package

--- test/exactly_lib_test/instructions/utils/file_properties.py
+++ test/exactly_lib_test/instructions/utils/file_properties.py
@@ -5,8 +5,8 @@ from contextlib import contextmanager
 from exactly_lib.instructions.utils import file_properties as sut
 from exactly_lib.instructions.utils.file_properties import FileType, CheckResult, PropertiesWithNegation, \
     new_properties_for_existence
+from exactly_lib_test.test_resources.execution.file_structure_utils import tmp_dir, tmp_dir_with
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_file, empty_dir, sym_link
-from exactly_lib_test.test_resources.file_structure_utils import tmp_dir, tmp_dir_with
 
 
 class FileCheckThatEvaluatesTo(sut.FilePropertiesCheck):
