585c356eda009a144f14748e5bb79f93e6672158
Emil Karlén <emil@member.fsf.org>
2016-12-21 20:47:55 +0100

    Test resources: Fixes invalid restoring of cwd

--- test/exactly_lib_test/test_resources/file_structure_utils.py
+++ test/exactly_lib_test/test_resources/file_structure_utils.py
@@ -17,12 +17,12 @@ def tmp_dir(contents: DirContents = empty_dir_contents()) -> pathlib.Path:
 
 @contextmanager
 def tmp_dir_as_cwd(contents: DirContents = empty_dir_contents()) -> pathlib.Path:
-    with preserved_cwd():
         with tempfile.TemporaryDirectory() as dir_name:
-            dir_path = resolved_path(dir_name)
-            contents.write_to(dir_path)
-            os.chdir(str(dir_path))
-            yield dir_path
+            with preserved_cwd():
+                dir_path = resolved_path(dir_name)
+                os.chdir(str(dir_path))
+                contents.write_to(dir_path)
+                yield dir_path
 
 
 def tmp_dir_with(file_element: FileSystemElement) -> pathlib.Path:
