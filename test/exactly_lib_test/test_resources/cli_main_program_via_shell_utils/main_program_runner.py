2d0466212775c90bd6d86f4ee608b84c384a944c
Emil Karlén <emil@member.fsf.org>
2016-12-21 14:03:46 +0100

    REFACT: Tests: Using shutil.which to find installed executable

--- test/exactly_lib_test/test_resources/cli_main_program_via_shell_utils/main_program_runner.py
+++ test/exactly_lib_test/test_resources/cli_main_program_via_shell_utils/main_program_runner.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import shutil
 import unittest
 
 from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
@@ -9,7 +8,7 @@ from exactly_lib_test.test_resources.process import SubProcessResult, run_subpro
 class RunInstalledProgramViaOsInSubProcess(MainProgramRunner):
     def __init__(self, executable_file_name_base: str):
         self._executable_file_name_base = executable_file_name_base
-        self._main_program_path = _find_executable(executable_file_name_base)
+        self._main_program_path = shutil.which(executable_file_name_base)
 
     def description_for_test_name(self) -> str:
         return 'run installed program'
@@ -20,37 +19,3 @@ class RunInstalledProgramViaOsInSubProcess(MainProgramRunner):
         cmd_and_args = [self._main_program_path] + arguments
         stdin_contents = ''
         return run_subprocess(cmd_and_args, stdin_contents)
-
-
-def _find_executable(executable, path=None):
-    #
-    # https://gist.github.com/4368898
-    # Public domain code by anatoly techtonik <techtonik@gmail.com>
-    #
-    # NOTE: Some part of the function have been removed, since not used in this context.
-    #
-    """Find if 'executable' can be run. Looks for it in 'path'
-    (string that lists directories separated by 'os.pathsep';
-    defaults to os.environ['PATH']). Checks for all executable
-    extensions. Returns full path or None if no command is found.
-    """
-    if path is None:
-        path = os.environ['PATH']
-    paths = path.split(os.pathsep)
-    extlist = ['']
-    if sys.platform == 'win32':
-        pathext = os.environ['PATHEXT'].lower().split(os.pathsep)
-        (base, ext) = os.path.splitext(executable)
-        if ext.lower() not in pathext:
-            extlist = pathext
-    for ext in extlist:
-        execname = executable + ext
-        if os.path.isfile(execname):
-            return execname
-        else:
-            for p in paths:
-                f = os.path.join(p, execname)
-                if os.path.isfile(f):
-                    return f
-    else:
-        return None
