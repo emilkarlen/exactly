e63c3ffd6917fbad6d39e953996fac57aa21f1af
Emil Karlén <emil@member.fsf.org>
2016-12-21 13:45:14 +0100

    instruction dir: cli options for relativity: Adds test to ensure that --rel-result is not allowed

---
+++ test/exactly_lib_test/instructions/utils/arg_parse/test_resources.py
@@ -0,0 +1,17 @@
+from exactly_lib.instructions.utils.arg_parse import relative_path_options
+
+
+def args_with_rel_ops(arg_str: str, **kwargs) -> str:
+    if kwargs:
+        format_map = dict(list(_FORMAT_MAP.items()) + list(kwargs.items()))
+        return arg_str.format_map(format_map)
+    return arg_str.format_map(_FORMAT_MAP)
+
+
+_FORMAT_MAP = {
+    'rel_home_option': relative_path_options.REL_HOME_OPTION,
+    'rel_cwd_option': relative_path_options.REL_CWD_OPTION,
+    'rel_tmp_option': relative_path_options.REL_TMP_OPTION,
+    'rel_act_option': relative_path_options.REL_ACT_OPTION,
+    'rel_result_option': relative_path_options.REL_RESULT_OPTION,
+}
