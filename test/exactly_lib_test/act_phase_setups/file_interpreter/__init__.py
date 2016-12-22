7101e4fa827e81eec6d92d34a4c18dd5e710ce4f
Emil Karlén <emil@member.fsf.org>
2016-12-22 08:57:59 +0100

    file interpreter/shell: Organization of modules

--- test/exactly_lib_test/act_phase_setups/file_interpreter/__init__.py
+++ test/exactly_lib_test/act_phase_setups/file_interpreter/__init__.py
@@ -1,15 +1,11 @@
 import unittest
 
-from exactly_lib_test.act_phase_setups.file_interpreter import act_phase_contents
-from exactly_lib_test.act_phase_setups.file_interpreter.configuration import TheConfiguration
-from exactly_lib_test.act_phase_setups.test_resources.act_source_and_executor import suite_for_execution
+from exactly_lib_test.act_phase_setups.file_interpreter import executable_file
 
 
 def suite() -> unittest.TestSuite:
     tests = []
-    the_configuration = TheConfiguration()
-    tests.append(act_phase_contents.suite_for(the_configuration))
-    tests.append(suite_for_execution(the_configuration))
+    tests.append(executable_file.suite())
     return unittest.TestSuite(tests)
 
 
