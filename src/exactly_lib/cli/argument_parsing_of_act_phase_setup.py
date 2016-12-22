d47648bf910aa657478f37ecd70922346d86548a
Emil Karlén <emil.karlen@fripost.org>
2016-12-22 08:15:33 +0100

    Removes unnecessary special interpreter for test

--- src/exactly_lib/cli/argument_parsing_of_act_phase_setup.py
+++ src/exactly_lib/cli/argument_parsing_of_act_phase_setup.py
@@ -1,10 +1,8 @@
 import shlex
 
-from exactly_lib.act_phase_setups.interpreter import python3, act_phase_setup
+from exactly_lib.act_phase_setups.interpreter import act_phase_setup
 from exactly_lib.processing.act_phase import ActPhaseSetup
 from exactly_lib.util.process_execution.os_process_execution import Command
-
-INTERPRETER_FOR_TEST = 'test-language'
 
 
 def resolve_act_phase_setup_from_argparse_argument(default_setup: ActPhaseSetup,
@@ -18,10 +16,7 @@ def resolve_act_phase_setup_from_argparse_argument(default_setup: ActPhaseSetup,
 def _resolve_act_phase_setup(default_setup: ActPhaseSetup,
                              interpreter: str = None) -> ActPhaseSetup:
     if interpreter:
-        if interpreter == INTERPRETER_FOR_TEST:
-            return python3.new_act_phase_setup()
-        else:
-            return _new_for_generic_script_language_setup(interpreter)
+        return _new_for_generic_script_language_setup(interpreter)
     return default_setup
 
 
