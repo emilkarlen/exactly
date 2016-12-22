50146e9b9c5d9bb7470c76158625f27f1bd204f9
Emil Karlén <emil.karlen@fripost.org>
2016-12-22 14:11:11 +0100

    file interpreter/shell:actor instruction: REFACT of test

--- test/exactly_lib_test/instructions/configuration/actor/__init__.py
+++ test/exactly_lib_test/instructions/configuration/actor/__init__.py
@@ -18,7 +18,7 @@ from exactly_lib_test.test_case.test_resources.act_phase_instruction import inst
 from exactly_lib_test.test_case.test_resources.act_phase_os_process_executor import \
     ActPhaseOsProcessExecutorThatRecordsArguments
 from exactly_lib_test.test_resources import file_structure
-from exactly_lib_test.test_resources.file_structure import empty_file
+from exactly_lib_test.test_resources.file_structure import empty_file, DirContents
 from exactly_lib_test.test_resources.parse import new_source2
 from exactly_lib_test.test_resources.programs import shell_commands
 from exactly_lib_test.test_resources.value_assertions import process_result_assertions as pr
@@ -28,11 +28,12 @@ from exactly_lib_test.test_resources.value_assertions import value_assertion as 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(TestFailingParseForAnyActor),
-        unittest.makeSuite(TestFailingParseForCommandLine),
-        unittest.makeSuite(TestFailingParseForInterpreter),
-        unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForFileInterpreterActor),
+        unittest.makeSuite(TestFailingParseForCommandLineActor),
+        unittest.makeSuite(TestFailingParseForSourceInterpreterActor),
+        unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForFileInterpreterActorForExecutableFile),
+        unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForFileInterpreterActorForShellCommand),
         unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForSourceInterpreterActor),
-        unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForShellCommandInterpreterActor),
+        unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForSourceInterpreterActorForShellCommand),
         unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForCommandLineActorForExecutableFile),
         unittest.makeSuite(TestSuccessfulParseAndInstructionExecutionForCommandLineActorForShellCommand),
         unittest.makeSuite(TestShellHandlingViaExecution),
@@ -52,14 +53,14 @@ class TestFailingParseForAnyActor(unittest.TestCase):
             sut.Parser().apply(source)
 
 
-class TestFailingParseForCommandLine(unittest.TestCase):
+class TestFailingParseForCommandLineActor(unittest.TestCase):
     def test_fail_when_extra_unexpected_argument(self):
         source = new_source2(actor_utils.COMMAND_LINE_ACTOR_OPTION + ' extra-unexpected-argument')
         with self.assertRaises(SingleInstructionInvalidArgumentException):
             sut.Parser().apply(source)
 
 
-class TestFailingParseForInterpreter(unittest.TestCase):
+class TestFailingParseForSourceInterpreterActor(unittest.TestCase):
     def test_fail_when_missing_program_argument(self):
         source = new_source2(actor_utils.SOURCE_INTERPRETER_OPTION)
         with self.assertRaises(SingleInstructionInvalidArgumentException):
@@ -70,7 +71,8 @@ class _NonShellExecutionCheckHelper:
     def __init__(self, cli_option: str):
         self.cli_option = cli_option
 
-    def apply(self, put: unittest.TestCase,
+    def apply(self,
+              put: unittest.TestCase,
               instruction_argument_source_template: str,
               act_phase_source_lines: list,
               expected_cmd_and_args: va.ValueAssertion,
@@ -96,6 +98,55 @@ class _NonShellExecutionCheckHelper:
         put.assertTrue(len(actual_cmd_and_args) > 0,
                        'List of arguments is expected to contain at least the file|interpreter argument')
         expected_cmd_and_args.apply_with_message(put, actual_cmd_and_args, 'actual_cmd_and_args')
+
+
+class _ShellExecutionCheckerHelper:
+    def __init__(self, cli_option: str):
+        self.cli_option = cli_option
+
+    def apply(self,
+              put: unittest.TestCase,
+              instruction_argument_source_template: str,
+              act_phase_source_lines: list,
+              expectation_of_cmd_and_args: va.ValueAssertion,
+              home_dir_contents: file_structure.DirContents = file_structure.DirContents([]),
+              ):
+        # ARRANGE #
+        instruction_argument_source = instruction_argument_source_template.format(
+            actor_option=self.cli_option,
+            shell_option=actor_utils.SHELL_COMMAND_INTERPRETER_ACTOR_KEYWORD,
+        )
+        os_process_executor = ActPhaseOsProcessExecutorThatRecordsArguments()
+        arrangement = Arrangement(instruction_argument_source,
+                                  act_phase_source_lines,
+                                  act_phase_process_executor=os_process_executor,
+                                  home_dir_contents=home_dir_contents)
+        expectation = Expectation()
+        # ACT #
+        _check(put, arrangement, expectation)
+        # ASSERT #
+        put.assertTrue(os_process_executor.command.shell,
+                       'Command should indicate shell execution')
+        actual_cmd_and_args = os_process_executor.command.args
+        put.assertIsInstance(actual_cmd_and_args, str,
+                             'Arguments of command to execute should be a string')
+        expectation_of_cmd_and_args.apply_with_message(put, actual_cmd_and_args, 'cmd_and_args')
+
+
+def initial_part_of_command_without_file_argument_is(
+        expected_command_except_final_file_name_part: str) -> va.ValueAssertion:
+    class RetClass(va.ValueAssertion):
+        def apply(self,
+                  put: unittest.TestCase,
+                  actual_cmd_and_args: str,
+                  message_builder: va.MessageBuilder = va.MessageBuilder()):
+            put.assertTrue(len(actual_cmd_and_args) > len(expected_command_except_final_file_name_part),
+                           'Command line string is expected to contain at least the argument of the instruction')
+            command_head = actual_cmd_and_args[:len(expected_command_except_final_file_name_part)]
+            put.assertEqual(command_head,
+                            expected_command_except_final_file_name_part)
+
+    return RetClass()
 
 
 def equals_with_last_element_removed(expected: list) -> va.ValueAssertion:
@@ -128,13 +179,14 @@ class TestSuccessfulParseAndInstructionExecutionForSourceInterpreterActor(unitte
                     ['executable with space', 'arg2', 'arg 3'])
 
 
-class TestSuccessfulParseAndInstructionExecutionForFileInterpreterActor(unittest.TestCase):
+class TestSuccessfulParseAndInstructionExecutionForFileInterpreterActorForExecutableFile(unittest.TestCase):
     helper = _NonShellExecutionCheckHelper(actor_utils.FILE_INTERPRETER_OPTION)
 
     def _check(self, instruction_argument_source_template: str,
                act_phase_source_lines: list,
                cmd_and_args: va.ValueAssertion,
-               home_dir_contents: file_structure.DirContents = file_structure.DirContents([])):
+               home_dir_contents: file_structure.DirContents = file_structure.DirContents([]),
+               ):
         self.helper.apply(self,
                           instruction_argument_source_template,
                           act_phase_source_lines=act_phase_source_lines,
@@ -190,47 +242,69 @@ def is_interpreter_with_source_file_and_arguments(interpreter: str,
     return RetClass()
 
 
-class TestSuccessfulParseAndInstructionExecutionForShellCommandInterpreterActor(unittest.TestCase):
+class TestSuccessfulParseAndInstructionExecutionForSourceInterpreterActorForShellCommand(unittest.TestCase):
+    helper = _ShellExecutionCheckerHelper(actor_utils.SOURCE_INTERPRETER_OPTION)
+
     def _check(self, instruction_argument_source_template: str,
-               expected_command_except_final_file_name_part: str):
-        # ARRANGE #
-        instruction_argument_source = instruction_argument_source_template.format(
-            actor_option=actor_utils.SOURCE_INTERPRETER_OPTION,
-            shell_option=actor_utils.SHELL_COMMAND_INTERPRETER_ACTOR_KEYWORD,
-        )
-        os_process_executor = ActPhaseOsProcessExecutorThatRecordsArguments()
-        arrangement = Arrangement(instruction_argument_source,
-                                  ['this is act phase source code that is not used in the test'],
-                                  act_phase_process_executor=os_process_executor)
-        expectation = Expectation()
-        # ACT #
-        _check(self, arrangement, expectation)
-        # ASSERT #
-        self.assertTrue(os_process_executor.command.shell,
-                        'Command should indicate shell execution')
-        actual_cmd_and_args = os_process_executor.command.args
-        self.assertIsInstance(actual_cmd_and_args, str,
-                              'Arguments of command to execute should be a string')
-        self.assertTrue(len(actual_cmd_and_args) > len(expected_command_except_final_file_name_part),
-                        'Command line string is expected to contain at least the argument of the instruction')
-        command_head = actual_cmd_and_args[:len(expected_command_except_final_file_name_part)]
-        self.assertEqual(command_head,
-                         expected_command_except_final_file_name_part)
+               expected_command_except_final_file_name_part: va.ValueAssertion):
+        self.helper.apply(self, instruction_argument_source_template,
+                          ['this is act phase source code that is not used in the test'],
+                          expected_command_except_final_file_name_part)
 
     def test_single_command(self):
-        self._check('{actor_option} {shell_option} arg', 'arg')
+        self._check('{actor_option} {shell_option} arg',
+                    initial_part_of_command_without_file_argument_is('arg'))
 
     def test_command_with_arguments(self):
         self._check('{actor_option} {shell_option} arg arg1 --arg2',
-                    'arg arg1 --arg2')
+                    initial_part_of_command_without_file_argument_is('arg arg1 --arg2'))
 
     def test_quoting(self):
         self._check("{actor_option} {shell_option} 'arg with space' arg2 \"arg 3\"",
-                    "'arg with space' arg2 \"arg 3\"")
+                    initial_part_of_command_without_file_argument_is("'arg with space' arg2 \"arg 3\""))
 
     def test_with_interpreter_keyword(self):
         self._check('{actor_option} {shell_option} arg1 arg2',
-                    'arg1 arg2')
+                    initial_part_of_command_without_file_argument_is('arg1 arg2'))
+
+
+class TestSuccessfulParseAndInstructionExecutionForFileInterpreterActorForShellCommand(unittest.TestCase):
+    helper = _ShellExecutionCheckerHelper(actor_utils.FILE_INTERPRETER_OPTION)
+
+    def _check(self, instruction_argument_source_template: str,
+               act_phase_source_lines: list,
+               expected_command_except_final_file_name_part: va.ValueAssertion,
+               home_dir_contents: DirContents,
+               ):
+        self.helper.apply(self,
+                          instruction_argument_source_template,
+                          act_phase_source_lines,
+                          expected_command_except_final_file_name_part,
+                          home_dir_contents)
+
+    def test_single_command(self):
+        self._check('{actor_option} {shell_option} arg',
+                    ['file.src'],
+                    initial_part_of_command_without_file_argument_is('arg'),
+                    home_dir_contents=file_in_home_dir('file.src'))
+
+    def test_command_with_arguments(self):
+        self._check('{actor_option} {shell_option} arg arg1 --arg2',
+                    ['file.src'],
+                    initial_part_of_command_without_file_argument_is('arg arg1 --arg2'),
+                    home_dir_contents=file_in_home_dir('file.src'))
+
+    def test_quoting(self):
+        self._check("{actor_option} {shell_option} 'arg with space' arg2 \"arg 3\"",
+                    ['file.src'],
+                    initial_part_of_command_without_file_argument_is("'arg with space' arg2 \"arg 3\""),
+                    home_dir_contents=file_in_home_dir('file.src'))
+
+    def test_with_interpreter_keyword(self):
+        self._check('{actor_option} {shell_option} arg1 arg2',
+                    ['file.src'],
+                    initial_part_of_command_without_file_argument_is('arg1 arg2'),
+                    home_dir_contents=file_in_home_dir('file.src'))
 
 
 class TestSuccessfulParseAndInstructionExecutionForCommandLineActorForExecutableFile(unittest.TestCase):
