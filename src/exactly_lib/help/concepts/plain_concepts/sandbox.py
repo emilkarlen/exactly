463a88945265f0df089723ee2a26a873a32f2ab4
Emil Karlén <emil.karlen@fripost.org>
2016-12-23 01:14:44 +0100

    sandbox concept: Adds info about relativity-options

--- src/exactly_lib/help/concepts/plain_concepts/sandbox.py
+++ src/exactly_lib/help/concepts/plain_concepts/sandbox.py
@@ -1,11 +1,13 @@
+from exactly_lib import program_info
 from exactly_lib.common.help.cross_reference_id import TestCasePhaseInstructionCrossReference
 from exactly_lib.default.program_modes.test_case.default_instruction_names import CHANGE_DIR_INSTRUCTION_NAME
-from exactly_lib.execution.environment_variables import ENV_VAR_RESULT, ENV_VAR_ACT
+from exactly_lib.execution import environment_variables
 from exactly_lib.help.concepts.contents_structure import PlainConceptDocumentation, Name
 from exactly_lib.help.concepts.some_concept_names import CURRENT_WORKING_DIRECTORY_CONCEPT_NAME
 from exactly_lib.help.utils import formatting
-from exactly_lib.help.utils.formatting import AnyInstructionNameDictionary
+from exactly_lib.help.utils.formatting import AnyInstructionNameDictionary, InstructionName
 from exactly_lib.help.utils.phase_names import phase_name_dictionary, SETUP_PHASE_NAME
+from exactly_lib.instructions.utils.arg_parse import relative_path_options
 from exactly_lib.test_case import sandbox_directory_structure as sds
 from exactly_lib.util.description import DescriptionWithSubSections
 from exactly_lib.util.textformat.parse import normalize_and_parse
@@ -61,6 +63,8 @@ def sandbox_directories_info_sections(phase_name_dictionary: dict,
                 _act_dir_description_paragraphs(instruction, phase_name_dictionary)),
         section(sds.SUB_DIRECTORY__RESULT,
                 _result_dir_description_paragraphs(instruction, phase_name_dictionary)),
+        section(sds.SUB_DIRECTORY__TMP + '/' + sds.SUB_DIRECTORY__TMP_USER,
+                _tmp_user_dir_description_paragraphs()),
     ]
 
 
@@ -69,8 +73,10 @@ def _act_dir_description_paragraphs(instruction: AnyInstructionNameDictionary, p
     ret_val.extend(normalize_and_parse(
         _ACT_DIR_DESCRIPTION.format(phase=phase,
                                     instruction=instruction,
-                                    cwd=formatting.concept(CURRENT_WORKING_DIRECTORY_CONCEPT_NAME.singular))))
-    ret_val.extend(_act_dir_environment_variables(phase=phase))
+                                    cwd=formatting.concept(CURRENT_WORKING_DIRECTORY_CONCEPT_NAME.singular),
+                                    cd_instruction=InstructionName(CHANGE_DIR_INSTRUCTION_NAME))))
+    ret_val.extend(_dir_env_variables_and_rel_options(env_var_name=environment_variables.ENV_VAR_ACT,
+                                                      rel_option=relative_path_options.REL_ACT_OPTION))
     return ret_val
 
 
@@ -81,7 +87,16 @@ def _result_dir_description_paragraphs(instruction: AnyInstructionNameDictionary
                                        instruction=instruction)))
     ret_val.append(docs.simple_header_only_list(sds.RESULT_FILE_ALL,
                                                 lists.ListType.ITEMIZED_LIST))
-    ret_val.extend(_result_dir_environment_variables(phase))
+    ret_val.extend(_result_dir_env_variable_and_rel_option(phase))
+    return ret_val
+
+
+def _tmp_user_dir_description_paragraphs():
+    ret_val = []
+    ret_val.extend(normalize_and_parse(
+        _USR_TMP_DIR_DESCRIPTION.format(program_name=formatting.program_name(program_info.PROGRAM_NAME))))
+    ret_val.extend(_dir_env_variables_and_rel_options(env_var_name=environment_variables.ENV_VAR_TMP,
+                                                      rel_option=relative_path_options.REL_TMP_OPTION))
     return ret_val
 
 
@@ -89,12 +104,14 @@ _ACT_DIR_DESCRIPTION = """\
 This directory is the {cwd} when the {phase[setup]} phase begin.
 
 
-If it is not changed, it will also be the {cwd} for the {phase[act]} phase (hence its name).
+If it is not changed by the {cd_instruction} instruction,
+it will also be the {cwd} for the {phase[act]} phase.
 
 
 (Files and directories that {phase[setup]:syntax} creates
 are installed into the {cwd}, if no instruction options are used to change this.)
 """
+
 _RESULT_DIR_DESCRIPTION = """\
 This directory is initially empty.
 
@@ -102,18 +119,47 @@ It is populated when the {phase[act]} phase is executed
 with the following files (with obvious contents):
 """
 
+_USR_TMP_DIR_DESCRIPTION = """\
+A directory that the writer of the test case can store arbitrary files.
 
-def _result_dir_environment_variables(phase: dict) -> list:
-    return docs.paras('{env_var}: The value of this environment variable is the absolute path of this directory '
-                      '(after the {phase[act]} phase has been executed).'
-                      .format(phase=phase,
-                              env_var=ENV_VAR_RESULT))
+{program_name} does not touch contents of this directory.
+"""
 
 
-def _act_dir_environment_variables(phase: dict) -> list:
-    return docs.paras('{env_var}: The value of this environment variable is the absolute path of this directory.'
-                      .format(phase=phase,
-                              env_var=ENV_VAR_ACT))
+def _result_dir_env_variable_and_rel_option(phase: dict) -> list:
+    return [_dir_info_items_table(environment_variables.ENV_VAR_RESULT,
+                                  relative_path_options.REL_RESULT_OPTION,
+                                  _RESULT_DIR_ENV_VARIABLE.format(phase=phase))
+            ]
+
+
+_RESULT_DIR_ENV_VARIABLE = """\
+The value of this environment variable
+is the absolute path of this directory
+(after the {phase[act]} phase has been executed)."""
+
+
+def _dir_env_variables_and_rel_options(env_var_name: str,
+                                       rel_option: str) -> list:
+    return [_dir_info_items_table(env_var_name,
+                                  rel_option,
+                                  'The value of this environment variable is the absolute path of this directory.')
+            ]
+
+
+def _dir_info_items_table(env_var_name: str,
+                          rel_option: str,
+                          env_var_description: str) -> ParagraphItem:
+    return docs.first_column_is_header_table([
+        [
+            docs.cell(docs.paras(env_var_name)),
+            docs.cell(docs.paras(env_var_description)),
+        ],
+        [
+            docs.cell(docs.paras(rel_option)),
+            docs.cell(docs.paras('This option (accepted by many instructions) refers to this directory.')),
+        ],
+    ])
 
 
 def directory_structure_list_section(dir_with_sub_dir_list: list) -> Section:
