4bcba84ad6839459cd48b12cb346d776b35241de
Emil Karlén <emil.karlen@fripost.org>
2017-02-12 17:33:27 +0100

    instr-desc: test resources: Improves assertion on ParserSource to be aware of preconditions in ParserSource

--- test/exactly_lib_test/section_document/test_resources/parse_source.py
+++ test/exactly_lib_test/section_document/test_resources/parse_source.py
@@ -13,11 +13,17 @@ def assert_source(is_at_eof: asrt.ValueAssertion = asrt.anything_goes(),
     return asrt.And([
         asrt.is_instance(ParseSource, 'Value to apply assertions on must be a {}'.format(ParseSource)),
         asrt.sub_component('is_at_eof', ParseSource.is_at_eof.fget, is_at_eof),
-        asrt.sub_component('is_at_eol', ParseSource.is_at_eol.fget, is_at_eol),
         asrt.sub_component('has_current_line', ParseSource.has_current_line.fget, has_current_line),
-        asrt.sub_component('current_line_number', ParseSource.current_line_number.fget, current_line_number),
-        asrt.sub_component('current_line_text', ParseSource.current_line_text.fget, current_line_text),
-        asrt.sub_component('remaining_part_of_current_line', ParseSource.remaining_part_of_current_line.fget,
-                           remaining_part_of_current_line),
+        asrt.Or([
+            asrt.sub_component('has_current_line', ParseSource.has_current_line.fget, asrt.equals(False)),
+            # The following must only be checked if has_current_line (because of precondition of ParseSource):
+            asrt.And([
+                asrt.sub_component('is_at_eol', ParseSource.is_at_eol.fget, is_at_eol),
+                asrt.sub_component('current_line_number', ParseSource.current_line_number.fget, current_line_number),
+                asrt.sub_component('current_line_text', ParseSource.current_line_text.fget, current_line_text),
+                asrt.sub_component('remaining_part_of_current_line', ParseSource.remaining_part_of_current_line.fget,
+                                   remaining_part_of_current_line),
+            ])
+        ]),
         asrt.sub_component('remaining_source', ParseSource.remaining_source.fget, remaining_source),
     ])
