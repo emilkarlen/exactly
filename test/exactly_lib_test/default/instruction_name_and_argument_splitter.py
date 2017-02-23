2d5a3aa24f6f867166c6c7ce81c912cce5a393c5
Emil Karlén <emil.karlen@fripost.org>
2017-02-23 18:47:46 +0100

    instr-desc: Adapts  instruction name splitter (for parser) to new parsers

--- test/exactly_lib_test/default/instruction_name_and_argument_splitter.py
+++ test/exactly_lib_test/default/instruction_name_and_argument_splitter.py
@@ -11,47 +11,39 @@ def suite() -> unittest.TestSuite:
 
 class TestCase(unittest.TestCase):
     def test_single_character_name_and_no_argument(self):
-        self._check('i', 'i', '')
+        self._check('i', 'i')
 
     def test_multi_character_name_and_no_argument(self):
         self._check('instruction',
-                    'instruction',
-                    '')
+                    'instruction')
 
     def test_valid_name_characters(self):
         self._check('abczABCZ01239.-$ argument',
-                    'abczABCZ01239.-$',
-                    ' argument')
+                    'abczABCZ01239.-$')
 
     def test_skip_initial_space(self):
         self._check('   name argument',
-                    'name',
-                    ' argument')
+                    'name')
 
     def test_do_not_skip_trailing_space(self):
         self._check('name :argument  ',
-                    'name',
-                    ' :argument  ')
+                    'name')
 
     def test_under_score(self):
         self._check('a_b c',
-                    'a_b',
-                    ' c')
+                    'a_b')
 
     def test_dollar(self):
         self._check('$ command and args',
-                    '$',
-                    ' command and args')
+                    '$')
 
     def _check(self,
                line: str,
-               expected_name: str,
-               expected_argument: str):
+               expected_name: str):
         # ACT #
-        (actual_name, actual_arg) = splitter(line)
+        actual_name = splitter(line)
         # ASSERT #
         self.assertEqual(expected_name, actual_name, 'Name')
-        self.assertEqual(expected_argument, actual_arg, 'Argument')
 
 
 if __name__ == '__main__':
