47191e2be1664e87e3d175f5a43310949c68ce05
Emil Karlén <emil@member.fsf.org>
2017-02-23 18:30:59 +0100

    instr-desc: Removes unused code

--- test/exactly_lib_test/test_resources/main_program/main_program_test_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from exactly_lib.common.exit_value import ExitValue
-from exactly_lib_test.test_resources.process import ExpectedSubProcessResult
-
-
-def process_result_for(exit_value: ExitValue) -> ExpectedSubProcessResult:
-    return ExpectedSubProcessResult(exitcode=exit_value.exit_code,
-                                    stdout=exit_value.exit_identifier + os.linesep)
