525704bee87fb841474fdc9359b1580e011d1f70
Emil Karlén <emil@member.fsf.org>
2017-02-23 20:29:57 +0100

    instr-desc: Removes unused code: instruction parser using Python argparse

--- test/exactly_lib_test/section_document/parser_implementations/__init__.py
+++ test/exactly_lib_test/section_document/parser_implementations/__init__.py
@@ -1,6 +1,5 @@
 import unittest
 
-from exactly_lib_test.section_document.parser_implementations import instruction_parser_using_argument_parser
 from exactly_lib_test.section_document.parser_implementations import new_section_element_parser
 from exactly_lib_test.section_document.parser_implementations import optional_description_and_instruction_parser
 from exactly_lib_test.section_document.parser_implementations import parser_for_dictionary_of_instructions
@@ -9,7 +8,6 @@ from exactly_lib_test.section_document.parser_implementations import token_parse
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
-    ret_val.addTest(instruction_parser_using_argument_parser.suite())
     ret_val.addTest(new_section_element_parser.suite())
     ret_val.addTest(optional_description_and_instruction_parser.suite())
     ret_val.addTest(parser_for_dictionary_of_instructions.suite())
