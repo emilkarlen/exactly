a7241700f6802f2af2ff292d861267f2ddadff81
Emil Karlén <emil@member.fsf.org>
2017-02-23 22:18:53 +0100

    instr-desc: Removes unused code (related to old parse structures)

--- test/exactly_lib_test/util/__init__.py
+++ test/exactly_lib_test/util/__init__.py
@@ -1,13 +1,12 @@
 import unittest
 
-from exactly_lib_test.util import functional, line_source, textformat, tables, cli_syntax
+from exactly_lib_test.util import functional, textformat, tables, cli_syntax
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(functional.suite())
     ret_val.addTest(tables.suite())
-    ret_val.addTest(line_source.suite())
     ret_val.addTest(textformat.suite())
     ret_val.addTest(cli_syntax.suite())
     return ret_val
