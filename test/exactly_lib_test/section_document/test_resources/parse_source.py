ed111e0e01877dbe54b6e0e33d990fe7497308f8
Emil Karlén <emil.karlen@fripost.org>
2017-02-26 12:03:48 +0100

    instr-desc: ParseSource: Adds access of "column index"

--- test/exactly_lib_test/section_document/test_resources/parse_source.py
+++ test/exactly_lib_test/section_document/test_resources/parse_source.py
@@ -8,6 +8,7 @@ def assert_source(is_at_eof: asrt.ValueAssertion = asrt.anything_goes(),
                   has_current_line: asrt.ValueAssertion = asrt.anything_goes(),
                   current_line_number: asrt.ValueAssertion = asrt.anything_goes(),
                   current_line_text: asrt.ValueAssertion = asrt.anything_goes(),
+                  column_index: asrt.ValueAssertion = asrt.anything_goes(),
                   remaining_part_of_current_line: asrt.ValueAssertion = asrt.anything_goes(),
                   remaining_source: asrt.ValueAssertion = asrt.anything_goes(),
                   ) -> asrt.ValueAssertion:
@@ -23,6 +24,7 @@ def assert_source(is_at_eof: asrt.ValueAssertion = asrt.anything_goes(),
                 asrt.sub_component('is_at_eol__except_for_space', ParseSource.is_at_eol__except_for_space.fget,
                                    is_at_eol__except_for_space),
                 asrt.sub_component('current_line_number', ParseSource.current_line_number.fget, current_line_number),
+                asrt.sub_component('column_index', ParseSource.column_index.fget, column_index),
                 asrt.sub_component('current_line_text', ParseSource.current_line_text.fget, current_line_text),
                 asrt.sub_component('remaining_part_of_current_line', ParseSource.remaining_part_of_current_line.fget,
                                    remaining_part_of_current_line),
@@ -30,6 +32,16 @@ def assert_source(is_at_eof: asrt.ValueAssertion = asrt.anything_goes(),
         ]),
         asrt.sub_component('remaining_source', ParseSource.remaining_source.fget, remaining_source),
     ])
+
+
+every_line_is_consumed = assert_source(has_current_line=asrt.Boolean(False))
+
+
+def is_at_beginning_of_line(current_line_number: int) -> asrt.ValueAssertion:
+    return assert_source(
+        current_line_number=asrt.equals(current_line_number),
+        column_index=asrt.equals(0),
+    )
 
 
 source_is_at_end = assert_source(is_at_eof=asrt.Boolean(True),
