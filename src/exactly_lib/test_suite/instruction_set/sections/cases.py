bc9798b0ab64db3a16859113b9bb3499d0b6eb9e
Emil Karlén <emil.karlen@fripost.org>
2017-02-26 20:49:49 +0100

    instr-desc: Additional tests related to new parse structures: test suite/cases

--- src/exactly_lib/test_suite/instruction_set/sections/cases.py
+++ src/exactly_lib/test_suite/instruction_set/sections/cases.py
@@ -1,9 +1,8 @@
 from exactly_lib.section_document.document_parser import SectionElementParser
-from exactly_lib.section_document.parser_implementations.instruction_parsers import \
-    InstructionParserThatConsumesCurrentLine
+from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.section_document.parser_implementations.section_element_parsers import \
-    InstructionWithoutDescriptionParser, StandardSyntaxElementParser
-from exactly_lib.test_suite.instruction_set import instruction, utils
+    InstructionWithoutDescriptionParser, StandardSyntaxElementParser, InstructionParser
+from exactly_lib.test_suite.instruction_set import utils
 from exactly_lib.test_suite.instruction_set.instruction import Environment, TestSuiteInstruction
 
 
@@ -12,42 +11,19 @@ def new_parser() -> SectionElementParser:
 
 
 class TestCaseSectionInstruction(TestSuiteInstruction):
+    def __init__(self, resolver: utils.FileNamesResolver):
+        self._resolver = resolver
+
     def resolve_paths(self,
                       environment: Environment) -> list:
         """
         :raises FileNotAccessibleError: A referenced file is not accessible.
         :return: [pathlib.Path]
         """
-        raise NotImplementedError()
+        return self._resolver.resolve(environment)
 
 
-class _CasesSectionParser(InstructionParserThatConsumesCurrentLine):
-    def _parse(self, rest_of_line: str) -> TestCaseSectionInstruction:
-        line_text = rest_of_line.strip()
-        return _TestCaseWildcardFileInstruction(line_text) \
-            if utils.is_wildcard_pattern(line_text) \
-            else _TestCaseNonWildcardFileInstruction(line_text)
-
-
-class _TestCaseNonWildcardFileInstruction(TestCaseSectionInstruction):
-    """
-    Resolves a single path from a file-name that does not contain wild-cards.
-    """
-
-    def __init__(self, file_name: str):
-        self._file_name = file_name
-
-    def resolve_paths(self, environment: instruction.Environment) -> list:
-        return utils.resolve_non_wildcard_path(self._file_name, environment)
-
-
-class _TestCaseWildcardFileInstruction(TestCaseSectionInstruction):
-    """
-    Resolves a list of paths from a file-name pattern.
-    """
-
-    def __init__(self, pattern: str):
-        self._pattern = pattern
-
-    def resolve_paths(self, environment: instruction.Environment) -> list:
-        return utils.resolve_wildcard_paths(self._pattern, environment)
+class _CasesSectionParser(InstructionParser):
+    def parse(self, source: ParseSource) -> TestCaseSectionInstruction:
+        resolver = utils.parse_file_names_resolver(source)
+        return TestCaseSectionInstruction(resolver)
--- src/exactly_lib/test_suite/instruction_set/utils.py
+++ src/exactly_lib/test_suite/instruction_set/utils.py
@@ -1,7 +1,55 @@
+from exactly_lib.section_document.parse_source import ParseSource
+from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionInvalidArgumentException
+from exactly_lib.section_document.parser_implementations.token_parse import parse_token_on_current_line
 from exactly_lib.test_suite.instruction_set import instruction
 from exactly_lib.test_suite.instruction_set.instruction import FileNotAccessibleSimpleError
 
 _WILDCARD_CHARACTERS = ('*', '?', '[')
+
+
+class FileNamesResolver:
+    def resolve(self, environment: instruction.Environment) -> list:
+        raise NotImplementedError()
+
+
+def parse_file_names_resolver(source: ParseSource) -> FileNamesResolver:
+    token = parse_token_on_current_line(source, 'file name or glob pattern')
+    source.consume_initial_space_on_current_line()
+    if not source.is_at_eol:
+        msg = 'Superfluous argument: `{}\''.format(source.remaining_part_of_current_line)
+        raise SingleInstructionInvalidArgumentException(msg)
+    source.consume_current_line()
+    if token.is_quoted:
+        return FileNamesResolverForPlainFileName(token.string)
+    else:
+        if is_wildcard_pattern(token.string):
+            return FileNamesResolverForGlobPattern(token.string)
+        else:
+            return FileNamesResolverForPlainFileName(token.string)
+
+
+class FileNamesResolverForPlainFileName(FileNamesResolver):
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+
+    def resolve(self, environment: instruction.Environment) -> list:
+        path = environment.suite_file_dir_path / self.file_name
+        if not path.is_file():
+            raise FileNotAccessibleSimpleError(path)
+        return [path]
+
+
+class FileNamesResolverForGlobPattern(FileNamesResolver):
+    def __init__(self, pattern: str):
+        self.pattern = pattern
+
+    def resolve(self, environment: instruction.Environment) -> list:
+        paths = sorted(environment.suite_file_dir_path.glob(self.pattern))
+        for path in paths:
+            if not path.is_file():
+                raise FileNotAccessibleSimpleError(path)
+        return paths
 
 
 def resolve_non_wildcard_path(file_name: str, environment: instruction.Environment) -> list:
