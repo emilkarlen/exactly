a277388d371398bd7816a0fffe83c8be9da00fb6
Emil Karlén <emil.karlen@fripost.org>
2017-02-26 20:56:01 +0100

    instr-desc: Additional tests related to new parse structures: test suite/suites

--- test/exactly_lib_test/test_suite/instruction_set/sections/__init__.py
+++ test/exactly_lib_test/test_suite/instruction_set/sections/__init__.py
@@ -1,12 +1,13 @@
 import unittest
 
-from exactly_lib_test.test_suite.instruction_set.sections import configuration, cases
+from exactly_lib_test.test_suite.instruction_set.sections import configuration, cases, suites
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         configuration.suite(),
         cases.suite(),
+        suites.suite(),
     ])
 
 
