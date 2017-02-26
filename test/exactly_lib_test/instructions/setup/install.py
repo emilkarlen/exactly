23b31d24b05e4f61e96ee23a73a6e019ef0509ae
Emil Karlén <emil@member.fsf.org>
2017-02-26 16:26:54 +0100

    instr-desc: Additional tests related to new parse structures: setup/install

--- test/exactly_lib_test/instructions/setup/install.py
+++ test/exactly_lib_test/instructions/setup/install.py
@@ -1,59 +1,66 @@
 import unittest
 
 from exactly_lib.instructions.setup import install as sut
-from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
 from exactly_lib_test.instructions.setup.test_resources.instruction_check import TestCaseBase, Arrangement, \
     Expectation
 from exactly_lib_test.instructions.test_resources.assertion_utils import sh_check, svh_check
 from exactly_lib_test.instructions.test_resources.check_description import suite_for_instruction_documentation
+from exactly_lib_test.instructions.test_resources.single_line_source_instruction_utils import \
+    equivalent_source_variants, \
+    equivalent_source_variants__with_source_check
 from exactly_lib_test.test_resources.execution.sds_check import sds_contents_check as sds_contents_check
 from exactly_lib_test.test_resources.execution.sds_check import sds_populator
 from exactly_lib_test.test_resources.file_structure import DirContents, File, Dir, empty_file, empty_dir
-from exactly_lib_test.test_resources.parse import source4
 
 
 class TestParse(unittest.TestCase):
     def test_fail_when_there_is_no_arguments(self):
-        source = source4('')
-        with self.assertRaises(SingleInstructionInvalidArgumentException):
-            sut.Parser().parse(source)
+        instruction_argument = ''
+        for source in equivalent_source_variants(self, instruction_argument):
+            with self.assertRaises(SingleInstructionInvalidArgumentException):
+                sut.Parser().parse(source)
 
     def test_fail_when_there_is_more_than_two_arguments(self):
-        source = source4('argument1 argument2 argument3')
-        with self.assertRaises(SingleInstructionInvalidArgumentException):
-            sut.Parser().parse(source)
+        instruction_argument = 'argument1 argument2 argument3'
+        for source in equivalent_source_variants(self, instruction_argument):
+            with self.assertRaises(SingleInstructionInvalidArgumentException):
+                sut.Parser().parse(source)
 
     def test_succeed_when_there_is_exactly_one_argument(self):
-        source = source4('single-argument')
-        sut.Parser().parse(source)
+        instruction_argument = 'single-argument'
+        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
+            sut.Parser().parse(source)
 
     def test_succeed_when_there_is_exactly_two_arguments(self):
-        source = source4('argument1 argument2')
-        sut.Parser().parse(source)
+        instruction_argument = 'argument1 argument2'
+        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
+            sut.Parser().parse(source)
 
     def test_argument_shall_be_parsed_using_shell_syntax(self):
-        source = source4("'argument 1' 'argument 2'")
-        sut.Parser().parse(source)
+        instruction_argument = "'argument 1' 'argument 2'"
+        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
+            sut.Parser().parse(source)
 
 
 class TestCaseBaseForParser(TestCaseBase):
     def _run(self,
-             source: ParseSource,
+             instruction_argument: str,
              arrangement: Arrangement,
              expectation: Expectation):
-        self._check(sut.Parser(), source, arrangement, expectation)
+        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
+            self._check(sut.Parser(), source, arrangement, expectation)
 
 
 class TestValidationErrorScenarios(TestCaseBaseForParser):
     def test_ERROR_when_file_does_not_exist__without_explicit_destination(self):
-        self._run(source4('source-that-do-not-exist'),
+        self._run('source-that-do-not-exist',
                   Arrangement(),
                   Expectation(pre_validation_result=svh_check.is_validation_error()))
 
     def test_ERROR_when_file_does_not_exist__with_explicit_destination(self):
-        self._run(source4('source-that-do-not-exist destination'),
+        self._run('source-that-do-not-exist destination',
                   Arrangement(),
                   Expectation(pre_validation_result=svh_check.is_validation_error()))
 
@@ -63,7 +70,7 @@ class TestSuccessfulScenarios(TestCaseBaseForParser):
         file_name = 'existing-file'
         file_to_install = DirContents([(File(file_name,
                                              'contents'))])
-        self._run(source4(file_name),
+        self._run(file_name,
                   Arrangement(home_dir_contents=file_to_install),
                   Expectation(main_side_effects_on_files=sds_contents_check.act_dir_contains_exactly(
                       file_to_install))
@@ -72,7 +79,7 @@ class TestSuccessfulScenarios(TestCaseBaseForParser):
     def test_install_file__with_explicit_destination__non_existing_file(self):
         src = 'src-file'
         dst = 'dst-file'
-        self._run(source4('{} {}'.format(src, dst)),
+        self._run('{} {}'.format(src, dst),
                   Arrangement(home_dir_contents=DirContents([(File(src,
                                                                    'contents'))])),
                   Expectation(
@@ -88,7 +95,7 @@ class TestSuccessfulScenarios(TestCaseBaseForParser):
         home_dir_contents = [file_to_install]
         act_dir_contents = [empty_dir(dst)]
         act_dir_contents_after = [Dir(dst, [file_to_install])]
-        self._run(source4('{} {}'.format(src, dst)),
+        self._run('{} {}'.format(src, dst),
                   Arrangement(
                       home_dir_contents=DirContents(home_dir_contents),
                       sds_contents_before_main=sds_populator.act_dir_contents(DirContents(act_dir_contents))),
@@ -105,7 +112,7 @@ class TestSuccessfulScenarios(TestCaseBaseForParser):
                                              Dir('d2',
                                                  [File('f', 'f')])
                                              ])])
-        self._run(source4(src_dir),
+        self._run(src_dir,
                   Arrangement(home_dir_contents=files_to_install),
                   Expectation(main_side_effects_on_files=sds_contents_check.act_dir_contains_exactly(
                       files_to_install))
@@ -122,7 +129,7 @@ class TestSuccessfulScenarios(TestCaseBaseForParser):
                                  ])]
         act_dir_contents_before = DirContents([empty_dir(dst_dir)])
         act_dir_contents_after = DirContents([Dir(dst_dir, files_to_install)])
-        self._run(source4('{} {}'.format(src_dir, dst_dir)),
+        self._run('{} {}'.format(src_dir, dst_dir),
                   Arrangement(
                       home_dir_contents=DirContents(files_to_install),
                       sds_contents_before_main=sds_populator.act_dir_contents(act_dir_contents_before)),
@@ -137,7 +144,7 @@ class TestFailingScenarios(TestCaseBaseForParser):
         file_name = 'existing-file'
         file_to_install = DirContents([(File(file_name,
                                              'contents'))])
-        self._run(source4(file_name),
+        self._run(file_name,
                   Arrangement(
                       home_dir_contents=file_to_install,
                       sds_contents_before_main=sds_populator.act_dir_contents(DirContents(
@@ -151,7 +158,7 @@ class TestFailingScenarios(TestCaseBaseForParser):
         dst = 'dst-file-name'
         home_dir_contents = DirContents([(empty_file(src))])
         act_dir_contents = DirContents([empty_file(dst)])
-        self._run(source4('{} {}'.format(src, dst)),
+        self._run('{} {}'.format(src, dst),
                   Arrangement(
                       home_dir_contents=home_dir_contents,
                       sds_contents_before_main=sds_populator.act_dir_contents(act_dir_contents)
@@ -167,7 +174,7 @@ class TestFailingScenarios(TestCaseBaseForParser):
         home_dir_contents = DirContents([(empty_file(src))])
         act_dir_contents = DirContents([Dir(dst,
                                             [empty_file(src)])])
-        self._run(source4('{} {}'.format(src, dst)),
+        self._run('{} {}'.format(src, dst),
                   Arrangement(
                       home_dir_contents=home_dir_contents,
                       sds_contents_before_main=sds_populator.act_dir_contents(act_dir_contents)),
