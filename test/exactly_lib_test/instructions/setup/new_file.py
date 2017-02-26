1673daec0140bf5bed0616bd101e241d522e6126
Emil Karlén <emil.karlen@fripost.org>
2017-02-26 15:36:26 +0100

    instr-desc: Additional tests related to new parse structures: setup/file

--- test/exactly_lib_test/instructions/setup/new_file.py
+++ test/exactly_lib_test/instructions/setup/new_file.py
@@ -6,10 +6,13 @@ from exactly_lib.util.string import lines_content
 from exactly_lib_test.instructions.setup.test_resources.instruction_check import TestCaseBase, Arrangement, \
     Expectation
 from exactly_lib_test.instructions.test_resources.assertion_utils import sh_check
+from exactly_lib_test.instructions.test_resources.single_line_source_instruction_utils import \
+    equivalent_source_variants__with_source_check
+from exactly_lib_test.section_document.test_resources.parse_source import every_line_is_consumed
 from exactly_lib_test.test_resources.execution.sds_check.sds_contents_check import act_dir_contains_exactly
 from exactly_lib_test.test_resources.execution.sds_check.sds_populator import act_dir_contents
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir, Dir, empty_file, File
-from exactly_lib_test.test_resources.parse import argument_list_source, remaining_source
+from exactly_lib_test.test_resources.parse import argument_list_source
 
 
 class TestCaseBaseForParser(TestCaseBase):
@@ -22,16 +25,18 @@ class TestCaseBaseForParser(TestCaseBase):
 
 class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
     def test_file_in_sub_dir__sub_dir_exists(self):
-        self._run(remaining_source('existing-directory/file-name.txt'),
-                  Arrangement(sds_contents_before_main=act_dir_contents(DirContents([
-                      empty_dir('existing-directory')
-                  ]))
-                  ),
-                  Expectation(main_side_effects_on_files=act_dir_contains_exactly(DirContents([
-                      Dir('existing-directory', [
-                          empty_file('file-name.txt')])
-                  ])))
-                  )
+        instruction_argument = 'existing-directory/file-name.txt'
+        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
+            self._run(source,
+                      Arrangement(sds_contents_before_main=act_dir_contents(DirContents([
+                          empty_dir('existing-directory')
+                      ]))
+                      ),
+                      Expectation(main_side_effects_on_files=act_dir_contains_exactly(DirContents([
+                          Dir('existing-directory', [
+                              empty_file('file-name.txt')])
+                      ])))
+                      )
 
     def test_single_line_contents(self):
         self._run(argument_list_source(['file name', '<<MARKER'],
@@ -41,16 +46,19 @@ class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
                   Expectation(main_side_effects_on_files=act_dir_contains_exactly(DirContents([
                       File('file name',
                            lines_content(['single line']))
-                  ])))
+                  ])),
+                      source=every_line_is_consumed)
                   )
 
     def test_argument_is_existing_file(self):
-        self._run(remaining_source('existing-file'),
-                  Arrangement(sds_contents_before_main=act_dir_contents(DirContents([
-                      empty_file('existing-file')
-                  ]))),
-                  Expectation(main_result=sh_check.is_hard_error())
-                  )
+        instruction_argument = 'existing-file'
+        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
+            self._run(source,
+                      Arrangement(sds_contents_before_main=act_dir_contents(DirContents([
+                          empty_file('existing-file')
+                      ]))),
+                      Expectation(main_result=sh_check.is_hard_error())
+                      )
 
 
 def suite() -> unittest.TestSuite:
