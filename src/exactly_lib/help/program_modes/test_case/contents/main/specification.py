4ad5d009f2c7d7a190b2a1c9f9d287769fe6aeff
Emil Karlén <emil.karlen@fripost.org>
2017-02-27 05:07:14 +0100

    Help text: REFACT: Code org of "Test case overview"

--- src/exactly_lib/help/program_modes/test_case/contents/main/specification.py
+++ src/exactly_lib/help/program_modes/test_case/contents/main/specification.py
@@ -1,8 +1,5 @@
 from exactly_lib.common.help import cross_reference_id as cross_ref
-from exactly_lib.help.program_modes.test_case.contents.main.intro_environment import execution_documentation
-from exactly_lib.help.program_modes.test_case.contents.main.intro_intro import intro_intro_documentation
-from exactly_lib.help.program_modes.test_case.contents.main.intro_phases import phases_documentation
-from exactly_lib.help.program_modes.test_case.contents.main.intro_test_case import test_case_intro_documentation
+from exactly_lib.help.program_modes.test_case.contents.main.overview.renderer import OverviewDocumentationRenderer
 from exactly_lib.help.program_modes.test_case.contents.main.ref_test_case_files import TestCaseFileDocumentationRenderer
 from exactly_lib.help.program_modes.test_case.contents.main.ref_test_case_processing import \
     test_case_processing_documentation
@@ -26,21 +23,8 @@ class SpecificationRenderer(TestCaseHelpRendererBase):
 
         ow_target_factory = cross_ref.sub_component_factory('overview',
                                                             target_factory)
-
         self._OVERVIEW_TI = ow_target_factory.root('Overview')
-
-        self._OV__INTRO_TI = ow_target_factory.sub('Introduction',
-                                                   'introduction')
-
-        self._OV__TEST_CASES_TI = ow_target_factory.sub('Test cases',
-                                                        'test-cases')
-
-        self._OV__ENVIRONMENT_TI = ow_target_factory.sub('Environment',
-                                                         'environment')
-
-        self._OV__PHASES_TI = ow_target_factory.sub('Phases',
-                                                    'phases')
-
+        self._overview_renderer = OverviewDocumentationRenderer(self.setup, ow_target_factory)
         self._OUTCOME_TI = target_factory.sub('Test outcome',
                                               'outcome')
 
@@ -55,13 +39,8 @@ class SpecificationRenderer(TestCaseHelpRendererBase):
 
     def target_info_hierarchy(self) -> list:
         return [
-            cross_ref.TargetInfoNode(self._OVERVIEW_TI, [
-                _leaf(self._OV__INTRO_TI),
-                _leaf(self._OV__TEST_CASES_TI),
-                _leaf(self._OV__ENVIRONMENT_TI),
-                _leaf(self._OV__PHASES_TI),
-
-            ]),
+            cross_ref.TargetInfoNode(self._OVERVIEW_TI,
+                                     self._overview_renderer.target_info_hierarchy()),
             _leaf(self._OUTCOME_TI),
             cross_ref.TargetInfoNode(self._TEST_CASE_FILES_TI,
                                      self._test_case_file_renderer.target_info_hierarchy()),
@@ -70,25 +49,11 @@ class SpecificationRenderer(TestCaseHelpRendererBase):
 
     def apply(self, environment: RenderingEnvironment) -> doc.SectionContents:
         setup = Setup(self.test_case_help)
-        test_case_intro_contents = test_case_intro_documentation(setup)
-        phases_contents = phases_documentation(setup)
-        execution_contents = execution_documentation(setup)
         return doc.SectionContents(
             [para(ONE_LINE_DESCRIPTION)],
             [
                 doc.Section(self._OVERVIEW_TI.anchor_text(),
-                            doc.SectionContents(
-                                [],
-                                [
-                                    doc.Section(self._OV__INTRO_TI.anchor_text(),
-                                                intro_intro_documentation(setup)),
-                                    doc.Section(self._OV__TEST_CASES_TI.anchor_text(),
-                                                test_case_intro_contents),
-                                    doc.Section(self._OV__ENVIRONMENT_TI.anchor_text(),
-                                                execution_contents),
-                                    doc.Section(self._OV__PHASES_TI.anchor_text(),
-                                                phases_contents),
-                                ])),
+                            self._overview_renderer.apply(environment)),
                 doc.Section(self._TEST_CASE_FILES_TI.anchor_text(),
                             self._test_case_file_renderer.apply(environment)),
                 doc.Section(self._TEST_CASE_PROCESSING_TI.anchor_text(),
