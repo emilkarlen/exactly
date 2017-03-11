2c46685defd13f09b06accf2343be58b627d1960
Emil Karlén <emil@member.fsf.org>
2017-03-11 03:54:24 +0100

    val-def: parse_destination_path: Removes unused variant for list-of-arguments

--- src/exactly_lib/instructions/utils/arg_parse/parse_destination_path.py
+++ src/exactly_lib/instructions/utils/arg_parse/parse_destination_path.py
@@ -43,39 +43,6 @@ def parse_destination_path__token_stream(options: RelOptionArgumentConfiguration
         return DestinationPath(relativity_type, path_argument)
 
 
-# TODO [instr-desc] Remove when new parser structures are fully integrated
-# (perhaps should remain since there are valid usages of this variant, but then rename methods)
-def parse_destination_path(options: RelOptionArgumentConfiguration,
-                           path_argument_is_mandatory: bool,
-                           arguments: list) -> (DestinationPath, list):
-    (destination_type, remaining_arguments) = _parse_destination_type(options.options,
-                                                                      arguments)
-    if not remaining_arguments:
-        if path_argument_is_mandatory:
-            raise SingleInstructionInvalidArgumentException(
-                'Missing {} argument: {}'.format(options.argument_syntax_name,
-                                                 arguments))
-        path_argument = pathlib.PurePath()
-        return DestinationPath(destination_type, path_argument), remaining_arguments
-    else:
-        ensure_is_not_option_argument(remaining_arguments[0])
-        path_argument = pathlib.PurePosixPath(remaining_arguments[0])
-        return DestinationPath(destination_type, path_argument), remaining_arguments[1:]
-
-
-def _parse_destination_type(options: RelOptionsConfiguration,
-                            arguments: list) -> (RelOptionType, list):
-    rel_option_type = options.default_option
-    if arguments and is_option_argument(arguments[0]):
-        option_argument = arguments[0]
-        rel_option_type = _resolve_relativity_option_type(option_argument)
-        if rel_option_type not in options.accepted_options:
-            msg = 'Option cannot be used in this context: {}'.format(option_argument)
-            raise SingleInstructionInvalidArgumentException(msg)
-        return rel_option_type, arguments[1:]
-    return rel_option_type, arguments
-
-
 def _parse_relativity_type__token_stream(options: RelOptionsConfiguration,
                                          source: TokenStream2) -> RelOptionType:
     rel_option_type = options.default_option
