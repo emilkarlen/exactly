196213ca8a4fce7ececc2061cd525fd529464a3f
Emil Karlén <emil@member.fsf.org>
2017-03-14 23:42:58 +0100

    val-def: Adds StringValue

--- src/exactly_lib/value_definition/concrete_values.py
+++ src/exactly_lib/value_definition/concrete_values.py
@@ -2,6 +2,15 @@ from exactly_lib.test_case_file_structure.file_ref import FileRef
 from exactly_lib.value_definition.value_structure import Value
 
 
+class StringValue(Value):
+    def __init__(self, string: str):
+        self._string = string
+
+    @property
+    def string(self) -> str:
+        return self._string
+
+
 class FileRefValue(Value):
     def __init__(self, file_ref: FileRef):
         self._file_ref = file_ref
@@ -22,7 +31,12 @@ class ValueVisitor:
         """
         if isinstance(value, FileRefValue):
             return self._visit_file_ref(value)
+        if isinstance(value, StringValue):
+            return self._visit_string(value)
         raise TypeError('Unknown {}: {}'.format(Value, str(value)))
+
+    def _visit_string(self, value: StringValue):
+        raise NotImplementedError()
 
     def _visit_file_ref(self, value: FileRefValue):
         raise NotImplementedError()
--- test/exactly_lib_test/value_definition/concrete_values.py
+++ test/exactly_lib_test/value_definition/concrete_values.py
@@ -1,6 +1,7 @@
 import unittest
 
 from exactly_lib.value_definition import concrete_values as sut
+from exactly_lib.value_definition.concrete_values import StringValue
 from exactly_lib_test.test_case_file_structure.test_resources.simple_file_ref import file_ref_test_impl
 
 
@@ -11,25 +12,45 @@ def suite() -> unittest.TestSuite:
 class TestValueVisitor(unittest.TestCase):
     def test_visit_file_ref(self):
         # ARRANGE #
-        visitor = _ValueVisitorTestThatRegistersClassOfVisitedObjects()
+        visitor = _ValueVisitorTestThatRegistersClassOfVisitedObjects('ret-val')
         # ACT #
-        visitor.visit(sut.FileRefValue(file_ref_test_impl()))
+        ret_val = visitor.visit(sut.FileRefValue(file_ref_test_impl()))
         # ASSERT #
+        self.assertEqual('ret-val', ret_val,
+                         'Visitor is expected to return value from visit-method')
         self.assertListEqual(visitor.visited_classes,
                              [sut.FileRefValue],
                              'visited classes')
 
+    def test_visit_string(self):
+        # ARRANGE #
+        visitor = _ValueVisitorTestThatRegistersClassOfVisitedObjects('return value')
+        # ACT #
+        ret_val = visitor.visit(StringValue('string'))
+        # ASSERT #
+        self.assertEqual('return value', ret_val,
+                         'Visitor is expected to return value from visit-method')
+        self.assertListEqual(visitor.visited_classes,
+                             [sut.StringValue],
+                             'visited classes')
+
     def test_visit_non_sub_class_should_raise_exception(self):
         # ARRANGE #
-        visitor = _ValueVisitorTestThatRegistersClassOfVisitedObjects()
+        visitor = _ValueVisitorTestThatRegistersClassOfVisitedObjects('return value')
         # ACT #
         with self.assertRaises(TypeError):
             visitor.visit('a string is not a ValueReference')
 
 
 class _ValueVisitorTestThatRegistersClassOfVisitedObjects(sut.ValueVisitor):
-    def __init__(self):
+    def __init__(self, ret_val):
+        self.ret_val = ret_val
         self.visited_classes = []
 
     def _visit_file_ref(self, value: sut.FileRefValue):
-        self.visited_classes.append(sut.FileRefValue)
+        self.visited_classes.append(type(value))
+        return self.ret_val
+
+    def _visit_string(self, value: sut.StringValue):
+        self.visited_classes.append(type(value))
+        return self.ret_val
