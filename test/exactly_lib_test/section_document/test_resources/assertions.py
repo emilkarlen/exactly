4d13a3326cc3c85a39ba522705770bd3a7c983df
Emil Karlén <emil@member.fsf.org>
2017-03-15 23:09:06 +0100

    val-def: Adds value assertion for Line

--- test/exactly_lib_test/section_document/test_resources/assertions.py
+++ test/exactly_lib_test/section_document/test_resources/assertions.py
@@ -2,12 +2,13 @@ import unittest
 
 from exactly_lib.util.line_source import Line, LineSequence
 from exactly_lib_test.test_resources.assertions.assert_utils import assertion_message
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 
 
 def assert_equals_line(test_case: unittest.TestCase,
                        expected: Line,
                        actual: Line,
-                       message_header: str=None):
+                       message_header: str = None):
     """
     :param expected: May be None
     :param actual: May be None
@@ -24,10 +25,21 @@ def assert_equals_line(test_case: unittest.TestCase,
                           assertion_message('Line text', message_header))
 
 
+def equals_line(expected: Line) -> asrt.ValueAssertion:
+    return asrt.is_instance_with(Line,
+                                 asrt.And([
+                                     asrt.sub_component('line_number',
+                                                        asrt.equals(expected.line_number)),
+                                     asrt.sub_component('text',
+                                                        Line.text.fget,
+                                                        asrt.equals(expected.text))
+                                 ]))
+
+
 def assert_equals_line_sequence(test_case: unittest.TestCase,
                                 expected: LineSequence,
                                 actual: LineSequence,
-                                message_header: str=None):
+                                message_header: str = None):
     """
     :param expected: May be None
     :param actual: May be None
