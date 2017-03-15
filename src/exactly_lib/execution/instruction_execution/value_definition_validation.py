a3b10545029b50b12169e78e544f22e3aab46186
Emil Karlén <emil.karlen@fripost.org>
2017-03-15 20:28:04 +0100

    val-def: Adaption to new structures: value_definition_validation

--- src/exactly_lib/execution/instruction_execution/value_definition_validation.py
+++ src/exactly_lib/execution/instruction_execution/value_definition_validation.py
@@ -1,66 +1,49 @@
 from exactly_lib.execution.instruction_execution.single_instruction_executor import \
     PartialInstructionControlledFailureInfo, PartialControlledFailureEnum
-from exactly_lib.test_case_file_structure.file_ref_relativity import RelOptionType
 from exactly_lib.util import error_message_format
 from exactly_lib.util.symbol_table import SymbolTable
-from exactly_lib.value_definition import symbol_table_contents as sym_tbl, value_definition_usage
-from exactly_lib.value_definition.symbol_table_contents import FileRefValue
+from exactly_lib.value_definition import value_structure as vs
+from exactly_lib.value_definition.value_structure import ValueContainer
 
 
-def validate_pre_sds(value_usage: value_definition_usage.ValueUsage,
+def validate_pre_sds(value_usage: vs.ValueUsage2,
                      symbol_table: SymbolTable) -> PartialInstructionControlledFailureInfo:
-    if isinstance(value_usage, value_definition_usage.ValueReference):
-        assert isinstance(value_usage, value_definition_usage.ValueReference)
+    if isinstance(value_usage, vs.ValueReference2):
+        assert isinstance(value_usage, vs.ValueReference2)
         if not symbol_table.contains(value_usage.name):
             return PartialInstructionControlledFailureInfo(
                 PartialControlledFailureEnum.VALIDATION,
                 'Referenced definition `{}\' is undefined.'.format(value_usage.name))
-        if isinstance(value_usage, value_definition_usage.ValueReferenceOfPath):
-            err_msg = _validate_reference_of_path(value_usage, symbol_table)
+        else:
+            err_msg = _validate_reference(value_usage, symbol_table)
             if err_msg is not None:
                 return PartialInstructionControlledFailureInfo(PartialControlledFailureEnum.VALIDATION, err_msg)
         return None
-    elif isinstance(value_usage, value_definition_usage.ValueDefinition):
-        assert isinstance(value_usage, value_definition_usage.ValueDefinition)
+    elif isinstance(value_usage, vs.ValueDefinition2):
+        assert isinstance(value_usage, vs.ValueDefinition2)
         if symbol_table.contains(value_usage.name):
-            file_ref_value = symbol_table.lookup(value_usage.name)
-            assert isinstance(file_ref_value, FileRefValue), 'Currently only type FileRefValue:s are supported'
+            already_defined_value_container = symbol_table.lookup(value_usage.name)
+            assert isinstance(already_defined_value_container, ValueContainer), 'Value in SymTbl must be ValueContainer'
             return PartialInstructionControlledFailureInfo(
                 PartialControlledFailureEnum.VALIDATION,
                 'Name `{}\' has already been defined:\n\n{}'.format(
                     value_usage.name,
                     error_message_format.source_line(
-                        file_ref_value.source)))
+                        already_defined_value_container.source)))
         else:
-            for referenced_value in value_usage.referenced_values:
+            for referenced_value in value_usage.references:
                 failure_info = validate_pre_sds(referenced_value, symbol_table)
                 if failure_info is not None:
                     return failure_info
             symbol_table.add(value_usage.symbol_table_entry)
             return None
     else:
-        raise TypeError('Unknown variant of {}: {}'.format(str(value_definition_usage.ValueUsage),
+        raise TypeError('Unknown variant of {}: {}'.format(str(vs.ValueUsage2),
                                                            str(value_usage)))
 
 
-def _validate_reference_of_path(value_usage: value_definition_usage.ValueReferenceOfPath,
-                                value_definitions: SymbolTable) -> str:
-    referenced_value = value_definitions.lookup(value_usage.name)
-    if not isinstance(referenced_value, sym_tbl.FileRefValue):
-        return 'Referenced definition {} is not a path.'.format(value_usage.name)
-    assert isinstance(referenced_value, sym_tbl.FileRefValue)
-    actual_relativity = referenced_value.file_ref.relativity(value_definitions)
-    if actual_relativity not in value_usage.valid_relativities.rel_option_types:
-        return _invalid_relativity_error_message(value_usage, actual_relativity)
-    return None
-
-
-def _invalid_relativity_error_message(value_usage: value_definition_usage.ValueReferenceOfPath,
-                                      actual_relativity: RelOptionType) -> str:
-    # TODO [val-def] Improve error message -
-    # - include valid relativities
-    # - use option strings to display relativities (or environment variables)
-    return ('Referenced variable `{}\' c,'
-            ' since it is relative {}'.format(value_usage.name,
-                                              actual_relativity
-                                              ))
+def _validate_reference(value_usage: vs.ValueReference2,
+                        value_definitions: SymbolTable) -> str:
+    referenced_value_container = value_definitions.lookup(value_usage.name)
+    assert isinstance(referenced_value_container, ValueContainer), 'Values in SymbolTable must be ValueContainer'
+    return value_usage.value_restriction.is_satisfied_by(value_definitions, referenced_value_container.value)
