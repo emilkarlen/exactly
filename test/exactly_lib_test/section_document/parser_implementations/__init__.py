8dcdadfeba2aeff55ff518bf97900805e67b3c47
Emil Karlén <emil.karlen@fripost.org>
2017-03-17 20:48:19 +0100

    val-def: Test resources for assertions on TokenStream2

--- test/exactly_lib_test/section_document/parser_implementations/__init__.py
+++ test/exactly_lib_test/section_document/parser_implementations/__init__.py
@@ -3,6 +3,7 @@ import unittest
 from exactly_lib_test.section_document.parser_implementations import optional_description_and_instruction_parser
 from exactly_lib_test.section_document.parser_implementations import parser_for_dictionary_of_instructions
 from exactly_lib_test.section_document.parser_implementations import section_element_parsers
+from exactly_lib_test.section_document.parser_implementations import test_resources_test
 from exactly_lib_test.section_document.parser_implementations import token_parse, token_stream2
 
 
@@ -13,6 +14,7 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(parser_for_dictionary_of_instructions.suite())
     ret_val.addTest(token_parse.suite())
     ret_val.addTest(token_stream2.suite())
+    ret_val.addTest(test_resources_test.suite())
     return ret_val
 
 
