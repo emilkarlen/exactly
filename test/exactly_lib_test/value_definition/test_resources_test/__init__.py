66c500f2d14a1b1626994f2ea91f4b2df4bb409a
Emil Karlén <emil@member.fsf.org>
2017-03-18 01:05:14 +0100

    val-def: Test of test resources: concrete_restriction_assertion

--- test/exactly_lib_test/value_definition/test_resources_test/__init__.py
+++ test/exactly_lib_test/value_definition/test_resources_test/__init__.py
@@ -1,10 +1,12 @@
 import unittest
 
+from exactly_lib_test.value_definition.test_resources_test import concrete_restriction_assertion
 from exactly_lib_test.value_definition.test_resources_test import value_definition, value_reference
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
+    ret_val.addTest(concrete_restriction_assertion.suite())
     ret_val.addTest(value_reference.suite())
     ret_val.addTest(value_definition.suite())
     return ret_val
