b413e92b0357d8e111971fd53fed0053f34b6121
Emil Karlén <emil.karlen@fripost.org>
2017-03-18 05:23:57 +0100

    val-def: Test of test resources: equals_file_ref_value

--- test/exactly_lib_test/value_definition/test_resources_test/value_structure_assertions.py
+++ test/exactly_lib_test/value_definition/test_resources_test/value_structure_assertions.py
@@ -10,6 +10,7 @@ from exactly_lib_test.value_definition.test_resources import concrete_value_asse
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(TestEqualsString),
+        unittest.makeSuite(TestEqualsFileRef),
     ])
 
 
@@ -34,3 +35,26 @@ class TestEqualsString(unittest.TestCase):
         put = test_case_with_failure_exception_set_to_test_exception()
         with put.assertRaises(TestException):
             sut.equals_string_value(expected).apply_without_message(put, actual)
+
+
+class TestEqualsFileRef(unittest.TestCase):
+    def test_equals(self):
+        # ARRANGE #
+        value = FileRefValue(file_ref_test_impl('file-name'))
+        sut.equals_file_ref_value(value).apply_without_message(self, value)
+
+    def test_not_equals__different_file_name(self):
+        # ARRANGE #
+        expected = FileRefValue(file_ref_test_impl('expected-file-name'))
+        actual = FileRefValue(file_ref_test_impl('actual-file-name'))
+        put = test_case_with_failure_exception_set_to_test_exception()
+        with put.assertRaises(TestException):
+            sut.equals_file_ref_value(expected).apply_without_message(put, actual)
+
+    def test_not_equals__different_type(self):
+        # ARRANGE #
+        expected = FileRefValue(file_ref_test_impl('file-name'))
+        actual = StringValue('expected string')
+        put = test_case_with_failure_exception_set_to_test_exception()
+        with put.assertRaises(TestException):
+            sut.equals_file_ref_value(expected).apply_without_message(put, actual)
