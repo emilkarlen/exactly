5e16c02f12febc32d94e577eb75f3213c9e5724f
Emil Karlén <emil.karlen@fripost.org>
2017-03-18 05:18:09 +0100

    val-def: Test of test resources: equals_string_value

---
+++ test/exactly_lib_test/value_definition/test_resources_test/value_structure_assertions.py
@@ -0,0 +1,36 @@
+import unittest
+
+from exactly_lib.value_definition.concrete_values import StringValue, FileRefValue
+from exactly_lib_test.test_case_file_structure.test_resources.simple_file_ref import file_ref_test_impl
+from exactly_lib_test.test_resources.test_of_test_resources_util import \
+    test_case_with_failure_exception_set_to_test_exception, TestException
+from exactly_lib_test.value_definition.test_resources import concrete_value_assertion as sut
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(TestEqualsString),
+    ])
+
+
+class TestEqualsString(unittest.TestCase):
+    def test_equals(self):
+        # ARRANGE #
+        value = StringValue('s')
+        sut.equals_string_value(value).apply_without_message(self, value)
+
+    def test_not_equals__different_string(self):
+        # ARRANGE #
+        expected = StringValue('expected string')
+        actual = StringValue('actual string')
+        put = test_case_with_failure_exception_set_to_test_exception()
+        with put.assertRaises(TestException):
+            sut.equals_string_value(expected).apply_without_message(put, actual)
+
+    def test_not_equals__different_type(self):
+        # ARRANGE #
+        expected = StringValue('expected string')
+        actual = FileRefValue(file_ref_test_impl('file-name'))
+        put = test_case_with_failure_exception_set_to_test_exception()
+        with put.assertRaises(TestException):
+            sut.equals_string_value(expected).apply_without_message(put, actual)
