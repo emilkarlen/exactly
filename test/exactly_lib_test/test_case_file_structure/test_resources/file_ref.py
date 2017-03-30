4b79ae4589ffdfcbd87467eb55e078c7c9bafe4c
Emil Karlén <emil.karlen@fripost.org>
2017-03-30 21:50:39 +0200

    symbols: Completes impl of PathPartAsStringSymbolReference

--- test/exactly_lib_test/test_case_file_structure/test_resources/file_ref.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/file_ref.py
@@ -2,23 +2,23 @@ import pathlib
 import unittest
 
 from exactly_lib.test_case_file_structure.file_ref import FileRef
-from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants
 from exactly_lib.test_case_file_structure.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.test_case_file_structure.relativity_root import RelOptionType
 from exactly_lib.test_case_file_structure.sandbox_directory_structure import SandboxDirectoryStructure
 from exactly_lib.util.line_source import Line
 from exactly_lib.util.symbol_table import SymbolTable
+from exactly_lib.value_definition import concrete_restrictions
 from exactly_lib.value_definition import value_structure as vs
-from exactly_lib.value_definition.concrete_restrictions import FileRefRelativityRestriction
-from exactly_lib.value_definition.concrete_values import FileRefValue
-from exactly_lib.value_definition.value_structure import ValueReference, ValueContainer
+from exactly_lib.value_definition.concrete_values import FileRefValue, StringValue
+from exactly_lib.value_definition.value_structure import ValueReference, ValueContainer, Value
 from exactly_lib_test.test_case_file_structure.test_resources.concrete_path_part import equals_path_part
 from exactly_lib_test.test_case_file_structure.test_resources.simple_file_ref import file_ref_test_impl
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.value_definition.test_resources import value_reference_assertions as vr_tr
 from exactly_lib_test.value_definition.test_resources.concrete_restriction_assertion import \
-    equals_file_ref_relativity_restriction
+    equals_value_restriction
 from exactly_lib_test.value_definition.test_resources.value_definition_utils import file_ref_value
 
 
@@ -66,12 +66,14 @@ class _FileRefAssertion(asrt.ValueAssertion):
                                                     self._fake_value_definitions_according_to_expected())
 
     def _fake_value_definitions_according_to_expected(self) -> SymbolTable:
+        value_constructor = _ValueCorrespondingToValueRestriction()
         elements = {}
         for ref in self._expected.value_references_of_paths():
-            assert isinstance(ref, vs.ValueReference)
+            assert isinstance(ref, vs.ValueReference), "Informs IDE of type"
             value_restriction = ref.value_restriction
-            assert isinstance(value_restriction, FileRefRelativityRestriction)
-            elements[ref.name] = _value_container(file_ref_val_test_impl(value_restriction.accepted))
+            assert isinstance(value_restriction, concrete_restrictions.ValueRestriction)
+            value = value_constructor.visit(value_restriction)
+            elements[ref.name] = _value_container(value)
         return SymbolTable(elements)
 
     def _equals_value_references(self,
@@ -88,9 +90,8 @@ class _FileRefAssertion(asrt.ValueAssertion):
             assert isinstance(actual_ref, ValueReference)
             assert isinstance(expected_ref, ValueReference)
             expected_value_restriction = expected_ref.value_restriction
-            assert isinstance(expected_value_restriction, FileRefRelativityRestriction)
             vr_tr.equals_value_reference(expected_ref.name,
-                                         equals_file_ref_relativity_restriction(expected_value_restriction)
+                                         equals_value_restriction(expected_value_restriction)
                                          ).apply(put, actual_ref,
                                                  mb.for_sub_component('[%d]' % idx))
 
@@ -103,3 +104,14 @@ def file_ref_val_test_impl(valid_relativities: PathRelativityVariants) -> FileRe
 
 def _value_container(value: FileRefValue) -> ValueContainer:
     return ValueContainer(Line(1, 'source line'), value)
+
+
+class _ValueCorrespondingToValueRestriction(concrete_restrictions.ValueRestrictionVisitor):
+    def visit_none(self, x: concrete_restrictions.NoRestriction) -> Value:
+        return StringValue('a string (from <no restriction>)')
+
+    def visit_string(self, x: concrete_restrictions.StringRestriction) -> Value:
+        return StringValue('a string (from <string value restriction>)')
+
+    def visit_file_ref_relativity(self, x: concrete_restrictions.FileRefRelativityRestriction) -> Value:
+        return file_ref_val_test_impl(x.accepted)
