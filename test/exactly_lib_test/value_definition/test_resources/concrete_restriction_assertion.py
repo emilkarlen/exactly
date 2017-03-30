4a2e6780a399d9cee06fa70a2552f90ef4832def
Emil Karlén <emil@member.fsf.org>
2017-03-30 19:07:21 +0200

    symbols: test utility: equals_string_restriction

--- test/exactly_lib_test/value_definition/test_resources/concrete_restriction_assertion.py
+++ test/exactly_lib_test/value_definition/test_resources/concrete_restriction_assertion.py
@@ -1,6 +1,13 @@
 from exactly_lib.value_definition.concrete_restrictions import FileRefRelativityRestriction, StringRestriction
+from exactly_lib.value_definition.value_structure import ValueRestriction
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.value_definition.test_resources.path_relativity import equals_path_relativity_variants
+
+is_string_value_restriction = asrt.is_instance(StringRestriction)
+
+
+def equals_string_restriction(expected: StringRestriction) -> asrt.ValueAssertion:
+    return is_string_value_restriction
 
 
 def equals_file_ref_relativity_restriction(expected: FileRefRelativityRestriction) -> asrt.ValueAssertion:
@@ -10,4 +17,5 @@ def equals_file_ref_relativity_restriction(expected: FileRefRelativityRestrictio
                                                     equals_path_relativity_variants(expected.accepted)))
 
 
-is_string_value_restriction = asrt.is_instance(StringRestriction)
+def equals_value_restriction(expected: ValueRestriction) -> asrt.ValueAssertion:
+    raise NotImplementedError()
--- test/exactly_lib_test/value_definition/test_resources_test/concrete_restriction_assertion.py
+++ test/exactly_lib_test/value_definition/test_resources_test/concrete_restriction_assertion.py
@@ -1,7 +1,8 @@
 import unittest
 
 from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants, RelOptionType
-from exactly_lib.value_definition.concrete_restrictions import FileRefRelativityRestriction, NoRestriction
+from exactly_lib.value_definition.concrete_restrictions import FileRefRelativityRestriction, NoRestriction, \
+    StringRestriction
 from exactly_lib_test.test_resources.test_of_test_resources_util import \
     test_case_with_failure_exception_set_to_test_exception, TestException
 from exactly_lib_test.value_definition.test_resources import concrete_restriction_assertion as sut
@@ -9,8 +10,26 @@ from exactly_lib_test.value_definition.test_resources import concrete_restrictio
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
+        unittest.makeSuite(TestEqualsStringRestriction),
         unittest.makeSuite(TestEqualsFileRefRelativityRestriction),
     ])
+
+
+class TestEqualsStringRestriction(unittest.TestCase):
+    def test_equals(self):
+        test_cases = [
+            StringRestriction(),
+        ]
+        for restriction in test_cases:
+            with self.subTest():
+                sut.equals_string_restriction(restriction).apply_without_message(self, restriction)
+
+    def test_not_equals__different__types(self):
+        put = test_case_with_failure_exception_set_to_test_exception()
+        expected = StringRestriction()
+        actual = NoRestriction()
+        with put.assertRaises(TestException):
+            sut.equals_string_restriction(expected).apply_without_message(put, actual)
 
 
 class TestEqualsFileRefRelativityRestriction(unittest.TestCase):
