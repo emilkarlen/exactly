ea325267e0740ee2285ba0dbeaf57fe951b2f588
Emil Karlén <emil@member.fsf.org>
2017-03-30 20:25:50 +0200

    symbols: Test of: PathPartAsFixedPath

--- test/exactly_lib_test/test_case_file_structure/concrete_path_parts.py
+++ test/exactly_lib_test/test_case_file_structure/concrete_path_parts.py
@@ -1,10 +1,46 @@
 import unittest
 
 from exactly_lib.test_case_file_structure import concrete_path_parts as sut
+from exactly_lib.util.symbol_table import empty_symbol_table
 
 
 def suite() -> unittest.TestSuite:
-    return unittest.makeSuite(TestPathPartVisitor)
+    return unittest.TestSuite([
+        unittest.makeSuite(TestPathPartAsFixedPath),
+        unittest.makeSuite(TestPathPartVisitor),
+    ])
+
+
+class TestPathPartAsFixedPath(unittest.TestCase):
+    def test_file_name(self):
+        # ARRANGE #
+        path_part = sut.PathPartAsFixedPath('the file name')
+        # ACT #
+        actual = path_part.file_name
+        # ASSERT #
+        self.assertEquals('the file name',
+                          actual,
+                          'file name')
+
+    def test_value_references(self):
+        # ARRANGE #
+        path_part = sut.PathPartAsFixedPath('the file name')
+        # ACT #
+        actual = path_part.value_references
+        # ASSERT #
+        self.assertEquals([],
+                          actual,
+                          'value references')
+
+    def test_resolve(self):
+        # ARRANGE #
+        path_part = sut.PathPartAsFixedPath('the file name')
+        # ACT #
+        actual = path_part.resolve(empty_symbol_table())
+        # ASSERT #
+        self.assertEquals('the file name',
+                          actual,
+                          'resolved file name')
 
 
 class TestPathPartVisitor(unittest.TestCase):
