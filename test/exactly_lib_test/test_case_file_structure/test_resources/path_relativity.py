eae2208c3eea0fa34be1ec71d773ce6c6be3a151
Emil Karlén <emil.karlen@fripost.org>
2017-04-02 16:16:45 +0200

    file-struct: Adds assertion on SpecificPathRelativity

--- test/exactly_lib_test/test_case_file_structure/test_resources/path_relativity.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/path_relativity.py
@@ -1,5 +1,21 @@
-from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants
+from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants, SpecificPathRelativity
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+
+
+def equals_path_relativity(expected: SpecificPathRelativity) -> asrt.ValueAssertion:
+    return asrt.is_instance_with(SpecificPathRelativity,
+                                 asrt.and_([
+                                     asrt.sub_component('is_absolute',
+                                                        SpecificPathRelativity.is_absolute.fget,
+                                                        asrt.equals(expected.is_absolute)),
+                                     asrt.sub_component('is_relative',
+                                                        SpecificPathRelativity.is_relative.fget,
+                                                        asrt.equals(expected.is_relative)),
+                                     asrt.sub_component('relativity_type',
+                                                        SpecificPathRelativity.relativity_type.fget,
+                                                        asrt.equals(expected.relativity_type)),
+                                 ])
+                                 )
 
 
 def path_relativity_variants_equals(expected: PathRelativityVariants) -> asrt.ValueAssertion:
