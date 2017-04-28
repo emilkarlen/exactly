7d5a96c995142780114b8d78aa35b395f85f306d
Emil Karlén <emil@member.fsf.org>
2017-04-28 21:50:41 +0200

    help rendering: HtmlDocGeneratorForHelpHelp: Removes unused inheritance

--- src/exactly_lib/help/html_doc/parts/help.py
+++ src/exactly_lib/help/html_doc/parts/help.py
@@ -1,16 +1,14 @@
 from exactly_lib.common.help import cross_reference_id as cross_ref
-from exactly_lib.common.help.cross_reference_id import CustomTargetInfoFactory, CrossReferenceId
-from exactly_lib.help.html_doc.parts.utils.section_document_renderer_base import HtmlDocGeneratorForSectionDocumentBase
+from exactly_lib.common.help.cross_reference_id import CustomTargetInfoFactory
 from exactly_lib.help.program_modes.help.cli_syntax import HelpCliSyntaxDocumentation
 from exactly_lib.help.utils.cli_program_documentation_rendering import ProgramDocumentationSectionContentsRenderer
 from exactly_lib.help.utils.section_contents_renderer import RenderingEnvironment
 from exactly_lib.util.textformat.structure import document as doc
 
 
-class HtmlDocGeneratorForHelpHelp(HtmlDocGeneratorForSectionDocumentBase):
-    def __init__(self,
-                 rendering_environment: RenderingEnvironment):
-        super().__init__(rendering_environment)
+class HtmlDocGeneratorForHelpHelp:
+    def __init__(self, rendering_environment: RenderingEnvironment):
+        self.rendering_environment = rendering_environment
 
     def apply(self, targets_factory: CustomTargetInfoFactory) -> (list, doc.SectionContents):
         cli_syntax_targets_factory = cross_ref.sub_component_factory('cli-syntax',
@@ -33,9 +31,3 @@ class HtmlDocGeneratorForHelpHelp(HtmlDocGeneratorForSectionDocumentBase):
     def _cli_syntax_contents(self) -> doc.SectionContents:
         renderer = ProgramDocumentationSectionContentsRenderer(HelpCliSyntaxDocumentation())
         return renderer.apply(self.rendering_environment)
-
-    def _section_cross_ref_target(self, phase):
-        raise ValueError('This method should never be called for ' + str(type(self)))
-
-    def _instruction_cross_ref_target(self, instruction_doc, section) -> CrossReferenceId:
-        raise ValueError('This method should never be called for ' + str(type(self)))
