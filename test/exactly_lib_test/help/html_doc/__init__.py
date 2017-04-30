6e375afc2cb82f2f94c8a56d2721777573807cba
Emil Karlén <emil.karlen@fripost.org>
2017-04-30 16:29:04 +0200

    help rendering: use SectionGenerator: suite doc

--- test/exactly_lib_test/help/html_doc/__init__.py
+++ test/exactly_lib_test/help/html_doc/__init__.py
@@ -1,11 +1,12 @@
 import unittest
 
-from exactly_lib_test.help.html_doc.parts import test_case
+from exactly_lib_test.help.html_doc.parts import test_case, test_suite
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_case.suite())
+    ret_val.addTest(test_suite.suite())
     return ret_val
 
 
