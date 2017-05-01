7a5eb64459522ba15f4a38de1f31346385558aac
Emil Karlén <emil.karlen@fripost.org>
2017-05-01 21:33:59 +0200

    FileRef: Fixes invalid arguments to file_path_pre_or_post_sds

--- src/exactly_lib/instructions/assert_/utils/file_contents/instruction_for_equality.py
+++ src/exactly_lib/instructions/assert_/utils/file_contents/instruction_for_equality.py
@@ -74,7 +74,7 @@ class EqualsAssertionInstruction(AssertPhaseInstruction):
                                             directory=str(environment.sds.tmp.internal_dir))
         else:
             file_ref = self._expected_contents.file_reference_resolver.resolve(environment.symbols)
-            return file_ref.file_path_pre_or_post_sds(environment)
+            return file_ref.file_path_pre_or_post_sds(environment.home_and_sds)
 
     def _file_ref_check_for_expected(self) -> FileRefCheck:
         return FileRefCheck(self._expected_contents.file_reference_resolver,
