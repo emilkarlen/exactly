ca58cbbd3765ba2ba98c61a84028fa4eaa6ba1ab
Emil Karlén <emil@member.fsf.org>
2017-05-09 19:06:46 +0200

    symbols in more phases: act phase: test_symbol_that_does_meet_restriction_in_validate_symbols

--- test/exactly_lib_test/execution/partial_execution/phase_step_sequence/act_phase_symbol_handling.py
+++ test/exactly_lib_test/execution/partial_execution/phase_step_sequence/act_phase_symbol_handling.py
@@ -3,20 +3,64 @@ import unittest
 from exactly_lib.execution.phase_step_identifiers import phase_step_simple as phase_step
 from exactly_lib.execution.result import PartialResultStatus
 from exactly_lib.symbol.concrete_restrictions import StringRestriction
+from exactly_lib.test_case.phases.cleanup import PreviousPhase
 from exactly_lib_test.execution.partial_execution.test_resources.recording.test_case_generation_for_sequence_tests import \
     TestCaseGeneratorThatRecordsExecutionWithExtraInstructionList, \
     TestCaseGeneratorForExecutionRecording
 from exactly_lib_test.execution.partial_execution.test_resources.recording.test_case_that_records_phase_execution import \
     Expectation, Arrangement, TestCaseBase
 from exactly_lib_test.execution.test_resources import instruction_test_resources as test
+from exactly_lib_test.execution.test_resources.execution_recording.phase_steps import SYMBOL_VALIDATION_STEPS__ONCE, \
+    PRE_SDS_VALIDATION_STEPS__ONCE
 from exactly_lib_test.execution.test_resources.test_actions import action_that_returns, action_that_raises
 from exactly_lib_test.symbol.test_resources import symbol_utils
 from exactly_lib_test.symbol.test_resources.symbol_utils import symbol_reference
-from exactly_lib_test.test_resources.expected_instruction_failure import ExpectedFailureForPhaseFailure
+from exactly_lib_test.test_resources.expected_instruction_failure import ExpectedFailureForPhaseFailure, \
+    ExpectedFailureForNoFailure
 
 
 def suite() -> unittest.TestSuite:
-    return unittest.makeSuite(TestFailingScenarios)
+    return unittest.TestSuite([
+        unittest.makeSuite(TestSuccessfulScenarios),
+        unittest.makeSuite(TestFailingScenarios),
+    ])
+
+
+class TestSuccessfulScenarios(TestCaseBase):
+    def test_symbol_that_does_meet_restriction_in_validate_symbols(self):
+        test_case = _single_successful_instruction_in_each_phase()
+        symbol_name = 'symbol_name'
+        reference_to_string_symbol = symbol_utils.symbol_reference(symbol_name,
+                                                                   StringRestriction())
+        definition_of_string_symbol = symbol_utils.string_symbol_definition(symbol_name)
+        symbol_usages = [
+            definition_of_string_symbol,
+            reference_to_string_symbol,
+        ]
+        self._check(
+            Arrangement(test_case,
+                        act_executor_symbol_usages=action_that_returns(symbol_usages)),
+            Expectation(PartialResultStatus.PASS,
+                        ExpectedFailureForNoFailure(),
+                        SYMBOL_VALIDATION_STEPS__ONCE +
+
+                        PRE_SDS_VALIDATION_STEPS__ONCE +
+
+                        [phase_step.SETUP__MAIN,
+
+                         phase_step.SETUP__VALIDATE_POST_SETUP,
+                         phase_step.ACT__VALIDATE_POST_SETUP,
+                         phase_step.BEFORE_ASSERT__VALIDATE_POST_SETUP,
+                         phase_step.ASSERT__VALIDATE_POST_SETUP,
+
+                         phase_step.ACT__PREPARE,
+                         phase_step.ACT__EXECUTE,
+
+                         phase_step.BEFORE_ASSERT__MAIN,
+                         phase_step.ASSERT__MAIN,
+                         (phase_step.CLEANUP__MAIN, PreviousPhase.ASSERT),
+                         ],
+                        sandbox_directory_structure_should_exist=True))
 
 
 class TestFailingScenarios(TestCaseBase):
