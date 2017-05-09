e30242b545d9488b2cff4ea2b087dbc3e16484f5
Emil Karlén <emil@member.fsf.org>
2017-05-09 18:55:45 +0200

    symbols in more phases: act phase: test_symbol_that_does_not_meet_restriction_in_validate_symbols

--- test/exactly_lib_test/execution/partial_execution/phase_step_sequence/act_phase_symbol_handling.py
+++ test/exactly_lib_test/execution/partial_execution/phase_step_sequence/act_phase_symbol_handling.py
@@ -2,6 +2,7 @@ import unittest
 
 from exactly_lib.execution.phase_step_identifiers import phase_step_simple as phase_step
 from exactly_lib.execution.result import PartialResultStatus
+from exactly_lib.symbol.concrete_restrictions import StringRestriction
 from exactly_lib_test.execution.partial_execution.test_resources.recording.test_case_generation_for_sequence_tests import \
     TestCaseGeneratorThatRecordsExecutionWithExtraInstructionList, \
     TestCaseGeneratorForExecutionRecording
@@ -9,6 +10,7 @@ from exactly_lib_test.execution.partial_execution.test_resources.recording.test_
     Expectation, Arrangement, TestCaseBase
 from exactly_lib_test.execution.test_resources import instruction_test_resources as test
 from exactly_lib_test.execution.test_resources.test_actions import action_that_returns, action_that_raises
+from exactly_lib_test.symbol.test_resources import symbol_utils
 from exactly_lib_test.symbol.test_resources.symbol_utils import symbol_reference
 from exactly_lib_test.test_resources.expected_instruction_failure import ExpectedFailureForPhaseFailure
 
@@ -38,6 +40,27 @@ class TestFailingScenarios(TestCaseBase):
             Arrangement(test_case,
                         act_executor_symbol_usages=action_that_raises(test.ImplementationErrorTestException())),
             Expectation(PartialResultStatus.IMPLEMENTATION_ERROR,
+                        ExpectedFailureForPhaseFailure.new_with_step(phase_step.ACT__VALIDATE_SYMBOLS),
+                        [
+                            phase_step.SETUP__VALIDATE_SYMBOLS,
+                            phase_step.ACT__VALIDATE_SYMBOLS,
+                        ],
+                        sandbox_directory_structure_should_exist=False))
+
+    def test_symbol_that_does_not_meet_restriction_in_validate_symbols(self):
+        test_case = _single_successful_instruction_in_each_phase()
+        symbol_name = 'symbol_name'
+        reference_to_string_symbol = symbol_utils.symbol_reference(symbol_name,
+                                                                   StringRestriction())
+        definition_of_path_symbol = symbol_utils.file_ref_symbol_definition(symbol_name)
+        symbol_usages = [
+            definition_of_path_symbol,
+            reference_to_string_symbol,
+        ]
+        self._check(
+            Arrangement(test_case,
+                        act_executor_symbol_usages=action_that_returns(symbol_usages)),
+            Expectation(PartialResultStatus.VALIDATE,
                         ExpectedFailureForPhaseFailure.new_with_step(phase_step.ACT__VALIDATE_SYMBOLS),
                         [
                             phase_step.SETUP__VALIDATE_SYMBOLS,
