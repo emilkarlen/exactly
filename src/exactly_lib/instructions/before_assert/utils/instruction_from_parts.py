5f034fb02060ff22e1ab0b8e0630527deb5523eb
Emil Karlén <emil@member.fsf.org>
2017-05-25 19:12:55 +0200

    Utils for multi-phase instructions using instr parts: phase: before-assert

--- src/exactly_lib/instructions/before_assert/utils/instruction_from_parts.py
+++ src/exactly_lib/instructions/before_assert/utils/instruction_from_parts.py
@@ -1,6 +1,9 @@
+from exactly_lib.instructions.multi_phase_instructions.utils.parser import InstructionPartsParser
 from exactly_lib.instructions.utils.instruction_parts import InstructionParts, \
     InstructionInfoForConstructingAnInstructionFromParts
 from exactly_lib.instructions.utils.pre_or_post_validation import PreOrPostSdsSvhValidationErrorValidator
+from exactly_lib.section_document.parse_source import ParseSource
+from exactly_lib.section_document.parser_implementations.section_element_parsers import InstructionParser
 from exactly_lib.test_case.os_services import OsServices
 from exactly_lib.test_case.phases.before_assert import BeforeAssertPhaseInstruction
 from exactly_lib.test_case.phases.common import InstructionEnvironmentForPreSdsStep, \
@@ -14,6 +17,9 @@ class BeforeAssertPhaseInstructionFromValidatorAndExecutor(BeforeAssertPhaseInst
                  instruction_setup: InstructionParts):
         self.setup = instruction_setup
         self._validator = PreOrPostSdsSvhValidationErrorValidator(instruction_setup.validator)
+
+    def symbol_usages(self) -> list:
+        return self.setup.symbol_usages
 
     def validate_pre_sds(self,
                          environment: InstructionEnvironmentForPreSdsStep) -> svh.SuccessOrValidationErrorOrHardError:
@@ -30,6 +36,15 @@ class BeforeAssertPhaseInstructionFromValidatorAndExecutor(BeforeAssertPhaseInst
         return self.setup.executor.apply_as_non_assertion(environment,
                                                           environment.phase_logging,
                                                           os_services)
+
+
+class Parser(InstructionParser):
+    def __init__(self, instruction_parts_parser: InstructionPartsParser):
+        self.instruction_parts_parser = instruction_parts_parser
+
+    def parse(self, source: ParseSource) -> BeforeAssertPhaseInstruction:
+        instruction_parts = self.instruction_parts_parser.parse(source)
+        return BeforeAssertPhaseInstructionFromValidatorAndExecutor(instruction_parts)
 
 
 def instruction_info_for(instruction_name: str) -> InstructionInfoForConstructingAnInstructionFromParts:
