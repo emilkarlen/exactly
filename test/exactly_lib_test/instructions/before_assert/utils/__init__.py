e3684886b2b8b8c16774c51d4f866af26726a28c
Emil Karlén <emil.karlen@fripost.org>
2017-05-25 19:12:55 +0200

    Utils for multi-phase instructions using instr parts: phase: before-assert

--- test/exactly_lib_test/instructions/before_assert/utils/__init__.py
+++ test/exactly_lib_test/instructions/before_assert/utils/__init__.py
@@ -1,10 +1,14 @@
 import unittest
 
-from exactly_lib_test.instructions.before_assert.utils import instruction_from_parts_that_executes_sub_process
+from exactly_lib_test.instructions.before_assert.utils import instruction_from_parts_that_executes_sub_process, \
+    instruction_from_parts
 
 
 def suite() -> unittest.TestSuite:
-    return instruction_from_parts_that_executes_sub_process.suite()
+    return unittest.TestSuite([
+        instruction_from_parts.suite(),
+        instruction_from_parts_that_executes_sub_process.suite(),
+    ])
 
 
 def run_suite():
