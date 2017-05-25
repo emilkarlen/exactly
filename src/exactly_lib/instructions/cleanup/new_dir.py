16edbb186af8bfb464951aab7e9d58065d574075
Emil Karlén <emil.karlen@fripost.org>
2017-05-25 19:59:18 +0200

    multi-phase_instr: Impl using new multi-phase utils: cleanup/new_dir

--- src/exactly_lib/instructions/cleanup/new_dir.py
+++ src/exactly_lib/instructions/cleanup/new_dir.py
@@ -1,32 +1,9 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
-from exactly_lib.instructions.multi_phase_instructions import new_dir as mkdir_utils
-from exactly_lib.section_document.parser_implementations.instruction_parsers import \
-    InstructionParserThatConsumesCurrentLine
-from exactly_lib.symbol.concrete_values import FileRefResolver
-from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.cleanup import CleanupPhaseInstruction, PreviousPhase
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
-from exactly_lib.test_case.phases.result import sh
+from exactly_lib.instructions.cleanup.utils import instruction_from_parts
+from exactly_lib.instructions.multi_phase_instructions import new_dir as new_dir_utils
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        Parser(),
-        mkdir_utils.TheInstructionDocumentation(instruction_name))
-
-
-class Parser(InstructionParserThatConsumesCurrentLine):
-    def _parse(self, rest_of_line: str) -> CleanupPhaseInstruction:
-        argument = mkdir_utils.parse(rest_of_line)
-        return _Instruction(argument)
-
-
-class _Instruction(CleanupPhaseInstruction):
-    def __init__(self, dir_path_resolver: FileRefResolver):
-        self.dir_path_resolver = dir_path_resolver
-
-    def main(self,
-             environment: InstructionEnvironmentForPostSdsStep,
-             os_services: OsServices,
-             previous_phase: PreviousPhase) -> sh.SuccessOrHardError:
-        return mkdir_utils.execute_and_return_sh(environment.path_resolving_environment, self.dir_path_resolver)
+        instruction_from_parts.Parser(new_dir_utils.Parser()),
+        new_dir_utils.TheInstructionDocumentation(instruction_name))
