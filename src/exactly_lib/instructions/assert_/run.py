6ff99e14e77abf1a91d648b83e03114bdb5b8b68
Emil Karlén <emil@member.fsf.org>
2017-05-26 20:51:21 +0200

    multi-phase_instr: REFACT: Simplifies "run" instruction using new tools

--- src/exactly_lib/instructions/assert_/run.py
+++ src/exactly_lib/instructions/assert_/run.py
@@ -1,10 +1,10 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
-from exactly_lib.instructions.assert_.utils.instruction_from_parts import instruction_info_for
+from exactly_lib.instructions.assert_.utils import instruction_from_parts
 from exactly_lib.instructions.multi_phase_instructions import run
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        run.instruction_parser(instruction_info_for(instruction_name)),
+        instruction_from_parts.Parser(run.parts_parser(instruction_name)),
         run.TheInstructionDocumentation(instruction_name,
-                                        "Runs a program and PASS if, and only if, its exit code is 0"))
+                                        description_rest_text=run.ASSERT_PHASE_DESCRIPTION_REST))
