4f6afda83ab35b0c7bd1db6ee18b240d06e32839
Emil Karlén <emil@member.fsf.org>
2017-05-26 15:21:17 +0200

    multi-phase_instr: REFACT of "change_dir" using new "instruction embryo"

--- src/exactly_lib/instructions/before_assert/change_dir.py
+++ src/exactly_lib/instructions/before_assert/change_dir.py
@@ -1,32 +1,11 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
+from exactly_lib.instructions.before_assert.utils import instruction_from_parts
 from exactly_lib.instructions.multi_phase_instructions import change_dir as cd_utils
-from exactly_lib.section_document.parser_implementations.instruction_parsers import \
-    InstructionParserThatConsumesCurrentLine
-from exactly_lib.symbol.concrete_values import FileRefResolver
-from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.before_assert import BeforeAssertPhaseInstruction
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
-from exactly_lib.test_case.phases.result import sh
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        Parser(),
-        cd_utils.TheInstructionDocumentation(instruction_name, is_after_act_phase=True))
-
-
-class Parser(InstructionParserThatConsumesCurrentLine):
-    def _parse(self, rest_of_line: str) -> BeforeAssertPhaseInstruction:
-        destination_directory = cd_utils.parse(rest_of_line, is_after_act_phase=True)
-        return _Instruction(destination_directory)
-
-
-class _Instruction(BeforeAssertPhaseInstruction):
-    def __init__(self, destination_directory: FileRefResolver):
-        self.destination_directory = destination_directory
-
-    def main(self,
-             environment: InstructionEnvironmentForPostSdsStep,
-             os_services: OsServices) -> sh.SuccessOrHardError:
-        return cd_utils.execute_with_sh_result(self.destination_directory,
-                                               environment.path_resolving_environment)
+        instruction_from_parts.Parser(cd_utils.PartsParser(is_after_act_phase=True)),
+        cd_utils.TheInstructionDocumentation(instruction_name,
+                                             is_after_act_phase=True,
+                                             is_in_assert_phase=False))
