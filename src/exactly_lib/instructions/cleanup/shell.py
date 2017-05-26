e2bc7f38ca8aa7b749ef8b2e08cc31290d112848
Emil Karlén <emil.karlen@fripost.org>
2017-05-26 21:44:12 +0200

    multi-phase_instr: REFACT: Simplifies "shell" instruction using new tools

--- src/exactly_lib/instructions/cleanup/shell.py
+++ src/exactly_lib/instructions/cleanup/shell.py
@@ -1,10 +1,9 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
-from exactly_lib.instructions.cleanup.utils.instruction_from_parts import \
-    instruction_info_for
+from exactly_lib.instructions.cleanup.utils import instruction_from_parts
 from exactly_lib.instructions.multi_phase_instructions import shell as shell_common
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-        shell_common.instruction_parser(instruction_info_for(instruction_name)),
+        instruction_from_parts.Parser(shell_common.parts_parser(instruction_name)),
         shell_common.DescriptionForNonAssertPhaseInstruction(instruction_name))
