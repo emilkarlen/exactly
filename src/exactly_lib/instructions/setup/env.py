fc371b23f81fe0e9be7a0501268f606b738b7386
Emil Karlén <emil@member.fsf.org>
2017-05-26 14:36:52 +0200

    multi-phase_instr: REFACT of "env" using new "instruction embryo"

--- src/exactly_lib/instructions/setup/env.py
+++ src/exactly_lib/instructions/setup/env.py
@@ -1,27 +1,9 @@
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
-from exactly_lib.instructions.multi_phase_instructions import env
-from exactly_lib.test_case.os_services import OsServices
-from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
-from exactly_lib.test_case.phases.result import sh
-from exactly_lib.test_case.phases.setup import SetupPhaseInstruction, SetupSettingsBuilder
+from exactly_lib.instructions.multi_phase_instructions import env as env_instruction
+from exactly_lib.instructions.setup.utils import instruction_from_parts
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
     return SingleInstructionSetup(
-            env.Parser(_Instruction),
-        env.TheInstructionDocumentation(instruction_name))
-
-
-class _Instruction(SetupPhaseInstruction):
-    def __init__(self,
-                 executor: env.Executor):
-        self.executor = executor
-
-    def main(self,
-             environment: InstructionEnvironmentForPostSdsStep,
-             os_services: OsServices,
-             settings_builder: SetupSettingsBuilder) -> sh.SuccessOrHardError:
-        return env.execute_and_return_sh(self.executor, environment.environ)
-
-
-PARSER = env.Parser(_Instruction)
+        instruction_from_parts.Parser(env_instruction.PartsParser()),
+        env_instruction.TheInstructionDocumentation(instruction_name))
--- test/exactly_lib_test/instructions/multi_phase_instructions/utils/__init__.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/utils/__init__.py
@@ -1,11 +1,12 @@
 import unittest
 
-from exactly_lib_test.instructions.multi_phase_instructions.utils import parse
+from exactly_lib_test.instructions.multi_phase_instructions.utils import parse, instruction_embryo
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         parse.suite(),
+        instruction_embryo.suite(),
     ])
 
 
