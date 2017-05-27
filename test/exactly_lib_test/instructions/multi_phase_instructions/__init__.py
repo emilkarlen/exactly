77eed7b05cdd02d0506178dd4d6a58863a94159e
Emil Karlén <emil@member.fsf.org>
2017-05-27 15:44:04 +0200

    run instruction: Test of instruction embryo properties

--- test/exactly_lib_test/instructions/multi_phase_instructions/__init__.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/__init__.py
@@ -1,7 +1,10 @@
 import unittest
 
 from exactly_lib_test.instructions.multi_phase_instructions import \
-    test_resources_test, utils, new_dir, change_dir, run, new_file, env, shell
+    test_resources_test, utils, \
+    new_dir, change_dir, \
+    run, run_tests_of_instruction_embryo, \
+    new_file, env, shell
 
 
 def suite() -> unittest.TestSuite:
@@ -12,6 +15,7 @@ def suite() -> unittest.TestSuite:
         change_dir.suite(),
         new_file.suite(),
         run.suite(),
+        run_tests_of_instruction_embryo.suite(),
         env.suite(),
         shell.suite(),
     ])
