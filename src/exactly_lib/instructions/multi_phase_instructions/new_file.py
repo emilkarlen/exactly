13f7069cefcfe6d0108a2dfd3039b2868b62702a
Emil Karlén <emil@member.fsf.org>
2017-05-28 01:49:58 +0200

    setup/stdin instruction: man page: Adds symbol-reference info

--- src/exactly_lib/instructions/multi_phase_instructions/new_file.py
+++ src/exactly_lib/instructions/multi_phase_instructions/new_file.py
@@ -37,9 +37,10 @@ class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderi
             dt.paths_uses_posix_syntax())
 
     def invokation_variants(self) -> list:
-        arguments = path_syntax.mandatory_path_with_optional_relativity(_PATH_ARGUMENT,
-                                                                        True,
-                                                                        REL_OPT_ARG_CONF.path_suffix_is_required)
+        arguments = path_syntax.mandatory_path_with_optional_relativity(
+            _PATH_ARGUMENT,
+            REL_OPT_ARG_CONF.options.is_rel_symbol_option_accepted,
+            REL_OPT_ARG_CONF.path_suffix_is_required)
         here_doc_arg = a.Single(a.Multiplicity.MANDATORY, dt.HERE_DOCUMENT)
         return [
             InvokationVariant(self._cl_syntax_for_args(arguments),
