be30ef8885c783ea26d6880fd98ae439f90dcf12
Emil Karlén <emil@member.fsf.org>
2017-05-28 20:25:08 +0200

    Test of relativity root resolvers

--- test/exactly_lib_test/test_case_file_structure/__init__.py
+++ test/exactly_lib_test/test_case_file_structure/__init__.py
@@ -1,13 +1,14 @@
 import unittest
 
 from exactly_lib_test.test_case_file_structure import concrete_path_parts, file_refs, relativity_validation
-from exactly_lib_test.test_case_file_structure import sandbox_directory_structure, test_resources_test
+from exactly_lib_test.test_case_file_structure import sandbox_directory_structure, relativity_root, test_resources_test
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(sandbox_directory_structure.suite())
+    ret_val.addTest(relativity_root.suite())
     ret_val.addTest(relativity_validation.suite())
     ret_val.addTest(concrete_path_parts.suite())
     ret_val.addTest(file_refs.suite())
