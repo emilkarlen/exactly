f48dafc2aa712d2dae7f8bcd52f8a318d0bc27a4
Emil Karlén <emil.karlen@fripost.org>
2017-05-28 22:00:34 +0200

    Adds REL_X_OPTION_MAP from new types of Relativity Option Types

--- src/exactly_lib/test_case_file_structure/relative_path_options.py
+++ src/exactly_lib/test_case_file_structure/relative_path_options.py
@@ -1,6 +1,8 @@
 from exactly_lib.help_texts import file_ref as file_ref_texts
+from exactly_lib.test_case_file_structure import relativity_root
+from exactly_lib.test_case_file_structure.path_relativity import RelSdsOptionType, RelNonHomeOptionType
 from exactly_lib.test_case_file_structure.relativity_root import RelOptionType, RelRootResolver, \
-    resolver_for_tmp_user, resolver_for_cwd, resolver_for_home, resolver_for_act, resolver_for_result
+    RelSdsRootResolver, RelNonHomeRootResolver
 from exactly_lib.util.cli_syntax.elements import argument
 
 
@@ -24,21 +26,97 @@ class RelOptionInfo(tuple):
         return self[2]
 
 
+class RelSdsOptionInfo(tuple):
+    def __new__(cls,
+                option_name: argument.OptionName,
+                root_resolver: RelSdsRootResolver,
+                description: str):
+        return tuple.__new__(cls, (option_name, root_resolver, description))
+
+    @property
+    def option_name(self) -> argument.OptionName:
+        return self[0]
+
+    @property
+    def root_resolver(self) -> RelSdsRootResolver:
+        return self[1]
+
+    @property
+    def description(self) -> str:
+        return self[2]
+
+
+class RelNonHomeOptionInfo(tuple):
+    def __new__(cls,
+                option_name: argument.OptionName,
+                root_resolver: RelNonHomeRootResolver,
+                description: str):
+        return tuple.__new__(cls, (option_name, root_resolver, description))
+
+    @property
+    def option_name(self) -> argument.OptionName:
+        return self[0]
+
+    @property
+    def root_resolver(self) -> RelNonHomeRootResolver:
+        return self[1]
+
+    @property
+    def description(self) -> str:
+        return self[2]
+
+
+REL_SDS_OPTIONS_MAP = {
+    RelSdsOptionType.REL_ACT: RelSdsOptionInfo(file_ref_texts.REL_ACT_OPTION_NAME,
+                                               relativity_root.resolver_rel_sds__for_act,
+                                               file_ref_texts.RELATIVITY_DESCRIPTION_ACT),
+
+    RelSdsOptionType.REL_TMP: RelSdsOptionInfo(file_ref_texts.REL_TMP_OPTION_NAME,
+                                               relativity_root.resolver_rel_sds__for_tmp_user,
+                                               file_ref_texts.RELATIVITY_DESCRIPTION_TMP),
+
+    RelSdsOptionType.REL_RESULT: RelSdsOptionInfo(file_ref_texts.REL_RESULT_OPTION_NAME,
+                                                  relativity_root.resolver_rel_sds__for_result,
+                                                  file_ref_texts.RELATIVITY_DESCRIPTION_RESULT),
+}
+
+REL_NON_HOME_OPTIONS_MAP = {
+    RelNonHomeOptionType.REL_CWD: RelNonHomeOptionInfo(file_ref_texts.REL_CWD_OPTION_NAME,
+                                                       relativity_root.resolver_rel_non_home__for_cwd,
+                                                       file_ref_texts.RELATIVITY_DESCRIPTION_CWD),
+
+    RelNonHomeOptionType.REL_ACT: RelNonHomeOptionInfo(file_ref_texts.REL_ACT_OPTION_NAME,
+                                                       relativity_root.resolver_rel_non_home__for_act,
+                                                       file_ref_texts.RELATIVITY_DESCRIPTION_ACT),
+
+    RelNonHomeOptionType.REL_TMP: RelNonHomeOptionInfo(file_ref_texts.REL_TMP_OPTION_NAME,
+                                                       relativity_root.resolver_rel_non_home__for_tmp_user,
+                                                       file_ref_texts.RELATIVITY_DESCRIPTION_TMP),
+
+    RelNonHomeOptionType.REL_RESULT: RelNonHomeOptionInfo(file_ref_texts.REL_RESULT_OPTION_NAME,
+                                                          relativity_root.resolver_rel_non_home__for_result,
+                                                          file_ref_texts.RELATIVITY_DESCRIPTION_RESULT),
+}
+
 REL_OPTIONS_MAP = {
     RelOptionType.REL_HOME: RelOptionInfo(file_ref_texts.REL_HOME_OPTION_NAME,
-                                          resolver_for_home,
+                                          relativity_root.resolver_for_home,
                                           file_ref_texts.RELATIVITY_DESCRIPTION_HOME),
+
     RelOptionType.REL_CWD: RelOptionInfo(file_ref_texts.REL_CWD_OPTION_NAME,
-                                         resolver_for_cwd,
+                                         relativity_root.resolver_for_cwd,
                                          file_ref_texts.RELATIVITY_DESCRIPTION_CWD),
+
     RelOptionType.REL_ACT: RelOptionInfo(file_ref_texts.REL_ACT_OPTION_NAME,
-                                         resolver_for_act,
+                                         relativity_root.resolver_for_act,
                                          file_ref_texts.RELATIVITY_DESCRIPTION_ACT),
+
     RelOptionType.REL_TMP: RelOptionInfo(file_ref_texts.REL_TMP_OPTION_NAME,
-                                         resolver_for_tmp_user,
+                                         relativity_root.resolver_for_tmp_user,
                                          file_ref_texts.RELATIVITY_DESCRIPTION_TMP),
+
     RelOptionType.REL_RESULT: RelOptionInfo(file_ref_texts.REL_RESULT_OPTION_NAME,
-                                            resolver_for_result,
+                                            relativity_root.resolver_for_result,
                                             file_ref_texts.RELATIVITY_DESCRIPTION_RESULT),
 }
 
--- test/exactly_lib_test/test_case_file_structure/__init__.py
+++ test/exactly_lib_test/test_case_file_structure/__init__.py
@@ -1,6 +1,7 @@
 import unittest
 
 from exactly_lib_test.test_case_file_structure import concrete_path_parts, file_refs, relativity_validation
+from exactly_lib_test.test_case_file_structure import relativity_root, relative_path_options
 from exactly_lib_test.test_case_file_structure import sandbox_directory_structure, relativity_root, test_resources_test
 
 
@@ -9,6 +10,7 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(sandbox_directory_structure.suite())
     ret_val.addTest(relativity_root.suite())
+    ret_val.addTest(relative_path_options.suite())
     ret_val.addTest(relativity_validation.suite())
     ret_val.addTest(concrete_path_parts.suite())
     ret_val.addTest(file_refs.suite())
