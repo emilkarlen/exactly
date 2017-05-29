13108a9da6f66f96521d28e7027a1732764fe176
Emil Karlén <emil@member.fsf.org>
2017-05-30 01:08:13 +0200

    Test resources: REFACT: dir populators: home-or-sds-populator

--- test/exactly_lib_test/test_case_file_structure/test_resources/home_and_sds_check/home_and_sds_populators.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/home_and_sds_check/home_and_sds_populators.py
@@ -1,21 +1,17 @@
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_file_structure.path_relativity import RelOptionType
 from exactly_lib.test_case_file_structure.relative_path_options import REL_OPTIONS_MAP
+from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import HomeOrSdsPopulator
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check import sds_populator
 from exactly_lib_test.test_resources import file_structure
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir_contents
-
-
-class HomeOrSdsPopulator:
-    def write_to(self, home_and_sds: HomeAndSds):
-        raise NotImplementedError()
 
 
 class HomeOrSdsPopulatorForHomeContents(HomeOrSdsPopulator):
     def __init__(self, home_dir_contents: file_structure.DirContents):
         self.home_dir_contents = home_dir_contents
 
-    def write_to(self, home_and_sds: HomeAndSds):
+    def populate_home_or_sds(self, home_and_sds: HomeAndSds):
         self.home_dir_contents.write_to(home_and_sds.home_dir_path)
 
 
@@ -26,7 +22,7 @@ class HomeOrSdsPopulatorForRelOptionType(HomeOrSdsPopulator):
         self.relativity = relativity
         self.dir_contents = dir_contents
 
-    def write_to(self, home_and_sds: HomeAndSds):
+    def populate_home_or_sds(self, home_and_sds: HomeAndSds):
         root_path = REL_OPTIONS_MAP[self.relativity].root_resolver.from_home_and_sds(home_and_sds)
         self.dir_contents.write_to(root_path)
 
@@ -35,7 +31,7 @@ class HomeOrSdsPopulatorForSdsContents(HomeOrSdsPopulator):
     def __init__(self, sds_contents: sds_populator.SdsPopulator):
         self.sds_contents = sds_contents
 
-    def write_to(self, home_and_sds: HomeAndSds):
+    def populate_home_or_sds(self, home_and_sds: HomeAndSds):
         self.sds_contents.populate_sds(home_and_sds.sds)
 
 
@@ -51,9 +47,9 @@ class _ListOfPopulators(HomeOrSdsPopulator):
     def __init__(self, populator_list: list):
         self.__populator_list = populator_list
 
-    def write_to(self, home_and_sds: HomeAndSds):
+    def populate_home_or_sds(self, home_and_sds: HomeAndSds):
         for populator in self.__populator_list:
-            populator.write_to(home_and_sds)
+            populator.populate_home_or_sds(home_and_sds)
 
 
 class HomeAndSdsContents(tuple):
--- test/exactly_lib_test/test_case_file_structure/test_resources/non_home_populator.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/non_home_populator.py
@@ -2,7 +2,6 @@ from exactly_lib.test_case_file_structure import relative_path_options
 from exactly_lib.test_case_file_structure.path_relativity import RelNonHomeOptionType
 from exactly_lib.test_case_file_structure.sandbox_directory_structure import SandboxDirectoryStructure
 from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import NonHomePopulator
-from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_populator import SdsPopulator
 from exactly_lib_test.test_resources.file_structure import DirContents
 
 
@@ -10,10 +9,6 @@ def rel_option(relativity: RelNonHomeOptionType,
                dir_contents: DirContents) -> NonHomePopulator:
     return _NonHomePopulatorForRelativityOption(relativity,
                                                 dir_contents)
-
-
-def from_sds_populator(sds_populator: SdsPopulator) -> NonHomePopulator:
-    return _NonHomePopulatorFromSdsPopulator(sds_populator)
 
 
 class _NonHomePopulatorForRelativityOption(NonHomePopulator):
