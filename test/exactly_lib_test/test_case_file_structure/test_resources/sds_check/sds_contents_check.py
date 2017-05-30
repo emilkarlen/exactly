608764fd3a0efded9465a6f8a06a978bc8021f4f
Emil Karlén <emil.karlen@fripost.org>
2017-05-30 22:16:59 +0200

    Test resources: Adds utility to check contents of "resolved" dir

--- test/exactly_lib_test/test_case_file_structure/test_resources/sds_check/sds_contents_check.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/sds_check/sds_contents_check.py
@@ -11,38 +11,38 @@ from exactly_lib_test.test_resources.value_assertions.value_assertion import Mes
 
 def act_dir_contains_exactly(expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
     return asrt.sub_component('act dir',
-                            SandboxDirectoryStructure.act_dir.fget,
-                            DirContainsExactly(expected_contents))
+                              SandboxDirectoryStructure.act_dir.fget,
+                              DirContainsExactly(expected_contents))
 
 
 def test_case_dir_contains_exactly(expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
     return asrt.sub_component('act dir',
-                            SandboxDirectoryStructure.test_case_dir.fget,
-                            DirContainsExactly(expected_contents))
+                              SandboxDirectoryStructure.test_case_dir.fget,
+                              DirContainsExactly(expected_contents))
 
 
 def tmp_user_dir_contains_exactly(expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
     return asrt.sub_component('tmp/usr dir',
-                            lambda sds: sds.tmp.user_dir,
-                            DirContainsExactly(expected_contents))
+                              lambda sds: sds.tmp.user_dir,
+                              DirContainsExactly(expected_contents))
 
 
 def tmp_internal_dir_contains_exactly(expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
     return asrt.sub_component('tmp/internal dir',
-                            lambda sds: sds.tmp.internal_dir,
-                            DirContainsExactly(expected_contents))
+                              lambda sds: sds.tmp.internal_dir,
+                              DirContainsExactly(expected_contents))
 
 
 def result_dir_contains_exactly(expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
     return asrt.sub_component('result dir',
-                            lambda sds: sds.result.root_dir,
-                            DirContainsExactly(expected_contents))
+                              lambda sds: sds.result.root_dir,
+                              DirContainsExactly(expected_contents))
 
 
 def cwd_contains_exactly(expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
     return asrt.sub_component('result dir',
-                            lambda sds: pathlib.Path().cwd(),
-                            DirContainsExactly(expected_contents))
+                              lambda sds: pathlib.Path().cwd(),
+                              DirContainsExactly(expected_contents))
 
 
 class SubDirOfSdsContainsExactly(asrt.ValueAssertion):
@@ -62,3 +62,27 @@ class SubDirOfSdsContainsExactly(asrt.ValueAssertion):
                                        self.sds__2__root_dir_path,
                                        DirContainsExactly(self._expected_contents))
         assertion.apply(put, sds, message_builder)
+
+
+class NonHomeDirContainsExactly(asrt.ValueAssertion):
+    def __init__(self,
+                 sds__2__root_dir_path: types.FunctionType,
+                 expected_contents: file_structure.DirContents,
+                 description: str = 'custom non-home directory'):
+        self._description = description
+        self._expected_contents = expected_contents
+        self.sds__2__root_dir_path = sds__2__root_dir_path
+
+    def apply(self,
+              put: unittest.TestCase,
+              sds: SandboxDirectoryStructure,
+              message_builder: MessageBuilder = MessageBuilder()):
+        assertion = asrt.sub_component(self._description,
+                                       self.sds__2__root_dir_path,
+                                       DirContainsExactly(self._expected_contents))
+        assertion.apply(put, sds, message_builder)
+
+
+def non_home_dir_contains_exactly(sds__2__root_dir_path: types.FunctionType,
+                                  expected_contents: file_structure.DirContents) -> asrt.ValueAssertion:
+    return NonHomeDirContainsExactly(sds__2__root_dir_path, expected_contents)
