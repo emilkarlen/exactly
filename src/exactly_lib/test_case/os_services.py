a3a514b9b4e45b4c83ecaab25e9f56782ae95389
Emil Karlén <emil.karlen@fripost.org>
2017-05-30 21:21:31 +0200

    Adds utility for instructions to report result via exceptions

--- src/exactly_lib/test_case/os_services.py
+++ src/exactly_lib/test_case/os_services.py
@@ -1,10 +1,11 @@
 import shutil
 import subprocess
 
+from exactly_lib.test_case import exception_detection
 from exactly_lib.test_case.act_phase_handling import ActPhaseOsProcessExecutor
 from exactly_lib.test_case.eh import ExitCodeOrHardError, new_eh_exit_code, new_eh_hard_error
 from exactly_lib.test_case.phases.result import sh
-from exactly_lib.util.failure_details import new_failure_details_from_exception
+from exactly_lib.util.failure_details import new_failure_details_from_exception, FailureDetails
 from exactly_lib.util.process_execution.os_process_execution import Command, ProcessExecutionSettings
 from exactly_lib.util.std import StdFiles
 
@@ -17,15 +18,27 @@ class OsServices:
     that may vary depending on operating system.
     """
 
-    def copy_file_preserve_as_much_as_possible(self,
-                                               src: str,
-                                               dst: str) -> sh.SuccessOrHardError:
+    def copy_file_preserve_as_much_as_possible__detect_ex(self, src: str, dst: str):
+        """
+        :raises DetectedException
+        """
         raise NotImplementedError()
 
-    def copy_tree_preserve_as_much_as_possible(self,
-                                               src: str,
-                                               dst: str) -> sh.SuccessOrHardError:
+    def copy_tree_preserve_as_much_as_possible__detect_ex(self, src: str, dst: str):
+        """
+        :raises DetectedException
+        """
         raise NotImplementedError()
+
+    def copy_file_preserve_as_much_as_possible(self, src: str, dst: str) -> sh.SuccessOrHardError:
+        return exception_detection.return_success_or_hard_error(
+            self.copy_file_preserve_as_much_as_possible__detect_ex,
+            src, dst)
+
+    def copy_tree_preserve_as_much_as_possible(self, src: str, dst: str) -> sh.SuccessOrHardError:
+        return exception_detection.return_success_or_hard_error(
+            self.copy_tree_preserve_as_much_as_possible__detect_ex,
+            src, dst)
 
 
 def new_default() -> OsServices:
@@ -37,19 +50,21 @@ def new_with_environ() -> OsServices:
 
 
 class _Default(OsServices):
-    def copy_file_preserve_as_much_as_possible(self, src: str, dst: str) -> sh.SuccessOrHardError:
+    def copy_file_preserve_as_much_as_possible__detect_ex(self, src: str, dst: str):
         try:
             shutil.copy2(src, dst)
-            return sh.new_sh_success()
         except OSError as ex:
-            return sh.new_sh_hard_error('Failed to copy {} -> {}: {}'.format(src, dst, str(ex)))
+            raise exception_detection.DetectedException(
+                FailureDetails('Failed to copy file {} -> {}:\n{}'.format(src, dst, str(ex)),
+                               ex))
 
-    def copy_tree_preserve_as_much_as_possible(self, src: str, dst: str) -> sh.SuccessOrHardError:
+    def copy_tree_preserve_as_much_as_possible__detect_ex(self, src: str, dst: str):
         try:
             shutil.copytree(src, dst)
-            return sh.new_sh_success()
         except OSError as ex:
-            return sh.new_sh_hard_error('Failed to copy {} -> {}: {}'.format(src, dst, str(ex)))
+            raise exception_detection.DetectedException(
+                FailureDetails('Failed to copy tree {} -> {}:\n{}'.format(src, dst, str(ex)),
+                               ex))
 
 
 class ActPhaseSubProcessExecutor(ActPhaseOsProcessExecutor):
