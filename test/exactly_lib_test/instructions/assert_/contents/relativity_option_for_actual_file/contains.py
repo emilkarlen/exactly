0be056dfc818ce18c399642d80f6d4f3bbe8e728
Emil Karlén <emil@member.fsf.org>
2017-05-30 18:06:12 +0200

    symbols: setup/install: Extracts test resource: MkSubDirAndMakeItCurrentDirectory

--- test/exactly_lib_test/instructions/assert_/contents/relativity_option_for_actual_file/contains.py
+++ test/exactly_lib_test/instructions/assert_/contents/relativity_option_for_actual_file/contains.py
@@ -8,7 +8,7 @@ from exactly_lib_test.instructions.assert_.test_resources.file_contents.instruct
     args, InstructionTestConfiguration
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.relativity_options import \
     suite_for__conf__rel_opts__negations, TestWithConfigurationAndRelativityOptionAndNegationBase, \
-    MkSubDirOfActAndMakeItCurrentDirectory
+    MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY
 from exactly_lib_test.instructions.assert_.test_resources.instruction_check import Expectation
 from exactly_lib_test.instructions.test_resources.arrangements import ArrangementPostAct
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir, File
@@ -33,7 +33,7 @@ class _ErrorWhenActualFileDoesNotExist(TestWithConfigurationAndRelativityOptionA
                  relativity_option=self.rel_opt.option_string,
                  maybe_not=self.not_opt.nothing__if_un_negated_else__not_option),
             ArrangementPostAct(
-                post_sds_population_action=MkSubDirOfActAndMakeItCurrentDirectory(),
+                post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY,
                 symbols=self.rel_opt.symbols.in_arrangement(),
             ),
             expectation_that_file_for_expected_contents_is_invalid(self.rel_opt),
@@ -49,7 +49,7 @@ class _ErrorWhenActualFileIsADirectory(TestWithConfigurationAndRelativityOptionA
             ArrangementPostAct(
                 home_or_sds_contents=self.rel_opt.populator_for_relativity_option_root(
                     DirContents([empty_dir('actual-dir')])),
-                post_sds_population_action=MkSubDirOfActAndMakeItCurrentDirectory(),
+                post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY,
                 symbols=self.rel_opt.symbols.in_arrangement(),
             ),
             expectation_that_file_for_expected_contents_is_invalid(self.rel_opt),
@@ -65,7 +65,7 @@ class _ContentsDoesNotContainALineThatMatches(TestWithConfigurationAndRelativity
             ArrangementPostAct(
                 home_or_sds_contents=self.rel_opt.populator_for_relativity_option_root(
                     DirContents([File('actual.txt', 'no match')])),
-                post_sds_population_action=MkSubDirOfActAndMakeItCurrentDirectory(),
+                post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY,
                 symbols=self.rel_opt.symbols.in_arrangement(),
             ),
             Expectation(
@@ -84,7 +84,7 @@ class _ContentsContainsALineThatMatches(TestWithConfigurationAndRelativityOption
             ArrangementPostAct(
                 home_or_sds_contents=self.rel_opt.populator_for_relativity_option_root(
                     DirContents([File('actual.txt', 'REG-matching-EX')])),
-                post_sds_population_action=MkSubDirOfActAndMakeItCurrentDirectory(),
+                post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY,
                 symbols=self.rel_opt.symbols.in_arrangement(),
             ),
             Expectation(
