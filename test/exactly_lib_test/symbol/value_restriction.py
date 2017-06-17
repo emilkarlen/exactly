f2b83ac92a55633032869e751f872b434d340853
Emil Karlén <emil.karlen@etraveli.com>
2017-06-17 12:01:55 +0200

    string-fragments: ReferenceRestrictions: Tests argument passing

--- test/exactly_lib_test/symbol/value_restriction.py
+++ test/exactly_lib_test/symbol/value_restriction.py
@@ -1,3 +1,4 @@
+import types
 import unittest
 from collections import Counter
 
@@ -45,7 +46,8 @@ class TestUsageOfDirectRestriction(unittest.TestCase):
 
         symbol_table = symbol_utils.symbol_table_from_entries(symbol_table_entries)
 
-        restriction_that_registers_processed_symbols = RestrictionThatRegistersProcessedSymbols(result=None)
+        restriction_that_registers_processed_symbols = RestrictionThatRegistersProcessedSymbols(
+            value_container_2_result__fun=unconditional_satisfaction)
         restrictions_to_test = ReferenceRestrictions(direct=restriction_that_registers_processed_symbols)
         # ACT #
         restrictions_to_test.is_satisfied_by(symbol_table, level_0_symbol.key, level_0_symbol.value)
@@ -101,7 +103,8 @@ class TestUsageOfRestrictionOnEveryReferencedSymbol(unittest.TestCase):
 
         symbol_table = symbol_utils.symbol_table_from_entries(symbol_table_entries)
 
-        restriction_that_registers_processed_symbols = RestrictionThatRegistersProcessedSymbols(result=None)
+        restriction_that_registers_processed_symbols = RestrictionThatRegistersProcessedSymbols(
+            value_container_2_result__fun=unconditional_satisfaction)
         restrictions_to_test = ReferenceRestrictions(every=restriction_that_registers_processed_symbols,
                                                      direct=unconditionally_satisfied_value_restriction())
         # ACT #
@@ -121,7 +124,7 @@ class TestUsageOfRestrictionOnEveryReferencedSymbol(unittest.TestCase):
         self.assertEqual(expected_processed_symbol,
                          actual_processed_symbols)
 
-    def test_that_every_referenced_symbol_is_processed_until_an_unsatisfied_symbol_is_reached(self):
+    def test_unconditionally_failing_value_restriction(self):
         # ARRANGE #
         restrictions_that_should_not_be_used = ReferenceRestrictions(
             direct=ValueRestrictionThatRaisesErrorIfApplied(),
@@ -138,7 +141,9 @@ class TestUsageOfRestrictionOnEveryReferencedSymbol(unittest.TestCase):
         symbol_table = symbol_utils.symbol_table_from_entries(symbol_table_entries)
 
         result_that_indicates_error = 'result that indicates error'
-        restriction_that_registers_processed_symbols = RestrictionThatRegistersProcessedSymbols(result=result_that_indicates_error)
+        function_that_reports_error = unconditional_dissatisfaction(result_that_indicates_error)
+        restriction_that_registers_processed_symbols = RestrictionThatRegistersProcessedSymbols(
+            value_container_2_result__fun=function_that_reports_error)
         restrictions_to_test = ReferenceRestrictions(every=restriction_that_registers_processed_symbols,
                                                      direct=unconditionally_satisfied_value_restriction())
         # ACT #
@@ -150,6 +155,52 @@ class TestUsageOfRestrictionOnEveryReferencedSymbol(unittest.TestCase):
         actual_processed_symbols = dict(restriction_that_registers_processed_symbols.visited.items())
         expected_processed_symbol = {
             level_0_symbol.key: 1,
+        }
+        self.assertEqual(expected_processed_symbol,
+                         actual_processed_symbols)
+
+    def test_combination_of_satisfied_and_dissatisified_symbols(self):
+        # ARRANGE #
+        satisfied_value_type = ValueType.STRING
+        disSatisfied_value_type = ValueType.PATH
+        level_2_symbol = symbol_table_entry('level_2_symbol',
+                                            references=[],
+                                            value_type=satisfied_value_type)
+        restrictions_that_should_not_be_used = ReferenceRestrictions(
+            direct=ValueRestrictionThatRaisesErrorIfApplied(),
+            every=ValueRestrictionThatRaisesErrorIfApplied())
+
+        level_1a_symbol = symbol_table_entry('level_1a_symbol',
+                                             references=[reference_to(level_2_symbol,
+                                                                      restrictions_that_should_not_be_used)],
+                                             value_type=disSatisfied_value_type)
+        level_1b_symbol = symbol_table_entry('level_1b_symbol',
+                                             references=[],
+                                             value_type=satisfied_value_type)
+        level_0_symbol = symbol_table_entry('level_0_symbol',
+                                            references=[reference_to(level_1a_symbol,
+                                                                     restrictions_that_should_not_be_used),
+                                                        reference_to(level_1b_symbol,
+                                                                     restrictions_that_should_not_be_used)],
+                                            value_type=satisfied_value_type)
+        symbol_table_entries = [level_0_symbol, level_1a_symbol, level_1b_symbol, level_2_symbol]
+
+        symbol_table = symbol_utils.symbol_table_from_entries(symbol_table_entries)
+
+        restriction_on_every = RestrictionThatRegistersProcessedSymbols(
+            value_container_2_result__fun=dissatisfaction_if_value_type_is(disSatisfied_value_type))
+        restrictions_to_test = ReferenceRestrictions(
+            every=restriction_on_every,
+            direct=RestrictionWithConstantResult(None))
+        # ACT #
+        actual_result = restrictions_to_test.is_satisfied_by(symbol_table, level_0_symbol.key, level_0_symbol.value)
+        # ASSERT #
+        self.assertIsNotNone(actual_result,
+                             'result of processing')
+        actual_processed_symbols = dict(restriction_on_every.visited.items())
+        expected_processed_symbol = {
+            level_0_symbol.key: 1,
+            level_1a_symbol.key: 1,
         }
         self.assertEqual(expected_processed_symbol,
                          actual_processed_symbols)
@@ -179,8 +230,8 @@ class ValueRestrictionThatRaisesErrorIfApplied(sut.ValueRestriction):
 
 
 class RestrictionThatRegistersProcessedSymbols(sut.ValueRestriction):
-    def __init__(self, result):
-        self.result = result
+    def __init__(self, value_container_2_result__fun: types.FunctionType):
+        self.value_container_2_result__fun = value_container_2_result__fun
         self.visited = Counter()
 
     def is_satisfied_by(self,
@@ -188,16 +239,19 @@ class RestrictionThatRegistersProcessedSymbols(sut.ValueRestriction):
                         symbol_name: str,
                         value: sut.ValueContainer) -> str:
         self.visited.update([symbol_name])
-        return self.result
+        return self.value_container_2_result__fun(value)
 
 
 class SymbolValueResolverForTest(SymbolValueResolver):
-    def __init__(self, references: list):
+    def __init__(self,
+                 references: list,
+                 value_type: ValueType):
+        self._value_type = value_type
         self._references = references
 
     @property
     def value_type(self) -> ValueType:
-        raise NotImplementedError('It is an error if this method is called')
+        return self._value_type
 
     def resolve(self, symbols: SymbolTable):
         raise NotImplementedError('It is an error if this method is called')
@@ -207,10 +261,33 @@ class SymbolValueResolverForTest(SymbolValueResolver):
         return self._references
 
 
-def symbol_table_entry(symbol_name: str, references) -> Entry:
+def symbol_table_entry(symbol_name: str,
+                       references,
+                       value_type: ValueType = ValueType.STRING) -> Entry:
     return Entry(symbol_name,
-                 symbol_utils.container(SymbolValueResolverForTest(references)))
+                 symbol_utils.container(SymbolValueResolverForTest(references,
+                                                                   value_type=value_type)))
 
 
 def reference_to(entry: Entry, restrictions: ReferenceRestrictions) -> SymbolReference:
     return SymbolReference(entry.key, restrictions)
+
+
+def unconditional_satisfaction(value: sut.ValueContainer) -> str:
+    return None
+
+
+def unconditional_dissatisfaction(result: str) -> types.FunctionType:
+    def ret_val(value: sut.ValueContainer) -> str:
+        return result
+
+    return ret_val
+
+
+def dissatisfaction_if_value_type_is(value_type: ValueType) -> types.FunctionType:
+    def ret_val(value: sut.ValueContainer) -> str:
+        if value.value.value_type is value_type:
+            return 'fail due to value type is ' + str(value_type)
+        return None
+
+    return ret_val
