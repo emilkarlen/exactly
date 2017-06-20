848a18479df0a3ae62a77248fb6090adcd09ec8f
Emil Karlén <emil.karlen@fripost.org>
2017-06-20 22:22:51 +0200

    file-ref-w-string-frag: Removes redundant tests

--- test/exactly_lib_test/execution/partial_execution/test_resources/recording/validate_symbols_utils.py
+++ test/exactly_lib_test/execution/partial_execution/test_resources/recording/validate_symbols_utils.py
@@ -14,8 +14,7 @@ from exactly_lib_test.execution.partial_execution.test_resources.test_case_gener
 from exactly_lib_test.execution.test_resources import instruction_test_resources as test
 from exactly_lib_test.execution.test_resources.instruction_test_resources import setup_phase_instruction_that
 from exactly_lib_test.symbol.test_resources import symbol_utils
-from exactly_lib_test.symbol.test_resources.concrete_restriction_assertion import ValueRestrictionWithConstantResult, \
-    value_restriction_that_is_unconditionally_satisfied
+from exactly_lib_test.symbol.test_resources.concrete_restriction_assertion import ValueRestrictionWithConstantResult
 from exactly_lib_test.test_resources.actions import do_return
 from exactly_lib_test.test_resources.expected_instruction_failure import ExpectedFailureForInstructionFailure
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
@@ -41,8 +40,7 @@ class Configuration:
 def suite_for(configuration: Configuration) -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTests([TestValidationErrorDueToReferenceToUndefinedSymbol(configuration),
-                      TestValidationErrorDueToFailedRestrictionOnDirectReferenceTarget(configuration),
-                      TestValidationErrorDueToFailedRestrictionOnIndirectReferenceTarget(configuration),
+                      TestValidationErrorDueToFailedReferenceRestrictions(configuration),
                       TestImplementationError(configuration),
                       ])
     return ret_val
@@ -76,7 +74,7 @@ class TestValidationErrorDueToReferenceToUndefinedSymbol(TestCaseBase):
         )
 
 
-class TestValidationErrorDueToFailedRestrictionOnDirectReferenceTarget(TestCaseBase):
+class TestValidationErrorDueToFailedReferenceRestrictions(TestCaseBase):
     def runTest(self):
         conf = self.configuration
         defined_symbol = symbol_utils.string_symbol_definition('symbol_name')
@@ -84,45 +82,11 @@ class TestValidationErrorDueToFailedRestrictionOnDirectReferenceTarget(TestCaseB
         reference_with_restriction_failure = SymbolReference(
             defined_symbol.name,
             ReferenceRestrictionsOnDirectAndIndirect(
-                direct=ValueRestrictionWithConstantResult(error_message_for_failed_restriction),
-                indirect=value_restriction_that_is_unconditionally_satisfied()))
+                direct=ValueRestrictionWithConstantResult(error_message_for_failed_restriction)))
 
         test_case = TestCaseGeneratorWithExtraInstrsBetweenRecordingInstr() \
             .add(PartialPhase.SETUP,
                  setup_phase_instruction_that(symbol_usages=do_return([defined_symbol]))) \
-            .add(conf.phase,
-                 conf.instruction_that_returns([reference_with_restriction_failure]))
-
-        execute_test_case_with_recording(
-            self,
-            Arrangement(test_case),
-            Expectation(PartialResultStatus.VALIDATE,
-                        ExpectedFailureForInstructionFailure.new_with_phase_and_message_assertion(
-                            conf.step,
-                            asrt.equals(error_message_for_failed_restriction)),
-                        conf.expected_steps_before_failing_instruction,
-                        sandbox_directory_structure_should_exist=False)
-        )
-
-
-class TestValidationErrorDueToFailedRestrictionOnIndirectReferenceTarget(TestCaseBase):
-    def runTest(self):
-        conf = self.configuration
-        def_of_indirectly_referenced_symbol = symbol_utils.string_symbol_definition('indirectly_ref_symbol')
-        def_of_directly_referenced_symbol = definition_with_reference('directly_ref_symbol',
-                                                                      def_of_indirectly_referenced_symbol.name)
-        error_message_for_failed_restriction = 'error message'
-        reference_with_restriction_failure = SymbolReference(
-            def_of_directly_referenced_symbol.name,
-            ReferenceRestrictionsOnDirectAndIndirect(direct=value_restriction_that_is_unconditionally_satisfied(),
-                                                     indirect=ValueRestrictionWithConstantResult(
-                                                         error_message_for_failed_restriction)))
-
-        test_case = TestCaseGeneratorWithExtraInstrsBetweenRecordingInstr() \
-            .add(PartialPhase.SETUP,
-                 setup_phase_instruction_that(symbol_usages=do_return([def_of_indirectly_referenced_symbol]))) \
-            .add(PartialPhase.SETUP,
-                 setup_phase_instruction_that(symbol_usages=do_return([def_of_directly_referenced_symbol]))) \
             .add(conf.phase,
                  conf.instruction_that_returns([reference_with_restriction_failure]))
 
