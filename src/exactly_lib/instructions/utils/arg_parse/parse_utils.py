fa0dd6e11b74f041f0e37891db0eaeda04ee34cc
Emil Karlén <emil.karlen@etraveli.com>
2017-07-08 23:55:49 +0200

    Removes unused deprecated code: TokenStream

--- src/exactly_lib/instructions/utils/arg_parse/parse_utils.py
+++ src/exactly_lib/instructions/utils/arg_parse/parse_utils.py
@@ -1,4 +1,3 @@
-import io
 import shlex
 
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
@@ -30,44 +29,3 @@ def ensure_is_not_option_argument(argument: str):
     """
     if is_option_argument(argument):
         raise SingleInstructionInvalidArgumentException('An option argument was not expected here: {}'.format(argument))
-
-
-class TokenStream:
-    def __init__(self, source: str):
-        self._source = source
-        if source is None:
-            self._head = None
-            self._tail_source_or_empty_string = ''
-        else:
-            source_io = io.StringIO(source)
-            lexer = shlex.shlex(source_io, posix=True)
-            lexer.whitespace_split = True
-            self._head = lexer.get_token()
-            self._tail_source_or_empty_string = source[source_io.tell():].lstrip()
-
-    @property
-    def source(self) -> str:
-        return self._source
-
-    @property
-    def is_null(self) -> bool:
-        return self._head is None
-
-    @property
-    def head(self) -> str:
-        return self._head
-
-    @property
-    def tail(self):  # -> TokenStream
-        return TokenStream(self.tail_source)
-
-    @property
-    def tail_source_or_empty_string(self) -> str:
-        return self._tail_source_or_empty_string
-
-    @property
-    def tail_source(self) -> str:
-        if self._tail_source_or_empty_string:
-            return self._tail_source_or_empty_string
-        else:
-            return None
--- test/exactly_lib_test/instructions/utils/arg_parse/parse_utils.py
+++ test/exactly_lib_test/instructions/utils/arg_parse/parse_utils.py
@@ -8,7 +8,6 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(unittest.makeSuite(TestCases))
-    ret_val.addTest(unittest.makeSuite(TestTokenStream))
     return ret_val
 
 
@@ -26,104 +25,6 @@ class TestCases(unittest.TestCase):
         actual = sut.split_arguments_list_string('"in double quotes" ' + "'in single quotes'")
         self.assertEqual(['in double quotes', 'in single quotes'],
                          actual)
-
-
-class TestTokenStream(unittest.TestCase):
-    def test_should_be_null_when_source_is_none(self):
-        actual = sut.TokenStream(None)
-        self.assertTrue(actual.is_null)
-        self.assertIsNone(actual.source)
-
-    def test_should_be_null_when_source_is_empty_string(self):
-        actual = sut.TokenStream('')
-        self.assertTrue(actual.is_null)
-        self.assertEqual('',
-                         actual.source)
-
-    def test_should_be_null_when_source_is_only_white_space(self):
-        actual = sut.TokenStream('   ')
-        self.assertTrue(actual.is_null)
-        self.assertEqual('   ',
-                         actual.source)
-
-    def test_single_token(self):
-        actual = sut.TokenStream('a ')
-        self.assertFalse(actual.is_null)
-        self.assertEqual('a',
-                         actual.head)
-        self.assertEqual('a ',
-                         actual.source)
-        self.assertIsNone(actual.tail_source)
-        self.assertEqual('',
-                         actual.tail_source_or_empty_string)
-
-    def test_single_token_with_quoted_windows_file_name(self):
-        actual = sut.TokenStream('c:\\\\dir ')
-        self.assertFalse(actual.is_null)
-        self.assertEqual('c:\\dir',
-                         actual.head)
-        self.assertEqual('c:\\\\dir ',
-                         actual.source)
-        self.assertIsNone(actual.tail_source)
-        self.assertEqual('',
-                         actual.tail_source_or_empty_string)
-
-    def test_single_token_with_multiple_trailing_ws(self):
-        actual = sut.TokenStream('a   ')
-        self.assertFalse(actual.is_null)
-        self.assertEqual('a',
-                         actual.head)
-        self.assertEqual('a   ',
-                         actual.source)
-        self.assertIsNone(actual.tail_source)
-        self.assertEqual('',
-                         actual.tail_source_or_empty_string)
-
-    def test_single_token_quoted(self):
-        actual = sut.TokenStream('"a b" ')
-        self.assertFalse(actual.is_null)
-        self.assertEqual('a b',
-                         actual.head)
-        self.assertEqual('"a b" ',
-                         actual.source)
-        self.assertIsNone(actual.tail_source)
-        self.assertEqual('',
-                         actual.tail_source_or_empty_string)
-
-    def test_single_token_quoted_empty_string(self):
-        actual = sut.TokenStream('"" ')
-        self.assertFalse(actual.is_null)
-        self.assertEqual('',
-                         actual.head)
-        self.assertEqual('"" ',
-                         actual.source)
-        self.assertIsNone(actual.tail_source)
-        self.assertEqual('',
-                         actual.tail_source_or_empty_string)
-
-    def test_multiple_tokens(self):
-        actual = sut.TokenStream('a b  ')
-        self.assertFalse(actual.is_null)
-        self.assertEqual('a',
-                         actual.head)
-        self.assertEqual('a b  ',
-                         actual.source)
-        self.assertEqual('b  ',
-                         actual.tail_source)
-        self.assertEqual('b  ',
-                         actual.tail_source_or_empty_string)
-
-    def test_multiple_tokens_tail(self):
-        tokens0 = sut.TokenStream('a b  ')
-        actual = tokens0.tail
-        self.assertFalse(actual.is_null)
-        self.assertEqual('b',
-                         actual.head)
-        self.assertEqual('b  ',
-                         actual.source)
-        self.assertIsNone(actual.tail_source)
-        self.assertEqual('',
-                         actual.tail_source_or_empty_string)
 
 
 if __name__ == '__main__':
