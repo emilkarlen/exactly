a98e83c347fce74454f000045e2214e1fe7b73c6
Emil Karlén <emil.karlen@etraveli.com>
2017-07-09 17:17:11 +0200

    file-ref: PathPart: Removes deprecated PathPartResolverAsStringSymbolReference

---
+++ test/exactly_lib_test/symbol/test_resources/value_resolvers.py
@@ -0,0 +1,16 @@
+from exactly_lib.symbol import string_resolver
+from exactly_lib.symbol import symbol_usage
+from exactly_lib.symbol.concrete_restrictions import no_restrictions
+from exactly_lib.symbol.string_resolver import StringResolver
+from exactly_lib.symbol.value_restriction import ReferenceRestrictions
+
+
+def string_resolver_of_single_symbol_reference(
+        symbol_name: str,
+        restrictions: ReferenceRestrictions = no_restrictions()) -> StringResolver:
+    symbol_reference = symbol_usage.SymbolReference(symbol_name,
+                                                    restrictions)
+    fragments = [
+        string_resolver.SymbolStringFragmentResolver(symbol_reference)
+    ]
+    return StringResolver(tuple(fragments))
