1af660332035368c02cb9d206a7205d40ce3dea7
Emil Karlén <emil.karlen@etraveli.com>
2017-07-11 19:41:35 +0200

    StringValue: Test of equals_string_fragment

--- test/exactly_lib_test/type_system_values/test_resources_test/__init__.py
+++ test/exactly_lib_test/type_system_values/test_resources_test/__init__.py
@@ -1,11 +1,11 @@
 import unittest
 
-from exactly_lib_test.type_system_values.test_resources_test import concrete_path_part
-from exactly_lib_test.type_system_values.test_resources_test import file_ref
+from exactly_lib_test.type_system_values.test_resources_test import string_value, concrete_path_part, file_ref
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
+    ret_val.addTest(string_value.suite())
     ret_val.addTest(concrete_path_part.suite())
     ret_val.addTest(file_ref.suite())
     return ret_val
