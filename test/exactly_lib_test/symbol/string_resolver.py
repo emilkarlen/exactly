fb12e3d03fcee0f87534eea58b84e108e293f416
Emil Karlén <emil.karlen@fripost.org>
2017-07-11 20:03:52 +0200

    StringValueResolver: Test of ConstantStringFragmentResolver

--- test/exactly_lib_test/symbol/string_resolver.py
+++ test/exactly_lib_test/symbol/string_resolver.py
@@ -6,10 +6,42 @@ from exactly_lib.type_system_values.value_type import ValueType
 from exactly_lib.util.symbol_table import empty_symbol_table
 from exactly_lib_test.symbol.test_resources.concrete_value_assertions import equals_string_fragments
 from exactly_lib_test.type_system_values.test_resources import string_value as asrt_sv
+from exactly_lib_test.type_system_values.test_resources.string_value import equals_string_fragment
 
 
 def suite() -> unittest.TestSuite:
-    return unittest.makeSuite(StringResolverTest)
+    return unittest.TestSuite([
+        unittest.makeSuite(TestConstantStringFragmentResolver),
+        unittest.makeSuite(StringResolverTest),
+    ])
+
+
+class TestConstantStringFragmentResolver(unittest.TestCase):
+    string_constant = 'string constant'
+    fragment = sut.ConstantStringFragmentResolver(string_constant)
+
+    def test_should_be_string_constant(self):
+        self.assertTrue(self.fragment.is_string_constant)
+
+    def test_should_have_no_references(self):
+        self.assertEqual((),
+                         self.fragment.references)
+
+    def test_string_constant(self):
+        string_constant = 'string constant'
+        fragment = sut.ConstantStringFragmentResolver(string_constant)
+        self.assertEqual(string_constant,
+                         fragment.string_constant)
+
+    def test_resolve_should_give_string_constant(self):
+        # ARRANGE #
+        string_constant = 'string constant'
+        fragment = sut.ConstantStringFragmentResolver(string_constant)
+        # ACT #
+        actual = fragment.resolve(empty_symbol_table())
+        # ASSERT #
+        assertion = equals_string_fragment(csv.ConstantFragment(string_constant))
+        assertion.apply_without_message(self, actual)
 
 
 class StringResolverTest(unittest.TestCase):
