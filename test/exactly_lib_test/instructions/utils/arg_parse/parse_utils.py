fccf097f1f075fcc713e59aa470bb57613b4fe1a
Emil Karlén <emil.karlen@etraveli.com>
2017-07-12 02:46:37 +0200

    instruction parse: Adds method for constr TokenStream and handle syntax error

--- test/exactly_lib_test/instructions/utils/arg_parse/parse_utils.py
+++ test/exactly_lib_test/instructions/utils/arg_parse/parse_utils.py
@@ -8,7 +8,36 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(unittest.makeSuite(TestCases))
+    ret_val.addTest(unittest.makeSuite(TestNewTokenStream))
     return ret_val
+
+
+class TestNewTokenStream(unittest.TestCase):
+    def test_pass_when_valid_syntax(self):
+        source = 'token1 token2'
+        actual = sut.new_token_stream(source)
+        self.assertEqual(source,
+                         actual.source,
+                         'source string')
+        self.assertEqual('token1',
+                         actual.head.source_string,
+                         'source string')
+
+    def test_raise_invalid_argument_exception_WHEN_first_token_is_invalid(self):
+        cases = [
+            (
+                'start of first token is invalid',
+                '"token1 token2'
+            ),
+            (
+                'end of first token is invalid',
+                'token1" token2'
+            ),
+        ]
+        for name, source in cases:
+            with self.subTest(name=name):
+                with self.assertRaises(SingleInstructionInvalidArgumentException):
+                    sut.new_token_stream(source)
 
 
 class TestCases(unittest.TestCase):
