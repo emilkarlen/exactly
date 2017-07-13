2b0d01e087a47caea7dc49272d23b9e9bf33c082
Emil Karlén <emil.karlen@etraveli.com>
2017-07-13 17:44:50 +0200

    symbols: Removes duplicate tests

--- test/exactly_lib_test/symbol/concrete_resolvers.py
+++ test/exactly_lib_test/symbol/concrete_resolvers.py
@@ -2,44 +2,15 @@ import unittest
 
 from exactly_lib.symbol import concrete_resolvers as sut
 from exactly_lib.symbol import string_resolver as sr
-from exactly_lib.symbol.concrete_restrictions import NoRestriction, ReferenceRestrictionsOnDirectAndIndirect
 from exactly_lib.symbol.string_resolver import string_constant
-from exactly_lib.symbol.symbol_usage import SymbolReference
 from exactly_lib.symbol.value_resolvers.file_ref_resolvers import FileRefConstant
 from exactly_lib_test.test_case_file_structure.test_resources.simple_file_ref import file_ref_test_impl
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        unittest.makeSuite(TestStringFragments),
         unittest.makeSuite(TestValueVisitor),
     ])
-
-
-class TestStringFragments(unittest.TestCase):
-    def test_string_constant_fragment(self):
-        # ARRANGE #
-        value = 'string value'
-        actual = sr.ConstantStringFragmentResolver(value)
-        # ASSERT #
-        self.assertIsInstance(actual, sr.StringFragmentResolver)
-        self.assertTrue(actual.is_string_constant, 'is_string_constant')
-        self.assertFalse(actual.is_symbol, 'is_symbol')
-        self.assertEqual(value, actual.string_constant,
-                         'string_constant')
-
-    def test_symbol_fragment(self):
-        # ARRANGE #
-        symbol_name = 'symbol_name'
-        actual = sr.SymbolStringFragmentResolver(SymbolReference(symbol_name,
-                                                                 ReferenceRestrictionsOnDirectAndIndirect(
-                                                                     NoRestriction())))
-        # ASSERT #
-        self.assertIsInstance(actual, sr.StringFragmentResolver)
-        self.assertFalse(actual.is_string_constant, 'is_string_constant')
-        self.assertTrue(actual.is_symbol, 'is_symbol')
-        self.assertEqual(symbol_name, actual.symbol_name,
-                         symbol_name)
 
 
 class TestValueVisitor(unittest.TestCase):
