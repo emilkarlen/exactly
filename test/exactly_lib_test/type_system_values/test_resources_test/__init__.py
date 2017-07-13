996f71041b1b402bc70b0e89c40cbab5ae5ecd09
Emil Karlén <emil.karlen@fripost.org>
2017-07-13 20:56:27 +0200

    list values: ListValue: Test resources for equality test of ListValue

--- test/exactly_lib_test/type_system_values/test_resources_test/__init__.py
+++ test/exactly_lib_test/type_system_values/test_resources_test/__init__.py
@@ -1,12 +1,14 @@
 import unittest
 
-from exactly_lib_test.type_system_values.test_resources_test import string_value, concrete_path_part, file_ref
+from exactly_lib_test.type_system_values.test_resources_test import string_value, list_value, \
+    concrete_path_part, file_ref
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(string_value.suite())
     ret_val.addTest(concrete_path_part.suite())
+    ret_val.addTest(list_value.suite())
     ret_val.addTest(file_ref.suite())
     return ret_val
 
