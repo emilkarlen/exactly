11af8fbef81ba581ff67b0173bd904f2eec07800
Emil Karlén <emil.karlen@fripost.org>
2017-07-14 20:47:17 +0200

    symbols: Renames test resources

--- test/exactly_lib_test/symbol/test_resources_test/__init__.py
+++ test/exactly_lib_test/symbol/test_resources_test/__init__.py
@@ -2,7 +2,7 @@ import unittest
 
 from exactly_lib_test.symbol.test_resources_test import concrete_value_assertions__file_ref, \
     concrete_value_assertions__string, concrete_value_assertions, \
-    value_structure_assertions, symbol_reference_assertions, \
+    resolver_structure_assertions, symbol_reference_assertions, \
     path_relativity, file_ref_relativity
 
 
@@ -13,7 +13,7 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(concrete_value_assertions__string.suite())
     ret_val.addTest(concrete_value_assertions__file_ref.suite())
     ret_val.addTest(concrete_value_assertions.suite())
-    ret_val.addTest(value_structure_assertions.suite())
+    ret_val.addTest(resolver_structure_assertions.suite())
     ret_val.addTest(symbol_reference_assertions.suite())
     return ret_val
 
