dc68324f9b81c10a74974217d16e7150798a7cdd
Emil Karlén <emil.karlen@fripost.org>
2017-07-15 01:37:00 +0200

    test resources: Shortens tests using utility assert_that_assertion_fails
    resolver_structure_assertions

--- test/exactly_lib_test/symbol/test_resources_test/resolver_structure_assertions.py
+++ test/exactly_lib_test/symbol/test_resources_test/resolver_structure_assertions.py
@@ -7,8 +7,7 @@ from exactly_lib.symbol.value_resolvers.file_ref_resolvers import FileRefConstan
 from exactly_lib.util.line_source import Line
 from exactly_lib_test.symbol.test_resources import value_structure_assertions as sut
 from exactly_lib_test.test_case_file_structure.test_resources.simple_file_ref import file_ref_test_impl
-from exactly_lib_test.test_resources.test_of_test_resources_util import \
-    test_case_with_failure_exception_set_to_test_exception, TestException
+from exactly_lib_test.test_resources.value_assertions.assert_that_assertion_fails import assert_that_assertion_fails
 
 
 def suite() -> unittest.TestSuite:
@@ -38,17 +37,16 @@ class TestEqualsValueContainer(unittest.TestCase):
         common_value = string_constant('common string value')
         expected = ResolverContainer(Line(4, 'source code 4'), common_value)
         actual = ResolverContainer(Line(5, 'source code 5'), common_value)
-        put = test_case_with_failure_exception_set_to_test_exception()
-        sut.equals_container(expected, ignore_source_line=True).apply_without_message(put, actual)
+        assertion = sut.equals_container(expected, ignore_source_line=True)
+        assert_that_assertion_fails(assertion, actual)
 
     def test_fail__different_source_line_and_source_line_check_is_not_ignored(self):
         # ARRANGE #
         common_value = FileRefConstant(file_ref_test_impl('common file-name'))
         expected = ResolverContainer(Line(1, 'source code 1'), common_value)
         actual = ResolverContainer(Line(2, 'source code 2'), common_value)
-        put = test_case_with_failure_exception_set_to_test_exception()
-        with put.assertRaises(TestException):
-            sut.equals_container(expected, ignore_source_line=False).apply_without_message(put, actual)
+        assertion = sut.equals_container(expected, ignore_source_line=False)
+        assert_that_assertion_fails(assertion, actual)
 
 
 class TestEqualsValueDefinition(unittest.TestCase):
@@ -85,11 +83,8 @@ class TestEqualsValueDefinition(unittest.TestCase):
                                              string_constant('common string value'))
         expected_symbol = SymbolDefinition('expected value name', common_container)
         actual_symbol = SymbolDefinition('actual value name', common_container)
-        put = test_case_with_failure_exception_set_to_test_exception()
-        with put.assertRaises(TestException):
-            # ACT #
-            assertion = sut.equals_symbol(expected_symbol)
-            assertion.apply_without_message(put, actual_symbol)
+        assertion = sut.equals_symbol(expected_symbol)
+        assert_that_assertion_fails(assertion, actual_symbol)
 
     def test_fail__failing_assertion_on_container(self):
         # ARRANGE #
@@ -101,8 +96,5 @@ class TestEqualsValueDefinition(unittest.TestCase):
         actual_symbol = SymbolDefinition(common_name,
                                          ResolverContainer(common_name_source,
                                                            string_constant('actual string value')))
-        put = test_case_with_failure_exception_set_to_test_exception()
-        with put.assertRaises(TestException):
-            # ACT #
-            assertion = sut.equals_symbol(expected_symbol)
-            assertion.apply_without_message(put, actual_symbol)
+        assertion = sut.equals_symbol(expected_symbol)
+        assert_that_assertion_fails(assertion, actual_symbol)
