564070338e83d612af283d7a6478b5f108960434
Emil Karlén <emil.karlen@fripost.org>
2017-07-14 20:24:40 +0200

    symbols: restrictions: Split tests and test resources into separate modules

--- test/exactly_lib_test/symbol/restrictions/reference_restrictions.py
+++ test/exactly_lib_test/symbol/restrictions/reference_restrictions.py
@@ -2,186 +2,29 @@ import types
 import unittest
 from collections import Counter
 
-from exactly_lib.symbol.path_resolver import FileRefResolver
 from exactly_lib.symbol.resolver_structure import SymbolValueResolver, ResolverContainer
 from exactly_lib.symbol.restriction import ReferenceRestrictions, ValueRestrictionFailure
 from exactly_lib.symbol.restriction import ValueRestriction
 from exactly_lib.symbol.restrictions import reference_restrictions as sut
 from exactly_lib.symbol.restrictions import value_restrictions as vr
-from exactly_lib.symbol.string_resolver import string_constant
 from exactly_lib.symbol.symbol_usage import SymbolReference
-from exactly_lib.symbol.value_resolvers.file_ref_resolvers import FileRefConstant
-from exactly_lib.test_case_file_structure.path_relativity import RelOptionType, PathRelativityVariants
 from exactly_lib.type_system_values.value_type import ValueType
 from exactly_lib.util.symbol_table import SymbolTable, Entry
-from exactly_lib.util.symbol_table import empty_symbol_table
 from exactly_lib_test.symbol.test_resources import symbol_utils
 from exactly_lib_test.symbol.test_resources.concrete_restriction_assertion import \
     value_restriction_that_is_unconditionally_satisfied, value_restriction_that_is_unconditionally_unsatisfied, \
     is_failure_of_direct_reference, is_failure_of_indirect_reference
-from exactly_lib_test.test_case_file_structure.test_resources.simple_file_ref import file_ref_test_impl
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.util.test_resources import symbol_tables
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        unittest.makeSuite(TestNoRestriction),
-        unittest.makeSuite(TestStringRestriction),
-        unittest.makeSuite(TestFileRefRelativityRestriction),
-        unittest.makeSuite(TestValueRestrictionVisitor),
         unittest.makeSuite(TestReferenceRestrictionVisitor),
         unittest.makeSuite(TestUsageOfDirectRestriction),
         unittest.makeSuite(TestUsageOfRestrictionOnIndirectReferencedSymbol),
         unittest.makeSuite(TestOrReferenceRestrictions),
     ])
-
-
-class TestNoRestriction(unittest.TestCase):
-    def test_pass(self):
-        # ARRANGE #
-        test_cases = [
-            string_constant('string'),
-            file_ref_constant_resolver(),
-        ]
-        restriction = vr.NoRestriction()
-        symbols = empty_symbol_table()
-        for value in test_cases:
-            with self.subTest(msg='value=' + str(value)):
-                container = symbol_utils.container(value)
-                # ACT #
-                actual = restriction.is_satisfied_by(symbols, 'symbol_name', container)
-                # ASSERT #
-                self.assertIsNone(actual)
-
-
-class TestStringRestriction(unittest.TestCase):
-    def test_pass(self):
-        # ARRANGE #
-        test_cases = [
-            string_constant('string'),
-            string_constant(''),
-        ]
-        restriction = vr.StringRestriction()
-        symbols = empty_symbol_table()
-        for value in test_cases:
-            with self.subTest(msg='value=' + str(value)):
-                container = symbol_utils.container(value)
-                # ACT #
-                actual = restriction.is_satisfied_by(symbols, 'symbol_name', container)
-                # ASSERT #
-                self.assertIsNone(actual)
-
-    def test_fail__not_a_string(self):
-        # ARRANGE #
-        test_cases = [
-            file_ref_constant_resolver(),
-        ]
-        restriction = vr.StringRestriction()
-        symbols = empty_symbol_table()
-        for value in test_cases:
-            with self.subTest(msg='value=' + str(value)):
-                container = symbol_utils.container(value)
-                # ACT #
-                actual = restriction.is_satisfied_by(symbols, 'symbol_name', container)
-                # ASSERT #
-                self.assertIsNotNone(actual,
-                                     'Result should denote failing validation')
-
-
-class TestFileRefRelativityRestriction(unittest.TestCase):
-    def test_pass(self):
-        # ARRANGE #
-        test_cases = [
-            FileRefConstant(file_ref_test_impl(relativity=RelOptionType.REL_ACT)),
-            FileRefConstant(file_ref_test_impl(relativity=RelOptionType.REL_HOME)),
-        ]
-        restriction = vr.FileRefRelativityRestriction(
-            PathRelativityVariants(
-                {RelOptionType.REL_ACT, RelOptionType.REL_HOME, RelOptionType.REL_RESULT},
-                False))
-        symbols = empty_symbol_table()
-        for value in test_cases:
-            with self.subTest(msg='value=' + str(value)):
-                container = symbol_utils.container(value)
-                # ACT #
-                actual = restriction.is_satisfied_by(symbols, 'symbol_name', container)
-                # ASSERT #
-                self.assertIsNone(actual)
-
-    def test_fail__relative_paths(self):
-        # ARRANGE #
-        test_cases = [
-            FileRefConstant(file_ref_test_impl(relativity=RelOptionType.REL_ACT)),
-            FileRefConstant(file_ref_test_impl(relativity=RelOptionType.REL_HOME)),
-        ]
-        restriction = vr.FileRefRelativityRestriction(
-            PathRelativityVariants(
-                {RelOptionType.REL_RESULT},
-                False))
-        symbols = empty_symbol_table()
-        for value in test_cases:
-            with self.subTest(msg='value=' + str(value)):
-                container = symbol_utils.container(value)
-                # ACT #
-                actual = restriction.is_satisfied_by(symbols, 'symbol_name', container)
-                # ASSERT #
-                self.assertIsNotNone(actual,
-                                     'Result should denote failing validation')
-
-
-class TestValueRestrictionVisitor(unittest.TestCase):
-    def test_none(self):
-        # ARRANGE #
-        expected_return_value = 72
-        visitor = _VisitorThatRegisterClassOfVisitMethod(expected_return_value)
-        # ACT #
-        actual_return_value = visitor.visit(vr.NoRestriction())
-        # ASSERT #
-        self.assertEqual([vr.NoRestriction],
-                         visitor.visited_classes,
-                         'visited classes')
-        self.assertEqual(expected_return_value,
-                         actual_return_value,
-                         'return value')
-
-    def test_string(self):
-        # ARRANGE #
-        expected_return_value = 87
-        visitor = _VisitorThatRegisterClassOfVisitMethod(expected_return_value)
-        # ACT #
-        actual_return_value = visitor.visit(vr.StringRestriction())
-        # ASSERT #
-        self.assertEqual([vr.StringRestriction],
-                         visitor.visited_classes,
-                         'visited classes')
-        self.assertEqual(expected_return_value,
-                         actual_return_value,
-                         'return value')
-
-    def test_file_ref(self):
-        # ARRANGE #
-        expected_return_value = 69
-        visitor = _VisitorThatRegisterClassOfVisitMethod(expected_return_value)
-        # ACT #
-        actual_return_value = visitor.visit(
-            vr.FileRefRelativityRestriction(
-                PathRelativityVariants(set(), False)))
-        # ASSERT #
-        self.assertEqual([vr.FileRefRelativityRestriction],
-                         visitor.visited_classes,
-                         'visited classes')
-        self.assertEqual(expected_return_value,
-                         actual_return_value,
-                         'return value')
-
-    def test_visit_invalid_object_should_raise_exception(self):
-        # ARRANGE #
-        visitor = _VisitorThatRegisterClassOfVisitMethod("not used")
-        non_concept = 'a string is not a sub class of ' + str(ValueRestriction)
-        # ACT & ASSERT #
-        with self.assertRaises(TypeError):
-            visitor.visit(non_concept)
 
 
 class TestReferenceRestrictionVisitor(unittest.TestCase):
@@ -221,26 +64,6 @@ class TestReferenceRestrictionVisitor(unittest.TestCase):
         # ACT & ASSERT #
         with self.assertRaises(TypeError):
             visitor.visit(invalid_value)
-
-
-class _VisitorThatRegisterClassOfVisitMethod(
-    vr.ValueRestrictionVisitor):
-    def __init__(self, return_value):
-        self.visited_classes = []
-        self.return_value = return_value
-
-    def visit_none(self, x: vr.NoRestriction):
-        self.visited_classes.append(vr.NoRestriction)
-        return self.return_value
-
-    def visit_string(self, x: vr.StringRestriction):
-        self.visited_classes.append(vr.StringRestriction)
-        return self.return_value
-
-    def visit_file_ref_relativity(self,
-                                  x: vr.FileRefRelativityRestriction):
-        self.visited_classes.append(vr.FileRefRelativityRestriction)
-        return self.return_value
 
 
 class _ReferenceRestrictionsVisitorThatRegisterClassOfVisitMethod(sut.ReferenceRestrictionsVisitor):
@@ -742,8 +565,3 @@ def dissatisfaction_if_value_type_is(value_type: ValueType) -> types.FunctionTyp
         return None
 
     return ret_val
-
-
-def file_ref_constant_resolver() -> FileRefResolver:
-    return FileRefConstant(file_ref_test_impl('file-name-rel-home',
-                                              relativity=RelOptionType.REL_HOME))
