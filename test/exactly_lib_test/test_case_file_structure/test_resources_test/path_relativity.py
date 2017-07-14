045136408869f178022737fa9485aa0e6bb999b6
Emil Karlén <emil.karlen@fripost.org>
2017-07-15 01:45:14 +0200

    test resources: Shortens tests using utility assert_that_assertion_fails
    path_relativity

--- test/exactly_lib_test/test_case_file_structure/test_resources_test/path_relativity.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources_test/path_relativity.py
@@ -3,8 +3,7 @@ import unittest
 from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants, RelOptionType, \
     SPECIFIC_ABSOLUTE_RELATIVITY, specific_relative_relativity
 from exactly_lib_test.test_case_file_structure.test_resources import path_relativity as sut
-from exactly_lib_test.test_resources.test_of_test_resources_util import \
-    test_case_with_failure_exception_set_to_test_exception, TestException
+from exactly_lib_test.test_resources.value_assertions.assert_that_assertion_fails import assert_that_assertion_fails
 
 
 def suite() -> unittest.TestSuite:
@@ -38,25 +37,19 @@ class TestEqualsPathRelativity(unittest.TestCase):
 class TestNotEqualsPathRelativity(unittest.TestCase):
     def test_differs__because_on_is_absolute(self):
         # ARRANGE #
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = SPECIFIC_ABSOLUTE_RELATIVITY
         actual = specific_relative_relativity(RelOptionType.REL_ACT)
         assertion = sut.equals_path_relativity(expected)
-        # ASSERT #
-        with put.assertRaises(TestException):
-            # ACT #
-            assertion.apply_with_message(put, actual, 'NotEquals')
+        # ACT & ASSERT #
+        assert_that_assertion_fails(assertion, actual)
 
     def test_differs__both_are_relative_but_with_different_relativity(self):
         # ARRANGE #
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = specific_relative_relativity(RelOptionType.REL_ACT)
         actual = specific_relative_relativity(RelOptionType.REL_HOME)
         assertion = sut.equals_path_relativity(expected)
-        # ASSERT #
-        with put.assertRaises(TestException):
-            # ACT #
-            assertion.apply_with_message(put, actual, 'NotEquals')
+        # ACT & ASSERT #
+        assert_that_assertion_fails(assertion, actual)
 
 
 class TestEqualsPathRelativityVariants(unittest.TestCase):
@@ -65,7 +58,7 @@ class TestEqualsPathRelativityVariants(unittest.TestCase):
             PathRelativityVariants({rel_option_type}, accept_absolute)
             for accept_absolute in [False, True]
             for rel_option_type in RelOptionType
-            ]
+        ]
 
         for value in test_cases:
             with self.subTest(msg='RelOptionTypes={}, absolute={}'.format(value.rel_option_types, value.absolute)):
@@ -74,22 +67,22 @@ class TestEqualsPathRelativityVariants(unittest.TestCase):
 
 class TestNotEqualsPathRelativityVariants(unittest.TestCase):
     def test_differs__absolute(self):
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = PathRelativityVariants({RelOptionType.REL_ACT}, False)
         actual = PathRelativityVariants({RelOptionType.REL_ACT}, True)
-        with put.assertRaises(TestException):
-            sut.path_relativity_variants_equals(expected).apply_with_message(put, actual, 'NotEquals')
+        assertion = sut.path_relativity_variants_equals(expected)
+        # ACT & ASSERT #
+        assert_that_assertion_fails(assertion, actual)
 
     def test_differs__rel_option_types__expected_has_super_set(self):
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = PathRelativityVariants({RelOptionType.REL_ACT, RelOptionType.REL_HOME}, False)
         actual = PathRelativityVariants({RelOptionType.REL_ACT}, False)
-        with put.assertRaises(TestException):
-            sut.path_relativity_variants_equals(expected).apply_with_message(put, actual, 'NotEquals')
+        assertion = sut.path_relativity_variants_equals(expected)
+        # ACT & ASSERT #
+        assert_that_assertion_fails(assertion, actual)
 
     def test_differs__rel_option_types__single_different_value(self):
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = PathRelativityVariants({RelOptionType.REL_ACT}, False)
         actual = PathRelativityVariants({RelOptionType.REL_HOME}, False)
-        with put.assertRaises(TestException):
-            sut.path_relativity_variants_equals(expected).apply_with_message(put, actual, 'NotEquals')
+        assertion = sut.path_relativity_variants_equals(expected)
+        # ACT & ASSERT #
+        assert_that_assertion_fails(assertion, actual)
