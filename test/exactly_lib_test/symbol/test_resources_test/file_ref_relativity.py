3c1e8496cb8ba5d4d7546de60180c3b3fd5c873a
Emil Karlén <emil.karlen@fripost.org>
2017-07-15 01:33:49 +0200

    test resources: Shortens tests using utility assert_that_assertion_fails
    file_ref_relativity

--- test/exactly_lib_test/symbol/test_resources_test/file_ref_relativity.py
+++ test/exactly_lib_test/symbol/test_resources_test/file_ref_relativity.py
@@ -3,8 +3,7 @@ import unittest
 from exactly_lib.symbol.restrictions.value_restrictions import NoRestriction, FileRefRelativityRestriction
 from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants, RelOptionType
 from exactly_lib_test.symbol.restrictions.test_resources import concrete_restriction_assertion as sut
-from exactly_lib_test.test_resources.test_of_test_resources_util import \
-    test_case_with_failure_exception_set_to_test_exception, TestException
+from exactly_lib_test.test_resources.value_assertions.assert_that_assertion_fails import assert_that_assertion_fails
 
 
 def suite() -> unittest.TestSuite:
@@ -25,15 +24,13 @@ class TestEqualsFileRefRelativityRestriction(unittest.TestCase):
                 sut.equals_file_ref_relativity_restriction(restriction).apply_without_message(self, restriction)
 
     def test_not_equals__different__types(self):
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = FileRefRelativityRestriction(PathRelativityVariants({RelOptionType.REL_HOME}, False))
         actual = NoRestriction()
-        with put.assertRaises(TestException):
-            sut.equals_file_ref_relativity_restriction(expected).apply_without_message(put, actual)
+        assertion = sut.equals_file_ref_relativity_restriction(expected)
+        assert_that_assertion_fails(assertion, actual)
 
     def test_not_equals__different__accepted_relativity_variants(self):
-        put = test_case_with_failure_exception_set_to_test_exception()
         expected = FileRefRelativityRestriction(PathRelativityVariants({RelOptionType.REL_HOME}, False))
         actual = FileRefRelativityRestriction(PathRelativityVariants({RelOptionType.REL_ACT}, False))
-        with put.assertRaises(TestException):
-            sut.equals_file_ref_relativity_restriction(expected).apply_without_message(put, actual)
+        assertion = sut.equals_file_ref_relativity_restriction(expected)
+        assert_that_assertion_fails(assertion, actual)
