688df8ede260767acb468100ccbbc81bbb35d0ef
Emil Karlén <emil.karlen@fripost.org>
2017-07-15 06:34:06 +0200

    list values: Adds test resource for assertion on ListResolver

--- test/exactly_lib_test/symbol/test_resources/list_assertions.py
+++ test/exactly_lib_test/symbol/test_resources/list_assertions.py
@@ -1,9 +1,16 @@
+import unittest
+
 from exactly_lib.symbol import list_resolver
+from exactly_lib.symbol.list_resolver import ListResolver
+from exactly_lib.symbol.resolver_structure import SymbolValueResolver
+from exactly_lib.type_system_values.value_type import ValueType
 from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.symbol.test_resources.assertion_utils import symbol_table_with_values_matching_references
+from exactly_lib_test.symbol.test_resources.concrete_value_assertions import _EqualsSymbolValueResolverBase
 from exactly_lib_test.symbol.test_resources.symbol_reference_assertions import equals_symbol_references, \
     equals_symbol_reference
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+from exactly_lib_test.type_system_values.test_resources.list_value import equals_list_value
 from exactly_lib_test.type_system_values.test_resources.string_value import equals_string_value
 
 
@@ -33,3 +40,44 @@ def equals_list_resolver_element(expected: list_resolver.Element,
     return asrt.is_instance_with(
         list_resolver.Element,
         asrt.and_(component_assertions))
+
+
+def equals_list_resolver_elements(elements: list,
+                                  symbols: SymbolTable = None) -> asrt.ValueAssertion:
+    element_assertions = [equals_list_resolver_element(element, symbols)
+                          for element in elements]
+    return asrt.matches_sequence(element_assertions)
+
+
+def equals_list_resolver(expected: ListResolver,
+                         symbols: SymbolTable = None) -> asrt.ValueAssertion:
+    if symbols is None:
+        symbols = symbol_table_with_values_matching_references(expected.references)
+    assertion_on_symbol_references = equals_symbol_references(expected.references)
+
+    return _EqualsListValueResolverAssertion(assertion_on_symbol_references,
+                                             expected,
+                                             symbols)
+
+
+class _EqualsListValueResolverAssertion(_EqualsSymbolValueResolverBase):
+    def __init__(self,
+                 expected_symbol_references: asrt.ValueAssertion,
+                 expected_resolver: ListResolver,
+                 symbols_for_checking_resolving: SymbolTable):
+        super().__init__(ValueType.LIST,
+                         expected_symbol_references,
+                         equals_list_value(expected_resolver.resolve(symbols_for_checking_resolving)),
+                         symbols_for_checking_resolving)
+        self._expected_resolver = expected_resolver
+
+    def _custom_check_of_resolver(self,
+                                  put: unittest.TestCase,
+                                  actual: SymbolValueResolver,
+                                  message_builder: asrt.MessageBuilder):
+        put.assertIsInstance(actual, ListResolver,
+                             'Actual value is expected to be a ' + str(ListResolver))
+        assert isinstance(actual, ListResolver)
+        elements_assertion = equals_list_resolver_elements(list(self._expected_resolver.elements))
+        elements_assertion.apply(put, actual.elements,
+                                 message_builder.for_sub_component('elements'))
--- test/exactly_lib_test/symbol/test_resources_test/list_assertions.py
+++ test/exactly_lib_test/symbol/test_resources_test/list_assertions.py
@@ -1,23 +1,26 @@
 import unittest
 
-from exactly_lib.symbol import list_resolver
+from exactly_lib.symbol import list_resolver as lr
 from exactly_lib.symbol import string_resolver as sr
-from exactly_lib_test.symbol.test_resources import list_assertions as sut
+from exactly_lib.util.symbol_table import SymbolTable
+from exactly_lib_test.symbol.test_resources import list_assertions as sut, symbol_utils as su
 from exactly_lib_test.symbol.test_resources.symbol_utils import symbol_reference
+from exactly_lib_test.test_resources.name_and_value import NameAndValue
 from exactly_lib_test.test_resources.value_assertions.assert_that_assertion_fails import assert_that_assertion_fails
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(TestEqualsElement),
+        unittest.makeSuite(TestEqualsResolver),
     ])
 
 
 class TestEqualsElement(unittest.TestCase):
     def test_equals(self):
         test_cases = [
-            list_resolver.StringResolverElement(sr.string_constant('value')),
-            list_resolver.SymbolReferenceElement(symbol_reference('symbol_name')),
+            lr.StringResolverElement(sr.string_constant('value')),
+            lr.SymbolReferenceElement(symbol_reference('symbol_name')),
         ]
         for element in test_cases:
             with self.subTest(msg=str(element)):
@@ -25,32 +28,84 @@ class TestEqualsElement(unittest.TestCase):
 
     def test_string_not_equals_symbol_ref(self):
         # ARRANGE #
-        string_element = list_resolver.StringResolverElement(sr.string_constant('value'))
-        symbol_reference_element = list_resolver.SymbolReferenceElement(symbol_reference('symbol_name'))
+        string_element = lr.StringResolverElement(sr.string_constant('value'))
+        symbol_reference_element = lr.SymbolReferenceElement(symbol_reference('symbol_name'))
         assertion = sut.equals_list_resolver_element(string_element)
         # ACT & ASSERT #
         assert_that_assertion_fails(assertion, symbol_reference_element)
 
     def test_symbol_ref_not_equals_string(self):
         # ARRANGE #
-        string_element = list_resolver.StringResolverElement(sr.string_constant('value'))
-        symbol_reference_element = list_resolver.SymbolReferenceElement(symbol_reference('symbol_name'))
+        string_element = lr.StringResolverElement(sr.string_constant('value'))
+        symbol_reference_element = lr.SymbolReferenceElement(symbol_reference('symbol_name'))
         assertion = sut.equals_list_resolver_element(symbol_reference_element)
         # ACT & ASSERT #
         assert_that_assertion_fails(assertion, string_element)
 
     def test_string_not_equals_string_with_different_value(self):
         # ARRANGE #
-        expected = list_resolver.StringResolverElement(sr.string_constant('expected value'))
-        actual = list_resolver.StringResolverElement(sr.string_constant('actual value'))
+        expected = lr.StringResolverElement(sr.string_constant('expected value'))
+        actual = lr.StringResolverElement(sr.string_constant('actual value'))
         assertion = sut.equals_list_resolver_element(expected)
         # ACT & ASSERT #
         assert_that_assertion_fails(assertion, actual)
 
     def test_symbol_ref_not_equals_symbol_ref_with_different_symbol_name(self):
         # ARRANGE #
-        expected = list_resolver.SymbolReferenceElement(symbol_reference('expected_symbol_name'))
-        actual = list_resolver.SymbolReferenceElement(symbol_reference('actual_symbol_name'))
+        expected = lr.SymbolReferenceElement(symbol_reference('expected_symbol_name'))
+        actual = lr.SymbolReferenceElement(symbol_reference('actual_symbol_name'))
         assertion = sut.equals_list_resolver_element(expected)
         # ACT & ASSERT #
         assert_that_assertion_fails(assertion, actual)
+
+
+class TestEqualsResolver(unittest.TestCase):
+    def test_equals(self):
+        cases = [
+            lr.ListResolver([lr.StringResolverElement(sr.string_constant('value'))]),
+            lr.ListResolver([lr.SymbolReferenceElement(symbol_reference('symbol_name'))]),
+        ]
+        for resolver in cases:
+            with self.subTest(msg=str(resolver)):
+                sut.equals_list_resolver(resolver).apply_without_message(self, resolver)
+
+    def test_not_equals(self):
+        string_symbol = NameAndValue('string_symbol_name', 'string symbol value')
+        cases = [
+            Case('different number of elements',
+                 expected=
+                 lr.ListResolver([]),
+                 actual=
+                 lr.ListResolver([lr.StringResolverElement(sr.string_constant('value'))]),
+                 ),
+            Case('different value of single string',
+                 expected=
+                 lr.ListResolver([lr.StringResolverElement(sr.string_constant('expected value'))]),
+                 actual=
+                 lr.ListResolver([lr.StringResolverElement(sr.string_constant('actual value'))]),
+                 ),
+            Case('different element types, but same resolved value',
+                 expected=
+                 lr.ListResolver([lr.StringResolverElement(sr.string_constant(string_symbol.value))]),
+                 actual=
+                 lr.ListResolver([lr.SymbolReferenceElement(su.symbol_reference(string_symbol.name))]),
+                 symbols=
+                 su.symbol_table_with_single_string_value(string_symbol.name, string_symbol.value),
+                 ),
+        ]
+        for case in cases:
+            with self.subTest(msg=case.name):
+                assertion = sut.equals_list_resolver(case.expected, case.symbols)
+                assert_that_assertion_fails(assertion, case.actual)
+
+
+class Case:
+    def __init__(self,
+                 name: str,
+                 expected: lr.ListResolver,
+                 actual: lr.ListResolver,
+                 symbols: SymbolTable = None):
+        self.name = name
+        self.expected = expected
+        self.actual = actual
+        self.symbols = symbols
