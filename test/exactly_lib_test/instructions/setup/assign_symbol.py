eba4cd3c997f8f5d269314a1847a55c64fbf1b1f
Emil Karlén <emil.karlen@fripost.org>
2017-07-15 02:08:05 +0200

    symbols: Renames test resource to match source code module name

--- test/exactly_lib_test/instructions/setup/assign_symbol.py
+++ test/exactly_lib_test/instructions/setup/assign_symbol.py
@@ -25,11 +25,11 @@ from exactly_lib_test.instructions.test_resources.check_description import suite
 from exactly_lib_test.instructions.test_resources.single_line_source_instruction_utils import \
     equivalent_source_variants__with_source_check
 from exactly_lib_test.instructions.utils.arg_parse.parse_string import string_resolver_from_fragments
-from exactly_lib_test.symbol.test_resources import value_structure_assertions as vs_asrt
+from exactly_lib_test.symbol.test_resources import resolver_structure_assertions as vs_asrt
+from exactly_lib_test.symbol.test_resources.resolver_structure_assertions import equals_container
 from exactly_lib_test.symbol.test_resources.symbol_usage_assertions import assert_symbol_usages_is_singleton_list
 from exactly_lib_test.symbol.test_resources.symbol_utils import string_value_constant_container, \
     container
-from exactly_lib_test.symbol.test_resources.value_structure_assertions import equals_container
 from exactly_lib_test.test_resources.parse import remaining_source
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.util.test_resources.symbol_table_assertions import assert_symbol_table_is_singleton
