cb9784b4fe75abcc77f00e730dbedef46dd9612c
Emil Karlén <emil.karlen@fripost.org>
2017-07-16 23:08:10 +0200

    symbols in process exe: Adds assertion method: matches_list_resolver

--- test/exactly_lib_test/symbol/test_resources/list_assertions.py
+++ test/exactly_lib_test/symbol/test_resources/list_assertions.py
@@ -4,8 +4,9 @@ from exactly_lib.symbol import concrete_resolvers
 from exactly_lib.symbol import list_resolver
 from exactly_lib.symbol.list_resolver import ListResolver
 from exactly_lib.symbol.resolver_structure import SymbolValueResolver
+from exactly_lib.type_system_values.list_value import ListValue
 from exactly_lib.type_system_values.value_type import ValueType
-from exactly_lib.util.symbol_table import SymbolTable
+from exactly_lib.util.symbol_table import SymbolTable, empty_symbol_table
 from exactly_lib_test.symbol.test_resources.assertion_utils import symbol_table_with_values_matching_references
 from exactly_lib_test.symbol.test_resources.concrete_value_assertions import _EqualsSymbolValueResolverBase
 from exactly_lib_test.symbol.test_resources.symbol_reference_assertions import equals_symbol_references, \
@@ -61,6 +62,17 @@ def equals_list_resolver(expected: ListResolver,
                                              symbols)
 
 
+def matches_list_resolver(expected_resolved_value: ListValue,
+                          expected_symbol_references: asrt.ValueAssertion,
+                          symbols: SymbolTable = None) -> asrt.ValueAssertion:
+    if symbols is None:
+        symbols = empty_symbol_table()
+
+    return _MatchesListValueResolverAssertion(expected_symbol_references,
+                                              expected_resolved_value,
+                                              symbols)
+
+
 def equals_constant_list(expected_str_list: list) -> asrt.ValueAssertion:
     return equals_list_resolver(concrete_resolvers.list_constant(expected_str_list))
 
@@ -86,3 +98,21 @@ class _EqualsListValueResolverAssertion(_EqualsSymbolValueResolverBase):
         elements_assertion = equals_list_resolver_elements(list(self._expected_resolver.elements))
         elements_assertion.apply(put, actual.elements,
                                  message_builder.for_sub_component('elements'))
+
+
+class _MatchesListValueResolverAssertion(_EqualsSymbolValueResolverBase):
+    def __init__(self,
+                 expected_symbol_references: asrt.ValueAssertion,
+                 expected_resolved_value: ListValue,
+                 symbols_for_checking_resolving: SymbolTable):
+        super().__init__(ValueType.LIST,
+                         expected_symbol_references,
+                         equals_list_value(expected_resolved_value),
+                         symbols_for_checking_resolving)
+
+    def _custom_check_of_resolver(self,
+                                  put: unittest.TestCase,
+                                  actual: SymbolValueResolver,
+                                  message_builder: asrt.MessageBuilder):
+        put.assertIsInstance(actual, ListResolver,
+                             'Actual value is expected to be a ' + str(ListResolver))
