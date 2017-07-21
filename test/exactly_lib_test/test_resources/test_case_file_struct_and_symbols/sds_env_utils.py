161d03097b04e16e476e1dc394912d4fd1cd922e
Emil Karlén <emil.karlen@fripost.org>
2017-07-21 21:17:58 +0200

    Test resource: makes utility generally available: MkDirAndChangeToItInsideOfSdsButOutsideOfAnyOfTheRelativityOptionDirs

--- test/exactly_lib_test/test_resources/test_case_file_struct_and_symbols/sds_env_utils.py
+++ test/exactly_lib_test/test_resources/test_case_file_struct_and_symbols/sds_env_utils.py
@@ -1,10 +1,12 @@
 import os
+import pathlib
 import tempfile
 from contextlib import contextmanager
 
 from exactly_lib import program_info
 from exactly_lib.symbol.value_resolvers.path_resolving_environment import PathResolvingEnvironmentPostSds
 from exactly_lib.test_case_file_structure import sandbox_directory_structure as sds_module
+from exactly_lib.test_case_file_structure.sandbox_directory_structure import SandboxDirectoryStructure
 from exactly_lib.util.file_utils import resolved_path_name, preserved_cwd
 from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check import sds_populator
@@ -29,3 +31,15 @@ def sds_with_act_as_curr_dir(contents: sds_populator.SdsPopulator = sds_populato
             pre_contents_population_action.apply(environment)
             contents.populate_sds(sds)
             yield environment
+
+
+class MkDirAndChangeToItInsideOfSdsButOutsideOfAnyOfTheRelativityOptionDirs(SdsAction):
+    DIR_NAME = 'not-a-std-sub-dir-of-sds'
+
+    def apply(self, environment: PathResolvingEnvironmentPostSds):
+        dir_path = self.resolve_dir_path(environment.sds)
+        dir_path.mkdir(parents=True, exist_ok=True)
+        os.chdir(str(dir_path))
+
+    def resolve_dir_path(self, sds: SandboxDirectoryStructure) -> pathlib.Path:
+        return sds.root_dir / self.DIR_NAME
