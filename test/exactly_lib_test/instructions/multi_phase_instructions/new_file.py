79d28407c03782ad4836b7d83e7277fddd7f89ea
Emil Karlén <emil.karlen@etraveli.com>
2017-07-21 22:06:43 +0200

    symbols in here-doc: file: Converts tests to test of InstructionEmbryo

--- test/exactly_lib_test/instructions/multi_phase_instructions/new_file.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/new_file.py
@@ -9,16 +9,13 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 from exactly_lib.symbol.value_resolvers.path_resolving_environment import PathResolvingEnvironmentPostSds, \
     PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
-from exactly_lib.test_case_file_structure.path_relativity import RelOptionType, specific_relative_relativity, \
-    RelSdsOptionType
+from exactly_lib.test_case_file_structure.path_relativity import RelOptionType, RelSdsOptionType
 from exactly_lib.util.string import lines_content
-from exactly_lib.util.symbol_table import empty_symbol_table
 from exactly_lib_test.instructions.multi_phase_instructions.test_resources import \
     instruction_embryo_check as embryo_check
 from exactly_lib_test.instructions.test_resources.check_description import suite_for_instruction_documentation
-from exactly_lib_test.instructions.test_resources.relativity_options import conf_rel_sds
+from exactly_lib_test.instructions.test_resources.relativity_options import conf_rel_sds, conf_rel_any
 from exactly_lib_test.instructions.utils.arg_parse.test_resources import args_with_rel_ops
-from exactly_lib_test.test_case_file_structure.test_resources.path_relativity import equals_path_relativity
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_contents_check import \
     act_dir_contains_exactly, tmp_user_dir_contains_exactly, SubDirOfSdsContainsExactly
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_populator import contents_in
@@ -30,20 +27,27 @@ from exactly_lib_test.test_resources.test_case_file_struct_and_symbols.home_and_
 from exactly_lib_test.test_resources.test_case_file_struct_and_symbols.sds_env_utils import \
     MkDirAndChangeToItInsideOfSdsButOutsideOfAnyOfTheRelativityOptionDirs
 from exactly_lib_test.test_resources.value_assertions import file_assertions as f_asrt
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion, ValueIsNone, ValueIsNotNone
-from exactly_lib_test.type_system_values.test_resources.concrete_path_part import equals_path_part_string
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(TestFailingParseWithNoContents),
+        unittest.makeSuite(TestFailingParseWithContents),
+        unittest.makeSuite(TestSuccessfulParseWithNoContents),
         unittest.makeSuite(TestSuccessfulParseWithContents),
         unittest.makeSuite(TestSuccessfulScenariosNoContent),
         unittest.makeSuite(TestSuccessfulScenariosWithContent),
         unittest.makeSuite(TestFailingScenarios),
-        unittest.makeSuite(TestParseWithContents),
         suite_for_instruction_documentation(sut.TheInstructionDocumentation('instruction name')),
     ])
+
+
+DISALLOWED_RELATIVITIES = [
+    RelOptionType.REL_RESULT,
+    RelOptionType.REL_HOME,
+]
 
 
 class TestFailingParseWithNoContents(unittest.TestCase):
@@ -58,14 +62,15 @@ class TestFailingParseWithNoContents(unittest.TestCase):
             _parse_and_get_file_info(single_line_source(arguments))
 
     def test_disallowed_relativities(self):
-        cases = [
-            args_with_rel_ops('{rel_result_option} file'),
-            args_with_rel_ops('{rel_home_option} file'),
-        ]
-        for arguments in cases:
-            with self.subTest(source=arguments):
-                with self.assertRaises(SingleInstructionInvalidArgumentException):
-                    _parse_and_get_file_info(single_line_source(arguments))
+        for relativity in DISALLOWED_RELATIVITIES:
+            for following_lines in [[], ['following line']]:
+                with self.subTest(relativity=str(relativity),
+                                  following_lines=repr(following_lines)):
+                    option_conf = conf_rel_any(relativity)
+                    source = remaining_source('{rel_opt} file-name'.format(rel_opt=option_conf.option_string),
+                                              following_lines)
+                    with self.assertRaises(SingleInstructionInvalidArgumentException):
+                        _just_parse(source)
 
     def test_fail_when_superfluous_arguments__without_option(self):
         arguments = 'expected-argument superfluous-argument'
@@ -78,6 +83,40 @@ class TestFailingParseWithNoContents(unittest.TestCase):
             _parse_and_get_file_info(single_line_source(arguments))
 
 
+class TestFailingParseWithContents(unittest.TestCase):
+    def test_path_is_mandatory__with_option(self):
+        arguments = args_with_rel_ops('{rel_act_option} <<MARKER superfluous ')
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            source = remaining_source(arguments,
+                                      ['MARKER'])
+            _just_parse(source)
+
+    def test_disallowed_relativities(self):
+        for relativity in DISALLOWED_RELATIVITIES:
+            for following_lines in [[], ['following line']]:
+                with self.subTest(relativity=str(relativity),
+                                  following_lines=repr(following_lines)):
+                    option_conf = conf_rel_any(relativity)
+                    source = remaining_source('{rel_opt} file-name <<MARKER'.format(rel_opt=option_conf.option_string),
+                                              ['MARKER'] + following_lines)
+                    with self.assertRaises(SingleInstructionInvalidArgumentException):
+                        _just_parse(source)
+
+    def test_fail_when_superfluous_arguments__without_option(self):
+        arguments = 'expected-argument <<MARKER superfluous-argument'
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            source = remaining_source(arguments,
+                                      ['MARKER'])
+            _just_parse(source)
+
+    def test_fail_when_superfluous_arguments__with_option(self):
+        arguments = args_with_rel_ops('{rel_act_option}  expected-argument <<MARKER superfluous-argument')
+        source = remaining_source(arguments,
+                                  ['MARKER'])
+        with self.assertRaises(SingleInstructionInvalidArgumentException):
+            _just_parse(source)
+
+
 class TestCaseBase(unittest.TestCase):
     def _check(self,
                source: ParseSource,
@@ -86,6 +125,63 @@ class TestCaseBase(unittest.TestCase):
                ):
         parser = sut.EmbryoParser()
         embryo_check.check(self, parser, source, arrangement, expectation)
+
+
+class TestSuccessfulParseWithNoContents(TestCaseBase):
+    def test_when_no_option_path_should_be_relative_cwd(self):
+        file_name = 'file-name.txt'
+        expected_file = fs.empty_file(file_name)
+        source = remaining_source('{file_name}'.format(file_name=file_name), )
+        arrangement = embryo_check.ArrangementWithSds(
+            pre_contents_population_action=SETUP_CWD_ACTION,
+        )
+        expectation = embryo_check.Expectation(
+            side_effects_on_home=f_asrt.dir_is_empty(),
+            symbol_usages=asrt.is_empty_list,
+            main_side_effects_on_sds=SubDirOfSdsContainsExactly(SETUP_CWD_REL_SDS_ACTION.resolve_dir_path,
+                                                                fs.DirContents([expected_file])),
+        )
+        self._check(source, arrangement, expectation)
+
+    def test_explicit_relativity_of_home(self):
+        file_name = 'file-name.txt'
+        expected_file = fs.empty_file(file_name)
+        source = remaining_source(args_with_rel_ops('{rel_cwd_option} {file_name}  ',
+                                                    file_name=file_name),
+                                  )
+        arrangement = embryo_check.ArrangementWithSds(
+            pre_contents_population_action=SETUP_CWD_ACTION,
+        )
+        expectation = embryo_check.Expectation(
+            side_effects_on_home=f_asrt.dir_is_empty(),
+            symbol_usages=asrt.is_empty_list,
+            main_side_effects_on_sds=SubDirOfSdsContainsExactly(SETUP_CWD_REL_SDS_ACTION.resolve_dir_path,
+                                                                fs.DirContents([expected_file])),
+        )
+        self._check(source, arrangement, expectation)
+
+    def test_accepted_relativity_options_other_than_home(self):
+        file_name = 'a-file-name.txt'
+        expected_file = fs.empty_file(file_name)
+        accepted_relativity_options = {RelSdsOptionType.REL_ACT,
+                                       RelSdsOptionType.REL_TMP}
+        for relativity_option in accepted_relativity_options:
+            with self.subTest(relativity_option=str(relativity_option)):
+                rel_opt_conf = conf_rel_sds(relativity_option)
+                source = remaining_source(
+                    '{rel_opt} {file_name} '.format(rel_opt=rel_opt_conf.option_string,
+                                                    file_name=file_name),
+                )
+                arrangement = embryo_check.ArrangementWithSds(
+                    pre_contents_population_action=SETUP_CWD_ACTION,
+                )
+                expectation = embryo_check.Expectation(
+                    side_effects_on_home=f_asrt.dir_is_empty(),
+                    symbol_usages=asrt.is_empty_list,
+                    main_side_effects_on_sds=SubDirOfSdsContainsExactly(rel_opt_conf.root_dir__sds,
+                                                                        fs.DirContents([expected_file])),
+                )
+                self._check(source, arrangement, expectation)
 
 
 class TestSuccessfulParseWithContents(TestCaseBase):
@@ -102,6 +198,7 @@ class TestSuccessfulParseWithContents(TestCaseBase):
         )
         expectation = embryo_check.Expectation(
             side_effects_on_home=f_asrt.dir_is_empty(),
+            symbol_usages=asrt.is_empty_list,
             main_side_effects_on_sds=SubDirOfSdsContainsExactly(SETUP_CWD_REL_SDS_ACTION.resolve_dir_path,
                                                                 fs.DirContents([expected_file])),
         )
@@ -121,6 +218,7 @@ class TestSuccessfulParseWithContents(TestCaseBase):
         )
         expectation = embryo_check.Expectation(
             side_effects_on_home=f_asrt.dir_is_empty(),
+            symbol_usages=asrt.is_empty_list,
             main_side_effects_on_sds=SubDirOfSdsContainsExactly(SETUP_CWD_REL_SDS_ACTION.resolve_dir_path,
                                                                 fs.DirContents([expected_file])),
         )
@@ -146,55 +244,15 @@ class TestSuccessfulParseWithContents(TestCaseBase):
                 )
                 expectation = embryo_check.Expectation(
                     side_effects_on_home=f_asrt.dir_is_empty(),
+                    symbol_usages=asrt.is_empty_list,
                     main_side_effects_on_sds=SubDirOfSdsContainsExactly(rel_opt_conf.root_dir__sds,
                                                                         fs.DirContents([expected_file])),
                 )
                 self._check(source, arrangement, expectation)
 
 
-class TestParseWithContents(unittest.TestCase):
-    def test_fail_when_superfluous_arguments(self):
-        source = argument_list_source(['file name', '<<MARKER', 'superfluous argument'],
-                                      ['single line',
-                                       'MARKER'])
-        with self.assertRaises(SingleInstructionInvalidArgumentException):
-            _parse_and_get_file_info(source)
-
-    def test_single_line(self):
-        source = argument_list_source(['file name', '<<MARKER'],
-                                      ['single line',
-                                       'MARKER'])
-        actual = _parse_and_get_file_info(source)
-        symbol_table = empty_symbol_table()
-        relativity_assertion = equals_path_relativity(specific_relative_relativity(RelOptionType.REL_CWD))
-        actual_file_ref = actual.file_ref.resolve(symbol_table)
-        relativity_assertion.apply_with_message(self, actual_file_ref.relativity(),
-                                                'relativity')
-        equals_path_part_string('file name').apply_with_message(self, actual_file_ref.path_suffix(),
-                                                                'destination_path/path_suffix')
-        self.assertEqual(lines_content(['single line']),
-                         actual.contents.resolve(symbol_table).value_when_no_dir_dependencies())
-        self.assertFalse(source.has_current_line)
-
-    def test_single_line__with_option(self):
-        source = argument_list_source(['--rel-tmp', 'file name', '<<MARKER'],
-                                      ['single line',
-                                       'MARKER',
-                                       'following line'])
-        actual = _parse_and_get_file_info(source)
-        symbol_table = empty_symbol_table()
-        relativity_assertion = equals_path_relativity(specific_relative_relativity(RelOptionType.REL_TMP))
-        actual_file_ref = actual.file_ref.resolve(symbol_table)
-        relativity_assertion.apply_with_message(self, actual_file_ref.relativity(),
-                                                'relativity')
-        equals_path_part_string('file name').apply_with_message(self,
-                                                                actual_file_ref.path_suffix(),
-                                                                'destination_path/path_suffix')
-        self.assertEqual(lines_content(['single line']),
-                         actual.contents.resolve(symbol_table).value_when_no_dir_dependencies())
-        self.assertTrue(source.has_current_line)
-        self.assertEqual('following line',
-                         source.current_line_text)
+def _just_parse(source: ParseSource):
+    sut.EmbryoParser().parse(source)
 
 
 def _parse_and_get_file_info(source: ParseSource) -> sut.FileInfo:
@@ -297,7 +355,7 @@ class TestSuccessfulScenariosWithContent(TestCaseBase):
 
 
 class TestFailingScenarios(TestCaseBase):
-    def test_argument_is_existing_file(self):
+    def test_argument_is_existing_file__no_contents(self):
         self._check_argument(single_line_source('existing-file'),
                              sds_test.Arrangement(
                                  sds_contents_before=contents_in(RelSdsOptionType.REL_ACT,
