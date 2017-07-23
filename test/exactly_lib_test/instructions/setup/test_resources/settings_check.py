9c98bc142442c53a75958cd3449d82d3349844f5
Emil Karlén <emil.karlen@fripost.org>
2017-07-23 12:16:38 +0200

    symbols in here-doc: setup instr check: Adds util to check SettingsBuilder

--- test/exactly_lib_test/instructions/setup/test_resources/settings_check.py
+++ test/exactly_lib_test/instructions/setup/test_resources/settings_check.py
@@ -4,6 +4,22 @@ from exactly_lib.test_case.phases import common
 from exactly_lib.test_case.phases.setup import SetupSettingsBuilder
 
 
+class Model(tuple):
+    def __new__(cls,
+                actual: SetupSettingsBuilder,
+                environment: common.InstructionEnvironmentForPostSdsStep,
+                ):
+        return tuple.__new__(cls, (actual, environment))
+
+    @property
+    def actual(self) -> SetupSettingsBuilder:
+        return self[0]
+
+    @property
+    def environment(self) -> common.InstructionEnvironmentForPostSdsStep:
+        return self[1]
+
+
 class Assertion:
     def apply(self,
               put: unittest.TestCase,
