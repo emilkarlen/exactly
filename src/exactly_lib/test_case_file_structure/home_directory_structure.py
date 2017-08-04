dd84029dae2379a4c518b85c2148189fa9ce7371
Emil Karlén <emil.karlen@etraveli.com>
2017-08-04 20:33:25 +0200

    hds: Adds HomeDirectoryStructure

---
+++ src/exactly_lib/test_case_file_structure/home_directory_structure.py
@@ -0,0 +1,11 @@
+import pathlib
+
+
+class HomeDirectoryStructure:
+    def __init__(self,
+                 case_home: pathlib.Path):
+        self._case_home = case_home
+
+    @property
+    def case_home(self) -> pathlib.Path:
+        return self._case_home
--- test/exactly_lib_test/test_case_file_structure/__init__.py
+++ test/exactly_lib_test/test_case_file_structure/__init__.py
@@ -1,14 +1,16 @@
 import unittest
 
 from exactly_lib_test.test_case_file_structure import dir_dependent_value
+from exactly_lib_test.test_case_file_structure import home_directory_structure, sandbox_directory_structure
 from exactly_lib_test.test_case_file_structure import relativity_root, relative_path_options
+from exactly_lib_test.test_case_file_structure import relativity_root, test_resources_test
 from exactly_lib_test.test_case_file_structure import relativity_validation
-from exactly_lib_test.test_case_file_structure import sandbox_directory_structure, relativity_root, test_resources_test
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources_test.suite())
+    ret_val.addTest(home_directory_structure.suite())
     ret_val.addTest(sandbox_directory_structure.suite())
     ret_val.addTest(relativity_root.suite())
     ret_val.addTest(dir_dependent_value.suite())
