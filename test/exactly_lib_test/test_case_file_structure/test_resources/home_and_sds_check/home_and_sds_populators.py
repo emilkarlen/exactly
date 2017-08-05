d1176ea8fd3aa8cdebc1a26b97de48e2296c99d4
Emil Karlén <emil.karlen@etraveli.com>
2017-08-05 22:29:04 +0200

    hds: Removes unused class: HomeAndSdsContents

--- test/exactly_lib_test/test_case_file_structure/test_resources/home_and_sds_check/home_and_sds_populators.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/home_and_sds_check/home_and_sds_populators.py
@@ -2,9 +2,7 @@ from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_file_structure.path_relativity import RelOptionType
 from exactly_lib.test_case_file_structure.relative_path_options import REL_OPTIONS_MAP
 from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import HomeOrSdsPopulator
-from exactly_lib_test.test_case_file_structure.test_resources.sds_check import sds_populator
 from exactly_lib_test.test_resources import file_structure
-from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir_contents
 
 
 class HomeOrSdsPopulatorForRelOptionType(HomeOrSdsPopulator):
@@ -34,19 +32,3 @@ class _ListOfPopulators(HomeOrSdsPopulator):
     def populate_home_or_sds(self, home_and_sds: HomeAndSds):
         for populator in self.__populator_list:
             populator.populate_home_or_sds(home_and_sds)
-
-
-class HomeAndSdsContents(tuple):
-    def __new__(cls,
-                home_dir_contents: DirContents = empty_dir_contents(),
-                sds_contents: sds_populator.SdsPopulator = sds_populator.empty()):
-        return tuple.__new__(cls, (home_dir_contents,
-                                   sds_contents))
-
-    @property
-    def home_dir_contents(self) -> DirContents:
-        return self[0]
-
-    @property
-    def sds_contents(self) -> sds_populator.SdsPopulator:
-        return self[1]
