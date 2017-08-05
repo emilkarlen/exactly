850456754619d0ebe2c7823352b481b3d93e6c70
Emil Karlén <emil.karlen@etraveli.com>
2017-08-05 22:21:55 +0200

    hds: Replaces home_dir_contents with hds_contents
    home_directory_structure

--- test/exactly_lib_test/test_case_file_structure/test_resources/hds_utils.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/hds_utils.py
@@ -6,16 +6,13 @@ from exactly_lib.test_case_file_structure.home_directory_structure import HomeDi
 from exactly_lib.util.file_utils import resolved_path
 from exactly_lib_test.test_case_file_structure.test_resources import home_populators
 from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import HomePopulator
-from exactly_lib_test.test_resources import file_structure as fs
 
 
 @contextmanager
 def home_directory_structure(contents: HomePopulator = home_populators.empty(),
-                             case_dir_contents: fs.DirContents = fs.empty_dir_contents(),
                              prefix: str = program_info.PROGRAM_NAME + '-test-hds-') \
         -> HomeDirectoryStructure:
     with tempfile.TemporaryDirectory(prefix=prefix) as case_dir_name:
         hds = HomeDirectoryStructure(resolved_path(case_dir_name))
         contents.populate_hds(hds)
-        case_dir_contents.write_to(hds.case_dir)
         yield hds
