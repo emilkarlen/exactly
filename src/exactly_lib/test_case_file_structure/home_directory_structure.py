ddf659ad4600a43dd8abfde4826904e71645a450
Emil Karlén <emil.karlen@fripost.org>
2017-08-06 11:54:42 +0200

    hds: Makes HomeDirectoryStructure immutable

--- src/exactly_lib/test_case_file_structure/home_directory_structure.py
+++ src/exactly_lib/test_case_file_structure/home_directory_structure.py
@@ -1,17 +1,17 @@
 import pathlib
 
 
-class HomeDirectoryStructure:
-    def __init__(self,
-                 case_dir: pathlib.Path,
-                 act_dir: pathlib.Path):
-        self._case_dir = case_dir
-        self._act_dir = act_dir
+class HomeDirectoryStructure(tuple):
+    def __new__(cls,
+                case_dir: pathlib.Path,
+                act_dir: pathlib.Path):
+        return tuple.__new__(cls, (case_dir,
+                                   act_dir))
 
     @property
     def case_dir(self) -> pathlib.Path:
-        return self._case_dir
+        return self[0]
 
     @property
     def act_dir(self) -> pathlib.Path:
-        return self._act_dir
+        return self[1]
