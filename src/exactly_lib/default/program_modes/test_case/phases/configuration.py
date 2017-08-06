d0186d424c2fa3dfc648661b5975fd6fc2d23b16
Emil Karlén <emil.karlen@etraveli.com>
2017-08-06 14:22:48 +0200

    hds: Adds conf/act-home instruction to default instruction set

--- src/exactly_lib/default/program_modes/test_case/phases/configuration.py
+++ src/exactly_lib/default/program_modes/test_case/phases/configuration.py
@@ -1,13 +1,13 @@
 from exactly_lib.common.instruction_setup import instruction_set_from_name_and_setup_constructor_list
-from exactly_lib.help_texts.test_case.instructions.instruction_names import EXECUTION_MODE_INSTRUCTION_NAME, \
-    HOME_CASE_DIRECTORY_INSTRUCTION_NAME, ACTOR_INSTRUCTION_NAME, TIMEOUT_INSTRUCTION_NAME
-from exactly_lib.instructions.configuration import home, execution_mode, actor, timeout
+from exactly_lib.help_texts.test_case.instructions import instruction_names
+from exactly_lib.instructions.configuration import act_home, home, execution_mode, actor, timeout
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
     [
-        (HOME_CASE_DIRECTORY_INSTRUCTION_NAME, home.setup),
-        (EXECUTION_MODE_INSTRUCTION_NAME, execution_mode.setup),
-        (ACTOR_INSTRUCTION_NAME, actor.setup),
-        (TIMEOUT_INSTRUCTION_NAME, timeout.setup),
+        (instruction_names.HOME_CASE_DIRECTORY_INSTRUCTION_NAME, home.setup),
+        (instruction_names.HOME_ACT_DIRECTORY_INSTRUCTION_NAME, act_home.setup),
+        (instruction_names.EXECUTION_MODE_INSTRUCTION_NAME, execution_mode.setup),
+        (instruction_names.ACTOR_INSTRUCTION_NAME, actor.setup),
+        (instruction_names.TIMEOUT_INSTRUCTION_NAME, timeout.setup),
     ]
 )
