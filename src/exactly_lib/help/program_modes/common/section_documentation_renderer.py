4f6e267d02dc8c162c9c1f2aa9b9ade9bb09fc80
Emil Karlén <emil.karlen@fripost.org>
2017-08-09 15:20:36 +0200

    help texts: REFACT: Moves SecDocRenderer to separate module:
    SectionDocumentationRendererBase

---
+++ src/exactly_lib/help/program_modes/common/section_documentation_renderer.py
@@ -0,0 +1,21 @@
+from exactly_lib.help.program_modes.common.contents_structure import SectionDocumentation
+from exactly_lib.help.program_modes.common.renderers import default_section_para
+from exactly_lib.help.utils.rendering.section_contents_renderer import SectionContentsRenderer
+from exactly_lib.util.textformat.structure import structures as docs
+
+
+class SectionDocumentationRendererBase(SectionContentsRenderer):
+    def __init__(self, section_documentation: SectionDocumentation):
+        self.__section_documentation = section_documentation
+
+    def _default_section_info(self, default_section_name: str) -> list:
+        ret_val = []
+        if self.__section_documentation.name.plain == default_section_name:
+            ret_val.append(default_section_para(self.__section_documentation.section_concept_name))
+        return ret_val
+
+    def _mandatory_info_para(self):
+        mandatory_or_optional = 'mandatory' if self.__section_documentation.is_mandatory() else 'optional'
+        return docs.para('The {} {} is {}.'.format(self.__section_documentation.name,
+                                                   self.__section_documentation.section_concept_name,
+                                                   mandatory_or_optional))
--- src/exactly_lib/help/program_modes/test_suite/section/common.py
+++ src/exactly_lib/help/program_modes/test_suite/section/common.py
@@ -1,6 +1,7 @@
 from exactly_lib.help.program_modes.common.contents_structure import SectionInstructionSet, \
-    SectionDocumentation, SectionDocumentationRendererBase
+    SectionDocumentation
 from exactly_lib.help.program_modes.common.renderers import instruction_set_list
+from exactly_lib.help.program_modes.common.section_documentation_renderer import SectionDocumentationRendererBase
 from exactly_lib.help.utils.rendering.section_contents_renderer import RenderingEnvironment, SectionContentsRenderer
 from exactly_lib.help.utils.rendering.see_also_section import see_also_sections
 from exactly_lib.help_texts.cross_reference_id import TestSuiteSectionInstructionCrossReference
