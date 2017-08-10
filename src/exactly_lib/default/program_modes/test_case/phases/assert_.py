5454a457cafd2b5c8feca34fb7c08595409d8fca
Emil Karlén <emil.karlen@fripost.org>
2017-08-10 02:20:39 +0200

    assert/dir-contents: Adds the instruction to the default setup

--- src/exactly_lib/default/program_modes/test_case/phases/assert_.py
+++ src/exactly_lib/default/program_modes/test_case/phases/assert_.py
@@ -1,12 +1,14 @@
 from exactly_lib.common.instruction_setup import instruction_set_from_name_and_setup_constructor_list
 from exactly_lib.help_texts.test_case.instructions.instruction_names import CHANGE_DIR_INSTRUCTION_NAME, \
     SHELL_INSTRUCTION_NAME
-from exactly_lib.instructions.assert_ import change_dir, contents_of_file, env, run, exitcode, new_dir, \
+from exactly_lib.instructions.assert_ import change_dir, \
+    contents_of_dir, contents_of_file, env, run, exitcode, new_dir, \
     shell, existence_of_file, stdout, stderr
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
     [
         ('contents', contents_of_file.setup),
+        ('dir-contents', contents_of_dir.setup),
         ('dir', new_dir.setup),
         ('env', env.setup),
         ('run', run.setup),
