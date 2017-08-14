27e706cd5753caba2ddd60f364aff472eb3d386a
Emil Karlén <emil.karlen@etraveli.com>
2017-08-14 07:53:58 +0200

    assert/contents-of-dir: Adds util method to combine predicates using "and"

--- src/exactly_lib/util/functional.py
+++ src/exactly_lib/util/functional.py
@@ -1,3 +1,6 @@
+import types
+
+
 class Composition:
     def __init__(self, g, f):
         self.g = g
@@ -5,3 +8,22 @@ class Composition:
 
     def __call__(self, arg):
         return self.g(self.f(arg))
+
+
+def and_predicate(predicates: list) -> types.FunctionType:
+    if not predicates:
+        return lambda x: True
+    if len(predicates) == 1:
+        return predicates[0]
+    return _AndPredicate(predicates)
+
+
+class _AndPredicate:
+    def __init__(self, predicates: list):
+        self.predicates = predicates
+
+    def __call__(self, *args, **kwargs):
+        for predicate in self.predicates:
+            if not predicate(*args, **kwargs):
+                return False
+        return True
