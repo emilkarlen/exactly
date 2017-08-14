2bdd049704d8e4c0694056b25ddd7b01a52b82b5
Emil Karlén <emil.karlen@fripost.org>
2017-08-14 19:37:30 +0200

    instructions: Adds utility to return SVH via exceptions

---
+++ src/exactly_lib/instructions/utils/return_svh_via_exceptions.py
@@ -0,0 +1,34 @@
+from exactly_lib.test_case.phases.result import svh
+
+
+class SvhValidationException(Exception):
+    def __init__(self, err_msg: str):
+        super().__init__(err_msg)
+        self._err_msg = err_msg
+
+    @property
+    def err_msg(self) -> str:
+        return self._err_msg
+
+
+class SvhHardErrorException(Exception):
+    def __init__(self, err_msg: str):
+        super().__init__(err_msg)
+        self._err_msg = err_msg
+
+    @property
+    def err_msg(self) -> str:
+        return self._err_msg
+
+
+def translate_svh_exception_to_svh(action,
+                                   *args, **kwargs) -> svh.SuccessOrValidationErrorOrHardError:
+    try:
+        action(*args, **kwargs)
+        return svh.new_svh_success()
+
+    except SvhValidationException as ex:
+        return svh.new_svh_validation_error(ex.err_msg)
+
+    except SvhHardErrorException as ex:
+        return svh.new_svh_hard_error(ex.err_msg)
