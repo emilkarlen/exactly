4949d8d4b3afdb3f2ba0e904ef3b2fa15d1d992e
Emil Karlén <emil.karlen@fripost.org>
2017-08-14 22:44:18 +0200

    instructions: REFACT: Moves instr doc utils to parse util module

--- src/exactly_lib/instructions/assert_/exitcode.py
+++ src/exactly_lib/instructions/assert_/exitcode.py
@@ -1,11 +1,11 @@
 from exactly_lib.common.help.instruction_documentation_with_text_parser import \
     InstructionDocumentationWithCommandLineRenderingBase
-from exactly_lib.common.help.syntax_contents_structure import InvokationVariant, SyntaxElementDescription
+from exactly_lib.common.help.syntax_contents_structure import InvokationVariant
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
-from exactly_lib.instructions.assert_.utils import return_pfh_via_exceptions
+from exactly_lib.instructions.assert_.utils import return_pfh_via_exceptions, negation_of_assertion
 from exactly_lib.instructions.assert_.utils.expression import comprison_structures
 from exactly_lib.instructions.assert_.utils.expression import instruction
-from exactly_lib.instructions.assert_.utils.expression.integer_comparators import NAME_2_OPERATOR
+from exactly_lib.instructions.assert_.utils.expression import parse
 from exactly_lib.instructions.assert_.utils.expression.parse import parse_integer_comparison_operator_and_rhs
 from exactly_lib.instructions.utils.parse.token_stream_parse_prime import new_token_parser
 from exactly_lib.section_document.parser_implementations.instruction_parsers import \
@@ -13,7 +13,6 @@ from exactly_lib.section_document.parser_implementations.instruction_parsers imp
 from exactly_lib.test_case.os_services import OsServices
 from exactly_lib.test_case.phases import common as i
 from exactly_lib.test_case.phases.assert_ import AssertPhaseInstruction
-from exactly_lib.util.cli_syntax.elements import argument as a
 
 
 def setup(instruction_name: str) -> SingleInstructionSetup:
@@ -22,67 +21,66 @@ def setup(instruction_name: str) -> SingleInstructionSetup:
         TheInstructionDocumentation(instruction_name))
 
 
-INTEGER_ARGUMENT = a.Named('INTEGER')
-OPERATOR_ARGUMENT = a.Named('OPERATOR')
+_PROPERTY_NAME = 'exit code'
 
 
 class TheInstructionDocumentation(InstructionDocumentationWithCommandLineRenderingBase):
     def __init__(self, name: str):
-        self.integer_arg = a.Single(a.Multiplicity.MANDATORY,
-                                    INTEGER_ARGUMENT)
-        self.op_arg = a.Single(a.Multiplicity.MANDATORY,
-                               OPERATOR_ARGUMENT)
         super().__init__(name, {
-            'INTEGER': INTEGER_ARGUMENT.name,
-            'OPERATOR': OPERATOR_ARGUMENT.name,
+            'INTEGER': parse.INTEGER_ARGUMENT.name,
+            'OPERATOR': parse.OPERATOR_ARGUMENT.name,
+            'EXIT_CODE': _PROPERTY_NAME,
         })
 
     def single_line_description(self) -> str:
-        return 'Tests the exit code'
+        return 'Tests the ' + _PROPERTY_NAME
 
     def invokation_variants(self) -> list:
         return [
-            InvokationVariant(self._cl_syntax_for_args([self.integer_arg]),
-                              self._paragraphs(
-                                  """\
-                                  PASS if, and only if, the exit code is exactly {INTEGER}.
-                                  """)),
-            InvokationVariant(self._cl_syntax_for_args([self.op_arg, self.integer_arg]),
-                              self._paragraphs(
-                                  """\
-                                  PASS if, and only if, the given expression evaluates to True.
+            InvokationVariant(self._cl_syntax_for_args([
+                negation_of_assertion.optional_negation_argument_usage(),
+                parse.MANDATORY_INTEGER_ARGUMENT,
+            ]),
+                self._paragraphs(
+                    """\
+                    PASS if, and only if, the {EXIT_CODE} is exactly {INTEGER}.
+                    """)),
+            InvokationVariant(self._cl_syntax_for_args([
+                negation_of_assertion.optional_negation_argument_usage(),
+                parse.MANDATORY_OPERATOR_ARGUMENT,
+                parse.MANDATORY_INTEGER_ARGUMENT,
+            ]),
+                self._paragraphs(
+                    """\
+                    PASS if, and only if, the given expression evaluates to True.
 
-                                  The actual exit code is the left operand.
-                                  """))
+                    The actual {EXIT_CODE} is the left operand.
+                    """))
         ]
 
     def syntax_element_descriptions(self) -> list:
-        operators_list = ', '.join(sorted(NAME_2_OPERATOR.keys()))
-        operator_text = 'One of: ' + operators_list + '.'
-        integer_text = 'An integer in the interval [0, 255].'
-        return [
-            SyntaxElementDescription(INTEGER_ARGUMENT.name,
-                                     self._paragraphs(integer_text)),
-            SyntaxElementDescription(OPERATOR_ARGUMENT.name,
-                                     self._paragraphs(operator_text))
-        ]
+        return parse.syntax_element_descriptions_with_negation_operator(
+            'An integer in the interval [0, 255].')
 
 
 class Parser(InstructionParserThatConsumesCurrentLine):
     def _parse(self, rest_of_line: str) -> AssertPhaseInstruction:
         parser = new_token_parser(rest_of_line)
+        expectation_type = parser.consume_optional_negation_operator()
         cmp_op_and_rhs = parse_integer_comparison_operator_and_rhs(parser,
                                                                    must_be_within_byte_range)
         parser.report_superfluous_arguments_if_not_at_eol()
-        cmp_setup = comprison_structures.IntegerComparisonSetup(ExitCodeResolver(),
-                                                                cmp_op_and_rhs.operator,
-                                                                cmp_op_and_rhs.integer_resolver)
+        cmp_setup = comprison_structures.IntegerComparisonSetup(
+            expectation_type,
+            ExitCodeResolver(),
+            cmp_op_and_rhs.operator,
+            cmp_op_and_rhs.integer_resolver)
         return instruction.Instruction(cmp_setup)
 
 
 class ExitCodeResolver(comprison_structures.ActualValueResolver):
     def __init__(self):
-        super().__init__('exitcode')
+        super().__init__(_PROPERTY_NAME)
 
     def resolve(self, environment: i.InstructionEnvironmentForPostSdsStep, os_services: OsServices):
         sds = environment.sds
@@ -91,7 +89,9 @@ class ExitCodeResolver(comprison_structures.ActualValueResolver):
         except IOError:
             rel_path = sds.relative_to_sds_root(sds.result.exitcode_file)
             raise return_pfh_via_exceptions.PfhHardErrorException(
-                'Cannot read exit code from file ' + str(rel_path))
+                'Cannot read {exit_code} from file {file}'.format(
+                    exit_code=_PROPERTY_NAME,
+                    file=str(rel_path)))
         try:
             contents = f.read()
             return int(contents)
@@ -99,9 +99,10 @@ class ExitCodeResolver(comprison_structures.ActualValueResolver):
             raise return_pfh_via_exceptions.PfhHardErrorException(
                 'Failed to read contents from %s' % str(sds.result.exitcode_file))
         except ValueError:
-            msg = 'The contents of the file for Exit Code ("{}") is not an integer: "{}"'.format(
-                str(sds.result.exitcode_file),
-                contents)
+            msg = 'The contents of the file for {exit_code} ("{file}") is not an integer: "{contents}"'.format(
+                exit_code=_PROPERTY_NAME,
+                file=str(sds.result.exitcode_file),
+                contents=contents)
             raise return_pfh_via_exceptions.PfhHardErrorException(msg)
         finally:
             f.close()
