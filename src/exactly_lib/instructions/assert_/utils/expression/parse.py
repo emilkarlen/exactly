9cebb56234ef176b993c336ca132d2abb1f5ed1d
Emil Karlén <emil.karlen@etraveli.com>
2017-08-15 11:56:33 +0200

    instructions: REFACT: Moves tmp struct to parse util module

--- src/exactly_lib/instructions/assert_/utils/expression/parse.py
+++ src/exactly_lib/instructions/assert_/utils/expression/parse.py
@@ -5,9 +5,8 @@ from exactly_lib.help_texts.test_case.instructions import assign_symbol as help_
 from exactly_lib.instructions.assert_.utils import negation_of_assertion
 from exactly_lib.instructions.assert_.utils.expression import comprison_structures
 from exactly_lib.instructions.assert_.utils.expression import integer_comparators
+from exactly_lib.instructions.assert_.utils.expression.comprison_structures import IntegerResolver
 from exactly_lib.instructions.utils.parse.token_stream_parse_prime import TokenParserPrime
-from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
-    SingleInstructionInvalidArgumentException
 from exactly_lib.symbol.restrictions.reference_restrictions import string_made_up_by_just_strings
 from exactly_lib.symbol.string_resolver import StringResolver
 from exactly_lib.test_case_utils.parse import parse_string
@@ -43,17 +42,25 @@ def syntax_element_descriptions_with_negation_operator(
     return [negation_of_assertion.syntax_element_description()] + syntax_element_descriptions(integer_text)
 
 
+class IntegerComparisonOperatorAndRhs:
+    def __init__(self,
+                 operator: integer_comparators.ComparisonOperator,
+                 integer_resolver: IntegerResolver):
+        self.integer_resolver = integer_resolver
+        self.operator = operator
+
+
 def parse_integer_comparison_operator_and_rhs(
         parser: TokenParserPrime,
         custom_integer_restriction: types.FunctionType = None
-) -> comprison_structures.IntegerComparisonOperatorAndRhs:
+) -> IntegerComparisonOperatorAndRhs:
     def parse_integer_token_and_return(
             comparison_operator: integer_comparators.ComparisonOperator,
-            integer_token: Token) -> comprison_structures.IntegerComparisonOperatorAndRhs:
+            integer_token: Token) -> IntegerComparisonOperatorAndRhs:
 
         integer_resolver = integer_resolver_of(integer_token, custom_integer_restriction)
-        return comprison_structures.IntegerComparisonOperatorAndRhs(comparison_operator,
-                                                                    integer_resolver)
+        return IntegerComparisonOperatorAndRhs(comparison_operator,
+                                               integer_resolver)
 
     token = parser.consume_mandatory_token('Missing comparison expression')
 
@@ -84,13 +91,3 @@ def _string_resolver_of(value_token: Token) -> StringResolver:
                 string_type=help_texts.TYPE_INFO_DICT[ValueType.STRING].type_name,
             )
         ))
-
-
-def _parse_int_argument(argument) -> int:
-    try:
-        expected = int(argument)
-    except ValueError:
-        raise SingleInstructionInvalidArgumentException('Argument must be an integer')
-    if expected < 0 or expected > 255:
-        raise SingleInstructionInvalidArgumentException('Argument must be an integer in the interval [0, 255]')
-    return expected
