98073d1c182ae7747fa84e0efdcdb296e5e33e53
Emil Karlén <emil.karlen@etraveli.com>
2017-08-18 14:41:13 +0200

    examples: REFACT: Rename package "sandbox" -> "sandbox-directories"

--- examples/make-executables.py
+++ examples/make-executables.py
@@ -128,7 +128,7 @@ def _resolve_root(script_file_path_name: str) -> pathlib.Path:
 
 src_base_dir = pathlib.Path('executables-src')
 first_step_dir = pathlib.Path('first-step')
-sandbox_dir = pathlib.Path('sandbox')
+sandbox_dir = pathlib.Path('sandbox-directories')
 home_dir = pathlib.Path('home-directories')
 cleanup_dir = pathlib.Path('cleanup')
 external_programs_dir = pathlib.Path('external-programs')
