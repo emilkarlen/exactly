4b99beec49660beb9a088a4bb24fd7a6cb537389
Emil Karlén <emil.karlen@etraveli.com>
2017-08-22 21:33:07 +0200

    file-sel: REFACT: Removes unused code

--- src/exactly_lib/util/dir_contents_selection.py
+++ src/exactly_lib/util/dir_contents_selection.py
@@ -1,6 +1,5 @@
 """Functionality for accessing a subset of the files in a directory."""
 import fnmatch
-import itertools
 import os
 import pathlib
 import types
@@ -8,24 +7,6 @@ import types
 from exactly_lib.test_case_utils import file_properties
 from exactly_lib.test_case_utils.file_properties import FileType
 from exactly_lib.util import functional
-
-
-class DirContentsSelector:
-    def files(self, directory: pathlib.Path) -> iter:
-        """
-        :param directory: A path that is an existing directory
-        :return: Iterable of all files in the directory that is in the
-        subset that the object represents.
-        """
-        raise NotImplementedError('abstract method')
-
-    @property
-    def restriction_descriptions(self) -> list:
-        """
-        A list of strings that each describe a restriction
-        that must be met by a file to be included in the set.
-        """
-        raise NotImplementedError('abstract method')
 
 
 class Selectors(tuple):
@@ -186,15 +167,6 @@ class _FilesGetter:
         return functional.and_predicate(predicates)
 
 
-class EveryFileInDir(DirContentsSelector):
-    def files(self, directory: pathlib.Path) -> iter:
-        return directory.iterdir()
-
-    @property
-    def restriction_descriptions(self) -> list:
-        return []
-
-
 class NameFilter:
     def __init__(self, pattern: str):
         self.pattern = pattern
@@ -209,40 +181,3 @@ class TypeFilter:
 
     def __call__(self, names):
         return fnmatch.filter(names, self.pattern)
-
-
-class NamePatternSelector(DirContentsSelector):
-    def __init__(self, glob_pattern: str):
-        self.glob_pattern = glob_pattern
-
-    def files(self, directory: pathlib.Path) -> iter:
-        return directory.glob(self.glob_pattern)
-
-    @property
-    def restriction_descriptions(self) -> list:
-        return ['name matches glob pattern ' + self.glob_pattern]
-
-
-class FileTypeSelector(DirContentsSelector):
-    def __init__(self, included_file_type: file_properties.FileType):
-        self.included_file_type = included_file_type
-
-    def files(self, directory: pathlib.Path) -> iter:
-        predicate = file_properties.TYPE_INFO[self.included_file_type].pathlib_path_predicate
-        return filter(predicate, directory.iterdir())
-
-    @property
-    def restriction_descriptions(self) -> list:
-        return ['file is a ' + file_properties.TYPE_INFO[self.included_file_type].description]
-
-
-class AndSelector(DirContentsSelector):
-    def __init__(self, selectors: list):
-        self.selectors = selectors
-
-    def files(self, directory: pathlib.Path) -> iter:
-        raise NotImplementedError('todo')
-
-    @property
-    def restriction_descriptions(self) -> list:
-        return list(itertools.chain(map(lambda s: s.restriction_descriptions, self.selectors)))
