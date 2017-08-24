d67511674940b7fee263e1f73d5a2d1f297a0c62
Emil Karlén <emil.karlen@fripost.org>
2017-08-24 19:26:21 +0200

    file-sel: Adds FileSelectorStructureVisitor

---
+++ test/exactly_lib_test/type_system_values/file_selector.py
@@ -0,0 +1,47 @@
+import unittest
+
+from exactly_lib.type_system_values import file_selector as sut
+from exactly_lib.type_system_values.file_selector import FileSelector
+from exactly_lib.util.dir_contents_selection import Selectors
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(TestFileSelectorStructureVisitor),
+    ])
+
+
+class TestFileSelectorStructureVisitor(unittest.TestCase):
+    def test_visit_selectors(self):
+        # ARRANGE #
+        instance = sut.FileSelector(Selectors())
+        visitor = AVisitorThatRecordsVisitedMethods()
+        # ACT #
+        ret_val = visitor.visit(instance)
+        # ASSERT #
+        self.assertEqual([Selectors],
+                         visitor.visited_types)
+        self.assertIs(instance.selectors,
+                      ret_val)
+
+    def test_raise_type_error_WHEN_visited_object_is_of_unknown_class(self):
+        # ARRANGE #
+        instance = 'A value of a type that is not a ' + str(FileSelector)
+        visitor = AVisitorThatRecordsVisitedMethods()
+        # ACT #
+        with self.assertRaises(TypeError):
+            visitor.visit(instance)
+        # ASSERT #
+        self.assertIsNot(visitor.visited_types,
+                         'No visit method should have been executed.')
+
+    pass
+
+
+class AVisitorThatRecordsVisitedMethods(sut.FileSelectorStructureVisitor):
+    def __init__(self):
+        self.visited_types = []
+
+    def visit_selectors(self, selectors: Selectors):
+        self.visited_types.append(Selectors)
+        return selectors
