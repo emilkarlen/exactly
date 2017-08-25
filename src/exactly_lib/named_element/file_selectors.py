6d0c2b50167ee49b37360504ef77e6dfe44a5034
Emil Karlén <emil.karlen@fripost.org>
2017-08-25 20:27:16 +0200

    file-sel: Adds AND combinator resolver

--- src/exactly_lib/named_element/file_selectors.py
+++ src/exactly_lib/named_element/file_selectors.py
@@ -1,7 +1,10 @@
+import itertools
+
 from exactly_lib.named_element.named_element_usage import NamedElementReference
 from exactly_lib.named_element.resolver_structure import FileSelectorResolver, ElementType
 from exactly_lib.named_element.restriction import ElementTypeRestriction
 from exactly_lib.type_system_values.file_selector import FileSelector
+from exactly_lib.util import dir_contents_selection as dcs
 from exactly_lib.util.symbol_table import SymbolTable
 
 
@@ -46,3 +49,24 @@ class FileSelectorReference(FileSelectorResolver):
 
     def __str__(self):
         return str(type(self)) + '\'' + str(self._name_of_referenced_resolver) + '\''
+
+
+class FileSelectorAnd(FileSelectorResolver):
+    """
+    A :class:`FileSelectorResolver` that combines selectors using AND.
+    """
+
+    def __init__(self, resolvers: list):
+        self._resolvers = tuple(resolvers)
+        self._references = list(itertools.chain.from_iterable([r.references for r in resolvers]))
+
+    def resolve(self, symbols: SymbolTable) -> FileSelector:
+        selectors = dcs.and_all([resolver.resolve(symbols).selectors for resolver in self._resolvers])
+        return FileSelector(selectors)
+
+    @property
+    def references(self) -> list:
+        return self._references
+
+    def __str__(self):
+        return str(type(self)) + '\'' + 'num resolvers = ' + str(len(self._resolvers)) + '\''
