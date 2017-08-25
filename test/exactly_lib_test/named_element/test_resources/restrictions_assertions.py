6a3746c1a9a999242786bc19feb5a46b21179f75
Emil Karlén <emil.karlen@fripost.org>
2017-08-25 19:12:20 +0200

    file-sel: Adds test resource: is_element_type_restriction

---
+++ test/exactly_lib_test/named_element/test_resources/restrictions_assertions.py
@@ -0,0 +1,15 @@
+from exactly_lib.named_element.resolver_structure import ElementType
+from exactly_lib.named_element.restriction import ElementTypeRestriction
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+
+
+def is_element_type_restriction(expected: ElementType) -> asrt.ValueAssertion:
+    """
+    Assertion on a :class:`ReferenceRestrictions`,
+    that it is a :class:`ElementTypeRestriction`,
+    with a given :class:`ElementType`.
+    """
+    return asrt.is_instance_with(ElementTypeRestriction,
+                                 asrt.sub_component('element_type',
+                                                    ElementTypeRestriction.element_type.fget,
+                                                    asrt.is_(expected)))
