e54ac7a7e691f7f80ef6e28b4b35c0db3ca45abc
Emil Karlén <emil.karlen@fripost.org>
2017-08-27 12:56:09 +0200

    file-trans: file-contents: checking utils: Checker

--- test/exactly_lib_test/instructions/assert_/utils/__init__.py
+++ test/exactly_lib_test/instructions/assert_/utils/__init__.py
@@ -2,7 +2,7 @@ import unittest
 
 from exactly_lib_test.instructions.assert_.utils import instruction_from_parts_that_executes_sub_process, \
     instruction_from_parts, return_pfh_via_exceptions, \
-    expression
+    expression, checker
 
 
 def suite() -> unittest.TestSuite:
@@ -10,6 +10,7 @@ def suite() -> unittest.TestSuite:
         return_pfh_via_exceptions.suite(),
         instruction_from_parts.suite(),
         instruction_from_parts_that_executes_sub_process.suite(),
+        checker.suite(),
         expression.suite(),
     ])
 
