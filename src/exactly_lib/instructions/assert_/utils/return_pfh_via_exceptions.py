ddae6aa26867b2a4f9f88fafd2622918056f1ea4
Emil Karlén <emil.karlen@fripost.org>
2017-08-27 12:22:37 +0200

    file-trans: file-contents: REFACT: Introduces PfhException
    as base class for exceptions that represent a PFH value

--- src/exactly_lib/instructions/assert_/utils/return_pfh_via_exceptions.py
+++ src/exactly_lib/instructions/assert_/utils/return_pfh_via_exceptions.py
@@ -1,9 +1,29 @@
 from exactly_lib.test_case.phases.result import pfh
 
 
-class PfhFailException(Exception):
-    def __init__(self, err_msg: str):
+class PfhException(Exception):
+    def __init__(self,
+                 status: pfh.PassOrFailOrHardErrorEnum,
+                 err_msg: str):
         super().__init__(err_msg)
+        self._status = status
+        self._err_msg = err_msg
+
+    """An exception that represents a
+    :class:`pfh.PassOrFailOrHardError` value
+    that is not :class:`pfh.PassOrFailOrHardErrorEnum.PASS`.
+    """
+
+    @property
+    def pfh(self) -> pfh.PassOrFailOrHardError:
+        return pfh.PassOrFailOrHardError(self._status,
+                                         self._err_msg)
+
+
+class PfhFailException(PfhException):
+    def __init__(self, err_msg: str):
+        super().__init__(pfh.PassOrFailOrHardErrorEnum.FAIL,
+                         err_msg)
         self._err_msg = err_msg
 
     @property
@@ -11,9 +31,10 @@ class PfhFailException(Exception):
         return self._err_msg
 
 
-class PfhHardErrorException(Exception):
+class PfhHardErrorException(PfhException):
     def __init__(self, err_msg: str):
-        super().__init__(err_msg)
+        super().__init__(pfh.PassOrFailOrHardErrorEnum.HARD_ERROR,
+                         err_msg)
         self._err_msg = err_msg
 
     @property
@@ -27,8 +48,5 @@ def translate_pfh_exception_to_pfh(action,
         action(*args, **kwargs)
         return pfh.new_pfh_pass()
 
-    except PfhFailException as ex:
-        return pfh.new_pfh_fail(ex.err_msg)
-
-    except PfhHardErrorException as ex:
-        return pfh.new_pfh_hard_error(ex.err_msg)
+    except PfhException as ex:
+        return ex.pfh
