1b9e8ce0cc9f669449a2613cdfe3dfdcbb241fcb
Emil Karlén <emil.karlen@fripost.org>
2017-08-28 20:45:42 +0200

    file-trans: ExpressionParser: REFACT: Removes restriction to parse NamedElementResolver

--- src/exactly_lib/test_case_utils/parse/expression/expression_parser.py
+++ src/exactly_lib/test_case_utils/parse/expression/expression_parser.py
@@ -1,4 +1,3 @@
-from exactly_lib.named_element.resolver_structure import NamedElementResolver
 from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
@@ -9,13 +8,13 @@ from .grammar import Grammar
 
 
 def parse_from_parse_source(grammar: Grammar,
-                            source: ParseSource) -> NamedElementResolver:
+                            source: ParseSource):
     with token_stream_parse_prime.from_parse_source(source) as tp:
         return parse(grammar, tp)
 
 
 def parse(grammar: Grammar,
-          parser: TokenParserPrime) -> NamedElementResolver:
+          parser: TokenParserPrime):
     return _Parser(grammar, parser).parse()
 
 
@@ -27,13 +26,13 @@ class _Parser:
         self.grammar = grammar
         self.complex_expressions_keys = self.grammar.complex_expressions.keys()
 
-    def parse(self) -> NamedElementResolver:
+    def parse(self):
         if not self.grammar.complex_expressions:
             return self.parse_mandatory_simple()
         else:
             return self.parse_with_complex_expressions()
 
-    def parse_with_complex_expressions(self) -> NamedElementResolver:
+    def parse_with_complex_expressions(self):
         expression = self.parse_mandatory_simple()
 
         complex_operator_name = self.parse_optional_complex_operator_name()
@@ -44,13 +43,13 @@ class _Parser:
 
         return expression
 
-    def parse_optional_complex_operator_name(self):
+    def parse_optional_complex_operator_name(self) -> str:
         return self.parser.consume_optional_constant_string_that_must_be_unquoted_and_equal(
             self.complex_expressions_keys)
 
     def complex_operator_sequence_for_single_operator(self,
                                                       complex_operator_name: str,
-                                                      first_expression: NamedElementResolver) -> NamedElementResolver:
+                                                      first_expression):
         single_accepted_operator = [complex_operator_name]
 
         expressions = [first_expression]
@@ -65,12 +64,12 @@ class _Parser:
 
         return self.grammar.complex_expressions[complex_operator_name].mk_complex(expressions)
 
-    def parse_mandatory_simple(self) -> NamedElementResolver:
+    def parse_mandatory_simple(self):
         return self.parser.parse_mandatory_string_that_must_be_unquoted(self.grammar.concept.name.singular,
                                                                         self.parse_simple,
                                                                         must_be_on_current_line=True)
 
-    def parse_simple(self, selector_name: str) -> NamedElementResolver:
+    def parse_simple(self, selector_name: str):
         if selector_name in self.grammar.simple_expressions:
             return self.grammar.simple_expressions[selector_name].parse_arguments(self.parser)
         elif not symbol_syntax.is_symbol_name(selector_name):
