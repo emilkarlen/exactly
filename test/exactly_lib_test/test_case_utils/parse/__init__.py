22c5bd4be93568936ab594785b58440b1467d344
Emil Karlén <emil.karlen@etraveli.com>
2017-08-28 20:47:05 +0200

    file-trans: ExpressionParser: Test of syntax documentation

--- test/exactly_lib_test/test_case_utils/parse/__init__.py
+++ test/exactly_lib_test/test_case_utils/parse/__init__.py
@@ -1,6 +1,7 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.parse import expression_parser, parse_file_selector
+from exactly_lib_test.test_case_utils.parse import expression
+from exactly_lib_test.test_case_utils.parse import parse_file_selector
 from exactly_lib_test.test_case_utils.parse import symbol_syntax, misc_utils, parse_string, parse_here_document, \
     parse_list, parse_file_ref, parse_here_doc_or_file_ref
 
@@ -14,7 +15,7 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(parse_file_ref.suite())
     ret_val.addTest(parse_here_document.suite())
     ret_val.addTest(parse_here_doc_or_file_ref.suite())
-    ret_val.addTest(expression_parser.suite())
+    ret_val.addTest(expression.suite())
     ret_val.addTest(parse_file_selector.suite())
     return ret_val
 
---
+++ test/exactly_lib_test/test_case_utils/parse/expression/syntax_documentation.py
@@ -0,0 +1,22 @@
+import unittest
+
+from exactly_lib.test_case_utils.parse.expression import syntax_documentation as sut
+from exactly_lib_test.common.help.test_resources.syntax_contents_structure_assertions import \
+    is_syntax_element_description
+from .test_resources import ast
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(Test),
+    ])
+
+
+class Test(unittest.TestCase):
+    def test_syntax_element_description(self):
+        actual = sut.syntax_element_description(ast.GRAMMAR_WITH_ALL_COMPONENTS)
+        is_syntax_element_description.apply_without_message(self, actual)
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
