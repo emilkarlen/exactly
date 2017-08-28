47810440ae5bb395e63ad1440d56452bb582891f
Emil Karlén <emil.karlen@etraveli.com>
2017-08-29 01:54:21 +0200

    file-trans: ExpressionParser: REFACT: Rename of module

--- test/exactly_lib_test/test_case_utils/parse/expression/__init__.py
+++ test/exactly_lib_test/test_case_utils/parse/expression/__init__.py
@@ -1,12 +1,12 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.parse.expression import syntax_documentation, expression_parser
+from exactly_lib_test.test_case_utils.parse.expression import syntax_documentation, parser
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(syntax_documentation.suite())
-    ret_val.addTest(expression_parser.suite())
+    ret_val.addTest(parser.suite())
     return ret_val
 
 
