427c1cacc5a8c2ecfe8ecc7304fdd5e45aaf91f3
Emil Karlén <emil.karlen@fripost.org>
2017-08-30 00:12:03 +0200

    file-trans: REFACT: Moves test resources to new package

--- test/exactly_lib_test/test_case_utils/lines_transformers/__init__.py
+++ test/exactly_lib_test/test_case_utils/lines_transformers/__init__.py
@@ -2,12 +2,15 @@ import unittest
 
 from exactly_lib_test.test_case_utils.lines_transformers import parse_lines_transformer
 from exactly_lib_test.test_case_utils.lines_transformers import sequence, env_vars_replacement, replace
+from exactly_lib_test.test_case_utils.lines_transformers import test_resources_test
 from exactly_lib_test.test_case_utils.lines_transformers import vistor
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(vistor.suite())
+    # Test resources use the visitor, that is tested above
+    ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(sequence.suite())
     ret_val.addTest(env_vars_replacement.suite())
     ret_val.addTest(replace.suite())
