8ed5371dd3aad695dc558df865dfbea25dcbb369
Emil Karlén <emil.karlen@etraveli.com>
2017-08-29 14:20:21 +0200

    file-trans: New transformer: sequence: impl no sub-transformers

--- test/exactly_lib_test/test_case_utils/lines_transformers/sequence.py
+++ test/exactly_lib_test/test_case_utils/lines_transformers/sequence.py
@@ -1,6 +1,7 @@
 import unittest
 
 from exactly_lib.type_system_values.lines_transformer import IdentityLinesTransformer, SequenceLinesTransformer
+from exactly_lib_test.test_case_file_structure.test_resources.paths import fake_home_and_sds
 
 
 def suite() -> unittest.TestSuite:
@@ -25,3 +26,20 @@ class Test(unittest.TestCase):
 
         self.assertEqual(transformers_given_to_constructor,
                          list(transformers_from_sequence))
+
+    def test_WHEN_sequence_of_transformers_is_empty_THEN_output_SHOULD_be_equal_to_input(self):
+        # ARRANGE #
+        tcds = fake_home_and_sds()
+        sequence = SequenceLinesTransformer([])
+
+        input_lines = ['first',
+                       'second',
+                       'third']
+        input_iter = iter(input_lines)
+        # ACT #
+        output = sequence.transform(tcds, input_iter)
+        # ASSERT #
+        output_lines = list(output)
+
+        self.assertEqual(input_lines,
+                         output_lines)
