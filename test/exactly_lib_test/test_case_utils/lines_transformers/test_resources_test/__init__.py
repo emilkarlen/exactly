4825126d0144bdbdbc70473d5608d7ee782f5833
Emil Karlén <emil.karlen@etraveli.com>
2017-08-30 00:22:38 +0200

    file-trans: REFACT: Test resource for asrt on line-trans: rename

--- test/exactly_lib_test/test_case_utils/lines_transformers/test_resources_test/__init__.py
+++ test/exactly_lib_test/test_case_utils/lines_transformers/test_resources_test/__init__.py
@@ -1,10 +1,10 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.lines_transformers.test_resources_test import lines_transformer_assertions
+from exactly_lib_test.test_case_utils.lines_transformers.test_resources_test import value_assertions
 
 
 def suite() -> unittest.TestSuite:
-    return lines_transformer_assertions.suite()
+    return value_assertions.suite()
 
 
 if __name__ == '__main__':
