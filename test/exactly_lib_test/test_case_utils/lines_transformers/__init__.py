e19abdbcc770c6989323e9f08b99cf930793c083
Emil Karlén <emil.karlen@fripost.org>
2017-08-29 20:10:29 +0200

    file-trans: Adds replace transformer

--- test/exactly_lib_test/test_case_utils/lines_transformers/__init__.py
+++ test/exactly_lib_test/test_case_utils/lines_transformers/__init__.py
@@ -1,6 +1,6 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.lines_transformers import sequence, env_vars_replacement
+from exactly_lib_test.test_case_utils.lines_transformers import sequence, env_vars_replacement, replace
 from exactly_lib_test.test_case_utils.lines_transformers import vistor
 
 
@@ -9,6 +9,7 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(vistor.suite())
     ret_val.addTest(sequence.suite())
     ret_val.addTest(env_vars_replacement.suite())
+    ret_val.addTest(replace.suite())
     return ret_val
 
 
