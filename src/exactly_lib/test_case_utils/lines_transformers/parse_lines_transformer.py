7c0bd7da05eb5561fcb41712622164b5ab7751a1
Emil Karlén <emil.karlen@etraveli.com>
2017-08-29 22:03:28 +0200

    file-trans: parser: REFACT: Moves resolvers to line-trans package

--- src/exactly_lib/test_case_utils/lines_transformers/parse_lines_transformer.py
+++ src/exactly_lib/test_case_utils/lines_transformers/parse_lines_transformer.py
@@ -1,8 +1,8 @@
-from exactly_lib.named_element.lines_transformers import LinesTransformerConstant
 from exactly_lib.named_element.resolver_structure import LinesTransformerResolver
 from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.section_document.parser_implementations import token_parse
 from exactly_lib.test_case_utils.lines_transformers import custom_transformers as ct
+from exactly_lib.test_case_utils.lines_transformers.resolvers import LinesTransformerConstant
 from exactly_lib.test_case_utils.lines_transformers.transformers import IdentityLinesTransformer
 from exactly_lib.util.cli_syntax import option_syntax
 from exactly_lib.util.cli_syntax.elements import argument as a
