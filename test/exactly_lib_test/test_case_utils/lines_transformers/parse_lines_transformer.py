c6c566bffc2cdfc460b2719497c0526a381dfcf8
Emil Karlén <emil.karlen@fripost.org>
2017-08-29 22:58:32 +0200

    file-trans: parser: Adds skeleton methods

---
+++ test/exactly_lib_test/test_case_utils/lines_transformers/parse_lines_transformer.py
@@ -0,0 +1,13 @@
+import unittest
+
+from exactly_lib.test_case_utils.lines_transformers import parse_lines_transformer as sut
+from exactly_lib_test.test_resources.parse import remaining_source
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.makeSuite(Test)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        sut.parse_lines_transformer(remaining_source(sut.WITH_REPLACED_ENV_VARS_OPTION))
