9d69ee319baa89fe887fbe435f9353fb999e60a9
Emil Karlén <emil.karlen@etraveli.com>
2017-08-29 22:22:00 +0200

    file-trans: Adds value types

---
+++ src/exactly_lib/help_texts/types.py
@@ -0,0 +1,20 @@
+from exactly_lib.help_texts.cross_reference_id import EntityCrossReferenceId
+from exactly_lib.help_texts.entity_names import TYPE_ENTITY_TYPE_NAME
+from exactly_lib.help_texts.name_and_cross_ref import SingularAndPluralNameAndCrossReferenceId, Name
+
+
+def type_cross_ref(type_name: str) -> EntityCrossReferenceId:
+    return EntityCrossReferenceId(TYPE_ENTITY_TYPE_NAME, type_name)
+
+
+def name_and_ref_target(name: Name,
+                        single_line_description_str: str) -> SingularAndPluralNameAndCrossReferenceId:
+    return SingularAndPluralNameAndCrossReferenceId(name,
+                                                    single_line_description_str,
+                                                    type_cross_ref(name.singular))
+
+
+LINES_TRANSFORMER_CONCEPT_INFO = name_and_ref_target(
+    Name('lines transformer', 'lines transformers'),
+    'Transforms the lines of a text file.',
+)
