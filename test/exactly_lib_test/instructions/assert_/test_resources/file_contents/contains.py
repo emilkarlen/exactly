6b13e3474a608787f04f7572e8b1034eb9ab887c
Emil Karlén <emil.karlen@fripost.org>
2017-08-30 22:56:51 +0200

    file-trans: Integrates into contents instructions: contains checker

--- test/exactly_lib_test/instructions/assert_/test_resources/file_contents/contains.py
+++ test/exactly_lib_test/instructions/assert_/test_resources/file_contents/contains.py
@@ -5,8 +5,11 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 from exactly_lib.test_case_file_structure import environment_variables
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_utils.lines_transformers.parse_lines_transformer import WITH_REPLACED_ENV_VARS_OPTION_NAME
+from exactly_lib.test_case_utils.lines_transformers.resolvers import LinesTransformerConstant
 from exactly_lib.util.cli_syntax.option_syntax import option_syntax
 from exactly_lib.util.string import lines_content
+from exactly_lib.util.symbol_table import SymbolTable
+from exactly_lib_test.instructions.assert_.test_resources.file_contents import contents_transformation
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.instruction_test_configuration import \
     InstructionTestConfigurationForContentsOrEquals, \
     TestWithConfigurationAndNegationArgumentBase, suite_for__conf__not_argument, args
@@ -14,6 +17,10 @@ from exactly_lib_test.instructions.assert_.test_resources.file_contents.relativi
     MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY
 from exactly_lib_test.instructions.assert_.test_resources.instruction_check import Expectation
 from exactly_lib_test.instructions.test_resources.arrangements import ActResultProducer
+from exactly_lib_test.named_element.test_resources.lines_transformer import is_lines_transformer_reference_to
+from exactly_lib_test.named_element.test_resources.named_elem_utils import container
+from exactly_lib_test.test_resources.name_and_value import NameAndValue
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 
 
 def suite_for(configuration: InstructionTestConfigurationForContentsOrEquals) -> unittest.TestSuite:
@@ -26,8 +33,7 @@ def suite_for(configuration: InstructionTestConfigurationForContentsOrEquals) ->
         _ALineMatchesRegEx,
         _AWholeLineMatchesRegEx,
 
-        _ShouldReplaceEnvVarsWhenOptionIsGiven,
-        _ShouldNotReplaceEnvVarsWhenOptionIsNotGiven,
+        _WhenLinesTransformerIsGivenThenComparisonShouldBeAppliedToTransformedContents,
     ]
     return suite_for__conf__not_argument(configuration, test_cases)
 
@@ -157,3 +163,50 @@ class _ShouldNotReplaceEnvVarsWhenOptionIsNotGiven(TestWithConfigurationAndNegat
 
 
 _WITH_REPLACED_ENV_VARS_OPTION = option_syntax(WITH_REPLACED_ENV_VARS_OPTION_NAME)
+
+
+class _WhenLinesTransformerIsGivenThenComparisonShouldBeAppliedToTransformedContents(
+    TestWithConfigurationAndNegationArgumentBase):
+    def runTest(self):
+        # ARRANGE #
+        named_transformer = NameAndValue('the_transformer',
+                                         LinesTransformerConstant(
+                                             contents_transformation.ToUppercaseLinesTransformer()))
+
+        contents_generator = contents_transformation.TransformedContentsSetup(
+            original='some\ntext',
+            transformed='SOME\nTEXT',
+        )
+
+        symbols = SymbolTable({
+            named_transformer.name: container(named_transformer.value)
+        })
+
+        expected_symbol_reference_to_transformer = is_lines_transformer_reference_to(named_transformer.name)
+
+        expected_symbol_usages = asrt.matches_sequence([
+            expected_symbol_reference_to_transformer
+        ])
+
+        reg_ex_that_matches_uppercase_character = '[A-Z]'
+
+        self._check_single_instruction_line_with_source_variants(
+            instruction_argument=
+            self.configuration.first_line_argument(
+                args("{transform_option} {transformer} {maybe_not} {contains} '{reg_ex}'",
+                     transformer=named_transformer.name,
+                     maybe_not=self.maybe_not.nothing__if_positive__not_option__if_negative,
+                     reg_ex=reg_ex_that_matches_uppercase_character,
+                     )),
+            arrangement=
+            self.configuration.arrangement_for_contents_from_fun(
+                contents_generator.contents_before_replacement,
+                post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY,
+                symbols=symbols,
+            ),
+            expectation=
+            Expectation(
+                main_result=self.maybe_not.pass__if_positive__fail__if_negative,
+                symbol_usages=expected_symbol_usages,
+            ),
+        )
