9cec6d7a096d9e318829ea065f5485fe3e6b328c
Emil Karlén <emil.karlen@etraveli.com>
2017-08-31 00:35:57 +0200

    file-trans: Integrates into contents instructions: empty checker
    adds test case with transformer that deletes all file contents

--- test/exactly_lib_test/instructions/assert_/test_resources/file_contents/empty.py
+++ test/exactly_lib_test/instructions/assert_/test_resources/file_contents/empty.py
@@ -2,6 +2,10 @@ import unittest
 
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
+from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_utils.lines_transformers.resolvers import LinesTransformerConstant
+from exactly_lib.type_system_values.lines_transformer import LinesTransformer
+from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.instruction_test_configuration import \
     args, InstructionTestConfigurationForContentsOrEquals, TestWithConfigurationAndNegationArgumentBase, \
     suite_for__conf__not_argument
@@ -10,6 +14,10 @@ from exactly_lib_test.instructions.assert_.test_resources.file_contents.relativi
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.transformations import \
     TRANSFORMER_OPTION_ALTERNATIVES
 from exactly_lib_test.instructions.assert_.test_resources.instruction_check import Expectation
+from exactly_lib_test.named_element.test_resources.lines_transformer import is_lines_transformer_reference_to
+from exactly_lib_test.named_element.test_resources.named_elem_utils import container
+from exactly_lib_test.test_resources.name_and_value import NameAndValue
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 
 
 def suite_for(configuration: InstructionTestConfigurationForContentsOrEquals) -> unittest.TestSuite:
@@ -18,6 +26,8 @@ def suite_for(configuration: InstructionTestConfigurationForContentsOrEquals) ->
         ParseShouldFailWhenThereAreSuperfluousArgumentsInFormOfValidHereDocument,
         ActualFileIsEmpty,
         ActualFileIsNonEmpty,
+
+        ActualFileIsEmptyAfterTransformation,
     ]
     return suite_for__conf__not_argument(configuration, test_cases)
 
@@ -82,3 +92,42 @@ class ActualFileIsNonEmpty(TestWithConfigurationAndNegationArgumentBase):
                         post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY),
                     Expectation(main_result=self.maybe_not.fail__if_positive__pass_if_negative),
                 )
+
+
+class ActualFileIsEmptyAfterTransformation(TestWithConfigurationAndNegationArgumentBase):
+    def runTest(self):
+        # ARRANGE #
+        named_transformer = NameAndValue('the_transformer',
+                                         LinesTransformerConstant(
+                                             DeleteEverythingLinesTransformer()))
+
+        original_file_contents = 'some\ntext'
+
+        symbols = SymbolTable({
+            named_transformer.name: container(named_transformer.value)
+        })
+
+        expected_symbol_reference_to_transformer = is_lines_transformer_reference_to(named_transformer.name)
+
+        expected_symbol_usages = asrt.matches_sequence([expected_symbol_reference_to_transformer])
+
+        self._check_single_instruction_line_with_source_variants(
+            self.configuration.first_line_argument(
+                args('{transform_option} {the_transformer} {maybe_not} {empty}',
+                     the_transformer=named_transformer.name,
+                     maybe_not=self.maybe_not.nothing__if_positive__not_option__if_negative)),
+            self.configuration.arrangement_for_contents(
+                original_file_contents,
+                post_sds_population_action=MK_SUB_DIR_OF_ACT_AND_MAKE_IT_CURRENT_DIRECTORY,
+                symbols=symbols),
+            Expectation(
+                main_result=self.maybe_not.pass__if_positive__fail__if_negative,
+                symbol_usages=expected_symbol_usages),
+        )
+
+
+class DeleteEverythingLinesTransformer(LinesTransformer):
+    def transform(self,
+                  tcds: HomeAndSds,
+                  lines: iter) -> iter:
+        return map(lambda x: '', lines)
