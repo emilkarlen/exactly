4018fad2f66f42e44ac9fa843537c1b2472ec78d
Emil Karlén <emil.karlen@fripost.org>
2017-08-30 23:20:27 +0200

    file-trans: Integrates into contents instructions: empty checker

--- test/exactly_lib_test/instructions/assert_/test_resources/file_contents/transformations.py
+++ test/exactly_lib_test/instructions/assert_/test_resources/file_contents/transformations.py
@@ -1,8 +1,8 @@
-from exactly_lib.test_case_utils.lines_transformers import parse_lines_transformer
-from exactly_lib.util.cli_syntax.option_syntax import option_syntax
+from exactly_lib_test.test_case_utils.lines_transformers.test_resources import argument_syntax
 
 REPLACE_ENV_VARS_OPTION_ALTERNATIVES = [
     '',
-    option_syntax(
-        parse_lines_transformer.WITH_REPLACED_ENV_VARS_OPTION_NAME),
+    argument_syntax.syntax_for_transformer_option(
+        argument_syntax.syntax_for_replace_transformer('a', 'A')
+    ),
 ]
