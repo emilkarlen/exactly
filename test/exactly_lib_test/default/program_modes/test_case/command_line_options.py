22e617c31cfee2c3d73b70429888f60b0bc052f6
Emil Karlén <emil.karlen@fripost.org>
2017-08-31 19:32:26 +0200

    tc-result-reporting: Adds test of test-case-file-does-not-exist

--- test/exactly_lib_test/default/program_modes/test_case/command_line_options.py
+++ test/exactly_lib_test/default/program_modes/test_case/command_line_options.py
@@ -1,23 +1,32 @@
 import unittest
 
 from exactly_lib.cli import main_program
+from exactly_lib.processing import exit_values
 from exactly_lib_test.default.test_resources.internal_main_program_runner import \
     main_program_runner_with_default_setup__in_same_process
-from exactly_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_without_preprocessor
+from exactly_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_without_preprocessor, \
+    SetupWithJustMainProgramRunner, tests_for_setup_with_just_main_program_runner
 from exactly_lib_test.test_resources.main_program.main_program_check_for_test_case import \
     SetupWithoutPreprocessorAndTestActor
 from exactly_lib_test.test_resources.main_program.main_program_runner import MainProgramRunner
+from exactly_lib_test.test_resources.process import SubProcessResult
+from exactly_lib_test.test_resources.value_assertions import process_result_assertions as asrt_process_result
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.test_resources.value_assertions.process_result_info_assertions import \
     is_process_result_for_exit_code
 
 
 def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
-    tests = [
+    tests_without_preprocessor = [
         InvalidCommandLineOptionShouldExitWithInvalidUsageStatus(),
     ]
+    tests_with_just_main_program_runner = [
+        TestCaseFileDoesNotExistShouldExitWithInvalidUsageStatus(),
+    ]
     ret_val = unittest.TestSuite()
-    ret_val.addTest(tests_for_setup_without_preprocessor(tests, main_program_runner))
+    ret_val.addTest(tests_for_setup_without_preprocessor(tests_without_preprocessor, main_program_runner))
+    ret_val.addTest(
+        tests_for_setup_with_just_main_program_runner(tests_with_just_main_program_runner, main_program_runner))
     return ret_val
 
 
@@ -34,6 +43,17 @@ class InvalidCommandLineOptionShouldExitWithInvalidUsageStatus(SetupWithoutPrepr
 
     def test_case(self) -> str:
         return ''
+
+
+class TestCaseFileDoesNotExistShouldExitWithInvalidUsageStatus(SetupWithJustMainProgramRunner):
+    def arguments(self) -> list:
+        return ['name-of-non-existing-test.case']
+
+    def check(self,
+              put: unittest.TestCase,
+              actual_result: SubProcessResult):
+        expectation = asrt_process_result.is_result_for_exit_value(exit_values.NO_EXECUTION__FILE_ACCESS_ERROR)
+        expectation.apply_without_message(put, actual_result)
 
 
 if __name__ == '__main__':
