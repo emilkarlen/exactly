b400af3d40bf7e2b1a65616632123820a5c9c392
Emil Karlén <emil.karlen@fripost.org>
2017-08-31 15:57:54 +0200

    suite-conf-for-case: REFACT: Implements separate reporters for separate output types

--- src/exactly_lib/cli/program_modes/test_case/execution.py
+++ src/exactly_lib/cli/program_modes/test_case/execution.py
@@ -1,5 +1,5 @@
-from exactly_lib.cli.program_modes.test_case.result_reporting import ResultReporter
-from exactly_lib.cli.program_modes.test_case.settings import Output, TestCaseExecutionSettings
+from exactly_lib.cli.program_modes.test_case import result_reporting
+from exactly_lib.cli.program_modes.test_case.settings import TestCaseExecutionSettings
 from exactly_lib.processing import test_case_processing, processors
 from exactly_lib.processing.processors import TestCaseDefinition
 from exactly_lib.util.std import StdOutputFiles
@@ -7,24 +7,18 @@ from exactly_lib.util.std import StdOutputFiles
 
 class Executor:
     def __init__(self,
-                 output: StdOutputFiles,
+                 std_output_files: StdOutputFiles,
                  test_case_definition: TestCaseDefinition,
                  settings: TestCaseExecutionSettings):
         self._test_case_definition = test_case_definition
         self._settings = settings
-        self._result_reporter = ResultReporter(output,
-                                               settings.output)
+        self._result_reporter = get_reporter(std_output_files,
+                                             settings)
 
     def execute(self) -> int:
-        is_keep_sandbox = self._derive_should_keep_sandbox()
+        is_keep_sandbox = self._settings.is_keep_sandbox or self._result_reporter.depends_on_result_in_sandbox()
         result = self._process(is_keep_sandbox)
         return self._result_reporter.report(result)
-
-    def _derive_should_keep_sandbox(self) -> bool:
-        if self._settings.output is Output.ACT_PHASE_OUTPUT:
-            return True
-        else:
-            return self._settings.is_keep_sandbox
 
     def _process(self,
                  is_keep_sds: bool) -> test_case_processing.Result:
@@ -34,3 +28,8 @@ class Executor:
                                                  self._settings.sandbox_directory_root_name_prefix)
         processor = processors.new_processor_that_is_allowed_to_pollute_current_process(configuration)
         return processor.apply(test_case_processing.TestCaseSetup(self._settings.file_path))
+
+
+def get_reporter(std_output_files: StdOutputFiles,
+                 settings: TestCaseExecutionSettings) -> result_reporting.ResultReporter:
+    return result_reporting.RESULT_REPORTERS[settings.output](std_output_files)
