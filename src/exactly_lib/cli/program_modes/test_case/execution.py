2820287600198cdc41d6b05fb9fd58acaf473615
Emil Karlén <emil.karlen@fripost.org>
2017-08-31 20:12:38 +0200

    tc-result-reporting: REFACT: Removes unnecessary class
    now when ResultReporter does a lot of the job

--- src/exactly_lib/cli/program_modes/test_case/execution.py
+++ src/exactly_lib/cli/program_modes/test_case/execution.py
@@ -5,29 +5,28 @@ from exactly_lib.processing.processors import TestCaseDefinition
 from exactly_lib.util.std import StdOutputFiles
 
 
-class Executor:
-    def __init__(self,
-                 std_output_files: StdOutputFiles,
-                 test_case_definition: TestCaseDefinition,
-                 settings: TestCaseExecutionSettings):
-        self._test_case_definition = test_case_definition
-        self._settings = settings
-        self._result_reporter = get_reporter(std_output_files,
-                                             settings)
+def execute(std_output_files: StdOutputFiles,
+            test_case_definition: TestCaseDefinition,
+            settings: TestCaseExecutionSettings) -> int:
+    result_reporter = get_reporter(std_output_files,
+                                   settings)
+    is_keep_sandbox = result_reporter.depends_on_result_in_sandbox()
+    result = _process(is_keep_sandbox,
+                      test_case_definition,
+                      settings)
+    return result_reporter.report(result)
 
-    def execute(self) -> int:
-        is_keep_sandbox = self._result_reporter.depends_on_result_in_sandbox()
-        result = self._process(is_keep_sandbox)
-        return self._result_reporter.report(result)
 
-    def _process(self,
-                 is_keep_sds: bool) -> test_case_processing.Result:
-        configuration = processors.Configuration(self._test_case_definition,
-                                                 self._settings.handling_setup,
-                                                 is_keep_sds,
-                                                 self._settings.sandbox_directory_root_name_prefix)
-        processor = processors.new_processor_that_is_allowed_to_pollute_current_process(configuration)
-        return processor.apply(test_case_processing.TestCaseSetup(self._settings.file_path))
+def _process(is_keep_sds: bool,
+             test_case_definition: TestCaseDefinition,
+             settings: TestCaseExecutionSettings
+             ) -> test_case_processing.Result:
+    configuration = processors.Configuration(test_case_definition,
+                                             settings.handling_setup,
+                                             is_keep_sds,
+                                             settings.sandbox_directory_root_name_prefix)
+    processor = processors.new_processor_that_is_allowed_to_pollute_current_process(configuration)
+    return processor.apply(test_case_processing.TestCaseSetup(settings.file_path))
 
 
 def get_reporter(std_output_files: StdOutputFiles,
