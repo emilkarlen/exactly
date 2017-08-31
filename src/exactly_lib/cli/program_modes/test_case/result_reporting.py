80f07ed2ae02892ae17fc9c833c3d7466cdbd228
Emil Karlén <emil.karlen@etraveli.com>
2017-08-31 17:19:14 +0200

    tc-result-reporting: --act: Normal reporting when tc cannot be executed

--- src/exactly_lib/cli/program_modes/test_case/result_reporting.py
+++ src/exactly_lib/cli/program_modes/test_case/result_reporting.py
@@ -4,10 +4,62 @@ import shutil
 from exactly_lib.cli.program_modes.test_case.settings import Output
 from exactly_lib.common.exit_value import ExitValue
 from exactly_lib.execution import full_execution
+from exactly_lib.execution.result import FullResultStatus, FullResult
 from exactly_lib.execution.result_reporting import print_error_message_for_full_result, print_error_info
 from exactly_lib.processing import test_case_processing, exit_values
 from exactly_lib.processing.test_case_processing import ErrorInfo
 from exactly_lib.util.std import StdOutputFiles, FilePrinter
+
+FULL_EXECUTION__SKIPPED = {FullResultStatus.SKIPPED}
+
+FULL_EXECUTION__VALIDATE = {FullResultStatus.VALIDATE}
+
+FULL_EXECUTION__COMPLETE = {FullResultStatus.PASS,
+                            FullResultStatus.FAIL,
+                            FullResultStatus.XPASS,
+                            FullResultStatus.XFAIL,
+                            }
+
+
+class FullExecutionHandler:
+    """Helper class for handling some different cases of result of test case execution."""
+
+    def __init__(self,
+                 exit_value: ExitValue,
+                 output_files: StdOutputFiles):
+        self.exit_value = exit_value
+        self._std = output_files
+        self._out_printer = FilePrinter(output_files.out)
+        self._err_printer = FilePrinter(output_files.err)
+
+    def handle(self, result: FullResult):
+        status = result.status
+        if status in FULL_EXECUTION__SKIPPED:
+            return self.skipped(result)
+        elif status in FULL_EXECUTION__VALIDATE:
+            return self.validation(result)
+        elif status in FULL_EXECUTION__COMPLETE:
+            return self.complete(result)
+        else:
+            return self.hard_error_or_implementation_error(result)
+
+    def complete(self, result: FullResult):
+        raise NotImplementedError('abstract method')
+
+    def skipped(self, result: FullResult):
+        return self._default_non_complete_execution(result)
+
+    def validation(self, result: FullResult):
+        return self._default_non_complete_execution(result)
+
+    def hard_error_or_implementation_error(self, result: FullResult):
+        return self._default_non_complete_execution(result)
+
+    def _default_non_complete_execution(self, result: FullResult) -> int:
+        return _report_exit_value_and_optional_error_message_for_full_execution(self.exit_value,
+                                                                                result,
+                                                                                self._out_printer,
+                                                                                self._err_printer)
 
 
 class ResultReporter:
@@ -59,6 +111,15 @@ class ResultReporterWithExitCodeFromExitValue(ResultReporter):
         raise NotImplementedError('abstract method')
 
 
+def _report_exit_value_and_optional_error_message_for_full_execution(exit_value: ExitValue,
+                                                                     result: full_execution.FullResult,
+                                                                     stdout_printer: FilePrinter,
+                                                                     stderr_printer: FilePrinter) -> int:
+    stdout_printer.write_line(result.status.name)
+    print_error_message_for_full_result(stderr_printer, result)
+    return exit_value.exit_code
+
+
 class ResultReporterForNormalOutput(ResultReporterWithExitCodeFromExitValue):
     """Reports the result of the execution via exitcode, stdout, stderr."""
 
@@ -73,15 +134,8 @@ class ResultReporterForPreserveAndPrintSandboxDir(ResultReporterWithExitCodeFrom
         self._out_printer.write_line(str(the_full_result.sds.root_dir))
 
 
-class ResultReporterForActPhaseOutput(ResultReporter):
-    """Reports the result of the execution via exitcode, stdout, stderr."""
-
-    def depends_on_result_in_sandbox(self) -> bool:
-        return True
-
-    def report_full_execution(self,
-                              exit_value: ExitValue,
-                              result: full_execution.FullResult) -> int:
+class FullExecutionHandlerForActPhaseOutput(FullExecutionHandler):
+    def complete(self, result: FullResult):
         def copy_file(input_file_path: pathlib.Path,
                       output_file):
             with input_file_path.open() as f:
@@ -98,6 +152,19 @@ class ResultReporterForActPhaseOutput(ResultReporter):
         exit_code = act_phase_exit_code(result.sds.result.exitcode_file)
         shutil.rmtree(str(result.sds.root_dir))
         return exit_code
+
+
+class ResultReporterForActPhaseOutput(ResultReporter):
+    """Reports the result of the execution via exitcode, stdout, stderr."""
+
+    def depends_on_result_in_sandbox(self) -> bool:
+        return True
+
+    def report_full_execution(self,
+                              exit_value: ExitValue,
+                              result: full_execution.FullResult) -> int:
+        handler = FullExecutionHandlerForActPhaseOutput(exit_value, self._std)
+        return handler.handle(result)
 
 
 RESULT_REPORTERS = {
