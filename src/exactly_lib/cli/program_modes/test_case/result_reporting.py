0a8018f5c9f303f42b633495c41aa60209d87218
Emil Karlén <emil.karlen@etraveli.com>
2017-08-31 15:57:54 +0200

    suite-conf-for-case: REFACT: Implements separate reporters for separate output types

--- src/exactly_lib/cli/program_modes/test_case/result_reporting.py
+++ src/exactly_lib/cli/program_modes/test_case/result_reporting.py
@@ -2,6 +2,7 @@ import pathlib
 import shutil
 
 from exactly_lib.cli.program_modes.test_case.settings import Output
+from exactly_lib.common.exit_value import ExitValue
 from exactly_lib.execution import full_execution
 from exactly_lib.execution.result_reporting import print_error_message_for_full_result, print_error_info
 from exactly_lib.processing import test_case_processing, exit_values
@@ -12,30 +13,75 @@ from exactly_lib.util.std import StdOutputFiles, FilePrinter
 class ResultReporter:
     """Reports the result of the execution via exitcode, stdout, stderr."""
 
-    def __init__(self,
-                 output_files: StdOutputFiles,
-                 output_type: Output):
+    def __init__(self, output_files: StdOutputFiles):
         self._std = output_files
         self._out_printer = FilePrinter(output_files.out)
         self._err_printer = FilePrinter(output_files.err)
-        self._output_type = output_type
 
     def report(self, result: test_case_processing.Result) -> int:
-        if self._output_type is Output.ACT_PHASE_OUTPUT:
-            return self.report_result_of_act_phase_execution(result)
-        else:
-            return self.report_result_of_normal_execution(result)
-
-    def report_result_of_normal_execution(self, result: test_case_processing.Result) -> int:
         exit_value = exit_values.from_result(result)
         if result.status is test_case_processing.Status.EXECUTED:
-            self._output_report_of_full_result(result.execution_result)
+            return self.report_full_execution(exit_value, result.execution_result)
         else:
-            self.__output_error_result(exit_value.exit_identifier,
-                                       result.error_info)
+            return self._report_unable_to_execute(exit_value,
+                                                  result.error_info)
+
+    def depends_on_result_in_sandbox(self) -> bool:
+        raise NotImplementedError('abstract method')
+
+    def report_full_execution(self,
+                              exit_value: ExitValue,
+                              result: full_execution.FullResult) -> int:
+        raise NotImplementedError('abstract method')
+
+    def _report_unable_to_execute(self,
+                                  exit_value: ExitValue,
+                                  error_info: ErrorInfo) -> int:
+        self._out_printer.write_line(exit_value.exit_identifier)
+        print_error_info(self._err_printer, error_info)
         return exit_value.exit_code
 
-    def report_result_of_act_phase_execution(self, result: test_case_processing.Result) -> int:
+
+class ResultReporterWithExitCodeFromExitValue(ResultReporter):
+    """Reports the result of the execution via exitcode, stdout, stderr."""
+
+    def depends_on_result_in_sandbox(self) -> bool:
+        return False
+
+    def report_full_execution(self,
+                              exit_value: ExitValue,
+                              result: full_execution.FullResult) -> int:
+        self._print_output_to_stdout_for_full_result(result)
+        print_error_message_for_full_result(self._err_printer, result)
+        return exit_value.exit_code
+
+    def _print_output_to_stdout_for_full_result(self, the_full_result: full_execution.FullResult):
+        raise NotImplementedError('abstract method')
+
+
+class ResultReporterForNormalOutput(ResultReporterWithExitCodeFromExitValue):
+    """Reports the result of the execution via exitcode, stdout, stderr."""
+
+    def _print_output_to_stdout_for_full_result(self, the_full_result: full_execution.FullResult):
+        self._out_printer.write_line(the_full_result.status.name)
+
+
+class ResultReporterForPreserveAndPrintSandboxDir(ResultReporterWithExitCodeFromExitValue):
+    """Reports the result of the execution via exitcode, stdout, stderr."""
+
+    def _print_output_to_stdout_for_full_result(self, the_full_result: full_execution.FullResult):
+        self._out_printer.write_line(str(the_full_result.sds.root_dir))
+
+
+class ResultReporterForActPhaseOutput(ResultReporter):
+    """Reports the result of the execution via exitcode, stdout, stderr."""
+
+    def depends_on_result_in_sandbox(self) -> bool:
+        return True
+
+    def report_full_execution(self,
+                              exit_value: ExitValue,
+                              result: full_execution.FullResult) -> int:
         def copy_file(input_file_path: pathlib.Path,
                       output_file):
             with input_file_path.open() as f:
@@ -47,26 +93,15 @@ class ResultReporter:
                 exit_code_string = f.read()
                 return int(exit_code_string)
 
-        full_result = result.execution_result
-
-        copy_file(full_result.sds.result.stdout_file, self._std.out)
-        copy_file(full_result.sds.result.stderr_file, self._std.err)
-        exit_code = act_phase_exit_code(full_result.sds.result.exitcode_file)
-        shutil.rmtree(str(full_result.sds.root_dir))
+        copy_file(result.sds.result.stdout_file, self._std.out)
+        copy_file(result.sds.result.stderr_file, self._std.err)
+        exit_code = act_phase_exit_code(result.sds.result.exitcode_file)
+        shutil.rmtree(str(result.sds.root_dir))
         return exit_code
 
-    def __output_error_result(self,
-                              stdout_error_code: str,
-                              error_info: ErrorInfo):
-        self._out_printer.write_line(stdout_error_code)
-        print_error_info(self._err_printer, error_info)
 
-    def _output_report_of_full_result(self, the_full_result: full_execution.FullResult):
-        self._print_output_to_stdout_for_full_result(the_full_result)
-        print_error_message_for_full_result(self._err_printer, the_full_result)
-
-    def _print_output_to_stdout_for_full_result(self, the_full_result: full_execution.FullResult):
-        if self._output_type is Output.STATUS_CODE:
-            self._out_printer.write_line(the_full_result.status.name)
-        elif self._output_type is Output.SANDBOX_DIRECTORY_STRUCTURE_ROOT:
-            self._out_printer.write_line(str(the_full_result.sds.root_dir))
+RESULT_REPORTERS = {
+    Output.STATUS_CODE: ResultReporterForNormalOutput,
+    Output.SANDBOX_DIRECTORY_STRUCTURE_ROOT: ResultReporterForPreserveAndPrintSandboxDir,
+    Output.ACT_PHASE_OUTPUT: ResultReporterForActPhaseOutput,
+}
