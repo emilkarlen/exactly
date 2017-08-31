263fbea5fda63711e9d8ef9f34a0369bed3eaadb
Emil Karlén <emil.karlen@etraveli.com>
2017-08-31 13:19:46 +0200

    suite-conf-for-case: REFACT: Moves test resources to case / suite packages

--- test/exactly_lib_test/default/program_modes/test_case/preprocessing.py
+++ test/exactly_lib_test/default/program_modes/test_case/preprocessing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from exactly_lib_test.default.test_resources import default_main_program_case_preprocessing
+from exactly_lib_test.default.program_modes.test_case.test_resources import preprocessing_tests
 from exactly_lib_test.default.test_resources.internal_main_program_runner import \
     main_program_runner_with_default_setup__in_same_process
 from exactly_lib_test.test_resources.main_program.main_program_check_base import tests_for_setup_with_preprocessor
@@ -16,9 +16,10 @@ def suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
 def suite() -> unittest.TestSuite:
     return suite_for(main_program_runner_with_default_setup__in_same_process())
 
+
 TESTS = [
-    default_main_program_case_preprocessing.TransformationIntoTestCaseThatPass(),
-    default_main_program_case_preprocessing.TransformationIntoTestCaseThatParserError(),
+    preprocessing_tests.TransformationIntoTestCaseThatPass(),
+    preprocessing_tests.TransformationIntoTestCaseThatParserError(),
 ]
 
 if __name__ == '__main__':
--- test/exactly_lib_test/default/program_modes/test_suite/integration_tests.py
+++ test/exactly_lib_test/default/program_modes/test_suite/integration_tests.py
@@ -6,7 +6,8 @@ from exactly_lib.execution.exit_values import NO_EXECUTION__SYNTAX_ERROR, \
     EXECUTION__PASS
 from exactly_lib.test_suite import exit_values
 from exactly_lib.util.string import lines_content
-from exactly_lib_test.default.test_resources import default_main_program_suite_preprocessing as pre_proc_tests
+from exactly_lib_test.default.program_modes.test_suite.test_resources import \
+    default_main_program_suite_preprocessing as pre_proc_tests
 from exactly_lib_test.default.test_resources.internal_main_program_runner import \
     main_program_runner_with_default_setup__in_same_process
 from exactly_lib_test.test_resources.file_structure import DirContents, File
