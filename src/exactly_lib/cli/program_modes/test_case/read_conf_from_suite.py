e5c2acfeeb0b1d1b3f92554dd9b4144ce1c4fdb8
Emil Karlén <emil.karlen@fripost.org>
2017-09-01 23:03:20 +0200

    use-suite-conf: REFACT: Reads just configuration from suite
    not test cases and sub suites

--- src/exactly_lib/cli/program_modes/test_case/read_conf_from_suite.py
+++ src/exactly_lib/cli/program_modes/test_case/read_conf_from_suite.py
@@ -1,20 +1,17 @@
 import pathlib
 
 from exactly_lib.processing.test_case_handling_setup import TestCaseHandlingSetup
-from exactly_lib.test_suite import suite_hierarchy_reading
-from exactly_lib.test_suite.instruction_set.test_suite_definition import TestSuiteDefinition
+from exactly_lib.section_document import document_parser
+from exactly_lib.test_suite import suite_file_reading
 
 
 def resolve_handling_setup_from_suite(default_handling_setup: TestCaseHandlingSetup,
-                                      test_suite_definition: TestSuiteDefinition,
+                                      configuration_section_parser: document_parser.SectionElementParser,
                                       suite_to_read_config_from: pathlib.Path) -> TestCaseHandlingSetup:
     if not suite_to_read_config_from:
         return default_handling_setup
 
-    suite_reader = suite_hierarchy_reading.Reader(
-        suite_hierarchy_reading.Environment(
-            test_suite_definition.configuration_section_parser,
-            default_handling_setup)
-    )
-    test_suite = suite_reader.apply(suite_to_read_config_from)
-    return test_suite.test_case_handling_setup
+    suite_document = suite_file_reading.read_suite_document(suite_to_read_config_from,
+                                                            configuration_section_parser)
+    return suite_file_reading.resolve_test_case_handling_setup(suite_document,
+                                                               default_handling_setup)
