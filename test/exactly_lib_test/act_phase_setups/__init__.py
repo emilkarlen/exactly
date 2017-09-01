70d8cc65d39b73bbef3d7bc33236268e1f4d1c0d
Emil Karlén <emil.karlen@etraveli.com>
2017-09-02 00:40:55 +0200

    null-actor: Adds null actor

--- test/exactly_lib_test/act_phase_setups/__init__.py
+++ test/exactly_lib_test/act_phase_setups/__init__.py
@@ -1,12 +1,13 @@
 import unittest
 
-from exactly_lib_test.act_phase_setups import command_line, source_interpreter, util, file_interpreter
+from exactly_lib_test.act_phase_setups import null, command_line, source_interpreter, util, file_interpreter
 from exactly_lib_test.act_phase_setups.util import executor_made_of_parts
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(executor_made_of_parts.suite())
+    ret_val.addTest(null.suite())
     ret_val.addTest(command_line.suite())
     ret_val.addTest(file_interpreter.suite())
     ret_val.addTest(source_interpreter.suite())
