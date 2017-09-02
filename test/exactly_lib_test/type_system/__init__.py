e5430201b30827676836bbd5f1d731ae0308d4da
Emil Karlén <emil.karlen@fripost.org>
2017-09-02 12:35:00 +0200

    REFACT: Organizes package type_system

--- test/exactly_lib_test/type_system/__init__.py
+++ test/exactly_lib_test/type_system/__init__.py
@@ -1,18 +1,13 @@
 import unittest
 
-from exactly_lib_test.type_system import test_resources_test
+from exactly_lib_test.type_system import data
 from exactly_lib_test.type_system import utils
-from exactly_lib_test.type_system.data import concrete_path_parts, file_refs, list_value, string_value
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(utils.suite())
-    ret_val.addTest(test_resources_test.suite())
-    ret_val.addTest(concrete_path_parts.suite())
-    ret_val.addTest(file_refs.suite())
-    ret_val.addTest(string_value.suite())
-    ret_val.addTest(list_value.suite())
+    ret_val.addTest(data.suite())
     return ret_val
 
 
---
+++ test/exactly_lib_test/type_system/data/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+from exactly_lib_test.type_system.data import concrete_path_parts, file_refs, list_value, string_value
+from exactly_lib_test.type_system.data import test_resources_test
+
+
+def suite() -> unittest.TestSuite:
+    ret_val = unittest.TestSuite()
+    ret_val.addTest(test_resources_test.suite())
+    ret_val.addTest(concrete_path_parts.suite())
+    ret_val.addTest(file_refs.suite())
+    ret_val.addTest(string_value.suite())
+    ret_val.addTest(list_value.suite())
+    return ret_val
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
