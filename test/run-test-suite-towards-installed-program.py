f0ec45f19995c94b75967e384b0d2551016cbe23
Emil Karlén <emil.karlen@fripost.org>
2017-09-02 22:36:36 +0200

    test resources: REFACT: Moves main program runners to better locations

--- test/run-test-suite-towards-installed-program.py
+++ test/run-test-suite-towards-installed-program.py
@@ -5,7 +5,7 @@ import unittest
 
 import complete_test_suite
 from exactly_lib import program_info
-from exactly_lib_test.test_resources.cli_main_program_via_sub_process_utils.main_program_runner import \
+from exactly_lib_test.default.test_resources.installed_program_main_program_runner import \
     RunInstalledProgramViaOsInSubProcess
 
 # This value is from setup.py
