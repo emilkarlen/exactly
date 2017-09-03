ef64ed9f2ed674401051d6258364a871ab9c4985
Emil Karlén <emil.karlen@etraveli.com>
2017-09-03 18:32:37 +0200

    parse str/hd/path: Parsing of strings

--- src/exactly_lib/test_case_utils/parse/parse_here_doc_or_file_ref.py
+++ src/exactly_lib/test_case_utils/parse/parse_here_doc_or_file_ref.py
@@ -6,7 +6,7 @@ from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
 from exactly_lib.test_case_utils.parse import parse_here_document, parse_file_ref
-from exactly_lib.test_case_utils.parse.parse_here_document import HereDocumentContentsParsingException
+from exactly_lib.test_case_utils.parse import parse_string
 from exactly_lib.test_case_utils.parse.rel_opts_configuration import RelOptionArgumentConfiguration
 from exactly_lib.util.cli_syntax import option_parsing
 from exactly_lib.util.cli_syntax.elements import argument as a
@@ -68,26 +68,21 @@ class StringResolverOrFileRef(tuple):
 def parse_from_parse_source(source: ParseSource,
                             conf: RelOptionArgumentConfiguration = CONFIGURATION) -> StringResolverOrFileRef:
     source.consume_initial_space_on_current_line()
-    try:
-        copy_of_source = source.copy
-        here_doc = parse_here_document.parse_as_last_argument(False, copy_of_source)
-        source.catch_up_with(copy_of_source)
-        if source.is_at_eol__except_for_space:
-            source.consume_current_line()
+    if source.is_at_eol:
+        return _raise_ex(source)
+    remaining_part_of_current_line = source.remaining_part_of_current_line
+    first_argument = remaining_part_of_current_line.split(maxsplit=1)[0]
+    if option_parsing.matches(FILE_ARGUMENT_OPTION, first_argument):
+        source.consume(len(first_argument))
+        source.consume_initial_space_on_current_line()
+        file_reference = parse_file_ref.parse_file_ref_from_parse_source(source, conf)
+        return StringResolverOrFileRef(SourceType.PATH, None, file_reference)
+    elif first_argument.startswith(parse_here_document.DOCUMENT_MARKER_PREFIX):
+        here_doc = parse_here_document.parse_as_last_argument(False, source)
         return StringResolverOrFileRef(SourceType.HERE_DOC, here_doc, None)
-    except HereDocumentContentsParsingException as ex:
-        raise ex
-    except SingleInstructionInvalidArgumentException:
-        if source.is_at_eol:
-            return _raise_ex(source)
-        first_argument = source.remaining_part_of_current_line.split(maxsplit=1)[0]
-        if option_parsing.matches(FILE_ARGUMENT_OPTION, first_argument):
-            source.consume(len(first_argument))
-            source.consume_initial_space_on_current_line()
-            file_reference = parse_file_ref.parse_file_ref_from_parse_source(source, conf)
-            return StringResolverOrFileRef(SourceType.PATH, None, file_reference)
-        else:
-            return _raise_ex(source)
+    else:
+        string_resolver = parse_string.parse_string_resolver_from_parse_source(source)
+        return StringResolverOrFileRef(SourceType.STRING, string_resolver, None)
 
 
 def _raise_ex(source: ParseSource) -> StringResolverOrFileRef:
