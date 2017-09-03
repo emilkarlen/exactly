2041d568a3742b044aaf7ced0065464f1dd76544
Emil Karlén <emil.karlen@fripost.org>
2017-09-03 16:57:57 +0200

    parse str/hd/path: Adds --file option for file argument

--- src/exactly_lib/test_case_utils/parse/parse_here_doc_or_file_ref.py
+++ src/exactly_lib/test_case_utils/parse/parse_here_doc_or_file_ref.py
@@ -8,11 +8,16 @@ from exactly_lib.section_document.parser_implementations.instruction_parser_for_
 from exactly_lib.test_case_utils.parse import parse_here_document, parse_file_ref
 from exactly_lib.test_case_utils.parse.parse_here_document import HereDocumentContentsParsingException
 from exactly_lib.test_case_utils.parse.rel_opts_configuration import RelOptionArgumentConfiguration
+from exactly_lib.util.cli_syntax import option_parsing
+from exactly_lib.util.cli_syntax.elements import argument as a
 
 CONFIGURATION = parse_file_ref.ALL_REL_OPTIONS_CONFIG
 
+FILE_ARGUMENT_OPTION = a.OptionName(long_name='file')
+
 
 class SourceType(enum.Enum):
+    STRING = 1
     HERE_DOC = 2
     PATH = 3
 
@@ -30,23 +35,29 @@ class StringResolverOrFileRef(tuple):
                                     else file_reference.references)))
 
     @property
-    def is_here_document(self) -> bool:
-        return self.source_type is SourceType.HERE_DOC
-
-    @property
-    def is_file_ref(self) -> bool:
-        return self.source_type is SourceType.PATH
-
-    @property
     def source_type(self) -> SourceType:
         return self[0]
 
     @property
+    def is_file_ref(self) -> bool:
+        """
+        Tells if the source is a path.
+        If not, it is either a string or a here doc accessed via `string_resolver`
+        """
+        return self.source_type is SourceType.PATH
+
+    @property
     def string_resolver(self) -> StringResolver:
+        """
+        :return: Not None iff :class:`SourceType` is NOT `SourceType.PATH`
+        """
         return self[1]
 
     @property
     def file_reference_resolver(self) -> FileRefResolver:
+        """
+        :return: Not None iff :class:`SourceType` is `SourceType.PATH`
+        """
         return self[2]
 
     @property
@@ -56,6 +67,7 @@ class StringResolverOrFileRef(tuple):
 
 def parse_from_parse_source(source: ParseSource,
                             conf: RelOptionArgumentConfiguration = CONFIGURATION) -> StringResolverOrFileRef:
+    source.consume_initial_space_on_current_line()
     try:
         copy_of_source = source.copy
         here_doc = parse_here_document.parse_as_last_argument(False, copy_of_source)
@@ -66,9 +78,18 @@ def parse_from_parse_source(source: ParseSource,
     except HereDocumentContentsParsingException as ex:
         raise ex
     except SingleInstructionInvalidArgumentException:
-        try:
+        if source.is_at_eol:
+            return _raise_ex(source)
+        first_argument = source.remaining_part_of_current_line.split(maxsplit=1)[0]
+        if option_parsing.matches(FILE_ARGUMENT_OPTION, first_argument):
+            source.consume(len(first_argument))
+            source.consume_initial_space_on_current_line()
             file_reference = parse_file_ref.parse_file_ref_from_parse_source(source, conf)
             return StringResolverOrFileRef(SourceType.PATH, None, file_reference)
-        except SingleInstructionInvalidArgumentException:
-            msg = 'Neither a "here document" nor a file reference: {}'.format(source.remaining_part_of_current_line)
-            raise SingleInstructionInvalidArgumentException(msg)
+        else:
+            return _raise_ex(source)
+
+
+def _raise_ex(source: ParseSource) -> StringResolverOrFileRef:
+    msg = 'Neither a "here document" nor a file reference: {}'.format(source.remaining_part_of_current_line)
+    raise SingleInstructionInvalidArgumentException(msg)
