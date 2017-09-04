e048ac577d3b0a10c5413393ad959949494c4824
Emil Karlén <emil.karlen@etraveli.com>
2017-09-04 13:02:47 +0200

    REFACT: Rename File Selector -> File Matcher

--- test/exactly_lib_test/test_case_utils/file_selectors/test_resources_test/__init__.py
+++ test/exactly_lib_test/test_case_utils/file_selectors/test_resources_test/__init__.py
@@ -1,12 +1,12 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.file_selectors.test_resources_test import file_selector_assertions
+from exactly_lib_test.test_case_utils.file_selectors.test_resources_test import file_matcher_assertions
 from exactly_lib_test.test_case_utils.file_selectors.test_resources_test import file_selector_resolver_assertions
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        file_selector_assertions.suite(),
+        file_matcher_assertions.suite(),
         file_selector_resolver_assertions.suite(),
     ])
 
