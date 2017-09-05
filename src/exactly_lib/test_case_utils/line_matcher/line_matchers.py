c67938e45c819a37a707ca57cc94ad47ec5a39b5
Emil Karlén <emil.karlen@fripost.org>
2017-09-05 18:34:47 +0200

    line-matcher: Adds LineMatcherRegex

---
+++ src/exactly_lib/test_case_utils/line_matcher/line_matchers.py
@@ -0,0 +1,31 @@
+from exactly_lib.type_system.logic.line_matcher import LineMatcher
+
+
+class LineMatcherRegex(LineMatcher):
+    """Matches lines that matches a given regex."""
+
+    def __init__(self, compiled_regular_expression):
+        self._compiled_regular_expression = compiled_regular_expression
+
+    def matches(self, line: str) -> bool:
+        raise NotImplementedError('todo')
+
+
+class LineMatcherStructureVisitor:
+    """
+    Visits all variants of :class:`FileMatcher`.
+
+    The existence of this class means that the structure of :class:`FileMatcher`s
+    is fixed. The reason for this is to, among other things, support optimizations
+    of selectors.
+    """
+
+    def visit(self, line_matcher: LineMatcher):
+        if isinstance(line_matcher, LineMatcherRegex):
+            return self.visit_regex(line_matcher)
+        else:
+            raise TypeError('Unknown {}: {}'.format(LineMatcher,
+                                                    str(line_matcher)))
+
+    def visit_regex(self, matcher: LineMatcherRegex):
+        raise NotImplementedError('abstract method')
--- test/exactly_lib_test/test_case_utils/__init__.py
+++ test/exactly_lib_test/test_case_utils/__init__.py
@@ -2,7 +2,7 @@ import unittest
 
 from exactly_lib_test.test_case_utils import expression
 from exactly_lib_test.test_case_utils import file_properties, sub_process_execution, parse
-from exactly_lib_test.test_case_utils import lines_transformers, file_matcher
+from exactly_lib_test.test_case_utils import line_matcher, lines_transformers, file_matcher
 
 
 def suite() -> unittest.TestSuite:
@@ -10,8 +10,9 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(expression.suite())
     ret_val.addTest(file_properties.suite())
     ret_val.addTest(sub_process_execution.suite())
-    ret_val.addTest(lines_transformers.suite())
+    ret_val.addTest(line_matcher.suite())
     ret_val.addTest(file_matcher.suite())
+    ret_val.addTest(lines_transformers.suite())
     ret_val.addTest(parse.suite())
     return ret_val
 
