a4c9fe91f84fda92aacd7e63c8eac23da9ad6475
Emil Karlén <emil.karlen@fripost.org>
2017-09-05 22:02:02 +0200

    line-matcher: Adds resolvers

---
+++ src/exactly_lib/test_case_utils/line_matcher/resolvers.py
@@ -0,0 +1,71 @@
+from exactly_lib.named_element.named_element_usage import NamedElementReference
+from exactly_lib.named_element.object_with_symbol_references import references_from_objects_with_symbol_references
+from exactly_lib.named_element.resolver_structure import LineMatcherResolver
+from exactly_lib.named_element.restriction import ValueTypeRestriction
+from exactly_lib.test_case_utils.line_matcher import line_matchers
+from exactly_lib.type_system.logic.line_matcher import LineMatcher
+from exactly_lib.type_system.value_type import ValueType
+from exactly_lib.util.symbol_table import SymbolTable
+
+
+class LineMatcherConstantResolver(LineMatcherResolver):
+    """
+    A :class:`LineMatcherResolver` that is a constant :class:`LineMatcher`
+    """
+
+    def __init__(self, value: LineMatcher):
+        self._value = value
+
+    def resolve(self, symbols: SymbolTable) -> LineMatcher:
+        return self._value
+
+    @property
+    def references(self) -> list:
+        return []
+
+    def __str__(self):
+        return str(type(self)) + '\'' + str(self._value) + '\''
+
+
+class LineMatcherReferenceResolver(LineMatcherResolver):
+    """
+    A :class:`LineMatcherResolver` that is a reference to a symbol
+    """
+
+    def __init__(self, name_of_referenced_resolver: str):
+        self._name_of_referenced_resolver = name_of_referenced_resolver
+        self._references = [NamedElementReference(name_of_referenced_resolver,
+                                                  ValueTypeRestriction(ValueType.LINE_MATCHER))]
+
+    def resolve(self, symbols: SymbolTable) -> LineMatcher:
+        container = symbols.lookup(self._name_of_referenced_resolver)
+        resolver = container.resolver
+        assert isinstance(resolver, LineMatcherResolver)
+        return resolver.resolve(symbols)
+
+    @property
+    def references(self) -> list:
+        return self._references
+
+    def __str__(self):
+        return str(type(self)) + '\'' + str(self._name_of_referenced_resolver) + '\''
+
+
+class LineMatcherAndResolver(LineMatcherResolver):
+    """
+    Resolver of :class:`LineMatcherAnd`
+    """
+
+    def __init__(self, line_matcher_resolver_list: list):
+        self._resolvers = line_matcher_resolver_list
+        self._references = references_from_objects_with_symbol_references(line_matcher_resolver_list)
+
+    def resolve(self, symbols: SymbolTable) -> LineMatcher:
+        return line_matchers.LineMatcherAnd([
+            resolver.resolve(symbols)
+            for resolver in self._resolvers
+        ])
+
+    @property
+    def references(self) -> list:
+        return self._references
--- test/exactly_lib_test/named_element/test_resources/line_matcher.py
+++ test/exactly_lib_test/named_element/test_resources/line_matcher.py
@@ -8,7 +8,8 @@ from exactly_lib_test.test_resources.value_assertions import value_assertion as 
 
 
 class LineMatcherResolverConstantTestImpl(LineMatcherResolver):
-    def __init__(self, resolved_value: LineMatcher,
+    def __init__(self,
+                 resolved_value: LineMatcher,
                  references: list = ()):
         self._resolved_value = resolved_value
         self._references = list(references)
@@ -25,9 +26,9 @@ class LineMatcherResolverConstantTestImpl(LineMatcherResolver):
         return self._resolved_value
 
 
-IS_LINE_REFERENCE_RESTRICTION = is_value_type_restriction(ValueType.LINE_MATCHER)
+IS_LINE_MATCHER_REFERENCE_RESTRICTION = is_value_type_restriction(ValueType.LINE_MATCHER)
 
 
-def is_file_matcher_reference_to(symbol_name: str) -> asrt.ValueAssertion:
+def is_line_matcher_reference_to(symbol_name: str) -> asrt.ValueAssertion:
     return asrt_ne.matches_reference(asrt.equals(symbol_name),
-                                     IS_LINE_REFERENCE_RESTRICTION)
+                                     IS_LINE_MATCHER_REFERENCE_RESTRICTION)
