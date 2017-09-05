bc94bd20422bbd2c6149c8ab28a6966bbf8c6d4a
Emil Karlén <emil.karlen@etraveli.com>
2017-09-05 19:21:40 +0200

    line-matcher: Adds test resources: value_assertions

--- test/exactly_lib_test/test_case_utils/line_matcher/__init__.py
+++ test/exactly_lib_test/test_case_utils/line_matcher/__init__.py
@@ -1,14 +1,14 @@
 import unittest
 
+from exactly_lib_test.test_case_utils.line_matcher import regex, visitor
 # from exactly_lib_test.test_case_utils.file_matcher import parse_file_matcher
 # from exactly_lib_test.test_case_utils.file_matcher import resolvers
-# from exactly_lib_test.test_case_utils.file_matcher import test_resources_test
-from exactly_lib_test.test_case_utils.line_matcher import regex, visitor
+from exactly_lib_test.test_case_utils.line_matcher import test_resources_test
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        # test_resources_test.suite(),
+        test_resources_test.suite(),
         regex.suite(),
         visitor.suite(),
         # resolvers.suite(),
