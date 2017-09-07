115a4c829bc7c2c8f325246f0db3089727ef9efa
Emil Karlén <emil.karlen@fripost.org>
2017-09-07 18:45:37 +0200

    file-matcher: Adds FileMatcherConstant

--- src/exactly_lib/type_system/logic/file_matcher.py
+++ src/exactly_lib/type_system/logic/file_matcher.py
@@ -2,7 +2,13 @@ import pathlib
 
 
 class FileMatcher:
-    """Selects files from a directory according the a file condition."""
+    """Matches a path of an existing file."""
 
     def select_from(self, directory: pathlib.Path) -> iter:
+        raise NotImplementedError('abstract method')
+
+    def matches(self, path: pathlib.Path) -> bool:
+        """
+        :param path: The path of an existing file (but may be a broken symbolic link).
+        """
         raise NotImplementedError('abstract method')
