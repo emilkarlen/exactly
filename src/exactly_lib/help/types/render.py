40f095c9f342deafb59738604b0d6747f8785c1d
emil <emil.karlen@etraveli.com>
2017-09-07 23:45:22 +0200

    help: Improves rendering of list of types

--- src/exactly_lib/help/types/render.py
+++ src/exactly_lib/help/types/render.py
@@ -1,9 +1,12 @@
 from exactly_lib.help.program_modes.common.render_syntax_contents import invokation_variants_content
 from exactly_lib.help.types.contents_structure import TypeDocumentation
 from exactly_lib.help.utils.doc_utils import synopsis_section
+from exactly_lib.help.utils.rendering.entity_documentation_rendering import \
+    entity_doc_list_renderer_as_single_line_description
 from exactly_lib.help.utils.rendering.section_contents_renderer import RenderingEnvironment, SectionContentsRenderer
 from exactly_lib.help.utils.textformat_parser import TextParser
 from exactly_lib.help_texts.test_case.phase_names import ACT_PHASE_NAME
+from exactly_lib.type_system.value_type import ElementType
 from exactly_lib.util.textformat.structure import document as doc
 from exactly_lib.util.textformat.structure import structures as docs
 
@@ -13,7 +16,6 @@ class IndividualTypeRenderer(SectionContentsRenderer):
         self.doc = type_doc
         self.rendering_environment = None
         format_map = {
-            # 'type_concept': formatting.concept(TYPE_CONCEPT_INFO.name().singular),
             'act_phase': ACT_PHASE_NAME.emphasis,
         }
         self._parser = TextParser(format_map)
@@ -30,3 +32,25 @@ class IndividualTypeRenderer(SectionContentsRenderer):
                                             [])))
 
         return doc.SectionContents(initial_paragraphs, sub_sections)
+
+
+class TypesListRenderer(SectionContentsRenderer):
+    def __init__(self, type_doc_list: list):
+        self.type_doc_list = type_doc_list
+
+    def apply(self, environment: RenderingEnvironment) -> doc.SectionContents:
+        data_types = self._types_of(ElementType.SYMBOL)
+        logic_types = self._types_of(ElementType.LOGIC)
+        return doc.SectionContents([],
+                                   [
+                                       doc.Section(docs.text('Data types'),
+                                                   entity_doc_list_renderer_as_single_line_description(
+                                                       data_types).apply(environment)),
+                                       doc.Section(docs.text('Logic types'),
+                                                   entity_doc_list_renderer_as_single_line_description(
+                                                       logic_types).apply(environment)),
+                                   ])
+
+    def _types_of(self, element_type: ElementType) -> list:
+        return list(filter(lambda type_doc: type_doc.element_type is element_type,
+                           self.type_doc_list))
