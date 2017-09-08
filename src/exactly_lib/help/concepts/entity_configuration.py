eacacb014861a010c2358e4888fd93c15529ae20
emil <emil.karlen@etraveli.com>
2017-09-08 16:29:20 +0200

    help: Partitions concepts into "configuration parameters" and non-conf-params

--- src/exactly_lib/help/concepts/entity_configuration.py
+++ src/exactly_lib/help/concepts/entity_configuration.py
@@ -1,10 +1,10 @@
+from exactly_lib.help.concepts import render
 from exactly_lib.help.concepts.all_concepts import all_concepts
 from exactly_lib.help.concepts.contents_structure import concepts_help
-from exactly_lib.help.concepts.render import IndividualConceptRenderer, AllConceptsRendererGetter
 from exactly_lib.help.contents_structure import EntityConfiguration
-from exactly_lib.help.html_doc.parts.utils.entities_list_renderer import FlatEntityListHierarchyGeneratorGetter
 
-CONCEPT_ENTITY_CONFIGURATION = EntityConfiguration(concepts_help(all_concepts()),
-                                                   IndividualConceptRenderer,
-                                                   AllConceptsRendererGetter(),
-                                                   FlatEntityListHierarchyGeneratorGetter(IndividualConceptRenderer))
+CONCEPT_ENTITY_CONFIGURATION = EntityConfiguration(
+    concepts_help(all_concepts()),
+    render.IndividualConceptRenderer,
+    render.list_renderer_getter(),
+    render.hierarchy_generator_getter())
