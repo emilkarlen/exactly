0b40f87bde69a97771422b83a78d82fcac3ebe2f
Emil Karlén <emil.karlen@fripost.org>
2017-09-08 03:19:40 +0200

    help: REFACT: Improves impl of type help

--- src/exactly_lib/help/utils/rendering/section_contents_renderer.py
+++ src/exactly_lib/help/utils/rendering/section_contents_renderer.py
@@ -1,5 +1,6 @@
 from exactly_lib.help.utils.rendering.cross_reference import CrossReferenceTextConstructor
 from exactly_lib.util.textformat.structure import document as doc
+from exactly_lib.util.textformat.structure.core import Text
 
 
 class RenderingEnvironment(tuple):
@@ -26,6 +27,42 @@ class SectionContentsRenderer:
 class SectionRenderer:
     def apply(self, environment: RenderingEnvironment) -> doc.Section:
         raise NotImplementedError()
+
+
+class SectionRendererFromSectionContentsRenderer(SectionRenderer):
+    def __init__(self,
+                 header: Text,
+                 section_contents_renderer: SectionContentsRenderer
+                 ):
+        self.header = header
+        self.section_contents_renderer = section_contents_renderer
+
+    def apply(self, environment: RenderingEnvironment) -> doc.Section:
+        return doc.Section(self.header,
+                           self.section_contents_renderer.apply(environment))
+
+
+def section_contents_renderer_with_sub_sections(
+        section_renderer_list: list,
+        initial_paragraphs: list = None) -> SectionContentsRenderer:
+    return _SectionContentsRendererWithSubSections(section_renderer_list,
+                                                   initial_paragraphs if initial_paragraphs is not None else [])
+
+
+class _SectionContentsRendererWithSubSections(SectionContentsRenderer):
+    def __init__(self, section_renderer_list: list,
+                 initial_paragraphs: list):
+        """
+        :type section_renderer_list: :class:`SectionRenderer`
+        :type initial_paragraphs: list of :class:`ParagraphItem`
+        """
+        self.section_renderer_list = section_renderer_list
+        self.initial_paragraphs = initial_paragraphs
+
+    def apply(self, environment: RenderingEnvironment) -> doc.SectionContents:
+        return doc.SectionContents(self.initial_paragraphs,
+                                   [section_renderer.apply(environment)
+                                    for section_renderer in self.section_renderer_list])
 
 
 class SectionContentsRendererForConstantContents(SectionContentsRenderer):
