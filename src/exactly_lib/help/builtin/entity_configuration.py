8c405d38db7311ce505c7f8ace8c49940e4ae2ed
emil <emil.karlen@etraveli.com>
2017-09-08 14:07:06 +0200

    help: Rendering of partitioned entity set: Partitions builtin symbols

--- src/exactly_lib/help/builtin/entity_configuration.py
+++ src/exactly_lib/help/builtin/entity_configuration.py
@@ -1,13 +1,10 @@
 from exactly_lib.help.builtin import render
 from exactly_lib.help.builtin.contents_structure import builtin_symbols_help
 from exactly_lib.help.contents_structure import EntityConfiguration
-from exactly_lib.help.html_doc.parts.utils.entities_list_renderer import FlatEntityListHierarchyGeneratorGetter
-from exactly_lib.help.utils.rendering.entity_documentation_rendering import \
-    FlatListRendererWithSingleLineDescriptionGetter
 
 
 def builtin_symbols_entity_configuration(all_builtin_symbols_docs: list) -> EntityConfiguration:
     return EntityConfiguration(builtin_symbols_help(all_builtin_symbols_docs),
                                render.IndividualBuiltinSymbolRenderer,
-                               FlatListRendererWithSingleLineDescriptionGetter(),
-                               FlatEntityListHierarchyGeneratorGetter(render.IndividualBuiltinSymbolRenderer))
+                               render.list_renderer_getter(),
+                               render.hierarchy_generator_getter())
