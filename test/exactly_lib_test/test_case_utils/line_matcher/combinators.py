38bd9e1b02916579fd3fea6ba0751bb0d8a7255a
emil <emil.karlen@etraveli.com>
2017-09-09 14:38:26 +0200

    file-matcher: REFACT: Extracts test resources for test of combinators

--- test/exactly_lib_test/test_case_utils/line_matcher/combinators.py
+++ test/exactly_lib_test/test_case_utils/line_matcher/combinators.py
@@ -1,7 +1,7 @@
 import unittest
 
 from exactly_lib.test_case_utils.line_matcher import line_matchers as sut
-from exactly_lib_test.test_case_utils.test_resources import combinator_matcher_check
+from exactly_lib_test.test_case_utils.test_resources import matcher_combinators_check
 
 
 def suite() -> unittest.TestSuite:
@@ -12,7 +12,7 @@ def suite() -> unittest.TestSuite:
     ])
 
 
-class LineMatcherConfiguration(combinator_matcher_check.MatcherConfiguration):
+class LineMatcherConfiguration(matcher_combinators_check.MatcherConfiguration):
     def irrelevant_model(self):
         return 'irrelevant line model'
 
@@ -20,37 +20,37 @@ class LineMatcherConfiguration(combinator_matcher_check.MatcherConfiguration):
         return sut.LineMatcherConstant(result)
 
     def matcher_that_registers_model_argument_and_returns_constant(self, result: bool
-                                                                   ) -> combinator_matcher_check.MatcherThatRegistersModelArgument:
+                                                                   ) -> matcher_combinators_check.MatcherThatRegistersModelArgument:
         return LineMatcherThatRegistersModelArgument(result)
 
 
-class TestAnd(combinator_matcher_check.TestAndBase):
+class TestAnd(matcher_combinators_check.TestAndBase):
     # To debug an individual test case - override the test method in the super class
     # and call super.
     @property
-    def configuration(self) -> combinator_matcher_check.MatcherConfiguration:
+    def configuration(self) -> matcher_combinators_check.MatcherConfiguration:
         return LineMatcherConfiguration()
 
     def new_combinator_to_check(self, constructor_argument):
         return sut.LineMatcherAnd(constructor_argument)
 
 
-class TestOr(combinator_matcher_check.TestOrBase):
+class TestOr(matcher_combinators_check.TestOrBase):
     # To debug an individual test case - override the test method in the super class
     # and call super.
     @property
-    def configuration(self) -> combinator_matcher_check.MatcherConfiguration:
+    def configuration(self) -> matcher_combinators_check.MatcherConfiguration:
         return LineMatcherConfiguration()
 
     def new_combinator_to_check(self, constructor_argument):
         return sut.LineMatcherOr(constructor_argument)
 
 
-class TestNot(combinator_matcher_check.TestNotBase):
+class TestNot(matcher_combinators_check.TestNotBase):
     # To debug an individual test case - override the test method in the super class
     # and call super.
     @property
-    def configuration(self) -> combinator_matcher_check.MatcherConfiguration:
+    def configuration(self) -> matcher_combinators_check.MatcherConfiguration:
         return LineMatcherConfiguration()
 
     def new_combinator_to_check(self, constructor_argument):
@@ -58,7 +58,7 @@ class TestNot(combinator_matcher_check.TestNotBase):
 
 
 class LineMatcherThatRegistersModelArgument(sut.LineMatcher,
-                                            combinator_matcher_check.MatcherThatRegistersModelArgument):
+                                            matcher_combinators_check.MatcherThatRegistersModelArgument):
     def matches(self, line: str) -> bool:
         self.register_argument(line)
         return self._constant_result
