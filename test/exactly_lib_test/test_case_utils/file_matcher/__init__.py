c196382b10c3864d81fb7266844e8ffea10bf108
emil <emil.karlen@etraveli.com>
2017-09-09 20:21:08 +0200

    file-matcher: Deletes tests of resolvers

--- test/exactly_lib_test/test_case_utils/file_matcher/__init__.py
+++ test/exactly_lib_test/test_case_utils/file_matcher/__init__.py
@@ -1,7 +1,6 @@
 import unittest
 
 from exactly_lib_test.test_case_utils.file_matcher import parse_file_matcher
-from exactly_lib_test.test_case_utils.file_matcher import resolvers
 from exactly_lib_test.test_case_utils.file_matcher import test_resources_test
 from exactly_lib_test.test_case_utils.file_matcher import visitor, constant, name_glob_pattern, file_type, combinators
 
@@ -14,7 +13,6 @@ def suite() -> unittest.TestSuite:
         name_glob_pattern.suite(),
         file_type.suite(),
         combinators.suite(),
-        resolvers.suite(),
         parse_file_matcher.suite(),
     ])
 
