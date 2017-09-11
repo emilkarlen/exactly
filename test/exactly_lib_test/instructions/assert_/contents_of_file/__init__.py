e08534398d873110bef568801052eddd8590f91c
emil <emil.karlen@etraveli.com>
2017-09-11 18:08:58 +0200

    contents/num-lines: REFACT: Adds module for all common tests

--- test/exactly_lib_test/instructions/assert_/contents_of_file/__init__.py
+++ test/exactly_lib_test/instructions/assert_/contents_of_file/__init__.py
@@ -7,25 +7,20 @@ from exactly_lib_test.instructions.assert_.contents_of_file.relativity_option_fo
     empty as empty_with_relative_actual, \
     equals as equals_with_relative_actual
 from exactly_lib_test.instructions.assert_.contents_of_file.test_resources import TestConfigurationForFile
-from exactly_lib_test.instructions.assert_.contents_of_file.test_resources import TestConfigurationForFile
-from exactly_lib_test.instructions.assert_.test_resources.file_contents import empty
-from exactly_lib_test.instructions.assert_.test_resources.file_contents import equals
-from exactly_lib_test.instructions.assert_.test_resources.file_contents import line_matches
-from exactly_lib_test.instructions.assert_.test_resources.file_contents import parse_invalid_syntax
+from exactly_lib_test.instructions.assert_.test_resources.file_contents import common_tests
 from exactly_lib_test.instructions.test_resources.check_description import suite_for_instruction_documentation
 
 
 def suite() -> unittest.TestSuite:
     configuration = TestConfigurationForFile()
     return unittest.TestSuite([
+        common_tests.suite_for(configuration),
+
         parse.suite(),
-        parse_invalid_syntax.suite_for(configuration),
-        empty.suite_for(configuration),
         empty_with_relative_actual.suite_for(configuration),
-        equals.suite_for(configuration),
         equals_with_relative_actual.suite_for(configuration),
-        line_matches.suite_for(configuration),
         any_line_matches_with_relative_actual.suite_for(configuration),
+
         suite_for_instruction_documentation(sut.TheInstructionDocumentation('instruction name')),
     ])
 
---
+++ test/exactly_lib_test/instructions/assert_/test_resources/file_contents/common_tests.py
@@ -0,0 +1,19 @@
+"""Tests that applies for every contents checking instruction."""
+
+import unittest
+
+from exactly_lib_test.instructions.assert_.test_resources.file_contents import empty
+from exactly_lib_test.instructions.assert_.test_resources.file_contents import equals
+from exactly_lib_test.instructions.assert_.test_resources.file_contents import line_matches
+from exactly_lib_test.instructions.assert_.test_resources.file_contents import parse_invalid_syntax
+from exactly_lib_test.instructions.assert_.test_resources.file_contents.equals import \
+    InstructionTestConfigurationForEquals
+
+
+def suite_for(configuration: InstructionTestConfigurationForEquals) -> unittest.TestSuite:
+    return unittest.TestSuite([
+        parse_invalid_syntax.suite_for(configuration),
+        empty.suite_for(configuration),
+        equals.suite_for(configuration),
+        line_matches.suite_for(configuration),
+    ])
