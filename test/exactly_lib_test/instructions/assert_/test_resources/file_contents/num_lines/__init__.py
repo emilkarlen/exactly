2eefe467810bec968731815c13b4c33f303126e0
emil <emil.karlen@etraveli.com>
2017-09-11 18:40:57 +0200

    contents/num-lines: Test of valid assertions

--- test/exactly_lib_test/instructions/assert_/test_resources/file_contents/num_lines/__init__.py
+++ test/exactly_lib_test/instructions/assert_/test_resources/file_contents/num_lines/__init__.py
@@ -3,10 +3,11 @@ import unittest
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.instruction_test_configuration import \
     InstructionTestConfigurationForContentsOrEquals
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.num_lines import \
-    invalid_syntax_test_cases
+    invalid_syntax_test_cases, valid_syntax_test_cases
 
 
 def suite_for(configuration: InstructionTestConfigurationForContentsOrEquals) -> unittest.TestSuite:
     return unittest.TestSuite([
         invalid_syntax_test_cases.suite_for(configuration),
+        valid_syntax_test_cases.suite_for(configuration),
     ])
