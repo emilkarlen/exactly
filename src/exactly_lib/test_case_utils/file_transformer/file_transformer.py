449118fbbb55ab0582ca37b3588d415c56346f4c
Emil Karlén <emil.karlen@fripost.org>
2017-09-12 23:05:18 +0200

    REFACT: Removes FileTransformer: Removes resolvers

--- src/exactly_lib/test_case_utils/file_transformer/file_transformer.py
+++ src/exactly_lib/test_case_utils/file_transformer/file_transformer.py
@@ -3,7 +3,6 @@ import pathlib
 from exactly_lib.test_case.os_services import OsServices
 from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep
 from exactly_lib.type_system.logic.lines_transformer import LinesTransformer
-from exactly_lib.util.symbol_table import SymbolTable
 
 
 class DestinationFilePathGetter:
@@ -41,22 +40,4 @@ class FileTransformer:
 
     @property
     def corresponding_lines_transformer(self) -> LinesTransformer:
-        raise NotImplementedError('abstract method')
-
-
-class FileTransformerResolver:
-    """Resolver of a :class:`FileTransformer`"""
-
-    # TODO: Maybe this class should be removed,
-    # it is a wrapping of new LinesTransformer into old structures.
-    # But perhaps it is motivated to have it. Have not looked into it.
-
-    @property
-    def references(self) -> list:
-        """
-        :rtype [`NamedElementReference`]:
-        """
-        return []
-
-    def resolve(self, named_elements: SymbolTable) -> FileTransformer:
         raise NotImplementedError('abstract method')
