99ad67bd048ad6580e965f107391ec6ad25c76ad
Emil Karlén <emil.karlen@fripost.org>
2017-09-12 23:04:00 +0200

    REFACT: Removes FileTransformer: Removes parsers and resolver-from-lines-transformer

--- test/exactly_lib_test/instructions/assert_/utils/file_contents/contents_checkers.py
+++ test/exactly_lib_test/instructions/assert_/utils/file_contents/contents_checkers.py
@@ -4,11 +4,12 @@ import unittest
 from exactly_lib.instructions.assert_.utils.file_contents.parts import contents_checkers as sut
 from exactly_lib.instructions.assert_.utils.return_pfh_via_exceptions import PfhHardErrorException
 from exactly_lib.named_element.named_element_usage import NamedElementReference
+from exactly_lib.named_element.resolver_structure import LinesTransformerResolver
 from exactly_lib.named_element.restriction import ValueTypeRestriction
 from exactly_lib.test_case import os_services as oss
-from exactly_lib.test_case_utils.file_transformer.file_transformer import FileTransformerResolver, FileTransformer
-from exactly_lib.test_case_utils.file_transformer.file_transformers import ConstantFileTransformerResolver, \
-    IdentityFileTransformer
+from exactly_lib.test_case_utils.lines_transformer.resolvers import LinesTransformerConstant
+from exactly_lib.test_case_utils.lines_transformer.transformers import IdentityLinesTransformer
+from exactly_lib.type_system.logic.lines_transformer import LinesTransformer
 from exactly_lib.type_system.value_type import ValueType
 from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.named_element.test_resources import resolver_structure_assertions as asrt_rs
@@ -20,12 +21,10 @@ from exactly_lib_test.test_resources.value_assertions import value_assertion as 
 
 
 def suite() -> unittest.TestSuite:
-    return unittest.TestSuite([
-        unittest.makeSuite(TestFileTransformerAsChecker),
-    ])
+    return unittest.makeSuite(TestFileTransformerAsAssertionPart)
 
 
-class TestFileTransformerAsChecker(unittest.TestCase):
+class TestFileTransformerAsAssertionPart(unittest.TestCase):
     the_os_services = oss.new_default()
     environment = fake_post_sds_environment()
 
@@ -41,40 +40,39 @@ class TestFileTransformerAsChecker(unittest.TestCase):
                                       is_value_type_restriction(ref_1_info.value)),
         ])
 
-        ft_with_references = FileTransformerResolverWithReferences([ref_1])
-        checker = sut.FileTransformerAsAssertionPart(ft_with_references)
+        lt_with_references = LinesTransformerResolverWithReferences([ref_1])
+        assertion_part = sut.FileTransformerAsAssertionPart(lt_with_references)
 
         # ACT #
-        actual = checker.references
+        actual = assertion_part.references
         # ASSERT #
         expected_references.apply_without_message(self, actual)
 
     def test_PfhHardError_SHOULD_be_raised_WHEN_file_does_not_exist(self):
         # ARRANGE #
-        transformer_resolver = ConstantFileTransformerResolver(IdentityFileTransformer())
-        checker = sut.FileTransformerAsAssertionPart(transformer_resolver)
+        transformer_resolver = LinesTransformerConstant(IdentityLinesTransformer())
+        assertion_part = sut.FileTransformerAsAssertionPart(transformer_resolver)
         # ACT & ASSERT #
         with self.assertRaises(PfhHardErrorException):
-            checker.check(self.environment, self.the_os_services,
-                          pathlib.Path('a file that does not exist'))
+            assertion_part.check(self.environment, self.the_os_services,
+                                 pathlib.Path('a file that does not exist'))
 
     def test_PfhHardError_SHOULD_be_raised_WHEN_file_does_exist_but_is_not_a_regular_file(self):
         # ARRANGE #
-        transformer_resolver = ConstantFileTransformerResolver(IdentityFileTransformer())
-        checker = sut.FileTransformerAsAssertionPart(transformer_resolver)
+        transformer_resolver = LinesTransformerConstant(IdentityLinesTransformer())
+        assertion_part = sut.FileTransformerAsAssertionPart(transformer_resolver)
         # ACT & ASSERT #
         with tmp_dir() as path_of_existing_directory:
             with self.assertRaises(PfhHardErrorException):
-                checker.check(self.environment, self.the_os_services,
-                              path_of_existing_directory)
+                assertion_part.check(self.environment, self.the_os_services,
+                                     path_of_existing_directory)
 
 
-class FileTransformerResolverWithReferences(FileTransformerResolver):
+class LinesTransformerResolverWithReferences(LinesTransformerResolver):
     def __init__(self, references: list):
         self._references = references
 
-    def resolve(self,
-                named_elements: SymbolTable) -> FileTransformer:
+    def resolve(self, named_elements: SymbolTable) -> LinesTransformer:
         raise NotImplementedError('should not be used in these tests')
 
     @property
