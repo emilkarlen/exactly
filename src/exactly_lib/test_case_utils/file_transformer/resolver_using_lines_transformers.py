35a0e520d6320a0f4de1eff22e0b557055702b04
Emil Karlén <emil.karlen@fripost.org>
2017-09-12 19:27:45 +0200

    REFACT: Remove FileTransformer: Introduces UniqueFileNameInExistingDirGetter
    for resolving destination path

--- src/exactly_lib/test_case_utils/file_transformer/resolver_using_lines_transformers.py
+++ src/exactly_lib/test_case_utils/file_transformer/resolver_using_lines_transformers.py
@@ -1,6 +1,7 @@
 from exactly_lib.named_element.resolver_structure import LinesTransformerResolver
 from exactly_lib.test_case_utils.file_transformer import file_transformers
 from exactly_lib.test_case_utils.file_transformer.file_transformer import FileTransformerResolver, FileTransformer
+from exactly_lib.test_case_utils.file_transformer.from_lines_transformer import UniqueFileNameInExistingDirGetter
 from exactly_lib.test_case_utils.lines_transformer.transformers import IdentityLinesTransformer
 from exactly_lib.util.symbol_table import SymbolTable
 
@@ -16,7 +17,8 @@ class ResolveFileTransformerFromLinesTransformer(FileTransformerResolver):
         else:
             from exactly_lib.test_case_utils.file_transformer.from_lines_transformer import \
                 FileTransformerFromLinesTransformer
-            return FileTransformerFromLinesTransformer(lines_transformer)
+            return FileTransformerFromLinesTransformer(UniqueFileNameInExistingDirGetter(),
+                                                       lines_transformer)
 
     @property
     def references(self) -> list:
