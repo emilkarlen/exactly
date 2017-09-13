6f24ca07c674538f59dfa6a67c09d567b0bc5630
Emil Karlén <emil.karlen@fripost.org>
2017-09-13 19:17:13 +0200

    dir-cont-fil: REFACT: Splits tests into separate modules

--- test/exactly_lib_test/instructions/assert_/contents_of_dir/test_resources.py
+++ test/exactly_lib_test/instructions/assert_/contents_of_dir/test_resources.py
@@ -2,17 +2,23 @@ import unittest
 
 from exactly_lib.instructions.assert_ import contents_of_dir as sut
 from exactly_lib.instructions.assert_.utils.file_contents_resources import EMPTINESS_CHECK_ARGUMENT
+from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
+    SingleInstructionInvalidArgumentException
 from exactly_lib.test_case_file_structure.path_relativity import RelSdsOptionType, RelHomeOptionType, \
     PathRelativityVariants, RelOptionType
+from exactly_lib.util.expectation_type import ExpectationType
 from exactly_lib_test.instructions.assert_.test_resources.instr_arg_variant_check.check_with_neg_and_rel_opts import \
     InstructionChecker, InstructionArgumentsVariantConstructor
 from exactly_lib_test.instructions.assert_.test_resources.instr_arg_variant_check.negation_argument_handling import \
     ExpectationTypeConfig
+from exactly_lib_test.instructions.assert_.test_resources.instruction_check import TestCaseBase
 from exactly_lib_test.instructions.test_resources.assertion_utils import pfh_check as asrt_pfh
+from exactly_lib_test.instructions.test_resources.single_line_source_instruction_utils import equivalent_source_variants
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_populator import SdsSubDirResolverFromSdsFun
 from exactly_lib_test.test_case_utils.test_resources import relativity_options as rel_opt_conf
 from exactly_lib_test.test_case_utils.test_resources.relativity_options import RelativityOptionConfiguration
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_file, sym_link
+from exactly_lib_test.test_resources.name_and_value import NameAndValue
 from exactly_lib_test.test_resources.test_case_file_struct_and_symbols.home_and_sds_actions import \
     MkSubDirAndMakeItCurrentDirectory
 
@@ -44,6 +50,41 @@ class TestCaseBaseForParser(unittest.TestCase):
             sut.Parser(),
             ACCEPTED_REL_OPT_CONFIGURATIONS,
         )
+
+
+class TestParseInvalidSyntaxBase(TestCaseBase):
+    @property
+    def test_cases_with_negation_operator_place_holder(self) -> list:
+        raise NotImplementedError('abstract method')
+
+    def test_raise_exception_WHEN_syntax_is_invalid(self):
+
+        self._assert_each_case_raises_SingleInstructionInvalidArgumentException(
+            self.test_cases_with_negation_operator_place_holder)
+
+    def test_raise_exception_WHEN_relativity_is_unaccepted(self):
+
+        test_cases = [
+            NameAndValue('relativity:' + rel_opt_config.option_string,
+                         instruction_arguments_for_emptiness_check(rel_opt_config,
+                                                                   'file-name')
+                         )
+            for rel_opt_config in UNACCEPTED_REL_OPT_CONFIGURATIONS
+        ]
+
+        self._assert_each_case_raises_SingleInstructionInvalidArgumentException(test_cases)
+
+    def _assert_each_case_raises_SingleInstructionInvalidArgumentException(self, test_cases: list):
+        parser = sut.Parser()
+        for expectation_type in ExpectationType:
+            etc = ExpectationTypeConfig(expectation_type)
+            for name_and_instruction_argument_str in test_cases:
+                with self.subTest(case_name=name_and_instruction_argument_str.name,
+                                  expectation_type=str(expectation_type)):
+                    first_line_arguments = replace_not_op(etc, name_and_instruction_argument_str.value)
+                    for source in equivalent_source_variants(self, first_line_arguments):
+                        with self.assertRaises(SingleInstructionInvalidArgumentException):
+                            parser.parse(source)
 
 
 class TestCommonFailureConditionsBase:
