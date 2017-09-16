d85b485febc9e85b798ed131f92c992bcea18aaf
Emil Karlén <emil.karlen@fripost.org>
2017-09-16 14:01:25 +0200

    dir-cont-iter: Adds parsing of AssertionPart for file contents

--- src/exactly_lib/instructions/assert_/contents_of_dir/assertions/quant_over_files.py
+++ src/exactly_lib/instructions/assert_/contents_of_dir/assertions/quant_over_files.py
@@ -1,4 +1,5 @@
 from exactly_lib.instructions.assert_.contents_of_dir.assertions import common
+from exactly_lib.instructions.assert_.utils.assertion_part import AssertionPart
 from exactly_lib.test_case.phases.common import InstructionEnvironmentForPostSdsStep, \
     InstructionEnvironmentForPreSdsStep
 from exactly_lib.test_case.phases.result import svh
@@ -7,10 +8,16 @@ from exactly_lib.util.logic_types import Quantifier
 
 class InstructionForQuantifiedAssertion(common._InstructionBase):
     def __init__(self,
+                 settings: common.Settings,
                  quantifier: Quantifier,
-                 settings: common.Settings):
+                 actual_file_assertion_part: AssertionPart,
+                 ):
+        """
+        :param actual_file_assertion_part: An assertion part on a :class:`FileToCheck`
+        """
         super().__init__(settings)
         self.quantifier = quantifier
+        self._actual_file_assertion_part = actual_file_assertion_part
         self._symbol_usages = settings.path_to_check.references + settings.file_matcher.references
 
     def symbol_usages(self) -> list:
