af7a0337cdab2aefa2fed78c9c151fa69e4ea878
Emil Karlén <emil.karlen@etraveli.com>
2017-09-24 10:12:06 +0200

    transform: Integration tests: TestSymbolUsages

--- test/exactly_lib_test/instructions/multi_phase_instructions/instruction_integration_test_resources/transform_instruction_test.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/instruction_integration_test_resources/transform_instruction_test.py
@@ -2,7 +2,8 @@ import unittest
 
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
-from exactly_lib.test_case_file_structure.path_relativity import RelOptionType, RelHomeOptionType
+from exactly_lib.test_case.phases.common import TestCaseInstructionWithSymbols
+from exactly_lib.test_case_file_structure.path_relativity import RelOptionType, RelHomeOptionType, RelSdsOptionType
 from exactly_lib.test_case_utils.lines_transformer.resolvers import LinesTransformerConstant
 from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.instructions.assert_.test_resources.file_contents.contents_transformation import \
@@ -13,13 +14,18 @@ from exactly_lib_test.instructions.multi_phase_instructions.instruction_integrat
 from exactly_lib_test.instructions.test_resources.check_description import suite_for_documentation_instance
 from exactly_lib_test.instructions.test_resources.single_line_source_instruction_utils import \
     equivalent_source_variants__with_source_check
+from exactly_lib_test.named_element.symbol.restrictions.test_resources.concrete_restriction_assertion import \
+    equals_symbol_reference_restrictions
 from exactly_lib_test.named_element.test_resources.lines_transformer import is_lines_transformer_reference_to
 from exactly_lib_test.named_element.test_resources.named_elem_utils import container
+from exactly_lib_test.named_element.test_resources.resolver_structure_assertions import matches_reference
 from exactly_lib_test.section_document.test_resources.parse_source import remaining_source
 from exactly_lib_test.test_case_file_structure.test_resources import home_populators
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_contents_check import dir_contains_exactly
+from exactly_lib_test.test_case_utils.parse.parse_file_ref import file_ref_reference_restrictions
 from exactly_lib_test.test_case_utils.test_resources.path_arg_with_relativity import PathArgumentWithRelativity
-from exactly_lib_test.test_case_utils.test_resources.relativity_options import conf_rel_any, conf_rel_home
+from exactly_lib_test.test_case_utils.test_resources.relativity_options import conf_rel_any, conf_rel_home, \
+    symbol_conf_rel_home, symbol_conf_rel_sds
 from exactly_lib_test.test_resources.file_structure import DirContents, File, empty_file
 from exactly_lib_test.test_resources.name_and_value import NameAndValue
 from exactly_lib_test.test_resources.test_case_file_struct_and_symbols.home_and_sds_utils import \
@@ -29,9 +35,10 @@ from exactly_lib_test.test_resources.value_assertions import value_assertion as 
 
 def suite_for(conf: ConfigurationBase) -> unittest.TestSuite:
     common_test_cases = [
-        TestSuccessfullyCreateFileInExistingDirectory,
+        TestSymbolUsages,
         TestFailingValidationPreSds,
         TestFailingWhenDestinationFileExists,
+        TestSuccessfullyCreateFileInExistingDirectory,
     ]
     suites = [tc(conf)
               for tc in common_test_cases]
@@ -55,6 +62,60 @@ class TestCaseBase(unittest.TestCase):
     def shortDescription(self):
         return '\n / '.join([str(type(self)),
                              str(type(self.conf))])
+
+
+class TestSymbolUsages(TestCaseBase):
+    def runTest(self):
+        # ARRANGE #
+        src_path_symbol_name = 'src_path_symbol'
+        dst_path_symbol_name = 'dst_path_symbol'
+        transformer_symbol_name = 'transformer_symbol'
+
+        line_transformer_variants = [
+            NameAndValue('no transformer',
+                         ('',
+                          [])
+                         ),
+            NameAndValue('reference to transformer symbol',
+                         (transformer_symbol_name,
+                          [
+                              is_lines_transformer_reference_to(transformer_symbol_name)
+                          ])
+                         ),
+        ]
+        dst_file_arg = PathArgumentWithRelativity('dst-file',
+                                                  symbol_conf_rel_sds(RelSdsOptionType.REL_TMP,
+                                                                      dst_path_symbol_name,
+                                                                      transform.DST_PATH_RELATIVITY_VARIANTS))
+        src_path_rel_variants = transform.src_path_relativity_variants(not self.conf.phase_is_after_act())
+        src_file_arg = PathArgumentWithRelativity('src-file',
+                                                  symbol_conf_rel_home(RelHomeOptionType.REL_HOME_CASE,
+                                                                       src_path_symbol_name,
+                                                                       src_path_rel_variants))
+        path_symbol_reference_assertions = [
+            matches_reference(asrt.equals(src_path_symbol_name),
+                              equals_symbol_reference_restrictions(
+                                  file_ref_reference_restrictions(src_path_rel_variants))),
+
+            matches_reference(asrt.equals(dst_path_symbol_name),
+                              equals_symbol_reference_restrictions(
+                                  file_ref_reference_restrictions(transform.DST_PATH_RELATIVITY_VARIANTS))),
+
+        ]
+        for line_transformer_case in line_transformer_variants:
+            with self.subTest(line_transformer=line_transformer_case.name):
+                symbol_reference_assertions = path_symbol_reference_assertions + line_transformer_case.value[1]
+                lines_transformer_arg = line_transformer_case.value[0]
+                source = remaining_source(transform.ArgumentsConstructor(src_file_arg,
+                                                                         dst_file_arg,
+                                                                         lines_transformer_arg).construct())
+                # ACT #
+                instruction = self.conf.parser().parse(source)
+                assert isinstance(instruction, TestCaseInstructionWithSymbols)  # Sanity check
+                # ASSERT #
+                expected_symbol_references = asrt.matches_sequence(symbol_reference_assertions)
+                expected_symbol_references.apply_without_message(self,
+                                                                 instruction.symbol_usages())
 
 
 class TestParseShouldFailWhenRelativityOfSourceIsRelResult(TestCaseBase):
