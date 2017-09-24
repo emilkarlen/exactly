bea1204c7dad27c99f110146a67116f6ee5514d9
Emil Karlén <emil.karlen@etraveli.com>
2017-09-24 06:13:30 +0200

    transform: Test resources: Improves error messages of failing assertions
    of svh and sh

--- test/exactly_lib_test/test_case/test_resources/sh_assertions.py
+++ test/exactly_lib_test/test_case/test_resources/sh_assertions.py
@@ -1,12 +1,28 @@
+import unittest
+
 from exactly_lib.test_case.phases.result import sh
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+from exactly_lib_test.test_resources.value_assertions.value_assertion import MessageBuilder
 
 
 def is_success() -> asrt.ValueAssertion:
-    return is_sh_and(asrt.sub_component('is_success',
-                                        sh.SuccessOrHardError.is_success.fget,
-                                        asrt.equals(True,
-                                                    'Status is expected to be success')))
+    return _IsSuccess()
+
+
+class _IsSuccess(asrt.ValueAssertion):
+    def __init__(self):
+        pass
+
+    def apply(self,
+              put: unittest.TestCase,
+              value,
+              message_builder: MessageBuilder = MessageBuilder()):
+        put.assertIsInstance(value, sh.SuccessOrHardError)
+        if not value.is_success:
+            put.fail('\n'.join([
+                'Expected: success',
+                'Actual  : hard_error: ' + str(value.failure_message)
+            ]))
 
 
 def is_hard_error(assertion_on_error_message: asrt.ValueAssertion = asrt.anything_goes()) -> asrt.ValueAssertion:
