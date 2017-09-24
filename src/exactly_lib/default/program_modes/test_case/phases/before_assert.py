1d8d9bba68ce7582ab0e3f2bca2f9c06b3b07b2e
Emil Karlén <emil.karlen@fripost.org>
2017-09-24 10:16:55 +0200

    transform: Adds to default instruction set

--- src/exactly_lib/default/program_modes/test_case/phases/before_assert.py
+++ src/exactly_lib/default/program_modes/test_case/phases/before_assert.py
@@ -1,14 +1,14 @@
 from exactly_lib.common.instruction_setup import instruction_set_from_name_and_setup_constructor_list
-from exactly_lib.help_texts.test_case.instructions.instruction_names import CHANGE_DIR_INSTRUCTION_NAME, \
-    SHELL_INSTRUCTION_NAME
-from exactly_lib.instructions.before_assert import change_dir, env, run, shell, new_dir
+from exactly_lib.help_texts.test_case.instructions import instruction_names
+from exactly_lib.instructions.before_assert import change_dir, env, run, shell, new_dir, transform
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
     [
         ('dir', new_dir.setup),
         ('env', env.setup),
         ('run', run.setup),
-        (CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
-        (SHELL_INSTRUCTION_NAME, shell.setup),
+        (instruction_names.CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
+        (instruction_names.SHELL_INSTRUCTION_NAME, shell.setup),
+        (instruction_names.TRANSFORM_FILE_INSTRUCTION_NAME, transform.setup),
     ]
 )
--- src/exactly_lib/default/program_modes/test_case/phases/cleanup.py
+++ src/exactly_lib/default/program_modes/test_case/phases/cleanup.py
@@ -1,14 +1,14 @@
 from exactly_lib.common.instruction_setup import instruction_set_from_name_and_setup_constructor_list
-from exactly_lib.help_texts.test_case.instructions.instruction_names import CHANGE_DIR_INSTRUCTION_NAME, \
-    SHELL_INSTRUCTION_NAME
-from exactly_lib.instructions.cleanup import change_dir, env, run, new_dir, shell
+from exactly_lib.help_texts.test_case.instructions import instruction_names
+from exactly_lib.instructions.cleanup import change_dir, env, run, new_dir, shell, transform
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
     [
         ('dir', new_dir.setup),
         ('env', env.setup),
         ('run', run.setup),
-        (CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
-        (SHELL_INSTRUCTION_NAME, shell.setup),
+        (instruction_names.CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
+        (instruction_names.SHELL_INSTRUCTION_NAME, shell.setup),
+        (instruction_names.TRANSFORM_FILE_INSTRUCTION_NAME, transform.setup),
     ]
 )
