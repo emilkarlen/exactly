1619c04eb5c7023f191a6774616c4f0944005d78
Emil Karlén <emil.karlen@etraveli.com>
2017-09-24 06:16:21 +0200

    transform: REFACT: Removes unused test code

--- test/exactly_lib_test/instructions/setup/new_dir.py
+++ test/exactly_lib_test/instructions/setup/new_dir.py
@@ -2,14 +2,12 @@ import unittest
 
 from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.instructions.setup import new_dir as sut
-from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib_test.instructions.multi_phase_instructions.instruction_integration_test_resources import \
     new_dir_instruction_test
 from exactly_lib_test.instructions.multi_phase_instructions.instruction_integration_test_resources.new_dir_instruction_test import \
     Configuration
 from exactly_lib_test.instructions.setup.test_resources.configuration import SetupConfigurationBase
-from exactly_lib_test.instructions.setup.test_resources.instruction_check import TestCaseBase, Arrangement, \
-    Expectation
+from exactly_lib_test.instructions.setup.test_resources.instruction_check import Expectation
 from exactly_lib_test.test_case.test_resources import sh_assertions
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 
@@ -26,11 +24,3 @@ class TheConfiguration(SetupConfigurationBase, Configuration):
 
 def suite() -> unittest.TestSuite:
     return new_dir_instruction_test.suite_for(TheConfiguration())
-
-
-class TestCaseBaseForParser(TestCaseBase):
-    def _run(self,
-             source: ParseSource,
-             arrangement: Arrangement,
-             expectation: Expectation):
-        self._check(sut.setup('instr-name'), source, arrangement, expectation)
