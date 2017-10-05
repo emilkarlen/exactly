493c88daebd2e222be60174086b8ebcc53daeccb
Emil Karlén <emil.karlen@fripost.org>
2017-10-05 04:00:11 +0200

    see-also-utils: SeeAlsoUrlInfo

--- test/exactly_lib_test/common/help/see_also.py
+++ test/exactly_lib_test/common/help/see_also.py
@@ -8,6 +8,7 @@ from exactly_lib.util.textformat.structure.core import StringText
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(SeeAlsoItemVisitorTest),
+        unittest.makeSuite(TestSeeAlsoUrlInfo),
         unittest.makeSuite(TestCrossReferenceSet),
     ])
 
@@ -50,6 +51,31 @@ class ArgumentRecordingVisitor(sut.SeeAlsoItemVisitor):
     def visit_text(self, x: sut.TextSeeAlsoItem):
         self.visited_classes.append(sut.TextSeeAlsoItem)
         return x
+
+
+class TestSeeAlsoUrlInfo(unittest.TestCase):
+    def test_equals(self):
+        # ARRANGE #
+        title = 'the title'
+        url = 'the url'
+        expected = sut.SeeAlsoUrlInfo(title, url)
+        actual = sut.SeeAlsoUrlInfo(title, url)
+        # ASSERT #
+        self.assertEqual(expected, actual)
+
+    def test_not_equals(self):
+        # ARRANGE #
+        expected_title = 'expected title'
+        expected_url = 'expected url'
+        expected = sut.SeeAlsoUrlInfo(expected_title, expected_url)
+        actuals = [
+            sut.SeeAlsoUrlInfo('actual title', expected_url),
+            sut.SeeAlsoUrlInfo(expected_title, 'actual url')
+        ]
+        for actual in actuals:
+            with self.subTest(actual=actual):
+                # ASSERT #
+                self.assertNotEqual(expected, actual)
 
 
 class TestCrossReferenceSet(unittest.TestCase):
