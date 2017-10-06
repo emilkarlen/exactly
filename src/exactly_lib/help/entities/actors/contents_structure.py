e17f5e4460fe2cfbacb76810bf5ea1d40ee42f23
Emil Karlén <emil.karlen@fripost.org>
2017-10-06 22:19:35 +0200

    help: REFACT: see-also-elements as SeeAlsoTarget: ActorDocumentation

--- src/exactly_lib/help/entities/actors/contents_structure.py
+++ src/exactly_lib/help/entities/actors/contents_structure.py
@@ -1,4 +1,3 @@
-from exactly_lib.common.help.see_also import CrossReferenceIdSeeAlsoItem
 from exactly_lib.help.utils.entity_documentation import EntitiesHelp, EntityDocumentationBase
 from exactly_lib.help_texts.cross_reference_id import TestCasePhaseCrossReference
 from exactly_lib.help_texts.entity_names import ACTOR_ENTITY_TYPE_NAME
@@ -22,21 +21,15 @@ class ActorDocumentation(EntityDocumentationBase):
     def act_phase_contents_syntax(self) -> SectionContents:
         raise NotImplementedError()
 
-    def see_also_items(self) -> list:
-        """
-        :rtype: [`SeeAlsoItem`]
-        """
-        return [CrossReferenceIdSeeAlsoItem(x) for x in self.see_also_targets()]
-
     def see_also_targets(self) -> list:
         """
-        :rtype [`CrossReferenceTarget`]
+        :returns: A new list of :class:`SeeAlsoTarget`, which may contain duplicate elements.
         """
         return self.__see_also_common() + self._see_also_specific()
 
     def __see_also_common(self) -> list:
         """
-        :rtype [`CrossReferenceTarget`]
+        :rtype [`SeeAlsoTarget`]
         """
         from exactly_lib.help.entities.concepts.configuration_parameters.actor import ACTOR_CONCEPT
         from exactly_lib.help_texts.test_case.phase_names import ACT_PHASE_NAME
@@ -47,7 +40,7 @@ class ActorDocumentation(EntityDocumentationBase):
 
     def _see_also_specific(self) -> list:
         """
-        :rtype [`CrossReferenceTarget`]
+        :rtype [`SeeAlsoTarget`]
         """
         return []
 
