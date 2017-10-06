5a1df2568dae7fa8b2a7bc1c1ca2acb9115c42a7
Emil Karlén <emil.karlen@etraveli.com>
2017-10-06 14:17:19 +0200

    help: syntax element: Adds REGEX

--- src/exactly_lib/help/entities/syntax_elements/all_syntax_elements.py
+++ src/exactly_lib/help/entities/syntax_elements/all_syntax_elements.py
@@ -1,7 +1,8 @@
-from exactly_lib.help.entities.syntax_elements.element import here_document
+from exactly_lib.help.entities.syntax_elements.element import here_document, regex
 
 ALL_SYNTAX_ELEMENT_DOCS = [
     here_document.DOCUMENTATION,
+    regex.DOCUMENTATION,
 ]
 
 NAME_2_SYNTAX_ELEMENT_DOC = dict(map(lambda x: (x.singular_name(), x), ALL_SYNTAX_ELEMENT_DOCS))
