d781fa0bce461ef6efe49c8738311d2b65f5f5aa
emil <emil@ffs.org>
2017-11-12 17:38:13 +0100

    help: Adds syntax elements for all data types

---
+++ src/exactly_lib/help/entities/syntax_elements/element/type_path.py
@@ -0,0 +1,11 @@
+from exactly_lib.help.entities.syntax_elements.contents_structure import SyntaxElementDocumentation
+from exactly_lib.help_texts.entity import syntax_element
+from exactly_lib.util.textformat.parse import normalize_and_parse
+
+_MAIN_DESCRIPTION_REST = """\
+"""
+
+DOCUMENTATION = SyntaxElementDocumentation(syntax_element.PATH_SYNTAX_ELEMENT,
+                                           normalize_and_parse(_MAIN_DESCRIPTION_REST),
+                                           [],
+                                           [])
