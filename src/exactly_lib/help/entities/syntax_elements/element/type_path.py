10a7dd735092ea208d3cdc7ab8bb4881c4e601f4
Emil Karlén <emil.karlen@fripost.org>
2017-11-13 00:18:38 +0100

    help: PATH: Improves documentation

--- src/exactly_lib/help/entities/syntax_elements/element/type_path.py
+++ src/exactly_lib/help/entities/syntax_elements/element/type_path.py
@@ -8,7 +8,11 @@ from exactly_lib.help_texts.entity import types
 from exactly_lib.help_texts.entity.syntax_element import STRING_SYNTAX_ELEMENT
 from exactly_lib.help_texts.names import formatting
 from exactly_lib.instructions.utils.documentation import documentation_text
+from exactly_lib.instructions.utils.documentation.relative_path_options_documentation import \
+    relativity_options_paragraph
+from exactly_lib.test_case_file_structure.path_relativity import PathRelativityVariants, RelOptionType
 from exactly_lib.util.cli_syntax.elements import argument as a
+from exactly_lib.util.textformat.structure.core import ParagraphItem
 
 
 class _Documentation(SyntaxElementDocumentation):
@@ -19,6 +23,7 @@ class _Documentation(SyntaxElementDocumentation):
         self._relativity_name = instruction_arguments.RELATIVITY_ARGUMENT
 
         self._parser = TextParser({
+            'RELATIVITY_OPTION': self._relativity_name.name,
             'PATH_STRING': self._string_name.name,
             'posix_syntax': documentation_text.POSIX_SYNTAX,
             'string_type': formatting.keyword(types.STRING_TYPE_INFO.name.singular),
@@ -34,8 +39,8 @@ class _Documentation(SyntaxElementDocumentation):
 
     def syntax_element_descriptions(self) -> list:
         return [
-            self._relativity_sed(),
             self._string_sed(),
+            self._relativity_sed(),
         ]
 
     def main_description_rest(self) -> list:
@@ -47,8 +52,11 @@ class _Documentation(SyntaxElementDocumentation):
         ]
 
     def _relativity_sed(self) -> SyntaxElementDescription:
+        description_rest = self._parser.fnap(_RELATIVITY_DESCRIPTION_REST)
+        description_rest.append(self._relativity_options_paragraph())
+
         return SyntaxElementDescription(self._relativity_name.name,
-                                        self._parser.fnap(_RELATIVITY_DESCRIPTION_REST))
+                                        description_rest)
 
     def _string_sed(self) -> SyntaxElementDescription:
         return SyntaxElementDescription(self._string_name.name,
@@ -63,6 +71,11 @@ class _Documentation(SyntaxElementDocumentation):
 
         ]
 
+    def _relativity_options_paragraph(self) -> ParagraphItem:
+        variants = PathRelativityVariants(RelOptionType, True)
+        return relativity_options_paragraph(self._string_name.name,
+                                            variants)
+
 
 DOCUMENTATION = _Documentation()
 
@@ -74,8 +87,39 @@ A relative or absolute path, using {posix_syntax}.
 
 
 It is a value of type {string_type}, and thus uses {string_syntax_element} syntax.
+
+
+If it is a relative path, then if {RELATIVITY_OPTION} is ...
+
+
+  * given
+  
+    {PATH_STRING} is relative to the directory specified by {RELATIVITY_OPTION}
+
+  * not given
+  
+    {PATH_STRING} is relative to the default relativity root directory,
+    
+    as specified by the place where the path is used.
+
+
+The default relativity depends on the instruction,
+and also on the argument position of the instruction. 
+
+
+If {PATH_STRING} is an absolute path, then {RELATIVITY_OPTION} must not be given.
 """
 
 _RELATIVITY_DESCRIPTION_REST = """\
 Specifies a directory that {PATH_STRING} is relative to.
+
+
+The available relativities depends on the instruction,
+and also on the argument position of the instruction. 
+
+
+If {PATH_STRING} is an absolute path, then {RELATIVITY_OPTION} must not be given.
+
+
+Forms:
 """
