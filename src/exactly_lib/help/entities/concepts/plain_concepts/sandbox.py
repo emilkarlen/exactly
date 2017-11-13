c56c19aef3fd123204a08abbceae4bb702ec1437
Emil Karlén <emil@member.fsf.org>
2017-11-13 01:16:53 +0100

    help: REFACT: Doc of "sandbox"

--- src/exactly_lib/help/entities/concepts/plain_concepts/sandbox.py
+++ src/exactly_lib/help/entities/concepts/plain_concepts/sandbox.py
@@ -1,5 +1,6 @@
 from exactly_lib import program_info
 from exactly_lib.help.entities.concepts.contents_structure import PlainConceptDocumentation
+from exactly_lib.help.utils.textformat_parser import TextParser
 from exactly_lib.help_texts import file_ref as file_ref_texts
 from exactly_lib.help_texts.cross_reference_id import TestCasePhaseInstructionCrossReference
 from exactly_lib.help_texts.entity import concepts
@@ -9,7 +10,6 @@ from exactly_lib.help_texts.test_case.instructions.instruction_names import CHAN
 from exactly_lib.help_texts.test_case.phase_names import phase_name_dictionary, SETUP_PHASE_NAME
 from exactly_lib.test_case_file_structure import sandbox_directory_structure as sds, environment_variables
 from exactly_lib.util.description import DescriptionWithSubSections
-from exactly_lib.util.textformat.parse import normalize_and_parse
 from exactly_lib.util.textformat.structure import lists
 from exactly_lib.util.textformat.structure import structures as docs
 from exactly_lib.util.textformat.structure.core import ParagraphItem
@@ -20,14 +20,20 @@ class _SandboxConcept(PlainConceptDocumentation):
     def __init__(self):
         super().__init__(concepts.SANDBOX_CONCEPT_INFO)
 
+        self._parser = TextParser({
+            'program_name': formatting.program_name(program_info.PROGRAM_NAME),
+            'phase': phase_name_dictionary(),
+            'instruction': AnyInstructionNameDictionary(),
+            'cwd': formatting.concept(concepts.CURRENT_WORKING_DIRECTORY_CONCEPT_INFO.name.singular),
+            'cd_instruction': InstructionName(CHANGE_DIR_INSTRUCTION_NAME),
+        })
+
     def purpose(self) -> DescriptionWithSubSections:
-        phase = phase_name_dictionary()
-        instruction = AnyInstructionNameDictionary()
         rest_paragraphs = []
         sub_sections = []
-        rest_paragraphs.extend(normalize_and_parse(_SANDBOX_PRE_DIRECTORY_TREE.format(phase=phase)))
+        rest_paragraphs += self._parser.fnap(_SANDBOX_PRE_DIRECTORY_TREE)
         sub_sections.append(directory_structure_list_section(sds.execution_directories))
-        sub_sections.extend(sandbox_directories_info_sections(phase, instruction))
+        sub_sections += self._sandbox_directories_info_sections()
         return DescriptionWithSubSections(self.single_line_description(),
                                           SectionContents(rest_paragraphs, sub_sections))
 
@@ -37,6 +43,56 @@ class _SandboxConcept(PlainConceptDocumentation):
                                                    CHANGE_DIR_INSTRUCTION_NAME),
             concepts.ENVIRONMENT_VARIABLE_CONCEPT_INFO.cross_reference_target,
         ]
+
+    def _sandbox_directories_info_sections(self) -> list:
+        def section(directory_name: str, paragraph_items: list) -> Section:
+            return docs.section('%s/' % directory_name,
+                                paragraph_items)
+
+        return [
+            section(sds.SUB_DIRECTORY__ACT,
+                    self._act_dir_description_paragraphs()),
+            section(sds.SUB_DIRECTORY__RESULT,
+                    self._result_dir_description_paragraphs()),
+            section(sds.PATH__TMP_USER,
+                    self._tmp_user_dir_description_paragraphs()),
+            docs.section('Other directories',
+                         self._other_directories_than_those_listed())
+        ]
+
+    def _act_dir_description_paragraphs(self) -> list:
+        ret_val = []
+        ret_val.extend(self._fnap(_ACT_DIR_DESCRIPTION))
+        ret_val.extend(_dir_env_variables_and_rel_options(env_var_name=environment_variables.ENV_VAR_ACT,
+                                                          rel_option=file_ref_texts.REL_ACT_OPTION))
+        return ret_val
+
+    def _result_dir_description_paragraphs(self) -> list:
+        ret_val = []
+        ret_val += self._fnap(_RESULT_DIR_DESCRIPTION)
+        ret_val.append(docs.simple_header_only_list(sds.RESULT_FILE_ALL,
+                                                    lists.ListType.ITEMIZED_LIST))
+        ret_val += self._result_dir_env_variable_and_rel_option()
+        return ret_val
+
+    def _tmp_user_dir_description_paragraphs(self) -> list:
+        ret_val = []
+        ret_val += self._fnap(_USR_TMP_DIR_DESCRIPTION)
+        ret_val += _dir_env_variables_and_rel_options(env_var_name=environment_variables.ENV_VAR_TMP,
+                                                      rel_option=file_ref_texts.REL_TMP_OPTION)
+        return ret_val
+
+    def _result_dir_env_variable_and_rel_option(self) -> list:
+        return [_dir_info_items_table(environment_variables.ENV_VAR_RESULT,
+                                      file_ref_texts.REL_RESULT_OPTION,
+                                      self._parser.format(_RESULT_DIR_ENV_VARIABLE))
+                ]
+
+    def _other_directories_than_those_listed(self) -> list:
+        return self._fnap(_OTHER_DIRECTORIES_THAN_THOSE_LISTED)
+
+    def _fnap(self, template: str) -> list:
+        return self._parser.fnap(template)
 
 
 SANDBOX_CONCEPT = _SandboxConcept()
@@ -48,58 +104,6 @@ Each test case uses its own sandbox.
 The sandbox is created just before the {phase[setup]} phase is executed,
 and deleted after the test case has finished.
 """
-
-
-def sandbox_directories_info_sections(phase_name_dictionary: dict,
-                                      instruction: AnyInstructionNameDictionary) -> list:
-    def section(directory_name: str, paragraph_items: list) -> Section:
-        return docs.section('%s/' % directory_name,
-                            paragraph_items)
-
-    return [
-        section(sds.SUB_DIRECTORY__ACT,
-                _act_dir_description_paragraphs(instruction, phase_name_dictionary)),
-        section(sds.SUB_DIRECTORY__RESULT,
-                _result_dir_description_paragraphs(instruction, phase_name_dictionary)),
-        section(sds.PATH__TMP_USER,
-                _tmp_user_dir_description_paragraphs()),
-        docs.section('Other directories',
-                     _other_directories_than_those_listed())
-    ]
-
-
-def _act_dir_description_paragraphs(instruction: AnyInstructionNameDictionary, phase: dict) -> list:
-    ret_val = []
-    ret_val.extend(normalize_and_parse(
-        _ACT_DIR_DESCRIPTION.format(
-            phase=phase,
-            instruction=instruction,
-            cwd=formatting.concept(concepts.CURRENT_WORKING_DIRECTORY_CONCEPT_INFO.name.singular),
-            cd_instruction=InstructionName(CHANGE_DIR_INSTRUCTION_NAME))))
-    ret_val.extend(_dir_env_variables_and_rel_options(env_var_name=environment_variables.ENV_VAR_ACT,
-                                                      rel_option=file_ref_texts.REL_ACT_OPTION))
-    return ret_val
-
-
-def _result_dir_description_paragraphs(instruction: AnyInstructionNameDictionary, phase: dict):
-    ret_val = []
-    ret_val.extend(normalize_and_parse(
-        _RESULT_DIR_DESCRIPTION.format(phase=phase,
-                                       instruction=instruction)))
-    ret_val.append(docs.simple_header_only_list(sds.RESULT_FILE_ALL,
-                                                lists.ListType.ITEMIZED_LIST))
-    ret_val.extend(_result_dir_env_variable_and_rel_option(phase))
-    return ret_val
-
-
-def _tmp_user_dir_description_paragraphs():
-    ret_val = []
-    ret_val.extend(normalize_and_parse(
-        _USR_TMP_DIR_DESCRIPTION.format(program_name=formatting.program_name(program_info.PROGRAM_NAME))))
-    ret_val.extend(_dir_env_variables_and_rel_options(env_var_name=environment_variables.ENV_VAR_TMP,
-                                                      rel_option=file_ref_texts.REL_TMP_OPTION))
-    return ret_val
-
 
 _ACT_DIR_DESCRIPTION = """\
 This directory is the {cwd} when the {phase[setup]} phase begin.
@@ -126,13 +130,6 @@ A directory that {program_name} does not touch.
 The test case can use it as a place where it is safe to put temporary files without
 the risk of clashes with files from other program.
 """
-
-
-def _result_dir_env_variable_and_rel_option(phase: dict) -> list:
-    return [_dir_info_items_table(environment_variables.ENV_VAR_RESULT,
-                                  file_ref_texts.REL_RESULT_OPTION,
-                                  _RESULT_DIR_ENV_VARIABLE.format(phase=phase))
-            ]
 
 
 _RESULT_DIR_ENV_VARIABLE = """\
@@ -178,11 +175,6 @@ def _directory_structure_list(dir_with_sub_dir_list: list) -> ParagraphItem:
         items.append(lists.HeaderContentListItem(docs.text(dir_wsd.name + '/'), sub_dirs_items))
     return lists.HeaderContentList(items,
                                    lists.Format(lists.ListType.ITEMIZED_LIST))
-
-
-def _other_directories_than_those_listed() -> list:
-    return docs.normalize_and_parse(_OTHER_DIRECTORIES_THAN_THOSE_LISTED.format(
-        program_name=formatting.program_name(program_info.PROGRAM_NAME)))
 
 
 _OTHER_DIRECTORIES_THAN_THOSE_LISTED = """\
