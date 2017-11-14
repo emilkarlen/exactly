96107874ac855ac8797be5e022d5535f3b2ef428
Emil Karlén <emil@member.fsf.org>
2017-11-14 06:56:08 +0100

    help : REFACT: Renames function

--- src/exactly_lib/help/entities/types/entity_configuration.py
+++ src/exactly_lib/help/entities/types/entity_configuration.py
@@ -6,5 +6,5 @@ from exactly_lib.help.entities.types.contents_structure import types_help
 TYPE_ENTITY_CONFIGURATION = EntityConfiguration(
     types_help(all_types()),
     render.IndividualTypeRenderer,
-    render.type_list_render_getter(),
+    render.list_render_getter(),
     render.hierarchy_generator_getter())
