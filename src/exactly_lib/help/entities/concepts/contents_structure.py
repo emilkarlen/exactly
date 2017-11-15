946ae70fc3345f0fd38ba83de75f79a2cbcdeaac
Emil Karlén <emil.karlen@fripost.org>
2017-11-15 22:39:57 +0100

    help: conf param, concepts: Removes redundant code
    now methods are inherited from new base class

--- src/exactly_lib/help/entities/concepts/contents_structure.py
+++ src/exactly_lib/help/entities/concepts/contents_structure.py
@@ -1,9 +1,8 @@
 from exactly_lib.help.utils.entity_documentation import EntitiesHelp, \
     command_line_names_as_singular_name, EntityDocumentationBase
-from exactly_lib.help_texts.name_and_cross_ref import SingularAndPluralNameAndCrossReferenceId, CrossReferenceId
+from exactly_lib.help_texts.name_and_cross_ref import SingularAndPluralNameAndCrossReferenceId
 from exactly_lib.util.description import DescriptionWithSubSections
 from exactly_lib.util.name import Name, name_with_plural_s
-from exactly_lib.util.textformat.structure.core import Text
 from exactly_lib.util.textformat.structure.structures import para
 
 CONCEPT_ENTITY_TYPE_NAMES = command_line_names_as_singular_name(name_with_plural_s('concept'))
@@ -24,15 +23,6 @@ class ConceptDocumentation(EntityDocumentationBase):
 
     def name(self) -> Name:
         return self._info.name
-
-    def single_line_description(self) -> Text:
-        return self._info.single_line_description
-
-    def singular_name(self) -> str:
-        return self.name().singular
-
-    def cross_reference_target(self) -> CrossReferenceId:
-        return self._info.cross_reference_target
 
     def purpose(self) -> DescriptionWithSubSections:
         raise NotImplementedError()
