bcb17e9d43eba0996cc7c231b03f174eb41430d1
Emil Karlén <emil.karlen@fripost.org>
2017-11-15 18:05:23 +0100

    Removes unused code: custom_transformers.py

--- src/exactly_lib/test_case_utils/lines_transformer/custom_transformers.py
+++
@@ -1,7 +0,0 @@
-from exactly_lib.test_case_utils.lines_transformer.env_vars_replacement import EnvVarReplacementLinesTransformer
-
-ENV_VAR_REPLACEMENT_TRANSFORMER_NAME = 'env-var-replacement'
-
-CUSTOM_LINES_TRANSFORMERS = {
-    ENV_VAR_REPLACEMENT_TRANSFORMER_NAME: EnvVarReplacementLinesTransformer(ENV_VAR_REPLACEMENT_TRANSFORMER_NAME)
-}
