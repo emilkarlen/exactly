d29bb887f70cfbe4b698f85d572160d0b899b544
Emil Karlén <emil.karlen@fripost.org>
2017-11-15 18:05:40 +0100

    REFACT: Uses TextParser for text formatting

--- src/exactly_lib/test_case_utils/lines_transformer/env_vars_replacement.py
+++ src/exactly_lib/test_case_utils/lines_transformer/env_vars_replacement.py
@@ -5,9 +5,9 @@ from exactly_lib.help_texts.names.formatting import program_name
 from exactly_lib.test_case_file_structure import environment_variables
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_utils.lines_transformer.transformers import CustomLinesTransformer
-from exactly_lib.util.textformat.parse import normalize_and_parse
 from exactly_lib.util.textformat.structure import structures as docs
 from exactly_lib.util.textformat.structure.document import SectionContents
+from exactly_lib.util.textformat.textformat_parser import TextParser
 
 HOME_ENV_VAR_WITH_REPLACEMENT_PRECEDENCE = environment_variables.ENV_VAR_HOME_CASE
 
@@ -54,14 +54,14 @@ def _first_is(key_of_first_element: str, all_vars: dict) -> iter:
 
 
 def with_replaced_env_vars_help() -> SectionContents:
-    format_map = {
+    text_parser = TextParser({
         'checked_file': 'checked file',
         'program_name': program_info.PROGRAM_NAME,
         'home_act_env_var': environment_variables.ENV_VAR_HOME_ACT,
         'home_case_env_var': environment_variables.ENV_VAR_HOME_CASE,
         'home_env_var_with_replacement_precedence': HOME_ENV_VAR_WITH_REPLACEMENT_PRECEDENCE,
-    }
-    prologue = normalize_and_parse(_WITH_REPLACED_ENV_VARS_PROLOGUE.format_map(format_map))
+    })
+    prologue = text_parser.fnap(_WITH_REPLACED_ENV_VARS_PROLOGUE)
     variables_list = [docs.simple_header_only_list(sorted(environment_variables.ALL_REPLACED_ENV_VARS),
                                                    docs.lists.ListType.VARIABLE_LIST)]
     return SectionContents(prologue + variables_list)
