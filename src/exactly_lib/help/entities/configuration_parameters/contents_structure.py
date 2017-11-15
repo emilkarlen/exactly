7c6dce62913aae59eeb3465ac5d26291fe5d01cf
Emil Karlén <emil.karlen@etraveli.com>
2017-11-15 22:39:57 +0100

    help: conf param, concepts: Removes redundant code
    now methods are inherited from new base class

--- src/exactly_lib/help/entities/configuration_parameters/contents_structure.py
+++ src/exactly_lib/help/entities/configuration_parameters/contents_structure.py
@@ -2,10 +2,9 @@ from exactly_lib.help.utils.entity_documentation import EntitiesHelp, \
     EntityTypeNames, EntityDocumentationBase
 from exactly_lib.help_texts.entity.concepts import CONFIGURATION_PARAMETER_CONCEPT_INFO
 from exactly_lib.help_texts.entity.conf_params import ConfigurationParameterInfo
-from exactly_lib.help_texts.name_and_cross_ref import CrossReferenceId
 from exactly_lib.help_texts.names import formatting
 from exactly_lib.util.description import DescriptionWithSubSections
-from exactly_lib.util.textformat.structure.core import ParagraphItem, Text
+from exactly_lib.util.textformat.structure.core import ParagraphItem
 from exactly_lib.util.textformat.structure.structures import para
 
 CONF_PARAM_ENTITY_TYPE_NAMES = EntityTypeNames(
@@ -23,17 +22,8 @@ class ConfigurationParameterDocumentation(EntityDocumentationBase):
     def name_and_cross_ref_target(self) -> ConfigurationParameterInfo:
         return self._info
 
-    def single_line_description(self) -> Text:
-        return self._info.single_line_description
-
-    def singular_name(self) -> str:
-        return self._info.singular_name
-
     def configuration_parameter_name(self) -> str:
         return self._info.configuration_parameter_name
-
-    def cross_reference_target(self) -> CrossReferenceId:
-        return self._info.cross_reference_target
 
     def purpose(self) -> DescriptionWithSubSections:
         raise NotImplementedError()
