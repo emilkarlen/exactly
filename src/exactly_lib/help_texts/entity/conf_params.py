23d9ba5aa09e94622d9c7894c364fffcd7e28b80
emil <emil@ffs.org>
2017-11-18 22:02:22 +0100

    help: REFACT: Moves description of TC dirs to new help_text module

--- src/exactly_lib/help_texts/entity/conf_params.py
+++ src/exactly_lib/help_texts/entity/conf_params.py
@@ -1,11 +1,12 @@
 from exactly_lib import program_info
 from exactly_lib.help_texts import conf_params
+from exactly_lib.help_texts import test_case_file_structure
 from exactly_lib.help_texts.cross_reference_id import EntityCrossReferenceId
 from exactly_lib.help_texts.entity import concepts
 from exactly_lib.help_texts.entity.all_entity_types import CONF_PARAM_ENTITY_TYPE_NAMES
 from exactly_lib.help_texts.name_and_cross_ref import SingularNameAndCrossReferenceId, CrossReferenceId
 from exactly_lib.help_texts.names import formatting
-from exactly_lib.help_texts.test_case.phase_names import phase_name_dictionary
+from exactly_lib.help_texts.test_case.phase_names import PHASE_NAME_DICTIONARY
 
 
 class ConfigurationParameterInfo(SingularNameAndCrossReferenceId):
@@ -29,9 +30,9 @@ def _cross_ref(concept_name: str) -> EntityCrossReferenceId:
                                   concept_name)
 
 
-def conf_param_info(name: str,
-                    configuration_parameter_name: str,
-                    single_line_description_str: str) -> ConfigurationParameterInfo:
+def _conf_param_info(name: str,
+                     configuration_parameter_name: str,
+                     single_line_description_str: str) -> ConfigurationParameterInfo:
     return ConfigurationParameterInfo(name,
                                       configuration_parameter_name,
                                       single_line_description_str,
@@ -40,35 +41,33 @@ def conf_param_info(name: str,
 
 def _format(s: str) -> str:
     return s.format(program_name=formatting.program_name(program_info.PROGRAM_NAME),
-                    phase=phase_name_dictionary())
+                    phase=PHASE_NAME_DICTIONARY)
 
 
-ACTOR_CONF_PARAM_INFO = conf_param_info(
+def _of_tc_dir_info(x: test_case_file_structure.TcDirInfo) -> ConfigurationParameterInfo:
+    return _conf_param_info(x.informative_name,
+                            x.identifier,
+                            x.single_line_description_str)
+
+
+ACTOR_CONF_PARAM_INFO = _conf_param_info(
     'actor',
     conf_params.ACTOR,
     concepts.ACTOR_CONCEPT_INFO.single_line_description_str
 )
 
-EXECUTION_MODE_CONF_PARAM_INFO = conf_param_info(
+EXECUTION_MODE_CONF_PARAM_INFO = _conf_param_info(
     'execution mode',
     conf_params.EXECUTION_MODE,
     _format('Determines how the outcome of the {phase[assert]} phase is interpreted, '
             'or if the test case should be skipped.')
 )
 
-HOME_CASE_DIRECTORY_CONF_PARAM_INFO = conf_param_info(
-    'home directory',
-    conf_params.HOME_CASE_DIRECTORY,
-    'Default location of files referenced from the test case.'
-)
+HOME_CASE_DIRECTORY_CONF_PARAM_INFO = _of_tc_dir_info(test_case_file_structure.HDS_CASE_INFO)
 
-HOME_ACT_DIRECTORY_CONF_PARAM_INFO = conf_param_info(
-    'act-home directory',
-    conf_params.HOME_ACT_DIRECTORY,
-    _format('Default location of files referenced from the {phase[act]} phase.')
-)
+HOME_ACT_DIRECTORY_CONF_PARAM_INFO = _of_tc_dir_info(test_case_file_structure.HDS_ACT_INFO)
 
-TIMEOUT_CONF_PARAM_INFO = conf_param_info(
+TIMEOUT_CONF_PARAM_INFO = _conf_param_info(
     'timeout',
     conf_params.TIMEOUT,
     _format('Timeout of sub processes executed by instructions and the {phase[act]} phase.')
