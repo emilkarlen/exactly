c87d2fd9942d7e4ea05ae1cae06afb7366054c56
Emil Karlén <emil.karlen@fripost.org>
2017-11-20 23:25:44 +0100

    help: syntax element: STRING: Version 1 of ok doc

--- src/exactly_lib/help/entities/types/objects/data_types.py
+++ src/exactly_lib/help/entities/types/objects/data_types.py
@@ -2,6 +2,7 @@ from exactly_lib.help.entities.types.contents_structure import TypeDocumentation
 from exactly_lib.help_texts.entity import types, syntax_element
 from exactly_lib.help_texts.names.formatting import InstructionName
 from exactly_lib.help_texts.test_case.instructions import instruction_names
+from exactly_lib.instructions.utils.documentation.documentation_text import POSIX_SYNTAX
 from exactly_lib.type_system.value_type import TypeCategory
 from exactly_lib.util.textformat.structure.document import empty_contents
 from exactly_lib.util.textformat.textformat_parser import TextParser
@@ -14,14 +15,22 @@ Useful when the SUT is a command line program,
 and for the {run} instruction, e.g.
 """
 
+_STRING_MAIN_DESCRIPTION_REST = """\
+Used for arbitrary string values, and relative file names.
+ 
+ 
+When used as a relative file name, {POSIX_SYNTAX} should be used.
+"""
+
 _TEXT_PARSER = TextParser({
     'run': InstructionName(instruction_names.RUN_INSTRUCTION_NAME),
+    'POSIX_SYNTAX': POSIX_SYNTAX,
 })
 
 STRING_DOCUMENTATION = TypeDocumentation(TypeCategory.DATA,
                                          types.STRING_TYPE_INFO,
                                          syntax_element.STRING_SYNTAX_ELEMENT,
-                                         empty_contents())
+                                         _TEXT_PARSER.section_contents(_STRING_MAIN_DESCRIPTION_REST))
 
 LIST_DOCUMENTATION = TypeDocumentation(TypeCategory.DATA,
                                        types.LIST_TYPE_INFO,
