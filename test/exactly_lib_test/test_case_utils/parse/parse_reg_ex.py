5bf2d1774d5916d82560056170b01beb6698b34d
Emil Karlén <emil.karlen@fripost.org>
2017-11-22 18:26:33 +0100

    reg-ex/ignore-case: Makes special parse method parse --ignore-case

--- test/exactly_lib_test/test_case_utils/parse/parse_reg_ex.py
+++ test/exactly_lib_test/test_case_utils/parse/parse_reg_ex.py
@@ -15,22 +15,29 @@ from exactly_lib_test.util.test_resources.quoting import surrounded_by_soft_quot
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        unittest.makeSuite(TestFailingParse),
-        unittest.makeSuite(TestSuccessfulParse),
+        unittest.makeSuite(TestParseWithoutIgnoreCaseOption),
+        unittest.makeSuite(TestParseWithIgnoreCaseOption),
     ])
 
 
 class Expectation:
     def __init__(self,
-                 matching_string: str,
+                 pattern_string: str,
+                 matching_string_list: list,
                  non_matching_string: str,
                  token_stream: asrt.ValueAssertion = asrt.anything_goes()):
+        self.pattern_string = pattern_string
         self.non_matching_string = non_matching_string
-        self.matching_string = matching_string
+        self.matching_string_list = matching_string_list
         self.token_stream = token_stream
 
 
-class TestFailingParse(unittest.TestCase):
+class TestParseWithoutIgnoreCaseOption(unittest.TestCase):
+    def _check(self,
+               source: TokenParserPrime,
+               expectation: Expectation):
+        _check(self, source, expectation)
+
     def test_failing_parse(self):
         cases = [
             (
@@ -52,67 +59,50 @@ class TestFailingParse(unittest.TestCase):
                 with self.assertRaises(SingleInstructionInvalidArgumentException):
                     sut.parse_regex(source)
 
-
-class TestSuccessfulParse(unittest.TestCase):
-    def _check(self,
-               source: TokenParserPrime,
-               expectation: Expectation):
-        # ACT #
-        actual_reg_ex = sut.parse_regex(source)
-        # ASSERT #
-        expectation.token_stream.apply_with_message(self,
-                                                    source.token_stream,
-                                                    'token stream')
-        self.assertTrue(bool(actual_reg_ex.search(expectation.matching_string)),
-                        'reg-ex should match "{}"'.format(expectation.matching_string))
-
-        self.assertFalse(bool(actual_reg_ex.search(expectation.non_matching_string)),
-                         'reg-ex should not match "{}"'.format(expectation.non_matching_string))
-
     def test_successful_parse_of_unquoted_tokens(self):
         # ARRANGE #
         regex_str = 'a.*regex'
 
         text_on_following_line = 'text on following line'
 
+        def _arg(format_string: str) -> str:
+            return format_string.format(
+                regex=regex_str)
+
         cases = [
             SourceCase(
-                'transformer is only source',
+                'REG-EX is only source',
                 source=
-                remaining_source('{regex}'.format(
-                    regex=regex_str,
-                )),
+                remaining_source(_arg('{regex}')),
                 source_assertion=
                 assert_token_stream(is_null=asrt.is_true),
             ),
             SourceCase(
-                'transformer is followed by a token',
+                'REG-EX is followed by a token',
                 source=
-                remaining_source('{regex} following_token'.format(
-                    regex=regex_str,
-                )),
+                remaining_source(_arg('{regex} following_token')),
                 source_assertion=
                 assert_token_stream(
                     is_null=asrt.is_false,
                     remaining_part_of_current_line=asrt.equals('following_token')),
             ),
             SourceCase(
-                'transformer is only element on current line, but followed by more lines',
+                'REG-EX is only element on current line, but followed by more lines',
                 source=
-                remaining_source('{regex}'.format(
-                    regex=regex_str,
-                ),
-                    following_lines=[text_on_following_line]),
+                remaining_source(_arg('{regex}'),
+                                 following_lines=[text_on_following_line]),
                 source_assertion=
                 assert_token_stream(
                     is_null=asrt.is_false,
+                    remaining_part_of_current_line=asrt.equals(''),
                     remaining_source=asrt.equals('\n' + text_on_following_line)),
             ),
         ]
         for case in cases:
             with self.subTest(case_name=case.name):
                 self._check(case.source,
-                            Expectation('aBCregex',
+                            Expectation(regex_str,
+                                        ['aBCregex'],
                                         'AbcREGEX',
                                         case.source_assertion))
 
@@ -151,6 +141,147 @@ class TestSuccessfulParse(unittest.TestCase):
         for case in cases:
             with self.subTest(case_name=case.name):
                 self._check(case.source,
-                            Expectation(' regex',
+                            Expectation(regex_str,
+                                        [' regex'],
                                         ' REGEX',
                                         case.source_assertion))
+
+
+class TestParseWithIgnoreCaseOption(unittest.TestCase):
+    def _check(self,
+               source: TokenParserPrime,
+               expectation: Expectation):
+        _check(self, source, expectation)
+
+    def test_failing_parse(self):
+        cases = [
+            (
+                'missing REG-EX string',
+                remaining_source(sut.IGNORE_CASE_OPTION),
+            ),
+            (
+                'no REG-EX argument, but it appears on the following line',
+                remaining_source(sut.IGNORE_CASE_OPTION,
+                                 ['regex']),
+            ),
+            (
+                'invalid REG-EX',
+                remaining_source('{option} {invalid_reg_ex}'.format(
+                    option=sut.IGNORE_CASE_OPTION,
+                    invalid_reg_ex='**'
+                )),
+            ),
+        ]
+        for name, source in cases:
+            with self.subTest(case_name=name):
+                with self.assertRaises(SingleInstructionInvalidArgumentException):
+                    sut.parse_regex(source)
+
+    def test_successful_parse_of_unquoted_tokens(self):
+        # ARRANGE #
+        regex_str = 'a.*regex'
+
+        text_on_following_line = 'text on following line'
+
+        def _arg(format_string: str) -> str:
+            return format_string.format(
+                option=sut.IGNORE_CASE_OPTION,
+                regex=regex_str)
+
+        cases = [
+            SourceCase(
+                'REG-EX is only source',
+                source=
+                remaining_source(_arg('{option} {regex}')),
+                source_assertion=
+                assert_token_stream(is_null=asrt.is_true),
+            ),
+            SourceCase(
+                'REG-EX is followed by a token',
+                source=
+                remaining_source(_arg('{option} {regex} following_token')),
+                source_assertion=
+                assert_token_stream(
+                    is_null=asrt.is_false,
+                    remaining_part_of_current_line=asrt.equals('following_token')),
+            ),
+            SourceCase(
+                'REG-EX is only element on current line, but followed by more lines',
+                source=
+                remaining_source(_arg('{option} {regex}'),
+                                 following_lines=[text_on_following_line]),
+                source_assertion=
+                assert_token_stream(
+                    is_null=asrt.is_false,
+                    remaining_source=asrt.equals('\n' + text_on_following_line)),
+            ),
+        ]
+        for case in cases:
+            with self.subTest(case_name=case.name):
+                self._check(case.source,
+                            Expectation(regex_str,
+                                        ['aBCregex', 'AbcREGEX'],
+                                        'other',
+                                        case.source_assertion))
+
+    def test_successful_parse_of_quoted_tokens(self):
+        # ARRANGE #
+        regex_str = '.* regex'
+
+        text_on_following_line = 'text on following line'
+
+        cases = [
+            SourceCase(
+                'soft quotes',
+
+                source=
+                remaining_source('{option} {regex}'.format(
+                    option=sut.IGNORE_CASE_OPTION,
+                    regex=surrounded_by_soft_quotes(regex_str),
+                )),
+
+                source_assertion=
+                assert_token_stream(is_null=asrt.is_true),
+            ),
+            SourceCase(
+                'hard quotes, and text on following line',
+
+                source=
+                remaining_source('{option} {regex}'.format(
+                    option=sut.IGNORE_CASE_OPTION,
+                    regex=surrounded_by_hard_quotes(regex_str),
+                ),
+                    following_lines=[text_on_following_line]),
+
+                source_assertion=
+                assert_token_stream(
+                    remaining_source=asrt.equals('\n' + text_on_following_line)),
+            ),
+        ]
+        for case in cases:
+            with self.subTest(case_name=case.name):
+                self._check(case.source,
+                            Expectation(regex_str,
+                                        [' regex', ' REGEX'],
+                                        ' rrr',
+                                        case.source_assertion))
+
+
+def _check(put: unittest.TestCase,
+           source: TokenParserPrime,
+           expectation: Expectation):
+    # ACT #
+    actual_reg_ex = sut.parse_regex(source)
+    # ASSERT #
+    expectation.token_stream.apply_with_message(put,
+                                                source.token_stream,
+                                                'token stream')
+    put.assertEqual(expectation.pattern_string,
+                    actual_reg_ex.pattern,
+                    'regex pattern string')
+    for matching_string in expectation.matching_string_list:
+        put.assertTrue(bool(actual_reg_ex.search(matching_string)),
+                       'reg-ex should match "{}"'.format(matching_string))
+
+    put.assertFalse(bool(actual_reg_ex.search(expectation.non_matching_string)),
+                    'reg-ex should not match "{}"'.format(expectation.non_matching_string))
