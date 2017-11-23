080e4c2cd5c4f557034a664b20da89efca97b1d5
Emil Karlén <emil.karlen@etraveli.com>
2017-11-23 18:00:47 +0100

    builtins: Adds case converter lines transformers to builtin symbols

--- src/exactly_lib/default/program_modes/test_case/builtin_symbols/lines_transformers.py
+++ src/exactly_lib/default/program_modes/test_case/builtin_symbols/lines_transformers.py
@@ -1,18 +1,26 @@
 from exactly_lib.cli.main_program import BuiltinSymbol
-from exactly_lib.test_case_utils.lines_transformer import env_vars_replacement
+from exactly_lib.test_case_utils.lines_transformer import env_vars_replacement, case_converters
 from exactly_lib.test_case_utils.lines_transformer.resolvers import LinesTransformerConstant
+from exactly_lib.util.textformat.structure.document import empty_contents
 
 EXACTLY_TEST_CASE_DIRS_REPLACEMENT = 'REPLACE_TEST_CASE_DIRS'
 
-_RESOLVER = LinesTransformerConstant(
-    env_vars_replacement.EnvVarReplacementLinesTransformer())
-
-_SINGLE_LINE_DESCRIPTION = ''
+_TO_UPPER_SINGLE_LINE_DESCRIPTION = ''
 
 ALL = (
     BuiltinSymbol(EXACTLY_TEST_CASE_DIRS_REPLACEMENT,
-                  _RESOLVER,
+                  LinesTransformerConstant(env_vars_replacement.EnvVarReplacementLinesTransformer()),
                   env_vars_replacement.SINGLE_LINE_DESCRIPTION,
                   env_vars_replacement.with_replaced_env_vars_help(),
+                  ),
+    BuiltinSymbol('TO_UPPER_CASE',
+                  LinesTransformerConstant(case_converters.ToUpperCaseLinesTransformer()),
+                  'Converts all cased characters to uppercase',
+                  empty_contents(),
+                  ),
+    BuiltinSymbol('TO_LOWER_CASE',
+                  LinesTransformerConstant(case_converters.ToLowerCaseLinesTransformer()),
+                  'Converts all cased characters to lowercase',
+                  empty_contents(),
                   ),
 )
