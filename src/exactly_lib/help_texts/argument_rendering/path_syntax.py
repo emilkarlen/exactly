b006d3cb094e77ab9452c7c35d2b73b2daddfcba
Emil Karlén <emil.karlen@fripost.org>
2017-11-23 17:22:22 +0100

    instr-path-doc: Slims relativity options description: "file"

--- src/exactly_lib/help_texts/argument_rendering/path_syntax.py
+++ src/exactly_lib/help_texts/argument_rendering/path_syntax.py
@@ -1,5 +1,4 @@
 from exactly_lib.help_texts.entity import syntax_elements
-from exactly_lib.help_texts.instruction_arguments import OPTIONAL_RELATIVITY_ARGUMENT_USAGE
 from exactly_lib.util.cli_syntax.elements import argument as a
 
 
@@ -11,10 +10,7 @@ def mandatory_path_with_optional_relativity(path_argument: a.Named,
                                             path_suffix_is_required: bool = True) -> list:
     multiplicity = a.Multiplicity.MANDATORY if path_suffix_is_required else a.Multiplicity.OPTIONAL
     path_part = a.Single(multiplicity, path_argument)
-    return [
-        OPTIONAL_RELATIVITY_ARGUMENT_USAGE,
-        path_part,
-    ]
+    return [path_part]
 
 
 def the_path_of(a_file_description: str) -> str:
