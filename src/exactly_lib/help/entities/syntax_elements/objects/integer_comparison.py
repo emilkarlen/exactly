b3c9c076442ecf6d0c377e5b1239994935de2929
Emil Karlén <emil.karlen@etraveli.com>
2017-11-26 20:40:03 +0100

    help: syntax-element INTEGER-COMPARISON: Adds syntax-element doc

---
+++ src/exactly_lib/help/entities/syntax_elements/objects/integer_comparison.py
@@ -0,0 +1,63 @@
+from exactly_lib.common.help.syntax_contents_structure import InvokationVariant, SyntaxElementDescription
+from exactly_lib.help.entities.syntax_elements.contents_structure import syntax_element_documentation
+from exactly_lib.help_texts.argument_rendering import cl_syntax
+from exactly_lib.help_texts.entity import syntax_elements
+from exactly_lib.instructions.assert_.utils.expression import comparators
+from exactly_lib.instructions.assert_.utils.expression.integer import syntax
+from exactly_lib.util.cli_syntax.elements import argument as a
+from exactly_lib.util.textformat.structure import structures as docs
+from exactly_lib.util.textformat.textformat_parser import TextParser
+
+_MANDATORY_OPERATOR_ARGUMENT = a.Single(a.Multiplicity.MANDATORY,
+                                        syntax.OPERATOR_ARGUMENT)
+
+_MANDATORY_INTEGER_ARGUMENT = a.Single(a.Multiplicity.MANDATORY,
+                                       syntax_elements.INTEGER_SYNTAX_ELEMENT.argument)
+
+_TEXT_PARSER = TextParser({
+    'INTEGER': syntax_elements.INTEGER_SYNTAX_ELEMENT.argument.name,
+})
+
+_DESCRIPTION_OF_IMPLICIT_EQUALS = """\
+Matches if, and only if, the actual value is exactly {INTEGER}.
+"""
+
+_DESCRIPTION_OF_COMPARISON_WITH_OPERATOR = """\
+Matches if, and only if, the actual value satisfies the comparison.
+
+
+The actual value that is matched serves as the left hand side in the comparison.
+"""
+
+
+def _operator_syntax_element_description() -> SyntaxElementDescription:
+    operators_list = ' '.join(sorted(comparators.NAME_2_OPERATOR.keys()))
+    operator_text = 'One of ' + operators_list
+    return SyntaxElementDescription(syntax.OPERATOR_ARGUMENT.name,
+                                    docs.paras(operator_text))
+
+
+DOCUMENTATION = syntax_element_documentation(
+    None,
+    syntax_elements.INTEGER_COMPARISON_SYNTAX_ELEMENT,
+    [],
+    invokation_variants=[
+        InvokationVariant(cl_syntax.cl_syntax_for_args([
+            _MANDATORY_INTEGER_ARGUMENT,
+        ]),
+            _TEXT_PARSER.fnap(_DESCRIPTION_OF_IMPLICIT_EQUALS)),
+
+        InvokationVariant(cl_syntax.cl_syntax_for_args([
+            _MANDATORY_OPERATOR_ARGUMENT,
+            _MANDATORY_INTEGER_ARGUMENT,
+        ]),
+            _TEXT_PARSER.fnap(_DESCRIPTION_OF_COMPARISON_WITH_OPERATOR))
+    ]
+    ,
+    syntax_element_descriptions=[
+        _operator_syntax_element_description(),
+    ],
+    see_also_targets=[
+        syntax_elements.INTEGER_SYNTAX_ELEMENT.cross_reference_target,
+    ]
+)
