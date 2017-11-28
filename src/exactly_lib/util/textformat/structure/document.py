9951a8401212fd8128ae09aa1d0017228267377c
Emil Karlén <emil.karlen@fripost.org>
2017-11-28 20:40:24 +0100

    text/article: Introduces SectionItem hierarchy and Article

--- src/exactly_lib/util/textformat/structure/document.py
+++ src/exactly_lib/util/textformat/structure/document.py
@@ -7,7 +7,7 @@ class SectionContents(tuple):
                 sections: list = None):
         """
         :type initial_paragraphs: [core.ParagraphItem]
-        :type sections: [Section]
+        :type sections: [SectionItem]
         """
         return tuple.__new__(cls, (initial_paragraphs,
                                    [] if sections is None else sections))
@@ -22,7 +22,7 @@ class SectionContents(tuple):
     @property
     def sections(self) -> list:
         """
-        :return: [Section]
+        :return: [SectionItem]
         """
         return self[1]
 
@@ -35,19 +35,58 @@ def empty_contents() -> SectionContents:
     return SectionContents([], [])
 
 
-class Section(tuple):
-    def __new__(cls,
-                header: core.Text,
-                contents: SectionContents):
-        return tuple.__new__(cls, (header, contents))
+class SectionItem:
+    def __init__(self,
+                 header: core.Text,
+                 contents: SectionContents
+                 ):
+        self._header = header
+        self._contents = contents
 
     @property
     def header(self) -> core.Text:
-        return self[0]
+        return self._header
 
     @property
     def contents(self) -> SectionContents:
-        """
-        :return: [core.ParagraphItem]
-        """
-        return self[1]
+        return self._contents
+
+
+class Section(SectionItem):
+    pass
+
+
+class Article(SectionItem):
+    """
+    A section item with a semantic meaning of a self contained mini doc
+    (corresponding to HTML article element).
+    """
+
+    def __init__(self,
+                 header: core.Text,
+                 abstract_paragraphs: list,
+                 contents: SectionContents
+                 ):
+        super().__init__(header, contents)
+        self._abstract_paragraphs = abstract_paragraphs
+
+    @property
+    def abstract_paragraphs(self) -> list:
+        return self._abstract_paragraphs
+
+
+class SectionItemVisitor:
+    def visit(self, item: SectionItem):
+        if isinstance(item, Section):
+            return self.visit_section(item)
+        elif isinstance(item, Article):
+            return self.visit_article(item)
+        else:
+            raise TypeError('Unknown {}: {}'.format(SectionItem,
+                                                    str(item)))
+
+    def visit_section(self, section: Section):
+        raise NotImplemented('abstract method')
+
+    def visit_article(self, article: Article):
+        raise NotImplemented('abstract method')
--- test/exactly_lib_test/util/textformat/__init__.py
+++ test/exactly_lib_test/util/textformat/__init__.py
@@ -1,11 +1,12 @@
 import unittest
 
-from exactly_lib_test.util.textformat import formatting, parse, utils
+from exactly_lib_test.util.textformat import formatting, structure, parse, utils
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(formatting.suite())
+    ret_val.addTest(structure.suite())
     ret_val.addTest(parse.suite())
     ret_val.addTest(utils.suite())
     return ret_val
