79aafbfef082c715c540246b80a40a7f02b2325c
Emil Karlén <emil.karlen@etraveli.com>
2017-11-30 00:30:47 +0100

    help/htmldoc: REFACT: Do not use hard coded css classes

--- src/exactly_lib/help/html_doc/page_setup.py
+++ src/exactly_lib/help/html_doc/page_setup.py
@@ -1,79 +1,90 @@
 from xml.etree.ElementTree import Element, SubElement
 
 from exactly_lib import program_info
+from exactly_lib.help.html_doc import css_classes
 from exactly_lib.help_texts import formatting
+from exactly_lib.help_texts.entity import all_entity_types
 from exactly_lib.util.textformat.formatting.html.utils import ElementPopulator, ComplexElementPopulator
 
 PAGE_TITLE = 'Reference Manual for %s' % formatting.program_name(program_info.PROGRAM_NAME)
 TOC_TITLE = 'Table of Contents'
-ELEMENT_STYLES = """\
-pre {
+
+_CSS_CLASSES = {
+    'entity': css_classes.ENTITY,
+    'concept_entity': all_entity_types.CONCEPT_ENTITY_TYPE_NAMES,
+}
+
+ELEMENT_STYLES_ = """\
+pre {{
   background-color : #EEFFCC;
   padding: 7px;
   border: 1px solid #cEdFaC;
-}
+}}
 
-h1 {
+h1 {{
   background-color: #80bfff;
   padding: 5px;
-}
+}}
 
-article h1 {
+article h1 {{
     font-size: large;
     padding: 0px;
     background-color: transparent;
-}
+}}
 
-article h2 {
+article h2 {{
     font-size: medium;
-}
+}}
 
-article h3 {
+article h3 {{
     font-size: medium;
-}
+}}
 
-article header h1 {
+article header h1 {{
     padding: 7px;
     font-size: x-large;
     font-family: monospace;
     background-color: #c0c0c0;
-}
+}}
 
-article.entity.concept header h1 {
+article.{entity}.{concept_entity} header h1 {{
     font-family: inherit;
-}
+}}
 
-article header p {
+article header p {{
     font-style: italic;
     font-size: 110%;
-}
+}}
 
-table {
+table {{
   border-collapse: collapse;
-}
+}}
 
-th {
+th {{
   font-weight: normal;
   background-color: #f4f4f4;
   text-align: left;
   padding-right: 5px;
   vertical-align: top;
-}
+}}
 
-td {
+td {{
   vertical-align: top;
-}
+}}
 
-a:link {
+a:link {{
   text-decoration: none;
-}
+}}
 
 
-a:hover {
+a:hover {{
   text-decoration: underline;
-}
+}}
 
 """
+
+ELEMENT_STYLES = ELEMENT_STYLES_.format_map(_CSS_CLASSES)
+
 TITLE_STYLE = """\
 font-size: 250%;
 font-weight: bold;
