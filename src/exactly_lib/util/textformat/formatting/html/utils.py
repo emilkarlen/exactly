e4f6348e7aa9535dd91658ae54a1c0e75c680cc0
Emil Karlén <emil.karlen@fripost.org>
2017-12-01 15:43:24 +0100

    text-format: html rendering: Adds rendering of classes

--- src/exactly_lib/util/textformat/formatting/html/utils.py
+++ src/exactly_lib/util/textformat/formatting/html/utils.py
@@ -1,5 +1,7 @@
 from xml.etree.ElementTree import Element
 
+from exactly_lib.util.textformat.structure.core import TaggedItem
+
 
 class ElementPopulator:
     def apply(self, parent: Element):
@@ -14,3 +16,15 @@ class ComplexElementPopulator(ElementPopulator):
         for populator in self.populators:
             assert isinstance(populator, ElementPopulator)
             populator.apply(parent)
+
+
+def set_class_attribute(output: dict, tagged_item: TaggedItem):
+    if tagged_item.tags:
+        class_value = ' '.join(sorted(tagged_item.tags))
+        output['class'] = class_value
+
+
+def set_class_attribute_on_element(output: Element, tagged_item: TaggedItem):
+    if tagged_item.tags:
+        class_value = ' '.join(sorted(tagged_item.tags))
+        output.set('class', class_value)
