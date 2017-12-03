cbade01400be6649baab6bc969fb7b12e3e21320
Emil Karlén <emil.karlen@etraveli.com>
2017-12-03 16:55:58 +0100

    line-num-match: Implements assertion part "line_matches"

--- test/exactly_lib_test/instructions/assert_/utils/file_contents/parts/__init__.py
+++ test/exactly_lib_test/instructions/assert_/utils/file_contents/parts/__init__.py
@@ -1,10 +1,13 @@
 import unittest
 
-from exactly_lib_test.instructions.assert_.utils.file_contents.parts import file_assertion_part
+from exactly_lib_test.instructions.assert_.utils.file_contents.parts import file_assertion_part, line_matches
 
 
 def suite() -> unittest.TestSuite:
-    return file_assertion_part.suite()
+    return unittest.TestSuite([
+        file_assertion_part.suite(),
+        line_matches.suite(),
+    ])
 
 
 if __name__ == '__main__':
