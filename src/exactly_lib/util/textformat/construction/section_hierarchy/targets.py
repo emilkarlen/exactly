26335d98d52e42d00df9ba338700d0822d414e8a
Emil Karlén <emil.karlen@etraveli.com>
2017-12-03 04:11:00 +0100

    doc-build-to-util: Moves SectionContentsConstructorFromHierarchyGenerator to util

--- src/exactly_lib/util/textformat/construction/section_hierarchy/targets.py
+++ src/exactly_lib/util/textformat/construction/section_hierarchy/targets.py
@@ -62,6 +62,16 @@ class CustomTargetInfoFactory:
         raise NotImplementedError('abstract method')
 
 
+class NullCustomTargetInfoFactory(CustomTargetInfoFactory):
+    """A CustomTargetInfoFactory that build empty CrossReferenceTarget:s"""
+
+    def root(self, presentation: core.StringText) -> TargetInfo:
+        return TargetInfo(presentation, CrossReferenceTarget())
+
+    def sub_factory(self, local_name: str):
+        return self
+
+
 class CrossReferenceTextConstructor:
     def apply(self, x: CrossReferenceTarget) -> Text:
         raise NotImplementedError('abstract method')
