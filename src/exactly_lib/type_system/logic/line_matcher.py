74525b10fed8ddcea605411481ac02838b87ef8d
Emil Karlén <emil.karlen@fripost.org>
2017-12-03 13:26:31 +0100

    line-num-match: Adds line num to model: constant matcher

--- src/exactly_lib/type_system/logic/line_matcher.py
+++ src/exactly_lib/type_system/logic/line_matcher.py
@@ -2,7 +2,17 @@ from exactly_lib.type_system.logic.matcher_base_class import Matcher
 
 
 class LineMatcher(Matcher):
-    """Matches text lines."""
+    """
+    Matches text lines.
 
-    def matches(self, line: str) -> bool:
+    A line is a tuple (line number, line contents).
+
+    Line numbers start at 1.
+    """
+
+    def matches(self, line: tuple) -> bool:
+        """
+        :param line: (int, string) - (line number, line contents)
+        :return: If the line matches the condition represented by the matcher
+        """
         raise NotImplementedError('abstract method')
