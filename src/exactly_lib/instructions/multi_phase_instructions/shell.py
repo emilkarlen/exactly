4c108d8a277bdb928b1da516827dcc33dc83232a
Emil Karlén <emil.karlen@fripost.org>
2017-12-06 17:01:14 +0100

    file-proc-output: REFACT: shell: Moves help text to global variables

--- src/exactly_lib/instructions/multi_phase_instructions/shell.py
+++ src/exactly_lib/instructions/multi_phase_instructions/shell.py
@@ -10,6 +10,7 @@ from exactly_lib.instructions.multi_phase_instructions.utils.instruction_from_pa
 from exactly_lib.instructions.multi_phase_instructions.utils.instruction_part_utils import PartsParserFromEmbryoParser
 from exactly_lib.instructions.multi_phase_instructions.utils.instruction_parts import \
     InstructionPartsParser
+from exactly_lib.processing.exit_values import EXECUTION__HARD_ERROR
 from exactly_lib.section_document.parser_implementations.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
 from exactly_lib.section_document.parser_implementations.token_stream_parse_prime import TokenParserPrime
@@ -34,9 +35,9 @@ _COMMAND_SYNTAX_ELEMENT = 'COMMAND'
 _SINGLE_LINE_DESCRIPTION_FOR_NON_ASSERT_PHASE_INSTRUCTIONS = \
     "Executes a command using the current operating system's shell"
 
-SINGLE_LINE_DESCRIPTION_FOR_ASSERT_PHASE_INSTRUCTION = \
-    "Executes a command using the current operating system's shell," \
-    " and PASS if, and only if, its exit code is 0"
+SINGLE_LINE_DESCRIPTION_FOR_ASSERT_PHASE_INSTRUCTION = (
+    "Executes a command using the current operating system's shell,"
+    " and PASS if, and only if, its exit code is 0")
 
 
 class TheInstructionDocumentationBase(InstructionDocumentationWithCommandLineRenderingAndSplittedPartsForRestDocBase,
@@ -44,7 +45,10 @@ class TheInstructionDocumentationBase(InstructionDocumentationWithCommandLineRen
     def __init__(self,
                  name: str,
                  single_line_description: str):
-        super().__init__(name, {'COMMAND': _COMMAND_SYNTAX_ELEMENT})
+        super().__init__(name, {
+            'COMMAND': _COMMAND_SYNTAX_ELEMENT,
+            'HARD_ERROR': EXECUTION__HARD_ERROR.exit_identifier,
+        })
         self.__single_line_description = single_line_description
         self.command_arg = a.Named(_COMMAND_SYNTAX_ELEMENT)
 
@@ -52,24 +56,7 @@ class TheInstructionDocumentationBase(InstructionDocumentationWithCommandLineRen
         return self.__single_line_description
 
     def _main_description_rest_body(self) -> list:
-        text = """\
-        Which commands are available and the syntax of them depends
-        on the current operating system's shell and environment.
-
-
-        The shell takes care of interpreting {COMMAND}, so all features of the
-        shell can be used.
-
-
-        Use of the {instruction_name} instruction is not portable since it
-        uses the current operating system environment's shell.
-
-
-        On POSIX, the shell defaults to /bin/sh.
-
-        On Windows, the COMSPEC environment variable specifies the default shell.
-        """
-        return self._paragraphs(text)
+        return self._paragraphs(_MAIN_DESCRIPTION_REST_BODY)
 
     def invokation_variants(self) -> list:
         return [
@@ -80,12 +67,9 @@ class TheInstructionDocumentationBase(InstructionDocumentationWithCommandLineRen
         ]
 
     def syntax_element_descriptions(self) -> list:
-        text = """\
-        Something that is understood by the current operating system's shell.
-        """
         return [
             SyntaxElementDescription(self.command_arg.name,
-                                     self._paragraphs(text))
+                                     self._paragraphs(_COMMAND_SYNTAX_ELEMENT_DESCRIPTION))
         ]
 
 
@@ -95,10 +79,7 @@ class DescriptionForNonAssertPhaseInstruction(TheInstructionDocumentationBase):
                          _SINGLE_LINE_DESCRIPTION_FOR_NON_ASSERT_PHASE_INSTRUCTIONS)
 
     def _main_description_rest_prelude(self) -> list:
-        text = """\
-        It is considered an error if {COMMAND} exits with a non-zero exit code.
-        """
-        return self._paragraphs(text)
+        return self._paragraphs(_NON_ASSERT_PHASE_REST_PRELUDE)
 
 
 class SetupParser(spe_parts.ValidationAndSubProcessExecutionSetupParser):
@@ -119,3 +100,29 @@ class SetupParser(spe_parts.ValidationAndSubProcessExecutionSetupParser):
 _PARSE_FORMAT_MAP = {
     'COMMAND': _COMMAND_SYNTAX_ELEMENT
 }
+
+_MAIN_DESCRIPTION_REST_BODY = """\
+Which commands are available and the syntax of them depends
+on the current operating system's shell and environment.
+
+
+The shell takes care of interpreting {COMMAND}, so all features of the
+shell can be used.
+
+
+Use of the {instruction_name} instruction is not portable since it
+uses the current operating system environment's shell.
+
+
+On POSIX, the shell defaults to /bin/sh.
+
+On Windows, the COMSPEC environment variable specifies the default shell.
+"""
+
+_NON_ASSERT_PHASE_REST_PRELUDE = """\
+The result is {HARD_ERROR} if {COMMAND} exits with a non-zero exit code.
+"""
+
+_COMMAND_SYNTAX_ELEMENT_DESCRIPTION = """\
+Something that is understood by the current operating system's shell.
+"""
