1be4563ed65e1c1d25b57cb61ac64cbc7d3fa403
Emil Karlén <emil.karlen@etraveli.com>
2017-12-10 22:56:41 +0100

    file-proc-output: REFACT: "setup" phase: Uses std instruction names

--- src/exactly_lib/default/program_modes/test_case/phases/setup.py
+++ src/exactly_lib/default/program_modes/test_case/phases/setup.py
@@ -5,15 +5,15 @@ from exactly_lib.instructions.setup import change_dir, env, run, install, new_di
 
 INSTRUCTIONS = instruction_set_from_name_and_setup_constructor_list(
     [
-        ('dir', new_dir.setup),
-        ('env', env.setup),
-        (instruction_names.RUN_INSTRUCTION_NAME, run.setup),
-        (instruction_names.NEW_FILE_INSTRUCTION_NAME, new_file.setup),
-        ('install', install.setup),
-        (instruction_names.CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
-        (instruction_names.SHELL_INSTRUCTION_NAME, shell.setup),
         ('stdin', stdin.setup),
+        ('install', install.setup),
         (instruction_names.SYMBOL_DEFINITION_INSTRUCTION_NAME, define_symbol.setup),
+        (instruction_names.CHANGE_DIR_INSTRUCTION_NAME, change_dir.setup),
+        (instruction_names.ENV_VAR_INSTRUCTION_NAME, env.setup),
+        (instruction_names.NEW_FILE_INSTRUCTION_NAME, new_file.setup),
         (instruction_names.TRANSFORM_FILE_INSTRUCTION_NAME, transform.setup),
+        (instruction_names.NEW_DIR_INSTRUCTION_NAME, new_dir.setup),
+        (instruction_names.RUN_INSTRUCTION_NAME, run.setup),
+        (instruction_names.SHELL_INSTRUCTION_NAME, shell.setup),
     ]
 )
