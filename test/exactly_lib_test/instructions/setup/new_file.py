560a4b701c917873599786c4451d297c408469a6
Emil Karlén <emil.karlen@fripost.org>
2017-12-11 22:32:48 +0100

    file-proc-output: Makes test of integr of "file" in [setup] use common test suite

--- test/exactly_lib_test/instructions/setup/new_file.py
+++ test/exactly_lib_test/instructions/setup/new_file.py
@@ -1,123 +1,19 @@
 import unittest
 
-from exactly_lib.instructions.multi_phase_instructions import new_file
+from exactly_lib.common.instruction_setup import SingleInstructionSetup
 from exactly_lib.instructions.setup import new_file as sut
-from exactly_lib.section_document.parse_source import ParseSource
-from exactly_lib.symbol.data.restrictions.value_restrictions import FileRefRelativityRestriction
-from exactly_lib.test_case_file_structure.path_relativity import RelSdsOptionType
-from exactly_lib.type_system.data import file_refs
-from exactly_lib.type_system.data.concrete_path_parts import PathPartAsFixedPath
-from exactly_lib.util import symbol_table
-from exactly_lib.util.string import lines_content
-from exactly_lib_test.instructions.setup.test_resources.instruction_check import TestCaseBase, Arrangement, \
-    Expectation
-from exactly_lib_test.instructions.test_resources.single_line_source_instruction_utils import \
-    equivalent_source_variants__with_source_check
-from exactly_lib_test.section_document.test_resources.parse_source import argument_list_source
-from exactly_lib_test.section_document.test_resources.parse_source_assertions import every_line_is_consumed
-from exactly_lib_test.symbol.data.restrictions.test_resources.concrete_restriction_assertion import \
-    equals_file_ref_relativity_restriction
-from exactly_lib_test.symbol.data.test_resources.data_symbol_utils import file_ref_constant_container
-from exactly_lib_test.symbol.data.test_resources.symbol_reference_assertions import \
-    equals_symbol_reference_with_restriction_on_direct_target
-from exactly_lib_test.test_case.test_resources import sh_assertions
-from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_contents_check import \
-    act_dir_contains_exactly, tmp_user_dir_contains_exactly
-from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_populator import contents_in
-from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir, Dir, empty_file, File
-from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
-from exactly_lib_test.util.test_resources.symbol_tables import symbol_table_from_entries
+from exactly_lib_test.instructions.multi_phase_instructions.instruction_integration_test_resources import \
+    new_file_instruction_test
+from exactly_lib_test.instructions.setup.test_resources.configuration import SetupConfigurationBase
 
 
 def suite() -> unittest.TestSuite:
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(unittest.makeSuite(TestCasesThatTestIntegrationByAFewRandomTests))
-    ret_val.addTest(unittest.makeSuite(TestCasesThatTestIntegrationOfValueDefinitionByAFewRandomTests))
-    return ret_val
+    return new_file_instruction_test.suite_for(TheConfiguration())
 
 
-class TestCaseBaseForParser(TestCaseBase):
-    def _run(self,
-             source: ParseSource,
-             arrangement: Arrangement,
-             expectation: Expectation):
-        self._check(sut.setup('instr-name'), source, arrangement, expectation)
-
-
-class TestCasesThatTestIntegrationByAFewRandomTests(TestCaseBaseForParser):
-    def test_file_in_sub_dir__sub_dir_exists(self):
-        instruction_argument = 'existing-directory/file-name.txt'
-        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
-            self._run(source,
-                      Arrangement(sds_contents_before_main=contents_in(RelSdsOptionType.REL_ACT,
-                                                                       DirContents([
-                                                                           empty_dir('existing-directory')
-                                                                       ]))
-                                  ),
-                      Expectation(main_side_effects_on_sds=act_dir_contains_exactly(DirContents([
-                          Dir('existing-directory', [
-                              empty_file('file-name.txt')])
-                      ])))
-                      )
-
-    def test_single_line_contents(self):
-        self._run(argument_list_source(['file name', new_file.CONTENTS_ASSIGNMENT_TOKEN, '<<MARKER'],
-                                       ['single line',
-                                        'MARKER']),
-                  Arrangement(),
-                  Expectation(main_side_effects_on_sds=act_dir_contains_exactly(DirContents([
-                      File('file name',
-                           lines_content(['single line']))
-                  ])),
-                      source=every_line_is_consumed)
-                  )
-
-    def test_argument_is_existing_file(self):
-        instruction_argument = 'existing-file'
-        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
-            self._run(source,
-                      Arrangement(sds_contents_before_main=contents_in(RelSdsOptionType.REL_ACT,
-                                                                       DirContents([
-                                                                           empty_file('existing-file')
-                                                                       ]))),
-                      Expectation(main_result=sh_assertions.is_hard_error())
-                      )
-
-
-class TestCasesThatTestIntegrationOfValueDefinitionByAFewRandomTests(TestCaseBaseForParser):
-    def test_symbol_with_relativity_that_is_not_default_relativity(self):
-        instruction_argument = '--rel SYMBOL_NAME file-name.txt'
-        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
-            self._run(source,
-                      Arrangement(
-                          symbols=symbol_table_from_entries([
-                              symbol_table.Entry('SYMBOL_NAME',
-                                                 file_ref_constant_container(
-                                                     file_refs.rel_tmp_user(
-                                                         PathPartAsFixedPath('symbol-def-path-arg'))))])
-                      ),
-                      Expectation(
-                          main_side_effects_on_sds=tmp_user_dir_contains_exactly(DirContents([
-                              Dir('symbol-def-path-arg',
-                                  [empty_file('file-name.txt')])
-                          ])),
-                          symbol_usages=asrt.matches_sequence([
-                              equals_symbol_reference_with_restriction_on_direct_target(
-                                  'SYMBOL_NAME',
-                                  equals_file_ref_relativity_restriction(
-                                      FileRefRelativityRestriction(new_file.RELATIVITY_VARIANTS)))
-                          ]),
-                      )
-                      )
-
-    def test_WHEN_no_symbol_reference_THEN_no_symbol_usages(self):
-        instruction_argument = '--rel-tmp file-name.txt'
-        for source in equivalent_source_variants__with_source_check(self, instruction_argument):
-            self._run(source,
-                      Arrangement(),
-                      Expectation(
-                          symbol_usages=asrt.is_empty),
-                      )
+class TheConfiguration(SetupConfigurationBase):
+    def instruction_setup(self) -> SingleInstructionSetup:
+        return sut.setup('instruction name')
 
 
 if __name__ == '__main__':
