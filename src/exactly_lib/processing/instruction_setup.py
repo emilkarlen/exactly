e56eeebf74280df32864c8723d90e8accdddfaa8
Emil Karlén <emil.karlen@etraveli.com>
2017-12-17 18:18:19 +0100

    instr-fr-suite: Adds Py typing to InstructionsSetup

--- src/exactly_lib/processing/instruction_setup.py
+++ src/exactly_lib/processing/instruction_setup.py
@@ -1,15 +1,18 @@
+from typing import Dict
+
+from exactly_lib.common.instruction_setup import SingleInstructionSetup
+
+
 class InstructionsSetup(tuple):
     def __new__(cls,
-                config_instruction_set: dict,
-                setup_instruction_set: dict,
-                before_assert_instruction_set: dict,
-                assert_instruction_set: dict,
-                cleanup_instruction_set: dict):
+                config_instruction_set: Dict[str, SingleInstructionSetup],
+                setup_instruction_set: Dict[str, SingleInstructionSetup],
+                before_assert_instruction_set: Dict[str, SingleInstructionSetup],
+                assert_instruction_set: Dict[str, SingleInstructionSetup],
+                cleanup_instruction_set: Dict[str, SingleInstructionSetup]):
         """
-        Each dictionary is a mapping: instruction-name -> `SingleInstructionSetup`.
-
         Each SingleInstructionSetup should parse and construct an instruction for
-         the correct phase (of course). I.e., sub classes of Instruction.
+        the correct phase (of course). I.e., sub classes of Instruction.
         """
         return tuple.__new__(cls, (config_instruction_set,
                                    setup_instruction_set,
@@ -18,21 +21,21 @@ class InstructionsSetup(tuple):
                                    cleanup_instruction_set))
 
     @property
-    def config_instruction_set(self) -> dict:
+    def config_instruction_set(self) -> Dict[str, SingleInstructionSetup]:
         return self[0]
 
     @property
-    def setup_instruction_set(self) -> dict:
+    def setup_instruction_set(self) -> Dict[str, SingleInstructionSetup]:
         return self[1]
 
     @property
-    def before_assert_instruction_set(self) -> dict:
+    def before_assert_instruction_set(self) -> Dict[str, SingleInstructionSetup]:
         return self[2]
 
     @property
-    def assert_instruction_set(self) -> dict:
+    def assert_instruction_set(self) -> Dict[str, SingleInstructionSetup]:
         return self[3]
 
     @property
-    def cleanup_instruction_set(self) -> dict:
+    def cleanup_instruction_set(self) -> Dict[str, SingleInstructionSetup]:
         return self[4]
