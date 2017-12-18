3ff96ee27b7fef12d672405452e1ec4405de551c
Emil Karlén <emil.karlen@fripost.org>
2017-12-18 20:31:22 +0100

    instr-fr-suite: Adds type annotations for instruction_name_extractor_function

--- src/exactly_lib/default/program_modes/test_case/test_case_parser.py
+++ src/exactly_lib/default/program_modes/test_case/test_case_parser.py
@@ -1,10 +1,10 @@
-from typing import Callable
-
 from exactly_lib.help_texts.test_case import phase_names_plain
 from exactly_lib.processing.instruction_setup import InstructionsSetup
 from exactly_lib.processing.parse.instruction_section_element_parser import section_element_parser
 from exactly_lib.section_document import document_parser
 from exactly_lib.section_document.parse_source import ParseSource
+from exactly_lib.section_document.parser_implementations.parser_for_dictionary_of_instructions import \
+    InstructionNameExtractor
 from exactly_lib.test_case import test_case_doc, phase_identifier
 
 
@@ -26,7 +26,7 @@ class Parser:
         )
 
 
-def new_parser(instruction_name_extractor_function: Callable[[str], str],
+def new_parser(instruction_name_extractor_function: InstructionNameExtractor,
                act_phase_parser: document_parser.SectionElementParser,
                instructions_setup: InstructionsSetup) -> Parser:
     def dict_parser(instruction_set: dict) -> document_parser.SectionElementParser:
