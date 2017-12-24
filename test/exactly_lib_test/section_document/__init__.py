fed399a0440127bcae4d632cdfe50bc483fd8a0d
Emil Karlén <emil.karlen@fripost.org>
2017-12-24 16:20:33 +0100

    instr-fr-suite: Introduces assertion: equals_line_sequence

--- test/exactly_lib_test/section_document/__init__.py
+++ test/exactly_lib_test/section_document/__init__.py
@@ -1,11 +1,12 @@
 import unittest
 
-from exactly_lib_test.section_document import test_syntax, parser_implementations, \
+from exactly_lib_test.section_document import test_resources_test, test_syntax, parser_implementations, \
     parse_source, document_parser
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
+    ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(test_syntax.suite())
     ret_val.addTest(parse_source.suite())
     ret_val.addTest(document_parser.suite())
