79fc558fa3f5ba38b8c7d799cbd562e3db9d13b1
Emil Karlén <emil.karlen@fripost.org>
2017-12-28 14:34:10 +0100

    src-file-include: Adds test helpers to repo (storage)

--- storage/test_file_via_default_main_program.py
+++ storage/test_file_via_default_main_program.py
@@ -2,14 +2,26 @@ import unittest
 
 from exactly_lib.default.default_main_program_setup import default_main_program
 
-TEST_CASE_FILE = '/Users/emil/vcs/exactly/0/err_msg_tests/source-file-info/syntax-error-in-stand-alone-case.case'
+SRC_DIR = '/Users/emil/vcs/exactly/0/err_msg_tests/source-file-info/'
 
+TEST_CASE_FILE = SRC_DIR + 'syntax-error-in-stand-alone-case.case'
 
-# TEST_CASE_FILE = '/Users/emil/vcs/exactly/0/err_msg_tests/source-file-info/instruction-error-in-stand-alone-case.case'
+# TEST_CASE_FILE = SRC_DIR + 'instruction-error-in-stand-alone-case.case'
+
+# ARGS = [TEST_CASE_FILE]
+
+SUITE_FILE = SRC_DIR + 'instruction-error-of-case-instruction-in-suite.suite'
+
+EMPTY_CASE = SRC_DIR + 'empty.case'
+
+ARGS = ['--suite',
+        SUITE_FILE,
+        EMPTY_CASE
+        ]
 
 
 class TC(unittest.TestCase):
     def runTest(self):
         main_program = default_main_program()
-        exit_code = main_program.execute([TEST_CASE_FILE])
+        exit_code = main_program.execute(ARGS)
         print(str(exit_code))
