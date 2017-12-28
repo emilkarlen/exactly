c34b3b3cfa58248bbb70f9607c87b19f98b2464d
Emil Karlén <emil.karlen@fripost.org>
2017-12-28 21:10:44 +0100

    line_source_assertions: REFACT: Uses NEA (instead of NameAndValue+EA)

--- test/exactly_lib_test/util/test_resources_test/line_source_assertions.py
+++ test/exactly_lib_test/util/test_resources_test/line_source_assertions.py
@@ -2,9 +2,8 @@ import pathlib
 import unittest
 
 from exactly_lib.util.line_source import LineSequence, Line, SourceLocation
-from exactly_lib_test.test_resources.name_and_value import NameAndValue
 from exactly_lib_test.test_resources.test_of_test_resources_util import assert_that_assertion_fails
-from exactly_lib_test.test_resources.test_utils import EA, NEA
+from exactly_lib_test.test_resources.test_utils import NEA
 from exactly_lib_test.util.test_resources import line_source_assertions as sut
 
 
@@ -28,60 +27,62 @@ class TestEqualsLine(unittest.TestCase):
     def test_SHOULD_not_be_equal(self):
         # ARRANGE #
         cases = [
-            NameAndValue('unequal line number',
-                         EA(expected=Line(2, 'text'),
-                            actual=Line(1, 'text'))
-                         ),
-            NameAndValue('unequal text',
-                         EA(expected=Line(1, 'expected'),
-                            actual=Line(1, 'actual'))
-                         ),
+            NEA('unequal line number',
+                expected=Line(2, 'text'),
+                actual=Line(1, 'text')
+                ),
+            NEA('unequal text',
+                expected=Line(1, 'expected'),
+                actual=Line(1, 'actual')
+                ),
         ]
 
         for name_and_ea in cases:
             with self.subTest(name_and_ea.name):
-                assertion = sut.equals_line(name_and_ea.value.expected)
+                assertion = sut.equals_line(name_and_ea.expected)
                 # ACT & ASSERT #
-                assert_that_assertion_fails(assertion, name_and_ea.value.actual)
+                assert_that_assertion_fails(assertion, name_and_ea.actual)
 
 
 class TestEqualsLineSequence(unittest.TestCase):
     def test_SHOULD_be_equal(self):
         # ARRANGE #
         cases = [
-            NameAndValue('without lines',
-                         EA(expected=LineSequence(1, ()),
-                            actual=LineSequence(1, ()))),
-            NameAndValue('with lines',
-                         EA(expected=LineSequence(2, ('first', 'second')),
-                            actual=LineSequence(2, ('first', 'second')))),
+            NEA('without lines',
+                expected=LineSequence(1, ()),
+                actual=LineSequence(1, ())
+                ),
+            NEA('with lines',
+                expected=LineSequence(2, ('first', 'second')),
+                actual=LineSequence(2, ('first', 'second'))
+                ),
         ]
 
         for name_and_ea in cases:
             with self.subTest(name_and_ea.name):
-                assertion = sut.equals_line_sequence(name_and_ea.value.expected)
+                assertion = sut.equals_line_sequence(name_and_ea.expected)
                 # ACT & ASSERT #
-                assertion.apply_without_message(self, name_and_ea.value.actual)
+                assertion.apply_without_message(self, name_and_ea.actual)
 
     def test_SHOULD_not_be_equal(self):
         # ARRANGE #
         cases = [
-            NameAndValue('unequal first line',
-                         EA(expected=LineSequence(1, ()),
-                            actual=LineSequence(2, ()))),
-            NameAndValue('unequal single line',
-                         EA(expected=LineSequence(1, ('expected',)),
-                            actual=LineSequence(1, ('actual',)))),
-            NameAndValue('unequal number of lines',
-                         EA(expected=LineSequence(1, ('line', 'line')),
-                            actual=LineSequence(1, ('line',)))),
+            NEA('unequal first line',
+                expected=LineSequence(1, ()),
+                actual=LineSequence(2, ())),
+            NEA('unequal single line',
+                expected=LineSequence(1, ('expected',)),
+                actual=LineSequence(1, ('actual',))),
+            NEA('unequal number of lines',
+                expected=LineSequence(1, ('line', 'line')),
+                actual=LineSequence(1, ('line',))),
         ]
 
         for name_and_ea in cases:
             with self.subTest(name_and_ea.name):
-                assertion = sut.equals_line_sequence(name_and_ea.value.expected)
+                assertion = sut.equals_line_sequence(name_and_ea.expected)
                 # ACT & ASSERT #
-                assert_that_assertion_fails(assertion, name_and_ea.value.actual)
+                assert_that_assertion_fails(assertion, name_and_ea.actual)
 
 
 class TestEqualsSourceLocation(unittest.TestCase):
