07c047fdf7f48d1da50c081d0ab764c69d355197
Emil Karlén <emil@member.fsf.org>
2017-12-29 00:21:54 +0100

    sec-doc-parse: Test of SectionContentElementBuilder

--- test/exactly_lib_test/section_document/__init__.py
+++ test/exactly_lib_test/section_document/__init__.py
@@ -2,7 +2,7 @@ import unittest
 
 from exactly_lib_test.section_document import parse
 from exactly_lib_test.section_document import test_resources_test
-from exactly_lib_test.section_document import test_syntax, parser_implementations, \
+from exactly_lib_test.section_document import test_syntax, element_builder, parser_implementations, \
     parse_source
 
 
@@ -10,6 +10,7 @@ def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(test_syntax.suite())
+    ret_val.addTest(element_builder.suite())
     ret_val.addTest(parse_source.suite())
     ret_val.addTest(parse.suite())
     ret_val.addTest(parser_implementations.suite())
