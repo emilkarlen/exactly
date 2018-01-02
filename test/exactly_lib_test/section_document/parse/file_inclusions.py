be9c085b1a53169c52fe487fd8f5e9e2058f94dc
Emil Karlén <emil@member.fsf.org>
2018-01-02 19:32:02 +0100

    file-incl-parse: TestInclusionFromInclusion / test_source_locations_of_file_source_error

--- test/exactly_lib_test/section_document/parse/file_inclusions.py
+++ test/exactly_lib_test/section_document/parse/file_inclusions.py
@@ -8,10 +8,12 @@ from exactly_lib.section_document.model import SectionContentElement
 from exactly_lib.section_document.syntax import section_header
 from exactly_lib.util.line_source import SourceLocation, single_line_sequence
 from exactly_lib_test.section_document.parse.test_resources_for_parse_file import SECTION_1_NAME, \
-    ARBITRARY_INSTRUCTION_SOURCE_LINE, NO_FILE_INCLUSIONS, Expectation, check, \
+    ARBITRARY_OK_INSTRUCTION_SOURCE_LINE, NO_FILE_INCLUSIONS, Expectation, check, \
     matches_file_access_error, std_conf_arrangement, is_file_access_error, check_and_expect_exception, \
-    inclusion_of_file, SectionElementParserForInstructionAndInclusionLines, SECTION_2_NAME, Arrangement, \
-    is_file_source_error, matches_file_source_error, inclusion_of_list_of_files
+    inclusion_of_file, SectionElementParserForInclusionDirectiveAndOkAndInvalidInstructions, SECTION_2_NAME, \
+    Arrangement, \
+    is_file_source_error, matches_file_source_error, inclusion_of_list_of_files, ok_instruction, \
+    syntax_error_instruction
 from exactly_lib_test.section_document.test_resources.section_contents_elements import \
     equals_instruction_without_description
 from exactly_lib_test.test_resources.file_structure import DirContents, empty_dir, sym_link, file_with_lines, \
@@ -119,7 +121,7 @@ class TestInclusionDirectiveIsNotAllowedOutsideOfSection(unittest.TestCase):
         # ARRANGE #
         included_file_lines = [
             section_header(SECTION_1_NAME),
-            ARBITRARY_INSTRUCTION_SOURCE_LINE,
+            ARBITRARY_OK_INSTRUCTION_SOURCE_LINE,
         ]
         included_file = file_with_lines('included.src', included_file_lines)
 
@@ -166,7 +168,7 @@ class TestSectionSwitching(unittest.TestCase):
         included_file_name = 'included.src'
         included_file_path = Path(included_file_name)
 
-        instruction_1_in_included_file = 'instruction 1 in included file'
+        instruction_1_in_included_file = ok_instruction('instruction 1 in included file')
 
         cases = [
             NameAndValue(
@@ -234,8 +236,8 @@ class TestSectionSwitching(unittest.TestCase):
         included_file_name = 'included.src'
         included_file_path = Path(included_file_name)
 
-        instruction_1_in_included_file = 'instruction 1 in included file'
-        instruction_1_in_root_file = 'instruction 1 in root file'
+        instruction_1_in_included_file = ok_instruction('instruction 1 in included file')
+        instruction_1_in_root_file = ok_instruction('instruction 1 in root file')
 
         setup = SingleFileInclusionCheckSetup(
             sections_conf=SECTION_1_AND_2_WITHOUT_DEFAULT,
@@ -283,9 +285,9 @@ class TestCombinationOfDocuments(unittest.TestCase):
         included_file_name = 'included.src'
         included_file_path = Path(included_file_name)
 
-        instruction_1_in_included_file = 'instruction 1 in included file'
-        instruction_1_in_root_file = 'instruction 1 in root file'
-        instruction_2_in_root_file = 'instruction 2 in root file'
+        instruction_1_in_included_file = ok_instruction('instruction 1 in included file')
+        instruction_1_in_root_file = ok_instruction('instruction 1 in root file')
+        instruction_2_in_root_file = ok_instruction('instruction 2 in root file')
 
         cases = [
             NameAndValue(
@@ -489,10 +491,10 @@ class TestCombinationOfDocuments(unittest.TestCase):
         included_file_1_name = 'included-1.src'
         included_file_2_name = 'included-2.src'
 
-        instruction_in_included_file_1 = 'instruction in included file 1'
-        instruction_in_included_file_2 = 'instruction in included file 2'
-        instruction_1_in_root_file = 'instruction 1 in root file'
-        instruction_2_in_root_file = 'instruction 1 in root file'
+        instruction_in_included_file_1 = ok_instruction('instruction in included file 1')
+        instruction_in_included_file_2 = ok_instruction('instruction in included file 2')
+        instruction_1_in_root_file = ok_instruction('instruction 1 in root file')
+        instruction_2_in_root_file = ok_instruction('instruction 1 in root file')
 
         inclusion_directive_of_file_1_and_2 = inclusion_of_list_of_files([included_file_1_name,
                                                                           included_file_2_name])
@@ -565,8 +567,8 @@ class TestCombinationOfDocuments(unittest.TestCase):
         root_file_name = 'root.src'
         root_file_path = Path(root_file_name)
 
-        instruction_1_in_root_file = 'instruction 1 in root file'
-        instruction_2_in_root_file = 'instruction 1 in root file'
+        instruction_1_in_root_file = ok_instruction('instruction 1 in root file')
+        instruction_2_in_root_file = ok_instruction('instruction 1 in root file')
 
         inclusion_directive_of_file_1_and_2 = inclusion_of_list_of_files([])
 
@@ -614,7 +616,7 @@ class TestMultipleInclusionsOfSameFile(unittest.TestCase):
         included_file_name = 'included.src'
         included_file_path = Path(included_file_name)
 
-        instruction_in_included_file = 'instruction in included file'
+        instruction_in_included_file = ok_instruction('instruction in included file')
 
         cases = [
             NameAndValue(
@@ -741,10 +743,10 @@ class TestInclusionFromInclusion(unittest.TestCase):
     def test_instruction_source_locations(self):
         # ARRANGE #
 
-        instruction_in_root_file = 'instruction 1 in root file'
+        instruction_in_root_file = ok_instruction('instruction 1 in root file')
 
-        instruction_in_included_file_1 = 'instruction 1 in included file 1'
-        instruction_in_included_file_2 = 'instruction 1 in included file 2'
+        instruction_in_included_file_1 = ok_instruction('instruction 1 in included file 1')
+        instruction_in_included_file_2 = ok_instruction('instruction 1 in included file 2')
 
         included_file_2 = NameAndValue('included-file-2.src', [
             instruction_in_included_file_2,
@@ -836,6 +838,44 @@ class TestInclusionFromInclusion(unittest.TestCase):
         check_and_expect_exception(self, arrangement,
                                    is_file_access_error(expected_file_access_error))
 
+    def test_source_locations_of_file_source_error(self):
+        # ARRANGE #
+        error_message = 'the error message'
+        included_file_2 = NameAndValue('included-file-2.src', [
+            syntax_error_instruction(error_message),
+        ])
+
+        included_file_1 = NameAndValue('included-file-1.src', [
+            inclusion_of_file(included_file_2.name),
+        ])
+
+        root_file = NameAndValue('root.src', [
+            section_header(SECTION_1_NAME),
+            inclusion_of_file(included_file_1.name),
+        ])
+        root_file_path = Path(root_file.name)
+
+        arrangement = arrangement_of(SECTION_1_AND_2_WITHOUT_DEFAULT,
+                                     root_file,
+                                     [included_file_1, included_file_2])
+        expected_file_source_error = matches_file_source_error(
+            asrt.equals(SECTION_1_NAME),
+            [
+                SourceLocation(single_line_sequence(2,
+                                                    inclusion_of_file(included_file_1.name)),
+                               root_file_path),
+                SourceLocation(single_line_sequence(1, inclusion_of_file(included_file_2.name)),
+                               Path(included_file_1.name)),
+                SourceLocation(single_line_sequence(1,
+                                                    syntax_error_instruction(error_message)),
+                               Path(included_file_2.name)),
+
+            ]
+        )
+        # ACT & ASSERT #
+        check_and_expect_exception(self, arrangement,
+                                   is_file_source_error(expected_file_source_error))
+
 
 def check_single_file_inclusions(put: unittest.TestCase,
                                  setup: SingleFileInclusionCheckSetup,
@@ -864,16 +904,20 @@ def arrangement_of(sections_configuration: SectionsConfiguration,
 
 
 SECTION_1_AND_2_WITHOUT_DEFAULT = SectionsConfiguration([
-    SectionConfiguration(SECTION_1_NAME, SectionElementParserForInstructionAndInclusionLines(SECTION_1_NAME)
+    SectionConfiguration(SECTION_1_NAME,
+                         SectionElementParserForInclusionDirectiveAndOkAndInvalidInstructions(SECTION_1_NAME)
                          ),
-    SectionConfiguration(SECTION_2_NAME, SectionElementParserForInstructionAndInclusionLines(SECTION_2_NAME)
+    SectionConfiguration(SECTION_2_NAME,
+                         SectionElementParserForInclusionDirectiveAndOkAndInvalidInstructions(SECTION_2_NAME)
                          ),
 ])
 
 SECTION_1_AND_2_WITH_SECTION_1_AS_DEFAULT = SectionsConfiguration([
-    SectionConfiguration(SECTION_1_NAME, SectionElementParserForInstructionAndInclusionLines(SECTION_1_NAME)
+    SectionConfiguration(SECTION_1_NAME,
+                         SectionElementParserForInclusionDirectiveAndOkAndInvalidInstructions(SECTION_1_NAME)
                          ),
-    SectionConfiguration(SECTION_2_NAME, SectionElementParserForInstructionAndInclusionLines(SECTION_2_NAME)
+    SectionConfiguration(SECTION_2_NAME,
+                         SectionElementParserForInclusionDirectiveAndOkAndInvalidInstructions(SECTION_2_NAME)
                          ),
 ],
     default_section_name=SECTION_1_NAME)
