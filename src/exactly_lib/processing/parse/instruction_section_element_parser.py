2a397050319802dda41ac268a6ceee8699082332
Emil Karlén <emil@member.fsf.org>
2018-01-14 20:58:56 +0100

    file-incl-parse: Integrates file inclusion directive into test case parser
    for all phases

--- src/exactly_lib/processing/parse/instruction_section_element_parser.py
+++ src/exactly_lib/processing/parse/instruction_section_element_parser.py
@@ -1,12 +1,14 @@
 from typing import Dict
 
+from exactly_lib.help_texts.test_case.instructions import instruction_names
+from exactly_lib.processing.parse.file_inclusion_directive_parser import FileInclusionDirectiveParser
 from exactly_lib.section_document import document_parser
 from exactly_lib.section_document.element_parsers.optional_description_and_instruction_parser import \
     InstructionWithOptionalDescriptionParser
 from exactly_lib.section_document.element_parsers.parser_for_dictionary_of_instructions import \
     InstructionParserForDictionaryOfInstructions, InstructionNameExtractor
-from exactly_lib.section_document.element_parsers.section_element_parsers import standard_syntax_element_parser, \
-    InstructionParser
+from exactly_lib.section_document.element_parsers.section_element_parsers import InstructionParser, \
+    ParserFromSequenceOfParsers, StandardSyntaxCommentAndEmptyLineParser
 
 
 def instruction_parser(instruction_name_extractor_function: InstructionNameExtractor,
@@ -16,8 +18,11 @@ def instruction_parser(instruction_name_extractor_function: InstructionNameExtra
 
 
 def section_element_parser_of(the_instruction_parser: InstructionParser) -> document_parser.SectionElementParser:
-    return standard_syntax_element_parser(
-        InstructionWithOptionalDescriptionParser(the_instruction_parser))
+    return ParserFromSequenceOfParsers([
+        StandardSyntaxCommentAndEmptyLineParser(),
+        FileInclusionDirectiveParser(instruction_names.FILE_INCLUSION_DIRECTIVE_NAME),
+        InstructionWithOptionalDescriptionParser(the_instruction_parser)
+    ])
 
 
 def section_element_parser(instruction_name_extractor_function: InstructionNameExtractor,
