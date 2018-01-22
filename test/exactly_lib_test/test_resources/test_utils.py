e4bf494a1da137ba2ae13e697f41be95e659bc78
Emil Karlén <emil@member.fsf.org>
2018-01-22 20:51:18 +0100

    file-incl-parse: Adds test resource: NIE

--- test/exactly_lib_test/test_resources/test_utils.py
+++ test/exactly_lib_test/test_resources/test_utils.py
@@ -18,3 +18,15 @@ class NEA:
         self.name = name
         self.expected = expected
         self.actual = actual
+
+
+class NIE:
+    """A name, one expected value and one input value."""
+
+    def __init__(self,
+                 name: str,
+                 expected_value,
+                 input_value):
+        self.name = name
+        self.expected_value = expected_value
+        self.input_value = input_value
