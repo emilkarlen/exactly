d3185b1b604cbff5b9fafd203e485189c4c9d14d
Emil Karlén <emil@member.fsf.org>
2018-03-19 02:01:04 +0100

    def-str-hd: Adds feature to assign a string symbol from a here-doc

--- test/exactly_lib_test/instructions/multi_phase_instructions/define_symbol/string_type.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/define_symbol/string_type.py
@@ -9,9 +9,11 @@ from exactly_lib_test.instructions.multi_phase_instructions.define_symbol.test_c
 from exactly_lib_test.instructions.multi_phase_instructions.define_symbol.test_resources import *
 from exactly_lib_test.instructions.multi_phase_instructions.test_resources.instruction_embryo_check import Expectation
 from exactly_lib_test.instructions.test_resources.arrangements import ArrangementWithSds
+from exactly_lib_test.section_document.test_resources import parse_source_assertions as asrt_source
 from exactly_lib_test.symbol.data.test_resources import symbol_structure_assertions as vs_asrt
 from exactly_lib_test.symbol.data.test_resources.data_symbol_utils import string_constant_container, \
     container
+from exactly_lib_test.symbol.data.test_resources.here_doc_assertion_utils import here_doc_lines
 from exactly_lib_test.symbol.data.test_resources.symbol_structure_assertions import equals_container
 from exactly_lib_test.test_case_utils.parse.parse_string import string_resolver_from_fragments
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
@@ -22,6 +24,7 @@ def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         unittest.makeSuite(TestFailingParseDueToInvalidSyntax),
         unittest.makeSuite(TestSuccessfulDefinition),
+        unittest.makeSuite(TestSuccessfulDefinitionFromHereDocument),
     ])
 
 
@@ -123,6 +126,66 @@ class TestSuccessfulDefinition(TestCaseBaseForParser):
             constant(' between '),
             symbol(referred_symbol2.name),
         ])
+        container_of_expected_resolver = container(expected_resolver)
+        expected_definition = SymbolDefinition(name_of_defined_symbol,
+                                               container_of_expected_resolver)
+        expectation = Expectation(
+            symbol_usages=asrt.matches_sequence([
+                vs_asrt.equals_symbol(expected_definition, ignore_source_line=True),
+            ]),
+            symbols_after_main=assert_symbol_table_is_singleton(
+                name_of_defined_symbol,
+                equals_container(container_of_expected_resolver),
+            )
+        )
+        # ACT & ASSERT #
+        self._check(source, ArrangementWithSds(), expectation)
+
+
+class TestSuccessfulDefinitionFromHereDocument(TestCaseBaseForParser):
+    def test_assignment_of_single_constant_line(self):
+        value_str = 'value'
+        symbol_name = 'name1'
+        sb = SB.new_with(value_str=value_str,
+                         symbol_name=symbol_name)
+
+        source = sb.lines(
+            here_doc_lines(first_line_start='{string_type} {symbol_name} = ',
+                           marker='EOF',
+                           contents_lines=['{value_str}']) +
+            ['following line']
+        )
+        # EXPECTATION #
+        expected_container = string_constant_container(value_str + '\n')
+        expected_definition = SymbolDefinition(symbol_name, expected_container)
+        expectation = Expectation(
+            symbol_usages=asrt.matches_sequence([
+                vs_asrt.equals_symbol(expected_definition, ignore_source_line=True)
+            ]),
+            symbols_after_main=assert_symbol_table_is_singleton(
+                symbol_name,
+                equals_container(expected_container),
+            ),
+            source=asrt_source.is_at_beginning_of_line(4)
+        )
+        # ACT & ASSERT #
+        self._check(source, ArrangementWithSds(), expectation)
+
+    def test_assignment_of_single_symbol_reference(self):
+        # ARRANGE #
+        referred_symbol = SymbolWithReferenceSyntax('referred_symbol')
+        name_of_defined_symbol = 'defined_symbol'
+        sb = SB.new_with(referred_symbol=referred_symbol,
+                         name_of_defined_symbol=name_of_defined_symbol)
+
+        source = sb.lines(
+            here_doc_lines(first_line_start='{string_type} {name_of_defined_symbol} = ',
+                           marker='EOF',
+                           contents_lines=[str(referred_symbol)])
+        )
+        # EXPECTATION #
+        expected_resolver = string_resolver_from_fragments([symbol(referred_symbol.name),
+                                                            constant('\n')])
         container_of_expected_resolver = container(expected_resolver)
         expected_definition = SymbolDefinition(name_of_defined_symbol,
                                                container_of_expected_resolver)
--- test/exactly_lib_test/symbol/data/test_resources/here_doc_assertion_utils.py
+++ test/exactly_lib_test/symbol/data/test_resources/here_doc_assertion_utils.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from exactly_lib.test_case_utils.parse.parse_here_document import DOCUMENT_MARKER_PREFIX
 from exactly_lib.util.string import lines_content
 from exactly_lib.util.symbol_table import SymbolTable, empty_symbol_table
 from exactly_lib_test.symbol.data.test_resources import string_assertions
@@ -6,6 +9,15 @@ from exactly_lib_test.test_resources.value_assertions import value_assertion as 
 
 def contents_str_from_lines(lines_of_here_doc: list) -> str:
     return lines_content(lines_of_here_doc)
+
+
+def here_doc_lines(marker: str,
+                   contents_lines: List[str],
+                   first_line_start: str = '') -> List[str]:
+    return ([first_line_start + DOCUMENT_MARKER_PREFIX + marker] +
+            contents_lines +
+            [marker]
+            )
 
 
 def matches_resolved_value(expected_resolved_primitive_lines: list,
