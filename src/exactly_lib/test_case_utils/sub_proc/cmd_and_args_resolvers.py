7f4583c0b6ed512a7c893f57c0162219514e0dc2
Emil Karlén <emil.karlen@etraveli.com>
2018-03-23 21:21:19 +0100

    exe-pgm: REFACT: Moves impl class for "run" to "run" module

--- src/exactly_lib/test_case_utils/sub_proc/cmd_and_args_resolvers.py
+++ src/exactly_lib/test_case_utils/sub_proc/cmd_and_args_resolvers.py
@@ -1,6 +1,5 @@
 from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.symbol.resolver_structure import DataValueResolver
-from exactly_lib.test_case_utils.sub_proc.executable_file import ExecutableFile
 from exactly_lib.test_case_utils.sub_proc.sub_process_execution import CmdAndArgsResolver
 
 
@@ -18,22 +17,3 @@ class ConstantCmdAndArgsResolver(CmdAndArgsResolver):
     @property
     def symbol_usages(self) -> list:
         return self.__cmd_or_cmd_and_args_resolver.references
-
-
-class CmdAndArgsResolverForExecutableFileBase(CmdAndArgsResolver):
-    def __init__(self, executable: ExecutableFile):
-        self.__executable = executable
-
-    @property
-    def symbol_usages(self) -> list:
-        return self.__executable.symbol_usages
-
-    def _arguments(self, environment: PathResolvingEnvironmentPreOrPostSds) -> list:
-        raise NotImplementedError()
-
-    def resolve(self, environment: PathResolvingEnvironmentPreOrPostSds) -> list:
-        arguments_list_value = self.__executable.arguments.resolve(environment.symbols)
-        argument_strings = arguments_list_value.value_of_any_dependency(environment.home_and_sds)
-        return [self.__executable.path_string(environment)] + \
-               argument_strings + \
-               self._arguments(environment)
