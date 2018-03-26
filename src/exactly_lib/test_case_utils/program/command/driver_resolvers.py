d749474231fdfebdd46253caf265725982474249
Emil Karlén <emil.karlen@fripost.org>
2018-03-26 16:04:13 +0200

    exe-pgm: Adds CommandDriverResolver for "system program"

--- src/exactly_lib/test_case_utils/program/command/driver_resolvers.py
+++ src/exactly_lib/test_case_utils/program/command/driver_resolvers.py
@@ -2,6 +2,7 @@ from typing import Sequence
 
 from exactly_lib.symbol.data.file_ref_resolver import FileRefResolver
 from exactly_lib.symbol.data.list_resolver import ListResolver
+from exactly_lib.symbol.data.string_resolver import StringResolver
 from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.symbol.symbol_usage import SymbolReference
 from exactly_lib.test_case_utils.program.command.new_command_resolver import NewCommandDriverResolver
@@ -28,3 +29,24 @@ class NewCommandDriverResolverForExecutableFile(NewCommandDriverResolver):
     @property
     def references(self) -> Sequence[SymbolReference]:
         return self._executable_file.references
+
+
+class NewCommandDriverResolverForSystemProgram(NewCommandDriverResolver):
+    def __init__(self, program: StringResolver):
+        self._program = program
+
+    @property
+    def program(self) -> StringResolver:
+        return self._program
+
+    def resolve(self,
+                environment: PathResolvingEnvironmentPreOrPostSds,
+                arguments: ListResolver) -> Command:
+        return os_process_execution.executable_program_command(
+            self._program.resolve_value_of_any_dependency(environment),
+            arguments.resolve_value_of_any_dependency(environment)
+        )
+
+    @property
+    def references(self) -> Sequence[SymbolReference]:
+        return self._program.references
