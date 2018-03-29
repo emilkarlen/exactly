ab92a6b6f88d6090524181ab11f570ddd13500b3
emil <emil@member.fsf.org>
2018-03-29 14:15:32 +0200

    exe-pgm: Introduces StringOrFileRefValue

--- src/exactly_lib/test_case_utils/string_or_file.py
+++ src/exactly_lib/test_case_utils/string_or_file.py
@@ -4,6 +4,9 @@ from typing import Sequence
 from exactly_lib.symbol.data.file_ref_resolver import FileRefResolver
 from exactly_lib.symbol.data.string_resolver import StringResolver
 from exactly_lib.symbol.symbol_usage import SymbolReference
+from exactly_lib.symbol.utils import ValueResolver
+from exactly_lib.test_case_utils.util_values import StringOrFileRefValue
+from exactly_lib.util.symbol_table import SymbolTable
 
 
 class SourceType(enum.Enum):
@@ -12,21 +15,21 @@ class SourceType(enum.Enum):
     PATH = 3
 
 
-class StringOrFileRefResolver(tuple):
-    def __new__(cls,
-                source_type: SourceType,
-                string_resolver: StringResolver,
-                file_reference: FileRefResolver):
-        return tuple.__new__(cls, (source_type,
-                                   string_resolver,
-                                   file_reference,
-                                   (string_resolver.references
-                                    if string_resolver is not None
-                                    else file_reference.references)))
+class StringOrFileRefResolver(ValueResolver[StringOrFileRefValue]):
+    def __init__(self,
+                 source_type: SourceType,
+                 string_resolver: StringResolver,
+                 file_reference: FileRefResolver):
+        self._source_type = source_type
+        self._string = string_resolver
+        self._file_ref = file_reference
+        self._references = (string_resolver.references
+                            if string_resolver is not None
+                            else file_reference.references)
 
     @property
     def source_type(self) -> SourceType:
-        return self[0]
+        return self._source_type
 
     @property
     def is_file_ref(self) -> bool:
@@ -41,15 +44,25 @@ class StringOrFileRefResolver(tuple):
         """
         :return: Not None iff :class:`SourceType` is NOT `SourceType.PATH`
         """
-        return self[1]
+        return self._string
 
     @property
     def file_reference_resolver(self) -> FileRefResolver:
         """
         :return: Not None iff :class:`SourceType` is `SourceType.PATH`
         """
-        return self[2]
+        return self._file_ref
+
+    def resolve_value(self, symbols: SymbolTable) -> StringOrFileRefValue:
+        if self.is_file_ref:
+            return StringOrFileRefValue(None, self._file_ref.resolve(symbols))
+        else:
+            return StringOrFileRefValue(self._string.resolve(symbols), None)
+
+    @property
+    def references(self) -> Sequence[SymbolReference]:
+        return self.symbol_usages
 
     @property
     def symbol_usages(self) -> Sequence[SymbolReference]:
-        return self[3]
+        return self._references
