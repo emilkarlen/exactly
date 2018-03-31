888516132f4ac4fac79113876ded156df9faeed1
Emil Karlén <emil.karlen@fripost.org>
2018-03-31 23:49:21 +0200

    exe-pgm: "file": output from program: Adds handling of exe file programs

--- src/exactly_lib/test_case_utils/external_program/parse.py
+++ src/exactly_lib/test_case_utils/external_program/parse.py
@@ -1,15 +1,13 @@
-from exactly_lib.help_texts.test_case.instructions import instruction_names
 from exactly_lib.section_document import parser_classes
 from exactly_lib.section_document.element_parsers.token_stream_parser import TokenParser
 from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.section_document.parser_classes import Parser
+from exactly_lib.test_case_utils.external_program import syntax_elements
 from exactly_lib.test_case_utils.external_program.command.command_resolver import CommandResolver
 from exactly_lib.test_case_utils.external_program.executable_file import ExecutableFileWithArgsResolver
 from exactly_lib.test_case_utils.external_program.parse_impls import shell_program, \
     executable_file, executable_file_executable
 from exactly_lib.test_case_utils.external_program.program_resolver import ProgramResolver
-
-SHELL_COMMAND_TOKEN = instruction_names.SHELL_INSTRUCTION_NAME
 
 
 def parse_executable_file_with_args_from_parse_source(source: ParseSource) -> ExecutableFileWithArgsResolver:
@@ -41,8 +39,10 @@ def shell_command_line_program_parser() -> Parser[ProgramResolver]:
 
 
 def parse_program(parser: TokenParser) -> ProgramResolver:
-    parser.consume_mandatory_constant_unquoted_string(SHELL_COMMAND_TOKEN, False)
-    return parse_shell_command_line_as_program(parser)
+    if parser.consume_and_return_true_if_first_argument_is_unquoted_and_equals(syntax_elements.SHELL_COMMAND_TOKEN):
+        return parse_shell_command_line_as_program(parser)
+    else:
+        return parse_executable_file_as_program(parser)
 
 
 def program_parser() -> Parser[ProgramResolver]:
---
+++ test/exactly_lib_test/test_resources/programs/py_programs.py
@@ -0,0 +1,24 @@
+def py_pgm_that_exits_with_value_on_command_line(stderr_output: str) -> str:
+    return """
+import sys
+
+sys.stderr.write('{}');
+val = int(sys.argv[1])
+sys.exit(val)
+""".format(stderr_output)
+
+
+def single_line_pgm_that_prints_to_stdout_no_new_line(output: str) -> str:
+    return single_line_pgm_that_prints_to_no_new_line('stdout', output)
+
+
+def single_line_pgm_that_prints_to_stderr_no_new_line(output: str) -> str:
+    return single_line_pgm_that_prints_to_no_new_line('stderr', output)
+
+
+def single_line_pgm_that_prints_to_no_new_line(channel: str, output: str) -> str:
+    return _SINGLE_LINE_PGM_THAT_PRINTS_WITHOUT_NEW_LINE.format(channel=channel,
+                                                                str=output)
+
+
+_SINGLE_LINE_PGM_THAT_PRINTS_WITHOUT_NEW_LINE = 'import sys; sys.{channel}.write("""{str}""")'
