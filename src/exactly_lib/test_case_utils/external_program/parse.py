d22e3baae2f21f45fc67f2bdf72b3a89ea34406d
emil <emil@member.fsf.org>
2018-03-31 20:56:49 +0200

    exe-pgm/parse: Removes redundant base classes

--- src/exactly_lib/test_case_utils/external_program/parse.py
+++ src/exactly_lib/test_case_utils/external_program/parse.py
@@ -1,30 +1,17 @@
 from exactly_lib.section_document.element_parsers.token_stream_parser import from_parse_source, TokenParser
 from exactly_lib.section_document.parse_source import ParseSource
-from exactly_lib.section_document.parser_classes import Parser
 from exactly_lib.test_case_utils.external_program.command import command_resolvers
 from exactly_lib.test_case_utils.external_program.command.command_resolver import CommandResolver
-from exactly_lib.test_case_utils.external_program.command_and_stdin_resolver import CommandAndStdinResolver
-from exactly_lib.test_case_utils.external_program.program_resolver import ProgramResolver
 from exactly_lib.test_case_utils.parse.parse_executable_file import parse
 from exactly_lib.test_case_utils.parse.token_parser_extra import from_parse_source
 
 
-class CommandAndStdinParser(Parser[CommandAndStdinResolver]):
-    def parse_from_token_parser(self, parser: TokenParser) -> CommandAndStdinResolver:
-        raise NotImplementedError('abstract method')
-
-
-class ProgramParser(Parser[ProgramResolver]):
-    def parse_from_token_parser(self, parser: TokenParser) -> ProgramResolver:
-        raise NotImplementedError('abstract method')
-
-
-def parse_from_parse_source_new(source: ParseSource) -> CommandResolver:
+def parse_executable_file_from_parse_source_new(source: ParseSource) -> CommandResolver:
     with from_parse_source(source) as token_parser:
-        return command_from_token_parser(token_parser)
+        return parse_executable_file(token_parser)
 
 
-def command_from_token_parser(token_parser: TokenParser) -> CommandResolver:
+def parse_executable_file(token_parser: TokenParser) -> CommandResolver:
     exe_file = parse(token_parser.token_stream)
     return command_resolvers.for_executable_file(exe_file.executable_file) \
         .new_with_additional_arguments(exe_file.arguments)
