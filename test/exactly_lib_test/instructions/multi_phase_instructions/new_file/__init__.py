0eefbd23d1cf5cfa472bedc9b4dd822422c5ed58
emil <emil@member.fsf.org>
2018-03-31 22:47:04 +0200

    exe-pgm/parse: Renames methods (to match new structures)

--- test/exactly_lib_test/instructions/multi_phase_instructions/new_file/__init__.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/new_file/__init__.py
@@ -3,7 +3,7 @@ import unittest
 from exactly_lib.instructions.multi_phase_instructions import new_file as sut
 from exactly_lib_test.common.help.test_resources.check_documentation import suite_for_instruction_documentation
 from exactly_lib_test.instructions.multi_phase_instructions.new_file import contents_from_file, no_contents, \
-    contents_from_process, contents_from_string
+    contents_from_program, contents_from_string
 
 
 def suite() -> unittest.TestSuite:
@@ -12,7 +12,7 @@ def suite() -> unittest.TestSuite:
         no_contents.suite(),
         contents_from_string.suite(),
         contents_from_file.suite(),
-        contents_from_process.suite(),
+        contents_from_program.suite(),
 
         suite_for_instruction_documentation(sut.TheInstructionDocumentation('instruction name', False)),
         suite_for_instruction_documentation(sut.TheInstructionDocumentation('instruction name', True)),
