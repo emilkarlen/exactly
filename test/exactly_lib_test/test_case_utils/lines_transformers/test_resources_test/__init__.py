7622e1add71ca69029250933f551283dcfcf7eea
emil <emil@member.fsf.org>
2018-04-02 10:58:46 +0200

    exe-pgm: std(out/err): Test resources: Adds test lines transformers

--- test/exactly_lib_test/test_case_utils/lines_transformers/test_resources_test/__init__.py
+++ test/exactly_lib_test/test_case_utils/lines_transformers/test_resources_test/__init__.py
@@ -1,6 +1,7 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.lines_transformers.test_resources_test import resolver_assertions
+from exactly_lib_test.test_case_utils.lines_transformers.test_resources_test import resolver_assertions, \
+    test_transformers
 from exactly_lib_test.test_case_utils.lines_transformers.test_resources_test import value_assertions
 
 
@@ -8,6 +9,7 @@ def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         value_assertions.suite(),
         resolver_assertions.suite(),
+        test_transformers.suite(),
     ])
 
 
