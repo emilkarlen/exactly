62eb54ab78df36b7954810442c81f948fb1c4309
emil <emil@member.fsf.org>
2018-04-04 00:05:29 +0200

    program-type: validation: Test of post sds validation

--- test/exactly_lib_test/test_case_utils/program/parse/parse_with_reference_to_program.py
+++ test/exactly_lib_test/test_case_utils/program/parse/parse_with_reference_to_program.py
@@ -15,7 +15,8 @@ from exactly_lib_test.symbol.test_resources import program as asrt_pgm
 from exactly_lib_test.symbol.test_resources import resolver_structure_assertions as asrt_sym
 from exactly_lib_test.symbol.test_resources import symbol_utils
 from exactly_lib_test.test_case_file_structure.test_resources import home_populators
-from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import HomePopulator
+from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import HomePopulator, SdsPopulator
+from exactly_lib_test.test_case_file_structure.test_resources.sds_check import sds_populator
 from exactly_lib_test.test_case_utils.parse.test_resources import arguments_building as parse_args
 from exactly_lib_test.test_case_utils.parse.test_resources.arguments_building import ArgumentElements
 from exactly_lib_test.test_case_utils.program.test_resources import program_resolvers
@@ -112,6 +113,17 @@ class ValidationPreSdsCase:
         self.home_contents = home_contents
 
 
+class ValidationPostSdsCase:
+    def __init__(self,
+                 name: str,
+                 source: ArgumentElementRenderer,
+                 sds_contents: SdsPopulator,
+                 ):
+        self.name = name
+        self.source = source
+        self.sds_contents = sds_contents
+
+
 class TestValidation(unittest.TestCase):
     def test_failing_validation_pre_sds(self):
         parser = sut.program_parser()
@@ -168,6 +180,61 @@ class TestValidation(unittest.TestCase):
                     # ASSERT #
                     self.assertIsNone(actual)
 
+    def test_failing_validation_post_sds(self):
+        parser = sut.program_parser()
+
+        program_symbol_with_ref_to_non_exit_exe_file = NameAndValue(
+            'PGM_WITH_REF_TO_EXE_FILE',
+            program_resolvers.with_ref_to_exe_file(constant(simple_of_rel_option(RelOptionType.REL_TMP,
+                                                                                 'non-existing-exe-file')))
+        )
+
+        program_symbol_with_ref_to_non_exiting_file_as_argument = NameAndValue(
+            'PGM_WITH_REF_TO_SOURCE_FILE',
+            program_resolvers.interpret_py_source_file_that_must_exist(
+                constant(simple_of_rel_option(RelOptionType.REL_ACT,
+                                              'non-existing-python-file.py')))
+        )
+
+        symbols = SymbolTable({
+            program_symbol_with_ref_to_non_exit_exe_file.name:
+                symbol_utils.container(program_symbol_with_ref_to_non_exit_exe_file.value),
+
+            program_symbol_with_ref_to_non_exiting_file_as_argument.name:
+                symbol_utils.container(program_symbol_with_ref_to_non_exiting_file_as_argument.value)
+        })
+
+        cases = [
+            ValidationPostSdsCase('executable does not exist',
+                                  sym_ref_args.sym_ref_cmd_line(program_symbol_with_ref_to_non_exit_exe_file.name),
+                                  sds_populator.empty()
+                                  ),
+            ValidationPostSdsCase('source file does not exist',
+                                  sym_ref_args.sym_ref_cmd_line(
+                                      program_symbol_with_ref_to_non_exiting_file_as_argument.name),
+                                  sds_populator.empty()
+                                  ),
+        ]
+
+        for case in cases:
+            source = parse_source_of(case.source)
+
+            with self.subTest(case.name):
+                # ACT #
+                program_resolver = parser.parse(source)
+                # ASSERT #
+                self.assertIsInstance(program_resolver, ProgramResolver)
+                with home_and_sds_with_act_as_curr_dir(sds_contents=case.sds_contents,
+                                                       symbols=symbols) as environment:
+                    # ACT #
+                    actual = program_resolver.validator.validate_pre_sds_if_applicable(environment)
+                    # ASSERT #
+                    self.assertIsNone(actual)
+                    # ACT #
+                    actual = program_resolver.validator.validate_post_sds_if_applicable(environment)
+                    # ASSERT #
+                    self.assertIsNotNone(actual)
+
 
 def is_reference_data_type_symbol(symbol_name: str) -> asrt.ValueAssertion:
     return asrt_sym.matches_reference(asrt.equals(symbol_name),
@@ -176,3 +243,7 @@ def is_reference_data_type_symbol(symbol_name: str) -> asrt.ValueAssertion:
 
 def parse_source_of(single_line: ArgumentElementRenderer) -> ParseSource:
     return ArgumentElements([single_line]).as_remaining_source
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
