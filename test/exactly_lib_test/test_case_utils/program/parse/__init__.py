0e67ca3ecd1de246446587fc09dcec7072918d28
emil <emil@member.fsf.org>
2018-04-04 02:26:51 +0200

    program-type: "Any type parser": Implements parsing of sym-ref-program

--- test/exactly_lib_test/test_case_utils/program/parse/__init__.py
+++ test/exactly_lib_test/test_case_utils/program/parse/__init__.py
@@ -1,13 +1,14 @@
 import unittest
 
 from exactly_lib_test.test_case_utils.program.parse import parse_executable_file_executable, \
-    parse_with_reference_to_program
+    parse_with_reference_to_program, parse_program
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         parse_executable_file_executable.suite(),
         parse_with_reference_to_program.suite(),
+        parse_program.suite(),
     ])
 
 
