cb873461ca48e8d1cbfaa8b6cbc5adecccf4031e
Emil Karlén <emil@member.fsf.org>
2018-04-09 18:10:05 +0200

    exactly suite as part of test suite

--- test/run-test-suite.py
+++ test/run-test-suite.py
@@ -2,17 +2,17 @@
 A python 3 program that runs the test suite towards the source file structure.
 """
 import os
+import pathlib
 import sys
 import unittest
 
 SRC_DIR_NAME = 'src'
 
-this_dir = sys.path[0]
-prj_root_dir = os.path.dirname(this_dir)
-src_dir = os.path.join(prj_root_dir, SRC_DIR_NAME)
-sys.path.insert(0, src_dir)
+this_dir = pathlib.Path(sys.path[0])
+src_dir = this_dir.parent / SRC_DIR_NAME
+sys.path.insert(0, str(src_dir))
 
-os.chdir(this_dir)
+os.chdir(str(this_dir))
 
 from exactly_lib_test.test_resources.main_program.main_program_runners import RunDefaultMainProgramViaOsInSubProcess
 import complete_test_suite
