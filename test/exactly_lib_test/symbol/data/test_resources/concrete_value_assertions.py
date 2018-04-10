2f45feba2028788310b5c2cbce74edbbcb1cbcdb
Emil Karlén <emil.karlen@fripost.org>
2018-04-10 21:53:40 +0200

    system-command: Test resources: Replaces old resolver assertions with new

--- test/exactly_lib_test/symbol/data/test_resources/concrete_value_assertions.py
+++ test/exactly_lib_test/symbol/data/test_resources/concrete_value_assertions.py
@@ -1,19 +1,18 @@
 import unittest
+from typing import Sequence
 
-from exactly_lib.help_texts import type_system
 from exactly_lib.symbol.data.file_ref_resolver import FileRefResolver
 from exactly_lib.symbol.data.impl.string_resolver_impls import ConstantStringFragmentResolver, \
     SymbolStringFragmentResolver
 from exactly_lib.symbol.data.string_resolver import StringFragmentResolver, StringResolver
 from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
-from exactly_lib.symbol.resolver_structure import DataValueResolver
-from exactly_lib.test_case_file_structure.dir_dependent_value import DirDependentValue
+from exactly_lib.symbol.resolver_structure import SymbolValueResolver
 from exactly_lib.type_system.data.file_ref import FileRef
-from exactly_lib.type_system.value_type import DataValueType, TypeCategory
-from exactly_lib.util.symbol_table import SymbolTable, empty_symbol_table
+from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.symbol.data.test_resources.assertion_utils import \
     symbol_table_with_values_matching_references
 from exactly_lib_test.symbol.data.test_resources.symbol_reference_assertions import equals_symbol_references
+from exactly_lib_test.symbol.test_resources import resolver_assertions
 from exactly_lib_test.test_case_file_structure.test_resources.paths import fake_home_and_sds
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.type_system.data.test_resources.file_ref_assertions import equals_file_ref
@@ -24,22 +23,17 @@ from exactly_lib_test.type_system.data.test_resources.string_value_assertions im
 def equals_file_ref_resolver(expected: FileRefResolver) -> asrt.ValueAssertion:
     symbols = symbol_table_with_values_matching_references(expected.references)
     expected_file_ref = expected.resolve(symbols)
-    assertion_on_resolved_value = equals_file_ref(expected_file_ref)
-    assertion_on_symbol_references = equals_symbol_references(expected.references)
-    return _FileRefResolverAssertion(assertion_on_symbol_references,
-                                     assertion_on_resolved_value,
-                                     symbols)
+    return resolver_assertions.matches_resolver_of_path(equals_symbol_references(expected.references),
+                                                        equals_file_ref(expected_file_ref),
+                                                        symbols=symbols)
 
 
 def matches_file_ref_resolver(expected_resolved_value: FileRef,
                               expected_symbol_references: asrt.ValueAssertion,
                               symbol_table: SymbolTable = None) -> asrt.ValueAssertion:
-    if symbol_table is None:
-        symbol_table = empty_symbol_table()
-    assertion_on_resolved_value = equals_file_ref(expected_resolved_value)
-    return _FileRefResolverAssertion(expected_symbol_references,
-                                     assertion_on_resolved_value,
-                                     symbol_table)
+    return resolver_assertions.matches_resolver_of_path(expected_symbol_references,
+                                                        equals_file_ref(expected_resolved_value),
+                                                        symbols=symbol_table)
 
 
 def equals_string_fragment_resolver_with_exact_type(expected: StringFragmentResolver) -> asrt.ValueAssertion:
@@ -61,76 +55,23 @@ def equals_string_fragments(expected_fragments) -> asrt.ValueAssertion:
 
 
 def equals_string_resolver(expected: StringResolver,
-                           symbols: SymbolTable = None) -> asrt.ValueAssertion:
+                           symbols: SymbolTable = None) -> asrt.ValueAssertion[SymbolValueResolver]:
     if symbols is None:
         symbols = symbol_table_with_values_matching_references(expected.references)
+
     expected_resolved_value = expected.resolve(symbols)
-    assertion_on_resolved_value = equals_string_value(expected_resolved_value)
-    assertion_on_symbol_references = equals_symbol_references(expected.references)
-    assertion_on_fragments = equals_string_fragments(expected.fragments)
 
-    return _StringValueResolverAssertion(assertion_on_symbol_references,
-                                         assertion_on_resolved_value,
-                                         assertion_on_fragments,
-                                         symbols)
+    def get_fragment_resolvers(x: StringResolver) -> Sequence[StringFragmentResolver]:
+        return x.fragments
 
+    return resolver_assertions.matches_resolver_of_string(equals_symbol_references(expected.references),
+                                                          equals_string_value(expected_resolved_value),
+                                                          asrt.sub_component('fragment resolvers',
+                                                                             get_fragment_resolvers,
+                                                                             equals_string_fragments(
+                                                                                 expected.fragments)),
 
-class _EqualsSymbolValueResolverBase(asrt.ValueAssertion):
-    def __init__(self,
-                 expected_data_value_type: DataValueType,
-                 expected_symbol_references: asrt.ValueAssertion,
-                 expected_resolved_value: asrt.ValueAssertion,
-                 symbols_for_checking_resolving: SymbolTable):
-        self.expected_data_value_type = expected_data_value_type
-        self.expected_symbol_references = expected_symbol_references
-        self.expected_resolved_value = expected_resolved_value
-        self.symbols_for_checking_resolving = symbols_for_checking_resolving
-
-    def apply(self,
-              put: unittest.TestCase,
-              actual,
-              message_builder: asrt.MessageBuilder = asrt.MessageBuilder()):
-        put.assertIsInstance(actual, DataValueResolver,
-                             message_builder.apply('object type'))
-        assert isinstance(actual, DataValueResolver)  # Type info for IDE
-
-        self.assert_type_is_expected(put, actual, message_builder)
-
-        self.expected_symbol_references.apply(put, actual.references,
-                                              message_builder.for_sub_component('symbol_references'))
-
-        self._custom_check_of_resolver(put, actual, message_builder)
-
-        resolved_value = actual.resolve(self.symbols_for_checking_resolving)
-
-        put.assertIsInstance(resolved_value, DirDependentValue,
-                             message_builder.apply('object type of resolved value'))
-
-        self.expected_resolved_value.apply(put, resolved_value,
-                                           message_builder.for_sub_component('resolved value'))
-
-    def assert_type_is_expected(self,
-                                put: unittest.TestCase,
-                                actual: DataValueResolver,
-                                message_builder: asrt.MessageBuilder):
-        put.assertIs(TypeCategory.DATA,
-                     actual.type_category,
-                     'element_type')
-        put.assertIs(self.expected_data_value_type,
-                     actual.data_value_type,
-                     message_builder.apply('data_value_type'))
-
-        expected_value_type = type_system.DATA_TYPE_2_VALUE_TYPE[self.expected_data_value_type]
-
-        put.assertIs(expected_value_type,
-                     actual.value_type,
-                     message_builder.apply('value_type'))
-
-    def _custom_check_of_resolver(self,
-                                  put: unittest.TestCase,
-                                  actual: DataValueResolver,
-                                  message_builder: asrt.MessageBuilder):
-        pass
+                                                          symbols)
 
 
 class _EqualsStringFragmentAssertionForStringConstant(asrt.ValueAssertion):
@@ -244,45 +185,3 @@ class _EqualsStringFragments(asrt.ValueAssertion):
         put.assertIsInstance(value, tuple,
                              'Expects a tuple of StringFragments')
         asrt.matches_sequence(self._sequence_of_element_assertions).apply(put, value, message_builder)
-
-
-class _FileRefResolverAssertion(_EqualsSymbolValueResolverBase):
-    def __init__(self,
-                 expected_symbol_references: asrt.ValueAssertion,
-                 expected_resolved_value: asrt.ValueAssertion,
-                 symbols_for_checking_resolving: SymbolTable):
-        super().__init__(DataValueType.PATH,
-                         expected_symbol_references,
-                         expected_resolved_value,
-                         symbols_for_checking_resolving)
-
-    def _custom_check_of_resolver(self,
-                                  put: unittest.TestCase,
-                                  actual: DataValueResolver,
-                                  message_builder: asrt.MessageBuilder):
-        put.assertIsInstance(actual, FileRefResolver,
-                             message_builder.apply('Actual value is expected to be a ' + str(FileRefResolver)))
-
-
-class _StringValueResolverAssertion(_EqualsSymbolValueResolverBase):
-    def __init__(self,
-                 expected_symbol_references: asrt.ValueAssertion,
-                 expected_resolved_value: asrt.ValueAssertion,
-                 expected_string_fragments: asrt.ValueAssertion,
-                 symbols_for_checking_resolving: SymbolTable):
-        super().__init__(DataValueType.STRING,
-                         expected_symbol_references,
-                         expected_resolved_value,
-                         symbols_for_checking_resolving)
-        self._expected_string_fragments = expected_string_fragments
-
-    def _custom_check_of_resolver(self,
-                                  put: unittest.TestCase,
-                                  actual: DataValueResolver,
-                                  message_builder: asrt.MessageBuilder):
-        put.assertIsInstance(actual, StringResolver,
-                             'Actual value is expected to be a ' + str(StringResolver))
-        assert isinstance(actual, StringResolver)
-
-        self._expected_string_fragments.apply(put, actual.fragments,
-                                              message_builder.for_sub_component('fragments'))
--- test/exactly_lib_test/symbol/data/test_resources/list_assertions.py
+++ test/exactly_lib_test/symbol/data/test_resources/list_assertions.py
@@ -1,19 +1,15 @@
-import unittest
-from typing import Iterable, List
+from typing import Iterable, List, Sequence
 
 from exactly_lib.symbol.data import list_resolver
 from exactly_lib.symbol.data import list_resolvers
 from exactly_lib.symbol.data.list_resolver import ListResolver
-from exactly_lib.symbol.resolver_structure import DataValueResolver
 from exactly_lib.type_system.data.list_value import ListValue
-from exactly_lib.type_system.value_type import DataValueType
-from exactly_lib.util.symbol_table import SymbolTable, empty_symbol_table
+from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.symbol.data.test_resources.assertion_utils import \
     symbol_table_with_values_matching_references
-from exactly_lib_test.symbol.data.test_resources.concrete_value_assertions import \
-    _EqualsSymbolValueResolverBase
 from exactly_lib_test.symbol.data.test_resources.symbol_reference_assertions import equals_symbol_references, \
     equals_symbol_reference
+from exactly_lib_test.symbol.test_resources import resolver_assertions
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.type_system.data.test_resources.list_value_assertions import equals_list_value
 from exactly_lib_test.type_system.data.test_resources.string_value_assertions import equals_string_value
@@ -58,64 +54,29 @@ def equals_list_resolver(expected: ListResolver,
                          symbols: SymbolTable = None) -> asrt.ValueAssertion:
     if symbols is None:
         symbols = symbol_table_with_values_matching_references(expected.references)
-    assertion_on_symbol_references = equals_symbol_references(expected.references)
 
-    return _EqualsListValueResolverAssertion(assertion_on_symbol_references,
-                                             expected,
-                                             symbols)
+    expected_resolved_value = expected.resolve(symbols)
+
+    def get_element_resolvers(x: ListResolver) -> Sequence[list_resolver.Element]:
+        return x.elements
+
+    return resolver_assertions.matches_resolver_of_list(equals_symbol_references(expected.references),
+                                                        equals_list_value(expected_resolved_value),
+                                                        asrt.sub_component('element resolvers',
+                                                                           get_element_resolvers,
+                                                                           equals_list_resolver_elements(
+                                                                               list(expected.elements))),
+
+                                                        symbols)
 
 
 def matches_list_resolver(expected_resolved_value: ListValue,
                           expected_symbol_references: asrt.ValueAssertion,
                           symbols: SymbolTable = None) -> asrt.ValueAssertion:
-    if symbols is None:
-        symbols = empty_symbol_table()
-
-    return _MatchesListValueResolverAssertion(expected_symbol_references,
-                                              expected_resolved_value,
-                                              symbols)
+    return resolver_assertions.matches_resolver_of_list(expected_symbol_references,
+                                                        equals_list_value(expected_resolved_value),
+                                                        symbols=symbols)
 
 
 def equals_constant_list(expected_str_list: Iterable[str]) -> asrt.ValueAssertion:
     return equals_list_resolver(list_resolvers.from_str_constants(expected_str_list))
-
-
-class _EqualsListValueResolverAssertion(_EqualsSymbolValueResolverBase):
-    def __init__(self,
-                 expected_symbol_references: asrt.ValueAssertion,
-                 expected_resolver: ListResolver,
-                 symbols_for_checking_resolving: SymbolTable):
-        super().__init__(DataValueType.LIST,
-                         expected_symbol_references,
-                         equals_list_value(expected_resolver.resolve(symbols_for_checking_resolving)),
-                         symbols_for_checking_resolving)
-        self._expected_resolver = expected_resolver
-
-    def _custom_check_of_resolver(self,
-                                  put: unittest.TestCase,
-                                  actual: DataValueResolver,
-                                  message_builder: asrt.MessageBuilder):
-        put.assertIsInstance(actual, ListResolver,
-                             'Actual value is expected to be a ' + str(ListResolver))
-        assert isinstance(actual, ListResolver)
-        elements_assertion = equals_list_resolver_elements(list(self._expected_resolver.elements))
-        elements_assertion.apply(put, actual.elements,
-                                 message_builder.for_sub_component('elements'))
-
-
-class _MatchesListValueResolverAssertion(_EqualsSymbolValueResolverBase):
-    def __init__(self,
-                 expected_symbol_references: asrt.ValueAssertion,
-                 expected_resolved_value: ListValue,
-                 symbols_for_checking_resolving: SymbolTable):
-        super().__init__(DataValueType.LIST,
-                         expected_symbol_references,
-                         equals_list_value(expected_resolved_value),
-                         symbols_for_checking_resolving)
-
-    def _custom_check_of_resolver(self,
-                                  put: unittest.TestCase,
-                                  actual: DataValueResolver,
-                                  message_builder: asrt.MessageBuilder):
-        put.assertIsInstance(actual, ListResolver,
-                             'Actual value is expected to be a ' + str(ListResolver))
