e86010d0d5cba1946498d209c303a5736ecf7a20
Emil Karlén <emil.karlen@fripost.org>
2018-04-13 22:56:52 +0200

    system-command: Adds test of parse symbol-ref-program

--- test/exactly_lib_test/test_case_utils/program/parse/parse_with_reference_to_program.py
+++ test/exactly_lib_test/test_case_utils/program/parse/parse_with_reference_to_program.py
@@ -1,19 +1,28 @@
 import unittest
+from typing import List, Sequence
 
 from exactly_lib.section_document.element_parsers.instruction_parser_for_single_phase import \
     SingleInstructionInvalidArgumentException
 from exactly_lib.section_document.parse_source import ParseSource
+from exactly_lib.symbol.data import file_ref_resolvers, list_resolvers, string_resolvers
 from exactly_lib.symbol.data.file_ref_resolvers import constant
 from exactly_lib.symbol.program.program_resolver import ProgramResolver
-from exactly_lib.test_case_file_structure.path_relativity import RelOptionType
+from exactly_lib.test_case_file_structure.dir_dependent_value import DirDependentValue
+from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_file_structure.path_relativity import RelOptionType, RESOLVING_DEPENDENCY_OF
+from exactly_lib.test_case_utils.program.command import arguments_resolvers
 from exactly_lib.test_case_utils.program.parse import parse_with_reference_to_program as sut
+from exactly_lib.type_system.data import file_refs
 from exactly_lib.type_system.data.file_refs import simple_of_rel_option
+from exactly_lib.type_system.logic.program.program_value import Program
 from exactly_lib.util.parse.token import QuoteType, QUOTE_CHAR_FOR_TYPE
+from exactly_lib.util.process_execution import os_process_execution
 from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.symbol.data.restrictions.test_resources import concrete_restriction_assertion as asrt_rr
 from exactly_lib_test.symbol.test_resources import program as asrt_pgm
 from exactly_lib_test.symbol.test_resources import symbol_usage_assertions as asrt_sym_usage
 from exactly_lib_test.symbol.test_resources import symbol_utils
+from exactly_lib_test.test_case_file_structure.test_resources import dir_dep_value_assertions as asrt_dir_dep_val
 from exactly_lib_test.test_case_file_structure.test_resources import home_populators
 from exactly_lib_test.test_case_file_structure.test_resources.dir_populator import HomePopulator, SdsPopulator
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check import sds_populator
@@ -29,6 +38,9 @@ from exactly_lib_test.test_resources.test_case_file_struct_and_symbols.home_and_
     home_and_sds_with_act_as_curr_dir
 from exactly_lib_test.test_resources.test_utils import NIE
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+from exactly_lib_test.type_system.logic.test_resources import line_transformer_assertions as asrt_line_transformer
+from exactly_lib_test.type_system.logic.test_resources import program_assertions as asrt_pgm_val
+from exactly_lib_test.util.test_resources import command_assertions as asrt_command
 
 
 def suite() -> unittest.TestSuite:
@@ -36,6 +48,7 @@ def suite() -> unittest.TestSuite:
         unittest.makeSuite(TestFailingParse),
         unittest.makeSuite(TestSymbolReferences),
         unittest.makeSuite(TestValidation),
+        unittest.makeSuite(TestResolving),
         unittest.makeSuite(TestExecution),
     ])
 
@@ -79,16 +92,20 @@ class TestSymbolReferences(unittest.TestCase):
 
         cases = [
             NIE('just program reference symbol',
+                expected_value=
                 asrt.matches_sequence([
                     asrt_pgm.is_program_reference_to(program_symbol_name),
                 ]),
+                input_value=
                 sym_ref_args.sym_ref_cmd_line(program_symbol_name),
                 ),
             NIE('single argument that is a reference to a symbol',
+                expected_value=
                 asrt.matches_sequence([
                     asrt_pgm.is_program_reference_to(program_symbol_name),
                     is_reference_data_type_symbol(argument_symbol_name),
                 ]),
+                input_value=
                 sym_ref_args.sym_ref_cmd_line(program_symbol_name, [ab.symbol_reference(argument_symbol_name)]),
                 ),
         ]
@@ -287,6 +304,124 @@ class TestExecution(unittest.TestCase):
                                             contents_after_transformation=asrt.equals(''),
                                         )
                                     ))
+
+
+class ResolvingCase:
+    def __init__(self,
+                 name: str,
+                 actual_resolver: ProgramResolver,
+                 expected: asrt.ValueAssertion[DirDependentValue[Program]]):
+        self.name = name
+        self.actual_resolver = actual_resolver
+        self.expected = expected
+
+
+class TestResolving(unittest.TestCase):
+    @staticmethod
+    def _executable_file_case(expected_arguments: List[str]) -> Sequence[ResolvingCase]:
+        file_name = 'the exe file'
+
+        def case(relativity: RelOptionType) -> ResolvingCase:
+            exe_file_ref = file_refs.of_rel_option(relativity, file_refs.constant_path_part(file_name))
+
+            def assertion(tcds: HomeAndSds) -> asrt.ValueAssertion[Program]:
+                return asrt_pgm_val.matches_program(
+                    command=asrt_command.equals_executable_file_command(
+                        os_process_execution.executable_file_command(
+                            program_file=exe_file_ref.value_of_any_dependency(tcds),
+                            arguments=expected_arguments)
+                    ),
+                    stdin=asrt_pgm_val.no_stdin(),
+                    transformer=asrt_line_transformer.is_identity_transformer()
+                )
+
+            return ResolvingCase('relativity=' + str(relativity),
+                                 actual_resolver=program_resolvers.with_ref_to_exe_file(
+                                     file_ref_resolvers.constant(exe_file_ref),
+                                     arguments_resolvers.new_without_validation(
+                                         list_resolvers.from_str_constants(expected_arguments))),
+                                 expected=asrt_dir_dep_val.matches_dir_dependent_value(
+                                     asrt.equals({RESOLVING_DEPENDENCY_OF[relativity]}), assertion))
+
+        return [case(RelOptionType.REL_HOME_ACT),
+                case(RelOptionType.REL_TMP)]
+
+    @staticmethod
+    def _executable_program_case(expected_arguments: List[str]) -> Sequence[
+        ResolvingCase]:
+        the_executable_program = 'the executable program'
+
+        def assertion(tcds: HomeAndSds) -> asrt.ValueAssertion[Program]:
+            return asrt_pgm_val.matches_program(
+                command=asrt_command.equals_executable_program_command(
+                    os_process_execution.executable_program_command(
+                        program=the_executable_program,
+                        arguments=expected_arguments)
+                ),
+                stdin=asrt_pgm_val.no_stdin(),
+                transformer=asrt_line_transformer.is_identity_transformer()
+            )
+
+        case = ResolvingCase('', actual_resolver=program_resolvers.with_ref_to_program(
+            string_resolvers.str_constant(the_executable_program),
+            arguments_resolvers.new_without_validation(list_resolvers.from_str_constants(expected_arguments))),
+                             expected=asrt_dir_dep_val.matches_dir_dependent_value(asrt.equals(set()), assertion))
+        return [case]
+
+    def test(self):
+        # ARRANGE #
+
+        parser = sut.program_parser()
+
+        argument_cases = [
+            NameAndValue('no arguments', []
+                         ),
+            NameAndValue('single arguments', ['an argument']
+                         ),
+        ]
+
+        program_cases = [
+            NameAndValue('executable file', self._executable_file_case),
+            NameAndValue('executable program', self._executable_program_case),
+        ]
+
+        for argument_case in argument_cases:
+            for program_case in program_cases:
+                for resolving_case in program_case.value(argument_case.value):
+                    with self.subTest(program=program_case.name,
+                                      arguments=argument_case.name,
+                                      resolving_case=resolving_case.name):
+                        program_symbol = NameAndValue(
+                            'PROGRAM_SYMBOL',
+                            resolving_case.actual_resolver)
+
+                        source = parse_source_of(sym_ref_args.sym_ref_cmd_line(program_symbol.name))
+
+                        symbols = SymbolTable({
+                            program_symbol.name:
+                                symbol_utils.container(program_symbol.value)
+                        })
+
+                        expected_references_assertion = asrt.matches_sequence([
+                            asrt_pgm.is_program_reference_to(program_symbol.name),
+                        ])
+
+                        # ACT #
+
+                        actual = parser.parse(source)
+
+                        # ASSERT #
+
+                        actual_references = actual.references
+
+                        expected_references_assertion.apply_with_message(self,
+                                                                         actual_references,
+                                                                         'references')
+                        actual_resolved_value = actual.resolve(symbols)
+
+                        resolving_case.expected.apply_with_message(self,
+                                                                   actual_resolved_value,
+                                                                   'resolved value')
 
 
 def is_reference_data_type_symbol(symbol_name: str) -> asrt.ValueAssertion:
