aa37068920775ef0be5fa97ec58df5fbacdaa940
Emil Karlén <emil@member.fsf.org>
2018-04-14 15:52:42 +0200

    system-command: REFACT/proc exe: Introduces ExecutableFactory
    for translation of Command to Executable

--- test/exactly_lib_test/test_resources/os_services_that_raises.py
+++ test/exactly_lib_test/test_resources/os_services_that_raises.py
@@ -1,5 +1,7 @@
 from exactly_lib.test_case.os_services import OsServices
 from exactly_lib.test_case.phases.result import sh
+from exactly_lib.util.process_execution.command import Command
+from exactly_lib.util.process_execution.execution_elements import Executable
 
 
 class OsServicesThatRaises(OsServices):
@@ -7,4 +9,7 @@ class OsServicesThatRaises(OsServices):
         raise NotImplementedError('Should never be used')
 
     def copy_tree_preserve_as_much_as_possible(self, src: str, dst: str) -> sh.SuccessOrHardError:
+        raise NotImplementedError('Should never be used')
+
+    def make_executable__detect_ex(self, command: Command) -> Executable:
         raise NotImplementedError('Should never be used')
