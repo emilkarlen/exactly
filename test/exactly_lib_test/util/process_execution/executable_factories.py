4610fbb0cbd3b104aeea176a29b65854eb8b8b31
Emil Karlén <emil.karlen@fripost.org>
2018-04-21 23:01:05 +0200

    exe-pgm: Impl (and test) of ExecutableFactory:s

---
+++ test/exactly_lib_test/util/process_execution/executable_factories.py
@@ -0,0 +1,179 @@
+import pathlib
+import unittest
+
+from exactly_lib.util.process_execution import executable_factories as sut
+from exactly_lib.util.process_execution.command import Command
+from exactly_lib.util.process_execution.commands import CommandDriverForShell, CommandDriverForSystemProgram, \
+    CommandDriverForExecutableFile
+from exactly_lib_test.test_resources.test_utils import NIE
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.TestSuite([
+        unittest.makeSuite(TestExecutableFactoryForPosix),
+        unittest.makeSuite(TestExecutableFactoryForWindows),
+    ])
+
+
+class TestExecutableFactoryForPosix(unittest.TestCase):
+    factory = sut.ExecutableFactoryForPosix()
+
+    def test_shell(self):
+        # ARRANGE #
+        command_line = 'command line'
+        cases = [
+            NIE('without arguments',
+                expected_value=command_line,
+                input_value=[],
+                ),
+            NIE('with arguments',
+                expected_value=command_line + ' arg1 arg2',
+                input_value=['arg1', 'arg2'],
+                ),
+        ]
+        for case in cases:
+            with self.subTest(case.name):
+                command = Command(CommandDriverForShell(command_line),
+                                  case.input_value)
+                # ACT #
+                actual = self.factory.make(command)
+                # ASSERT #
+                self.assertTrue(actual.is_shell)
+
+                self.assertIsInstance(actual.arg_list_or_str, str)
+
+                self.assertEqual(case.expected_value, actual.arg_list_or_str)
+
+    def test_system_program(self):
+        # ARRANGE #
+        program_name = 'program_name'
+        cases = [
+            NIE('without arguments',
+                expected_value=[program_name],
+                input_value=[],
+                ),
+            NIE('with arguments',
+                expected_value=[program_name, 'arg1', 'arg2'],
+                input_value=['arg1', 'arg2'],
+                ),
+        ]
+        for case in cases:
+            with self.subTest(case.name):
+                command = Command(CommandDriverForSystemProgram(program_name),
+                                  case.input_value)
+                # ACT #
+                actual = self.factory.make(command)
+                # ASSERT #
+                self.assertFalse(actual.is_shell)
+
+                self.assertIsInstance(actual.arg_list_or_str, list)
+
+                self.assertEqual(case.expected_value, actual.arg_list_or_str)
+
+    def test_executable_file(self):
+        # ARRANGE #
+        the_file = pathlib.Path('the dir') / 'the file'
+        cases = [
+            NIE('without arguments',
+                expected_value=[str(the_file)],
+                input_value=[],
+                ),
+            NIE('with arguments',
+                expected_value=[str(the_file), 'arg1', 'arg2'],
+                input_value=['arg1', 'arg2'],
+                ),
+        ]
+        for case in cases:
+            with self.subTest(case.name):
+                command = Command(CommandDriverForExecutableFile(the_file),
+                                  case.input_value)
+                # ACT #
+                actual = self.factory.make(command)
+                # ASSERT #
+                self.assertFalse(actual.is_shell)
+
+                self.assertIsInstance(actual.arg_list_or_str, list)
+
+                self.assertEqual(case.expected_value, actual.arg_list_or_str)
+
+
+class TestExecutableFactoryForWindows(unittest.TestCase):
+    factory = sut.ExecutableFactoryForWindows()
+
+    def test_shell(self):
+        # ARRANGE #
+        command_line = 'command line'
+        cases = [
+            NIE('without arguments',
+                expected_value=command_line,
+                input_value=[],
+                ),
+            NIE('with arguments',
+                expected_value=command_line + ' arg1 arg2',
+                input_value=['arg1', 'arg2'],
+                ),
+        ]
+        for case in cases:
+            with self.subTest(case.name):
+                command = Command(CommandDriverForShell(command_line),
+                                  case.input_value)
+                # ACT #
+                actual = self.factory.make(command)
+                # ASSERT #
+                self.assertTrue(actual.is_shell)
+
+                self.assertIsInstance(actual.arg_list_or_str, str)
+
+                self.assertEqual(case.expected_value, actual.arg_list_or_str)
+
+    def test_system_program(self):
+        # ARRANGE #
+        program_name = 'program_name'
+        cases = [
+            NIE('without arguments',
+                expected_value=[program_name],
+                input_value=[],
+                ),
+            NIE('with arguments',
+                expected_value=[program_name, 'arg1', 'arg2'],
+                input_value=['arg1', 'arg2'],
+                ),
+        ]
+        for case in cases:
+            with self.subTest(case.name):
+                command = Command(CommandDriverForSystemProgram(program_name),
+                                  case.input_value)
+                # ACT #
+                actual = self.factory.make(command)
+                # ASSERT #
+                self.assertFalse(actual.is_shell)
+
+                self.assertIsInstance(actual.arg_list_or_str, list)
+
+                self.assertEqual(case.expected_value, actual.arg_list_or_str)
+
+    def test_executable_file(self):
+        # ARRANGE #
+        the_file = pathlib.Path('the dir') / 'the file'
+        cases = [
+            NIE('without arguments',
+                expected_value=[str(the_file)],
+                input_value=[],
+                ),
+            NIE('with arguments',
+                expected_value=[str(the_file), 'arg1', 'arg2'],
+                input_value=['arg1', 'arg2'],
+                ),
+        ]
+        for case in cases:
+            with self.subTest(case.name):
+                command = Command(CommandDriverForExecutableFile(the_file),
+                                  case.input_value)
+                # ACT #
+                actual = self.factory.make(command)
+                # ASSERT #
+                self.assertFalse(actual.is_shell)
+
+                self.assertIsInstance(actual.arg_list_or_str, list)
+
+                self.assertEqual(case.expected_value, actual.arg_list_or_str)
