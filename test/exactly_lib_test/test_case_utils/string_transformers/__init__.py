281be759573d51053c4a109ede66ba501a72a1ff
Emil Karlén <emil.karlen@etraveli.com>
2018-05-11 22:14:05 +0200

    str-trans: Renames package test/...

--- test/exactly_lib_test/test_case_utils/string_transformers/__init__.py
+++ test/exactly_lib_test/test_case_utils/string_transformers/__init__.py
@@ -2,9 +2,8 @@ import unittest
 
 from exactly_lib_test.test_case_utils.string_transformers import env_vars_replacement, replace, \
     select_transformer, case_converters
-from exactly_lib_test.test_case_utils.string_transformers import parse_lines_transformer
-from exactly_lib_test.test_case_utils.string_transformers import test_resources_test
-from exactly_lib_test.test_case_utils.string_transformers import vistor
+from exactly_lib_test.test_case_utils.string_transformers import \
+    parse_string_transformer, test_resources_test, vistor
 
 
 def suite() -> unittest.TestSuite:
@@ -14,7 +13,7 @@ def suite() -> unittest.TestSuite:
     ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(replace.suite())
     ret_val.addTest(select_transformer.suite())
-    ret_val.addTest(parse_lines_transformer.suite())
+    ret_val.addTest(parse_string_transformer.suite())
     ret_val.addTest(case_converters.suite())
     ret_val.addTest(env_vars_replacement.suite())
     return ret_val
