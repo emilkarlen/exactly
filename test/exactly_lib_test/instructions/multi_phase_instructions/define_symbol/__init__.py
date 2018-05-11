d1a55c180a2fce35e8611a616394de20386a67f5
Emil Karlén <emil.karlen@etraveli.com>
2018-05-11 21:52:06 +0200

    str-trans: Renames package test/define_symbol/...

--- test/exactly_lib_test/instructions/multi_phase_instructions/define_symbol/__init__.py
+++ test/exactly_lib_test/instructions/multi_phase_instructions/define_symbol/__init__.py
@@ -3,7 +3,8 @@ import unittest
 from exactly_lib.instructions.multi_phase_instructions import define_symbol as sut
 from exactly_lib_test.common.help.test_resources.check_documentation import suite_for_instruction_documentation
 from exactly_lib_test.instructions.multi_phase_instructions.define_symbol import \
-    common_failing_cases, string_type, path_type, list_type, line_matcher, file_matcher, lines_transformer, program_type
+    common_failing_cases, string_type, path_type, list_type, line_matcher, file_matcher, string_transformer, \
+    program_type
 
 
 def suite() -> unittest.TestSuite:
@@ -15,7 +16,7 @@ def suite() -> unittest.TestSuite:
         list_type.suite(),
         line_matcher.suite(),
         file_matcher.suite(),
-        lines_transformer.suite(),
+        string_transformer.suite(),
         program_type.suite(),
     ])
 
