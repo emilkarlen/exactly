01d976564c7dd7c5c9ca3ed1996a925b47e1c48e
Emil Karlén <emil@member.fsf.org>
2018-06-16 14:14:24 +0200

    continuous--act-output: Introduces FileWriteDistributor

--- test/exactly_lib_test/util/__init__.py
+++ test/exactly_lib_test/util/__init__.py
@@ -1,21 +1,24 @@
 import unittest
 
+from exactly_lib_test.util import file_write_distributor
 from exactly_lib_test.util import functional, textformat, tables, \
     cli_syntax, symbol_table, collection
-from exactly_lib_test.util import test_resources_test, process_execution
+from exactly_lib_test.util import test_resources_test
+from exactly_lib_test.util.process_execution import package_test_suite as process_execution
 
 
 def suite() -> unittest.TestSuite:
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(functional.suite())
-    ret_val.addTest(symbol_table.suite())
-    ret_val.addTest(test_resources_test.suite())  # These tests depend on symbol_table
-    ret_val.addTest(tables.suite())
-    ret_val.addTest(textformat.suite())
-    ret_val.addTest(cli_syntax.suite())
-    ret_val.addTest(collection.suite())
-    ret_val.addTest(process_execution.suite())
-    return ret_val
+    return unittest.TestSuite([
+        functional.suite(),
+        symbol_table.suite(),
+        test_resources_test.suite(),  # These tests depend on symbol_table,
+        tables.suite(),
+        textformat.suite(),
+        cli_syntax.suite(),
+        collection.suite(),
+        file_write_distributor.suite(),
+        process_execution.suite(),
+    ])
 
 
 if __name__ == '__main__':
