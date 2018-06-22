fab2e39b0609692038dffc02f731b986207359a1
Emil Karlén <emil@member.fsf.org>
2018-06-22 20:04:05 +0200

    path-rel-src-file: REFACT: Org of sec doc pgk

--- test/exactly_lib_test/section_document/__init__.py
+++ test/exactly_lib_test/section_document/__init__.py
@@ -3,13 +3,13 @@ import unittest
 from exactly_lib_test.section_document import element_builder
 from exactly_lib_test.section_document import test_resources_test, document_parser
 from exactly_lib_test.section_document import test_syntax, element_parsers, \
-    parse_source, section_element_parser
+    parse_source, parsed_section_element
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
     ret_val.addTest(test_resources_test.suite())
-    ret_val.addTest(section_element_parser.suite())
+    ret_val.addTest(parsed_section_element.suite())
     ret_val.addTest(test_syntax.suite())
     ret_val.addTest(element_builder.suite())
     ret_val.addTest(parse_source.suite())
