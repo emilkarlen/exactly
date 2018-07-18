9a0579c13b1e7424f43a9459f3a5cf58dd8507f1
Emil Karlén <emil.karlen@fripost.org>
2018-07-18 21:24:22 +0200

    rel-source-file: Test of parse_file_ref

--- test/exactly_lib_test/test_case_file_structure/test_resources/format_rel_option.py
+++ test/exactly_lib_test/test_case_file_structure/test_resources/format_rel_option.py
@@ -7,15 +7,16 @@ from exactly_lib.definitions import file_ref
 
 
 def format_rel_options(template: str) -> str:
-    return template.format_map(_FORMAT_MAP)
+    return template.format_map(FORMAT_MAP)
 
 
-_FORMAT_MAP = {
+FORMAT_MAP = {
     'rel_tmp': file_ref.REL_TMP_OPTION,
     'rel_act': file_ref.REL_ACT_OPTION,
     'rel_result': file_ref.REL_RESULT_OPTION,
     'rel_cd': file_ref.REL_CWD_OPTION,
-    'rel_home': file_ref.REL_HOME_CASE_OPTION,
+    'rel_case_home': file_ref.REL_HOME_CASE_OPTION,
     'rel_act_home': file_ref.REL_HOME_ACT_OPTION,
     'rel_symbol': file_ref.REL_symbol_OPTION,
+    'rel_source_file': file_ref.REL_source_file_dir_OPTION,
 }
