7705623c4a0a91a45df7a1444d05113019f8a5ea
Emil Karlén <emil.karlen@fripost.org>
2018-07-28 23:34:10 +0200

    path-rel-src-file: Improves "cd" concept help text

---
+++ src/exactly_lib/definitions/current_directory_texts.py
@@ -0,0 +1,81 @@
+from typing import List
+
+from exactly_lib.common.help.documentation_text import paths_uses_posix_syntax
+from exactly_lib.definitions import file_ref
+from exactly_lib.definitions import formatting
+from exactly_lib.definitions.entity import concepts, types
+from exactly_lib.definitions.formatting import InstructionName
+from exactly_lib.definitions.test_case.instructions import instruction_names
+from exactly_lib.util.textformat.structure.core import ParagraphItem
+from exactly_lib.util.textformat.textformat_parser import TextParser
+
+
+def abs_or_rel_path_of_existing(file_type: str,
+                                syntax_element: str,
+                                relativity_root: str) -> List[ParagraphItem]:
+    tp = TextParser({
+        'file_type': file_type,
+        'syntax_element': syntax_element,
+        'relativity_root': relativity_root
+    })
+    return tp.fnap(_PATH_DESCRIPTION) + paths_uses_posix_syntax()
+
+
+_PATH_DESCRIPTION = """\
+The absolute or relative path of an existing {file_type}.
+
+
+If {syntax_element} is relative, then it's relative to the {relativity_root}.
+"""
+
+
+def def_instruction_rel_cd_description(path_arg_name: str) -> List[ParagraphItem]:
+    tp = TextParser({
+        'current_directory_concept': formatting.concept_(
+            concepts.CURRENT_WORKING_DIRECTORY_CONCEPT_INFO),
+        'path_arg': path_arg_name,
+        'symbol_concept': formatting.concept(concepts.SYMBOL_CONCEPT_INFO.singular_name),
+        'symbols_concept': formatting.concept(concepts.SYMBOL_CONCEPT_INFO.plural_name),
+    })
+    return tp.fnap(_DEF_INSTRUCTION_REL_CD_DESCRIPTION)
+
+
+def cd_instruction_section_on_def_instruction() -> List[ParagraphItem]:
+    tp = TextParser({
+        'current_directory_concept': formatting.concept_(concepts.CURRENT_WORKING_DIRECTORY_CONCEPT_INFO),
+        'def_instruction': InstructionName(instruction_names.SYMBOL_DEFINITION_INSTRUCTION_NAME),
+        'symbol_concept': formatting.concept(concepts.SYMBOL_CONCEPT_INFO.singular_name),
+        'rel_cd_option': formatting.cli_option(file_ref.REL_CWD_OPTION),
+        'path_type': formatting.keyword(types.PATH_TYPE_INFO.name.singular),
+    })
+    return tp.fnap(_CD_INSTRUCTION_SECTION_ON_DEF_INSTRUCTION)
+
+
+_CD_INSTRUCTION_SECTION_ON_DEF_INSTRUCTION = """\
+The {def_instruction} is special because {symbol_concept} definitions are
+evaluated before the test case execution step.
+
+
+This means, that when a {path_type} {symbol_concept} is defined with a relativity of {current_directory_concept}
+({rel_cd_option})
+it is relative the directory that is current when the symbol is REFERENCED,
+
+not when it is defined.
+"""
+
+_DEF_INSTRUCTION_REL_CD_DESCRIPTION = """\
+NOTE: When a {path_arg} is defined to be relative the {current_directory_concept},
+
+it means that it is relative the directory that is current when the symbol is REFERENCED,
+
+not when it is defined.
+
+
+The {current_directory_concept} refers to the current directory during
+test case execution.
+
+But since definitions of {symbols_concept} happens before test case execution
+({symbols_concept} are constants),
+the {current_directory_concept} is not applicable in the context of
+{symbol_concept} definition.
+"""
