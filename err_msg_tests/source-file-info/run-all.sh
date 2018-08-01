57643763d682d700a8507358f1a911fee8a9372c
Emil Karlén <emil@member.fsf.org>
2018-08-02 01:22:51 +0200

    source-loc-report: Adds error message example

--- err_msg_tests/source-file-info/run-all.sh
+++ err_msg_tests/source-file-info/run-all.sh
@@ -11,3 +11,5 @@ hdr
 xly --suite syntax-error-of-case-instruction-in-suite.suite empty.case
 hdr
 xly --suite instruction-error-of-case-instruction-in-suite.suite empty.case
+hdr
+xly sym-def-should-show-src-file-of-def/sub1/the.case
