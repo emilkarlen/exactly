3190583507d9233d9ba3ceb046942a26e5727e1d
Emil Karlén <emil.karlen@fripost.org>
2018-09-01 14:25:30 +0200

    case-instr-in-suite: Merge master

--- test/exactly_lib_test/test_suite/execution/z_package_suite.py
+++ test/exactly_lib_test/test_suite/execution/z_package_suite.py
@@ -1,11 +1,12 @@
 import unittest
 
 from exactly_lib_test.test_suite.execution import execution_basics, env_vars_should_not_leak_between_cases, \
-    symbol_defs_should_not_leak_between_cases
+    symbol_defs_should_not_leak_between_cases, test_case_file_paths_given_to_test_case_processor
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
+        test_case_file_paths_given_to_test_case_processor.suite(),
         execution_basics.suite(),
         env_vars_should_not_leak_between_cases.suite(),
         symbol_defs_should_not_leak_between_cases.suite(),
