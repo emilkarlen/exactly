5e57ee816a2e09ec7b8852b820d9dc17616c5119
Emil Karlén <emil.karlen@etraveli.com>
2018-09-11 21:21:54 +0200

    Adds type hints

--- src/exactly_lib/help/entities/builtin/entity_configuration.py
+++ src/exactly_lib/help/entities/builtin/entity_configuration.py
@@ -1,10 +1,13 @@
+from typing import List
+
 from exactly_lib.help.contents_structure.entity import EntityTypeConfiguration
 from exactly_lib.help.entities.builtin import render
-from exactly_lib.help.entities.builtin.contents_structure import builtin_symbols_help
+from exactly_lib.help.entities.builtin.contents_structure import builtin_symbols_help, BuiltinSymbolDocumentation
 
 
-def builtin_symbols_entity_configuration(all_builtin_symbols_docs: list) -> EntityTypeConfiguration:
-    return EntityTypeConfiguration(builtin_symbols_help(all_builtin_symbols_docs),
+def builtin_symbols_entity_configuration(all_builtin_symbols: List[BuiltinSymbolDocumentation]
+                                         ) -> EntityTypeConfiguration:
+    return EntityTypeConfiguration(builtin_symbols_help(all_builtin_symbols),
                                    render.IndividualBuiltinSymbolConstructor,
                                    render.list_renderer_getter(),
                                    render.hierarchy_generator_getter())
