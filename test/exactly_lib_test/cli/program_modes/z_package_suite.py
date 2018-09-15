062e7a51781079f6b5168ec546b8eaf3b00ea514
Emil Karlén <emil.karlen@etraveli.com>
2018-09-15 16:03:25 +0200

    exactly-suite: suite FILE argument: accepts DIR if DIR contains exactly.suite

--- test/exactly_lib_test/cli/program_modes/z_package_suite.py
+++ test/exactly_lib_test/cli/program_modes/z_package_suite.py
@@ -7,10 +7,11 @@ from exactly_lib_test.test_resources.main_program.main_program_runner import Mai
 
 
 def suite_that_does_not_require_main_program_runner() -> unittest.TestSuite:
-    ret_val = unittest.TestSuite()
-    ret_val.addTest(test_case.suite_that_does_not_require_main_program_runner())
-    ret_val.addTest(help.suite())
-    return ret_val
+    return unittest.TestSuite([
+        test_case.suite_that_does_not_require_main_program_runner(),
+        test_suite.suite_that_does_not_require_main_program_runner(),
+        help.suite(),
+    ])
 
 
 def suite_that_does_require_main_program_runner(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
@@ -21,13 +22,18 @@ def suite_that_does_require_main_program_runner(main_program_runner: MainProgram
 
 
 def complete_suite_for(main_program_runner: MainProgramRunner) -> unittest.TestSuite:
-    ret_val = suite_that_does_not_require_main_program_runner()
-    ret_val.addTests(suite_that_does_require_main_program_runner(main_program_runner))
-    return ret_val
+    return unittest.TestSuite([
+        suite_that_does_not_require_main_program_runner(),
+        suite_that_does_require_main_program_runner(main_program_runner),
+    ])
 
 
-if __name__ == '__main__':
+def _run_complete_suite():
     from exactly_lib_test.default.test_resources.internal_main_program_runner import \
         main_program_runner_with_default_setup__in_same_process
 
     unittest.TextTestRunner().run(complete_suite_for(main_program_runner_with_default_setup__in_same_process()))
+
+
+if __name__ == '__main__':
+    _run_complete_suite()
