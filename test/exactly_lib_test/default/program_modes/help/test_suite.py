319951ee93e0a84201eb3a7ce526b83507869aec
Emil Karlén <emil.karlen@fripost.org>
2018-09-15 22:30:17 +0200

    remove suite/actor: Replace some uses of the instruction name

--- test/exactly_lib_test/default/program_modes/help/test_suite.py
+++ test/exactly_lib_test/default/program_modes/help/test_suite.py
@@ -1,8 +1,9 @@
 import unittest
+from typing import List
 
 from exactly_lib.cli.definitions.program_modes.help import arguments_for
 from exactly_lib.definitions.test_suite import section_names_plain
-from exactly_lib.definitions.test_suite.instruction_names import INSTRUCTION_NAME__ACTOR
+from exactly_lib.definitions.test_suite.instruction_names import INSTRUCTION_NAME__PREPROCESSOR
 from exactly_lib_test.default.program_modes.help.test_resources import HelpInvokation, RESULT_IS_SUCCESSFUL
 from exactly_lib_test.test_resources.main_program.constant_arguments_check import ProcessTestCase
 from exactly_lib_test.test_resources.main_program.constant_arguments_check_execution import test_suite_for_test_cases
@@ -16,7 +17,7 @@ def suite_that_does_require_main_program_runner(main_program_runner: MainProgram
     return ret_val
 
 
-def _test_cases_for_all_suite_sections() -> list:
+def _test_cases_for_all_suite_sections() -> List[ProcessTestCase]:
     return [
         ProcessTestCase("""help for "suite/section '%s'" SHOULD be successful""" % section_name,
                         HelpInvokation(arguments_for.suite_section_for_name(section_name)),
@@ -25,7 +26,7 @@ def _test_cases_for_all_suite_sections() -> list:
         ]
 
 
-def _global_test_cases() -> list:
+def _global_test_cases() -> List[ProcessTestCase]:
     return [
         ProcessTestCase('help for "suite cli syntax" SHOULD be successful',
                         HelpInvokation(arguments_for.suite_cli_syntax()),
@@ -38,6 +39,6 @@ def _global_test_cases() -> list:
         ProcessTestCase('help for "suite section instruction" SHOULD be successful',
                         HelpInvokation(arguments_for.suite_instruction_in_section(
                             section_names_plain.SECTION_NAME__CONF,
-                            INSTRUCTION_NAME__ACTOR)),
+                            INSTRUCTION_NAME__PREPROCESSOR)),
                         RESULT_IS_SUCCESSFUL),
     ]
