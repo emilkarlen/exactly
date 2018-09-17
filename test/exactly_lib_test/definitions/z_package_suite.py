b327f683192f3df7111e17406fe4504bf0e54227
Emil Karlén <emil.karlen@fripost.org>
2018-09-17 21:22:57 +0200

    textformat/construction: REFACT: Fixes name of test module

--- test/exactly_lib_test/definitions/z_package_suite.py
+++ test/exactly_lib_test/definitions/z_package_suite.py
@@ -1,11 +1,11 @@
 import unittest
 
-from exactly_lib_test.definitions import cross_reference_id
+from exactly_lib_test.definitions import concrete_cross_refs
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
-    ret_val.addTest(cross_reference_id.suite())
+    ret_val.addTest(concrete_cross_refs.suite())
     return ret_val
 
 
