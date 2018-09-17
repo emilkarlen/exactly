e38aca92be5e8f61804fde812637eb34981101ac
Emil Karlén <emil.karlen@etraveli.com>
2018-09-17 21:02:41 +0200

    textformat/construction: REFACT: Fixes name of test module

--- test/exactly_lib_test/util/textformat/construction/section_hierarchy/z_package_suite.py
+++ test/exactly_lib_test/util/textformat/construction/section_hierarchy/z_package_suite.py
@@ -1,13 +1,13 @@
 import unittest
 
 from exactly_lib_test.util.textformat.construction.section_hierarchy import table_of_contents, \
-    generation
+    hierarchy
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         table_of_contents.suite(),
-        generation.suite(),
+        hierarchy.suite(),
     ])
 
 
