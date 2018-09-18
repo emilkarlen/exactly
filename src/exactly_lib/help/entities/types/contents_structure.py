06a792f076071d28dc06fe25a536f6d75b678f25
Emil Karlén <emil.karlen@fripost.org>
2018-09-18 21:28:47 +0200

    help: type PATH: Adds see-also

--- src/exactly_lib/help/entities/types/contents_structure.py
+++ src/exactly_lib/help/entities/types/contents_structure.py
@@ -1,3 +1,7 @@
+from typing import List, Iterable
+
+from exactly_lib.common.help.syntax_contents_structure import InvokationVariant
+from exactly_lib.definitions.cross_ref.app_cross_ref import SeeAlsoTarget
 from exactly_lib.definitions.entity.all_entity_types import TYPE_ENTITY_TYPE_NAMES
 from exactly_lib.definitions.entity.syntax_elements import SyntaxElementInfo
 from exactly_lib.definitions.entity.types import TypeNameAndCrossReferenceId
@@ -14,13 +18,15 @@ class TypeDocumentation(EntityDocumentation):
                  type_category: TypeCategory,
                  name_and_cross_ref_target: TypeNameAndCrossReferenceId,
                  corresponding_syntax_element: SyntaxElementInfo,
-                 main_description_rest: SectionContents):
+                 main_description_rest: SectionContents,
+                 custom_see_also_targets: Iterable[SeeAlsoTarget] = ()):
         super().__init__(name_and_cross_ref_target)
         self._name_and_cross_ref_target = name_and_cross_ref_target
         self._type_identifier = name_and_cross_ref_target.identifier
         self._type_category = type_category
         self._corresponding_syntax_element = corresponding_syntax_element
         self._main_description_rest = main_description_rest
+        self._custom_see_also_targets = list(custom_see_also_targets)
 
     """
     Documents a type of the type system.
@@ -36,16 +42,17 @@ class TypeDocumentation(EntityDocumentation):
     def name(self) -> Name:
         return self._name_and_cross_ref_target.name
 
-    def invokation_variants(self) -> list:
+    def invokation_variants(self) -> List[InvokationVariant]:
         return []
 
     def main_description_rest(self) -> SectionContents:
         return self._main_description_rest
 
-    def see_also_targets(self) -> list:
-        return [
-            self._corresponding_syntax_element.cross_reference_target,
-        ]
+    def see_also_targets(self) -> List[SeeAlsoTarget]:
+        return ([self._corresponding_syntax_element.cross_reference_target]
+                +
+                self._custom_see_also_targets
+                )
 
 
 class LogicTypeWithExpressionGrammarDocumentation(TypeDocumentation):
@@ -67,16 +74,13 @@ class LogicTypeWithExpressionGrammarDocumentation(TypeDocumentation):
     Documents a type of the type system.
     """
 
-    def invokation_variants(self) -> list:
+    def invokation_variants(self) -> List[InvokationVariant]:
         return self._syntax.invokation_variants()
 
-    def see_also_targets(self) -> list:
+    def see_also_targets(self) -> List[SeeAlsoTarget]:
         return super().see_also_targets() + self._syntax.see_also_targets()
 
 
-def types_help(types: iter) -> EntityTypeHelp:
-    """
-    :param types: [TypeDocumentation]
-    """
+def types_help(types: Iterable[TypeDocumentation]) -> EntityTypeHelp:
     return EntityTypeHelp(TYPE_ENTITY_TYPE_NAMES,
                           types)
