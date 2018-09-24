cd0cfe2ac56d13c2ec98a23e6f7fd17303d503dd
Emil Karlén <emil.karlen@fripost.org>
2018-09-25 01:02:57 +0200

    help: Improves description of "actor"

--- src/exactly_lib/definitions/entity/concepts.py
+++ src/exactly_lib/definitions/entity/concepts.py
@@ -119,7 +119,7 @@ SYMBOL_CONCEPT_INFO = name_and_ref_target(
 
 ACTOR_CONCEPT_INFO = name_and_ref_target_for_entity_type(
     all_entity_types.ACTOR_ENTITY_TYPE_NAMES,
-    _format('Interprets the contents of the {phase[act]} phase, and executes it (as the {action_to_check}).')
+    _format('Resolves the {action_to_check} by interpreting the contents of the {act} phase.')
 )
 
 ACTION_TO_CHECK_CONCEPT_INFO = name_and_ref_target(
