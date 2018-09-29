480072686296030796f09a2d985d886dc6149516
Emil Karlén <emil.karlen@fripost.org>
2018-09-29 21:49:22 +0200

    string-matcher: REFACT: ErrorMessageResolvingEnvironment: Removes unused phase logging paths

--- src/exactly_lib/instructions/utils/error_messages.py
+++ src/exactly_lib/instructions/utils/error_messages.py
@@ -5,5 +5,4 @@ from exactly_lib.test_case_utils.err_msg.error_info import ErrorMessageResolving
 def err_msg_env_from_instr_env(
         instruction_env: InstructionEnvironmentForPostSdsStep) -> ErrorMessageResolvingEnvironment:
     return ErrorMessageResolvingEnvironment(instruction_env.home_and_sds,
-                                            instruction_env.phase_logging,
                                             instruction_env.symbols)
