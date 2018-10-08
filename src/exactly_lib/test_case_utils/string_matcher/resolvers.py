a5b76965c2deb38191f9d29e54d1434994fabc42
Emil Karlén <emil.karlen@fripost.org>
2018-10-08 22:06:00 +0200

    string-matcher: REFACT Impl of equals in terms of StringMatcher (cont'd)

--- src/exactly_lib/test_case_utils/string_matcher/resolvers.py
+++ src/exactly_lib/test_case_utils/string_matcher/resolvers.py
@@ -1,10 +1,13 @@
-from typing import Sequence, Callable
+from typing import Sequence, Callable, Set
 
 from exactly_lib.symbol import lookups
+from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.symbol.resolver_structure import StringMatcherResolver
 from exactly_lib.symbol.restriction import ValueTypeRestriction
 from exactly_lib.symbol.symbol_usage import SymbolReference
 from exactly_lib.test_case.pre_or_post_validation import PreOrPostSdsValidator
+from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
 from exactly_lib.type_system.logic import string_matcher_values
 from exactly_lib.type_system.logic.string_matcher import StringMatcher, StringMatcherValue
 from exactly_lib.type_system.value_type import ValueType
@@ -71,6 +74,51 @@ class StringMatcherResolverOfParts(StringMatcherResolver):
 
     def __str__(self):
         return str(type(self))
+
+
+class StringMatcherResolverOfParts2(StringMatcherResolver):
+    def __init__(self,
+                 references: Sequence[SymbolReference],
+                 validator: PreOrPostSdsValidator,
+                 resolving_dependencies: Callable[[SymbolTable], Set[DirectoryStructurePartition]],
+                 matcher: Callable[[PathResolvingEnvironmentPreOrPostSds], StringMatcher]):
+        self._matcher = matcher
+        self._resolving_dependencies = resolving_dependencies
+        self._validator = validator
+        self._references = references
+
+    def resolve(self, symbols: SymbolTable) -> StringMatcherValue:
+        def get_matcher(tcds: HomeAndSds) -> StringMatcher:
+            environment = PathResolvingEnvironmentPreOrPostSds(tcds, symbols)
+            return self._matcher(environment)
+
+        return StringMatcherValueFromParts(self._resolving_dependencies(symbols),
+                                           get_matcher)
+
+    @property
+    def references(self) -> Sequence[SymbolReference]:
+        return self._references
+
+    @property
+    def validator(self) -> PreOrPostSdsValidator:
+        return self._validator
+
+    def __str__(self):
+        return str(type(self))
+
+
+class StringMatcherValueFromParts(StringMatcherValue):
+    def __init__(self,
+                 resolving_dependencies: Set[DirectoryStructurePartition],
+                 matcher: Callable[[HomeAndSds], StringMatcher]):
+        self._matcher = matcher
+        self._resolving_dependencies = resolving_dependencies
+
+    def resolving_dependencies(self) -> Set[DirectoryStructurePartition]:
+        return self._resolving_dependencies
+
+    def value_of_any_dependency(self, home_and_sds: HomeAndSds) -> StringMatcher:
+        return self._matcher(home_and_sds)
 
 
 class StringMatcherReferenceResolver(StringMatcherResolver):
