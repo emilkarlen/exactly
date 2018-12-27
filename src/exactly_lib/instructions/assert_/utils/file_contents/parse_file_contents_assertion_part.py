ca133db81e2f843c3de4209b359e2c944dffd04c
Emil Karlén <emil.karlen@fripost.org>
2018-12-27 11:46:32 +0100

    string-matcher: file contents: MOVE helper module to better place

--- src/exactly_lib/instructions/assert_/utils/file_contents/parse_file_contents_assertion_part.py
+++ src/exactly_lib/instructions/assert_/utils/file_contents/parse_file_contents_assertion_part.py
@@ -1,7 +1,7 @@
 from exactly_lib.instructions.assert_.utils import assertion_part
 from exactly_lib.instructions.assert_.utils.assertion_part import AssertionPart
 from exactly_lib.instructions.assert_.utils.file_contents.parts.contents_checkers import FileTransformerAsAssertionPart
-from exactly_lib.instructions.assert_.utils.file_contents.string_matcher_assertion_part import \
+from exactly_lib.instructions.assert_.utils.file_contents.parts.string_matcher_assertion_part import \
     StringMatcherAssertionPart
 from exactly_lib.section_document.element_parsers.token_stream_parser import TokenParser
 from exactly_lib.test_case_utils.string_matcher.parse import parse_string_matcher
