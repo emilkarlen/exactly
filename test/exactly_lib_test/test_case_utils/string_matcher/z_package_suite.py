a8e84524955a9c5e691067f503a04aa12f2357ef
Emil Karlén <emil.karlen@fripost.org>
2018-12-28 15:32:15 +0100

    string-matcher: Adds test suite for parsing (empty)

--- test/exactly_lib_test/test_case_utils/string_matcher/z_package_suite.py
+++ test/exactly_lib_test/test_case_utils/string_matcher/z_package_suite.py
@@ -1,10 +1,14 @@
 import unittest
 
 from exactly_lib_test.test_case_utils.string_matcher import resolvers
+from exactly_lib_test.test_case_utils.string_matcher.parse import z_package_suite as parse
 
 
 def suite() -> unittest.TestSuite:
-    return resolvers.suite()
+    return unittest.TestSuite([
+        resolvers.suite(),
+        parse.suite(),
+    ])
 
 
 if __name__ == '__main__':
