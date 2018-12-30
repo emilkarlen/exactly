93d14141f713183cfe035da04337ac896503edc1
Emil Karlén <emil@member.fsf.org>
2018-12-30 11:58:49 +0100

    string-matcher: ADD test resource: matches_string_matcher_resolver

---
+++ test/exactly_lib_test/test_case_utils/string_matcher/test_resources/assertions.py
@@ -0,0 +1,61 @@
+from exactly_lib.symbol import resolver_structure
+from exactly_lib.symbol.resolver_structure import LogicValueResolver, StringMatcherResolver
+from exactly_lib.test_case.pre_or_post_validation import PreOrPostSdsValidator
+from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
+from exactly_lib.type_system.logic.string_matcher import StringMatcher, StringMatcherValue
+from exactly_lib.type_system.value_type import ValueType, LogicValueType
+from exactly_lib.util import symbol_table
+from exactly_lib_test.symbol.test_resources.resolver_assertions import is_resolver_of_logic_type
+from exactly_lib_test.test_case_file_structure.test_resources.paths import fake_home_and_sds
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion
+
+
+def matches_string_matcher_resolver(primitive_value: ValueAssertion[StringMatcher] = asrt.anything_goes(),
+                                    references: ValueAssertion = asrt.is_empty_sequence,
+                                    symbols: symbol_table.SymbolTable = None,
+                                    tcds: HomeAndSds = fake_home_and_sds(),
+                                    ) -> ValueAssertion[LogicValueResolver]:
+    symbols = symbol_table.symbol_table_from_none_or_value(symbols)
+
+    def resolve_value(resolver: LogicValueResolver):
+        return resolver.resolve(symbols)
+
+    def resolve_primitive_value(value: StringMatcherValue):
+        return value.value_of_any_dependency(tcds)
+
+    resolved_value_assertion = asrt.is_instance_with_many(
+        StringMatcherValue,
+        [
+            asrt.sub_component('resolving dependencies',
+                               lambda sm_value: sm_value.resolving_dependencies(),
+                               asrt.is_set_of(asrt.is_instance(DirectoryStructurePartition))),
+
+            asrt.sub_component('primitive value',
+                               resolve_primitive_value,
+                               asrt.is_instance_with(StringMatcher,
+                                                     primitive_value))
+        ])
+
+    return asrt.is_instance_with(
+        StringMatcherResolver,
+        asrt.and_([
+            is_resolver_of_logic_type(LogicValueType.STRING_MATCHER,
+                                      ValueType.STRING_MATCHER),
+
+            asrt.sub_component('validator',
+                               lambda resolver: resolver.validator,
+                               asrt.is_instance(PreOrPostSdsValidator)
+                               ),
+
+            asrt.sub_component('references',
+                               resolver_structure.get_references,
+                               references),
+
+            asrt.sub_component('resolved value',
+                               resolve_value,
+                               resolved_value_assertion
+                               ),
+        ])
+    )
---
+++ test/exactly_lib_test/test_case_utils/string_matcher/test_resources_test/assertions.py
@@ -0,0 +1,151 @@
+import unittest
+
+from typing import Sequence
+
+from exactly_lib.symbol.resolver_structure import StringMatcherResolver
+from exactly_lib.symbol.symbol_usage import SymbolReference
+from exactly_lib.test_case_utils.file_matcher.resolvers import FileMatcherConstantResolver
+from exactly_lib.test_case_utils.string_matcher.string_matchers import StringMatcherConstant
+from exactly_lib.util.symbol_table import singleton_symbol_table_2
+from exactly_lib_test.symbol.data.test_resources import data_symbol_utils
+from exactly_lib_test.symbol.test_resources import symbol_utils
+from exactly_lib_test.symbol.test_resources.string_matcher import StringMatcherResolverConstantTestImpl
+from exactly_lib_test.test_case_utils.string_matcher.test_resources import assertions as sut
+from exactly_lib_test.test_resources.name_and_value import NameAndValue
+from exactly_lib_test.test_resources.test_of_test_resources_util import assert_that_assertion_fails
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+from exactly_lib_test.type_system.logic.test_resources.values import FileMatcherTestImpl
+
+
+def suite() -> unittest.TestSuite:
+    return unittest.makeSuite(TestMatchesStringMatcherResolver)
+
+
+class TestMatchesStringMatcherResolver(unittest.TestCase):
+    def test_SHOULD_match_WHEN_whole_structure_is_valid(self):
+        # ARRANGE #
+        cases = [
+            NameAndValue('without symbol table',
+                         None),
+            NameAndValue('with symbol table',
+                         singleton_symbol_table_2(
+                             'the symbol name',
+                             symbol_utils.container(ARBITRARY_RESOLVER),
+                         )),
+
+        ]
+        resolver = ARBITRARY_RESOLVER
+        for case in cases:
+            with self.subTest(name=case.name):
+                assertion_to_check = sut.matches_string_matcher_resolver(symbols=case.value)
+                # ACT & ASSERT #
+                assertion_to_check.apply_without_message(self, resolver)
+
+    def test_SHOULD_not_match_WHEN_actual_is_file_matcher(self):
+        # ARRANGE #
+        cases = [
+            NameAndValue('without symbol table',
+                         None),
+            NameAndValue('with symbol table',
+                         singleton_symbol_table_2(
+                             'the symbol name',
+                             symbol_utils.container(ARBITRARY_RESOLVER),
+                         )),
+
+        ]
+        resolver_of_actual = FileMatcherConstantResolver(FileMatcherTestImpl())
+
+        for case in cases:
+            with self.subTest(name=case.name):
+                assertion_to_check = sut.matches_string_matcher_resolver(symbols=case.value)
+                # ACT & ASSERT #
+                assert_that_assertion_fails(assertion_to_check, resolver_of_actual)
+
+    def test_SHOULD_not_match_WHEN_validator_is_none(self):
+        # ARRANGE #
+        cases = [
+            NameAndValue('without symbol table',
+                         None),
+            NameAndValue('with symbol table',
+                         singleton_symbol_table_2(
+                             'the symbol name',
+                             symbol_utils.container(ARBITRARY_RESOLVER),
+                         )),
+
+        ]
+        resolver_of_actual = StringMatcherResolverConstantTestImpl(StringMatcherConstant(None),
+                                                                   validator=None)
+
+        for case in cases:
+            with self.subTest(name=case.name):
+                assertion_to_check = sut.matches_string_matcher_resolver(symbols=case.value)
+                # ACT & ASSERT #
+                assert_that_assertion_fails(assertion_to_check, resolver_of_actual)
+
+    def test_SHOULD_not_match_WHEN_assertion_on_matcher_fails(self):
+        # ARRANGE #
+        cases = [
+            NameAndValue('without symbol table',
+                         None),
+            NameAndValue('with symbol table',
+                         singleton_symbol_table_2(
+                             'the symbol name',
+                             symbol_utils.container(ARBITRARY_RESOLVER),
+                         )),
+
+        ]
+        resolver_of_actual = ARBITRARY_RESOLVER
+
+        for case in cases:
+            with self.subTest(name=case.name):
+                assertion_to_check = sut.matches_string_matcher_resolver(primitive_value=asrt.fail('unconditionally'),
+                                                                         symbols=case.value)
+                # ACT & ASSERT #
+                assert_that_assertion_fails(assertion_to_check, resolver_of_actual)
+
+    def test_SHOULD_match_WHEN_references_match(self):
+        # ARRANGE #
+        actual_reference = data_symbol_utils.symbol_reference('referenced element')
+        actual_references = [actual_reference]
+        actual_resolver = arbitrary_resolver_with_references(actual_references)
+
+        assertion_to_check = sut.matches_string_matcher_resolver(references=asrt.matches_sequence([
+            asrt.is_(actual_reference)
+        ]),
+        )
+        # ACT & ASSERT #
+        assertion_to_check.apply_without_message(self, actual_resolver)
+
+    def test_SHOULD_not_match_WHEN_references_do_not_match(self):
+        # ARRANGE #
+        actual_reference = data_symbol_utils.symbol_reference('referenced element')
+        actual_references = [actual_reference]
+        actual_resolver = arbitrary_resolver_with_references(actual_references)
+
+        cases = [
+            NameAndValue('assert no references',
+                         asrt.is_empty_sequence),
+            NameAndValue('assert single invalid reference',
+                         asrt.matches_sequence([
+                             asrt.not_(asrt.is_(actual_reference))
+                         ])),
+        ]
+
+        for case in cases:
+            with self.subTest(name=case.name):
+                assertion_to_check = sut.matches_string_matcher_resolver(references=case.value)
+                # ACT & ASSERT #
+                assert_that_assertion_fails(assertion_to_check, actual_resolver)
+
+
+ARBITRARY_RESOLVER = StringMatcherResolverConstantTestImpl(StringMatcherConstant(None),
+                                                           [])
+
+
+def arbitrary_resolver_with_references(references: Sequence[SymbolReference]) -> StringMatcherResolver:
+    return StringMatcherResolverConstantTestImpl(StringMatcherConstant(None),
+                                                 references)
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
