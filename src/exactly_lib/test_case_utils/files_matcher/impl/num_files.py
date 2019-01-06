6e1b8d74b54f1c2db0cce5320554cd7b4b6a49a3
Emil Karlén <emil.karlen@fripost.org>
2019-01-06 14:29:36 +0100

    files-matcher: REFACT Resolve file list via alternative matcher model / num-files

--- src/exactly_lib/test_case_utils/files_matcher/impl/num_files.py
+++ src/exactly_lib/test_case_utils/files_matcher/impl/num_files.py
@@ -1,7 +1,5 @@
-import pathlib
 from typing import Sequence, Optional
 
-from exactly_lib.symbol.data.file_ref_resolver import FileRefResolver
 from exactly_lib.symbol.object_with_symbol_references import references_from_objects_with_symbol_references
 from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.symbol.resolver_structure import FileMatcherResolver
@@ -9,13 +7,13 @@ from exactly_lib.symbol.symbol_usage import SymbolReference
 from exactly_lib.test_case_utils.condition import comparison_structures
 from exactly_lib.test_case_utils.condition.integer import parse_integer_condition as parse_expr
 from exactly_lib.test_case_utils.files_matcher import files_matchers, config
-from exactly_lib.test_case_utils.files_matcher.files_matchers import FilesMatcherResolverBase
-from exactly_lib.test_case_utils.files_matcher.structure import FilesSource, FilesMatcherResolver, \
+from exactly_lib.test_case_utils.files_matcher.files_matchers import FilesMatcherResolverBaseForNewModel
+from exactly_lib.test_case_utils.files_matcher.new_model import FilesMatcherModel
+from exactly_lib.test_case_utils.files_matcher.structure import FilesMatcherResolver, \
     Environment
 from exactly_lib.test_case_utils.validators import PreOrPostSdsValidatorFromValidatorViaExceptions, \
     SvhValidatorViaExceptionsFromPreAndPostSdsValidators
 from exactly_lib.type_system.error_message import ErrorMessageResolver
-from exactly_lib.type_system.logic import file_matcher as file_matcher_type
 
 
 def num_files_matcher(settings: files_matchers.Settings,
@@ -24,7 +22,7 @@ def num_files_matcher(settings: files_matchers.Settings,
     return _NumFilesMatcher(settings, operator_and_r_operand)
 
 
-class _NumFilesMatcher(FilesMatcherResolverBase):
+class _NumFilesMatcher(FilesMatcherResolverBaseForNewModel):
     def __init__(self,
                  settings: files_matchers.Settings,
                  operator_and_r_operand: parse_expr.IntegerComparisonOperatorAndRightOperand):
@@ -46,14 +44,13 @@ class _NumFilesMatcher(FilesMatcherResolverBase):
     def references(self) -> Sequence[SymbolReference]:
         return self._references
 
-    def matches(self,
-                environment: Environment,
-                files_source: FilesSource) -> Optional[ErrorMessageResolver]:
+    def matches_new(self,
+                    environment: Environment,
+                    files_source: FilesMatcherModel) -> Optional[ErrorMessageResolver]:
         comparison_handler = comparison_structures.ComparisonHandler(
-            self._settings.property_descriptor(config.NUM_FILES_PROPERTY_NAME,
-                                               files_source.path_of_dir),
+            files_source.error_message_info.property_descriptor(config.NUM_FILES_PROPERTY_NAME),
             self._settings.expectation_type,
-            NumFilesResolver(files_source.path_of_dir,
+            NumFilesResolver(files_source,
                              self._settings.file_matcher),
             self._operator_and_r_operand.operator,
             self._operator_and_r_operand.right_operand)
@@ -65,15 +62,11 @@ class _NumFilesMatcher(FilesMatcherResolverBase):
 
 class NumFilesResolver(comparison_structures.OperandResolver[int]):
     def __init__(self,
-                 path_to_check: FileRefResolver,
+                 path_to_check: FilesMatcherModel,
                  file_matcher: FileMatcherResolver):
         super().__init__(config.NUM_FILES_PROPERTY_NAME)
         self.path_to_check = path_to_check
         self.file_matcher = file_matcher
 
     def resolve_value_of_any_dependency(self, environment: PathResolvingEnvironmentPreOrPostSds) -> int:
-        path_to_check = self.path_to_check.resolve_value_of_any_dependency(environment)
-        assert isinstance(path_to_check, pathlib.Path), 'Resolved value should be a path'
-        file_matcher = self.file_matcher.resolve(environment.symbols)
-        selected_files = file_matcher_type.matching_files_in_dir(file_matcher, path_to_check)
-        return len(list(selected_files))
+        return len(list(self.path_to_check.files()))
