a6e23d451a4de6dfa511ff31608ee167957ab850
Emil Karlén <emil.karlen@fripost.org>
2019-01-27 01:17:56 +0100

    type-system: ADD FileMatcherValueFromPrimitiveValue

--- src/exactly_lib/type_system/logic/file_matchers.py
+++ src/exactly_lib/type_system/logic/file_matchers.py
@@ -1,11 +1,24 @@
 import pathlib
-from typing import Iterator
+from typing import Iterator, Set
 
-from exactly_lib.type_system.logic.file_matcher import FileMatcher
+from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
+from exactly_lib.type_system.logic.file_matcher import FileMatcher, FileMatcherValue
 
 
 def matching_files_in_dir(matcher: FileMatcher, dir_path: pathlib.Path) -> Iterator[pathlib.Path]:
-    """
-    :return: Iterator of :class:`pathlib.Path`
-    """
     return filter(matcher.matches, dir_path.iterdir())
+
+
+class FileMatcherValueFromPrimitiveValue(FileMatcherValue):
+    def __init__(self, primitive_value: FileMatcher):
+        self._primitive_value = primitive_value
+
+    def resolving_dependencies(self) -> Set[DirectoryStructurePartition]:
+        return set()
+
+    def value_when_no_dir_dependencies(self) -> FileMatcher:
+        return self._primitive_value
+
+    def value_of_any_dependency(self, tcds: HomeAndSds) -> FileMatcher:
+        return self._primitive_value
