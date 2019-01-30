4ddd33d93c10c73e394afe0f61814dbd03bde87d
Emil Karlén <emil.karlen@fripost.org>
2019-01-30 02:30:21 +0100

    line-matcher: ADD PreOrPostSdsValueValidator:s

--- src/exactly_lib/test_case/pre_or_post_value_validators.py
+++ src/exactly_lib/test_case/pre_or_post_value_validators.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import Optional, Sequence, Iterable
 
 from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreSds, \
     PathResolvingEnvironmentPostSds
 from exactly_lib.test_case.pre_or_post_validation import PreOrPostSdsValidator
-from exactly_lib.test_case.pre_or_post_value_validation import PreOrPostSdsValueValidator
+from exactly_lib.test_case.pre_or_post_value_validation import PreOrPostSdsValueValidator, constant_success_validator
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_file_structure.home_directory_structure import HomeDirectoryStructure
 from exactly_lib.util.symbol_table import SymbolTable
@@ -23,3 +23,32 @@ class ValueValidatorFromResolverValidator(PreOrPostSdsValueValidator):
     def validate_post_sds_if_applicable(self, tcds: HomeAndSds) -> Optional[str]:
         environment = PathResolvingEnvironmentPostSds(tcds.sds, self._symbols)
         return self._adapted.validate_post_sds_if_applicable(environment)
+
+
+def all_of(validators: Sequence[PreOrPostSdsValueValidator]) -> PreOrPostSdsValueValidator:
+    if len(validators) == 0:
+        return constant_success_validator()
+    elif len(validators) == 1:
+        return validators[0]
+    else:
+        return AndValidator(validators)
+
+
+class AndValidator(PreOrPostSdsValueValidator):
+    def __init__(self,
+                 validators: Iterable[PreOrPostSdsValueValidator]):
+        self.validators = validators
+
+    def validate_pre_sds_if_applicable(self, hds: HomeDirectoryStructure) -> Optional[str]:
+        for validator in self.validators:
+            result = validator.validate_pre_sds_if_applicable(hds)
+            if result is not None:
+                return result
+        return None
+
+    def validate_post_sds_if_applicable(self, tcds: HomeAndSds) -> Optional[str]:
+        for validator in self.validators:
+            result = validator.validate_post_sds_if_applicable(tcds)
+            if result is not None:
+                return result
+        return None
