46bbbd3a629030bc2d53deb34375b0288ea49f21
Emil Karlén <emil.karlen@fripost.org>
2019-01-31 08:44:27 +0100

    string-transformer/validation: Adds validation to StringTransformerValue

--- src/exactly_lib/type_system/logic/string_transformer.py
+++ src/exactly_lib/type_system/logic/string_transformer.py
@@ -1,6 +1,7 @@
 import functools
 from typing import Set, Iterable, Sequence
 
+from exactly_lib.test_case.pre_or_post_value_validation import PreOrPostSdsValueValidator, constant_success_validator
 from exactly_lib.test_case_file_structure.dir_dependent_value import MultiDirDependentValue
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
@@ -31,6 +32,9 @@ class StringTransformer:
 class StringTransformerValue(MultiDirDependentValue[StringTransformer]):
     def resolving_dependencies(self) -> Set[DirectoryStructurePartition]:
         return set()
+
+    def validator(self) -> PreOrPostSdsValueValidator:
+        return constant_success_validator()
 
     def value_when_no_dir_dependencies(self) -> StringTransformer:
         """
