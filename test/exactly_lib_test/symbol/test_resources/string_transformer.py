172475f217da97a640a457e943169f1b11463541
Emil Karlén <emil.karlen@fripost.org>
2019-01-31 08:43:34 +0100

    string-transformer/validation: ADD Test resources for result of validation (pre or post sds)

--- test/exactly_lib_test/symbol/test_resources/string_transformer.py
+++ test/exactly_lib_test/symbol/test_resources/string_transformer.py
@@ -1,7 +1,7 @@
 from typing import Sequence, Optional, Set
 
 from exactly_lib.symbol.logic.string_transformer import StringTransformerResolver
-from exactly_lib.symbol.symbol_usage import SymbolReference
+from exactly_lib.symbol.symbol_usage import SymbolReference, SymbolUsage
 from exactly_lib.test_case.pre_or_post_value_validation import PreOrPostSdsValueValidator, constant_success_validator
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
@@ -12,6 +12,7 @@ from exactly_lib.type_system.value_type import ValueType
 from exactly_lib.util.symbol_table import SymbolTable
 from exactly_lib_test.symbol.test_resources import symbol_usage_assertions as asrt_sym_usage
 from exactly_lib_test.symbol.test_resources.restrictions_assertions import is_value_type_restriction
+from exactly_lib_test.symbol.test_resources.symbols_setup import ResolverSymbolContext
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion
 
@@ -27,6 +28,16 @@ class StringTransformerConstantTestImpl(StringTransformer):
 
     def transform(self, lines: StringTransformerModel) -> StringTransformerModel:
         return self._result
+
+
+class StringTransformerIdentityTestImpl(StringTransformer):
+    """Matcher with no modification."""
+
+    def is_identity_transformer(self) -> bool:
+        return True
+
+    def transform(self, lines: StringTransformerModel) -> StringTransformerModel:
+        return lines
 
 
 class StringTransformerConstantSequenceTestImpl(StringTransformer):
@@ -49,7 +60,9 @@ class StringTransformerValueTestImpl(StringTransformerValue):
                  resolving_dependencies: Optional[Set[DirectoryStructurePartition]] = None):
         self._primitive_value = primitive_value
         self._validator = validator
-        self._resolving_dependencies = resolving_dependencies
+        self._resolving_dependencies = (set()
+                                        if resolving_dependencies is None
+                                        else resolving_dependencies)
 
     def resolving_dependencies(self) -> Set[DirectoryStructurePartition]:
         return self._resolving_dependencies
@@ -102,7 +115,7 @@ class StringTransformerResolverConstantValueTestImpl(StringTransformerResolver):
         return self._resolved_value
 
 
-def string_transformer_from_primitive_value(primitive_value: StringTransformer = StringTransformerConstantTestImpl(()),
+def string_transformer_from_primitive_value(primitive_value: StringTransformer = StringTransformerIdentityTestImpl(),
                                             references: Sequence[SymbolReference] = (),
                                             validator: PreOrPostSdsValueValidator = constant_success_validator(),
                                             ) -> StringTransformerResolver:
@@ -128,12 +141,41 @@ def string_transformer_from_result(result: StringTransformerModel,
     )
 
 
+def string_transformer_from_repeatable_result(result: Sequence[str],
+                                              references: Sequence[SymbolReference] = (),
+                                              validator: PreOrPostSdsValueValidator = constant_success_validator(),
+                                              ) -> StringTransformerResolver:
+    return StringTransformerResolverConstantValueTestImpl(
+        StringTransformerValueTestImpl(
+            StringTransformerConstantSequenceTestImpl(result),
+            validator
+        ),
+        references=references,
+    )
+
+
+class StringTransformerSymbolContext(ResolverSymbolContext[StringTransformerResolver]):
+    def __init__(self,
+                 name: str,
+                 resolver: StringTransformerResolver):
+        super().__init__(name)
+        self._resolver = resolver
+
+    @property
+    def resolver(self) -> StringTransformerResolver:
+        return self._resolver
+
+    @property
+    def reference_assertion(self) -> ValueAssertion[SymbolReference]:
+        return is_reference_to_string_transformer__ref(self.name)
+
+
 def model_with_num_lines(number_of_lines: int) -> StringTransformerModel:
     return iter(['line'] * number_of_lines)
 
 
 def arbitrary_transformer() -> StringTransformer:
-    return StringTransformerConstantTestImpl(model_with_num_lines(0))
+    return StringTransformerIdentityTestImpl()
 
 
 def arbitrary_transformer_value() -> StringTransformerValue:
@@ -141,12 +183,19 @@ def arbitrary_transformer_value() -> StringTransformerValue:
 
 
 def arbitrary_transformer_resolver() -> StringTransformerResolver:
-    return string_transformer_from_result(model_with_num_lines(0))
+    return string_transformer_from_primitive_value(arbitrary_transformer())
 
 
 IS_STRING_TRANSFORMER_REFERENCE_RESTRICTION = is_value_type_restriction(ValueType.STRING_TRANSFORMER)
 
 
-def is_reference_to_string_transformer(name_of_transformer: str) -> ValueAssertion:
+def is_reference_to_string_transformer(name_of_transformer: str) -> ValueAssertion[SymbolUsage]:
     return asrt_sym_usage.matches_reference(asrt.equals(name_of_transformer),
                                             IS_STRING_TRANSFORMER_REFERENCE_RESTRICTION)
+
+
+def is_reference_to_string_transformer__ref(name_of_transformer: str) -> ValueAssertion[SymbolReference]:
+    return asrt.is_instance_with(SymbolReference,
+                                 asrt_sym_usage.matches_reference(asrt.equals(name_of_transformer),
+                                                                  IS_STRING_TRANSFORMER_REFERENCE_RESTRICTION)
+                                 )
