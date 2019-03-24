49c343a7c05788802450c9804b47382bb95cdfbd
Emil Karlén <emil.karlen@fripost.org>
2019-03-24 15:57:44 +0100

    exists-file-matcher: Fixes invalid instruction test resources

--- test/exactly_lib_test/instructions/assert_/test_resources/instruction_check.py
+++ test/exactly_lib_test/instructions/assert_/test_resources/instruction_check.py
@@ -1,6 +1,5 @@
-import unittest
-
 import os
+import unittest
 from typing import Sequence
 
 from exactly_lib.execution import phase_step
@@ -20,8 +19,8 @@ from exactly_lib_test.section_document.test_resources.misc import ARBITRARY_FS_L
 from exactly_lib_test.test_case.result.test_resources import pfh_assertions, svh_assertions
 from exactly_lib_test.test_case.test_resources.arrangements import ArrangementPostAct, ActEnvironment
 from exactly_lib_test.test_case_file_structure.test_resources.sds_check.sds_utils import write_act_result
-from exactly_lib_test.test_case_utils.test_resources.validation import all_validations_passes, \
-    ValidationExpectationSvh
+from exactly_lib_test.test_case_utils.test_resources.validation import ValidationExpectationSvh, \
+    all_validations_passes__svh
 from exactly_lib_test.test_resources.test_case_file_struct_and_symbols.home_and_sds_utils import \
     home_and_sds_with_act_as_curr_dir
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
@@ -53,7 +52,7 @@ class Expectation:
 
 
 def expectation(
-        validation: ValidationExpectationSvh = all_validations_passes(),
+        validation: ValidationExpectationSvh = all_validations_passes__svh(),
         main_result: ValueAssertion[pfh.PassOrFailOrHardError] = pfh_assertions.is_pass(),
         symbol_usages: ValueAssertion[Sequence[SymbolUsage]] = asrt.is_empty_sequence,
         main_side_effects_on_sds: ValueAssertion[SandboxDirectoryStructure] = asrt.anything_goes(),
