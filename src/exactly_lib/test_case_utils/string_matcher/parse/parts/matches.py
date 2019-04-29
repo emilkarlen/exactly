d648a9d5fba62ff0037e969c7356a59e2b14476b
Emil Karlén <emil.karlen@fripost.org>
2019-04-29 02:31:38 +0200

    err-msgs: STRING-MATCHER/matches: Improves error message

--- src/exactly_lib/test_case_utils/string_matcher/parse/parts/matches.py
+++ src/exactly_lib/test_case_utils/string_matcher/parse/parts/matches.py
@@ -1,3 +1,5 @@
+import re
+import shlex
 from typing import Optional, Pattern, Match
 
 from exactly_lib.definitions.entity import syntax_elements
@@ -5,18 +7,18 @@ from exactly_lib.section_document.element_parsers.token_stream_parser import Tok
 from exactly_lib.symbol.logic.string_matcher import StringMatcherResolver
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_utils.err_msg import diff_msg
-from exactly_lib.test_case_utils.err_msg import err_msg_resolvers
 from exactly_lib.test_case_utils.regex import parse_regex
 from exactly_lib.test_case_utils.regex.error_messages import ExpectedValueResolver, ErrorMessageResolverConstructor
 from exactly_lib.test_case_utils.regex.regex_value import RegexResolver
 from exactly_lib.test_case_utils.string_matcher import matcher_options
 from exactly_lib.test_case_utils.string_matcher.resolvers import StringMatcherResolverFromValueWithValidation, \
     StringMatcherValueWithValidation
-from exactly_lib.type_system.error_message import ErrorMessageResolver, \
-    ConstantErrorMessageResolver
+from exactly_lib.type_system.error_message import ErrorMessageResolver, ErrorMessageResolvingEnvironment
 from exactly_lib.type_system.logic.string_matcher import FileToCheck, StringMatcher
 from exactly_lib.util.logic_types import ExpectationType
 from exactly_lib.util.symbol_table import SymbolTable
+
+_ERR_MSG_MAX_NUM_LINES_TO_DISPLAY = 10
 
 
 def parse(expectation_type: ExpectationType,
@@ -82,14 +84,14 @@ class MatchesRegexStringMatcher(StringMatcher):
         match = self._find_match(actual_contents)
         if match is None:
             if self._expectation_type is ExpectationType.POSITIVE:
-                return err_msg_resolvers.constant('Not found: ' + self._pattern.pattern)
+                return self._err_msg_resolver(actual_contents)
             else:
                 return None
         else:
             if self._expectation_type is ExpectationType.POSITIVE:
                 return None
             else:
-                return err_msg_resolvers.constant('Found: ' + self._pattern.pattern)
+                return self._err_msg_resolver(actual_contents)
 
     def _find_match(self, actual_contents: str) -> Match:
         if self._is_full_match:
@@ -97,10 +99,63 @@ class MatchesRegexStringMatcher(StringMatcher):
         else:
             return self._pattern.search(actual_contents)
 
-    def _actual_contents(self, model: FileToCheck) -> str:
+    @staticmethod
+    def _actual_contents(model: FileToCheck) -> str:
         with model.lines() as lines:
             return ''.join(lines)
 
     @property
     def option_description(self) -> str:
         return diff_msg.negation_str(self._expectation_type) + matcher_options.MATCHES_ARGUMENT
+
+    def _err_msg_resolver(self, actual_contents: str) -> ErrorMessageResolver:
+        return _ErrorMessageResolver(self._expectation_type,
+                                     self._is_full_match,
+                                     self._pattern,
+                                     actual_contents)
+
+
+class _ErrorMessageResolver(ErrorMessageResolver):
+    def __init__(self,
+                 expectation_type: ExpectationType,
+                 is_full_match: bool,
+                 pattern: Pattern[str],
+                 actual_contents: str,
+                 ):
+        self._expectation_type = expectation_type
+        self._is_full_match = is_full_match
+        self._pattern = pattern
+        self._actual_contents = actual_contents
+
+    def resolve(self, environment: ErrorMessageResolvingEnvironment) -> str:
+        regex_line = self._header() + self._flags_string() + shlex.quote(self._pattern.pattern)
+        return regex_line + '\nIn:\n' + self._contents_lines()
+
+    def _contents_lines(self) -> str:
+        if not self._actual_contents:
+            return '<empty string>'
+        lines = self._actual_contents.split(sep='\n', maxsplit=_ERR_MSG_MAX_NUM_LINES_TO_DISPLAY)
+
+        if len(lines) > _ERR_MSG_MAX_NUM_LINES_TO_DISPLAY:
+            lines[-1] = '...'
+
+        return '\n'.join(lines)
+
+    def _header(self) -> str:
+        start = ('Found no match of '
+                 if self._expectation_type is ExpectationType.POSITIVE
+                 else 'Found match of ')
+        return start + syntax_elements.REGEX_SYNTAX_ELEMENT.singular_name + ' '
+
+    def _flags_string(self) -> str:
+        flags = []
+        if self._is_full_match:
+            flags.append(matcher_options.FULL_MATCH_ARGUMENT_OPTION.long)
+        if self._pattern.flags & re.IGNORECASE:
+            flags.append(parse_regex.IGNORE_CASE_OPTION_NAME.long)
+
+        return (
+            '(' + ','.join(flags) + ') '
+            if flags
+            else ''
+        )
