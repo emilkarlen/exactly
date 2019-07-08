e7c84453bb87d771b57046572aaffbf6728cc4e7
Emil Karlén <emil.karlen@fripost.org>
2019-07-08 15:11:30 +0200

    err-msg-struct: ADD test resources: Assertions on MatchingResult

---
+++ test/exactly_lib_test/type_system/trace/test_resources/matching_result_assertions.py
@@ -0,0 +1,21 @@
+from exactly_lib.type_system.logic.matcher_base_class import MatchingResult
+from exactly_lib.type_system.trace.trace_rendering import NodeRenderer
+from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
+from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion
+from exactly_lib_test.type_system.trace.test_resources import trace_rendering_assertions as asrt_trace_rendering
+
+
+def matches(value: ValueAssertion[bool] = asrt.is_instance(bool),
+            trace: ValueAssertion[NodeRenderer[bool]] =
+            asrt_trace_rendering.matches_node_renderer()) -> ValueAssertion[MatchingResult]:
+    return asrt.is_instance_with_many(MatchingResult,
+                                      [
+                                          asrt.sub_component('value',
+                                                             MatchingResult.value.fget,
+                                                             asrt.is_instance_with(bool, value)
+                                                             ),
+                                          asrt.sub_component('trace',
+                                                             MatchingResult.trace.fget,
+                                                             asrt.is_instance_with(NodeRenderer, trace)
+                                                             ),
+                                      ])
--- test/exactly_lib_test/type_system/trace/test_resources_test/z_package_suite.py
+++ test/exactly_lib_test/type_system/trace/test_resources_test/z_package_suite.py
@@ -1,12 +1,14 @@
 import unittest
 
-from exactly_lib_test.type_system.trace.test_resources_test import trace_assertions, trace_rendering_assertions
+from exactly_lib_test.type_system.trace.test_resources_test import trace_assertions, trace_rendering_assertions, \
+    matching_result_assertions
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
         trace_assertions.suite(),
         trace_rendering_assertions.suite(),
+        matching_result_assertions.suite(),
     ])
 
 
