6ccca3c111f054efb88784aefbffd46cab3415e4
Emil Karlén <emil.karlen@fripost.org>
2019-08-01 12:56:18 +0200

    err-msg-struct: REMOVE unused code

--- src/exactly_lib/test_case_utils/err_msg/trace_doc/line_objects.py
+++
@@ -1,7 +0,0 @@
-from exactly_lib.common.report_rendering import text_doc
-from exactly_lib.common.report_rendering.text_doc import TraceDoc
-
-
-def of_pre_formatted_text(text: str,
-                          is_line_ended: bool = False) -> TraceDoc:
-    return text_doc.PreFormattedDoc(text, is_line_ended)
