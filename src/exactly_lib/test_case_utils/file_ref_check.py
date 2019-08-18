6d68164666e897b3e33b4ab1ca6d97652ffa054f
Emil Karlén <emil.karlen@fripost.org>
2019-08-19 00:00:09 +0200

    err-msg-struct: FileRefCheck: REFACT removes unused code etc

--- src/exactly_lib/test_case_utils/file_ref_check.py
+++ src/exactly_lib/test_case_utils/file_ref_check.py
@@ -8,7 +8,7 @@ from exactly_lib.test_case.result import svh
 from exactly_lib.test_case_utils import file_properties
 from exactly_lib.test_case_utils.err_msg2.described_path import DescribedPathPrimitive
 from exactly_lib.test_case_utils.err_msg2.path_impl import described_path_resolvers
-from exactly_lib.test_case_utils.file_properties import FilePropertiesCheck, CheckResult
+from exactly_lib.test_case_utils.file_properties import FilePropertiesCheck
 from exactly_lib.test_case_utils.file_ref_validator import FileRefValidatorBase
 
 
@@ -18,18 +18,6 @@ class FileRefCheck:
                  check: FilePropertiesCheck):
         self.file_ref_resolver = file_ref_resolver
         self.file_properties = check
-
-    def pre_sds_condition_result(self, environment: PathResolvingEnvironmentPreSds) -> CheckResult:
-        fr = self.file_ref_resolver.resolve(environment.symbols)
-        return self.file_properties.apply(fr.value_pre_sds(environment.hds))
-
-    def post_sds_condition_result(self, environment: PathResolvingEnvironmentPostSds) -> CheckResult:
-        fr = self.file_ref_resolver.resolve(environment.symbols)
-        return self.file_properties.apply(fr.value_post_sds(environment.sds))
-
-    def pre_or_post_sds_condition_result(self, environment: PathResolvingEnvironmentPreOrPostSds) -> CheckResult:
-        fr = self.file_ref_resolver.resolve(environment.symbols)
-        return self.file_properties.apply(fr.value_of_any_dependency(environment.home_and_sds))
 
 
 class FileRefCheckValidator(FileRefValidatorBase):
@@ -78,23 +66,17 @@ def pre_or_post_sds_failure_message_or_none(file_ref_check: FileRefCheck,
 
 def pre_sds_validate(file_ref_check: FileRefCheck,
                      environment: PathResolvingEnvironmentPreSds) -> svh.SuccessOrValidationErrorOrHardError:
-    mb_failure = pre_sds_failure_message_or_none(file_ref_check,
-                                                 environment)
-    return (
-        svh.new_svh_success()
-        if mb_failure is None
-        else svh.new_svh_validation_error(mb_failure)
+    return svh.new_maybe_svh_validation_error(
+        pre_sds_failure_message_or_none(file_ref_check,
+                                        environment)
     )
 
 
 def pre_or_post_sds_validate(file_ref_check: FileRefCheck,
                              environment: PathResolvingEnvironmentPreOrPostSds
                              ) -> svh.SuccessOrValidationErrorOrHardError:
-    mb_failure = pre_or_post_sds_failure_message_or_none(file_ref_check, environment)
-    return (
-        svh.new_svh_success()
-        if mb_failure is None
-        else svh.new_svh_validation_error(mb_failure)
+    return svh.new_maybe_svh_validation_error(
+        pre_or_post_sds_failure_message_or_none(file_ref_check, environment)
     )
 
 
@@ -102,8 +84,9 @@ def _failure_message_or_none(check: FilePropertiesCheck,
                              path: DescribedPathPrimitive) -> Optional[TextRenderer]:
     result = check.apply(path.primitive)
 
-    if result.is_success:
-        return None
-    else:
-        return file_properties.FailureRenderer(result.cause,
-                                               path.describer)
+    return (
+        None
+        if result.is_success
+        else file_properties.FailureRenderer(result.cause,
+                                             path.describer)
+    )
