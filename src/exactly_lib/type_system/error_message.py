827b7894b0dc5fc2c1a6039d38077ecbc6e27eab
Emil Karlén <emil.karlen@fripost.org>
2019-09-20 13:50:04 +0200

    err-msg-struct: REMOVE unused code: ErrorMessageResolvingEnvironment

--- src/exactly_lib/type_system/error_message.py
+++ src/exactly_lib/type_system/error_message.py
@@ -1,32 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List
 
 from exactly_lib.common.report_rendering import print
 from exactly_lib.common.report_rendering import text_docs
 from exactly_lib.common.report_rendering.text_doc import TextRenderer
-from exactly_lib.test_case_file_structure import sandbox_directory_structure as _sds
-from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
-from exactly_lib.util.symbol_table import SymbolTable
-
-
-class ErrorMessageResolvingEnvironment:
-    def __init__(self,
-                 tcds: HomeAndSds,
-                 symbols: Optional[SymbolTable] = None):
-        self._tcds = tcds
-        self._symbols = SymbolTable() if symbols is None else symbols
-
-    @property
-    def tcds(self) -> HomeAndSds:
-        return self._tcds
-
-    @property
-    def sds(self) -> _sds.SandboxDirectoryStructure:
-        return self._tcds.sds
-
-    @property
-    def symbols(self) -> SymbolTable:
-        return self._symbols
 
 
 class ErrorMessageResolver(ABC):
