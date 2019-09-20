cfd3f98d72a7e19e7dd074b2b5ae6d6e3cc20fb4
Emil Karlén <emil.karlen@fripost.org>
2019-09-20 14:28:30 +0200

    err-msg-struct: MOVE ErrorMessageResolver:s to separate package

--- src/exactly_lib/test_case_utils/err_msg/err_msg_resolvers.py
+++ src/exactly_lib/test_case_utils/err_msg/err_msg_resolvers.py
@@ -1,46 +1,46 @@
 from typing import Sequence, Callable
 
+from exactly_lib.common.report_rendering import print
 from exactly_lib.common.report_rendering.text_doc import TextRenderer
 from exactly_lib.test_case_utils.err_msg2 import path_rendering
 from exactly_lib.type_system.data.path_describer import PathDescriberForPrimitive
-from exactly_lib.type_system.err_msg import err_msg_resolver
-from exactly_lib.type_system.err_msg.err_msg_resolver import ErrorMessageResolver, ConstantErrorMessageResolver
+from exactly_lib.type_system.err_msg.err_msg_resolver import ErrorMessageResolver
 
 
 def itemized_list(items: Sequence[ErrorMessageResolver],
                   item_header_prefix: str = '* ',
                   separator: str = '\n') -> ErrorMessageResolver:
-    return ErrorMessageResolverList(items, item_header_prefix, separator)
+    return _ErrorMessageResolverList(items, item_header_prefix, separator)
 
 
 def section(header: str,
             contents: ErrorMessageResolver,
             separator: str = '\n\n') -> ErrorMessageResolver:
-    return ErrorMessageResolverSection(header, contents, separator)
+    return _ErrorMessageResolverSection(header, contents, separator)
 
 
 def sequence_of_parts(parts: Sequence[ErrorMessageResolver],
                       separator: str = '\n\n') -> ErrorMessageResolver:
-    return ErrorMessageResolverParts(parts, separator)
+    return _ErrorMessageResolverParts(parts, separator)
 
 
 def of_function(resolver: Callable[[], str]) -> ErrorMessageResolver:
-    return ErrorMessageResolverFromFunction(resolver)
+    return _ErrorMessageResolverFromFunction(resolver)
 
 
 def constant(msg: str) -> ErrorMessageResolver:
-    return ConstantErrorMessageResolver(msg)
+    return _ConstantErrorMessageResolver(msg)
 
 
 def text_doc(message: TextRenderer) -> ErrorMessageResolver:
-    return err_msg_resolver.OfTextDoc(message)
+    return _OfTextDoc(message)
 
 
 def of_path(path: PathDescriberForPrimitive) -> ErrorMessageResolver:
-    return ErrorMessageResolverOfPath(path)
+    return _ErrorMessageResolverOfPath(path)
 
 
-class ErrorMessageResolverList(ErrorMessageResolver):
+class _ErrorMessageResolverList(ErrorMessageResolver):
     """Simple list"""
 
     def __init__(self,
@@ -59,7 +59,7 @@ class ErrorMessageResolverList(ErrorMessageResolver):
         return self._separator.join(parts_output)
 
 
-class ErrorMessageResolverOfPath(ErrorMessageResolver):
+class _ErrorMessageResolverOfPath(ErrorMessageResolver):
     def __init__(self, path: PathDescriberForPrimitive):
         self._path = path
 
@@ -67,7 +67,7 @@ class ErrorMessageResolverOfPath(ErrorMessageResolver):
         return '\n'.join(path_rendering.path_strings(self._path))
 
 
-class ErrorMessageResolverParts(ErrorMessageResolver):
+class _ErrorMessageResolverParts(ErrorMessageResolver):
     """A sequence of resolvers separated by a constant string"""
 
     def __init__(self,
@@ -84,7 +84,7 @@ class ErrorMessageResolverParts(ErrorMessageResolver):
         return self._separator.join(parts_output)
 
 
-class ErrorMessageResolverSection(ErrorMessageResolver):
+class _ErrorMessageResolverSection(ErrorMessageResolver):
     """Simple section with a header and contents"""
 
     def __init__(self,
@@ -100,9 +100,25 @@ class ErrorMessageResolverSection(ErrorMessageResolver):
                                      self._contents.resolve()])
 
 
-class ErrorMessageResolverFromFunction(ErrorMessageResolver):
+class _ErrorMessageResolverFromFunction(ErrorMessageResolver):
     def __init__(self, resolver: Callable[[], str]):
         self._resolver = resolver
 
     def resolve(self) -> str:
         return self._resolver()
+
+
+class _ConstantErrorMessageResolver(ErrorMessageResolver):
+    def __init__(self, constant: str):
+        self._constant = constant
+
+    def resolve(self) -> str:
+        return self._constant
+
+
+class _OfTextDoc(ErrorMessageResolver):
+    def __init__(self, message: TextRenderer):
+        self._message = message
+
+    def resolve(self) -> str:
+        return print.print_to_str(self._message.render_sequence())
--- src/exactly_lib/type_system/err_msg/err_msg_resolver.py
+++ src/exactly_lib/type_system/err_msg/err_msg_resolver.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-from exactly_lib.common.report_rendering import print
 from exactly_lib.common.report_rendering import text_docs
 from exactly_lib.common.report_rendering.text_doc import TextRenderer
 
@@ -12,19 +11,3 @@ class ErrorMessageResolver(ABC):
 
     def resolve__tr(self) -> TextRenderer:
         return text_docs.single_pre_formatted_line_object(self.resolve())
-
-
-class ConstantErrorMessageResolver(ErrorMessageResolver):
-    def __init__(self, constant: str):
-        self._constant = constant
-
-    def resolve(self) -> str:
-        return self._constant
-
-
-class OfTextDoc(ErrorMessageResolver):
-    def __init__(self, message: TextRenderer):
-        self._message = message
-
-    def resolve(self) -> str:
-        return print.print_to_str(self._message.render_sequence())
