2728157788e91781664c5a9e4f96043e30953f67
Emil Karlén <emil.karlen@fripost.org>
2019-09-23 12:14:02 +0200

    files-matcher: REMOVE unused Environment

--- src/exactly_lib/symbol/logic/files_matcher.py
+++ src/exactly_lib/symbol/logic/files_matcher.py
@@ -3,7 +3,6 @@ from abc import ABC, abstractmethod
 from typing import Optional, Iterator
 
 from exactly_lib.symbol.logic.logic_value_resolver import LogicValueResolver
-from exactly_lib.symbol.path_resolving_environment import PathResolvingEnvironmentPreOrPostSds
 from exactly_lib.test_case.validation.pre_or_post_validation import PreOrPostSdsValidator
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.type_system.data.described_path import DescribedPathPrimitive
@@ -13,15 +12,6 @@ from exactly_lib.type_system.logic.file_matcher import FileMatcher
 from exactly_lib.type_system.value_type import LogicValueType, ValueType
 from exactly_lib.util.file_utils import TmpDirFileSpace
 from exactly_lib.util.symbol_table import SymbolTable
-
-
-class Environment:
-    def __init__(self,
-                 path_resolving_environment: PathResolvingEnvironmentPreOrPostSds,
-                 tmp_files_space: TmpDirFileSpace,
-                 ):
-        self.path_resolving_environment = path_resolving_environment
-        self.tmp_files_space = tmp_files_space
 
 
 class ErrorMessageInfo(ABC):
