eb5be64f35cbfa38bd10a92a1d99d0a65e4f70a0
Emil Karlén <emil.karlen@fripost.org>
2019-10-05 16:23:12 +0200

    err-msg-struct: description tree: REFACT Preparation for TreeDetail

--- src/exactly_lib/util/description_tree/tree.py
+++ src/exactly_lib/util/description_tree/tree.py
@@ -12,6 +12,37 @@ class Detail(ABC):
         pass
 
 
+NODE_DATA = TypeVar('NODE_DATA')
+
+
+class Node(Generic[NODE_DATA]):
+    def __init__(self,
+                 header: str,
+                 data: NODE_DATA,
+                 details: Sequence[Detail],
+                 children: Sequence['Node[NODE_DATA]']):
+        self._header = header
+        self._data = data
+        self._details = details
+        self._children = children
+
+    @property
+    def header(self) -> str:
+        return self._header
+
+    @property
+    def data(self) -> NODE_DATA:
+        return self._data
+
+    @property
+    def details(self) -> Sequence[Detail]:
+        return self._details
+
+    @property
+    def children(self) -> Sequence['Node[NODE_DATA]']:
+        return self._children
+
+
 class StringDetail(Detail):
     """A detail that is a string without any special structure."""
 
@@ -54,34 +85,3 @@ class DetailVisitor(Generic[RET], ABC):
     @abstractmethod
     def visit_pre_formatted_string(self, x: PreFormattedStringDetail) -> RET:
         pass
-
-
-NODE_DATA = TypeVar('NODE_DATA')
-
-
-class Node(Generic[NODE_DATA]):
-    def __init__(self,
-                 header: str,
-                 data: NODE_DATA,
-                 details: Sequence[Detail],
-                 children: Sequence['Node[NODE_DATA]']):
-        self._header = header
-        self._data = data
-        self._details = details
-        self._children = children
-
-    @property
-    def header(self) -> str:
-        return self._header
-
-    @property
-    def data(self) -> NODE_DATA:
-        return self._data
-
-    @property
-    def details(self) -> Sequence[Detail]:
-        return self._details
-
-    @property
-    def children(self) -> Sequence['Node[NODE_DATA]']:
-        return self._children
