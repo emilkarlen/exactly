2e872390704631e8587e33510adddc7dd06f2c62
Emil Karlén <emil.karlen@fripost.org>
2019-10-25 16:39:27 +0200

    string-transformer: tests: REMOVE unused code

--- test/exactly_lib_test/test_case_utils/string_transformers/test_resources_test/z_package_suite.py
+++ test/exactly_lib_test/test_case_utils/string_transformers/test_resources_test/z_package_suite.py
@@ -1,12 +1,11 @@
 import unittest
 
 from exactly_lib_test.test_case_utils.string_transformers.test_resources_test import \
-    resolver_assertions, test_transformers, value_assertions, integration_check
+    resolver_assertions, test_transformers, integration_check
 
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        value_assertions.suite(),
         resolver_assertions.suite(),
         test_transformers.suite(),
         integration_check.suite(),
--- test/exactly_lib_test/test_case_utils/string_transformers/z_package_suite.py
+++ test/exactly_lib_test/test_case_utils/string_transformers/z_package_suite.py
@@ -3,15 +3,13 @@ import unittest
 from exactly_lib_test.test_case_utils.string_transformers import env_vars_replacement, replace, \
     select_transformer, case_converters, sequence
 from exactly_lib_test.test_case_utils.string_transformers import \
-    parse_string_transformer, vistor
+    parse_string_transformer
 from exactly_lib_test.test_case_utils.string_transformers.test_resources_test import \
     z_package_suite as test_resources_test
 
 
 def suite() -> unittest.TestSuite:
     ret_val = unittest.TestSuite()
-    ret_val.addTest(vistor.suite())
-    # Test resources use the visitor, that is tested above
     ret_val.addTest(test_resources_test.suite())
     ret_val.addTest(replace.suite())
     ret_val.addTest(select_transformer.suite())
