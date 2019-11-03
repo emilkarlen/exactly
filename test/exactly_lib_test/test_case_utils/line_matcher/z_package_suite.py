a77c118d3050b46aef7a70328219399d007a9909
Emil Karlén <emil.karlen@fripost.org>
2019-11-03 13:12:15 +0100

    line-matcher: REMOVE unused code

--- test/exactly_lib_test/test_case_utils/line_matcher/z_package_suite.py
+++ test/exactly_lib_test/test_case_utils/line_matcher/z_package_suite.py
@@ -1,6 +1,6 @@
 import unittest
 
-from exactly_lib_test.test_case_utils.line_matcher import constant, line_number, regex, combinators, visitor
+from exactly_lib_test.test_case_utils.line_matcher import constant, line_number, regex, combinators
 from exactly_lib_test.test_case_utils.line_matcher import model_construction
 from exactly_lib_test.test_case_utils.line_matcher import parse_line_matcher
 from exactly_lib_test.test_case_utils.line_matcher.test_resources_test import z_package_suite as test_resources_test
@@ -8,8 +8,6 @@ from exactly_lib_test.test_case_utils.line_matcher.test_resources_test import z_
 
 def suite() -> unittest.TestSuite:
     return unittest.TestSuite([
-        visitor.suite(),
-        # Visitor (above) is used by the test resources
         test_resources_test.suite(),
         model_construction.suite(),
         constant.suite(),
