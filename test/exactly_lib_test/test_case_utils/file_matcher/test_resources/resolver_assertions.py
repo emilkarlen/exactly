90e982a4a57c31492b047b51aa901ad980d14e53
Emil Karlén <emil.karlen@fripost.org>
2019-11-03 21:45:45 +0100

    file-matcher: REMOVE unused code

--- test/exactly_lib_test/test_case_utils/file_matcher/test_resources/resolver_assertions.py
+++ test/exactly_lib_test/test_case_utils/file_matcher/test_resources/resolver_assertions.py
@@ -2,22 +2,11 @@ from typing import Sequence
 
 from exactly_lib.symbol.resolver_structure import SymbolValueResolver
 from exactly_lib.symbol.symbol_usage import SymbolReference
-from exactly_lib.type_system.logic.file_matcher import FileMatcher, FileMatcherValue
+from exactly_lib.type_system.logic.file_matcher import FileMatcherValue
 from exactly_lib.util import symbol_table
 from exactly_lib_test.symbol.test_resources import resolver_assertions
-from exactly_lib_test.test_case_utils.file_matcher.test_resources.value_assertions import value_equals_file_matcher
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion
-
-
-def resolved_value_equals_file_matcher(expected: FileMatcher,
-                                       references: ValueAssertion[Sequence[SymbolReference]] = asrt.is_empty_sequence,
-                                       symbols: symbol_table.SymbolTable = None
-                                       ) -> ValueAssertion[SymbolValueResolver]:
-    return resolver_assertions.matches_resolver_of_file_matcher(
-        references,
-        value_equals_file_matcher(expected),
-        symbols=symbols)
 
 
 def resolved_value_matches_file_matcher(value: ValueAssertion[FileMatcherValue],
--- test/exactly_lib_test/test_case_utils/file_matcher/test_resources/value_assertions.py
+++ test/exactly_lib_test/test_case_utils/file_matcher/test_resources/value_assertions.py
@@ -1,31 +1,14 @@
-import unittest
 from typing import Set
 
 from exactly_lib.test_case.validation.pre_or_post_value_validation import PreOrPostSdsValueValidator
 from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
 from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
-from exactly_lib.test_case_utils.file_matcher import file_matchers
-from exactly_lib.test_case_utils.file_matcher.impl import combinators
 from exactly_lib.test_case_utils.file_matcher.impl import name_glob_pattern, file_type, name_regex
-from exactly_lib.test_case_utils.file_matcher.impl.file_type import FileMatcherType
-from exactly_lib.test_case_utils.file_matcher.impl.name_glob_pattern import FileMatcherNameGlobPattern
-from exactly_lib.test_case_utils.file_matcher.impl.name_regex import FileMatcherBaseNameRegExPattern
 from exactly_lib.test_case_utils.file_properties import FileType
 from exactly_lib.type_system.logic.file_matcher import FileMatcher, FileMatcherValue
 from exactly_lib_test.test_case_file_structure.test_resources.paths import fake_home_and_sds
-from exactly_lib_test.test_case_utils.file_matcher.test_resources.visitor import FileMatcherStructureVisitor
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
-from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion, ValueAssertionBase
-
-
-def equals_file_matcher(expected: FileMatcher,
-                        description: str = '') -> ValueAssertion[FileMatcher]:
-    return _EqualsAssertion(expected, description)
-
-
-def value_equals_file_matcher(expected: FileMatcher,
-                              description: str = '') -> ValueAssertion[FileMatcherValue]:
-    return _EqualsAssertionValue(expected, description)
+from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion
 
 
 def is_type_matcher(expected: FileType) -> ValueAssertion[FileMatcher]:
@@ -122,121 +105,3 @@ def matches_file_matcher_value(
             ),
         ]
     )
-
-
-class _EqualsAssertion(ValueAssertionBase[FileMatcher]):
-    def __init__(self,
-                 expected: FileMatcher,
-                 description: str):
-        self.expected = expected
-        self.description = description
-
-    def _apply(self,
-               put: unittest.TestCase,
-               value,
-               message_builder: asrt.MessageBuilder):
-        assert_is_file_selector_type = asrt.is_instance(FileMatcher, self.description)
-        assert_is_file_selector_type.apply_with_message(put, value,
-                                                        'Value must be a ' + str(FileMatcher))
-        checker = _StructureChecker(put,
-                                    message_builder,
-                                    self.expected,
-                                    self.description
-                                    )
-        checker.visit(value)
-
-
-class _EqualsAssertionValue(ValueAssertionBase[FileMatcherValue]):
-    TCDS = fake_home_and_sds()
-
-    def __init__(self,
-                 expected: FileMatcher,
-                 description: str):
-        self.expected = expected
-        self.description = description
-
-    def _apply(self,
-               put: unittest.TestCase,
-               actual,
-               message_builder: asrt.MessageBuilder):
-        assert_is_file_selector_type = asrt.is_instance(FileMatcherValue, self.description)
-        assert_is_file_selector_type.apply_with_message(put, actual,
-                                                        'Value must be a ' + str(FileMatcherValue))
-        assert isinstance(actual, FileMatcherValue)
-
-        assertion_on_primitive_value = equals_file_matcher(self.expected, self.description)
-        assertion_on_primitive_value.apply(put,
-                                           actual.value_of_any_dependency(self.TCDS),
-                                           message_builder)
-
-
-class _StructureChecker(FileMatcherStructureVisitor):
-    def __init__(self,
-                 put: unittest.TestCase,
-                 message_builder: asrt.MessageBuilder,
-                 expected: FileMatcher,
-                 description: str):
-        self.put = put
-        self.message_builder = message_builder
-        self.expected = expected
-        self.description = description
-
-    def _common(self, actual: file_matchers.FileMatcher):
-        self.put.assertIsInstance(actual,
-                                  type(self.expected),
-                                  'class')
-        self.put.assertEqual(actual.option_description,
-                             self.expected.option_description,
-                             'option_description')
-
-    def visit_name_glob_pattern(self, actual: FileMatcherNameGlobPattern):
-        self._common(actual)
-        assert isinstance(self.expected, FileMatcherNameGlobPattern)  # Type info for IDE
-        self.put.assertEqual(self.expected.glob_pattern,
-                             actual.glob_pattern,
-                             'glob_pattern')
-
-    def visit_name_reg_ex_pattern(self, actual: FileMatcherBaseNameRegExPattern):
-        self._common(actual)
-        assert isinstance(self.expected, FileMatcherBaseNameRegExPattern)  # Type info for IDE
-        self.put.assertEqual(self.expected.reg_ex_pattern,
-                             actual.reg_ex_pattern,
-                             'reg_ex_pattern')
-
-    def visit_constant(self, actual: file_matchers.FileMatcherConstant):
-        self._common(actual)
-        assert isinstance(self.expected, file_matchers.FileMatcherConstant)  # Type info for IDE
-        self.put.assertEqual(self.expected.result_constant,
-                             actual.result_constant,
-                             'result_constant')
-
-    def visit_type(self, actual: FileMatcherType):
-        self._common(actual)
-        assert isinstance(self.expected, FileMatcherType)  # Type info for IDE
-        self.put.assertEqual(self.expected.file_type,
-                             actual.file_type,
-                             'file_type')
-
-    def visit_not(self, actual: combinators.FileMatcherNot):
-        self._common(actual)
-        assert isinstance(self.expected,
-                          combinators.FileMatcherNot)  # Type info for IDE
-        assertion_on_negated_matchers = equals_file_matcher(self.expected.negation)
-        assertion_on_negated_matchers.apply_with_message(self.put, actual.negation,
-                                                         'negated matcher')
-
-    def visit_and(self, actual: combinators.FileMatcherAnd):
-        self._common(actual)
-        assert isinstance(self.expected,
-                          combinators.FileMatcherAnd)  # Type info for IDE
-        assertion_on_sub_matchers = asrt.matches_sequence(list(map(equals_file_matcher, self.expected.matchers)))
-        assertion_on_sub_matchers.apply_with_message(self.put, actual.matchers,
-                                                     'sub matchers')
-
-    def visit_or(self, actual: combinators.FileMatcherOr):
-        self._common(actual)
-        assert isinstance(self.expected,
-                          combinators.FileMatcherOr)  # Type info for IDE
-        assertion_on_sub_matchers = asrt.matches_sequence(list(map(equals_file_matcher, self.expected.matchers)))
-        assertion_on_sub_matchers.apply_with_message(self.put, actual.matchers,
-                                                     'sub matchers')
