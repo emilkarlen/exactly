35bc0368788aba8230ccf2a0223ac911d65a26af
Emil Karlén <emil.karlen@fripost.org>
2019-11-05 15:55:32 +0100

    line-matcher: ADD delegation classes

--- src/exactly_lib/test_case_utils/line_matcher/impl/delegated.py
+++ src/exactly_lib/test_case_utils/line_matcher/impl/delegated.py
@@ -1,10 +1,19 @@
-from typing import Optional
+from typing import Optional, Set, List
 
+from exactly_lib.symbol.logic.line_matcher import LineMatcherResolver
+from exactly_lib.symbol.symbol_usage import SymbolReference
+from exactly_lib.test_case.validation import pre_or_post_validation
+from exactly_lib.test_case.validation.pre_or_post_validation import PreOrPostSdsValidator
+from exactly_lib.test_case.validation.pre_or_post_value_validation import PreOrPostSdsValueValidator
+from exactly_lib.test_case_file_structure.home_and_sds import HomeAndSds
+from exactly_lib.test_case_file_structure.path_relativity import DirectoryStructurePartition
+from exactly_lib.test_case_utils.matcher.resolver import MatcherResolver
 from exactly_lib.type_system.description.tree_structured import StructureRenderer
 from exactly_lib.type_system.err_msg.err_msg_resolver import ErrorMessageResolver
 from exactly_lib.type_system.logic.impls import combinator_matchers
-from exactly_lib.type_system.logic.line_matcher import LineMatcher, LineMatcherLine
-from exactly_lib.type_system.logic.matcher_base_class import MatcherWTraceAndNegation, MatchingResult
+from exactly_lib.type_system.logic.line_matcher import LineMatcher, LineMatcherLine, LineMatcherValue
+from exactly_lib.type_system.logic.matcher_base_class import MatchingResult, MatcherWTraceAndNegation, MatcherValue
+from exactly_lib.util.symbol_table import SymbolTable
 
 
 class LineMatcherDelegatedToMatcherWTrace(LineMatcher):
@@ -37,3 +46,44 @@ class LineMatcherDelegatedToMatcherWTrace(LineMatcher):
 
     def matches_w_trace(self, model: LineMatcherLine) -> MatchingResult:
         return self._delegated.matches_w_trace(model)
+
+
+class LineMatcherValueDelegatedToMatcher(LineMatcherValue):
+    def __init__(self, delegated: MatcherValue[LineMatcherLine]):
+        super().__init__()
+        self._delegated = delegated
+
+    def resolving_dependencies(self) -> Set[DirectoryStructurePartition]:
+        return set(DirectoryStructurePartition)
+
+    def structure(self) -> StructureRenderer:
+        return self._delegated.structure()
+
+    def value_when_no_dir_dependencies(self) -> LineMatcher:
+        """
+        :raises DirDependencyError: This value has dir dependencies.
+        """
+        raise ValueError(str(type(self)) + ' do not support this short cut.')
+
+    def value_of_any_dependency(self, home_and_sds: HomeAndSds) -> LineMatcher:
+        return LineMatcherDelegatedToMatcherWTrace(self._delegated.value_of_any_dependency(home_and_sds))
+
+
+class LineMatcherResolverDelegatedToMatcher(LineMatcherResolver):
+    def __init__(self, delegated: MatcherResolver[LineMatcherLine]):
+        super().__init__()
+        self._delegated = delegated
+
+    @property
+    def references(self) -> List[SymbolReference]:
+        return list(self._delegated.references)
+
+    @property
+    def validator(self) -> PreOrPostSdsValidator:
+        return pre_or_post_validation.PreOrPostSdsValidatorFromValueValidator(self._value_validator)
+
+    def resolve(self, symbols: SymbolTable) -> LineMatcherValue:
+        return LineMatcherValueDelegatedToMatcher(self._delegated.resolve(symbols))
+
+    def _value_validator(self, symbols: SymbolTable) -> PreOrPostSdsValueValidator:
+        return self._delegated.resolve(symbols).validator
