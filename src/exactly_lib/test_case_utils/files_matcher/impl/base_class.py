63193d6ed8698a3f1185ab9791ab9a1d2cb58c85
Emil Karlén <emil.karlen@fripost.org>
2019-12-25 13:31:51 +0100

    err-msg-struct: files-matcher: REMOVE unused default structure rendering

--- src/exactly_lib/test_case_utils/files_matcher/impl/base_class.py
+++ src/exactly_lib/test_case_utils/files_matcher/impl/base_class.py
@@ -1,10 +1,8 @@
 from abc import ABC
 
 from exactly_lib.test_case_utils.matcher.impls.impl_base_class import MatcherImplBase
-from exactly_lib.type_system.description.tree_structured import StructureRenderer
 from exactly_lib.type_system.logic.files_matcher import FilesMatcherModel
 from exactly_lib.type_system.logic.matcher_base_class import MatcherDdv, MatcherAdv
-from exactly_lib.util.description_tree import renderers
 
 
 class FilesMatcherImplBase(MatcherImplBase[FilesMatcherModel], ABC):
@@ -12,18 +10,7 @@ class FilesMatcherImplBase(MatcherImplBase[FilesMatcherModel], ABC):
 
 
 class FilesMatcherDdvImplBase(MatcherDdv[FilesMatcherModel], ABC):
-    def structure(self) -> StructureRenderer:
-        """
-        The structure of the object, that can be used in traced.
-
-        The returned tree is constant.
-        """
-        return renderers.NodeRendererFromParts(
-            'TODO files-matcher name',
-            None,
-            (),
-            (),
-        )
+    pass
 
 
 class FilesMatcherAdvImplBase(MatcherAdv[FilesMatcherModel], ABC):
