1958b7ee3ee26b3c38a6ff815113028d29e37513
Emil Karlén <emil.karlen@fripost.org>
2020-01-25 19:18:33 +0100

    dir-contents: Structure and trace

--- src/exactly_lib/test_case_utils/file_matcher/impl/dir_contents.py
+++ src/exactly_lib/test_case_utils/file_matcher/impl/dir_contents.py
@@ -1,3 +1,4 @@
+from exactly_lib.definitions import instruction_arguments
 from exactly_lib.definitions.entity import syntax_elements
 from exactly_lib.definitions.test_case import file_check_properties
 from exactly_lib.test_case_utils import file_properties
@@ -5,6 +6,7 @@ from exactly_lib.test_case_utils.file_matcher.impl import file_contents_utils
 from exactly_lib.test_case_utils.files_matcher import models
 from exactly_lib.type_system.logic.file_matcher import FileMatcherModel, GenericFileMatcherSdv
 from exactly_lib.type_system.logic.files_matcher import FilesMatcherModel, GenericFilesMatcherSdv
+from exactly_lib.util.cli_syntax import option_syntax
 
 
 class _SetupNonRecursive(file_contents_utils.Setup[FilesMatcherModel]):
@@ -25,7 +27,8 @@ SETUP__NON_RECURSIVE = _SetupNonRecursive()
 class _SetupRecursive(file_contents_utils.Setup[FilesMatcherModel]):
     def __init__(self):
         super().__init__(
-            file_check_properties.DIR_CONTENTS,
+            ' '.join((file_check_properties.DIR_CONTENTS,
+                      option_syntax.option_syntax(instruction_arguments.RECURSIVE_OPTION.name))),
             file_properties.FileType.DIRECTORY,
             syntax_elements.FILES_MATCHER_SYNTAX_ELEMENT,
         )
