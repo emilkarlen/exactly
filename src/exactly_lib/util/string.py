4f91889c5f81f94b7b16c4913153b76fa15daab2
Emil Karlén <emil.karlen@fripost.org>
2020-02-02 09:17:27 +0100

    string formatting: Improves test resources

--- src/exactly_lib/util/string.py
+++ src/exactly_lib/util/string.py
@@ -1,6 +1,8 @@
 import os
 from typing import Sequence, Any, Optional, Mapping
 
+from exactly_lib.util.name_and_value import NameAndValue
+
 
 def lines_content(lines: Sequence[str]) -> str:
     return '' \
@@ -28,18 +30,39 @@ class StringFormatter:
     def __init__(self, format_map: Optional[Mapping[str, Any]] = None):
         self._format_map = {} if format_map is None else format_map
 
-    def with_additional(self, format_map: Optional[Mapping[str, Any]]) -> 'StringFormatter':
+    @staticmethod
+    def of_name_and_values(elements: Sequence[NameAndValue[Any]]) -> 'StringFormatter':
+        return StringFormatter({
+            element.name: element.value
+            for element in elements
+        })
+
+    def with_additional(self,
+                        format_map: Optional[Mapping[str, Any]],
+                        **keyword_mappings) -> 'StringFormatter':
         d = dict(self._format_map)
         d.update(format_map)
-        return StringFormatter(d)
+        return StringFormatter(self._get_format_map(format_map, **keyword_mappings))
 
-    def format(self, template: str, extra: Optional[Mapping[str, Any]] = None) -> str:
+    def format(self,
+               template: str,
+               extra: Optional[Mapping[str, Any]] = None,
+               **keyword_mappings) -> str:
         """
         Formats the given string using the format map given in the constructor.
         """
-        if extra is None:
-            d = self._format_map
-        else:
-            d = dict(self._format_map)
-            d.update(extra)
-        return template.format_map(d)
+        return template.format_map(self._get_format_map(extra, **keyword_mappings))
+
+    def _get_format_map(self,
+                        extra: Optional[Mapping[str, Any]] = None,
+                        **keyword_mappings) -> Mapping[str, Any]:
+        if extra is None and not keyword_mappings:
+            return self._format_map
+
+        ret_val = dict(self._format_map)
+        if extra is not None:
+            ret_val.update(extra)
+        if keyword_mappings:
+            ret_val.update(keyword_mappings)
+
+        return ret_val
--- test/exactly_lib_test/util/test_resources/quoting.py
+++ test/exactly_lib_test/util/test_resources/quoting.py
@@ -2,25 +2,34 @@ from exactly_lib.util.parse.token import SOFT_QUOTE_CHAR, HARD_QUOTE_CHAR
 
 
 class Surrounded:
-    def __init__(self, delimiter: str, surrounded: str):
+    def __init__(self,
+                 before: str,
+                 after: str,
+                 surrounded: str,
+                 ):
+        self.after = after
+        self.before = before
         self.surrounded = surrounded
-        self.delimiter = delimiter
+
+    @staticmethod
+    def with_identical(before_and_after: str, surrounded: str) -> 'Surrounded':
+        return Surrounded(before_and_after, before_and_after, surrounded)
 
     def __str__(self):
-        return self.delimiter + self.surrounded + self.delimiter
+        return self.before + self.surrounded + self.after
 
 
 def surrounded_by_soft_quotes(surrounded: str) -> Surrounded:
-    return Surrounded(SOFT_QUOTE_CHAR, surrounded)
+    return Surrounded.with_identical(SOFT_QUOTE_CHAR, surrounded)
 
 
 def surrounded_by_soft_quotes_str(surrounded: str) -> str:
-    return str(Surrounded(SOFT_QUOTE_CHAR, surrounded))
+    return str(Surrounded.with_identical(SOFT_QUOTE_CHAR, surrounded))
 
 
 def surrounded_by_hard_quotes(surrounded: str) -> Surrounded:
-    return Surrounded(HARD_QUOTE_CHAR, surrounded)
+    return Surrounded.with_identical(HARD_QUOTE_CHAR, surrounded)
 
 
 def surrounded_by_hard_quotes_str(surrounded: str) -> str:
-    return str(Surrounded(HARD_QUOTE_CHAR, surrounded))
+    return str(Surrounded.with_identical(HARD_QUOTE_CHAR, surrounded))
