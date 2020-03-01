85e9eca17529ede692cec6a39b3964aba32e9428
Emil Karlén <emil.karlen@fripost.org>
2020-03-01 22:58:25 +0100

    files-matcher/contains,equals: test/specific cases for contains

--- test/exactly_lib_test/test_case_utils/files_matcher/files_condition_containment/z_package_suite.py
+++ test/exactly_lib_test/test_case_utils/files_matcher/files_condition_containment/z_package_suite.py
@@ -1,7 +1,8 @@
 import unittest
 
 from exactly_lib_test.test_case_utils.files_matcher.files_condition_containment import invalid_syntax, \
-    validation_failure, symbol_references, common_execution_outcome, specific_for_equals
+    validation_failure, symbol_references, common_execution_outcome, \
+    specific_for_equals, specific_for_contains
 
 
 def suite() -> unittest.TestSuite:
@@ -11,6 +12,7 @@ def suite() -> unittest.TestSuite:
         validation_failure.suite(),
         common_execution_outcome.suite(),
         specific_for_equals.suite(),
+        specific_for_contains.suite(),
     ])
 
 
