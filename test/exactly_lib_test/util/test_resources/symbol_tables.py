bb89da8bd9aaeda8ab72b991e987e9c219b081e6
Emil Karlén <emil.karlen@fripost.org>
2020-05-02 20:20:34 +0200

    logic types: Introduces Symbol Table Value: Removes unused test resources

--- test/exactly_lib_test/util/test_resources/symbol_tables.py
+++
@@ -1,9 +0,0 @@
-from typing import Iterable
-
-from exactly_lib.util.symbol_table import SymbolTable, Entry
-
-
-def symbol_table_from_entries(entries: Iterable[Entry]) -> SymbolTable:
-    elements = [(entry.key, entry.value)
-                for entry in entries]
-    return SymbolTable(dict(elements))
