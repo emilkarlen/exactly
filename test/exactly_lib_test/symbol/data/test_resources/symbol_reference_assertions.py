d737f33a1ac9efcd2c981215d26a68ee5b12cfa6
Emil Karlén <emil.karlen@fripost.org>
2020-05-29 18:39:01 +0200

    sym-ref-tests: REFACT do not use test of equality: is_reference_to_string_made_up_of_just_strings

--- test/exactly_lib_test/symbol/data/test_resources/symbol_reference_assertions.py
+++ test/exactly_lib_test/symbol/data/test_resources/symbol_reference_assertions.py
@@ -3,11 +3,10 @@ from typing import Sequence
 from exactly_lib.symbol.data.value_restriction import ValueRestriction
 from exactly_lib.symbol.sdv_structure import SymbolUsage, SymbolReference
 from exactly_lib.type_system.value_type import TypeCategory
+from exactly_lib_test.symbol.data.restrictions.test_resources import concrete_restriction_assertion
 from exactly_lib_test.symbol.data.restrictions.test_resources.concrete_restriction_assertion import \
     matches_restrictions_on_direct_and_indirect, equals_data_type_reference_restrictions, \
     is_any_data_type_reference_restrictions
-from exactly_lib_test.symbol.data.restrictions.test_resources.concrete_restrictions import \
-    string_made_up_of_just_strings_reference_restrictions
 from exactly_lib_test.symbol.test_resources import symbol_reference_assertions as asrt_sym_ref
 from exactly_lib_test.symbol.test_resources.restrictions_assertions import is_type_category_restriction
 from exactly_lib_test.symbol.test_resources.symbol_reference_assertions import matches_reference_2
@@ -57,6 +56,7 @@ def is_reference_to_data_type_symbol(symbol_name: str
 
 
 def is_reference_to_string_made_up_of_just_strings(symbol_name: str) -> ValueAssertion[SymbolReference]:
-    return equals_symbol_reference(
-        SymbolReference(symbol_name,
-                        string_made_up_of_just_strings_reference_restrictions()))
+    return matches_reference_2(
+        symbol_name,
+        concrete_restriction_assertion.is_string_made_up_of_just_strings_reference_restrictions()
+    )
