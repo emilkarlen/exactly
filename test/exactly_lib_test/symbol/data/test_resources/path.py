9a1b877b7b6e750cfe6392d0760a11bd73045b20
Emil Karlén <emil.karlen@fripost.org>
2020-05-30 21:21:45 +0200

    PathSymbolValueContext: Implements of_reference

--- test/exactly_lib_test/symbol/data/test_resources/path.py
+++ test/exactly_lib_test/symbol/data/test_resources/path.py
@@ -2,7 +2,9 @@ import pathlib
 from typing import Optional
 
 from exactly_lib.section_document.source_location import SourceLocationInfo
+from exactly_lib.symbol.data import path_part_sdvs
 from exactly_lib.symbol.data.path_sdv import PathSdv
+from exactly_lib.symbol.data.path_sdv_impls import path_with_symbol
 from exactly_lib.symbol.data.path_sdv_impls.constant import PathConstantSdv
 from exactly_lib.symbol.data.restrictions.reference_restrictions import ReferenceRestrictionsOnDirectAndIndirect, \
     OrReferenceRestrictions, OrRestrictionPart
@@ -99,9 +101,18 @@ class PathSymbolValueContext(DataSymbolValueContext[PathSdv]):
 
     @staticmethod
     def of_reference(referenced_symbol_name: str,
+                     accepted_relativities: PathRelativityVariants,
                      definition_source: Optional[SourceLocationInfo] = ARBITRARY_LINE_SEQUENCE_FOR_DEFINITION,
                      ) -> 'PathSymbolValueContext':
-        return PathSymbolValueContext()
+        return PathSymbolValueContext(
+            path_with_symbol.PathSdvRelSymbol(
+                path_part_sdvs.empty(),
+                SymbolReference(referenced_symbol_name,
+                                path_reference_restrictions(accepted_relativities))
+            ),
+            accepted_relativities,
+            definition_source,
+        )
 
     @staticmethod
     def of_arbitrary_value() -> 'PathSymbolValueContext':
