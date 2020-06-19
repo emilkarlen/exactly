15abb0908ee62b424776023d08b4d95c4ff65e0c
Emil Karlén <emil.karlen@fripost.org>
2020-06-19 09:10:25 +0200

    files-condition: REMOVE unused test resource (for integration check)

--- test/exactly_lib_test/test_case_utils/files_condition/test_resources/properties_checker.py
+++ test/exactly_lib_test/test_case_utils/files_condition/test_resources/properties_checker.py
@@ -1,30 +1,25 @@
 import unittest
-from pathlib import PurePosixPath
-from typing import Optional
 
 from exactly_lib.symbol.logic.logic_type_sdv import LogicSdv
 from exactly_lib.symbol.logic.resolving_environment import FullResolvingEnvironment
 from exactly_lib.test_case_utils.files_condition.structure import FilesCondition, FilesConditionSdv, \
     FilesConditionDdv
-from exactly_lib.type_system.logic.file_matcher import FileMatcher
-from exactly_lib.type_system.logic.matcher_base_class import MatchingResult
 from exactly_lib_test.test_case_utils.logic.test_resources.common_properties_checker import \
     CommonPropertiesConfiguration, Applier, CommonSdvPropertiesChecker
 from exactly_lib_test.test_case_utils.logic.test_resources.logic_type_checker import \
     WithDetailsDescriptionExecutionPropertiesChecker
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.test_resources.value_assertions.value_assertion import MessageBuilder
-from exactly_lib_test.type_system.logic.test_resources.file_matcher import FileMatcherModelThatMustNotBeAccessed
 
 
 class FilesConditionPropertiesConfiguration(
     CommonPropertiesConfiguration[FilesCondition,
-                                  Optional[PurePosixPath],
-                                  Optional[MatchingResult]]):
+                                  None,
+                                  None]):
     def __init__(self):
-        self._applier = _Applier()
+        self._applier = _UnusedApplier()
 
-    def applier(self) -> Applier[FilesCondition, Optional[PurePosixPath], Optional[MatchingResult]]:
+    def applier(self) -> Applier[FilesCondition, None, None]:
         return self._applier
 
     def new_sdv_checker(self) -> CommonSdvPropertiesChecker[FilesCondition]:
@@ -47,44 +42,11 @@ class _SdvPropertiesChecker(CommonSdvPropertiesChecker[FilesCondition]):
         )
 
 
-class _Applier(Applier[FilesCondition, Optional[PurePosixPath], Optional[MatchingResult]]):
-    """
-    Test fails if the files-condition does not contain the tested path,
-    or if it has no associated file-matcher.
-
-    A dummy file-matcher model is used.
-    """
-
+class _UnusedApplier(Applier[FilesCondition, None, None]):
     def apply(self,
               put: unittest.TestCase,
               message_builder: MessageBuilder,
               primitive: FilesCondition,
               resolving_environment: FullResolvingEnvironment,
-              input_: Optional[PurePosixPath]) -> Optional[MatchingResult]:
-        if input_ is None:
-            return None
-
-        file_matcher = self._associated_file_matcher(put, message_builder, input_, primitive)
-        return file_matcher.matches_w_trace(FileMatcherModelThatMustNotBeAccessed())
-
-    @staticmethod
-    def _associated_file_matcher(put,
-                                 message_builder,
-                                 file: PurePosixPath,
-                                 primitive: FilesCondition,
-                                 ) -> FileMatcher:
-        files = primitive.files
-        if file not in files:
-            put.fail(
-                message_builder.apply(
-                    'Test impl error: the files-condition does not contain path {}'.format(file)
-                )
-            )
-        mb_fm = files[file]
-        if mb_fm is None:
-            put.fail(
-                message_builder.apply(
-                    'Test impl error: the files-condition does not have an associated files matcher: {}'.format(file)
-                )
-            )
-        return mb_fm
+              input_: None) -> None:
+        return None
