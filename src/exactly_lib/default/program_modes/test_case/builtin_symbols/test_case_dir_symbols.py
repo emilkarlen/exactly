f8f7e51a55660de411297aecddedc6e261ad1c18
Emil Karlén <emil.karlen@fripost.org>
2020-06-28 17:22:33 +0200

    help/builtin paths: Improves help text

--- src/exactly_lib/default/program_modes/test_case/builtin_symbols/test_case_dir_symbols.py
+++ src/exactly_lib/default/program_modes/test_case/builtin_symbols/test_case_dir_symbols.py
@@ -1,11 +1,13 @@
 from exactly_lib.cli.main_program import BuiltinSymbol
+from exactly_lib.definitions import formatting
 from exactly_lib.definitions.tcds_symbols import SYMBOL_DESCRIPTION
 from exactly_lib.symbol import sdv_structure
 from exactly_lib.symbol.data import path_sdvs
+from exactly_lib.test_case_file_structure import relative_path_options
 from exactly_lib.test_case_file_structure import tcds_symbols
 from exactly_lib.test_case_file_structure.path_relativity import RelOptionType
 from exactly_lib.type_system.value_type import ValueType
-from exactly_lib.util.textformat.structure.document import SectionContents
+from exactly_lib.util.cli_syntax import option_syntax
 from exactly_lib.util.textformat.textformat_parser import TextParser
 
 
@@ -13,16 +15,24 @@ def __sdv_of(rel_option_type: RelOptionType) -> sdv_structure.SymbolDependentVal
     return path_sdvs.of_rel_option(rel_option_type)
 
 
-_TEXT_PARSER = TextParser()
-
-
 def _builtin(symbol_name: str, relativity: RelOptionType) -> BuiltinSymbol:
+    tp = TextParser({
+        'relativity_option':
+            formatting.cli_argument_option_string(
+                option_syntax.option_syntax(relative_path_options.REL_OPTIONS_MAP[relativity].option_name)
+            )
+    })
+
     return BuiltinSymbol(symbol_name,
                          ValueType.PATH,
                          __sdv_of(relativity),
                          SYMBOL_DESCRIPTION.as_single_line_description_str(symbol_name),
-                         SectionContents([]))
+                         tp.section_contents(_DESCRIPTION))
 
+
+_DESCRIPTION = """\
+This is the root directory of paths specified with {relativity_option}. 
+"""
 
 SYMBOL_HDS_CASE = _builtin(tcds_symbols.SYMBOL_HDS_CASE,
                            RelOptionType.REL_HDS_CASE)
