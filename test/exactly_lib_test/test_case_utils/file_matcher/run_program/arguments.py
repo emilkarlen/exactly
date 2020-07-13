db7ad731ba231216dfe8668bb9f56c42af061c02
Emil Karlén <emil.karlen@fripost.org>
2020-07-13 16:46:09 +0200

    file-matcher/run: ADD test of parse source consumption

--- test/exactly_lib_test/test_case_utils/file_matcher/run_program/arguments.py
+++ test/exactly_lib_test/test_case_utils/file_matcher/run_program/arguments.py
@@ -2,12 +2,10 @@ import unittest
 from typing import List
 
 from exactly_lib.symbol.symbol_syntax import symbol_reference_syntax_for_name
-from exactly_lib_test.section_document.test_resources import parse_source_assertions as asrt_source
 from exactly_lib_test.symbol.data.test_resources.symbol_reference_assertions import is_reference_to_data_type_symbol
 from exactly_lib_test.symbol.test_resources.program import is_reference_to_program
 from exactly_lib_test.test_case_utils.file_matcher.test_resources import argument_building as args
 from exactly_lib_test.test_case_utils.file_matcher.test_resources import integration_check
-from exactly_lib_test.test_case_utils.logic.test_resources.integration_check import ParseExpectation
 from exactly_lib_test.test_case_utils.matcher.test_resources.run_program import test_cases
 from exactly_lib_test.test_case_utils.program.test_resources import arguments_building as program_args
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
@@ -36,7 +34,7 @@ class TestPathOfModelFileShouldBeGivenAsLastArgument(unittest.TestCase):
 
         # ACT && ASSERT #
 
-        integration_check.CHECKER.check_multi(
+        integration_check.CHECKER.check_multi__w_source_variants_for_full_line_parser(
             self,
             args.RunProgram(
                 program_args.symbol_ref_command_elements(
@@ -44,15 +42,12 @@ class TestPathOfModelFileShouldBeGivenAsLastArgument(unittest.TestCase):
                     arguments=[symbol_reference_syntax_for_name(command_line_arg_list_symbol_name)],
                 )
             ).as_arguments,
-            ParseExpectation(
-                source=asrt_source.source_is_at_end,
-                symbol_references=asrt.matches_sequence([
-                    is_reference_to_program(program_symbol_name),
-                    is_reference_to_data_type_symbol(command_line_arg_list_symbol_name),
-                ]),
-            ),
             integration_check.constant_relative_file_name(model_file_path),
-            [
+            symbol_references=asrt.matches_sequence([
+                is_reference_to_program(program_symbol_name),
+                is_reference_to_data_type_symbol(command_line_arg_list_symbol_name),
+            ]),
+            execution=[
                 test_cases.argument_list_exe_case(
                     command_line_arguments,
                     expected_arguments_to_program(command_line_arguments,
