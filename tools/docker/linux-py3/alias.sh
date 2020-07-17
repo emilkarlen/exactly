edbf3707debd946b78a8f127281d8b4b08a70ef1
Emil Karlén <emil.karlen@fripost.org>
2020-07-18 01:17:11 +0200

    tools: IMPROVE Docker image for test of Exactly

---
+++ tools/docker/linux-py3/alias.sh
@@ -0,0 +1,11 @@
+alias xly='python /app/exactly/src/default-main-program-runner.py'
+
+alias cde='cd /app/exactly'
+
+alias ..='cd ..'
+
+alias l='ls'
+alias 1='ls -1'
+alias la='ls -a'
+alias ll='ls -lh'
+alias lla='ls -lah'
