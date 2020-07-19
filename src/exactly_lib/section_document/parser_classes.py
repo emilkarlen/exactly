6c45b932cd48187e61efa4fc4f9914300daf2653
Emil Karlén <emil.karlen@fripost.org>
2020-07-19 15:00:32 +0200

    parsing: REMOVE unused code (ParserFromSimpleParser)

--- src/exactly_lib/section_document/parser_classes.py
+++ src/exactly_lib/section_document/parser_classes.py
@@ -49,19 +49,6 @@ class ParserWithCurrentLineVariants(Generic[PARSE_RESULT], Parser[PARSE_RESULT])
         raise NotImplementedError('abstract method')
 
 
-class ParserFromSimpleParser(Parser[PARSE_RESULT]):
-    def __init__(self,
-                 simple: Parser[T],
-                 transformer: Callable[[T], PARSE_RESULT],
-                 consume_last_line_if_is_at_eol_after_parse: bool = True):
-        super().__init__(consume_last_line_if_is_at_eol_after_parse)
-        self._simple = simple
-        self._transformer = transformer
-
-    def parse_from_token_parser(self, parser: TokenParser) -> PARSE_RESULT:
-        return self._transformer(self._simple.parse_from_token_parser(parser))
-
-
 class ParserFromTokenParserFunction(Parser[PARSE_RESULT]):
     def __init__(self,
                  parser_function: Callable[[TokenParser], PARSE_RESULT],
