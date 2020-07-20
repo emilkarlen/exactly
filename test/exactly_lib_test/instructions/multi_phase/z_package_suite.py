4e1f0c6884f86f2fd1a25dcff8eee16eb689c282
Emil Karlén <emil.karlen@fripost.org>
2020-07-20 13:41:08 +0200

    instruction/run: MOVE tests into separate package

--- test/exactly_lib_test/instructions/multi_phase/z_package_suite.py
+++ test/exactly_lib_test/instructions/multi_phase/z_package_suite.py
@@ -2,10 +2,10 @@ import unittest
 
 from exactly_lib_test.instructions.multi_phase import \
     new_dir, change_dir, \
-    run, run_tests_of_instruction_embryo, \
     env, shell
 from exactly_lib_test.instructions.multi_phase.define_symbol import z_package_suite as define_symbol
 from exactly_lib_test.instructions.multi_phase.new_file import z_package_suite as new_file
+from exactly_lib_test.instructions.multi_phase.run_program import z_package_suite as run_program
 from exactly_lib_test.instructions.multi_phase.test_resources_test import z_package_suite as test_resources_test
 
 
@@ -15,8 +15,7 @@ def suite() -> unittest.TestSuite:
         new_dir.suite(),
         change_dir.suite(),
         new_file.suite(),
-        run.suite(),
-        run_tests_of_instruction_embryo.suite(),
+        run_program.suite(),
         env.suite(),
         shell.suite(),
         define_symbol.suite(),
