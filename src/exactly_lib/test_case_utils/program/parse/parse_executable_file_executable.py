e5f9f2167f9b8e9672f4dd31d675a9ae98d2f58d
Emil Karlén <emil.karlen@fripost.org>
2020-07-25 23:26:04 +0200

    TokenParserExtra: REFACT, REMOVE use of TokenParserExtra

--- src/exactly_lib/test_case_utils/program/parse/parse_executable_file_executable.py
+++ src/exactly_lib/test_case_utils/program/parse/parse_executable_file_executable.py
@@ -1,12 +1,12 @@
 import sys
 
+from exactly_lib.section_document.element_parsers import token_stream_parser
 from exactly_lib.section_document.element_parsers.token_stream import TokenStream
 from exactly_lib.section_document.element_parsers.token_stream_parser import TokenParser
 from exactly_lib.section_document.parse_source import ParseSource
 from exactly_lib.symbol.data import list_sdvs, path_sdvs
 from exactly_lib.symbol.data.path_sdv import PathSdv
 from exactly_lib.test_case_utils.parse import parse_path
-from exactly_lib.test_case_utils.parse.token_parser_extra import from_parse_source
 from exactly_lib.test_case_utils.program import syntax_elements
 from exactly_lib.test_case_utils.program.executable_file import ExecutableFileWithArgsResolver
 from exactly_lib.type_system.data import paths
@@ -14,7 +14,7 @@ from exactly_lib.util.cli_syntax import option_parsing
 
 
 def parse_from_parse_source(source: ParseSource) -> ExecutableFileWithArgsResolver:
-    with from_parse_source(source) as token_parser:
+    with token_stream_parser.from_parse_source(source) as token_parser:
         return parse_from_token_parser(token_parser)
 
 
