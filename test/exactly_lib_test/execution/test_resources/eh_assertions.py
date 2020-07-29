f203cf153c73519c1500ec382ef50cecc3257325
Emil Karlén <emil.karlen@fripost.org>
2020-07-29 21:45:43 +0200

    cmd-proc-exe: eh_assertions: IMPROVE check

--- test/exactly_lib_test/execution/test_resources/eh_assertions.py
+++ test/exactly_lib_test/execution/test_resources/eh_assertions.py
@@ -1,7 +1,11 @@
 """
 ValueAssertion:s on ExitCodeOrHardError
 """
+from typing import Optional
+
 from exactly_lib.test_case.result.eh import ExitCodeOrHardError
+from exactly_lib.test_case.result.failure_details import FailureDetails
+from exactly_lib_test.test_case.result.test_resources import failure_details_assertions as asrt_failure_details
 from exactly_lib_test.test_resources.value_assertions import value_assertion as asrt
 from exactly_lib_test.test_resources.value_assertions.value_assertion import ValueAssertion
 
@@ -21,6 +25,23 @@ def is_exit_code(expected_exit_code: int) -> ValueAssertion[ExitCodeOrHardError]
     ])
 
 
-is_hard_error = asrt.OnTransformed(lambda value: value.is_hard_error,
-                                   asrt.Boolean(True,
-                                                'Status is expected to be hard error'))
+def matches_hard_error(failure_details: ValueAssertion[Optional[FailureDetails]] =
+                       asrt_failure_details.is_any_failure_message()) -> ValueAssertion[ExitCodeOrHardError]:
+    return asrt.is_instance_with__many(
+        ExitCodeOrHardError,
+        [
+            asrt.sub_component(
+                'is_hard_error',
+                ExitCodeOrHardError.is_hard_error.fget,
+                asrt.is_true,
+            ),
+            asrt.sub_component(
+                'failure details',
+                ExitCodeOrHardError.failure_details.fget,
+                asrt.is_optional_instance_with(FailureDetails, failure_details),
+            ),
+        ]
+    )
+
+
+is_hard_error = matches_hard_error()
