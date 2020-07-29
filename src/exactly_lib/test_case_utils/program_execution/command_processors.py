6c5bf69d73d02b2f7cff99da9c3f74f5b6c714ac
Emil Karlén <emil.karlen@fripost.org>
2020-07-29 12:42:14 +0200

    cmd-proc-exe: ADD CommandProcessor:s for exit code handling

--- src/exactly_lib/test_case_utils/program_execution/command_processors.py
+++ src/exactly_lib/test_case_utils/program_execution/command_processors.py
@@ -36,6 +36,20 @@ def processor_that_raises_hard_error_on_non_zero_exit_code(
     )
 
 
+def processor_that_raises_hard_error_on_non_zero_exit_code__command(
+        exit_code_agnostic_processor_w_hard_error: CommandProcessor[T],
+        get_exit_code: Callable[[T], int],
+        get_stderr: Callable[[T], Path],
+) -> CommandProcessor[T]:
+    from .impl import cmd_executor_w_exit_code_handling
+    return cmd_executor_w_exit_code_handling.Processor(
+        std_err_contents.STD_ERR_TEXT_READER,
+        get_exit_code,
+        get_stderr,
+        exit_code_agnostic_processor_w_hard_error,
+    )
+
+
 def processor_that_optionally_raises_hard_error_on_non_zero_exit_code(
         ignore_non_zero_exit_code: bool,
         os_services: OsServices,
@@ -53,6 +67,24 @@ def processor_that_optionally_raises_hard_error_on_non_zero_exit_code(
         processor_that_raises_hard_error_on_non_zero_exit_code(
             os_services,
             exe_of_executable,
+            get_exit_code,
+            get_stderr,
+        )
+    )
+
+
+def processor_that_optionally_raises_hard_error_on_non_zero_exit_code__command(
+        ignore_non_zero_exit_code: bool,
+        exit_code_agnostic_processor_w_hard_error: CommandProcessor[T],
+        get_exit_code: Callable[[T], int],
+        get_stderr: Callable[[T], Path],
+) -> CommandProcessor[T]:
+    return (
+        exit_code_agnostic_processor_w_hard_error
+        if ignore_non_zero_exit_code
+        else
+        processor_that_raises_hard_error_on_non_zero_exit_code__command(
+            exit_code_agnostic_processor_w_hard_error,
             get_exit_code,
             get_stderr,
         )
