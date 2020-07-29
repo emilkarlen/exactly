35638365012a9dab86326c0d745f9fee2c716321
Emil Karlén <emil.karlen@fripost.org>
2020-07-29 19:54:22 +0200

    cmd-proc-exe: DELETE deprecated proc executors

--- src/exactly_lib/util/process_execution/executors/read_stderr_on_error.py
+++ src/exactly_lib/util/process_execution/executors/read_stderr_on_error.py
@@ -6,8 +6,7 @@ from exactly_lib.test_case.command_executor import CommandExecutor
 from exactly_lib.test_case_utils.program_execution.command_processor import CommandProcessor
 from exactly_lib.type_system.logic.program.process_execution.command import Command
 from exactly_lib.util.file_utils.dir_file_space import DirFileSpace
-from exactly_lib.util.process_execution.execution_elements import ProcessExecutionSettings, Executable
-from exactly_lib.util.process_execution.process_executor import ProcessExecutor, ExecutableExecutor
+from exactly_lib.util.process_execution.execution_elements import ProcessExecutionSettings
 from exactly_lib.util.process_execution.result_files import ResultFile, DirWithResultFiles
 from . import store_result_in_files
 from ..process_output_files import ProcOutputFile
@@ -59,46 +58,6 @@ class ResultWithFiles(tuple):
         return self[2].path_of_std(ProcOutputFile.STDERR)
 
 
-class ExecutorThatStoresResultInFilesInDirAndReadsStderrOnNonZeroExitCode(ExecutableExecutor[ResultWithFiles]):
-    """An object must only be used for a single execution."""
-
-    def __init__(self,
-                 executor: ProcessExecutor,
-                 storage_dir: pathlib.Path,
-                 stdin: ContextManager[ProcessExecutionFile],
-                 stderr_msg_reader: TextFromFileReader,
-                 ):
-        self._executor = store_result_in_files.ExecutorThatStoresResultInFilesInDir(executor, storage_dir, stdin)
-        self._stderr_msg_reader = stderr_msg_reader
-
-    @property
-    def storage_dir(self) -> DirWithResultFiles:
-        return self._executor.storage_dir
-
-    def execute(self,
-                settings: ProcessExecutionSettings,
-                executable: Executable,
-                ) -> ResultWithFiles:
-        """
-        :return: Result has stderr contents iff exit code != 0
-        :raises ExecutionException: Either unable to execute, or execution timed out
-        """
-        result = self._executor.execute(settings, executable)
-
-        return ResultWithFiles(
-            result.exit_code,
-            self._stderr_for(result),
-            result.files,
-        )
-
-    def _stderr_for(self, result: store_result_in_files.ExitCodeAndFiles) -> Optional[str]:
-        if result.exit_code == 0:
-            return None
-
-        with result.files.path_of_result(ResultFile.STD_ERR).open('r') as f:
-            return self._stderr_msg_reader.read(f)
-
-
 class ProcessorThatStoresResultInFilesInDirAndReadsStderrOnNonZeroExitCode(CommandProcessor[ResultWithFiles]):
     """An object must only be used for a single execution."""
 
@@ -136,54 +95,6 @@ class ProcessorThatStoresResultInFilesInDirAndReadsStderrOnNonZeroExitCode(Comma
             return None
 
         with result.files.path_of_result(ResultFile.STD_ERR).open('r') as f:
-            return self._stderr_msg_reader.read(f)
-
-
-class ExecutorThatReadsStderrOnNonZeroExitCode(ExecutableExecutor[Result]):
-    """An object must only be used for a single execution."""
-
-    def __init__(self,
-                 executor: ProcessExecutor,
-                 tmp_file_space: DirFileSpace,
-                 stdin: ContextManager[ProcessExecutionFile],
-                 stderr_msg_reader: TextFromFileReader,
-                 ):
-        self._executor = executor
-        self._tmp_file_space = tmp_file_space
-        self._stdin = stdin
-        self._stderr_msg_reader = stderr_msg_reader
-
-    def execute(self,
-                settings: ProcessExecutionSettings,
-                executable: Executable,
-                ) -> Result:
-        """
-        :return: Result has stderr contents iff exit code != 0
-        :raises ExecutionException: Either unable to execute, or execution timed out
-        """
-        std_err_path = self._tmp_file_space.new_path('stderr')
-        with std_err_path.open('w') as stderr_f:
-            with self._stdin as stdin_f:
-                exit_code = self._executor.execute(
-                    executable,
-                    settings,
-                    StdFiles(
-                        stdin_f,
-                        StdOutputFiles(subprocess.DEVNULL,
-                                       stderr_f)
-                    ),
-                )
-
-        return Result(
-            exit_code,
-            self._stderr_for(exit_code, std_err_path),
-        )
-
-    def _stderr_for(self, exit_code: int, stderr_path: pathlib.Path) -> Optional[str]:
-        if exit_code == 0:
-            return None
-
-        with stderr_path.open('r') as f:
             return self._stderr_msg_reader.read(f)
 
 
--- src/exactly_lib/util/process_execution/executors/store_result_in_files.py
+++ src/exactly_lib/util/process_execution/executors/store_result_in_files.py
@@ -6,8 +6,7 @@ from exactly_lib.test_case_utils.program_execution.command_processor import Comm
 from exactly_lib.type_system.logic.program.process_execution.command import Command
 from exactly_lib.util.file_utils import ensure_file_existence
 from exactly_lib.util.file_utils.std import ProcessExecutionFile, StdFiles, StdOutputFiles
-from exactly_lib.util.process_execution.execution_elements import ProcessExecutionSettings, Executable
-from exactly_lib.util.process_execution.process_executor import ProcessExecutor, ExecutableExecutor
+from exactly_lib.util.process_execution.execution_elements import ProcessExecutionSettings
 from exactly_lib.util.process_execution.process_output_files import ProcOutputFile
 from exactly_lib.util.process_execution.result_files import ResultFile, DirWithResultFiles
 
@@ -31,61 +30,6 @@ class ExitCodeAndFiles(tuple):
     @property
     def stderr_file(self) -> pathlib.Path:
         return self[1].path_of_std(ProcOutputFile.STDERR)
-
-
-class ExecutorThatStoresResultInFilesInDir(ExecutableExecutor[ExitCodeAndFiles]):
-    """An object must only be used for a single execution."""
-
-    def __init__(self,
-                 executor: ProcessExecutor,
-                 storage_dir_created_on_demand: pathlib.Path,
-                 stdin: ContextManager[ProcessExecutionFile],
-                 ):
-        """
-        :param storage_dir_created_on_demand: Must be a directory that does not contain a file
-        with any of the process output file names, or a non-existing path,
-        that can be created as a dir.
-        """
-        self._storage_dir_created_on_demand = DirWithResultFiles(storage_dir_created_on_demand)
-        self._executor = executor
-        self._stdin = stdin
-
-    @property
-    def storage_dir(self) -> DirWithResultFiles:
-        return self._storage_dir_created_on_demand
-
-    def execute(self,
-                settings: ProcessExecutionSettings,
-                executable: Executable,
-                ) -> ExitCodeAndFiles:
-        """
-        :return: Exit code from successful execution
-        :raises ExecutionException: Either unable to execute, or execution timed out
-        """
-        storage_dir = self._storage_dir_created_on_demand
-        ensure_file_existence.ensure_directory_exists_as_a_directory__impl_error(storage_dir.directory)
-        stdout_path = storage_dir.path_of_result(ResultFile.STD_OUT)
-        stderr_path = storage_dir.path_of_result(ResultFile.STD_ERR)
-        exit_code_path = storage_dir.path_of_result(ResultFile.EXIT_CODE)
-
-        with self._stdin as f_stdin:
-            with stdout_path.open('w') as f_stdout:
-                with stderr_path.open('w') as f_stderr:
-                    exit_code = self._executor.execute(
-                        executable,
-                        settings,
-                        StdFiles(
-                            f_stdin,
-                            StdOutputFiles(
-                                f_stdout,
-                                f_stderr,
-                            ),
-                        ),
-                    )
-        with exit_code_path.open('w') as exit_code_f:
-            exit_code_f.write(str(exit_code))
-
-        return ExitCodeAndFiles(exit_code, self._storage_dir_created_on_demand)
 
 
 class ProcessorThatStoresResultInFilesInDir(CommandProcessor[ExitCodeAndFiles]):
