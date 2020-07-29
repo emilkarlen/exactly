182795525ab09a73b3c63e6aab98e02faa7501a2
Emil Karlén <emil.karlen@fripost.org>
2020-07-29 11:09:20 +0200

    cmd-proc-exe: ADD CommandExecutor

---
+++ src/exactly_lib/test_case/command_executor.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+from exactly_lib.type_system.logic.program.process_execution.command import Command
+from exactly_lib.util.file_utils.std import StdFiles
+from exactly_lib.util.process_execution.execution_elements import ProcessExecutionSettings
+
+
+class CommandExecutor(ABC):
+    """Executes a :class:`Command` (as a sub process)"""
+
+    @abstractmethod
+    def execute(self,
+                command: Command,
+                settings: ProcessExecutionSettings,
+                files: StdFiles,
+                ) -> int:
+        """
+        :raises :class:`HardErrorException`: Unable to execute.
+        :return: Exit code from execution.
+        """
