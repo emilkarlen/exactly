ce03403e0a94f18c41fdb2345a67a4bf6f5b4e66
Emil Karlén <emil.karlen@fripost.org>
2020-07-30 11:48:40 +0200

    cmd-proc-exe: CommandExecutor: ADD test impls

--- test/exactly_lib_test/test_case/test_resources/command_executors.py
+++ test/exactly_lib_test/test_case/test_resources/command_executors.py
@@ -1,42 +1,45 @@
 from exactly_lib.common.report_rendering.text_doc import TextRenderer
-from exactly_lib.test_case.actor import AtcOsProcessExecutor
+from exactly_lib.test_case.command_executor import CommandExecutor
 from exactly_lib.test_case.hard_error import HardErrorException
 from exactly_lib.type_system.logic.program.process_execution.command import Command
 from exactly_lib.util.file_utils.std import StdFiles
 from exactly_lib.util.process_execution.execution_elements import ProcessExecutionSettings
 
 
-class AtcOsProcessExecutorThatRecordsArguments(AtcOsProcessExecutor):
+class CommandExecutorThatRecordsArguments(CommandExecutor):
     def __init__(self):
         self.command = None
         self.process_execution_settings = None
 
     def execute(self,
                 command: Command,
-                std_files: StdFiles,
-                process_execution_settings: ProcessExecutionSettings) -> int:
+                settings: ProcessExecutionSettings,
+                files: StdFiles,
+                ) -> int:
         self.command = command
-        self.process_execution_settings = process_execution_settings
+        self.process_execution_settings = settings
         return 0
 
 
-class AtcOsProcessExecutorThatJustReturnsConstant(AtcOsProcessExecutor):
+class CommandExecutorThatJustReturnsConstant(CommandExecutor):
     def __init__(self, constant_return_value: int = 0):
         self.constant_return_value = constant_return_value
 
     def execute(self,
                 command: Command,
-                std_files: StdFiles,
-                process_execution_settings: ProcessExecutionSettings) -> int:
+                settings: ProcessExecutionSettings,
+                files: StdFiles,
+                ) -> int:
         return self.constant_return_value
 
 
-class AtcOsProcessExecutorThatRaisesHardError(AtcOsProcessExecutor):
+class CommandExecutorThatRaisesHardError(CommandExecutor):
     def __init__(self, err_msg: TextRenderer):
         self.err_msg = err_msg
 
     def execute(self,
                 command: Command,
-                std_files: StdFiles,
-                process_execution_settings: ProcessExecutionSettings) -> int:
+                settings: ProcessExecutionSettings,
+                files: StdFiles,
+                ) -> int:
         raise HardErrorException(self.err_msg)
